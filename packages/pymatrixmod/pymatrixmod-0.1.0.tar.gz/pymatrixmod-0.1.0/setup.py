f488d5477cacdcdc9560a5ce21c9c69d7ec194ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:30 +0000

    Add package pymatrixmod/0.1.0/pymatrixmod-0.1.0.tar.gz

---
+++ packages/pymatrixmod/pymatrixmod-0.1.0.tar.gz/pymatrixmod-0.1.0/setup.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+setup(
+	name='pymatrixmod',
+	version='0.1.0',
+	description='Package for working with matrix.',
+	author='Yuri Istomin',
+	author_email='yura.ist@icloud.com',
+	url='http://yuraist.com',
+	packages=find_packages()
+)
