1a2141fa3834507e199fccfa64193fcec258ea46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:24 +0000

    Add package work_h/0.4/work_h-0.4.tar.gz

---
+++ packages/work_h/work_h-0.4.tar.gz/work_h-0.4/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+
+setup(
+	name="work_h",
+	description="this for add math function",
+	version="0.4",
+	author="work_h",
+	author_email="jaga@gmail.com",
+	license="MIT",
+	package=["work_h"],
+	zip_safe=False
+	)
