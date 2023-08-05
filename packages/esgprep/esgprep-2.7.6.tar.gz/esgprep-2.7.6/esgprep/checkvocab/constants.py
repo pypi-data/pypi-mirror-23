976e690cd89c4d814d9fb6fadff01665714302c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:35 +0000

    Add package esgprep/2.7.6/esgprep-2.7.6.tar.gz

---
+++ packages/esgprep/esgprep-2.7.6.tar.gz/esgprep-2.7.6/esgprep/checkvocab/constants.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+    :platform: Unix
+    :synopsis: Constants used in this module.
+
+"""
+
+# Progress bar message length
+LEN_MSG = 42
+
+# Status messages
+STATUS = {0: 'ALL USED VALUES ARE PROPERLY DECLARED',
+          1: 'THERE WERE UNDECLARED VALUES USED',
+          2: 'NO USED VALUES FOUND',
+          4: 'NO DECLARED VALUES FOUND'}
