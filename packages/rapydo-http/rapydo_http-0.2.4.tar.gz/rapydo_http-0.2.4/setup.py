e52ec7c9f0930d9f371727096619300b56538da3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:24 +0000

    Add package rapydo-http/0.2.4/rapydo_http-0.2.4.tar.gz

---
+++ packages/rapydo-http/rapydo_http-0.2.4.tar.gz/rapydo_http-0.2.4/setup.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+from rapydo import __version__
+
+setup(
+    name='rapydo_http',
+    version=__version__,
+    author="Paolo D'Onorio De Meo",
+    author_email='p.donorio.de.meo@gmail.com',
+    url='https://github.com/rapydo/http-api',
+    license='MIT',
+    # flask_ext & rapydo ?
+    packages=find_packages(
+        where='.',
+        exclude=['test*', 'rapydo.OLD*']
+    ),
+    package_data={
+        'rapydo': [
+            'confs/services.yaml',
+            'swagger/*'
+        ],
+    },
+    install_requires=[
+        # various utilities
+        "attrs",
+        "better_exceptions",
+        "pyOpenSSL",
+        "PyJWT",
+        # Flask and plugins
+        "Flask",
+        "Flask-Cors",
+        "Flask-OAuthlib",
+        "Flask-RESTful",
+        "Flask-SQLAlchemy",
+        "flask-shell-ipython",
+        "flask_injector==v0.9.0",
+        "injector==0.12.0",
+        # DB drivers
+        "neomodel",
+        "psycopg2",
+        "pymodm",
+        # Swagger
+        "bravado-core",
+        "swagger-spec-validator",
+        # Rapydo framework
+        "rapydo-utils==0.4.3",
+        # TODO: complete this list
+        # from requirements in backend/requirements.txt
+    ]
+)
