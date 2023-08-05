52100ff5543a0bbe463d6971b3ba7465bdf37f25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:38 +0000

    Add package currency-cloud/1.0.0/currency_cloud-1.0.0.tar.gz

---
+++ packages/currency-cloud/currency_cloud-1.0.0.tar.gz/currency_cloud-1.0.0/src/currencycloud/__init__.py
@@ -0,0 +1,14 @@
+'''
+This is the official Python SDK for v2 of Currency Cloud's API. Additional documentation for each
+API endpoint can be found at connect.currencycloud.com. If you have any queries or you require
+support, please contact our sales team at sales@currencycloud.com.
+'''
+
+from currencycloud.client import Client
+from currencycloud.config import Config
+from currencycloud.http import Http
+
+__title__ = 'currencycloud'
+__version__ = '0.9.0'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2017 CurrencyCloud'
