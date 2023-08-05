366cf87def055276ea636c27356e39316e88dde4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:16 +0000

    Add package codebehind/1.4.9/codebehind-1.4.9.tar.gz

---
+++ packages/codebehind/codebehind-1.4.9.tar.gz/codebehind-1.4.9/setup.py
@@ -0,0 +1,41 @@
+
+import os
+from setuptools import find_packages, setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
+	README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+	name='codebehind',
+	version='1.4.9',
+	packages=find_packages(),
+	include_package_data=True,
+	license='MIT',
+	description='Collection of magics.',
+	long_description=README,
+	url='https://github.com/michaelhenry/codebehind/',
+	author='Michael Henry Pantaleon',
+	author_email='me@iamkel.net',
+	install_requires=[
+		'Django>=1.9','djangorestframework>=3.5.3',
+	],
+	classifiers=[
+		'Environment :: Web Environment',
+		'Framework :: Django',
+		'Framework :: Django :: 1.9',
+		'Intended Audience :: Developers',
+		'Operating System :: OS Independent',
+		'Programming Language :: Python',
+		'Programming Language :: Python :: 3',
+		'Programming Language :: Python :: 3.4',
+		'Programming Language :: Python :: 3.5',
+		'Topic :: Internet :: WWW/HTTP',
+		'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+	],
+)
+
+# upload using twine
+# https://pypi.python.org/pypi/twine
