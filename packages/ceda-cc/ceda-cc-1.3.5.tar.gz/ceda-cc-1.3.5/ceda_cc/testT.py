d95f38df2d9202f1bf0a5d468d7dcd6ce25654f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:20 +0000

    Add package ceda-cc/1.3.5/ceda-cc-1.3.5.tar.gz

---
+++ packages/ceda-cc/ceda-cc-1.3.5.tar.gz/ceda-cc-1.3.5/ceda_cc/testT.py
@@ -0,0 +1,12 @@
+
+
+
+import checkT
+
+ct = checkT.cfCalSupport('standard')
+
+tstval= ('19900601',1850)
+
+print ct.julianDay( 6,1, year=1990)
+
+print ct.dayOff( 1990 )
---
+++ packages/ceda-cc/ceda-cc-1.3.5.tar.gz/ceda-cc-1.3.5/ceda_cc/versionConfig.py
@@ -0,0 +1,4 @@
+
+version="1.3.5"
+versionComment="Resolved futureWarning concerning comparisons with None"
+
