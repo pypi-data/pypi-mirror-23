882328f123c593cd43c14b8587086c80625a6028
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:59 +0000

    Add package chalice/0.10.0/chalice-0.10.0.tar.gz

---
+++ packages/chalice/chalice-0.10.0.tar.gz/chalice-0.10.0/chalice/constants.py
@@ -0,0 +1,199 @@
+
+# This is the version that's written to the config file
+# on a `chalice new-project`.  It's also how chalice is able
+# to know when to warn you when changing behavior is introduced.
+CONFIG_VERSION = '2.0'
+
+
+TEMPLATE_APP = """\
+from chalice import Chalice
+
+app = Chalice(app_name='%s')
+
+
+@app.route('/')
+def index():
+    return {'hello': 'world'}
+
+
+# The view function above will return {"hello": "world"}
+# whenever you make an HTTP GET request to '/'.
+#
+# Here are a few more examples:
+#
+# @app.route('/hello/{name}')
+# def hello_name(name):
+#    # '/hello/james' -> {"hello": "james"}
+#    return {'hello': name}
+#
+# @app.route('/users', methods=['POST'])
+# def create_user():
+#     # This is the JSON body the user sent in their POST request.
+#     user_as_json = app.json_body
+#     # Suppose we had some 'db' object that we used to
+#     # read/write from our database.
+#     # user_id = db.create_user(user_as_json)
+#     return {'user_id': user_id}
+#
+# See the README documentation for more examples.
+#
+"""
+
+
+GITIGNORE = """\
+.chalice/deployments/
+.chalice/venv/
+"""
+
+DEFAULT_STAGE_NAME = 'dev'
+
+
+DEFAULT_LAMBDA_TIMEOUT = 60
+DEFAULT_LAMBDA_MEMORY_SIZE = 128
+MAX_LAMBDA_DEPLOYMENT_SIZE = 50 * (1024 ** 2)
+# This is the name of the main handler used to
+# handle API gateway requests.  This is used as a key
+# in the config module.
+DEFAULT_HANDLER_NAME = 'api_handler'
+
+
+LAMBDA_TRUST_POLICY = {
+    "Version": "2012-10-17",
+    "Statement": [{
+        "Sid": "",
+        "Effect": "Allow",
+        "Principal": {
+            "Service": "lambda.amazonaws.com"
+        },
+        "Action": "sts:AssumeRole"
+    }]
+}
+
+
+CLOUDWATCH_LOGS = {
+    "Effect": "Allow",
+    "Action": [
+        "logs:CreateLogGroup",
+        "logs:CreateLogStream",
+        "logs:PutLogEvents"
+    ],
+    "Resource": "arn:aws:logs:*:*:*"
+}
+
+
+CODEBUILD_POLICY = {
+    "Version": "2012-10-17",
+    # This is the policy straight from the console.
+    "Statement": [
+        {
+            "Action": [
+                "logs:CreateLogGroup",
+                "logs:CreateLogStream",
+                "logs:PutLogEvents"
+            ],
+            "Resource": "*",
+            "Effect": "Allow"
+        },
+        {
+            "Action": [
+                "s3:GetObject",
+                "s3:GetObjectVersion",
+                "s3:PutObject"
+            ],
+            "Resource": "arn:aws:s3:::*",
+            "Effect": "Allow"
+        }
+    ]
+}
+
+CODEPIPELINE_POLICY = {
+    "Version": "2012-10-17",
+    # Also straight from the console setup.
+    "Statement": [
+        {
+            "Action": [
+                "s3:GetObject",
+                "s3:GetObjectVersion",
+                "s3:GetBucketVersioning",
+                "s3:CreateBucket",
+                "s3:PutObject",
+                "s3:PutBucketVersioning"
+            ],
+            "Resource": "*",
+            "Effect": "Allow"
+        },
+        {
+            "Action": [
+                "codecommit:CancelUploadArchive",
+                "codecommit:GetBranch",
+                "codecommit:GetCommit",
+                "codecommit:GetUploadArchiveStatus",
+                "codecommit:UploadArchive"
+            ],
+            "Resource": "*",
+            "Effect": "Allow"
+        },
+        {
+            "Action": [
+                "cloudwatch:*",
+                "iam:PassRole"
+            ],
+            "Resource": "*",
+            "Effect": "Allow"
+        },
+        {
+            "Action": [
+                "lambda:InvokeFunction",
+                "lambda:ListFunctions"
+            ],
+            "Resource": "*",
+            "Effect": "Allow"
+        },
+        {
+            "Action": [
+                "cloudformation:CreateStack",
+                "cloudformation:DeleteStack",
+                "cloudformation:DescribeStacks",
+                "cloudformation:UpdateStack",
+                "cloudformation:CreateChangeSet",
+                "cloudformation:DeleteChangeSet",
+                "cloudformation:DescribeChangeSet",
+                "cloudformation:ExecuteChangeSet",
+                "cloudformation:SetStackPolicy",
+                "cloudformation:ValidateTemplate",
+                "iam:PassRole"
+            ],
+            "Resource": "*",
+            "Effect": "Allow"
+        },
+        {
+            "Action": [
+                "codebuild:BatchGetBuilds",
+                "codebuild:StartBuild"
+            ],
+            "Resource": "*",
+            "Effect": "Allow"
+        }
+    ]
+}
+
+
+WELCOME_PROMPT = r"""
+
+   ___  _  _    _    _     ___  ___  ___
+  / __|| || |  /_\  | |   |_ _|/ __|| __|
+ | (__ | __ | / _ \ | |__  | || (__ | _|
+  \___||_||_|/_/ \_\|____||___|\___||___|
+
+
+The python serverless microframework for AWS allows
+you to quickly create and deploy applications using
+Amazon API Gateway and AWS Lambda.
+
+Right now it's under developer preview and we're looking
+for customer feedback.
+
+If you'd like to proceded, then answer the questions
+below.
+
+Please enter the project name"""
