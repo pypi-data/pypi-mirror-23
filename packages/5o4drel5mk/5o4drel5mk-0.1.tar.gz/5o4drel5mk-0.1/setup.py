55f878337bc17878b011397e2ba2c12c21c2b2c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:52 +0000

    Add package 5o4drel5mk/0.1/5o4drel5mk-0.1.tar.gz

---
+++ packages/5o4drel5mk/5o4drel5mk-0.1.tar.gz/5o4drel5mk-0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages, setup
+
+setup(
+  name = '5o4drel5mk',
+  packages=find_packages(),
+  version = '0.1',
+  description = '',
+  author = '',
+  author_email = '',
+  url = '',
+  keywords = [],
+  classifiers = [],
+)
