0e84feeb0cf79ff543c76847cab2f937372424b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:28 +0000

    Add package i2c_lcd/0.1.0/i2c_lcd-0.1.0.tar.gz

---
+++ packages/i2c_lcd/i2c_lcd-0.1.0.tar.gz/i2c_lcd-0.1.0/docs/usage.rst
@@ -0,0 +1,9 @@
+=====
+Usage
+=====
+
+To use I2C LCD in a project::
+
+    import i2c_lcd
+    lcd = i2c_lcd.lcd()
+
---
+++ packages/i2c_lcd/i2c_lcd-0.1.0.tar.gz/i2c_lcd-0.1.0/tests/test_i2c_lcd.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+"""
+test_i2c_lcd
+----------------------------------
+
+Tests for `i2c_lcd` module.
+"""
+
+
+import sys
+import unittest
+
+from i2c_lcd import i2c_lcd
+
+
+class TestI2c_lcd(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_000_something(self):
+        pass
