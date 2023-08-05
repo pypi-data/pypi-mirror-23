3275aa13be0ceeba65b369a13a6705d7ccac3663
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:48 +0000

    Add package sc2gamedata/0.0.11/sc2gamedata-0.0.11.tar.gz

---
+++ packages/sc2gamedata/sc2gamedata-0.0.11.tar.gz/sc2gamedata-0.0.11/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name='sc2gamedata',
+  packages=['sc2gamedata'],
+  version='0.0.11',
+  description='Utility for making queries on the SC2 Game Data API',
+  author='Hugo Wainwright',
+  author_email='wainwrighthugo@gmail.com',
+  url='https://github.com/frugs/sc2api_queries',
+  download_url='https://github.com/frugs/sc2api_queries/tarball/0.0.11',
+  keywords=['ladder', 'starcraft', 'sc2', 'api', 'query', 'battle', 'net'],
+  classifiers=[],
+)
