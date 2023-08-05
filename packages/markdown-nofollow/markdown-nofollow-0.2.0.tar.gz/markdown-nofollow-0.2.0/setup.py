814903cac919fd0f61232007c75c6dd815f90e1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:52 +0000

    Add package markdown-nofollow/0.2.0/markdown-nofollow-0.2.0.tar.gz

---
+++ packages/markdown-nofollow/markdown-nofollow-0.2.0.tar.gz/markdown-nofollow-0.2.0/setup.py
@@ -0,0 +1,13 @@
+"""Installs markdown-nofollow."""
+from distutils.core import setup
+
+setup(
+    name='markdown-nofollow',
+    version='0.2.0',
+    description='Markdown Extension to add rel="nofollow" to all links.',
+    author='Dylan Brookes',
+    author_email='merelinguist@users.noreply.github.com',
+    packages=['markdown_nofollow'],
+    url='https://github.com/merelinguist/markdown-nofollow',
+    license='CC0'
+)
