5837eb0746d936840d5669b8b0c2cc6b64815ef0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:39 +0000

    Add package tripal/1.9/tripal-1.9.tar.gz

---
+++ packages/tripal/tripal-1.9.tar.gz/tripal-1.9/README.rst
@@ -0,0 +1,31 @@
+Tripal Library
+==================
+
+.. image:: https://travis-ci.org/abretaud/python-tripal.svg?branch=master
+    :target: https://travis-ci.org/abretaud/python-tripal
+
+.. image:: https://readthedocs.org/projects/python-tripal/badge/?version=latest
+    :target: http://python-tripal.readthedocs.io/en/latest/?badge=latest
+    :alt: Documentation Status
+
+A Python library for interacting with tripal_home_
+
+.. _tripal_home: http://tripal.info/
+
+History
+-------
+
+ - 1.9
+    - wait for job completion and get logs
+
+Requirements
+------------
+
+The tripal_rest_api_ module needs to be installed on your tripal instance.
+
+.. _tripal_rest_api: http://github.com/abretaud/tripal_rest_api
+
+License
+-------
+
+Available under the MIT License
---
+++ packages/tripal/tripal-1.9.tar.gz/tripal-1.9/setup.py
@@ -0,0 +1,27 @@
+# http://bugs.python.org/issue15881#msg170215
+from setuptools import setup, find_packages
+
+from os import listdir
+
+setup(
+    name="tripal",
+    version='1.9',
+    description="Tripal API library",
+    author="Anthony Bretaudeau",
+    author_email="anthony.bretaudeau@inra.fr",
+    url="https://github.com/abretaud/python-tripal",
+    install_requires=['requests>=2.4.3'],
+    packages=find_packages(),
+    license='MIT',
+    platforms="Posix; MacOS X; Windows",
+    scripts=['bin/' + f for f in listdir('bin')],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Scientific/Engineering",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.6",
+        "Programming Language :: Python :: 2.7"
+    ])
