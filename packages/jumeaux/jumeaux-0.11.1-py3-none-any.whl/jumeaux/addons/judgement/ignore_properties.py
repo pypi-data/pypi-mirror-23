3ab80dea5b1abf1ba7a16d683b4d7c11c283f8ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:19 +0000

    Add package jumeaux/0.11.1/jumeaux-0.11.1-py3-none-any.whl

---
+++ packages/jumeaux/jumeaux-0.11.1-py3-none-any.whl/jumeaux/addons/judgement/ignore_properties.py
@@ -0,0 +1,118 @@
+# -*- coding:utf-8 -*-
+
+"""For example of config
+judgement:
+  - name: jumeaux.addons.judgement.ignore_properties
+    config:
+      ignores:
+        - title: reason
+          image: https://......png
+          link: https://......
+          conditions:
+            - path: '/route'
+              changed:
+                - root['items'][0]
+                - root['unit']
+            - path: '/repositories'
+              added:
+                - root['items'][\d+]
+              removed:
+                - root['items']
+"""
+
+import logging
+import re
+
+from fn import _
+from owlmixin import OwlMixin
+from owlmixin.owlcollections import TList
+from typing import Optional, List
+
+from jumeaux.addons.judgement import JudgementExecutor
+from jumeaux.models import JudgementAddOnPayload, DiffKeys
+
+logger = logging.getLogger(__name__)
+
+
+class Condition(OwlMixin):
+    path: Optional[str]
+    added: TList[str]
+    removed: TList[str]
+    changed: TList[str]
+
+    def __init__(self, name: Optional[str]=None, path: Optional[str]=None,
+                 added: Optional[List[str]]=None, removed: Optional[List[str]]=None, changed: Optional[List[str]]=None):
+        self.name = name
+        self.path = path
+        self.added = TList(added) if added is not None else TList()
+        self.removed = TList(removed) if removed is not None else TList()
+        self.changed = TList(changed) if changed is not None else TList()
+
+
+class Ignore(OwlMixin):
+    title: str
+    conditions: TList[Condition]
+    image: Optional[str]
+    link: Optional[str]
+
+    def __init__(self, title: str, conditions: TList[Condition], image: Optional[str]=None, link: Optional[str]=None):
+        self.title = title
+        self.conditions = Condition.from_dicts(conditions)
+        self.image = image
+        self.link = link
+
+
+class Config(OwlMixin):
+    ignores: TList[Ignore]
+
+    def __init__(self, ignores):
+        self.ignores = Ignore.from_dicts(ignores)
+
+
+def exact_match(regexp: str, target: str):
+    return re.search(f'^{regexp}$', target)
+
+
+class Executor(JudgementExecutor):
+    config: Config
+
+    def __init__(self, config: dict):
+        self.config = Config.from_dict(config or {})
+
+    def exec(self, payload: JudgementAddOnPayload) -> JudgementAddOnPayload:
+        if payload.regard_as_same or payload.diff_keys is None:
+            return payload
+
+        def filter_diff_keys(diff_keys: DiffKeys, condition: Condition) -> DiffKeys:
+            if any([condition.path and not exact_match(condition.path, payload.path),
+                    condition.name and not exact_match(condition.name, payload.name)]):
+                return diff_keys
+
+            return DiffKeys.from_dict({
+                "added": diff_keys.added.reject(
+                    lambda dk: condition.added.any(lambda ig: exact_match(ig, dk))
+                ),
+                "removed": diff_keys.removed.reject(
+                    lambda dk: condition.removed.any(lambda ig: exact_match(ig, dk))
+                ),
+                "changed": diff_keys.changed.reject(
+                    lambda dk: condition.changed.any(lambda ig: exact_match(ig, dk))
+                )
+            })
+
+        filtered_diff_keys = self.config.ignores.flat_map(_.conditions).reduce(filter_diff_keys, payload.diff_keys)
+        logger.debug('-'*80)
+        logger.debug('filter_diff_keys')
+        logger.debug('-'*80)
+        logger.debug(filtered_diff_keys.to_pretty_json())
+
+        return JudgementAddOnPayload.from_dict({
+            "name": payload.name,
+            "path": payload.path,
+            "qs": payload.qs,
+            "headers": payload.headers,
+            "res_one": payload.res_one,
+            "res_other": payload.res_other,
+            "diff_keys": payload.diff_keys.to_dict(),
+            "regard_as_same": not (filtered_diff_keys.added or filtered_diff_keys.removed or filtered_diff_keys.changed)
+        })
