54f168ea6b373105da0c3a610e959bd69771dea6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:25 +0000

    Add package excise1/1.0.0/excise1-1.0.0.tar.gz

---
+++ packages/excise1/excise1-1.0.0.tar.gz/excise1-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+         name   = 'excise1',
+         version = '1.0.0',
+         py_modules = ['excise1'],
+         author = 'hfpython',
+         author_email = 'hfpython@headfirtlabs.com',
+         url = 'http://www.headfirstlabs.com',
+         description = 'A simple printer of nested lists',
+      )
