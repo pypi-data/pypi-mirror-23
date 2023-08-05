f10e7f6769c71340c3267a3a38c159e60e809b31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:09:32 +0000

    Add package applicationinsights-jupyter/0.9/applicationinsights_jupyter-0.9.tar.gz

---
+++ packages/applicationinsights-jupyter/applicationinsights_jupyter-0.9.tar.gz/applicationinsights_jupyter-0.9/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='applicationinsights_jupyter',
+    version='0.9',
+    description='Easy querying of Application Insights data for Jupyter',
+    url='http://github.com/jacobmsft/ApplicationInsights_Jupyter',
+    author='Suraj Jacob',
+    author_email='jacob.suraj@live.com',
+    license='MIT',
+    packages=['applicationinsights_jupyter'],
+    zip_safe=False)
