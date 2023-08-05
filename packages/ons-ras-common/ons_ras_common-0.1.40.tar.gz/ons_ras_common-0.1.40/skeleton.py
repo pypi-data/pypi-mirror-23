056ea2d17d9a3b0a6cec880713b8a96ae1e7ae5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:28 +0000

    Add package ons-ras-common/0.1.40/ons_ras_common-0.1.40.tar.gz

---
+++ packages/ons-ras-common/ons_ras_common-0.1.40.tar.gz/ons_ras_common-0.1.40/skeleton.py
@@ -0,0 +1,14 @@
+##############################################################################
+#                                                                            #
+#   Micros-ervices header template                                           #
+#   License: MIT                                                             #
+#   Copyright (c) 2017 Crown Copyright (Office for National Statistics)      #
+#                                                                            #
+##############################################################################
+
+
+class skeleton(object):
+
+    @staticmethod
+    def hello_world():
+        return "Hello World!"
