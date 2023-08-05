5d11574e239ddb41ce26210c10e9d678fc4a8a4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:50 +0000

    Add package robotica/0.1.2/robotica-0.1.2.tar.gz

---
+++ packages/robotica/robotica-0.1.2.tar.gz/robotica-0.1.2/tests/test_robotica.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Tests for `robotica` package."""
+
+import pytest
+
+from click.testing import CliRunner
+
+from robotica import robotica
+from robotica import cli
+
+
+def test_dummy():
+    """Sample pytest test function with the pytest fixture as an argument."""
+    assert True is not False
