86a9cfbeeb117eb00b2766b5f05496cb09d078a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:37 +0000

    Add package makecli/0.0.4/makecli-0.0.4.tar.gz

---
+++ packages/makecli/makecli-0.0.4.tar.gz/makecli-0.0.4/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages  # Always prefer setuptools over distutils
+from codecs import open  # To use a consistent encoding
+from os import path
+import subprocess
+from setuptools.command.install import install
+
+here = path.abspath(path.dirname(__file__))
+
+class MyInstall(install):
+    def run(self):
+        print("-- installing... (powered by makecli) --")
+        install.run(self)
+
+setup(
+        name = 'makecli',
+        version='0.0.4',
+        description='Tookit for generating command line interfaces',
+        long_description='README: https://github.com/qorzj/makecli',
+        url='https://github.com/qorzj/makecli',
+        author='qorzj',
+        author_email='inull@qq.com',
+        license='MIT',
+        platforms=['any'],
+
+        classifiers=[
+            ],
+        keywords='makecli cli fire',
+        packages = ['makecli'],
+        install_requires=['pyperclip'],
+
+        cmdclass={'install': MyInstall},
+        entry_points={
+            'console_scripts': [
+                'makecli = makecli.makecli:main',
+                ],
+            },
+    )
+
