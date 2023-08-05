e10e18ed2cdf564b15d92775bd6a4011905a0fc1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:36 +0000

    Add package listnestedp/1.0.0/listnestedp-1.0.0.tar.gz

---
+++ packages/listnestedp/listnestedp-1.0.0.tar.gz/listnestedp-1.0.0/setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+setup(
+
+		name	= 'listnestedp' ,
+		version = '1.0.0'	,
+		py_modules = ['listnestedp']	,
+		author		= 'ayshrwt' ,
+		author_email = 'aysh007rwt@gmail.com'	,
+		url			=  'http://www.rock_star.com' ,
+		description	=  'A simple printer of nested lists',
+
+
+
+
+
+	)
