8afc46a5157a02d36441b4ebe23a378a39699107
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:59 +0000

    Add package sed_eval/0.1.8/sed_eval-0.1.8.tar.gz

---
+++ packages/sed_eval/sed_eval-0.1.8.tar.gz/sed_eval-0.1.8/sed_eval/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+"""Top-level module for sed_eval"""
+
+# Import all submodules
+from . import sound_event
+from . import scene
+from . import io
+from . import util
+from . import metric
+from . import test
+
+__version__ = '0.1.4'
