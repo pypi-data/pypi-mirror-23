09720eaacb8c54673b4e32a851ef518b5d10b761
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:12 +0000

    Add package nesterDF/1.0.0/nesterDF-1.0.0.zip

---
+++ packages/nesterDF/nesterDF-1.0.0.zip/nesterDF-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name              = 'nesterDF',
+	version           = '1.0.0',
+	py_modules        = ['nesterDF'],
+	author            = 'WDF',
+	author_email      = 'WDF@gmail.com',
+	url               = 'http://www.headfirstlabs.com',
+	description       = 'A simple printer of nested lists',
+)
