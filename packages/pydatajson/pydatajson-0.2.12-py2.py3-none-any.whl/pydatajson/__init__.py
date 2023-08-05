3fe1f50186735d7bb0d617ac4b89804b6c85432d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:48 +0000

    Add package pydatajson/0.2.12/pydatajson-0.2.12-py2.py3-none-any.whl

---
+++ packages/pydatajson/pydatajson-0.2.12-py2.py3-none-any.whl/pydatajson/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Módulo pydatajson
+Conjunto de herramientas para validar y manipular la información presente en
+el archivo `data.json` de un Portal de Datos
+"""
+from .core import DataJson
+
+__author__ = """Datos Argentina"""
+__email__ = 'datos@modernizacion.gob.ar'
+__version__ = '0.2.12'
