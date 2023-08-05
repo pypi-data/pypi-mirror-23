f90bbb531e6302264e5943ddef6d6e615d53b6ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:25 +0000

    Add package wcpy/1.1/wcpy-1.1-py3-none-any.whl

---
+++ packages/wcpy/wcpy-1.1-py3-none-any.whl/wcpy-1.1.data/scripts/wc.py
@@ -0,0 +1,9 @@
+#!python
+
+from wcpy.main import get_argument_parser
+from wcpy.main import main
+
+if __name__ == '__main__':
+    parser = get_argument_parser()
+    args = parser.parse_args()
+    main(args)
