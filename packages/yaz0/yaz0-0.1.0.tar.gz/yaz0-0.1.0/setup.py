3e240b7709ec897919da88076bf9ee6d9ac4c8cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:24 +0000

    Add package yaz0/0.1.0/yaz0-0.1.0.tar.gz

---
+++ packages/yaz0/yaz0-0.1.0.tar.gz/yaz0-0.1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name="yaz0",
+      packages=["yaz0"],
+      version="0.1.0",
+      description="Python module for decompressing Nintendo Yaz0 data.",
+      author="Syroot",
+      author_email="dev@syroot.com",
+      license="MIT",
+      url="https://github.com/Syroot/yaz0",
+      download_url="https://github.com/Syroot/yaz0/archive/0.1.0.tar.gz")
