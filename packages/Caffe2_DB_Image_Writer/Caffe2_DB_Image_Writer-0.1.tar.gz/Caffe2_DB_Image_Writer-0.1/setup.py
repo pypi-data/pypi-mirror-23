b8d8f84838bb72dee8eeebc7ec4e3e37fca8c816
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:36 +0000

    Add package Caffe2_DB_Image_Writer/0.1/Caffe2_DB_Image_Writer-0.1.tar.gz

---
+++ packages/Caffe2_DB_Image_Writer/Caffe2_DB_Image_Writer-0.1.tar.gz/Caffe2_DB_Image_Writer-0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'Caffe2_DB_Image_Writer',
+    packages = find_packages(), 
+    scripts = ['db_caffe'],
+    version = '0.1',
+    description = 'caffe2 image writer',
+    author = 'kazami',
+    author_email = 'kazamizero@gmail.com',
+    url = 'https://github.com/Knight-X/Caffe2_IMG',
+    download_url = 'https://github.com/Knight-X/Caffe2_IMG/v1.0',
+    keywords = ['for easy create dataset'],
+    classifiers = [],
+    )
+
