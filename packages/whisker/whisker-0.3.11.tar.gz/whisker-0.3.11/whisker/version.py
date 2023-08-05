d245c7e113a2d97b08adec1d36c16592cfede7d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:46 +0000

    Add package whisker/0.3.11/whisker-0.3.11.tar.gz

---
+++ packages/whisker/whisker-0.3.11.tar.gz/whisker-0.3.11/whisker/version.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# whisker/version.py
+# Copyright (c) Rudolf Cardinal (rudolf@pobox.com).
+# See LICENSE for details.
+
+
+VERSION = '0.3.11'
+# Use semantic versioning: http://semver.org/
