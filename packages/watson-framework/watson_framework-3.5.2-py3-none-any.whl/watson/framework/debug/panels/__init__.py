a11a5cbdac237402cc174d76268702f968a40e02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:46 +0000

    Add package watson-framework/3.5.2/watson_framework-3.5.2-py3-none-any.whl

---
+++ packages/watson-framework/watson_framework-3.5.2-py3-none-any.whl/watson/framework/debug/panels/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+from watson.framework.debug.panels.application import Panel as Application
+from watson.framework.debug.panels.framework import Panel as Framework
+from watson.framework.debug.panels.profile import Panel as Profile
+from watson.framework.debug.panels.request import Panel as Request
+from watson.framework.debug.panels.logging import Panel as Logging
