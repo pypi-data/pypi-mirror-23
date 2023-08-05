22b5d5b04c6d3418ccba9656f0b34de89502e7fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:35 +0000

    Add package rlagents/0.0.25/rlagents-0.0.25.tar.gz

---
+++ packages/rlagents/rlagents-0.0.25.tar.gz/rlagents-0.0.25/rlagents/__init__.py
@@ -0,0 +1,34 @@
+import random
+from .tools import benchmark, gymwrapper, make_df
+from .mc import MCAgent
+from .ql import QLAgent
+
+__version__ = (0, 0, 25)
+__all__ = ['benchmark', '__version__', 'RandomAgent', 'gymwrapper',
+           'MCAgent', 'QLAgent', 'make_df'
+           ]
+
+
+class RandomAgent:
+    def __init__(self, actions):
+        self.actions = actions
+        self.reset()
+
+    def reset(self):
+        pass
+
+    def copy(self):
+        d = RandomAgent(self.actions)
+        return d
+
+    def get_action(self, observation):
+        return random.choice(self.actions)
+
+    def start_episode(self):
+        pass
+
+    def end_episode(self):
+        pass
+
+    def observe_reward(self, rew):
+        pass
