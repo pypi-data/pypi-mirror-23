1ca8439293f1e4b61cc73d960ca725bb66f55029
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:09:22 +0000

    Add package EoN/0.96rc3/EoN-0.96rc3.tar.gz

---
+++ packages/EoN/EoN-0.96rc3.tar.gz/EoN-0.96rc3/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='EoN',
+      packages = ['EoN'], 
+      version='0.96rc3',
+      description = 'Epidemics on Networks',
+      author = 'Joel C. Miller, Istvan Z. Kiss, and Peter Simon',
+      author_email = 'joel.c.miller.research@gmail.com',
+      url = 'https://springer-math.github.io/Mathematics-of-Epidemics-on-Networks/',
+      download_url = 'https://github.com/springer-math/Mathematics-of-Epidemics-on-Networks/archive/0.96rc3.tar.gz',
+      keywords = ['Epidemics on Networks', 'Epidemic Sonnet Works'],
+      )
