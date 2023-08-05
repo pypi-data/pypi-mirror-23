e06f4d542a580593d3218fed069527285582993c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:23 +0000

    Add package wcpy/1.0/wcpy-1.0-py3-none-any.whl

---
+++ packages/wcpy/wcpy-1.0-py3-none-any.whl/wcpy/__init__.py
@@ -0,0 +1,7 @@
+from wcpy.wc_extractor_file import WCExtractorFile
+from wcpy.wc_extractor_processor import WCExtractorProcessor, DIRECTION
+from wcpy.wc_extractor import WCExtractor
+
+__all__ = [ 'WCExtractorFile', 'WCExtractorProcessor', 'WCExtractor', 'DIRECTION', 'main' ]
+
+VERSION = "0.1"
