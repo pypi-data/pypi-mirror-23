b1d5e4c4667e4caa117f0afc761628aa979b6f65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:48 +0000

    Add package nesterhahaha/1.0.0/nesterhahaha-1.0.0.tar.gz

---
+++ packages/nesterhahaha/nesterhahaha-1.0.0.tar.gz/nesterhahaha-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+	name='nesterhahaha',
+    version='1.0.0',
+    py_modules=['nesterhahaha'],
+    author='michael',
+    author_email='michaelshu@foxmail.com',
+    description='A simple printer of nested lists',
+    )
