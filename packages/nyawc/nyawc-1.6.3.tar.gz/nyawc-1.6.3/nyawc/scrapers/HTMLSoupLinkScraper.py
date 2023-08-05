c24b174478410b142ea8ed04230dd4773635ba22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:46 +0000

    Add package nyawc/1.6.3/nyawc-1.6.3.tar.gz

---
+++ packages/nyawc/nyawc-1.6.3.tar.gz/nyawc-1.6.3/nyawc/scrapers/HTMLSoupLinkScraper.py
@@ -0,0 +1,119 @@
+# -*- coding: utf-8 -*-
+
+# MIT License
+#
+# Copyright (c) 2017 Tijme Gommers
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+from nyawc.http.Request import Request
+from nyawc.helpers.URLHelper import URLHelper
+
+class HTMLSoupLinkScraper:
+    """The HTMLSoupLinkScraper finds URLs from href attributes in HTML using BeautifulSoup.
+
+    Attributes:
+        content_types list(str): The supported content types.
+        __options (:class:`nyawc.Options`): The settins/options object.
+        __queue_item (:class:`nyawc.QueueItem`): The queue item containing the response to scrape.
+
+    """
+
+    content_types = [
+        "text/html",
+        "application/xhtml+xml"
+    ]
+
+    __options = None
+
+    __queue_item = None
+
+    def __init__(self, options, queue_item):
+        """Construct the HTMLSoupLinkScraper instance.
+
+        Args:
+            options (:class:`nyawc.Options`): The settins/options object.
+            queue_item (:class:`nyawc.QueueItem`): The queue item containing a response the scrape.
+
+        """
+
+        self.__options = options
+        self.__queue_item = queue_item
+
+    def get_requests(self):
+        """Get all the new requests that were found in the response.
+
+        Returns:
+            list(:class:`nyawc.http.Request`): A list of new requests that were found.
+
+        """
+
+        attributes = {
+            "src": True,
+            "href": True,
+            "link": True,
+            "script": True,
+            "url": True
+        }
+
+        host = self.__queue_item.request.url
+        soup = self.__queue_item.get_soup_response()
+        base_element = soup.find("base", href=True)
+        elements = soup.select("[{}]".format("],[".join(attributes.keys())))
+
+        # Always use the URL from the base element if it exists.
+        # https://www.w3schools.com/tags/tag_base.asp
+        if base_element:
+            host = base_element["href"]
+
+        found_requests = []
+
+        for element in elements:
+            for attribute in attributes.keys():
+                if not element.has_attr(attribute):
+                    continue
+
+                found_url = self.__trim_grave_accent(element[attribute])
+
+                if URLHelper.is_mailto(found_url):
+                    continue
+
+                absolute_url = URLHelper.make_absolute(host, found_url)
+                found_requests.append(Request(absolute_url))
+
+        return found_requests
+
+    def __trim_grave_accent(self, href):
+        """Trim grave accents manually (because BeautifulSoup doesn't support it).
+
+        Args:
+            href (str): The BeautifulSoup href value.
+
+        Returns:
+            str: The BeautifulSoup href value without grave accents.
+
+        """
+
+        if href.startswith("`"):
+            href = href[1:]
+
+        if href.endswith("`"):
+            href = href[:-1]
+
+        return href
---
+++ packages/nyawc/nyawc-1.6.3.tar.gz/nyawc-1.6.3/setup.py
@@ -0,0 +1,76 @@
+# -*- coding: utf-8 -*-
+
+# MIT License
+#
+# Copyright (c) 2017 Tijme Gommers
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+from setuptools import find_packages, setup
+from shutil import copyfile
+
+copyfile(".semver", "nyawc/.semver")
+
+with open("requirements.txt") as file:
+    requirements = file.read().splitlines()
+
+with open("README.rst") as file:
+    readme = file.read().replace(".svg?pypi=png.from.svg", ".png")
+
+with open(".semver") as file:
+    semver = file.read()
+
+setup(
+    version=semver,
+    name="nyawc",
+    description="A web crawler that gathers more than you can imagine.",
+    long_description=readme,
+    keywords = ["vulnerability", "bug-bounty", "security", "post", "get", "request", "crawler", "scraper", "scanner"],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Education",
+        "Intended Audience :: Information Technology",
+        "Intended Audience :: System Administrators",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Operating System :: MacOS",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 2.7",
+        "Topic :: Security"
+    ],
+    packages=find_packages(),
+    package_data={
+        "nyawc": [
+            ".semver"
+        ]
+    },
+    platforms=["any"],
+    author="Tijme Gommers",
+    author_email="tijme@finnwea.com",
+    license="MIT",
+    url="https://github.com/tijme/not-your-average-web-crawler",
+    install_requires=requirements
+)
