47010356fe5a7e8bd1eeb9c914e9465a2f6c0ab6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:11 +0000

    Add package humanity/3.0.2/humanity-3.0.2.tar.gz

---
+++ packages/humanity/humanity-3.0.2.tar.gz/humanity-3.0.2/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+from os.path import join, dirname
+import humanity
+
+
+setup(
+    name = 'humanity',
+    version = humanity.__version__,
+    description='Module for newbies, changes start index from 0 to 1',
+    
+	author='vlad1777d',
+	author_email='naumovvladislav@mail.ru',
+    
+    packages = find_packages (),
+    long_description = open ('README.md').read(),
+    
+    test_suite = 'unittest_humanity'
+)
+
