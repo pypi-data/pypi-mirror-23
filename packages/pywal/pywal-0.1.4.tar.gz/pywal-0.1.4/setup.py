9f4a5fc2b0ff446ebee05c94269b5f76991f6976
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:29 +0000

    Add package pywal/0.1.4/pywal-0.1.4.tar.gz

---
+++ packages/pywal/pywal-0.1.4.tar.gz/pywal-0.1.4/setup.py
@@ -0,0 +1,30 @@
+"""wal - setup.py"""
+from setuptools import setup
+
+
+DESC = (
+    "View the README at: https://github.com/dylanaraps/wal.py\n\n"
+    "Pypi doesn't like markdown OR rst with anchor links so "
+    "you'll have to view the documentation elsewhere.\n"
+)
+DESC = "".join(DESC)
+
+
+setup(
+    name="pywal",
+    version="0.1.4",
+    author="Dylan Araps",
+    author_email="dylan.araps@gmail.com",
+    description="🎨 Generate and change colorschemes on the fly",
+    long_description=DESC,
+    license="MIT",
+    url="https://github.com/dylanaraps/wal.py",
+    download_url="https://github.com/dylanaraps/wal.py/archive/0.1.4.tar.gz",
+    scripts=["wal"],
+    classifiers=[
+        "Environment :: X11 Applications",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 3.6",
+    ]
+)
