54ab5aae722da092c670c9a370cbf87de45cba8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:24 +0000

    Add package btcmarkets/0.0.2/btcmarkets-0.0.2.zip

---
+++ packages/btcmarkets/btcmarkets-0.0.2.zip/btcmarkets-0.0.2/setup.py
@@ -0,0 +1,13 @@
+
+from setuptools import setup
+
+setup(
+    name="btcmarkets",
+    packages=["btcmarkets"],
+    version="0.0.2",
+    description="Python wrapper for the BTCMarkets API",
+    author="Bradley McElroy",
+    author_email="bradley.mcelroy@live.com",
+    url="https://github.com/limx0/btcmarkets",
+    install_requires=['requests'],
+)
