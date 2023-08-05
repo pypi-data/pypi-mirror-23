f669f482ae29083944b96898fb7635c97959061f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:06 +0000

    Add package charguana/0.1.11/charguana-0.1.11.tar.gz

---
+++ packages/charguana/charguana-0.1.11.tar.gz/charguana-0.1.11/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+  name = 'charguana',
+  packages = ['charguana'],
+  version = '0.1.11',
+  description = 'A character vomiting library.',
+  author = 'Liling Tan',
+  license = 'MIT',
+  package_data={'charguana': ['data/perluniprops/*.txt']},
+  url = 'https://github.com/alvations/charguana',
+  download_url = 'https://github.com/alvations/charguana/tarball/0.1.11',
+  keywords = ['unicode', 'character', 'vommit', 'linguistics' ],
+  classifiers = [],
+)
