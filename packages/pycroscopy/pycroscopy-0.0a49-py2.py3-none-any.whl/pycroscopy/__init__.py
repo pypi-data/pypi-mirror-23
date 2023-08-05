f6e1e3bdef16e5e2b14e9209fddcd26bb9a895e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:52 +0000

    Add package pycroscopy/0.0a49/pycroscopy-0.0a49-py2.py3-none-any.whl

---
+++ packages/pycroscopy/pycroscopy-0.0a49-py2.py3-none-any.whl/pycroscopy/__init__.py
@@ -0,0 +1,20 @@
+# Not supporting for __all__
+from . import analysis
+from .analysis import *
+from . import io
+from .io import *
+from . import processing
+from .processing import *
+from . import viz
+from .viz import *
+
+from .__version__ import version as __version__
+from .__version__ import date as __date__
+
+# TODO: need to figure out what to do with external libs: numpy_groupies and pyqtgraph.
+
+__all__ = ['processing', 'analysis', 'io', 'viz', '__date__', '__version__']
+__all__+= io.__all__
+__all__+= processing.__all__
+__all__+= analysis.__all__
+__all__+= viz.__all__
