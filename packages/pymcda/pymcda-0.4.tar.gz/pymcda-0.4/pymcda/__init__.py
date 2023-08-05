7f57dfed948917a7cd25079d638293f14a73944b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:29 +0000

    Add package pymcda/0.4/pymcda-0.4.tar.gz

---
+++ packages/pymcda/pymcda-0.4.tar.gz/pymcda-0.4/pymcda/__init__.py
@@ -0,0 +1,15 @@
+import support
+import normalize
+import weightedsum
+import topsis
+import concordance
+import fuzzy
+import promethee
+
+try:
+	import numpy as np
+except ImportError:
+    print "numpy missing!"
+
+__version__ = '0.4'
+
---
+++ packages/pymcda/pymcda-0.4.tar.gz/pymcda-0.4/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(name='pymcda',
+      version='0.4',
+      description='mcda in pure python',
+      url='https://github.com/gmassei/pymcda',
+      author='gianluca massei',
+      author_email='g_massa@libero.it',
+      license='GPL',
+      packages=['pymcda'],
+      install_requires=['numpy'],
+      zip_safe=False)
+##TODO: pandas packages
+##http://www.scotttorborg.com/python-packaging/minimal.html
+##http://python-packaging.readthedocs.io/en/latest/minimal.html
+##python setup.py install
+
