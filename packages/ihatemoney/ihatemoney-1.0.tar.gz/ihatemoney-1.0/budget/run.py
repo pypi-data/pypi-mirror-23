a86d03ca11b049dad2769525bebd60799a120a72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:57 +0000

    Add package ihatemoney/1.0/ihatemoney-1.0.tar.gz

---
+++ packages/ihatemoney/ihatemoney-1.0.tar.gz/ihatemoney-1.0/budget/run.py
@@ -0,0 +1,102 @@
+import os
+import os.path
+import warnings
+
+from flask import Flask, g, request, session
+from flask_babel import Babel
+from flask_migrate import Migrate, upgrade, stamp
+from raven.contrib.flask import Sentry
+
+from web import main, db, mail
+from api import api
+from utils import PrefixedWSGI
+from utils import minimal_round
+
+app = Flask(__name__, instance_path='/etc/ihatemoney', instance_relative_config=True)
+
+
+def pre_alembic_db():
+    """ Checks if we are migrating from a pre-alembic ihatemoney
+    """
+    con = db.engine.connect()
+    tables_exist = db.engine.dialect.has_table(con, 'project')
+    alembic_setup = db.engine.dialect.has_table(con, 'alembic_version')
+    return tables_exist and not alembic_setup
+
+
+def configure():
+    """ A way to (re)configure the app, specially reset the settings
+    """
+    default_config_file = os.path.join(app.root_path, 'default_settings.py')
+    config_file = os.environ.get('IHATEMONEY_SETTINGS_FILE_PATH')
+
+    # Load default settings first
+    # Then load the settings from the path set in IHATEMONEY_SETTINGS_FILE_PATH var
+    # If not set, default to /etc/ihatemoney/ihatemoney.cfg
+    # If the latter doesn't exist no error is raised and the default settings are used
+    app.config.from_pyfile(default_config_file)
+    if config_file:
+        app.config.from_pyfile(config_file)
+    else:
+        app.config.from_pyfile('ihatemoney.cfg', silent=True)
+    app.wsgi_app = PrefixedWSGI(app)
+
+    # Deprecations
+    if 'DEFAULT_MAIL_SENDER' in app.config:
+        # Since flask-mail  0.8
+        warnings.warn(
+            "DEFAULT_MAIL_SENDER is deprecated in favor of MAIL_DEFAULT_SENDER"
+            +" and will be removed in further version",
+            UserWarning
+        )
+        if not 'MAIL_DEFAULT_SENDER' in app.config:
+            app.config['MAIL_DEFAULT_SENDER'] = DEFAULT_MAIL_SENDER
+
+configure()
+
+
+app.register_blueprint(main)
+app.register_blueprint(api)
+
+# custom jinja2 filters
+app.jinja_env.filters['minimal_round'] = minimal_round
+
+# db
+db.init_app(app)
+db.app = app
+
+# db migrations
+migrate = Migrate(app, db)
+migrations_path = os.path.join(app.root_path, 'migrations')
+
+if pre_alembic_db():
+    with app.app_context():
+        # fake the first migration
+        stamp(migrations_path, revision='b9a10d5d63ce')
+
+# auto-execute migrations on runtime
+with app.app_context():
+    upgrade(migrations_path)
+
+# mail
+mail.init_app(app)
+
+# translations
+babel = Babel(app)
+
+# sentry
+sentry = Sentry(app)
+
+@babel.localeselector
+def get_locale():
+    # get the lang from the session if defined, fallback on the browser "accept
+    # languages" header.
+    lang = session.get('lang', request.accept_languages.best_match(['fr', 'en']))
+    setattr(g, 'lang', lang)
+    return lang
+
+def main():
+    app.run(host="0.0.0.0", debug=True)
+
+if __name__ == '__main__':
+    main()
---
+++ packages/ihatemoney/ihatemoney-1.0.tar.gz/ihatemoney-1.0/budget/utils.py
@@ -0,0 +1,126 @@
+import base64
+import re
+import inspect
+
+from io import BytesIO, StringIO
+from jinja2 import filters
+from json import dumps
+from flask import redirect
+from werkzeug.routing import HTTPException, RoutingException
+import six
+
+import csv
+
+
+def slugify(value):
+    """Normalizes string, converts to lowercase, removes non-alpha characters,
+    and converts spaces to hyphens.
+
+    Copy/Pasted from ametaireau/pelican/utils itself took from django sources.
+    """
+    if isinstance(value, six.text_type):
+        import unicodedata
+        value = unicodedata.normalize('NFKD', value)
+        if six.PY2:
+            value = value.encode('ascii', 'ignore')
+    value = six.text_type(re.sub('[^\w\s-]', '', value).strip().lower())
+    return re.sub('[-\s]+', '-', value)
+
+class Redirect303(HTTPException, RoutingException):
+    """Raise if the map requests a redirect. This is for example the case if
+    `strict_slashes` are activated and an url that requires a trailing slash.
+
+    The attribute `new_url` contains the absolute destination url.
+    """
+    code = 303
+
+    def __init__(self, new_url):
+        RoutingException.__init__(self, new_url)
+        self.new_url = new_url
+
+    def get_response(self, environ):
+        return redirect(self.new_url, 303)
+
+
+class PrefixedWSGI(object):
+    '''
+    Wrap the application in this middleware and configure the
+    front-end server to add these headers, to let you quietly bind
+    this to a URL other than / and to an HTTP scheme that is
+    different than what is used locally.
+
+    It relies on "APPLICATION_ROOT" app setting.
+
+    Inspired from http://flask.pocoo.org/snippets/35/
+
+    :param app: the WSGI application
+    '''
+    def __init__(self, app):
+        self.app = app
+        self.wsgi_app = app.wsgi_app
+
+    def __call__(self, environ, start_response):
+        script_name = self.app.config['APPLICATION_ROOT']
+        if script_name:
+            environ['SCRIPT_NAME'] = script_name
+            path_info = environ['PATH_INFO']
+            if path_info.startswith(script_name):
+                environ['PATH_INFO'] = path_info[len(script_name):]
+
+        scheme = environ.get('HTTP_X_SCHEME', '')
+        if scheme:
+            environ['wsgi.url_scheme'] = scheme
+        return self.wsgi_app(environ, start_response)
+
+
+def minimal_round(*args, **kw):
+    """ Jinja2 filter: rounds, but display only non-zero decimals
+
+    from http://stackoverflow.com/questions/28458524/
+    """
+    # Use the original round filter, to deal with the extra arguments
+    res = filters.do_round(*args, **kw)
+    # Test if the result is equivalent to an integer and
+    # return depending on it
+    ires = int(res)
+    return (res if res != ires else ires)
+
+def list_of_dicts2json(dict_to_convert):
+    """Take a list of dictionnaries and turns it into
+    a json in-memory file
+    """
+    return BytesIO(dumps(dict_to_convert).encode('utf-8'))
+
+def list_of_dicts2csv(dict_to_convert):
+    """Take a list of dictionnaries and turns it into
+    a csv in-memory file, assume all dict have the same keys
+    """
+    # CSV writer has a different behavior in PY2 and PY3
+    # http://stackoverflow.com/a/37974772
+    try:
+        if six.PY3:
+            csv_file = StringIO()
+            # using list() for py3.4 compat. Otherwise, writerows() fails
+            # (expecting a sequence getting a view)
+            csv_data = [list(dict_to_convert[0].keys())]
+            for dic in dict_to_convert:
+                csv_data.append([dic[h] for h in dict_to_convert[0].keys()])
+        else:
+            csv_file = BytesIO()
+            csv_data = []
+            csv_data.append([key.encode('utf-8') for key in dict_to_convert[0].keys()])
+            for dic in dict_to_convert:
+                csv_data.append([dic[h].encode('utf8')
+                                 if isinstance(dic[h], unicode) else str(dic[h]).encode('utf8')
+                                 for h in dict_to_convert[0].keys()])
+    except (KeyError, IndexError):
+        csv_data = []
+    writer = csv.writer(csv_file)
+    writer.writerows(csv_data)
+    csv_file.seek(0)
+    if six.PY3:
+        csv_file = BytesIO(csv_file.getvalue().encode('utf-8'))
+    return csv_file
+
+# base64 encoding that works with both py2 and py3 and yield no warning
+base64_encode =  base64.encodestring if six.PY2 else base64.encodebytes
