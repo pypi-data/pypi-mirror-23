b285cf3d557b306a3202b8d44749fd852de78312
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:50 +0000

    Add package nxp-imu/0.0.1/nxp_imu-0.0.1-py3-none-any.whl

---
+++ packages/nxp-imu/nxp_imu-0.0.1-py3-none-any.whl/nxp_imu/__init__.py
@@ -0,0 +1,10 @@
+from .FXAS21002 import FXAS21002
+from .FXOS8700 import FXOS8700
+from .I2C import I2C
+from .IMU import IMU
+from .AHRS import AHRS
+from .version import __version__
+
+__author__ = 'Kevin J. Walchko'
+__license__ = 'MIT'
+__copyright__ = '2017 Kevin J. Walchko'
