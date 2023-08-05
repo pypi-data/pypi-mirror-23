6d4e7fa5635f2d21af13eee24d19b54252ea26d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:32 +0000

    Add package jiang/1.1.0/jiang-1.1.0.tar.gz

---
+++ packages/jiang/jiang-1.1.0.tar.gz/jiang-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+         name   = 'jiang',
+         version = '1.1.0',
+         py_modules = ['jiang'],
+         author = 'hfpython',
+         author_email = 'hfpython@headfirtlabs.com',
+         url = 'http://www.headfirstlabs.com',
+         description = 'A simple printer of nested lists',
+      )
