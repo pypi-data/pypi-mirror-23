fde5dcffa7c27cd1c9978dc4de6d07056c4a5e62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:03 +0000

    Add package topiceval/0.0.2.dev1/topiceval-0.0.2.dev1.tar.gz

---
+++ packages/topiceval/topiceval-0.0.2.dev1.tar.gz/topiceval-0.0.2.dev1/topiceval/preprocessing/params.py
@@ -0,0 +1,13 @@
+word_least_corpus_frequency = 5
+filter_n_most_frequent = 10
+word_least_document_frequency = 4
+word_highest_document_fraction = 0.5
+document_min_length = 8
+max_vocab = 2500
+
+# word_least_corpus_frequency = 3
+# filter_n_most_frequent = 5
+# word_least_document_frequency = 2
+# word_highest_document_fraction = 0.5
+# document_min_length = 5
+# max_vocab = 2500
