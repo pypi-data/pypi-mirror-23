c84963e2f84ed61bdd835ce89971c979aab36ee2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:38 +0000

    Add package hifive/1.4/hifive-1.4-cp27-none-macosx_10_10_intel.whl

---
+++ packages/hifive/hifive-1.4-cp27-none-macosx_10_10_intel.whl/hifive/version.py
@@ -0,0 +1,10 @@
+
+# THIS FILE IS GENERATED FROM HIFIVE SETUP.PY
+short_version = '1.4'
+version = '1.4'
+full_version = '1.4'
+git_revision = '40b3e101b59f8f714bc28a5f4a9cd274f5b19419'
+release = True
+
+if not release:
+    version = full_version
