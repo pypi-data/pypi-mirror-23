265e704e597b1849646c1a12588e2f42c20f47ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:42 +0000

    Add package expan/0.5.3/expan-0.5.3.tar.gz

---
+++ packages/expan/expan-0.5.3.tar.gz/expan-0.5.3/expan/core/__init__.py
@@ -0,0 +1,10 @@
+"""ExpAn core module.
+"""
+
+from __future__ import absolute_import
+
+__all__ = ["binning", "experiment", "experimentdata", "results", "statistics", "util", "version"]
+
+from expan.core.version import __version__, version
+
+print(('ExpAn core init: {}'.format(version())))
