00dbd0d1e11e48922fa111dbac174e6ddb756f03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:14 +0000

    Add package arrangelist/0.0.2/arrangelist-0.0.2.tar.gz

---
+++ packages/arrangelist/arrangelist-0.0.2.tar.gz/arrangelist-0.0.2/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'arrangelist',
+    version = '0.0.2',
+    py_modules = ['arrangelist'],
+    author = 'hfpython',
+    author_email = 'mainstring@foxmail.com',
+    url = 'http://tofuloss.com',
+    description = 'a_simple_printer_of_arrangelist_app',
+)
