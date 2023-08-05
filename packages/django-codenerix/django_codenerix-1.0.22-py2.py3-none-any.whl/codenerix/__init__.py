b0ea56504a4958ed94e5c1a506757c6f8baf2a06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:11:38 +0000

    Add package django-codenerix/1.0.22/django_codenerix-1.0.22-py2.py3-none-any.whl

---
+++ packages/django-codenerix/django_codenerix-1.0.22-py2.py3-none-any.whl/codenerix/__init__.py
@@ -0,0 +1,7 @@
+__version__ = "1.0.22"
+
+__authors__ = [
+    'Juan Miguel Taboada Godoy <juanmi@juanmitaboada.com>',
+    'Juan Soler Ruiz <soleronline@gmail.com>',
+    'Francisco Torrejon Puente <frantorrejon@gmail.com>',
+]
