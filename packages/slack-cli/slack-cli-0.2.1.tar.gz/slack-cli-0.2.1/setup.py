c493c8625c44307bd96158e7f5010223696d3430
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:14 +0000

    Add package slack-cli/0.2.1/slack-cli-0.2.1.tar.gz

---
+++ packages/slack-cli/slack-cli-0.2.1.tar.gz/slack-cli-0.2.1/setup.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import os
+from setuptools import setup
+
+def read(*paths):
+    """ read files """
+    with open(os.path.join(*paths), 'r') as filename:
+        return filename.read()
+
+def get_requirements():
+    path = os.path.join(os.path.dirname(__file__), 'requirements.txt')
+    requirements = [l.strip() for l in open(path)]
+    try:
+        import argparse# pylint: disable=unused-variable
+    except ImportError:
+        requirements.append('argparse')
+    return requirements
+
+setup(
+    name="slack-cli",
+    version="0.2.1",
+    description="Interact with Slack from the command line",
+    long_description=(read('README.rst')),
+    url="https://github.com/regisb/slack-cli",
+    install_requires=[
+        "appdirs",
+        "slacker==0.9.9",
+        "websocket-client==0.35.0",
+    ],
+    license='MIT',
+    author="Régis Behmo",
+    author_email="nospam@behmo.com",
+    packages=['slack_cli'],
+    entry_points={
+        'console_scripts': [
+            'slack-pipe=slack_cli.pipe:main',
+            'slack-run=slack_cli.run:main',
+            'slack-send=slack_cli.send:main',
+            'slack-stream=slack_cli.stream:main',
+            'slack-upload=slack_cli.upload:main',
+        ],
+    },
+    classifiers=[
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+    ]
+)
---
+++ packages/slack-cli/slack-cli-0.2.1.tar.gz/slack-cli-0.2.1/slack_cli/stream.py
@@ -0,0 +1,41 @@
+import json
+import slacker
+import websocket
+
+from . import utils
+
+
+def main():
+    parser = utils.get_parser("Stream Slack messages in real time")
+    parser.add_argument('sources', nargs='*',
+                        help="Channel, group or user name from which to tail messages. E.g: 'general random john'")
+    args, token = utils.parse_args(parser)
+    try:
+        loop(args, token)
+    except KeyboardInterrupt:
+        pass
+
+def loop(args, token):
+    source_ids = utils.get_source_ids(token, args.sources)
+    connection = get_rtm_websocket(token)
+    current_source_id = None
+    while True:
+        data = json.loads(connection.recv())
+        if data['type'] == 'hello':
+            continue
+        if data['type'] == 'message' and 'subtype' not in data:
+            source_id = data.get('channel') or data.get('group') or data.get('user')
+            if source_id not in source_ids:
+                continue
+            if len(args.sources) != 1 and current_source_id != source_id:
+                if current_source_id is not None:
+                    print("")
+                print("==> {} <==".format(source_ids[source_id]))
+                current_source_id = source_id
+            username = slacker.Users(token).info(data['user']).body['user']['name']
+            print(u"{}: {}".format(username, data['text']))
+
+def get_rtm_websocket(token):
+    slacker_api = slacker.BaseAPI(token)
+    url = slacker_api.get('rtm.start').body['url']
+    return websocket.create_connection(url)
