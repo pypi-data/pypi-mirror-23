64429573497acd05a66e9c2d6b09f7a6de877c34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:18 +0000

    Add package sve_common_tools/0.1.6.76/sve_common_tools-0.1.6.76.tar.gz

---
+++ packages/sve_common_tools/sve_common_tools-0.1.6.76.tar.gz/sve_common_tools-0.1.6.76/sve_common_tools/Sve_QualiITF.py
@@ -0,0 +1,68 @@
+import os
+
+
+class QualiITF:
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def createPkLogPath(globalInputs):
+        p =  QualiITF.getPrefixPath(globalInputs) + "Output\\PkLog_" + str(globalInputs["PkLog"]) + "\\"
+        if (os.path.isdir(p)):
+            pass
+        else:
+            os.makedirs(p)
+        return p
+
+    @staticmethod
+    def getPrefixPath(globalInputs):
+        return "T:\\_support\\" + globalInputs["Release"] + "\\" + globalInputs["ThemeName"] + "\\" + globalInputs[
+            "SuiteName"] + "\\"
+
+    @staticmethod
+    def getThemeName(globalInputs):
+        return globalInputs["ThemeName"]
+
+    @staticmethod
+    def getSuiteName(globalInputs):
+        return globalInputs["SuiteName"]
+
+    @staticmethod
+    def getTestName(globalInputs):
+        return globalInputs["TestName"]
+
+    @staticmethod
+    def getWhatifName(globalInputs):
+        return globalInputs["WhatifName"]
+
+    @staticmethod
+    def getRelease(globalInputs):
+        return globalInputs["Release"]
+
+    @staticmethod
+    def getSecret(globalInputs):
+        return globalInputs["Secret"]
+
+    @staticmethod
+    def getPkLog(globalInputs):
+        return globalInputs["PkLog"]
+
+    @staticmethod
+    def getQualiITFService(globalInputs):
+        return globalInputs["QualiITFService"]
+
+    @staticmethod
+    def getUserEmail(globalInputs):
+        return globalInputs["UserEmail"]
+
+    @staticmethod
+    def QualiITFService_Name():
+        return "QualiITF Service Instance"
+
+    @staticmethod
+    def QualiITFService_getAllParams():
+        return "getAllParams"
+
+    @staticmethod
+    def QualiITFService_getAllParamsExtended():
+        return "getAllParamsExtended"
