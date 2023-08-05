267f34b69c1e4f2134516f9e5bcf69e20fd7df8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:46 +0000

    Add package filetype/0.1.6/filetype-0.1.6.tar.gz

---
+++ packages/filetype/filetype-0.1.6.tar.gz/filetype-0.1.6/filetype/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+from .match import *  # noqa
+from .helpers import *  # noqa
+from .filetype import *  # noqa
+
+# Current package semver version
+__version__ = version = '0.1.6'
---
+++ packages/filetype/filetype-0.1.6.tar.gz/filetype-0.1.6/setup.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+# To use a consistent encoding
+
+description = ('Infer file type and MIME type of any file/buffer. '
+               'No libmagic dependency.')
+
+
+config = {
+    'name': 'filetype',
+    'description': description,
+    'author': 'Tomas Aparicio',
+    'author_email': 'tomas@aparicio.me',
+    'url': 'https://github.com/h2non/filetype.py',
+    'download_url': 'https://github.com/h2non/filetype.py/tarball/master',
+    'version': '0.1.6',
+    'license': 'MIT',
+    'packages': find_packages(exclude=['dist', 'build', 'docs', 'tests']),
+    'package_data': {
+        'filetype': ['LICENSE', '*.md'],
+    },
+    'keywords': [
+        'file',
+        'libmagic',
+        'magic',
+        'infer',
+        'numbers',
+        'magicnumbers',
+        'discovery',
+        'mime',
+        'type',
+        'kind',
+    ],
+    'classifiers': [
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: System',
+        'Topic :: System :: Filesystems',
+        'Topic :: Utilities',
+        'Operating System :: OS Independent'
+    ],
+}
+
+setup(**config)
