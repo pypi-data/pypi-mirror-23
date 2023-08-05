2136b367cb011d101278028e75b9f000a291a685
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:46 +0000

    Add package nestnainaing/1.0.0/nestnainaing-1.0.0.tar.gz

---
+++ packages/nestnainaing/nestnainaing-1.0.0.tar.gz/nestnainaing-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='nestnainaing',
+      version='1.0.0',
+      py_modules=['nest'],
+      author='nainaing',
+      author_email='1527460053@qq.com',
+      url='http://wxx.trade',
+      description='test'
+      )
