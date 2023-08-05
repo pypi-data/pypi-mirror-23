7f2f1d7f356da32675572a4440d3688bddaf2984
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:14 +0000

    Add package rexpython/0.1.1/rexpython-0.1.1.tar.gz

---
+++ packages/rexpython/rexpython-0.1.1.tar.gz/rexpython-0.1.1/README.txt
@@ -0,0 +1,11 @@
+=========
+RexPython
+=========
+
+This is python Rx implementation mimicing RxJava API as far as it can.
+
+
+Installation
+============
+pip install rexpython
+
---
+++ packages/rexpython/rexpython-0.1.1.tar.gz/rexpython-0.1.1/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+setup(
+    name='rexpython',
+    version='0.1.1',
+    author='Anton P. Linevich',
+    author_email='anton@linevich.com',
+    packages=['rexpython',],
+    scripts=[],
+    url='http://pypi.python.org/pypi/rexpython/',
+    license='LICENSE.txt',
+    description='simple Rx pipeline implementation',
+    long_description=open('README.txt').read(),
+    install_requires=[],
+)
