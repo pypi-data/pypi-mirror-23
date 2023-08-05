1114b9cb1f663ed16e10e0ded6733f3f0a82ae00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:26 +0000

    Add package icheckin/1.2.0/icheckin-1.2.0.tar.gz

---
+++ packages/icheckin/icheckin-1.2.0.tar.gz/icheckin-1.2.0/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+setup(
+	name = 'icheckin',
+	packages = ['icheckin'],
+	install_requires = [
+		'requests'
+	],
+	version = '1.2.0',
+	description = 'A console application for Sunway University\'s iCheckin',
+	author = 'Marcus Mu',
+	author_email = 'chunkhang@gmail.com',
+	license = 'UNLICENSE',
+	url = 'https://github.com/chunkhang/icheckin',
+	keywords = [
+		'icheckin', 
+		'sunway'
+	], 
+	classifiers = [
+		'Intended Audience :: End Users/Desktop',
+		'Programming Language :: Python :: 3 :: Only',
+		'Environment :: Console'
+	],
+	entry_points = {
+		'console_scripts': [
+			'icheckin=icheckin.icheckin:main'
+		]
+	}
+)
