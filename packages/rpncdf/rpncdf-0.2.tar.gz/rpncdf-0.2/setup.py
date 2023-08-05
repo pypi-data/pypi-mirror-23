5ece8062601a33ab7172e25ff1e3004cd8028779
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:53 +0000

    Add package rpncdf/0.2/rpncdf-0.2.tar.gz

---
+++ packages/rpncdf/rpncdf-0.2.tar.gz/rpncdf-0.2/setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+
+setup(name='rpncdf',
+      version='0.2',
+      description='Converts RPN standard files to NetCDF',
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3',
+          'Intended Audience :: Science/Research',
+          'Topic :: Scientific/Engineering :: Atmospheric Science'
+
+      ],
+      keywords='netcdf rpn gem wrf environmentcanada eccc ec',
+      url='http://github.com/doylejg/rpncdf',
+      author='Jonathan G. Doyle',
+      author_email='jonathan.g.doyle@gmail.com',
+      license='LGPLv3+',
+      packages=['rpncdf'],
+      install_requires=['numpy','netCDF4'],
+      entry_points="""
+      [console_scripts]
+      rpncdf = rpncdf:main
+      """,
+      include_package_data=True,
+      zip_safe=True)
