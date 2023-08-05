ad37415781c97a4ae0c155bdeedcd5f8f6c5cd94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:01 +0000

    Add package pconf/0.5.0/pconf-0.5.0.tar.gz

---
+++ packages/pconf/pconf-0.5.0.tar.gz/pconf-0.5.0/setup.py
@@ -0,0 +1,36 @@
+from os import path
+from setuptools import setup
+
+
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+def read(fname):
+    return open(path.join(path.dirname(__file__), fname)).read()
+
+
+setup(
+    name="pconf",
+    version="0.5.0",
+    author="Andras Maroy",
+    author_email="andras@maroy.hu",
+    description=("Hierarchical python configuration with files, environment variables, command-line arguments."),
+    license="MIT",
+    keywords="configuration hierarchical",
+    url="https://github.com/andrasmaroy/pconf",
+    packages=['pconf', 'tests'],
+    long_description=read('README.md'),
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+    ],
+    install_requires=['pyyaml'],
+    extras_require={
+        'test': ['pytest', 'mock'],
+    },
+)
---
+++ packages/pconf/pconf-0.5.0.tar.gz/pconf-0.5.0/tests/test_file.py
@@ -0,0 +1,88 @@
+from unittest import TestCase
+from mock import patch, mock_open, MagicMock
+from pconf.store.file import File
+import __builtin__
+
+
+TEST_FILE_PATH = 'test'
+TEST_FILE_DICT = {'file': 'result'}
+TEST_FILE_RAW = '{"file": "result"}'
+TEST_FILE_JSON = '{"file": "result"}'
+TEST_FILE_YAML = "---\n  file: result\n"
+
+
+def throw_ioerror(*args, **kwargs):
+    raise IOError('test')
+
+
+class TestFile(TestCase):
+    @patch('__builtin__.open')
+    def test_open_right_file(self, mock_open):
+        File(TEST_FILE_PATH)
+
+        mock_open.assert_called_once_with(TEST_FILE_PATH, 'r')
+
+    @patch('__builtin__.open', side_effect=throw_ioerror)
+    def test_open_nonexistent_file_returns_empty_dict(self, mock_open):
+        file_store = File(TEST_FILE_PATH)
+        result = file_store.get()
+
+        self.assertEqual(result, {})
+        self.assertIsInstance(result, dict)
+
+    @patch('__builtin__.open', side_effect=throw_ioerror)
+    def test_custom_encoding_without_parser_returns_empty_dict(self, mock_open):
+        file_store = File(TEST_FILE_PATH, encoding='foo')
+        result = file_store.get()
+
+        self.assertEqual(result, {})
+        self.assertIsInstance(result, dict)
+
+    @patch('__builtin__.open', mock_open(read_data=TEST_FILE_RAW))
+    def test_get_raw(self):
+        file_store = File(TEST_FILE_PATH)
+        result = file_store.get()
+
+        self.assertEqual(result, TEST_FILE_DICT)
+        self.assertIsInstance(result, dict)
+
+    @patch('__builtin__.open', mock_open(read_data=TEST_FILE_RAW))
+    def test_get_idempotent(self):
+        file_store = File(TEST_FILE_PATH)
+        result = file_store.get()
+
+        self.assertEqual(result, TEST_FILE_DICT)
+        self.assertIsInstance(result, dict)
+
+        result = file_store.get()
+
+        self.assertEqual(result, TEST_FILE_DICT)
+        self.assertIsInstance(result, dict)
+        __builtin__.open.assert_called_once_with(TEST_FILE_PATH, 'r')
+
+    @patch('__builtin__.open', mock_open(read_data=TEST_FILE_RAW))
+    def test_get_custom_encoding(self):
+        mock_parser = MagicMock()
+        mock_parser.return_value = TEST_FILE_DICT
+        file_store = File(TEST_FILE_PATH, encoding='custom', parser=mock_parser)
+        result = file_store.get()
+
+        mock_parser.assert_called_once_with(TEST_FILE_RAW)
+        self.assertEqual(result, TEST_FILE_DICT)
+        self.assertIsInstance(result, dict)
+
+    @patch('__builtin__.open', mock_open(read_data=TEST_FILE_JSON))
+    def test_get_json(self):
+        file_store = File(TEST_FILE_PATH, encoding='json')
+        result = file_store.get()
+
+        self.assertEqual(result, TEST_FILE_DICT)
+        self.assertIsInstance(result, dict)
+
+    @patch('__builtin__.open', mock_open(read_data=TEST_FILE_YAML))
+    def test_get_yaml(self):
+        file_store = File(TEST_FILE_PATH, encoding='yaml')
+        result = file_store.get()
+
+        self.assertEqual(result, TEST_FILE_DICT)
+        self.assertIsInstance(result, dict)
