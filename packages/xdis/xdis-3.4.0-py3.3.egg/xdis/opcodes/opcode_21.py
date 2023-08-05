a8db3f2a50db041bebef7abd0e2d2b12d5f1e98f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:12 +0000

    Add package xdis/3.4.0/xdis-3.4.0-py3.3.egg

---
+++ packages/xdis/xdis-3.4.0-py3.3.egg/xdis/opcodes/opcode_21.py
@@ -0,0 +1,38 @@
+# (C) Copyright 2017 by Rocky Bernstein
+"""
+CPython 2.1 bytecode opcodes
+
+This is similar to the opcode portion in Python 2.1's dis.py library.
+"""
+
+import xdis.opcodes.opcode_22 as opcode_22
+from xdis.opcodes.base import (
+    init_opdata, finalize_opcodes, format_extended_arg, rm_op)
+
+version = 2.1
+
+l = locals()
+init_opdata(l, opcode_22, version)
+
+# 2.1 bytecodes changes from 2.2
+rm_op(l, 'BINARY_FLOOR_DIVIDE',  26)
+rm_op(l, 'BINARY_TRUE_DIVIDE',   27)
+rm_op(l, 'INPLACE_FLOOR_DIVIDE', 28)
+rm_op(l, 'INPLACE_TRUE_DIVIDE',  29)
+rm_op(l, 'GET_ITER', 68)
+rm_op(l, 'YIELD_VALUE', 86)
+rm_op(l, 'FOR_ITER', 93)
+
+# FIXME remove (fix uncompyle6)
+def updateGlobal():
+    globals().update({'PJIF': l['opmap']['JUMP_IF_FALSE']})
+    globals().update({'PJIT': l['opmap']['JUMP_IF_TRUE']})
+    return
+
+updateGlobal()
+
+opcode_arg_fmt = {
+    'EXTENDED_ARG': format_extended_arg,
+}
+
+finalize_opcodes(l)
