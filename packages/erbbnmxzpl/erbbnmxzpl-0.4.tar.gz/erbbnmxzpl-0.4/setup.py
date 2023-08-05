114fa78176d5c780fe84d898eebf5e30a6d330ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:52 +0000

    Add package erbbnmxzpl/0.4/erbbnmxzpl-0.4.tar.gz

---
+++ packages/erbbnmxzpl/erbbnmxzpl-0.4.tar.gz/erbbnmxzpl-0.4/setup.py
@@ -0,0 +1,14 @@
+from setuptools import find_packages, setup
+
+setup(
+  name = 'erbbnmxzpl',
+  version = '0.4'
+            '',
+  description = '',
+  author = '',
+  author_email = '',
+  url = '',
+  keywords = [],
+  classifiers = [],
+  packages=find_packages()
+)
