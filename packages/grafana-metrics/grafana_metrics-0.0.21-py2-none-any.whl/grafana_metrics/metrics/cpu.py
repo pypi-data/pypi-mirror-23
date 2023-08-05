62301f92aa33827fda03fa230044f593c0e2a723
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:29 +0000

    Add package grafana-metrics/0.0.21/grafana_metrics-0.0.21-py2-none-any.whl

---
+++ packages/grafana-metrics/grafana_metrics-0.0.21-py2-none-any.whl/grafana_metrics/metrics/cpu.py
@@ -0,0 +1,29 @@
+# coding: utf-8
+from __future__ import unicode_literals
+
+from copy import copy
+
+import psutil
+from base import Metric, MetricData
+from time import sleep
+
+class CPU(Metric):
+
+    TYPE = 'cpu'
+
+    def collect(self):
+        sleep(10)
+        metrics = []
+        for cpu_num, cpu_percent in enumerate(psutil.cpu_percent(interval=1, percpu=True), start=1):
+            tags = copy(self.tags)
+            tags.update({
+                "cpu_num": cpu_num
+            })
+            metrics.append(MetricData(
+                name=self.measurement,
+                tags=tags,
+                fields={
+                    "percent": cpu_percent
+                }
+            ))
+        return metrics
