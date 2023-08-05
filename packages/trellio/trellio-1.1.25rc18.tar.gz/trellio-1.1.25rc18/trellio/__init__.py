176ab3c8ce14cd78018e40afe807a145fdc9c2d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:56 +0000

    Add package trellio/1.1.25rc18/trellio-1.1.25rc18.tar.gz

---
+++ packages/trellio/trellio-1.1.25rc18.tar.gz/trellio-1.1.25rc18/trellio/__init__.py
@@ -0,0 +1,19 @@
+__all__ = ['Host', 'TCPServiceClient', 'TCPService', 'HTTPService', 'HTTPServiceClient', 'api', 'request', 'subscribe',
+           'publish', 'xsubscribe', 'get', 'post', 'head', 'put', 'patch', 'delete', 'options', 'trace',
+           'RequestException', 'Response', 'Request', 'log', 'setup_logging', 'apideprecated',
+           'TrellioServiceException', 'TrellioServiceError', 'ConfigHandler', 'ManagementCommand',
+           'ManagementRegistry', 'InvalidCMDArguments', 'execute_from_command_line', 'Publisher', 'Subscriber']
+
+from .conf_manager import *
+from .exceptions import RequestException, TrellioServiceError, TrellioServiceException  # noqa
+from .host import Host  # noqa
+from .management import *
+from .pubsub import Publisher, Subscriber
+from .services import (TCPService, HTTPService, HTTPServiceClient, TCPServiceClient)  # noqa
+from .services import (api, request, subscribe, publish, xsubscribe, apideprecated)  # noqa
+from .services import (get, post, head, put, patch, delete, options, trace)  # noqa
+from .utils import log  # noqa
+from .utils.log import setup_logging  # noqa
+from .wrappers import Response, Request  # noqa
+
+__version__ = '1.1.25rc18'
