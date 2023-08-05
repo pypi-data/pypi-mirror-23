f83e343714c32360e114084e6618f712dd2b7e62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:01 +0000

    Add package CloudCRMSat/0.0.1/CloudCRMSat-0.0.1.tar.gz

---
+++ packages/CloudCRMSat/CloudCRMSat-0.0.1.tar.gz/CloudCRMSat-0.0.1/cloudsat/exemplo.py
@@ -0,0 +1,7 @@
+import os
+from datetime import datetime
+
+import download
+
+if __name__ == '__main__':
+    download.down_usgs_by_id('LC82270682017167LGN00','/Users/gustavojunior/Documents/','usgs.txt')
