883cb5ea34a46b7f439ba2e3c42933ca0d7fffc6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:57 +0000

    Add package django-activecollab-digger/0.3.2/django-activecollab-digger-0.3.2.tar.gz

---
+++ packages/django-activecollab-digger/django-activecollab-digger-0.3.2.tar.gz/django-activecollab-digger-0.3.2/setup.py
@@ -0,0 +1,61 @@
+"""
+A setuptools based setup module.
+
+See:
+https://packaging.python.org/en/latest/distributing.html
+https://github.com/pypa/sampleproject
+"""
+
+from codecs import open
+from os import path
+
+from setuptools import setup
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='django-activecollab-digger',
+
+    version='0.3.2',
+
+    description='Django ActiveCollab application',
+    long_description=long_description,
+
+    url='https://github.com/kingsdigitallab/django-activecollab-digger',
+
+    author='King\'s Digital Lab',
+    author_email='kdl-info@kcl.ac.uk',
+
+    license='MIT',
+
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Utilities',
+    ],
+
+    keywords='django activecollab',
+
+    packages=['activecollab_digger'],
+
+    include_package_data=True,
+
+    install_requires=['requests', 'simplejson'],
+
+    package_data={
+        'activecollab_digger': [
+            'templates/activecollab_digger/*.html'
+        ],
+    },
+)
