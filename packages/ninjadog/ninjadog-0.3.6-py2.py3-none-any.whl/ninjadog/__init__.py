bca0197aa71b1e41ae102c51b702c5242df2bb7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:35 +0000

    Add package ninjadog/0.3.6/ninjadog-0.3.6-py2.py3-none-any.whl

---
+++ packages/ninjadog/ninjadog-0.3.6-py2.py3-none-any.whl/ninjadog/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for ninjadog."""
+
+__author__ = """Stephan Fitzpatrick"""
+__email__ = 'knowsuchagency@gmail.com'
+__version__ = '0.3.6'
+
+from ninjadog.ninjadog import render
+from ninjadog.ext.pyramid import includeme
