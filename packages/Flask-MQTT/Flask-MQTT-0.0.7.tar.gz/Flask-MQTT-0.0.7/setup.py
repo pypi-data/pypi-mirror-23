f8801599049732f0b80026e37b4599b208eb7716
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:16 +0000

    Add package Flask-MQTT/0.0.7/Flask-MQTT-0.0.7.tar.gz

---
+++ packages/Flask-MQTT/Flask-MQTT-0.0.7.tar.gz/Flask-MQTT-0.0.7/setup.py
@@ -0,0 +1,35 @@
+import os
+import sys
+from setuptools import setup
+
+
+if sys.argv[-1] == 'test':
+    os.system('coverage run -m unittest discover -s tests')
+    os.system('coverage html --include flask_mqtt/*')
+    os.system('coverage report -m --include flask_mqtt/*')
+else:
+    setup(
+        name='Flask-MQTT',
+        version='0.0.7',
+        url='https://github.com/MrLeeh/Flask-MQTT',
+        license='MIT',
+        author='Stefan Lehmann',
+        author_email='stefan.st.lehmann@gmail.com',
+        description='Flask extension for the MQTT protocol',
+        packages=['flask_mqtt'],
+        platforms='any',
+        install_requires=[
+            'Flask',
+            'typing',
+            'paho-mqtt'
+        ],
+        classifiers=[
+            'Development Status :: 4 - Beta',
+            'Environment :: Web Environment',
+            'Intended Audience :: Developers',
+            'License :: OSI Approved :: MIT License',
+            'Programming Language :: Python',
+            'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+            'Topic :: Software Development :: Libraries :: Python Modules'
+        ]
+    )
