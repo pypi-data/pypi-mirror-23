907f82e0b7d3ce5f3b79ed70b2c7fa37cc41358c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:27 +0000

    Add package djangorestframework-simplejwt/1.3/djangorestframework_simplejwt-1.3-py2.py3-none-any.whl

---
+++ packages/djangorestframework-simplejwt/djangorestframework_simplejwt-1.3-py2.py3-none-any.whl/rest_framework_simplejwt/settings.py
@@ -0,0 +1,28 @@
+from __future__ import unicode_literals
+
+from datetime import timedelta
+
+from django.conf import settings
+from rest_framework.settings import APISettings
+
+USER_SETTINGS = getattr(settings, 'SIMPLE_JWT', None)
+
+DEFAULTS = {
+    'AUTH_HEADER_TYPE': 'Bearer',
+
+    'USER_ID_FIELD': 'id',
+    'PAYLOAD_ID_FIELD': 'user_id',
+
+    'TOKEN_LIFETIME': timedelta(days=1),
+    'TOKEN_REFRESH_LIFETIME': timedelta(days=7),
+
+    'SECRET_KEY': settings.SECRET_KEY,
+
+    'TOKEN_BACKEND': 'rest_framework_simplejwt.backends.TokenBackend',
+}
+
+IMPORT_STRING_SETTINGS = (
+    'TOKEN_BACKEND',
+)
+
+api_settings = APISettings(USER_SETTINGS, DEFAULTS, IMPORT_STRING_SETTINGS)
