cd8fbb0de1755128c8f6184999eda72972bdce6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:40 +0000

    Add package cnvcallertools/0.1.1/cnvcallertools-0.1.1.tar.gz

---
+++ packages/cnvcallertools/cnvcallertools-0.1.1.tar.gz/cnvcallertools-0.1.1/setup.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun 25 15:27:01 2017
+
+@author: Caiyd
+"""
+
+from setuptools import setup, find_packages
+
+
+VERSION = '0.1.1'
+
+
+setup(name='cnvcallertools',
+      version=VERSION,
+      description='command line tool kits from analysis CNVcaller result',
+      keywords='python cnvcaller commandline',
+      author='YudongCai',
+      author_email='yudongcai216@gmail.com',
+      license='MIT',
+      url='https://github.com/silvewheat/CNVcaller_tools',
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=True,
+      install_requires=[
+              'click',
+              'numpy',
+              'scipy',
+              'pandas'
+              ],
+      entry_points={
+              'console_scripts': [
+                      'cnvtk = cnvcallertools.main:main'
+                      ]
+              }
+      )
