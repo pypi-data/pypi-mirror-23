83d3481d7d309af60cb0e4d9f9ef30e42edb16e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:22 +0000

    Add package funny123/0.1/funny123-0.1.tar.gz

---
+++ packages/funny123/funny123-0.1.tar.gz/funny123-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='funny123',
+      version='0.1',
+      description='The funniest joke in the world',
+      author='Flying Circus',
+      author_email='flyingcircus@example.com',
+      license='MIT',
+      packages=['funny123'],
+      zip_safe=False)
