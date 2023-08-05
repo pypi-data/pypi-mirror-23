593f37af907ed66b24427f6cc1cd421dc90bfca0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:34 +0000

    Add package dkfileutils/1.3.0/dkfileutils-1.3.0-py2.py3-none-any.whl

---
+++ packages/dkfileutils/dkfileutils-1.3.0-py2.py3-none-any.whl/dkfileutils/pfind.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+"""CLI usage: ``pfind path filename`` will find the closest ancestor directory
+   conataining filename (used for finding syncspec.txt and config files).
+"""
+import os
+import sys
+
+
+def pfind(path, *fnames):
+    """Find the first fname in the closest ancestor directory.
+       For the purposes of this function, we are our own closest ancestor.
+    """
+
+    wd = os.path.abspath(path)
+    assert os.path.isdir(wd)
+
+    def parents():
+        """yield successive parent directories
+        """
+        parent = wd
+        yield parent
+        while 1:
+            parent, dirname = os.path.split(parent)
+            if not dirname:
+                return
+            yield parent
+
+    for d in parents():
+        curdirlist = os.listdir(d)
+        for fname in fnames:
+            if fname in curdirlist:
+                return os.path.join(d, fname)
+
+    return None
+
+
+if __name__ == "__main__":  # pragma: nocover
+    _path, filename = sys.argv[1], sys.argv[2]
+    print pfind(_path, filename)
