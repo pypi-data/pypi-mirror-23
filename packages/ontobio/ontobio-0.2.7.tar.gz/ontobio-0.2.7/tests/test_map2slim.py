1d1cf86c868df6e8b59ac0f6fe9624840017835b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:10 +0000

    Add package ontobio/0.2.7/ontobio-0.2.7.tar.gz

---
+++ packages/ontobio/ontobio-0.2.7.tar.gz/ontobio-0.2.7/tests/test_map2slim.py
@@ -0,0 +1,53 @@
+"""
+Test in-memory map2slim
+"""
+from ontobio.io.gafparser import GafParser, GpadParser
+from ontobio.assoc_factory import AssociationSetFactory
+from ontobio.ontol_factory import OntologyFactory
+import tempfile
+import logging
+
+POMBASE = "tests/resources/truncated-pombase.gaf"
+ONT = "tests/resources/go-truncated-pombase.json"
+
+SUBSET = [
+    'GO:0051169', ## nuclear transport
+    'GO:0006810', ## transport
+    'GO:0005730', ## nucleolus
+    'GO:0016604', ## nuclear body
+    'GO:0023052'  ## signaling
+
+]
+    
+def test_map2slim_gaf():
+    f = POMBASE
+    p = GafParser()
+    is_gaf = f == POMBASE
+    ont = OntologyFactory().create(ONT)
+    relations=['subClassOf', 'BFO:0000050']
+    m = ont.create_slim_mapping(subset_nodes=SUBSET, relations=relations)
+    #print(str(m))
+    assert m['GO:0071423'] == ['GO:0006810']
+    outfile = tempfile.NamedTemporaryFile(mode='w', delete=False)
+    p.map_to_subset(open(f,"r"), class_map=m, outfile=outfile)
+    for m in p.report.messages:
+        logging.debug("MESSAGE1: {}".format(m))
+    for m in p.report.messages:
+        logging.debug("MESSAGE1: {}".format(m))
+    logging.info("MESSAGES: {}".format(len(p.report.messages)))
+    p = GafParser()
+    logging.info("CLOSING: {}".format(outfile))
+    outfile.close()
+    
+    logging.info("Reading from: {}".format(outfile.name))
+    assocs = p.parse(outfile.name)
+    for m in p.report.messages:
+        logging.debug("MESSAGE2: {}".format(m))
+    assert len(assocs) > 100
+    cls_ids = set()
+    for a in assocs:
+        cid = a['object']['id']
+        assert cid in SUBSET
+        cls_ids.add(cid)
+        print(str(a))
+    print(cls_ids)
