afed92557a6b753684d90c7957cc8f51b91feacf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:10 +0000

    Add package hitchstory/0.2.4/hitchstory-0.2.4.tar.gz

---
+++ packages/hitchstory/hitchstory-0.2.4.tar.gz/hitchstory-0.2.4/hitchstory/__init__.py
@@ -0,0 +1,6 @@
+from hitchstory.story_collection import StoryCollection
+from hitchstory.engine import validate
+from hitchstory.engine import BaseEngine
+from hitchstory.engine import StorySchema
+
+__version__ = "0.2.4"
