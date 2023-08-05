6c5150220fc85327c34c4eccfd3805f86d2bc415
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:17 +0000

    Add package gleipnir/1.0.0/gleipnir-1.0.0.tar.gz

---
+++ packages/gleipnir/gleipnir-1.0.0.tar.gz/gleipnir-1.0.0/README.rst
@@ -0,0 +1,4 @@
+Gleipnir
+========
+
+*Hold Fenrir 🐺*
---
+++ packages/gleipnir/gleipnir-1.0.0.tar.gz/gleipnir-1.0.0/setup.py
@@ -0,0 +1,70 @@
+"""Packaging settings."""
+
+
+from codecs import open
+from os.path import abspath, dirname, join
+from subprocess import call
+
+from setuptools import Command, find_packages, setup
+
+from gleipnir import __version__
+
+
+this_dir = abspath(dirname(__file__))
+with open(join(this_dir, 'README.rst'), encoding='utf-8') as file:
+    long_description = file.read()
+
+
+class RunTests(Command):
+    """Run all tests."""
+    description = 'run tests'
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        """Run all tests!"""
+        errno = call(['py.test', '--cov=gleipnir', '--cov-report=term-missing'])
+        raise SystemExit(errno)
+
+
+setup(
+    name = 'gleipnir',
+    version = __version__,
+    description = 'Connecting to your AWS instances',
+    long_description = long_description,
+    url = 'https://github.com/mopinion/Gleipnir',
+    author = 'Floris Snuif',
+    author_email = 'floris@mopinion.com',
+    license = 'UNLICENSE',
+    classifiers = [
+        'Intended Audience :: Developers',
+        'Topic :: Utilities',
+        'License :: Public Domain',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+    ],
+    keywords = 'cli',
+    packages = find_packages(exclude=['docs', 'tests*']),
+    install_requires = ['docopt'],
+    extras_require = {
+        'test': ['coverage', 'pytest', 'pytest-cov'],
+    },
+    entry_points = {
+        'console_scripts': [
+            'gleipnir=gleipnir.cli:main',
+        ],
+    },
+    cmdclass = {'test': RunTests},
+)
