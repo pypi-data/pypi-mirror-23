8ac2cd88999354e33645618cad56c6b3cdfe1872
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:35 +0000

    Add package quantumworldX/0.18.14/quantumworldX-0.18.14.tar.gz

---
+++ packages/quantumworldX/quantumworldX-0.18.14.tar.gz/quantumworldX-0.18.14/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+setup(name='quantumworldX',
+      version='0.18.14',
+      description='Basic library for the QuantumWorld edX course',
+      url='https://pythonhosted.org/quantumworldX/',
+      author='Benjamin Sanchez Lengeling',
+      author_email='beangoben@gmail.com',
+      license='MIT',
+      packages=['quantumworldX'],
+      install_requires=['numpy>=1.11',
+                        'matplotlib>=1.5',
+                        'scipy>=0.12.0',
+                        'cycler'],
+      # download_url = 'https://github.com/peterldowns/mypackage/tarball/0.1',
+      keywords=['Quantum Chemistry', 'edX',
+                'Quantum Mechanics'],  # arbitrary keywords
+      classifiers=[],
+      )
