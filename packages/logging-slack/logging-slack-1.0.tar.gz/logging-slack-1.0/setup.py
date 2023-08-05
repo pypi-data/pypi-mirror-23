85119df246fee3442c3c238aaa84f0b578d1bdd1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:06:00 +0000

    Add package logging-slack/1.0/logging-slack-1.0.tar.gz

---
+++ packages/logging-slack/logging-slack-1.0.tar.gz/logging-slack-1.0/setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name="logging-slack",
+    version="1.0",
+    description="Logging handler for Slack Webhook",
+    url="https://github.com/frrakn/logging-slack",
+    author="frakn",
+    author_email="cheng.frank.chen+pypi@gmail.com",
+    license="MIT",
+    packages=find_packages(),
+    install_requires=[
+        "slackweb==1.0.5",
+    ])
