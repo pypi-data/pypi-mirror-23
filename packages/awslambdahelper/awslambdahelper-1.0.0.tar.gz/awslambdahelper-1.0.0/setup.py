45616c20f763943d9fa5a8e479e65a8a079d6f38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:48 +0000

    Add package awslambdahelper/1.0.0/awslambdahelper-1.0.0.tar.gz

---
+++ packages/awslambdahelper/awslambdahelper-1.0.0.tar.gz/awslambdahelper-1.0.0/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='awslambdahelper',
+    version='1.0.0',
+    install_requires=['boto3'],
+    setup_requires=['pytest-runner','twine'],
+    tests_require=['pytest', 'pytest-mock','moto'],
+    description='Handlers the nasty bits of AWS config rules',
+    url='http://github.com/drewsonne/awslambdahelper',
+    author='Drew J. Sonne',
+    author_email='drew.sonne@gmail.com',
+    license='GLPG',
+    packages=find_packages(),
+    zip_safe=True,
+    entry_points={
+        "console_scripts": [
+            "lambdahelper-bundle=awslambdahelper.cli:main",
+        ],
+    },
+)
