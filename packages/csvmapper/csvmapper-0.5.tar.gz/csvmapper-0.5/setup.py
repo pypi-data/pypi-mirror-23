f53970cdf145362536a49b5c84e7144390a88a9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:02 +0000

    Add package csvmapper/0.5/csvmapper-0.5.tar.gz

---
+++ packages/csvmapper/csvmapper-0.5.tar.gz/csvmapper-0.5/setup.py
@@ -0,0 +1,19 @@
+# ===========================
+#
+#  CSV-Mapper Setup
+#  Copyright (C) Samarjeet Singh
+#
+# ===========================
+
+from distutils.core import setup
+
+setup(name='csvmapper',
+      version='0.5',
+      description='CSV parser using pre-defined mapper file',
+      author='Samarjeet Singh',
+      author_email='hi@samarjeet.me',
+      url='https://github.com/samarjeet27/CSV-Mapper',
+      download_url = 'https://codeload.github.com/samarjeet27/CSV-Mapper/zip/master',
+      keywords= ['csv', 'mapper', 'csv-mapper', 'parser', 'csv-parser'],
+      packages=['csvmapper']
+     )
