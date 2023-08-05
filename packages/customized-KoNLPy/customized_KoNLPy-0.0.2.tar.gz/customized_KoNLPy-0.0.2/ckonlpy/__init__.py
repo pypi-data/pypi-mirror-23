e365e9027c2ae7b4133311e403f6f1709372813c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:12 +0000

    Add package customized-KoNLPy/0.0.2/customized_KoNLPy-0.0.2.tar.gz

---
+++ packages/customized-KoNLPy/customized_KoNLPy-0.0.2.tar.gz/customized_KoNLPy-0.0.2/ckonlpy/__init__.py
@@ -0,0 +1,11 @@
+__title__ = 'customized_KoNLPy'
+__version__ = '0.0.2'
+__author__ = 'Lovit'
+__license__ = 'GPL v3'
+__copyright__ = 'Copyright 2017 Lovit'
+
+from . import tag
+from . import custom_tag
+from .dictionary import CustomizedDictionary
+from .utils import installpath
+from .utils import loadtxt
