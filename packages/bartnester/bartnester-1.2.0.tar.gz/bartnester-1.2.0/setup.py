bc7b2d4de92cdb1b607102ab5e0e503132a86031
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:03 +0000

    Add package bartnester/1.2.0/bartnester-1.2.0.tar.gz

---
+++ packages/bartnester/bartnester-1.2.0.tar.gz/bartnester-1.2.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'bartnester',
+    version = '1.2.0',
+    py_modules = ['bartnester'],
+    author = 'bartkim0426',
+    author_email = 'bartkim0426@gmail.com',
+    url = 'https://github.com/bartkim0426',
+    description = 'simple'
+)
