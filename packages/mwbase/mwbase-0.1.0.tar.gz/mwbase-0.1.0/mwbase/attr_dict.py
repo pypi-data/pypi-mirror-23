82475390569bb4f3d7b8be9e51239befa7639428
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:01 +0000

    Add package mwbase/0.1.0/mwbase-0.1.0.tar.gz

---
+++ packages/mwbase/mwbase-0.1.0.tar.gz/mwbase-0.1.0/mwbase/attr_dict.py
@@ -0,0 +1,15 @@
+from collections import OrderedDict
+
+
+class AttrDict(OrderedDict):
+    def __init__(self, *args, **kwargs):
+        super(AttrDict, self).__init__(*args, **kwargs)
+
+    def __getattr__(self, attr):
+        if attr not in self:
+            raise AttributeError(attr)
+        else:
+            return self[attr]
+
+    def __setattr__(self, attr, value):
+        self[attr] = value
