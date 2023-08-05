1b331519ef2e268c2f57b6b0aa56adb3d4db785c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:03 +0000

    Add package osfclient/0.0.2/osfclient-0.0.2-py2.py3-none-any.whl

---
+++ packages/osfclient/osfclient-0.0.2-py2.py3-none-any.whl/osfclient/utils.py
@@ -0,0 +1,46 @@
+import os
+import six
+
+KNOWN_PROVIDERS = ['osfstorage', 'github']
+
+
+def norm_remote_path(path):
+    """Normalize `path`.
+
+    All remote paths are absolute.
+    """
+    path = os.path.normpath(path)
+    if path.startswith('/'):
+        return path[1:]
+    else:
+        return path
+
+
+def split_storage(path, default='osfstorage'):
+    """Extract storage name from file path.
+
+    If a path begins with a known storage provider the name is removed
+    from the path. Otherwise the `default` storage provider is returned
+    and the path is not modified.
+    """
+    path = norm_remote_path(path)
+
+    for provider in KNOWN_PROVIDERS:
+        if path.startswith(provider + '/'):
+            if six.PY3:
+                return path.split('/', maxsplit=1)
+            else:
+                return path.split('/', 1)
+
+    return (default, path)
+
+
+def makedirs(path, mode=511, exist_ok=False):
+    # mode 0777 is 511 in decimal
+    if six.PY3:
+        return os.makedirs(path, mode, exist_ok)
+    else:
+        if os.path.exists(path) and exist_ok:
+            return None
+        else:
+            return os.makedirs(path, mode)
