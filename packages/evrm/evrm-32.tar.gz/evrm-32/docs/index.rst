a430f5016bf02410e11fb7657dbd7e5fca60571e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:51 +0000

    Add package evrm/32/evrm-32.tar.gz

---
+++ packages/evrm/evrm-32.tar.gz/evrm-32/docs/index.rst
@@ -0,0 +1,56 @@
+.. title:: Gif toedienende artsen de cel in !!
+
+.. raw:: html
+
+    <br>
+
+Geachte Minister-President,
+
+In 2012 heb ik het Europeese Hof voor de Rechten van de Mens :ref:`aangeschreven <verzoek>` om een klacht tegen Nederland in te dienen.
+De :ref:`klacht <greffe>` betrof het afwezig zijn van verpleging in het nieuwe ambulante behandeltijdperk van de GGZ.
+De :ref:`uitspraak <uitspraak>` is niet-ontvankelijk. 
+Ik heb zowel :ref:`Koningin Beatrix <beuker>` en :ref:`Koning Willem-Alexander <beuker2>` aangeschreven over problemen met de invoering van de :ref:`(F)ACT <fact>` methodiek in Nederland.
+Het is niet mogelijk voor de Koningin, noch de Koning om verdere tussenkomst te verlenen. 
+
+Koningin Beatrix vermeld ministeriele verantwoordelijkheid, daarom went ik mij dan ook tot u, verantwoordelijk voor de zorg die aan de meest kwetsbaren in Nederland gegeven word.
+
+Er is bewijs dat antipsychotica :ref:`gif <bewijsgif>` en schadelijk voor de hersenen (1) zijn.
+
+In het belang van de patienten die niet een medicijn maar een gif krijgen toegedient dient u met de kennis dat deze medicijnen gif zijn dan ook te
+handelen, te handelen voor patienten voor wie het niet mogelijk om doormiddel van aangifte bij de politie een vergiftigende arts te doen laten stoppen.
+
+Om de overheid het Wetboek van Strafrecht ook bescherming te laten bieden aan GGZ patienten dient deze ook voor artsen te worden gehandhaaft.
+De politie zou zelf bloedspiegelwaardes moeten meten om mishandeling te bewijzen en daarmee direct een halt toe te roepen aan de mishandeling van de patient die anders voor de rest van zijn leven voortduurt.
+
+Minister-President, u dient op te treden voor de meest kwetsbaren in Nederland, zij die geen aangifte kunnen
+doen, door vervolging van mishandeling gepleegd door toediening van voor het
+leven of de gezondheid schadelijke stoffen voor de GGZ patient daadwerkelijk ook
+gestalte te doen geven.
+
+Ik verwacht dat u deze verantwoordelijkheid dan ook voortvarend ter hand neemt.
+
+Hoogachtend,
+
+.. raw:: html
+
+    <br>
+
+Bart Thate
+
+
+.. raw:: html
+
+    <br>
+
+(1) Hersenweefselverlies - http://www.ncbi.nlm.nih.gov/pmc/articles/PMC3476840/
+(2) Bij de behandeling met antipsychotica krijgt een patient door toediening van antipsychotica te maken krijgen met positieve en negatieve symptomen, symptomen van vergiftiging, onthoudingsverschijnselen en ernstige bijwerkingen, zie :ref:`symptomen`.
+
+| email is bthate@dds.nl or thatebart@gmail.com 
+| botfather on #dunkbots irc.freenode.net - :ref:`teksten <teksten>` 
+
+.. toctree::
+    :glob:
+    :hidden:
+
+    LICENSE
+    txt/*
---
+++ packages/evrm/evrm-32.tar.gz/evrm-32/setup.py
@@ -0,0 +1,122 @@
+#!/usr/bin/env python3
+#
+#
+
+import os
+import sys
+import os.path
+
+def j(*args):
+    if not args: return
+    todo = list(map(str, filter(None, args)))
+    return os.path.join(*todo)
+
+if sys.version_info.major < 3:
+    print("you need to run evrm with python3")
+    os._exit(1)
+
+try:
+    use_setuptools()
+except:
+    pass
+
+try:
+    from setuptools import setup
+except Exception as ex:
+    print(str(ex))
+    os._exit(1)
+
+target = "evrm"
+upload = []
+
+def uploadfiles(dir):
+    upl = []
+    if not os.path.isdir(dir):
+        print("%s does not exist" % dir)
+        os._exit(1)
+    for file in os.listdir(dir):
+        if not file or file.startswith('.'):
+            continue
+        d = dir + os.sep + file
+        if not os.path.isdir(d):
+            if file.endswith(".pyc") or file.startswith("__pycache"):
+                continue
+            upl.append(d)
+    return upl
+
+def uploadlist(dir):
+    upl = []
+
+    for file in os.listdir(dir):
+        if not file or file.startswith('.'):
+            continue
+        d = dir + os.sep + file
+        if os.path.isdir(d):   
+            upl.extend(uploadlist(d))
+        else:
+            if file.endswith(".pyc") or file.startswith("__pycache"):
+                continue
+            upl.append(d)
+
+    return upl
+
+setup(
+    name='evrm',
+    version='32',
+    url='https://bitbucket.org/thatebart/evrm',
+    author='Bart Thate',
+    author_email='bthate@dds.nl',
+    description="Gif toedienende artsen de cel in !!".upper(),
+    license='MIT',
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=["botlib"],
+    scripts=["bin/evrm"],
+    packages=['evrm', ],
+    long_description='''
+
+Geachte Minister-President,
+
+In 2012 heb ik het Europeese Hof voor de Rechten van de Mens aangeschreven om een klacht tegen Nederland in te dienen.
+De klacht betrof het afwezig zijn van verpleging in het nieuwe ambulante behandeltijdperk van de GGZ.
+De uitspraak is niet-ontvankelijk. 
+
+Ik heb zowel Koningin Beatrix en Koning Willem-Alexander aangeschreven over problemen met de invoering van de (F)ACT methodiek in Nederland.
+Het is niet mogelijk voor de Koningin, noch de Koning om verdere tussenkomst te verlenen. 
+Koningin Beatrix ministeriele verantwoordelijkheid, daarom went ik mij dan ook tot u.
+
+Er is bewijs dat de medicijnen die in de (F)ACT methodiek gebruikt worden gif zijn.
+
+Het OM laat na om de gif toedieningen in de GGZ die vandaag de dag worden gedaan zelfstandig te vervolgen. Dit nalaten om gif toedienende artsen te vervolgen is de reden dat er in Nederland nu 300 duizend schizofrenen met gif mishandelt kunnen worden.
+Dat men gif toedient en niet een medicijn dat geen schade kan maakt dat men niet zorg verleent maar het Wetboek van Strafrecht overtreed door met die gif toedieningen mishandeling te plegen.
+Om de overheid het Wetboek van Strafrecht ook bescherming te laten bieden aan GGZ patienten dient deze ook voor artsen te worden gehandhaaft en vooral wat het toedienen van voor het leven of de gezondheid schadelijke stoffen betreft.
+
+U, als vervanger van de Koning, bent verantwoordelijk voor de zorg die aan de meest kwetsbaren in de samenleving gegeven word.
+Het is uw taak om ook het Wetboek van Strafrecht bescherming te laten bieden aan GGZ patienten en dus ook te zorgen dat het OM wel gaat vervolgen voor mishandeling gepleegd door toediening van voor het leven of de gezondheid schadelijke stoffen.
+
+Ik verzoek u om de opsporing en vervolging van gif toedienende artsen voor de patient te doen, omdat aangifte doen voor hen onmogelijk is.
+
+Hoogachtend,
+
+Bart Thate
+
+| email is bthate@dds.nl or thatebart@gmail.com 
+| botfather on #dunkbots irc.freenode.net - :ref:`teksten <teksten>` 
+
+
+
+''',
+   data_files=[("docs", ["docs/conf.py","docs/index.rst"]),
+               (j('docs', 'jpg'), uploadlist(j("docs","jpg"))),
+               (j('docs', 'txt'), uploadlist(j("docs", "txt"))),
+               (j('docs', '_templates'), uploadlist(j("docs", "_templates")))
+              ],
+   package_data={'': ["*.crt"],
+                 },
+   classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: Unix',
+        'Programming Language :: Python',
+        'Topic :: Utilities'],
+)
