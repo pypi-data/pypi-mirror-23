8b56e35426074a22263b249a1df10012fb65bd1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:21 +0000

    Add package freegames/1.5.0/freegames-1.5.0-py2.py3-none-any.whl

---
+++ packages/freegames/freegames-1.5.0-py2.py3-none-any.whl/freegames/__init__.py
@@ -0,0 +1,12 @@
+"""Free Games
+
+"""
+
+from .utils import floor, line, path, square, vector
+
+__title__ = 'freegames'
+__version__ = '1.5.0'
+__build__ = 0x010500
+__author__ = 'Grant Jenks'
+__license__ = 'Apache 2.0'
+__copyright__ = 'Copyright 2017 Grant Jenks'
