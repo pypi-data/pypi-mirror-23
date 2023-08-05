c14d4e7a5d9e5144918a43914d1f6e8f56fe0943
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:46 +0000

    Add package ansible_tools_spidy/1.2/ansible_tools_spidy-1.2.tar.gz

---
+++ packages/ansible_tools_spidy/ansible_tools_spidy-1.2.tar.gz/ansible_tools_spidy-1.2/setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+setup(
+  name = 'ansible_tools_spidy',
+  packages = ['FILE_spidy','Utilities_spidy','constrains_spidy'], # this must be the same as the name above
+  version = '1.2',
+  description = 'Store ansible playbooks in mongodb, run ansible by extracting playbooks from mongo db ; command :- /usr/local/bin/ansible_playbook & yaml_to_mongo',
+  author = 'Anish V Tharagar',
+  author_email = 'anish.tharagar@gmail.com',
+  url = 'https://github.com/anishtharagar/code_repo_public.git', # use the URL to the github repo
+  download_url = 'https://github.com/anishtharagar/code_repo_public/packaging/', # I'll explain this in a second
+  keywords = ['mongo','ansible', 'playbooks', 'automation'], # arbitrary keywords
+  install_requires = [ 'pyaml','pymongo','libmagic', 'termcolor' ],
+  classifiers = [],
+  data_files=[('/usr/local/bin',['Tools_spidy/ansible_playbook','Tools_spidy/ansible_inventory_load','Tools_spidy/ansible_playbook_load']),
+	      ('/etc',['files_repo/mongo_connect.json'])],
+  license = 'MIT',
+)
