644b3471aacf5f74926174b5d79f4466e82dad6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:24 +0000

    Add package pysharefile/0.2/pysharefile-0.2.zip

---
+++ packages/pysharefile/pysharefile-0.2.zip/pysharefile-0.2/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(name='pysharefile',
+      version='0.2',
+      description='Utility modules for working with Sharefile in python 3',
+      url='http://github.com/storborg/funniest',
+      author='Austin Nafziger',
+      author_email='anafziger@42northrx.com',
+      license='MIT',
+      packages=['pysharefile'],
+      install_requires=[
+          'http',
+          'urllib',
+      ],
+      zip_safe=False)
