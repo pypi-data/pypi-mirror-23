d1270feff0abf02f6896638888c253e62fe5242f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:54 +0000

    Add package panoply-python-sdk/1.3.3/panoply-python-sdk-1.3.3.tar.gz

---
+++ packages/panoply-python-sdk/panoply-python-sdk-1.3.3.tar.gz/panoply-python-sdk-1.3.3/panoply/datasource.py
@@ -0,0 +1,30 @@
+import events
+
+# abstract DataSource object
+class DataSource(events.Emitter):
+
+    source = None
+    options = None
+
+    # data source constructor
+    def __init__(self, source, options = {}):
+        super(DataSource, self).__init__()
+        
+        self.source = source
+        self.options = options
+
+    # log a message
+    def log(self, *msgs):
+        if "logger" in self.options:
+            self.options["logger"](msgs)
+        else:
+            print msgs
+
+    def progress(self, loaded, total, msg):
+        self.fire("progress", {
+            "loaded": loaded,
+            "total": total,
+            "msg": msg
+        })
+
+
