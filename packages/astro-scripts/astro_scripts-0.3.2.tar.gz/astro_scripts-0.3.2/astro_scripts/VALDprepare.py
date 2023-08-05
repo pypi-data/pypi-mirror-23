4d81474b1b8a91267e026d7c16527508d9c88e81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:30 +0000

    Add package astro-scripts/0.3.2/astro_scripts-0.3.2.tar.gz

---
+++ packages/astro-scripts/astro_scripts-0.3.2.tar.gz/astro_scripts-0.3.2/astro_scripts/VALDprepare.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
+
+# My imports
+import argparse
+import gzip
+import os
+
+
+def _parser():
+    parser = argparse.ArgumentParser(description='Prepare the data downloaded '
+                                     'from VALD.')
+    parser.add_argument('input', help='input compressed file', type=str)
+    parser.add_argument('-o', '--output',
+                        help='Optional output',
+                        default=False, type=str)
+    return parser.parse_args()
+
+
+def main(input, output=False):
+
+    if not os.path.isfile(input):
+        raise IOError('File: %s does not exists' % input)
+
+    fname = input.rpartition('.')[0]
+    if not output:
+        output = '%s.dat' % fname
+    oref = '%s.ref' % fname
+
+    fout = ''
+    fref = ''
+    with gzip.open(input, 'r') as lines:
+        for i, line in enumerate(lines):
+            if i < 2:
+                fout += '# %s' % line.replace("'", '')
+            else:
+                fout += line.replace("'", '')
+            if 'References' in line:
+                break
+
+    with open(output, 'w') as fo:
+        fo.write(fout)
+
+
+def runner():
+    args = _parser()
+    input, output = args.input, args.output
+    main(input, output)
+
+
+if __name__ == '__main__':
+    runner()
