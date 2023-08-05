6f46f3ee1518fab92b32b5caff19a8a9c18b5805
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:19 +0000

    Add package optoanalysis/3.3.2/optoanalysis-3.3.2.tar.gz

---
+++ packages/optoanalysis/optoanalysis-3.3.2.tar.gz/optoanalysis-3.3.2/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+import os
+
+mypackage_root_dir = os.path.dirname(__file__)
+with open(os.path.join(mypackage_root_dir, 'requirements.txt')) as requirements_file:
+    requirements = requirements_file.read().splitlines()
+
+with open(os.path.join(mypackage_root_dir, 'optoanalysis/VERSION')) as version_file:
+    version = version_file.read().strip()
+
+
+setup(name='optoanalysis',
+      version=version,
+      description='Python package with functions for data analysis',
+      author='Ashley Setter',
+      author_email='A.Setter@soton.ac.uk',
+      url="https://github.com/AshleySetter/optoanalysis",
+      download_url="https://github.com/AshleySetter/optoanalysis/archive/{}.tar.gz".format(version),
+      include_package_data=True,
+      packages=['optoanalysis',
+                'optoanalysis.LeCroy',
+                'optoanalysis.Saleae',
+                'optoanalysis.sim_data',
+                'optoanalysis.thermo',
+      ],
+      install_requires=requirements,
+)
