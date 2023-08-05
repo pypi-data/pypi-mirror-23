8914f83dcae18b70cd7432b67223acbf5dfb40ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:29 +0000

    Add package who_name/0.3/who_name-0.3.tar.gz

---
+++ packages/who_name/who_name-0.3.tar.gz/who_name-0.3/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+	name="who_name",
+	version="0.3",
+	description="just praticse",
+	author="idk",
+	author_email="idk@gmail.com",
+	packages=["who_name"]
+	)
+
+# from setuptools import setup
+
+# setup(name='who_name',
+#       version='0.2',
+#       description='The funniest joke in the world',
+#       author='Flying Circus',
+#       author_email='flyingcircus@example.com',
+#       license='MIT',
+#       packages=['who_name'],
+#       zip_safe=False)
