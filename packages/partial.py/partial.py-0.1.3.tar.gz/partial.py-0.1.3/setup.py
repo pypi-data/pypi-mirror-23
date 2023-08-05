761cc238bdfad03b4e1276f56cac1b7ce2dfa96f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:45 +0000

    Add package partial.py/0.1.3/partial.py-0.1.3.tar.gz

---
+++ packages/partial.py/partial.py-0.1.3.tar.gz/partial.py-0.1.3/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+
+with open('README.rst') as rm:
+      long_description = rm.read()
+
+
+setup_kwargs = {
+      'name': 'partial.py',
+      'version': '0.1.3',
+      'description': 'Functional Python Library',
+      'long_description': long_description,
+      'url': 'https://github.com/marpple/partial.py',
+      'author': 'Marpple',
+      'author_email': 'dev@marpple.com',
+      'license': 'MIT',
+      'packages': ['partial'],
+      'package_dir': {'partial': 'src'},
+      'classifiers': [
+        'Development Status :: 1 - Planning',
+        'Environment :: Web Environment',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ]
+}
+
+setup(**setup_kwargs)
