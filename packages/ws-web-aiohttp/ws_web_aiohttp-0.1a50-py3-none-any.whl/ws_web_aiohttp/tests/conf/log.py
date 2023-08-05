9fe1e6a756457e27d499ad61977b4cbefa7917b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:18 +0000

    Add package ws-web-aiohttp/0.1a50/ws_web_aiohttp-0.1a50-py3-none-any.whl

---
+++ packages/ws-web-aiohttp/ws_web_aiohttp-0.1a50-py3-none-any.whl/ws_web_aiohttp/tests/conf/log.py
@@ -0,0 +1,50 @@
+import os
+import modconf
+import ssl
+
+LOG_FILE = 'dev/log/ws_web_aiohttp/debug.log'
+
+try:
+    os.makedirs(os.path.dirname(LOG_FILE))
+except: pass
+
+LOGGING = {
+        'version': 1,
+        'disable_existing_loggers': False,
+        'handlers': {
+            'file': {
+                'level': 'DEBUG',
+                'class': 'logging.FileHandler',
+                'filename':LOG_FILE,
+                'formatter':'basic'
+                },
+            'console':{
+                'level':'DEBUG',
+                'class':'logging.StreamHandler',
+                'formatter': 'basic'
+                },
+            },
+        'loggers':{
+            '__main__': {
+                'handlers': ['file'],
+                'level': 'DEBUG',
+                'propagate': True,
+                },
+            'ws_sheets_server': {
+                'handlers': ['file'],
+                'level': 'DEBUG',
+                'propagate': True,
+                },
+            'ws_web_aiohttp': {
+                'handlers': ['file'],
+                'level': 'DEBUG',
+                'propagate': True,
+                },
+            },
+        'formatters': {
+            "basic":{
+                "format":"%(asctime)s %(module)12s %(levelname)s %(message)s"
+                }
+            }
+        }
+
