808fd6395b92d73f3babfad0a3683749bc2da032
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:03 +0000

    Add package ws-sheets-server/0.4.1b11/ws_sheets_server-0.4.1b11-py3-none-any.whl

---
+++ packages/ws-sheets-server/ws_sheets_server-0.4.1b11-py3-none-any.whl/ws_sheets_server/tests/check_stored.py
@@ -0,0 +1,83 @@
+import asyncio
+import pickle
+import time
+import os
+import sys
+import json
+import logging
+import logging.config
+import modconf
+import subprocess
+
+import ws_storage.impl.filesystem
+import ws_sheets
+
+class TestCheckStored():
+
+    conf_mod = 'ws_sheets_server.tests.conf.simple'
+    conf_dir = None
+    d = True
+    
+    def setUp(self):
+        self.p1 = subprocess.Popen((
+            'pipenv', 'run',
+            'ws_storage', 
+            'runserver', 
+            'ws_storage.tests.conf.simple', 
+            '-d',
+            '--conf_dir', 
+            os.path.join(os.environ['HOME'], 'config'),
+            ))
+
+        time.sleep(2)
+
+        self.loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(self.loop)
+
+    def tearDown(self):
+        self.p1.kill()
+
+        print('close loop')
+        self.loop.close()
+
+    def test(self):
+        try:
+            self.setUp()
+            self.loop.run_until_complete(self.go())
+        finally:
+            self.tearDown()
+
+    async def go(self):
+        
+        conf = modconf.import_class(self.conf_mod, 'Conf', ('DEVELOP' if self.d else 'DEPLOY',), folder=self.conf_dir)
+        conf.log_console()
+        logging.config.dictConfig(conf.LOGGING)
+
+        stor = await ws_storage.impl.client.Storage.create(self.loop, conf.STORAGE_HOST, conf.STORAGE_PORT)
+       
+        print('storage={}'.format(stor))
+
+        lst = await stor.list_files()
+        
+        print('files =',lst)
+
+        fail = False
+        for f in lst:
+            if f == '_counter':
+                print('skip', repr(f))
+                continue
+            try:
+                print('f=',f)
+                b = await stor.read_binary(f)
+
+                o = pickle.loads(b)
+
+                print(repr(o))
+                #print(dir(o))
+            except Exception as e:
+                print(e)
+                fail = True
+
+        assert (not fail)
+
+    
