c25e73b2f97e8c72a08fb4b36cceccb996de5f48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:25 +0000

    Add package hitchrun/0.2.4/hitchrun-0.2.4.tar.gz

---
+++ packages/hitchrun/hitchrun-0.2.4.tar.gz/hitchrun-0.2.4/hitchrun/__init__.py
@@ -0,0 +1,5 @@
+from hitchrun import commandline
+from hitchrun.maintenance import hitch_maintenance
+from hitchrun.decorators import expected
+
+# DIR is monkey-patched into the module - see key_file.py
