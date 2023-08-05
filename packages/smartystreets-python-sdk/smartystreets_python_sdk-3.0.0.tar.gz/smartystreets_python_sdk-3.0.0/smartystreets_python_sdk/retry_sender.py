fcea086ed09e1c08630ba3791e0f5d7bacb4dcdd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:19 +0000

    Add package smartystreets-python-sdk/3.0.0/smartystreets_python_sdk-3.0.0.tar.gz

---
+++ packages/smartystreets-python-sdk/smartystreets_python_sdk-3.0.0.tar.gz/smartystreets_python_sdk-3.0.0/smartystreets_python_sdk/retry_sender.py
@@ -0,0 +1,30 @@
+from time import sleep
+
+
+class RetrySender:
+    MAX_BACKOFF_DURATION = 10
+    STATUS_OK = 200
+
+    def __init__(self, max_retries, inner):
+        self.inner = inner
+        self.max_retries = max_retries
+
+    def send(self, request):
+        response = self.inner.send(request)
+
+        for i in range(self.max_retries):
+            if response.status_code == RetrySender.STATUS_OK:
+                break
+
+            backoff(i)
+
+            response = self.inner.send(request)
+
+        return response
+
+
+def backoff(attempt):
+    print("There was an error processing the request. Retrying in {} seconds...".format(
+        min(attempt, RetrySender.MAX_BACKOFF_DURATION)))
+    sleep(min(attempt, RetrySender.MAX_BACKOFF_DURATION))
+    return
