b564ef12c50c96039468f7aed7f656f8a2ab7607
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:09:33 +0000

    Add package constant2/0.0.3/constant2-0.0.3.zip

---
+++ packages/constant2/constant2-0.0.3.zip/constant2-0.0.3/constant2/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+try:
+    from ._constant2 import Constant
+except:
+    pass
+
+__version__ = "0.0.3"
+__short_description__ = "provide extensive way of managing your constant variable."
+__license__ = "MIT"
+__author__ = "Sanhe Hu"
