ec7d861b9a0766668e9ff087251dd1dfcba9c787
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:29 +0000

    Add package Emalier-Sendgrid/1.0/Emalier_Sendgrid-1.0.tar.gz

---
+++ packages/Emalier-Sendgrid/Emalier_Sendgrid-1.0.tar.gz/Emalier_Sendgrid-1.0/setup.py
@@ -0,0 +1,13 @@
+#from distutils.core import setup
+from setuptools import setup, find_packages
+
+setup(
+    name='Emalier_Sendgrid',
+    version='1.0',
+    packages=find_packages(),
+    url='https://github.com/marcus800/Sendgrid-emailer/',
+    license='none',
+    author='marcus',
+    author_email='lewis@pinnaclevl.com',
+    description='Sendgrid-emalier'
+)
