d38b6abbcd36f8709e4c575893cb33df96c2a256
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:40 +0000

    Add package deployv/0.9.27/deployv-0.9.27.tar.gz

---
+++ packages/deployv/deployv-0.9.27.tar.gz/deployv-0.9.27/deployv/instance/__init__.py
@@ -0,0 +1,14 @@
+
+ODOO_BINARY = {
+    '7.0': 'openerp-server',
+    '8.0': 'odoo.py',
+    '9.0': 'odoo.py',
+    '10.0': 'odoo-bin',
+    'saas-14': 'odoo-bin',
+    'saas-15': 'odoo-bin',
+}
+
+SAAS_VERSIONS = {
+    'saas-14': 'saas-14',
+    'saas-15': 'saas-15',
+}
