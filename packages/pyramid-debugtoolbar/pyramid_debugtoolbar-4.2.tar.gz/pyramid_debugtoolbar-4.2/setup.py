e7f209262429a5de153891b8a4f69055a6b958cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:57 +0000

    Add package pyramid-debugtoolbar/4.2/pyramid_debugtoolbar-4.2.tar.gz

---
+++ packages/pyramid-debugtoolbar/pyramid_debugtoolbar-4.2.tar.gz/pyramid_debugtoolbar-4.2/setup.py
@@ -0,0 +1,83 @@
+##############################################################################
+#
+# Copyright (c) 2008-2013 Agendaless Consulting and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the BSD-like license at
+# http://www.repoze.org/LICENSE.txt.  A copy of the license should accompany
+# this distribution.  THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL
+# EXPRESS OR IMPLIED WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO,
+# THE IMPLIED WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND
+# FITNESS FOR A PARTICULAR PURPOSE
+#
+##############################################################################
+
+import os
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+try:
+    README = open(os.path.join(here, 'README.rst')).read()
+    CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
+except IOError:
+    README = CHANGES = ''
+
+install_requires = [
+    'pyramid>=1.4',
+    'pyramid_mako>=0.3.1', # lazy configuration loading works
+    'repoze.lru',
+    'Pygments',
+    ]
+
+extra_requires = [
+    'ipaddress',
+    ]
+
+testing_extras = [
+    'WebTest',
+    'nose',
+    'coverage',
+    ]
+
+docs_extras = [
+    'Sphinx >= 1.3.1',  # Read The Docs minimum version
+    'pylons-sphinx-themes >= 0.3',
+    ]
+
+setup(name='pyramid_debugtoolbar',
+      version='4.2',
+      description=('A package which provides an interactive HTML debugger '
+                   'for Pyramid application development'),
+      long_description=README + '\n\n' + CHANGES,
+      classifiers=[
+        "Intended Audience :: Developers",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Framework :: Pyramid",
+        "Topic :: Internet :: WWW/HTTP :: WSGI",
+        "License :: Repoze Public License",
+        ],
+      keywords='wsgi pylons pyramid transaction',
+      author=("Chris McDonough, Michael Merickel, Casey Duncan, "
+              "Blaise Laflamme"),
+      author_email="pylons-discuss@googlegroups.com",
+      url="http://docs.pylonsproject.org/projects/pyramid-debugtoolbar/en/latest/",
+      license="BSD",
+      packages=find_packages(exclude=('tests',)),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=install_requires,
+      extras_require = {
+          ':python_version<"3.3"': extra_requires,
+          'testing':testing_extras,
+          'docs':docs_extras,
+          },
+      test_suite="tests",
+      entry_points='',
+      )
