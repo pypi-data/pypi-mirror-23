363da08b736a8599ce076bd1a544f1b3f86cfa15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:51 +0000

    Add package quantapp/0.1.2/quantapp-0.1.2.tar.gz

---
+++ packages/quantapp/quantapp-0.1.2.tar.gz/quantapp-0.1.2/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+ 
+setup(
+    name='quantapp',                                                                # This is the name of your PyPI-package.
+    description='QuantApp Libraries for Systematic Investments and Data Science',   # This is the name of your PyPI-package.
+    version='0.1.2',                                                                  # Update the version number for new releases
+    scripts=['load_libraries'],                                                      # The name of your scipt, and also the command you'll be using for calling it
+
+    url='https://www.quantapp.net',
+    author='Arturo Rodriguez',
+    author_email='arturo.rodriguez@quantapp.net',
+    license='MIT',
+    python_requires='>=3'
+)
