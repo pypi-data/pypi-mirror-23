4e205498a90a8eced107473d64a616bb249f3b96
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:00 +0000

    Add package django-netjsongraph/0.3.1/django-netjsongraph-0.3.1.tar.gz

---
+++ packages/django-netjsongraph/django-netjsongraph-0.3.1.tar.gz/django-netjsongraph-0.3.1/django_netjsongraph/tests/utils.py
@@ -0,0 +1,43 @@
+import logging
+import os
+import sys
+from contextlib import contextmanager
+
+from django.test.runner import DiscoverRunner
+
+try:
+    from cStringIO import StringIO  # noqa
+except ImportError:
+    from io import StringIO  # noqa
+
+
+@contextmanager
+def redirect_stdout(stream):
+    sys.stdout = stream
+    try:
+        yield
+    finally:
+        sys.stdout = sys.__stdout__
+
+
+class LoggingDisabledTestRunner(DiscoverRunner):
+    def run_tests(self, test_labels, extra_tests=None, **kwargs):
+        # disable logging below CRITICAL while testing
+        logging.disable(logging.CRITICAL)
+        return super(LoggingDisabledTestRunner, self).run_tests(test_labels,
+                                                                extra_tests,
+                                                                **kwargs)
+
+
+class UnpublishMixin(object):
+    def _unpublish(self):
+        from ..models import Topology
+        t = Topology.objects.first()
+        t.published = False
+        t.save()
+
+
+class LoadMixin(object):
+    def _load(self, file):
+        d = os.path.dirname(os.path.abspath(__file__))
+        return open(os.path.join(d, file)).read()
