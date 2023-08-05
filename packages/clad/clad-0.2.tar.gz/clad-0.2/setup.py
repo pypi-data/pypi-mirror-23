dbd52780d66bb1aab4653546105947ba5c2fa391
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:06 +0000

    Add package clad/0.2/clad-0.2.tar.gz

---
+++ packages/clad/clad-0.2.tar.gz/clad-0.2/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+
+setup(
+    name='clad',
+    version='0.2',
+    author='Pavel Brodsky',
+    author_email='mcouthon@gmail.com',
+    packages=[
+        'clad',
+    ],
+    description='Cloudify dev/test environment assistant',
+    license='Apache License, Version 2.0',
+    install_requires=[
+        'argh>=0.26.2',
+        'sh>=1.12.14',
+        'colorama>=0.3.9'
+    ],
+    include_package_data=True,
+    entry_points={
+        'console_scripts': [
+            'clad = clad.clad:command'
+        ]
+    },
+)
