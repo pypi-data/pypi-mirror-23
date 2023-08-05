b26a56cf977e90455c41b14a09c9e374bb30640b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:18 +0000

    Add package new123/0.1/new123-0.1.tar.gz

---
+++ packages/new123/new123-0.1.tar.gz/new123-0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+	name="new123",
+	version="0.1",
+	description="for print helloworlsd",
+	author="superman",
+	license="MIT",
+	author_email="jaga@gmail.com",
+	packages=["funniest"],
+	zip_safe=False
+	)
