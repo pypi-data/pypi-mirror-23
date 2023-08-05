e36c8e9d27eec64ae45f691b3fb7905948249599
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:43 +0000

    Add package argskwargs/1.0.2/argskwargs-1.0.2.tar.gz

---
+++ packages/argskwargs/argskwargs-1.0.2.tar.gz/argskwargs-1.0.2/README.rst
@@ -0,0 +1,15 @@
+==========
+argskwargs
+==========
+
+.. image:: https://travis-ci.org/wbolster/argskwargs.svg?branch=master
+    :target: https://travis-ci.org/wbolster/argskwargs
+
+``argskwargs`` is a small Python library that provides a flexible container for
+positional and keyword arguments.
+
+* Documentation: https://argskwargs.readthedocs.io/
+
+* Python Package Index (PyPI): https://pypi.python.org/pypi/argskwargs/
+
+* Source code and issue tracker: https://github.com/wbolster/argskwargs
---
+++ packages/argskwargs/argskwargs-1.0.2.tar.gz/argskwargs-1.0.2/setup.py
@@ -0,0 +1,27 @@
+import os
+from setuptools import setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as fp:
+    long_description = fp.read()
+
+setup(
+    name='argskwargs',
+    description="flexible container for positional and keyword arguments",
+    long_description=long_description,
+    version='1.0.2',
+    author='Wouter Bolsterlee',
+    author_email='wouter@bolsterl.ee',
+    url="https://github.com/wbolster/argskwargs",
+    py_modules=['argskwargs'],
+    license='BSD',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
