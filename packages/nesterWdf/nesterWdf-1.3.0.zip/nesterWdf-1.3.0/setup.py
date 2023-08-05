4aec591209f4a15c1dbf00fafe4e230edb6dd450
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:18 +0000

    Add package nesterWdf/1.3.0/nesterWdf-1.3.0.zip

---
+++ packages/nesterWdf/nesterWdf-1.3.0.zip/nesterWdf-1.3.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = 'nesterWdf',
+    version = '1.3.0',
+    py_modules=['nesterWdf'],
+    author='wdf',
+    author_email='wdf@gmail.com',
+    url='http://www.headfirstlabs.com',
+    description='A simple printer of nested lists',
+    )
