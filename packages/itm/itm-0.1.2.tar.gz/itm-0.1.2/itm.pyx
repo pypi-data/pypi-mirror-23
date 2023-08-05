5781aac61e40326621c34e2f595fabd4b8e8be12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:30 +0000

    Add package itm/0.1.2/itm-0.1.2.tar.gz

---
+++ packages/itm/itm-0.1.2.tar.gz/itm-0.1.2/itm.pyx
@@ -0,0 +1,27 @@
+# distutils: language=c++
+# distutils: sources = isr84lib.cc
+"""Convert Lat/lng to/from ITM (Israeli Transverse Mercator)"""
+
+cimport decl
+
+__version__ = '0.1.2'
+
+def wgs842itm(double lat, double lng):
+    """Convert WGS84 lat/lng to ITM N/E"""
+    cdef int N, E
+
+    N = E = 0  # Make the compilter happy
+
+    decl.wgs842itm(lat, lng, N, E)
+    return N, E
+
+
+def itm2wgs84(int N, int E):
+    """Convert ITM N/E to lat/lng"""
+    cdef double lat, lng
+
+    lat = lng = 0.0  # Make the compilter happy
+
+    decl.itm2wgs84(N, E, lat, lng)
+
+    return lat, lng
