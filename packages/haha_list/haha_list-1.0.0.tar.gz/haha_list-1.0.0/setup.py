70f88bef34011dadcfbd95f3f4f5fdcac94b1404
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:47 +0000

    Add package haha_list/1.0.0/haha_list-1.0.0.tar.gz

---
+++ packages/haha_list/haha_list-1.0.0.tar.gz/haha_list-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name="haha_list",
+    version="1.0.0",
+    py_modules=['print_list'],
+    author="lile",
+    author_email="836217653@qq.com",
+    url = "http://www.cnblogs.com/lemon-le/",
+    description="A simple printer of print_list lists"
+)
