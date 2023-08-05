c9223d8bb45f3258b7802dcf579f801aebab24b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:41 +0000

    Add package booking_core/0.0.5/booking_core-0.0.5.tar.gz

---
+++ packages/booking_core/booking_core-0.0.5.tar.gz/booking_core-0.0.5/setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name='booking_core',
+    version='0.0.5',
+    description='Booking engine API',
+    author='Jonathan Rodriguez Alejos',
+    author_email='jrodriguez.5716@gmail.com',
+    classifiers=[
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+    ],
+    url='https://gitlab.com/booking-4geeks/booking-core',
+    download_url="https://gitlab.com/booking-4geeks/booking-core/repository/archive.tar.gz?ref=v0.5",
+    packages=find_packages(),
+    tests_require=[
+        'future'
+    ]
+)
