703ec5868836eeb5825fcbe9de9e11fed14eafec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:50 +0000

    Add package schul-cloud-search-tests/1.0.35/schul_cloud_search_tests-1.0.35-py3-none-any.whl

---
+++ packages/schul-cloud-search-tests/schul_cloud_search_tests-1.0.35-py3-none-any.whl/schul_cloud_search_tests/tests/assertions.py
@@ -0,0 +1,9 @@
+"""
+This module contains the assertions and error definitions.
+
+
+"""
+from schul_cloud_resources_server_tests.tests.assertions import assertIsError
+
+ERROR_CLIENT_REQUEST = 409 # https://httpstatuses.com/409
+Q = "Q" # The query string
