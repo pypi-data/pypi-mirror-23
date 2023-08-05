6938487a60f991fcb3a4e16e398eb8160223fac6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:48 +0000

    Add package django-live-log/1.1.2/django_live_log-1.1.2.tar.gz

---
+++ packages/django-live-log/django_live_log-1.1.2.tar.gz/django_live_log-1.1.2/setup.py
@@ -0,0 +1,33 @@
+import os
+from setuptools import find_packages, setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
+    README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='django_live_log',
+    version='1.1.2',
+    packages=find_packages(),
+    include_package_data=True,
+    license='MIT',  # example license
+    description='Simple package for live streaming of file.',
+    long_description=README,
+    url='https://github.com/abdullatheef/django_live_log',
+    author='Abdul Latheef',
+    author_email='latheefvkpadi@gmail.com',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',  # example license
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        # Replace these appropriately if you are stuck on Python 2.
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)
