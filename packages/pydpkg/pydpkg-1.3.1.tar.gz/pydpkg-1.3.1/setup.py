3d64ef69a61f16c9258d672cb1007dee3756196d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:01 +0000

    Add package pydpkg/1.3.1/pydpkg-1.3.1.tar.gz

---
+++ packages/pydpkg/pydpkg-1.3.1.tar.gz/pydpkg-1.3.1/setup.py
@@ -0,0 +1,36 @@
+from distutils.core import setup
+
+__VERSION__ = '1.3.1'
+
+setup(
+    name='pydpkg',
+    packages=['pydpkg'],  # this must be the same as the name above
+    version=__VERSION__,
+    description='A python library for parsing debian package control headers and comparing version strings',
+    author='Nathan J. Mehl',
+    author_email='n@climate.com',
+    url='https://github.com/theclimatecorporation/python-dpkg',
+    download_url='https://github.com/theclimatecorporation/python-dpkg/tarball/%s' % __VERSION__,
+    keywords=['apt', 'debian', 'dpkg', 'packaging'],
+    install_requires=[
+        'arpy==1.1.1',
+        'six==1.10.0',
+        'PGPy==0.4.1'
+    ],
+    extras_require={
+        'test': ['pep8==1.7.0', 'pytest==3.1.1', 'pylint==1.7.1']
+    },
+    scripts=[
+        'scripts/dpkg-inspect.py'
+    ],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: Implementation :: CPython",
+        "Topic :: System :: Archiving :: Packaging",
+        ]
+)
