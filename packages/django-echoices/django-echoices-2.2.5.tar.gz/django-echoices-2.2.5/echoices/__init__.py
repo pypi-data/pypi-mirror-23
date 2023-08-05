6f29bdd03c097e14fba40ef717a890beb5e0d8d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:05:58 +0000

    Add package django-echoices/2.2.5/django-echoices-2.2.5.tar.gz

---
+++ packages/django-echoices/django-echoices-2.2.5.tar.gz/django-echoices-2.2.5/echoices/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__license__ = 'GNU GPLv3'
+__author__ = 'Marc Bourqui <https://github.com/mbourqui>'
+__version__ = '2.2.5'
+__version_info__ = tuple([int(num) if num.isdigit() else num for num in __version__.replace('-', '.', 1).split('.')])
+__status__ = 'Stable'
