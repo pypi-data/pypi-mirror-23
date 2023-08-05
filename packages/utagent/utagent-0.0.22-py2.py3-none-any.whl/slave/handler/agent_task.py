ce8454f6920b355fdc688c1d7a3cc1d5b533f8c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:40 +0000

    Add package utagent/0.0.22/utagent-0.0.22-py2.py3-none-any.whl

---
+++ packages/utagent/utagent-0.0.22-py2.py3-none-any.whl/slave/handler/agent_task.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import json
+from slave.consts import TaskType
+from slave.globalvar import get_queue
+from slave.handler.macaca_task import (
+    run_macaca_test,
+    ui_recorder,
+    start_macaca_server
+)
+import logging
+import sys
+
+logger = logging.getLogger(__name__)
+
+
+def post_get(name, data):
+    return data[name]
+
+
+def handle_task():
+    while True:
+        try:
+            data = get_queue().get(timeout=3000)
+            if data:
+                task = json.load(data)
+                type = post_get('type', task)
+                if type == TaskType.macaca_test:
+                    case_url = post_get('case_url', task)
+                    name = post_get('name', task)
+                    run_macaca_test(name, case_url)
+                elif type == TaskType.ui_recorder:
+                    ui_recorder()
+                elif type == TaskType.macaca_server:
+                    start_macaca_server()
+        except KeyboardInterrupt:
+            logger.info("server is closed")
+            sys.exit()
