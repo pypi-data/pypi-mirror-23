7e069d82c91c44dd04062c61d06de37b6e45676c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:46 +0000

    Add package formica-cli/0.4.1/formica_cli-0.4.1-py2.py3-none-any.whl

---
+++ packages/formica-cli/formica_cli-0.4.1-py2.py3-none-any.whl/formica/change_set.py
@@ -0,0 +1,95 @@
+import sys
+
+import click
+from botocore.exceptions import ClientError, WaiterError
+from texttable import Texttable
+
+from formica import CHANGE_SET_FORMAT
+
+CHANGE_SET_HEADER = ['Action', 'LogicalId', 'PhysicalId', 'Type', 'Replacement', 'Changed']
+
+
+class ChangeSet:
+    def __init__(self, stack, client):
+        self.name = CHANGE_SET_FORMAT.format(stack=stack)
+        self.stack = stack
+        self.client = client
+
+    def create(self, template, change_set_type, parameters={}, tags={}, capabilities=[]):
+        optional_arguments = {}
+        if parameters:
+            optional_arguments['Parameters'] = [
+                {'ParameterKey': key, 'ParameterValue': value, 'UsePreviousValue': False} for (key, value) in
+                parameters.items()]
+        if tags:
+            optional_arguments['Tags'] = [{'Key': key, 'Value': value, } for (key, value) in
+                                          tags.items()]
+        if capabilities:
+            optional_arguments['Capabilities'] = capabilities
+        if change_set_type == 'UPDATE':
+            self.remove_existing_changeset()
+        self.client.create_change_set(StackName=self.stack, TemplateBody=template,
+                                      ChangeSetName=self.name, ChangeSetType=change_set_type, **optional_arguments)
+        click.echo('Change set submitted, waiting for CloudFormation to calculate changes ...')
+        waiter = self.client.get_waiter('change_set_create_complete')
+        try:
+            waiter.wait(ChangeSetName=self.name, StackName=self.stack)
+            click.echo('Change set created successfully')
+        except WaiterError as e:
+            status_reason = e.last_response.get('StatusReason', '')
+            click.echo(status_reason)
+            if "didn't contain changes" in status_reason:
+                sys.exit(0)
+            else:
+                sys.exit(1)
+
+    def describe(self):
+        change_set = self.client.describe_change_set(StackName=self.stack, ChangeSetName=self.name)
+        table = Texttable(max_width=150)
+
+        click.echo("Deployment metadata:")
+        parameters = ', '.join([parameter['ParameterKey'] + '=' + parameter['ParameterValue'] for parameter in
+                                change_set.get('Parameters', [])])
+        table.add_row(['Parameters', parameters])
+        tags = [tag['Key'] + '=' + tag['Value'] for tag in
+                change_set.get('Tags', [])]
+        table.add_row(["Tags ", ', '.join(tags)])
+        table.add_row(["Capabilities ", ', '.join(change_set.get('Capabilities', []))])
+        click.echo(table.draw() + '\n')
+
+        table.reset()
+        table = Texttable(max_width=150)
+        table.add_rows([CHANGE_SET_HEADER])
+
+        def __change_detail(change):
+            target_ = change['Target']
+            attribute = target_['Attribute']
+            if attribute == 'Properties':
+                return target_['Name']
+            else:
+                return attribute
+
+        for change in change_set['Changes']:
+            resource_change = change['ResourceChange']
+            table.add_row(
+                [resource_change['Action'],
+                 resource_change['LogicalResourceId'],
+                 resource_change.get('PhysicalResourceId', ''),
+                 resource_change['ResourceType'],
+                 resource_change.get('Replacement', ''),
+                 ', '.join(sorted(set([__change_detail(c) for c in resource_change['Details']])))
+                 ])
+
+        click.echo('Resource Changes:')
+        click.echo(table.draw())
+
+    def remove_existing_changeset(self):
+        try:
+            self.client.describe_change_set(StackName=self.stack,
+                                            ChangeSetName=self.name)
+            click.echo('Removing existing change set')
+            self.client.delete_change_set(StackName=self.stack,
+                                          ChangeSetName=self.name)
+        except ClientError as e:
+            if e.response['Error']['Code'] != 'ChangeSetNotFound':
+                raise e
---
+++ packages/formica-cli/formica_cli-0.4.1-py2.py3-none-any.whl/formica/cli.py
@@ -0,0 +1,206 @@
+#!/usr/bin/env python
+
+import click
+import logging
+from texttable import Texttable
+
+from formica import CHANGE_SET_FORMAT
+from formica.aws import AWS
+from formica.change_set import ChangeSet
+from formica.diff import Diff
+from formica.helper import aws_exceptions, session_wrapper
+from formica.stack_waiter import StackWaiter
+from .loader import Loader
+
+STACK_HEADERS = ['Name', 'Created At', 'Updated At', 'Status']
+RESOURCE_HEADERS = ['Logical ID', 'Physical ID', 'Type', 'Status']
+
+
+def aws_options(f):
+    f = session_wrapper(f)
+    f = click.option('--region', help='The AWS region to use.', metavar='REGION')(f)
+    f = click.option('--profile', help='The AWS profile to use.', metavar='PROFILE')(f)
+    return f
+
+
+def equals_option(ctx, param, value):
+    parameters = {}
+    try:
+        for param in value:
+            key, value = param.split('=', 2)
+            parameters[key] = value
+        return parameters
+    except ValueError:
+        raise click.BadParameter('needs to be in format KEY=VALUE')
+
+
+def csv_option(ctx, param, value):
+    if value:
+        return value.split(',')
+    else:
+        return value
+
+
+def stack_parameters(f):
+    return click.option('--parameter', help='Add a parameter. Repeat for multiple parameters',
+                        multiple=True, callback=equals_option, metavar='KEY=Value')(f)
+
+
+def stack_tags(f):
+    return click.option('--tag', help='Add a stack tag. Repeat for multipe tags', multiple=True,
+                        callback=equals_option, metavar='KEY=Value')(f)
+
+
+def capabilities(f):
+    return click.option('--capabilities', help='Set one or multiple stack capabilities', callback=csv_option,
+                        metavar='Cap1,Cap2')(f)
+
+
+def stack(message):
+    return click.option('--stack', help=message, required=True, metavar='STACK',
+                        envvar='FORMICA_STACK')
+
+
+@click.group()
+@click.version_option()
+@click.option('--debug/--no-debug', help='Enable debugging output')
+def main(debug):
+    logging.basicConfig(level=(logging.DEBUG if debug else logging.WARNING))
+
+
+@main.command()
+def template():
+    """Print the current template"""
+    loader = Loader()
+    loader.load()
+    click.echo(loader.template())
+
+
+@main.command()
+@stack('The stack you want to create.')
+@aws_exceptions
+@aws_options
+@stack_parameters
+@stack_tags
+@capabilities
+def new(stack, parameter, tag, capabilities):
+    """Create a change set for a new stack"""
+    client = AWS.current_session().client('cloudformation')
+    loader = Loader()
+    loader.load()
+    click.echo('Creating change set for new stack, ...')
+    change_set = ChangeSet(stack=stack, client=client)
+    change_set.create(template=loader.template(), change_set_type='CREATE', parameters=parameter, tags=tag,
+                      capabilities=capabilities)
+    change_set.describe()
+    click.echo('Change set created, please deploy.')
+
+
+@main.command()
+@stack('The stack to submit your changes to.')
+@aws_exceptions
+@aws_options
+@stack_parameters
+@stack_tags
+@capabilities
+def change(stack, parameter, tag, capabilities):
+    """Create a change set for an existing stack"""
+    client = AWS.current_session().client('cloudformation')
+    loader = Loader()
+    loader.load()
+
+    change_set = ChangeSet(stack=stack, client=client)
+    change_set.create(template=loader.template(), change_set_type='UPDATE', parameters=parameter, tags=tag,
+                      capabilities=capabilities)
+    change_set.describe()
+
+
+@main.command()
+@stack('The stack you want to deploy to.')
+@aws_exceptions
+@aws_options
+def deploy(stack):
+    """Deploy the latest change set for a stack"""
+    client = AWS.current_session().client('cloudformation')
+    last_event = client.describe_stack_events(StackName=stack)['StackEvents'][0]['EventId']
+    client.execute_change_set(ChangeSetName=(CHANGE_SET_FORMAT.format(stack=stack)), StackName=stack)
+    StackWaiter(stack, client).wait(last_event)
+
+
+@main.command()
+@aws_exceptions
+@aws_options
+def stacks():
+    """List all stacks"""
+    client = AWS.current_session().client('cloudformation')
+    stacks = client.describe_stacks()
+    table = Texttable(max_width=150)
+    table.add_rows([STACK_HEADERS])
+
+    for stack in stacks['Stacks']:
+        table.add_row(
+            [stack['StackName'],
+             stack['CreationTime'],
+             stack.get('LastUpdatedTime', ''),
+             stack['StackStatus']
+             ])
+
+    click.echo("Current Stacks:\n" + table.draw() + "\n")
+
+
+@main.command()
+@stack('The stack to describe')
+@aws_exceptions
+@aws_options
+def describe(stack):
+    """Describe the latest change-set of the stack"""
+    client = AWS.current_session().client('cloudformation')
+    change_set = ChangeSet(stack=stack, client=client)
+    change_set.describe()
+
+
+@main.command()
+@stack('The stack you want to remove.')
+@aws_exceptions
+@aws_options
+def remove(stack):
+    """Remove the configured stack"""
+    client = AWS.current_session().client('cloudformation')
+    stack_id = client.describe_stacks(StackName=stack)['Stacks'][0]['StackId']
+    click.echo('Removing Stack and waiting for it to be removed, ...')
+    last_event = client.describe_stack_events(StackName=stack)['StackEvents'][0]['EventId']
+    client.delete_stack(StackName=stack)
+    StackWaiter(stack_id, client).wait(last_event)
+
+
+@main.command()
+@stack('The stack to see the resources for.')
+@aws_exceptions
+@aws_options
+def resources(stack):
+    """List all resources of a stack"""
+    client = AWS.current_session().client('cloudformation')
+    paginator = client.get_paginator('list_stack_resources').paginate(StackName=stack)
+
+    table = Texttable(max_width=150)
+    table.add_rows([RESOURCE_HEADERS])
+
+    for page in paginator:
+        for resource in page['StackResourceSummaries']:
+            table.add_row(
+                [resource['LogicalResourceId'],
+                 resource['PhysicalResourceId'],
+                 resource['ResourceType'],
+                 resource['ResourceStatus']
+                 ])
+
+    click.echo(table.draw() + "\n")
+
+
+@main.command()
+@stack('The stack to diff with.')
+@aws_exceptions
+@aws_options
+def diff(stack):
+    """Print a diff between local and deployed stack"""
+    Diff(AWS.current_session()).run(stack)
