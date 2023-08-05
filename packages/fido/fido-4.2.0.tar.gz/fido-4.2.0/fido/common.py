4cfaf52a6ed06baef8f0933b0290495762183d1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:09 +0000

    Add package fido/4.2.0/fido-4.2.0.tar.gz

---
+++ packages/fido/fido-4.2.0.tar.gz/fido-4.2.0/fido/common.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+import six
+from twisted.web.http_headers import Headers
+from yelp_bytes import to_bytes
+
+
+def listify_headers(headers):
+    """Twisted agent requires header values as lists"""
+
+    for key, val in six.iteritems(headers):
+        if not isinstance(val, list):
+            headers[key] = [to_bytes(val)]
+        else:
+            headers[key] = [to_bytes(x) for x in val]
+    return Headers(headers)
