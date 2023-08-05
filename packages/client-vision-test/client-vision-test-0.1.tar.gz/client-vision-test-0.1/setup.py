5c8b85d0a6943bbac0c80df98e768db7a80c7344
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:48 +0000

    Add package client-vision-test/0.1/client-vision-test-0.1.tar.gz

---
+++ packages/client-vision-test/client-vision-test-0.1.tar.gz/client-vision-test-0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+
+setup(
+    name='client-vision-test',
+    packages=['vision'],
+    version='0.1',
+    description='An upload to s3 utility',
+    author='Fathom',
+    author_email='mirabel.ekwenugo@andela.com',
+    keywords=['upload', 'files', 's3'],
+    classifiers=[]
+)
