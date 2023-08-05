e0387e213ce332654c799fdcf6e22b71108baf82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:41 +0000

    Add package djangorestframework-simplejwt/2.0.0/djangorestframework_simplejwt-2.0.0.tar.gz

---
+++ packages/djangorestframework-simplejwt/djangorestframework_simplejwt-2.0.0.tar.gz/djangorestframework_simplejwt-2.0.0/rest_framework_simplejwt/utils.py
@@ -0,0 +1,16 @@
+from __future__ import unicode_literals
+
+from calendar import timegm
+
+from django.utils import six
+from django.utils.functional import lazy
+
+
+def datetime_to_epoch(dt):
+    return timegm(dt.utctimetuple())
+
+
+def format_lazy(s, *args, **kwargs):
+    return s.format(*args, **kwargs)
+
+format_lazy = lazy(format_lazy, six.text_type)
