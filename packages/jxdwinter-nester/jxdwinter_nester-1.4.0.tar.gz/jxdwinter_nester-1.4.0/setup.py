d2acde0802b2c5f89c628562b2ff083424576455
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:43 +0000

    Add package jxdwinter-nester/1.4.0/jxdwinter_nester-1.4.0.tar.gz

---
+++ packages/jxdwinter-nester/jxdwinter_nester-1.4.0.tar.gz/jxdwinter_nester-1.4.0/setup.py
@@ -0,0 +1,13 @@
+
+# coding:UTF-8 
+
+from distutils.core import setup
+setup(
+    name='jxdwinter_nester', 
+    version='1.4.0', 
+    py_modules=['jxdwinter_nester'], 
+    author='jxdwinter',
+    author_email = 'jxdwinter@gmail.com',
+    url = 'http://jiangxiaodong.me',
+    description = 'A simple printer of nested lists'
+)
