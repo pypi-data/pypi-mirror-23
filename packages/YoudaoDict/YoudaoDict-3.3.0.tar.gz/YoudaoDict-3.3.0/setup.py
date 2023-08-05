37bdb83074a1abf623c1b2bfdd7b47970d57c784
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:30 +0000

    Add package YoudaoDict/3.3.0/YoudaoDict-3.3.0.tar.gz

---
+++ packages/YoudaoDict/YoudaoDict-3.3.0.tar.gz/YoudaoDict-3.3.0/setup.py
@@ -0,0 +1,33 @@
+# coding=utf8
+from setuptools import setup, find_packages
+__author__ = 'hellflame'
+
+
+setup(
+    name='YoudaoDict',
+    version="3.3.0",
+    keywords=('Youdao', 'youdao', 'dict', 'youdao api', 'partly offline dict'),
+    description="终端通过有道api查询单词或者翻译词句",
+    license='MIT',
+    author='hellflame',
+    author_email='hellflamedly@gmail.com',
+    url="https://github.com/hellflame/youdao",
+    packages=find_packages(),
+    platforms="UNIX like",
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        "Environment :: Console",
+        'Operating System :: MacOS :: MacOS X',
+        'Operating System :: POSIX'
+    ],
+    entry_points={
+        'console_scripts': [
+            'youdao=youdao.run:main'
+        ]
+    }
+)
+
+
