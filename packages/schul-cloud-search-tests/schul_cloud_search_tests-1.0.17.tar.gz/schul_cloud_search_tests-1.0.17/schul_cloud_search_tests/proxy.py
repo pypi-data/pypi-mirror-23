97fa339fd71636ce96844966bec5bdf6aeae8183
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:57 +0000

    Add package schul-cloud-search-tests/1.0.17/schul_cloud_search_tests-1.0.17.tar.gz

---
+++ packages/schul-cloud-search-tests/schul_cloud_search_tests-1.0.17.tar.gz/schul_cloud_search_tests-1.0.17/schul_cloud_search_tests/proxy.py
@@ -0,0 +1,47 @@
+from bottle import Bottle, request
+import sys
+import os
+import requests
+from schul_cloud_resources_server_tests.tests.fixtures import StoppableWSGIRefServerAdapter
+if "" in sys.path:
+    sys.path.append(".")
+from schul_cloud_search_tests.search_tests import run_request_tests, run_response_tests
+
+ENDPOINT_STOP = "/stop"
+REDIRECT_TO = "http://localhost:8080"
+
+
+def test_response(target_url):
+    """Test the request and the response to the search engine."""
+    run_request_tests()
+    print("query string:", request.query_string)
+    response = requests.get(target_url + "?" + request.query_string)
+    run_response_tests(response)
+    return response.json()
+
+
+def get_app(endpoint="/", target_url="http://localhost:8080"):
+    """Return a bottle app that tests the request and the response."""
+    app = Bottle()
+    app.get(endpoint, callback=lambda: test_response(target_url))
+    return app
+
+
+def run(app, host="0.0.0.0", port=8081):
+    """Run a stoppable bottle app."""
+    server = StoppableWSGIRefServerAdapter(host=host, port=port)
+    app.get(ENDPOINT_STOP, callback=lambda: server.shutdown(blocking=False))
+    app.run(debug=True, server=server)
+    
+
+
+def main(host="0.0.0.0", port=8081, endpoint="/", target_url="http://localhost:8080"):
+    """Start the server."""
+    app = get_app(endpoint=endpoint, target_url=target_url)
+    run(app, host=host, port=port)
+
+
+__all__ = ["main", "app"]
+
+if __name__ == "__main__":
+    main()
