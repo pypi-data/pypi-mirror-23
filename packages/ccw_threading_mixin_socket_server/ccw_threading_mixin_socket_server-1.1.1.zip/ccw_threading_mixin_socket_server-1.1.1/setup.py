803cc01fa2deb9b16c89edb92050fb16117062d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:24 +0000

    Add package ccw_threading_mixin_socket_server/1.1.1/ccw_threading_mixin_socket_server-1.1.1.zip

---
+++ packages/ccw_threading_mixin_socket_server/ccw_threading_mixin_socket_server-1.1.1.zip/ccw_threading_mixin_socket_server-1.1.1/setup.py
@@ -0,0 +1,18 @@
+'''
+Created on 2017. 6. 28.
+
+@author: chiwon.choi
+'''
+
+from distutils.core import setup
+#from astroid.__pkginfo__ import author, author_email, description
+
+setup(
+      name = 'ccw_threading_mixin_socket_server',
+      version = '1.1.1',
+      py_modules = ['ccw_threading_mixin_socket_server'],
+      author = 'ccw',
+      author_email = 'ccw3435@naver.com',
+      #description = 'a simple python threading mixin socket server test code'
+      description = 'add chat_server_with_select'
+      )
