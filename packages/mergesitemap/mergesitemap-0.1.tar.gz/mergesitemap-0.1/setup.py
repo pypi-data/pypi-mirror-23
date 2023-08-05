5ead1ce346f1294a6e81faf0b2664e41d21da7b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:01 +0000

    Add package mergesitemap/0.1/mergesitemap-0.1.tar.gz

---
+++ packages/mergesitemap/mergesitemap-0.1.tar.gz/mergesitemap-0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='mergesitemap',
+      version='0.1',
+      description='Combines XML site maps',
+      url='http://github.com/kindlyops/mergesitemap',
+      author='Kindly Ops LLC',
+      author_email='support@kindlyops.com',
+      license='Apache2',
+      scripts=['bin/mergesitemap'],
+      python_requires='>=3',
+      zip_safe=False)
