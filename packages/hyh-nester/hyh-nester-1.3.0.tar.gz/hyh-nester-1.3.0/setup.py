2b2c3111575e4b56f60dba2a2283e08c52055e3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:34 +0000

    Add package hyh-nester/1.3.0/hyh-nester-1.3.0.tar.gz

---
+++ packages/hyh-nester/hyh-nester-1.3.0.tar.gz/hyh-nester-1.3.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup( 
+        name         = 'hyh-nester', 
+        version      = '1.3.0', 
+        ## TODO: be sure to change these next few lines to match your details!
+        py_modules   = ['hyh-nester'],
+        author       = 'mrhuangyuhui',
+        author_email = 'mrhuangyuhui@gmail.com',
+        url          = 'http://www.headfirstlabs.com',
+        description  = 'A simple printer of nested lists',
+     )
