08719b5fa094e94db607b2c4d32b9d5a9c9ae8ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:27 +0000

    Add package sia-scpy/0.1/sia-scpy-0.1.tar.gz

---
+++ packages/sia-scpy/sia-scpy-0.1.tar.gz/sia-scpy-0.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'sia-scpy',
+  packages = ['scpy'],
+  version = '0.1',
+  description = 'Python3 API bindings for the Sia API',
+  author = 'Emilio Mendoza',
+  author_email = 'emilio2601@gmail.com',
+  url = 'https://github.com/emilio2601/scpy',
+  download_url = 'https://github.com/emilio2601/scpy/archive/0.1.tar.gz',
+  keywords = ['python3', 'sia', 'api', 'storage'],
+  classifiers = [],
+)
