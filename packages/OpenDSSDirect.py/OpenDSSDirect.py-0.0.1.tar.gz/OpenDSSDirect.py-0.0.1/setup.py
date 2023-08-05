a643c829b4dcf19bd4907b5f538e01af246bd7e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:55 +0000

    Add package OpenDSSDirect.py/0.0.1/OpenDSSDirect.py-0.0.1.tar.gz

---
+++ packages/OpenDSSDirect.py/OpenDSSDirect.py-0.0.1.tar.gz/OpenDSSDirect.py-0.0.1/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name = 'OpenDSSDirect.py',
+    packages = ['opendssdirect'],
+    version = '0.0.1',  # Ideally should be same as your GitHub release tag varsion
+    description = 'Python direct-mode interface to OpenDSS',
+    author = 'Dheepak Krishnamurthy',
+    author_email = 'dheepak.krishnamurthy@nrel.gov',
+    url = 'https://github.com/NREL/OpenDSSDirect.py',
+    download_url = 'https://github.com/NREL/OpenDSSDirect.py',
+    keywords = ['OpenDSS', 'ctypes'],
+    classifiers = [],
+)
