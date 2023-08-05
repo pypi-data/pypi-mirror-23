2c2d768844c3a678877be04886f1cdbd705df61c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:15 +0000

    Add package infi.clickhouse_fdw/0.5.0/infi.clickhouse_fdw-0.5.0.tar.gz

---
+++ packages/infi.clickhouse_fdw/infi.clickhouse_fdw-0.5.0.tar.gz/infi.clickhouse_fdw-0.5.0/setup.py
@@ -0,0 +1,56 @@
+
+SETUP_INFO = dict(
+    name = 'infi.clickhouse_fdw',
+    version = '0.5.0',
+    author = 'Itai Shirav',
+    author_email = 'itais@infinidat.com',
+
+    url = 'https://git.infinidat.com/host-opensource/infi.clickhouse_fdw',
+    license = 'BSD',
+    description = """A PostgreSQL foreign data wrapper for ClickHouse""",
+    long_description = """A PostgreSQL foreign data wrapper for ClickHouse""",
+
+    # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers = [
+        "Intended Audience :: Developers",
+        "Intended Audience :: System Administrators",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+
+    install_requires = [
+'click',
+'colorama',
+'infi.clickhouse_orm',
+'pygments',
+'setuptools'
+],
+    namespace_packages = ['infi'],
+
+    package_dir = {'': 'src'},
+    package_data = {'': []},
+    include_package_data = True,
+    zip_safe = False,
+
+    entry_points = dict(
+        console_scripts = [
+'generate_clickhouse_fdw = infi.clickhouse_fdw.generate:run'
+],
+        gui_scripts = [],
+        ),
+)
+
+if SETUP_INFO['url'] is None:
+    _ = SETUP_INFO.pop('url')
+
+def setup():
+    from setuptools import setup as _setup
+    from setuptools import find_packages
+    SETUP_INFO['packages'] = find_packages('src')
+    _setup(**SETUP_INFO)
+
+if __name__ == '__main__':
+    setup()
+
