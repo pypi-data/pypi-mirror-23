7d96f1be088c9480baddb296b915e41d89fb1d7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:34 +0000

    Add package myNester007/1.1.0/myNester007-1.1.0.tar.gz

---
+++ packages/myNester007/myNester007-1.1.0.tar.gz/myNester007-1.1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(
+    name = "myNester007",
+    version = "1.1.0",
+    py_modules = ["nester"],
+    author = "zhaomeng",
+    author_email = "zhaomeng@ion.ac.cn",
+    url = 'http://www.headfirstlabs.com',
+    description = 'A simple printer of nested lists',
+    )
