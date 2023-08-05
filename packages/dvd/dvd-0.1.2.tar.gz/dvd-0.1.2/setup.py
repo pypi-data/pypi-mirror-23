7ddb7bdc68d52bff74171a4812b258ff116912c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:18 +0000

    Add package dvd/0.1.2/dvd-0.1.2.tar.gz

---
+++ packages/dvd/dvd-0.1.2.tar.gz/dvd-0.1.2/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup
+import sys
+import os
+from setuptools.command.install import install
+
+
+class Custom(install):
+    def run(self):
+        print 'Downloading weights for vgg net ....'
+	os.system(
+            'wget https://www.cs.toronto.edu/~frossard/vgg16/vgg16_weights.npz')
+        install.run(self)
+
+
+setup(name='dvd',
+      description='Deep Vision library for Dummies',
+      version='0.1.2',
+      author='Ajay Prasadh V, Arnav Varma',
+      author_email='ajayrfhp1710@gmail.com',
+      packages=['dvd'],
+      entry_points={
+          'console_scripts': ['dvd=dvd:main'],
+      },
+      cmdclass={
+          'install': Custom
+      },
+      url='https://github.com/ajayrfhp/dvd',
+      keywords=['Deep learning', 'Computer Vision',
+                'Vision', 'Machine learning'],
+      classifiers=[
+          'Operating System :: POSIX',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 2.7',
+          'Topic :: Utilities'
+      ],
+      )
