7e00f71c4fe4647cb3ed1f9c2f203d2ebc17e896
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:16 +0000

    Add package coderzyqin/1.0.0/coderzyqin-1.0.0.tar.gz

---
+++ packages/coderzyqin/coderzyqin-1.0.0.tar.gz/coderzyqin-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+    name='coderzyqin',
+    version='1.0.0',
+    py_modules=['coderzyqin'],
+    author='zyqin',
+    author_email='coderqinzy@163.com',
+    url='http://www.jianshu.com/u/4b125f6b2f7f',
+    description='A simple printer of zyqin'
+)
