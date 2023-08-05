3c1c5bac083d7bba99c3f4088f5038c73373fa58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:02 +0000

    Add package nester_ws/1.0.0/nester_ws-1.0.0.zip

---
+++ packages/nester_ws/nester_ws-1.0.0.zip/nester_ws-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name = 'nester_ws',
+	version = '1.0.0',
+	py_modules = ['nester'],
+	author = 'hfpython',
+	author_email = 'whether727@163.com',
+	url = 'https://www.headfirstlabs.com',
+	description = ' A simple function to print list',
+)
