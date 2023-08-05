e052d6ac5a13eb9687793b2c7778fccd66763538
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:44 +0000

    Add package nester_myfour/1.4.0/nester-myfour-1.4.0.zip

---
+++ packages/nester_myfour/nester-myfour-1.4.0.zip/nester-myfour-1.4.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='nester-myfour',
+    version='1.4.0',
+    py_modules=['print_lol'],
+    author='Myfour',
+    author_email='oz_myx@126.com',
+    url='http://www.headfirstlabs.com',
+    description='A print_lol() method!',
+    )
