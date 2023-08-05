12bc5c0e3d02a9fa8445df5486f2ffd1471bcb52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:04 +0000

    Add package schul-cloud-search-tests/1.0.46/schul_cloud_search_tests-1.0.46-py3-none-any.whl

---
+++ packages/schul-cloud-search-tests/schul_cloud_search_tests-1.0.46-py3-none-any.whl/schul_cloud_search_tests/tests/assertions.py
@@ -0,0 +1,22 @@
+"""
+This module contains the assertions and error definitions.
+
+Please see the specification for the errors.
+- https://github.com/schul-cloud/schul_cloud_search_tests/blob/master/README.rst#specification
+"""
+from schul_cloud_resources_server_tests.tests.assertions import assertIsError
+from schul_cloud_resources_api_v1.schema import get_schemas
+
+ERROR_CLIENT_REQUEST = 400 # https://httpstatuses.com/400
+Q = "Q" # The query string
+ERROR_SERVER_RESPONSE = 409 # https://httpstatuses.com/409
+
+ERROR = get_schemas()["error"].get_valid_examples()[0]
+
+
+def assertServerReplyIsWrong(response):
+    """Make sure the response is because the server responded in an
+    unspecified manner.
+    """
+    assert response.status_code == ERROR_SERVER_RESPONSE
+    assertIsError(response, ERROR_SERVER_RESPONSE)
