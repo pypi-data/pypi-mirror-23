b8b26d0434e4eb408b06aecfb764d926f7ab50d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:12 +0000

    Add package xdis/3.4.0/xdis-3.4.0.tar.gz

---
+++ packages/xdis/xdis-3.4.0.tar.gz/xdis-3.4.0/xdis/opcodes/opcode_20.py
@@ -0,0 +1,36 @@
+# (C) Copyright 2017 by Rocky Bernstein
+"""
+CPython 2.0 bytecode opcodes
+
+This is similar to the opcode portion in Python 2.0's dis.py library.
+"""
+
+import xdis.opcodes.opcode_21 as opcode_21
+from xdis.opcodes.base import (
+    init_opdata, finalize_opcodes, format_extended_arg, rm_op)
+
+version = 2.0
+
+l = locals()
+init_opdata(l, opcode_21, version)
+
+# 2.1 Bytecodes not in 2.0
+rm_op(l, 'CONTINUE_LOOP', 119)
+rm_op(l, 'MAKE_CLOSURE',  134)
+rm_op(l, 'LOAD_CLOSURE',  135)
+rm_op(l, 'LOAD_DEREF',    136)
+rm_op(l, 'STORE_DEREF',   137)
+
+# FIXME remove (fix uncompyle6)
+def updateGlobal():
+    globals().update({'PJIF': l['opmap']['JUMP_IF_FALSE']})
+    globals().update({'PJIT': l['opmap']['JUMP_IF_TRUE']})
+    return
+
+updateGlobal()
+
+opcode_arg_fmt = {
+    'EXTENDED_ARG': format_extended_arg,
+}
+
+finalize_opcodes(l)
