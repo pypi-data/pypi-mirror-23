6f67dccdabccd9de94830a2b1a7c868a34f6628e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:17 +0000

    Add package country-converter/0.4/country_converter-0.4.tar.gz

---
+++ packages/country-converter/country_converter-0.4.tar.gz/country_converter-0.4/setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup
+
+exec(open('country_converter/version.py').read())
+
+setup(
+    name='country_converter',
+    description=(
+        'The country converter (coco) - '
+        'a Python package for converting country names '
+        'between different classifications schemes.'),
+    long_description=open('README.rst').read(),
+    url='https://github.com/konstantinstadler/country_converter',
+    author='Konstantin Stadler',
+    author_email='konstantin.stadler@ntnu.no',
+    version=__version__,
+    packages=['country_converter', ],
+    package_data={'country_converter': ['country_data.tsv', '../LICENSE']},
+    license='GPLv3',
+    entry_points={
+        'console_scripts':
+        ['coco = country_converter.country_converter:main']},
+    install_requires=['pandas >= 0.17.0'],
+    classifiers=[
+          'Development Status :: 4 - Beta',
+          'Environment :: Console',
+          'Intended Audience :: End Users/Desktop',
+          'Intended Audience :: Developers',
+          'Intended Audience :: Science/Research',
+          'Operating System :: MacOS :: MacOS X',
+          'Operating System :: Microsoft :: Windows',
+          'Operating System :: POSIX',
+          'Programming Language :: Python',
+          'Topic :: Scientific/Engineering',
+          'Topic :: Utilities',
+          ],
+)
