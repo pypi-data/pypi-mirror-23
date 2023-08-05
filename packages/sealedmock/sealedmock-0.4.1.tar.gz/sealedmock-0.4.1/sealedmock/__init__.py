c3a6fe32b44a42243e8cf5d03efa29a47bc0c0d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:59 +0000

    Add package sealedmock/0.4.1/sealedmock-0.4.1.tar.gz

---
+++ packages/sealedmock/sealedmock-0.4.1.tar.gz/sealedmock-0.4.1/sealedmock/__init__.py
@@ -0,0 +1,109 @@
+"""Mock class that allows to whitelist the methods that can be called"""
+try:
+    from unittest.mock import *
+except ImportError:
+    from mock import *
+import functools
+from ._version import __version__
+
+
+def _extract_mock_name(in_mock):
+    """Prints the mock access path
+
+    Code from __repr__ code in mock.py
+
+    Given a mock prints the whole access chain since the root mock
+    """
+    _name_list = [in_mock._mock_new_name]
+    _parent = in_mock._mock_new_parent
+    last = in_mock
+
+    dot = '.'
+    if _name_list == ['()']:
+        dot = ''
+    seen = set()
+    while _parent is not None:
+        last = _parent
+
+        _name_list.append(_parent._mock_new_name + dot)
+        dot = '.'
+        if _parent._mock_new_name == '()':
+            dot = ''
+
+        _parent = _parent._mock_new_parent
+
+        # use ids here so as not to call __hash__ on the mocks
+        if id(_parent) in seen:
+            break
+        seen.add(id(_parent))
+
+    _name_list = list(reversed(_name_list))
+    _first = last._mock_name or 'mock'
+    if len(_name_list) > 1:
+        if _name_list[1] not in ('()', '().'):
+            _first += '.'
+    _name_list[0] = _first
+    return ''.join(_name_list)
+
+
+
+class SealedMockAttributeAccess(AttributeError):
+    """Attempted to access an attribute of a sealed mock"""
+
+
+class SealedMock(Mock):
+    """A Mock that can be sealed at any point of time
+
+    Once the mock is sealed it prevents any implicit mock creation
+
+    To seal the mock call seled_mock.sealed = True
+
+    :Example:
+
+    >>> import sealedmock
+    >>> m = sealedmock.SealedMock()
+    >>> m.method1.return_value.attr1.method2.return_value = 1
+    >>> m.sealed = True
+    >>> m.method1().attr1.method2()
+    >>> # 1
+    >>> m.method1().attr2
+    >>> # Exception: SealedMockAttributeAccess: mock.method1().attr2
+
+    """
+    def __init__(self, *args, **kwargs):
+        super(SealedMock, self).__init__(*args, **kwargs)
+        self.__dict__["_mock_sealed"] = False
+
+    def _get_child_mock(self, **kw):
+        if self.sealed:
+            attribute = "." + kw["name"] if "name" in kw else "()"
+            mock_name = _extract_mock_name(self) + attribute
+            raise SealedMockAttributeAccess(mock_name)
+        else:
+            return SealedMock(**kw)
+
+    @property
+    def sealed(self):
+        """Attribute that marks whether the mock can be extended dynamically
+
+        Once sealed is set to True no attribute that was not defined before can
+        be accessed.
+        :raises: SealedMockAttributeAccess
+        """
+        return self._mock_sealed
+
+    @sealed.setter
+    def sealed(self, value):
+        self._mock_sealed = value
+        for attr in dir(self):
+            try:
+                m = getattr(self, attr)
+            except AttributeError:
+                pass
+            else:
+                if isinstance(m, SealedMock):
+                    m.sealed = value
+
+
+patch = functools.partial(patch, new_callable=SealedMock)
+
---
+++ packages/sealedmock/sealedmock-0.4.1.tar.gz/sealedmock-0.4.1/setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+exec(open('sealedmock/_version.py').read())
+
+
+setup(
+    name='sealedmock',
+    packages=['sealedmock'],
+    version=__version__,
+    description='Mocks that whitelist its interface',
+    author='Mario Corchero',
+    author_email='mariocj89@gmail.com',
+    url='https://github.com/Mariocj89/sealedmock',
+    keywords=['mock', 'testing', 'unittest', 'integration', 'whitelist'],
+    license='MIT',
+    use_2to3=True,
+    install_requires=['mock'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Operating System :: OS Independent',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Software Development :: Testing',
+    ],
+)
