6e8b4d4ff80cf8feaf09bb92bd301d7a1880f89e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:40 +0000

    Add package biodome/2017.6.0/biodome-2017.6.0-py2.py3-none-any.whl

---
+++ packages/biodome/biodome-2017.6.0-py2.py3-none-any.whl/biodome.py
@@ -0,0 +1,39 @@
+"""
+biodome
+=======
+
+Controlled environments.
+
+"""
+import os
+import logging
+
+
+__version__ = '2017.6.0'
+logger = logging.getLogger(__name__)
+
+
+def biodome(name, default=None, cast=None):
+    # type: (str, Any, Callable) -> Any
+    raw_value = os.environ.get(name)
+    if raw_value is None:
+        return default
+
+    raw_value = raw_value.strip()
+
+    # Use the same type as default as the cast
+    type_ = cast or type(default)
+
+    if bool in (cast, type_):
+        return raw_value.lower() in (
+            '1', 'y', 'yes', 'on', 'active', 'activated', 'enabled', 'true',
+            't', 'ok', 'yeah',
+        )
+
+    try:
+        if type_ in (dict, list, set, tuple):
+            raw_value = eval(raw_value)
+            return (type(raw_value) == type_ and raw_value) or default
+        return type_(raw_value)
+    except:
+        return default
