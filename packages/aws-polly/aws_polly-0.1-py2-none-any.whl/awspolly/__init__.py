cb425814a30d26511cdca2da820f7cbdec9fc459
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:07 +0000

    Add package aws-polly/0.1/aws_polly-0.1-py2-none-any.whl

---
+++ packages/aws-polly/aws_polly-0.1-py2-none-any.whl/awspolly/__init__.py
@@ -0,0 +1,22 @@
+'''
+aws-polly: Main module
+
+Copyright 2017, Terrillo Walls
+Licensed under MIT.
+'''
+
+
+def main():
+    '''
+    Main function of the boilerplate code is the entry point of the 'awspolly' executable script (defined in setup.py).
+    
+    Use doctests, those are very helpful.
+    
+    >>> main()
+    Hello
+    >>> 2 + 2
+    4
+    '''
+    
+    print("Hello")
+
