60ca7bdecf89c5a5a4619094f995ccb13498453a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:09:37 +0000

    Add package remark/1.7.5/remark-1.7.5.tar.gz

---
+++ packages/remark/remark-1.7.5.tar.gz/remark-1.7.5/Remark/DocumentTypes/__init__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+# Description: Remark.DocumentTypes package initialization
+# Documentation: document_types.txt
+
+# Remark 1.7.5
+# Copyright (c) 2009 - 2017
+# Kalle Rutanen
+# Distributed under the MIT license (see license.txt).
+
+__all__ = [
+          	'CodeView_DocumentType',
+            'Copy_DocumentType',
+          	'CppCodeView_DocumentType',
+          	'DirectoryView_DocumentType',
+         	'Orphan_DocumentType',
+         	'RemarkPage_DocumentType',
+          ]
+ 
---
+++ packages/remark/remark-1.7.5.tar.gz/remark-1.7.5/Remark/Version.py
@@ -0,0 +1,14 @@
+# Description: Version-specific constants
+# Documentation: implementation.txt
+
+# Remark 1.7.5
+# Copyright (c) 2009 - 2017
+# Kalle Rutanen
+# Distributed under the MIT license (see license.txt).
+
+def remarkVersion():
+    '''
+    Returns the version of Remark as a string.
+    '''
+    return '1.7.5'
+
