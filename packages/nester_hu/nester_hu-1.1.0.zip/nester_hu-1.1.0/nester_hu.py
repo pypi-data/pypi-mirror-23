327cdd72832a8a1489a381b8eb886baefa11da9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:14 +0000

    Add package nester_hu/1.1.0/nester_hu-1.1.0.zip

---
+++ packages/nester_hu/nester_hu-1.1.0.zip/nester_hu-1.1.0/nester_hu.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+# movies = ["The Holy Grail", 1975, "Terry Jones & Terry Gilliam", 91, ["Graham Chapman", ["Michael Palin", "John Cleese", "Terry Gilliam", "Eric Idle", "Terry Jones"]]]
+
+def print_lol(the_list, level):
+	for each_item in the_list:
+		if isinstance(each_item, list):
+			print_lol(each_item, level+1)
+		else:
+			for tab_stop in range(level):
+				print("\t", end='')
+			print(each_item)
+		
---
+++ packages/nester_hu/nester_hu-1.1.0.zip/nester_hu-1.1.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+	name      =  'nester_hu',
+	version   =  '1.1.0',
+	py_modules=  ['nester_hu'],
+	author    =  'hfpython',
+	author_email= '568347223@qq.com',
+	url       =  'http://www.headfirstlabs.com',
+	description= 'A simple printer of nested lists',
+	)
