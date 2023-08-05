4a769cec2d0e8c332e160066e9048521fad6c911
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:37 +0000

    Add package qtx/0.1/qtx-0.1.tar.gz

---
+++ packages/qtx/qtx-0.1.tar.gz/qtx-0.1/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+setup(
+  name = 'qtx',
+  packages = ['qtx'],
+  version = '0.1',
+  description = 'API client for Quantextive AEX Data Platform',
+  author = 'Quantextive',
+  author_email = 'api@quantextive.com',
+  url = 'https://github.com/quantextive/qtx', #github repo
+  download_url = 'https://github.com/quantextive/qtx/archive/0.1.tar.gz',
+  keywords = ['africa', 'finance', 'quantextive'],
+  classifiers = [],
+)
+
