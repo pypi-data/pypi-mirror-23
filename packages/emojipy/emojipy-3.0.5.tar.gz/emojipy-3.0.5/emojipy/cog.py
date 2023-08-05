659c71ca72e628daf4c0c4f25cc2a4a4adda6355
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:51 +0000

    Add package emojipy/3.0.5/emojipy-3.0.5.tar.gz

---
+++ packages/emojipy/emojipy-3.0.5.tar.gz/emojipy-3.0.5/emojipy/cog.py
@@ -0,0 +1,17 @@
+#!/home/phuong/Desktop/emoji/env/bin/python3
+""" Cog code generation tool.
+    http://nedbatchelder.com/code/cog
+
+    Copyright 2004-2015, Ned Batchelder.
+"""
+
+import time
+start = time.clock()
+
+import sys
+from cogapp import Cog
+
+ret = Cog().main(sys.argv)
+
+#print "Time: %.2f sec" % (time.clock() - start)
+sys.exit(ret)
---
+++ packages/emojipy/emojipy-3.0.5.tar.gz/emojipy-3.0.5/setup.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8; -*-
+
+from setuptools import setup
+
+setup(name='emojipy',
+      version='3.0.5',
+      description='Python wrapper for emojione',
+      author='Kaviraj Kanagaraj',
+      author_email='kaviraj@launchyard.com',
+      packages=['emojipy',],
+      test_suite='emojipy.tests',
+      url = 'https://github.com/phuong/emojipy', 
+      download_url = 'https://github.com/phuong/emojipy/archive/master.zip', 
+      keywords = ['emojipy', 'emojione', 'python emoji'], 
+)
