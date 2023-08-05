f190853710ba6dce7e94c58ce91335ea6f735eca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:52 +0000

    Add package reactions/0.0.1/reactions-0.0.1.tar.gz

---
+++ packages/reactions/reactions-0.0.1.tar.gz/reactions-0.0.1/reactions/__init__.py
@@ -0,0 +1,2 @@
+def work_in_progress():
+    print('This package is still being worked on!')
---
+++ packages/reactions/reactions-0.0.1.tar.gz/reactions-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='reactions',
+      version='0.0.1',
+      description=('Python package to specify, simulate, and optimize over',
+                   'chemical reactions'),
+      url='https://bitbucket.org/ankur-gupta/reactions',
+      author='Ankur Gupta',
+      author_email='ankur.gupta.phd@gmail.com',
+      packages=['reactions'],
+      zip_safe=False)
