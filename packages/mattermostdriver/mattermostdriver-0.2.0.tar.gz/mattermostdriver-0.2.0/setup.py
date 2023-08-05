fd87b092f05f63ec85f013036b78b06e4cb3e786
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:45 +0000

    Add package mattermostdriver/0.2.0/mattermostdriver-0.2.0.tar.gz

---
+++ packages/mattermostdriver/mattermostdriver-0.2.0.tar.gz/mattermostdriver-0.2.0/setup.py
@@ -0,0 +1,45 @@
+import os.path
+import sys
+
+from setuptools import setup, find_packages
+
+root_dir = os.path.abspath(os.path.dirname(__file__))
+
+readme_file = os.path.join(root_dir, 'README.md')
+with open(readme_file, encoding='utf-8') as f:
+	long_description = f.read()
+
+py_version = sys.version_info[:2]
+
+if py_version < (3, 3):
+	raise Exception("python-mattermost-driver requires Python >= 3.3.")
+
+setup(
+	name='mattermostdriver',
+	version='0.2.0',
+	description='A Python Mattermost Driver',
+	long_description=long_description,
+	url='https://github.com/Vaelor/python-mattermost-driver',
+	author='Christian Plümer',
+	author_email='github@kuuku.net',
+	license='MIT',
+	classifiers=[
+		'Development Status :: 1 - Planning',
+		'Environment :: Web Environment',
+		'Intended Audience :: Developers',
+		'Programming Language :: Python',
+		'Programming Language :: Python :: 3',
+		'Programming Language :: Python :: 3.4',
+		'Programming Language :: Python :: 3.5',
+		'Programming Language :: Python :: 3.6',
+	],
+	package_dir={'': 'src'},
+	packages=find_packages('src'),
+	extras_require={
+		':python_version=="3.3"': ['asyncio'],
+	},
+	install_requires=[
+		'websockets>=3.2',
+		'requests>=2.1'
+	],
+)
