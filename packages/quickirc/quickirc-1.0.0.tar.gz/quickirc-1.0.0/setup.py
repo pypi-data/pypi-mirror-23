1efe31c1d80a6e702ae22835b4a2be35ca2b5777
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:25 +0000

    Add package quickirc/1.0.0/quickirc-1.0.0.tar.gz

---
+++ packages/quickirc/quickirc-1.0.0.tar.gz/quickirc-1.0.0/setup.py
@@ -0,0 +1,32 @@
+#! /usr/bin/env python2
+
+from distutils.core import setup
+setup(name="quickirc",
+      version="1.0.0",
+      py_modules=["quickirc"],
+      author="Iury O. G. Figueiredo",
+      author_email="ioliveira@id.uff.br",
+      url='https://github.com/iogf/qirc',
+      download_url='https://github.com/iogf/qirc/releases',
+      keywords=['untwisted', 'irc', 'protocol', 'library'],
+      classifiers=[],
+      description="A nifty python IRC library.",)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
