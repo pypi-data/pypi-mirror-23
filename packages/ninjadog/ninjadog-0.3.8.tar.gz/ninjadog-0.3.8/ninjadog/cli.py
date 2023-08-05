7ddf9a1697e84cde31421decd9335fa68b88862b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:56 +0000

    Add package ninjadog/0.3.8/ninjadog-0.3.8.tar.gz

---
+++ packages/ninjadog/ninjadog-0.3.8.tar.gz/ninjadog-0.3.8/ninjadog/cli.py
@@ -0,0 +1,65 @@
+"""ninjadog v0.3.8
+
+Render pug templates to html.
+
+Usage:
+    ninjadog string [options] <string>
+    ninjadog file [options] <file>
+    ninjadog - [options]
+    ninjadog -h | --help
+    ninjadog -V | --version
+
+
+Options:
+    -h --help                 show help and exit
+    -V --version              show version and exit
+    -f --file <file>          the filepath to the template
+    -p --pretty               pretty print output
+    -c --context <context>    json string to be passed as context
+    -j --with-jinja           render jinja2 syntax as well as pug
+    -v --verbose              verbose output
+
+
+Strings may be passed via pipe using `-` argument.
+
+i.e. 
+
+echo 'h1 hello {{ name }}' | ninjadog - -j -c '{"name": "Sam"}'
+
+outputs
+
+<h1>hello Sam</h1>
+
+"""
+import sys
+import typing as T
+
+from docopt import docopt
+
+from ninjadog import render
+
+
+def main(argv: T.Optional[T.Iterable] = None):
+    """Render pug template to stdout."""
+    args = docopt(__doc__, argv=argv, version='0.3.8')
+
+    if args['--file'] and args['<file>']:
+        raise ValueError("Cannot combine --file and <file> arguments")
+
+    string = sys.stdin.read() if args['-'] else args['<string>']
+    file = args['--file'] or args['<file>']
+    pretty = args['--pretty']
+    context = args['--context']
+    with_jinja = args['--with-jinja']
+    verbose = args['--verbose']
+
+    output = render(string=string, file=file, pretty=pretty, context=context, with_jinja=with_jinja)
+
+    if verbose:
+        print(args, file=sys.stderr, end='\n\n')
+
+    return output
+
+
+if __name__ == "__main__":
+    main()
---
+++ packages/ninjadog/ninjadog-0.3.8.tar.gz/ninjadog-0.3.8/tests/test_ninjadog.py
@@ -0,0 +1,148 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Tests for `ninjadog` package."""
+
+
+# TODO: test raises ValueError when pug cli can't be found and not passed explicitly to renderer
+
+
+def test_npm_installed():
+    from subprocess import Popen
+    assert Popen(('which', 'npm')).wait() == 0, 'npm must be installed'
+
+
+def test_pug_cli_exists():
+    from pathlib import Path
+    from ninjadog.constants import PUG_CLI_PATH
+    assert Path(PUG_CLI_PATH).exists()
+
+
+def test_hello_world():
+    from ninjadog import render
+    assert render('h1 hello world') == '<h1>hello world</h1>'
+
+
+def test_pug_variable():
+    from ninjadog import render
+    assert render('h1= title', context={'title': 'hello world'}) == '<h1>hello world</h1>'
+
+
+def test_jinja2_variable():
+    from ninjadog import render
+    assert render('h1 {{ title }}', context={'title': 'hello world'}, with_jinja=True) == '<h1>hello world</h1>'
+
+
+def test_context():
+    import ninjadog
+    context = {'name': 'Derp'}
+    assert ninjadog.render('h1 hello #{ name }', context=context) == '<h1>hello Derp</h1>'
+    assert ninjadog.render("h1= name", context=context) == '<h1>Derp</h1>'
+
+
+def test_conditional():
+    from textwrap import dedent
+    import ninjadog
+    string = dedent("""
+    if name == 'sam'
+        h1 hello #{ name }
+    """)
+    assert ninjadog.render(string, context={'name': 'sam'}) == '<h1>hello sam</h1>'
+
+    string = dedent("""
+    if person.name == 'sam'
+        h1 hello #{ person.name }
+    """)
+    assert ninjadog.render(string, context={'person': {'name': 'sam'}}) == '<h1>hello sam</h1>'
+
+
+def test_render_no_string_argument():
+    from tempfile import NamedTemporaryFile
+    import ninjadog
+    string = 'h1 hello'
+    with NamedTemporaryFile('w+') as tempfile:
+        tempfile.write(string)
+        tempfile.seek(0)
+        assert ninjadog.render(file=tempfile.name) == ninjadog.render(string) == '<h1>hello</h1>'
+
+
+def test_with_pug_with_jinja2():
+    from textwrap import dedent
+    from ninjadog import render
+    string = dedent("""
+    if person.name == "Bob"
+        h1 Hello Bob
+    else
+        h1 My name is #{ person.name }
+
+    p The persons's uppercase name is {{ person.get('name').upper() }}
+    p The person's name is #{ person.name }
+
+    if animal
+        h1 This should not output
+    else
+        p animal value is false
+    """).strip()
+
+    context = {'person': {'name': 'Bob'}, 'animal': None}
+
+    expected_output = dedent("""
+    <h1>Hello Bob</h1>
+    <p>The persons's uppercase name is BOB</p>
+    <p>The person's name is Bob</p>
+    <p>animal value is false</p>
+    """).strip()
+
+    actual_output = render(string, context=context, pretty=True, with_jinja=True).strip()
+
+    assert expected_output == actual_output
+
+
+def test_cli_string():
+    from ninjadog.cli import main
+    from ninjadog.utils import jsonify
+    context = jsonify({'title': 'hello, world'})
+    assert main(('string', 'h1= title', '-c', context)) == '<h1>hello, world</h1>'
+
+
+def test_cli_file():
+    from ninjadog.cli import main
+    from ninjadog.utils import jsonify
+    from tempfile import NamedTemporaryFile
+
+    context = jsonify({'title': 'hello, world'})
+
+    with NamedTemporaryFile('w+') as file:
+        file.write('h1= title');
+        file.seek(0)
+        assert main(('file', file.name, '-c', context)) == '<h1>hello, world</h1>'
+
+
+def test_extends():
+    from tempfile import gettempdir
+    from textwrap import dedent
+    from pathlib import Path
+    from ninjadog import render
+
+    parent_string = dedent("""
+    h1 Title
+    block content
+    """)
+
+    child_string = dedent("""
+    extends parent
+    block content
+        h2 Subtitle
+    """)
+
+    parent_path = Path(gettempdir(), 'parent.pug')
+    child_path = Path(gettempdir(), 'child.pug')
+
+    with parent_path.open('w+') as parent, child_path.open('w+') as child:
+        parent.write(parent_string)
+        parent.seek(0)
+        child.write(child_string)
+        child.seek(0)
+
+        assert render(file=child_path) == '<h1>Title</h1><h2>Subtitle</h2>'
+        assert render(file=str(child_path)) == '<h1>Title</h1><h2>Subtitle</h2>'
