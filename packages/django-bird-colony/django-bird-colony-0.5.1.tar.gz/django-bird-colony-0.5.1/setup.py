23c1acdaa186db24b0329f401f115e18aa4f1ac9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:21 +0000

    Add package django-bird-colony/0.5.1/django-bird-colony-0.5.1.tar.gz

---
+++ packages/django-bird-colony/django-bird-colony-0.5.1.tar.gz/django-bird-colony-0.5.1/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+# -*- mode: python -*-
+import os
+try:
+    from setuptools import setup
+except ImportError:
+    from ez_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup
+
+VERSION = '0.5.1'
+cls_txt = """
+Development Status :: 3 - Alpha
+Framework :: Django
+Intended Audience :: Science/Research
+License :: OSI Approved :: GNU General Public License (GPL)
+Programming Language :: Python
+Topic :: Scientific/Engineering
+Topic :: Internet :: WWW/HTTP
+Topic :: Internet :: WWW/HTTP :: Dynamic Content
+"""
+
+setup(
+    name="django-bird-colony",
+    version=VERSION,
+    description="A simple Django app for managing a bird colony",
+    long_description=open(os.path.join(os.path.dirname(__file__), 'README.md')).read(),
+    classifiers=[x for x in cls_txt.split("\n") if x],
+    author='C Daniel Meliza',
+    maintainer='C Daniel Meliza',
+    url = "https://github.com/melizalab/django-bird-colony",
+    packages=['birds'],
+    include_package_data=True,
+)
