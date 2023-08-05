585daec5533595d07d3aa5de3d6e4abfee447468
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:55 +0000

    Add package datahandling/3.2.7/datahandling-3.2.7.tar.gz

---
+++ packages/datahandling/datahandling-3.2.7.tar.gz/datahandling-3.2.7/datahandling/__init__.py
@@ -0,0 +1,23 @@
+"""
+datahandling
+============
+
+Package of functions for the Matter-Wave Interferometry 
+group for handling experimental data.
+
+
+"""
+
+# init file
+
+import os
+
+_mypackage_root_dir = os.path.dirname(__file__)
+_version_file = open(os.path.join(_mypackage_root_dir, 'VERSION'))
+__version__ = _version_file.read().strip()
+
+# the following line imports all the functions from datahandling.py
+from .datahandling import *
+import datahandling.thermo
+import datahandling.LeCroy
+import datahandling.Saleae
