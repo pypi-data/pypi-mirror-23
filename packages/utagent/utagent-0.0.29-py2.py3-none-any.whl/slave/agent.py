1ab421719e8cbc4da0defa0dd91ff3ea506ccefd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:15 +0000

    Add package utagent/0.0.29/utagent-0.0.29-py2.py3-none-any.whl

---
+++ packages/utagent/utagent-0.0.29-py2.py3-none-any.whl/slave/agent.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from multiprocessing import Process
+from api.api_server import start_api_server
+from handler.agent_task import handle_task
+from config import (
+    config,
+    environment
+)
+import os
+import click
+import slave
+
+import logging
+
+APP_DESC = """
+     A Terminal Tools For slave Agent
+"""
+
+logger = logging.getLogger(__name__)
+
+
+def output_version(ctx, param, value):
+    if not value or ctx.resilient_parsing:
+        return
+    click.echo("Version: %s" % slave.__version__)
+    ctx.exit()
+
+
+@click.command()
+@click.option('-v', '--version', is_flag=True, is_eager=True, callback=output_version, expose_value=False,
+              help="show the version of this tool")
+def parse_command():
+    config["macaca_dir"] = os.path.join(click.prompt('Please enter a path', default=os.getcwd()))
+    config["environment"] = environment[click.prompt('Please enter a environment', default="alpha")]
+    start_agent_client()
+
+
+def start_agent_client():
+    p1 = Process(target=start_api_server)
+    p2 = Process(target=handle_task)
+    p1.start()
+    p2.start()
+    p1.join()
+    p2.join()
+
+
+def main():
+    print(APP_DESC)
+    parse_command()
+
+
+if __name__ == "__main__":
+    main()
---
+++ packages/utagent/utagent-0.0.29-py2.py3-none-any.whl/slave/handler/macaca_task.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import subprocess
+import os
+
+from slave.util.file import File_Downloader
+from slave.config import config
+
+
+def start_macaca_server():
+    subprocess.Popen("macaca server --port 4444 --verbose", shell=True)
+
+
+def run_macaca_test(file_name, case_url):
+    File_Downloader.download(config["macaca_dir"] + 'eleme', file_name, case_url)
+    os.chdir(config["macaca_dir"])
+    subprocess.call('source run.sh eleme/%s' % file_name, shell=True)
+
+
+def ui_recorder():
+    os.chdir(config["macaca_dir"])
+
+    subprocess.Popen('uirecorder --mobile', shell=True)
+
+
+def init_uirecorder():
+    os.chdir(config["macaca_dir"])
+
+    subprocess.Popen('uirecorder init --mobile', shell=True)
