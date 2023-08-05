0eedf370fd0e9d8cea69ce0bbd4d455d38809d32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:00 +0000

    Add package gdxpy/0.1.1/gdxpy-0.1.1.tar.gz

---
+++ packages/gdxpy/gdxpy-0.1.1.tar.gz/gdxpy-0.1.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(name='gdxpy',
+      version='0.1.1',
+      description='Manipulate GAMS GDX files as Pandas data structures for convenient computation and visualization',
+      url='https://github.com/jackjackk/gdxpy',
+      author='Giacomo Marangoni',
+      author_email='jackjackk@gmail.com',
+      license='MIT',
+      keywords='gdx gams pandas',
+      packages=['gdxpy',],
+      install_requires=['numpy', 'pandas'],
+)
