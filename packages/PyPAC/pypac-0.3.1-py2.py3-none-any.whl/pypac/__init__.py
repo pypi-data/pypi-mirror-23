a5b9635fbecd267705a292a399e32f30fee30378
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:56 +0000

    Add package PyPAC/0.3.1/pypac-0.3.1-py2.py3-none-any.whl

---
+++ packages/PyPAC/pypac-0.3.1-py2.py3-none-any.whl/pypac/__init__.py
@@ -0,0 +1,9 @@
+from pypac.api import get_pac, collect_pac_urls, download_pac, PACSession
+
+
+__version__ = '0.3.1'
+
+
+__all__ = [
+    'get_pac', 'collect_pac_urls', 'download_pac', 'PACSession',
+]
