fffe5c2ee68f1881576869aefb87d8f53c8646e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:08:27 +0000

    Add package gdxcompare/0.1.1/gdxcompare-0.1.1.tar.gz

---
+++ packages/gdxcompare/gdxcompare-0.1.1.tar.gz/gdxcompare-0.1.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(name='gdxcompare',
+      version='0.1.1',
+      description='Visually compare time series across GAMS GDX files',
+      url='https://github.com/jackjackk/gdxcompare',
+      author='Giacomo Marangoni',
+      author_email='jackjackk@gmail.com',
+      license='MIT',
+      keywords='gdx gams dataviz',
+      packages=['gdxcompare',],
+      install_requires=['gdxpy>=0.1.2'],
+)
