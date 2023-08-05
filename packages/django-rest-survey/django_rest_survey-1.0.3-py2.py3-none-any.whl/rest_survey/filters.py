fbe782ea3919e07a79db57140664c428d687c04c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:54 +0000

    Add package django-rest-survey/1.0.3/django_rest_survey-1.0.3-py2.py3-none-any.whl

---
+++ packages/django-rest-survey/django_rest_survey-1.0.3-py2.py3-none-any.whl/rest_survey/filters.py
@@ -0,0 +1,76 @@
+from rest_framework_filters import FilterSet
+from .models import (
+    Survey,
+    Section,
+    Question,
+    SurveyedObject,
+    Answer,
+    OtherAnswer,
+)
+
+
+class OtherAnswerFilter(FilterSet):
+
+    class Meta:
+
+        model = OtherAnswer
+        fields = {
+            'id': ['exact'],
+            'value': ['icontains', ]
+        }
+
+
+class SurveyFilter(FilterSet):
+
+    class Meta:
+
+        model = Survey
+        fields = {
+            'id': ['exact'],
+            'name': ['icontains']
+        }
+
+
+class SectionFilter(FilterSet):
+
+    class Meta:
+        model = Section
+        fields = {
+            'id': ['exact'],
+            'survey_id': ['exact'],
+            'title': ['icontains']
+        }
+
+
+class QuestionFilter(FilterSet):
+
+    class Meta:
+        model = Question
+        fields = {
+            'id': ['exact'],
+            'survey_id': ['exact'],
+            'section_id': ['exact'],
+            'type': ['icontains'],
+            'text': ['icontains'],
+        }
+
+
+class AnswerFilter(FilterSet):
+
+    class Meta:
+        model = Answer
+        fields = {
+            'id': ['exact'],
+            'surveyed_id': ['exact'],
+            'question_id': ['exact'],
+        }
+
+
+class SurveyedObjectFilter(FilterSet):
+
+    class Meta:
+        model = SurveyedObject
+        fields = {
+            'id': ['exact'],
+            'survey_id': ['exact']
+        }
