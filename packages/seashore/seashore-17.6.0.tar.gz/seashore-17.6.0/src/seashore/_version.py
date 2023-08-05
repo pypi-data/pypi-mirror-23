39a66d501e8c5be324f22026f9a4677c64be6dd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:34 +0000

    Add package seashore/17.6.0/seashore-17.6.0.tar.gz

---
+++ packages/seashore/seashore-17.6.0.tar.gz/seashore-17.6.0/src/seashore/_version.py
@@ -0,0 +1,13 @@
+# Copyright (c) Shopkick 2017
+# See LICENSE for details.
+"""
+Provides seashore version information.
+"""
+
+# This file is auto-generated! Do not edit!
+# Use `python -m incremental.update seashore` to change this file.
+
+from incremental import Version
+
+__version__ = Version('seashore', 17, 6, 0)
+__all__ = ["__version__"]
