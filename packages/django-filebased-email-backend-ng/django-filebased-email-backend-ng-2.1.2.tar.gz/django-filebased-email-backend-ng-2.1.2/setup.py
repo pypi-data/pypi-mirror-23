d22157b4dfcb4d67b93c1aca23e4ee8f8a99a371
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:28 +0000

    Add package django-filebased-email-backend-ng/2.1.2/django-filebased-email-backend-ng-2.1.2.tar.gz

---
+++ packages/django-filebased-email-backend-ng/django-filebased-email-backend-ng-2.1.2.tar.gz/django-filebased-email-backend-ng-2.1.2/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='django-filebased-email-backend-ng',
+    version='2.1.2',
+    description="A better 'file' email backend for Django",
+
+    url="https://chris-lamb.co.uk/projects/django-filebased-email-backend-ng",
+    author="Chris Lamb",
+    author_email='chris@chris-lamb.co.uk',
+    license="BSD",
+
+    packages=find_packages(),
+
+    install_requires=(
+        'Django>=1.8',
+    ),
+)
