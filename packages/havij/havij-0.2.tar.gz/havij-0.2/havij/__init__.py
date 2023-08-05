ba827c954c11f5a5217da36c078eb2344cd812b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:15 +0000

    Add package havij/0.2/havij-0.2.tar.gz

---
+++ packages/havij/havij-0.2.tar.gz/havij-0.2/havij/__init__.py
@@ -0,0 +1,2 @@
+def poof():
+    return "Salam"
---
+++ packages/havij/havij-0.2.tar.gz/havij-0.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='havij',
+      version='0.2',
+      description='The Havijest in the world',
+      url='http://github.com/havij',
+      author='EmAm',
+      author_email='example@example.com',
+      license='MIT',
+      packages=['havij'],
+      zip_safe=False)
