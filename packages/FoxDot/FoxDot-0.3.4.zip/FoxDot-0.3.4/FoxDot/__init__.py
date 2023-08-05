52f54cb3246ed46e6e3bd068e44de84e93fa5c79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:03 +0000

    Add package FoxDot/0.3.4/FoxDot-0.3.4.zip

---
+++ packages/FoxDot/FoxDot-0.3.4.zip/FoxDot-0.3.4/FoxDot/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+
+"""
+    Copyright Ryan Kirkbride 2015
+    -----------------------------
+
+    FoxDot: Live Coding with Python and SuperCollider
+
+"""
+
+from lib import *
---
+++ packages/FoxDot/FoxDot-0.3.4.zip/FoxDot-0.3.4/FoxDot/lib/Patterns/Operations.py
@@ -0,0 +1,92 @@
+from __future__ import division
+from utils import *
+import Main
+import PlayString
+
+"""
+    Module for key operations on Python lists or FoxDot Patterns
+"""
+
+class POperand:
+
+    def __init__(self, func):
+        
+        self.operate = func
+
+    def __call__(self, A, B):
+        """ A is always a pattern.
+        """
+
+        # If the first pattern is empty, return the other as a pattern
+
+        if len(A) == 0:
+
+            return Main.Pattern(B)
+
+        # Get the dominant pattern type and convert B
+
+        cls = Main.Dominant(A, B)
+        
+        A = cls(A)
+        B = cls(B)
+
+        # Calculate total length before operations
+
+        i, length = 0, LCM(len(A), len(B))
+
+        P1 = []
+
+        while i < length:
+
+            try:
+
+                val = self.operate(A[i], B[i])
+
+            except ZeroDivisionError:
+
+                val = 0
+
+            P1.append(val)
+            i += 1
+
+        return cls(P1)
+
+# General operations
+def Nil(a, b):  return a
+def Add(a, b):  return a + b
+def Sub(a, b):  return a - b
+def Mul(a, b):  return a * b
+def Div(a, b):  return a / b
+def Mod(a, b):  return a % b
+def Pow(a, b):  return a ** b
+def Get(a, b):  return a[b]
+
+def rAdd(a, b): return b + a
+def rGet(a, b): return b[a]
+def rSub(a, b): return b - a
+def rDiv(a, b): return b / a
+def rMod(a, b): return b % a
+def rPow(a, b): return b ** a
+
+# Pattern operations
+PAdd = POperand(Add)
+
+PSub = POperand(Sub)
+PSub2 = POperand(rSub)
+
+PMul = POperand(Mul)
+
+PDiv = POperand(Div)
+PDiv2 = POperand(rDiv)
+
+PMod = POperand(Mod)
+PMod2 = POperand(rMod)
+
+PPow = POperand(Pow)
+PPow2 = POperand(rPow)
+
+PGet = POperand(Get)
+
+# Pattern comparisons -> need to maybe have a equals func?
+PEq = lambda a, b: (all([a[i]==b[i] for i in range(len(a))]) if len(a) == len(b) else False) if a.__class__ == b.__class__ else False
+PNe = lambda a, b: (any([a[i]!=b[i] for i in range(len(a))]) if len(a) == len(b) else True) if a.__class__ == b.__class__ else True
