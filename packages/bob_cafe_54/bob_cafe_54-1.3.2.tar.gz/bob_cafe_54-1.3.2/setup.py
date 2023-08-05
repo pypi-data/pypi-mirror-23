2f2cd5a0dfaa145153cdf9878f753f5e69bfd080
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:46 +0000

    Add package bob_cafe_54/1.3.2/bob_cafe_54-1.3.2.tar.gz

---
+++ packages/bob_cafe_54/bob_cafe_54-1.3.2.tar.gz/bob_cafe_54-1.3.2/setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+#  programa.py
+#  
+#  Copyright 2017 Henrique Brito <henriquebrito54@gmail.com>
+#  
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#  
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+
+from distutils.core import setup
+
+setup(
+	name = 'bob_cafe_54',
+	version = '1.3.2',
+	py_modules = ['bob_cafe_54'],
+	author = 'bobcafe',
+	author_email = 'bob_cafe_45@hotmail.com',
+	url = 'http://www.nonono.com',	
+	description = 'A simple printed of nested lists.',
+)
