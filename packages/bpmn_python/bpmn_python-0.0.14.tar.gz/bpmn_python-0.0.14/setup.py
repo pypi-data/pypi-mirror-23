2b098747a726b9b7ddf6d0ab505659583349d0b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:56 +0000

    Add package bpmn_python/0.0.14/bpmn_python-0.0.14.tar.gz

---
+++ packages/bpmn_python/bpmn_python-0.0.14.tar.gz/bpmn_python-0.0.14/setup.py
@@ -0,0 +1,34 @@
+#coding=utf-8
+import os
+
+from setuptools import setup, find_packages
+
+
+def read(fname):
+    """
+    Utility function to read the README file. Used for the long_description.
+    :param fname:
+    :return:
+    """
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    name="bpmn_python",
+    version="0.0.14",
+    author="Izbela Smietana, Krzysztof Honkisz",
+    # author_email = "honkiszkrzystof@gmail.com",
+    description=("Python library that allows to import/export BPMN diagram (as an XML file) and provides a simple "
+                 "visualization capabilities."),
+    license="GNU GENERAL PUBLIC LICENSE",
+    keywords=["bpmn", "xml"],
+    url="https://github.com/KrzyHonk/bpmn-python",
+    download_url="https://github.com/KrzyHonk/bpmn-python/tarball/0.0.14",
+    packages=find_packages(exclude=['docs', 'tests*']),
+    install_requires=[
+        'networkx',
+        'matplotlib',
+        'pydotplus',
+    ],
+    long_description=read('README.md'),
+)
