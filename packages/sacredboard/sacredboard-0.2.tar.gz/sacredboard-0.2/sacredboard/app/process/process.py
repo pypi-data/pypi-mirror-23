b60483d607c0a3a53bf7dc3a1bdfb101832065ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:42 +0000

    Add package sacredboard/0.2/sacredboard-0.2.tar.gz

---
+++ packages/sacredboard/sacredboard-0.2.tar.gz/sacredboard-0.2/sacredboard/app/process/process.py
@@ -0,0 +1,105 @@
+# coding=utf-8
+import atexit
+import os
+import select
+import time
+from subprocess import Popen, PIPE
+
+
+class Process:
+    instances = []  # type: List[Process]
+
+    def __init__(self, command):
+        if type(command == list):
+            self.command = command
+        else:
+            self.command = command.split(" ")
+
+        self.proc = None  # type: Popen
+
+    def run(self):
+        environment = os.environ.copy()
+        # necessary for reading from processes that don't flush
+        # stdout automatically
+        environment["PYTHONUNBUFFERED"] = "1"
+        self.proc = Popen(self.command, env=environment, stdout=PIPE)
+        Process.instances.append(self)
+
+    def is_running(self):
+        return self.proc is not None and not bool(self.proc.poll())
+
+    def read_line(self, time_limit=None):
+        if self.proc is not None:
+            poll_obj = select.poll()
+            poll_obj.register(self.proc.stdout, select.POLLIN)
+            start = time.time()
+            while time_limit is None or time.time() - start < time_limit:
+                poll_result = poll_obj.poll(0)
+                if poll_result:
+                    line = self.proc.stdout.readline().decode()
+                    return line
+                else:
+                    time.sleep(0.05)
+            raise TimeoutError()
+        else:
+            return None
+
+    def terminate(self, wait=False):
+        if self.proc is not None:
+            self.proc.stdout.close()
+            self.proc.terminate()
+            if wait:
+                self.proc.wait()
+
+    def pid(self):
+        if self.proc is not None:
+            return self.proc.pid
+        else:
+            return None
+
+    @staticmethod
+    def terminate_all(wait=False):
+        """
+
+        :param wait: Wait for each to terminate
+        :type wait: bool
+        :return:
+        :rtype:
+        """
+        for instance in Process.instances:
+            if instance.is_running():
+                instance.terminate(wait)
+
+    @staticmethod
+    def create_process(command):
+        if getattr(select, "poll", None) is not None:
+            return Process(command)
+        else:
+            return WindowsProcess(command)
+
+
+class WindowsProcess(Process):
+    def __init__(self, command):
+        Process.__init__(self, command)
+
+    def read_line(self, time_limit=None):
+        """ Time limit has no effect.
+         The operation will always block on Windows."""
+        if self.proc is not None:
+            return self.proc.stdout.readline().decode()
+        else:
+            return None
+
+
+# Kill all on exit
+atexit.register(Process.terminate_all)
+
+
+class ProcessError(Exception):
+    pass
+
+
+class UnexpectedOutputError(ProcessError):
+    def __init__(self, output, expected=None):
+        self.expected = expected
+        self.output = output
---
+++ packages/sacredboard/sacredboard-0.2.tar.gz/sacredboard-0.2/sacredboard/app/webapi/routes.py
@@ -0,0 +1,105 @@
+# coding=utf-8
+import re
+from pathlib import Path
+
+from flask import Blueprint
+from flask import current_app
+from flask import render_template
+from flask import request, Response, redirect, url_for
+
+from ..process.tensorboard import TensorboardNotFoundError,\
+                                  stop_all_tensorboards
+from ..process import process, tensorboard
+from ..webapi.runs import get_runs
+
+routes = Blueprint("routes", __name__)
+
+
+@routes.route("/")
+def index():
+    return redirect(url_for("routes.show_runs"))
+
+
+@routes.route("/_tests")
+def tests():
+    return redirect(url_for("static", filename="scripts/tests/index.html"))
+
+
+@routes.route("/runs")
+def show_runs():
+    # return render_template("runs.html", runs=data.runs(), type=type)
+    return render_template("runs.html", runs=[], type=type)
+
+
+@routes.route("/api/run")
+def api_runs():
+    return get_runs()
+
+
+@routes.route("/api/run/<run_id>")
+def api_run(run_id):
+    data = current_app.config["data"]
+    run = data.get_run(run_id)
+    records_total = 1 if run is not None else 0
+    if records_total == 0:
+        return Response(
+            render_template(
+                "api/error.js",
+                error_code=404,
+                error_message="Run %s not found." % run_id),
+            status=404,
+            mimetype="application/json")
+    records_filtered = records_total
+    return Response(render_template("api/runs.js", runs=[run], draw=1,
+                                    recordsTotal=records_total,
+                                    recordsFiltered=records_filtered,
+                                    full_object=True),
+                    mimetype="application/json")
+
+
+@routes.route("/tensorboard/start/<run_id>/<int:tflog_id>")
+def run_tensorboard(run_id, tflog_id):
+    data = current_app.config["data"]
+    # optimisticaly suppose the run exists...
+    run = data.get_run(run_id)
+    base_dir = Path(run["experiment"]["base_dir"])
+    log_dir = Path(run["info"]["tensorflow"]["logdirs"][tflog_id])
+    # TODO ugly!!!
+    if log_dir.is_absolute():
+        path_to_log_dir = log_dir
+    else:
+        path_to_log_dir = base_dir.joinpath(log_dir)
+
+    port = int(tensorboard.run_tensorboard(str(path_to_log_dir)))
+    url_root = request.url_root
+    url_parts = re.search("://([^:/]+)", url_root)
+    redirect_to_address = url_parts.group(1)
+    return redirect("http://%s:%d" % (redirect_to_address, port))
+
+
+@routes.route("/tensorboard/stop", methods=['GET', 'POST'])
+def close_tensorboards():
+    stop_all_tensorboards()
+    return "Stopping tensorboard"
+
+
+@routes.errorhandler(TensorboardNotFoundError)
+def handle_tensorboard_not_found(e):
+    return "Tensorboard not found on your system." \
+           " Please install tensorflow first. Sorry.", 503
+
+
+@routes.errorhandler(TimeoutError)
+def handle_tensorboard_timeout(e):
+    return "Tensorboard does not respond. Sorry.", 503
+
+
+@routes.errorhandler(process.UnexpectedOutputError)
+def handle_tensorboard_unexpected_output(e: process.UnexpectedOutputError):
+    return "Tensorboard outputted '%s'," \
+           " but the information expected was: '%s'. Sorry." \
+           % (e.output, e.expected), 503
+
+
+def setup_routes(app):
+    app.register_blueprint(routes)
