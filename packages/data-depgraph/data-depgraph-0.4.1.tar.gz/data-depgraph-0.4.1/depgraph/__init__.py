148e86ab10425465911fb8412c7b61075d25137e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:00 +0000

    Add package data-depgraph/0.4.1/data-depgraph-0.4.1.tar.gz

---
+++ packages/data-depgraph/data-depgraph-0.4.1.tar.gz/data-depgraph-0.4.1/depgraph/__init__.py
@@ -0,0 +1,4 @@
+from .depgraph import (Dataset, DatasetGroup,
+                       is_older, is_acyclic,
+                       graphviz, buildmanager, buildall,
+                       MISSING, PARENTNEWER, PARENTMISSING)
