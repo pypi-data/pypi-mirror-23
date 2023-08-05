8fb43f0bacd926a0e89c0f11e059faa270d31381
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:40 +0000

    Add package utagent/0.0.20/utagent-0.0.20-py2.py3-none-any.whl

---
+++ packages/utagent/utagent-0.0.20-py2.py3-none-any.whl/slave/config.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+config = {
+    "macaca_dir": None,
+    "environment": None,
+}
+
+
+environment = {
+    "alpha": "https://httpizza.alpha.elenet.me/waimai.slave/",
+}
+
+API_PORT = 9096
+
+# 工作目录
+WORK_SPACE = "/Users/mafei/UITest/"
+
+# 测试用例目录
+TEST_CASE_SPACE = "/Users/mafei/UITest/eleme/"
