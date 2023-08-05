e0fadbf35fb445345647bda7f3c97ee00f20baad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:20 +0000

    Add package ehp/1.1.0/ehp-1.1.0.tar.gz

---
+++ packages/ehp/ehp-1.1.0.tar.gz/ehp-1.1.0/setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+
+setup(name="ehp",
+      version="1.1.0",
+      py_modules=["ehp"],
+      author="Iury O. G. Figueiredo",
+      author_email="ioliveira@id.uff.br",
+      url="",
+      Description="Parsing html/xml documents in a faster and easier way")
+
+
+
+
+
+
+
+
+
