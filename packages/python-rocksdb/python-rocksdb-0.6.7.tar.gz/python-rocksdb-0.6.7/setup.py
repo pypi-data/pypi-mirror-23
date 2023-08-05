3c4dbad8185a6306777e2e5ef3c9072e8179cdf9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:37 +0000

    Add package python-rocksdb/0.6.7/python-rocksdb-0.6.7.tar.gz

---
+++ packages/python-rocksdb/python-rocksdb-0.6.7.tar.gz/python-rocksdb-0.6.7/setup.py
@@ -0,0 +1,49 @@
+from setuptools import setup
+from setuptools import find_packages
+from distutils.extension import Extension
+
+try:
+    from Cython.Build import cythonize
+except ImportError:
+    def cythonize(extensions): return extensions
+    sources = ['rocksdb/_rocksdb.cpp']
+else:
+    sources = ['rocksdb/_rocksdb.pyx']
+
+mod1 = Extension(
+    'rocksdb._rocksdb',
+    sources,
+    extra_compile_args=[
+        '-std=c++11',
+        '-O3',
+        '-Wall',
+        '-Wextra',
+        '-Wconversion',
+        '-fno-strict-aliasing'
+    ],
+    language='c++',
+    libraries=[
+        'rocksdb',
+        'snappy',
+        'bz2',
+        'z'
+    ]
+)
+
+setup(
+    name="python-rocksdb",
+    version='0.6.7',
+    description="Python bindings for RocksDB",
+    keywords='rocksdb',
+    author='Ming Hsuan Tu',
+    author_email="Use the github issues",
+    url="https://github.com/twmht/python-rocksdb",
+    license='BSD License',
+    install_requires=['setuptools'],
+    package_dir={'rocksdb': 'rocksdb'},
+    packages=find_packages('.'),
+    ext_modules=cythonize([mod1]),
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest'],
+    include_package_data=True
+)
