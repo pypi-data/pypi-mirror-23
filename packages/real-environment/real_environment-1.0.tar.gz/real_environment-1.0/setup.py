26bf62c048daf9821c60c99fc5846cff7d1fae25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:58 +0000

    Add package real-environment/1.0/real_environment-1.0.tar.gz

---
+++ packages/real-environment/real_environment-1.0.tar.gz/real_environment-1.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+
+setup(
+    name='real_environment',
+    version='1.0',
+    packages=['real_environment'],
+    url='https://github.com/cemkiy/python-shell-environment',
+    license='GNU GENERAL PUBLIC LICENSE',
+    author='Cem KIY',
+    author_email='cemekiy@gmail.com',
+    description='python shell environment management',
+    install_requires=[],
+    classifiers=(
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5'
+    ),
+)
