7d89842e0ec22f9076a25dba9a524c1e6bcf7db0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:09 +0000

    Add package robotica/0.1.6/robotica-0.1.6-py2.py3-none-any.whl

---
+++ packages/robotica/robotica-0.1.6-py2.py3-none-any.whl/robotica-0.1.6.dist-info/DESCRIPTION.rst
@@ -0,0 +1,101 @@
+========
+Robotica
+========
+
+
+.. image:: https://img.shields.io/pypi/v/robotica.svg
+        :target: https://pypi.python.org/pypi/robotica
+
+.. image:: https://img.shields.io/travis/brianmay/robotica.svg
+        :target: https://travis-ci.org/brianmay/robotica
+
+.. image:: https://readthedocs.org/projects/robotica/badge/?version=latest
+        :target: https://robotica.readthedocs.io/en/latest/?badge=latest
+        :alt: Documentation Status
+
+.. image:: https://pyup.io/repos/github/brianmay/robotica/shield.svg
+     :target: https://pyup.io/repos/github/brianmay/robotica/
+     :alt: Updates
+
+
+Robotic maid to scare innocent children
+
+
+* Free software: GNU General Public License v3
+* Documentation: https://robotica.readthedocs.io.
+
+
+Features
+--------
+
+* TODO
+
+Credits
+---------
+
+This package was created with Cookiecutter_ and the `audreyr/cookiecutter-pypackage`_ project template.
+
+.. _Cookiecutter: https://github.com/audreyr/cookiecutter
+.. _`audreyr/cookiecutter-pypackage`: https://github.com/audreyr/cookiecutter-pypackage
+
+
+
+=======
+History
+=======
+
+0.1.5 (2017-06-25)
+------------------
+
+Added
+~~~~~
+* disabled option for schedules, to disable without deleting.
+
+Fixes
+~~~~~
+* Don't replace other schedules unless this one is active.
+
+0.1.5 (2017-06-25)
+------------------
+
+Added
+~~~~~
+* Support acting on list of lights or groups.
+* Sending message to all lights asynchronously.
+* One schedule can override another schedule.
+
+0.1.4 (2017-06-24)
+------------------
+
+Fixes
+~~~~~
+* Add missing required depends.
+* Handle Device Offline errors correctly.
+
+0.1.3 (2017-06-24)
+------------------
+
+Added
+~~~~~
+* Schedule config file support.
+* Requires my fork of aiolifx.
+
+0.1.2 (2017-06-19)
+------------------
+
+Fixes
+~~~~~
+* PyPI meta information.
+* day_of_week value incorrect.
+
+0.1.1 (2017-06-18)
+------------------
+
+* No changes.
+
+0.1.0 (2017-06-18)
+------------------
+
+* First release on PyPI.
+
+
---
+++ packages/robotica/robotica-0.1.6-py2.py3-none-any.whl/robotica/lifx.py
@@ -0,0 +1,96 @@
+import asyncio
+import logging
+from typing import List, Coroutine, Callable
+
+import aiolifx
+from aiolifx.aiolifx import DeviceOffline
+
+logger = logging.getLogger(__name__)
+
+
+class Bulbs:
+    def __init__(self, loop: asyncio.AbstractEventLoop):
+        self._loop = loop
+        self.bulbs = []  # type: List[aiolifx.aiolifx.Light]
+
+    def register(self, bulb: aiolifx.aiolifx.Light) -> None:
+        logger.info("Register light %s.", bulb.mac_addr)
+        self._loop.create_task(self.async_register(bulb))
+
+    async def async_register(self, bulb: aiolifx.aiolifx.Light) -> None:
+        try:
+            await bulb.get_metadata(loop=self._loop)
+            self.bulbs.append(bulb)
+            logger.info("Got light %s (%s).", bulb.mac_addr, bulb.label)
+        except DeviceOffline:
+            logger.error("Light is offline %s", bulb.mac_addr)
+
+    def unregister(self, bulb: aiolifx.aiolifx.Light) -> None:
+        logger.info("Unregister light %s (%s).", bulb.mac_addr, bulb.label)
+        idx = 0
+        for x in list([y.mac_addr for y in self.bulbs]):
+            if x == bulb.mac_addr:
+                del(self.bulbs[idx])
+                break
+            idx += 1
+
+    def get_by_group(self, group: str) -> 'Bulbs':
+        result = Bulbs(self._loop)
+        result.bulbs = list(filter(lambda b: b.group == group, self.bulbs))
+        return result
+
+    def get_by_label(self, label: str) -> 'Bulbs':
+        result = Bulbs(self._loop)
+        result.bulbs = list(filter(lambda b: b.label == label, self.bulbs))
+        return result
+
+    def get_by_lists(self, *, groups: List[str]=None, labels: List[str]=None) -> 'Bulbs':
+        lights = set()
+        if groups is not None:
+            for group in groups:
+                lights |= set(filter(lambda b: b.group == group, self.bulbs))
+        if labels is not None:
+            for label in labels:
+                lights |= set(filter(lambda b: b.label == label, self.bulbs))
+        result = Bulbs(self._loop)
+        result.bulbs = list(lights)
+        return result
+
+    async def _do_for_every_light(self, fun: Callable[[aiolifx.aiolifx.Light], Coroutine[any, any, None]]):
+        coroutines = []
+        for bulb in self.bulbs:
+            coroutines.append(fun(bulb))
+        await asyncio.gather(*coroutines, loop=self._loop)
+
+    async def _wake_sup(self, bulb: aiolifx.aiolifx.Light) -> None:
+        try:
+            power = await bulb.get_power()
+            if not power:
+                await bulb.set_color([58275, 0, 0, 2500])
+            await bulb.set_power(True)
+            await bulb.set_color([58275, 0, 65365, 2500], duration=60000)
+        except DeviceOffline:
+            logger.info("Light is offline %s (%s).", bulb.mac_addr, bulb.label)
+
+    async def wake_up(self) -> None:
+        await self._do_for_every_light(self._wake_up)
+
+    async def _flash(self, bulb: aiolifx.aiolifx.Light) -> None:
+        try:
+            # transient, color, period,cycles,duty_cycle,waveform
+            await bulb.set_waveform({
+                "color": [0, 0, 0, 3500],
+                "transient": 1,
+                "period": 100,
+                "cycles": 30,
+                "duty_cycle": 0,
+                "waveform": 0
+            })
+        except DeviceOffline:
+            logger.info("Light is offline %s (%s).", bulb.mac_addr, bulb.label)
+
+    async def flash(self) -> None:
+        await self._do_for_every_light(self._flash)
+
+    def __str__(self):
+        return ", ".join([str(b.label) for b in self.bulbs])
