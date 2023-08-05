30de4b0c8584d1213c544bae5877981268427e1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:44 +0000

    Add package nougat/0.1.1a1/nougat-0.1.1a1.tar.gz

---
+++ packages/nougat/nougat-0.1.1a1.tar.gz/nougat-0.1.1a1/nougat/utils.py
@@ -0,0 +1,126 @@
+import inspect
+import json
+from nougat.exceptions import UnknownMiddlewareException, ConfigException, ResponseContentCouldNotFormat
+from cgi import parse_header
+
+def is_middleware(func):
+    """
+    test whether it is a middleware
+    :return: Boolean
+    """
+    args = list(inspect.signature(func).parameters.items())
+
+    if not inspect.iscoroutinefunction(func):
+        print("hello")
+        raise UnknownMiddlewareException("middleware {} should be awaitable".format(func.__name__))
+
+    if len(args) != 2:
+        raise UnknownMiddlewareException("middleware {} should has 2 params named ctx and next".format(func.__name__))
+
+    if args[0][0] != 'ctx':
+        raise UnknownMiddlewareException("the first param's name of middleware {} should be ctx".format(func.__name__))
+
+    if args[1][0] != 'next':
+        raise UnknownMiddlewareException("the second param's name of middleware {} should be next".format(func.__name__))
+
+
+def is_env_format(match, dict):
+    """
+    test whether it conform the standard
+    :return: Boolean
+    """
+    if not match.group('name').isupper():
+        raise ConfigException("<name>'{}' is not capitalized.".format(match.group('name')))
+
+    if not match.group('type') in dict:
+        raise ConfigException("<type>'{}' is out of list {}".format(match.group('type'), dict.keys()))
+
+
+def response_format(content):
+    """
+    format different type contents as str
+    """
+    if isinstance(content, str):
+        return "str", content
+    elif isinstance(content, list) or isinstance(content, dict):
+        return "json", json.dumps(content)
+    else:
+        try:
+            return "str", str(content)
+        except:
+            raise ResponseContentCouldNotFormat()
+
+
+def parse_multipart(fp, pdict):
+    """
+    """
+    import http.client
+    maxlen = 0
+
+    boundary = b""
+    if 'boundary' in pdict:
+        boundary = pdict['boundary']
+    nextpart = b"--" + boundary
+    lastpart = b"--" + boundary + b"--"
+    partdict = {}
+    terminator = b""
+
+    while terminator != lastpart:
+        bytes = -1
+        data = None
+        if terminator:
+            # At start of next part.  Read headers first.
+            headers = http.client.parse_headers(fp)
+            clength = headers.get('content-length')
+            if clength:
+                try:
+                    bytes = int(clength)
+                except ValueError:
+                    pass
+            if bytes > 0:
+                if maxlen and bytes > maxlen:
+                    raise ValueError('Maximum content length exceeded')
+                data = fp.read(bytes)
+            else:
+                data = b""
+        # Read lines until end of part.
+        lines = []
+        for line in fp.split(b"\n"):
+            if line.startswith(b"--"):
+                terminator = line.rstrip()
+                if terminator in (nextpart, lastpart):
+                    break
+            lines.append(line)
+        else:
+            terminator = lastpart
+
+        # Done with part.
+        if data is None:
+            continue
+        if bytes < 0:
+            if lines:
+                # Strip final line terminator
+                line = lines[-1]
+                if line[-2:] == b"\r\n":
+                    line = line[:-2]
+                elif line[-1:] == b"\n":
+                    line = line[:-1]
+                lines[-1] = line
+                data = b"".join(lines)
+        headers = http.client.parse_headers(fp)
+        line = headers['content-disposition']
+        if not line:
+            continue
+        key, params = parse_header(line)
+        if key != 'form-data':
+            continue
+        if 'name' in params:
+            name = params['name']
+        else:
+            continue
+        if name in partdict:
+            partdict[name].append(data)
+        else:
+            partdict[name] = [data]
+
+    return partdict
---
+++ packages/nougat/nougat-0.1.1a1.tar.gz/nougat-0.1.1a1/setup.py
@@ -0,0 +1,45 @@
+from setuptools import setup
+from os import path
+from nougat import __version__
+
+here = path.abspath(path.dirname(__file__))
+
+
+setup_kwargs = {
+    'name': 'nougat',
+    'version': __version__,
+    'description': 'Async API framework with human friendly params definition and automatic document',
+    'url': 'https://github.com/Kilerd/nougat',
+
+    'author': 'Kilerd Chan',
+    'author_email': 'blove694@gmail.com',
+
+    'license': 'MIT',
+
+    'classifiers': [
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Environment :: Web Environment',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+
+    'keywords': 'web framework async',
+
+    'packages': ['nougat'],
+    'install_requires': [
+        'httptools>=0.0.9',
+        'uvloop>=0.8.0',
+        'aiofiles>=0.3.0',
+        'aiohttp>=2.0.7',
+        'toml>=0.9.2'
+    ],
+}
+try:
+    setup(**setup_kwargs)
+
+except Exception as exception:
+
+    setup_kwargs['install_requires'].remove('uvloop>=0.8.0')
+    setup(**setup_kwargs)
