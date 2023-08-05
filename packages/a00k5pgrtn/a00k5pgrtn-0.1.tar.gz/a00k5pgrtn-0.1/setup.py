da0e0d8ba32f3503d97b90ab0b08169eba734d1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:52 +0000

    Add package a00k5pgrtn/0.1/a00k5pgrtn-0.1.tar.gz

---
+++ packages/a00k5pgrtn/a00k5pgrtn-0.1.tar.gz/a00k5pgrtn-0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages, setup
+
+setup(
+  name = 'a00k5pgrtn',
+  packages=find_packages(),
+  version = '0.1',
+  description = '',
+  author = '',
+  author_email = '',
+  url = '',
+  keywords = [],
+  classifiers = [],
+)
