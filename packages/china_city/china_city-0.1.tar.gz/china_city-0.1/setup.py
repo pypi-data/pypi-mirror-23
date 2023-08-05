a52ff903f27aed30e021fba8532ddbd53b7c650e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:31 +0000

    Add package china_city/0.1/china_city-0.1.tar.gz

---
+++ packages/china_city/china_city-0.1.tar.gz/china_city-0.1/setup.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+setup(
+    name='china_city',
+    version='0.1',
+    description='Include China city name',
+    author='Quintin',
+    author_email='quintinqiang@gmail.com',
+    install_requires=[],
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    url='https://github.com/FrankQiang/china_city',
+    keywords='China city',
+    classifiers=[
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
