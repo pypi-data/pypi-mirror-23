281a8fe8640a7cde81cf6ee0bfa2ad3156d44a0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:32 +0000

    Add package osfclient/0.0.1/osfclient-0.0.1.tar.gz

---
+++ packages/osfclient/osfclient-0.0.1.tar.gz/osfclient-0.0.1/osfclient/tests/test_uploading.py
@@ -0,0 +1,58 @@
+"""Test `osf upload` command"""
+
+from mock import call
+from mock import patch
+from mock import mock_open
+
+import pytest
+
+from osfclient import OSF
+from osfclient.cli import upload
+from osfclient.models import Project
+
+from osfclient.tests.mocks import MockArgs
+from osfclient.tests.mocks import MockProject
+from osfclient.tests.mocks import MockStorage
+
+
+def test_anonymous_doesnt_work():
+    args = MockArgs(project='1234')
+
+    with pytest.raises(SystemExit) as e:
+        upload(args)
+
+    expected = 'upload a file you need to provide a username and password'
+    assert expected in e.value.args[0]
+
+
+@patch.object(OSF, 'project', return_value=MockProject('1234'))
+@patch.object(Project, 'storage', return_value=MockStorage('osfstorage'))
+def test_select_project(Project_storage, OSF_project):
+    args = MockArgs(username='joe@example.com',
+                    project='1234',
+                    source='foo/bar.txt',
+                    destination='bar/bar/foo.txt')
+
+    def simple_getenv(key):
+        if key == 'OSF_PASSWORD':
+            return 'secret'
+
+    fake_open = mock_open()
+
+    with patch('osfclient.cli.open', fake_open):
+        with patch('osfclient.cli.os.getenv', side_effect=simple_getenv):
+            upload(args)
+
+    assert call('foo/bar.txt', 'rb') in fake_open.mock_calls
+
+    # the mock project created by calling OSF().project()
+    fake_project = OSF_project.return_value
+    expected = [call('osfstorage')]
+    assert fake_project._storage_mock.mock_calls == expected
+    # assert fake_project.mock_calls == expected
+
+    expected = [call.create_file('bar/bar/foo.txt',
+                                 fake_open.return_value, update=False)]
+    # we should call the create_file method on the return
+    # value of _storage_mock
+    assert fake_project._storage_mock.return_value.mock_calls == expected
---
+++ packages/osfclient/osfclient-0.0.1.tar.gz/osfclient-0.0.1/setup.py
@@ -0,0 +1,56 @@
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+
+
+setup(
+    name='osfclient',
+    version='0.0.1',
+
+    description='An OSF command line library',
+    long_description='An OSF command line library',
+
+    # The project's main homepage.
+    url='https://github.com/dib-lab/osf-cli',
+
+    # Author details
+    author='The OSF-cli authors',
+    # Choose your license
+    license='BSD3',
+
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'License :: OSI Approved :: BSD License',
+        'Topic :: Utilities'
+    ],
+
+    # You can just specify the packages manually here if your project is
+    # simple. Or you can use find_packages().
+    packages=find_packages(),
+
+    # List run-time dependencies here.  These will be installed by pip when
+    # your project is installed. For an analysis of "install_requires" vs pip's
+    # requirements files see:
+    # https://packaging.python.org/en/latest/requirements.html
+    install_requires=['requests', 'tqdm'],
+
+    # To provide executable scripts, use entry points in preference to the
+    # "scripts" keyword. Entry points provide cross-platform support and allow
+    # pip to create the appropriate form of executable for the target platform.
+    entry_points={
+        'console_scripts': [
+            'osf=osfclient.__main__:main',
+        ],
+    },
+)
