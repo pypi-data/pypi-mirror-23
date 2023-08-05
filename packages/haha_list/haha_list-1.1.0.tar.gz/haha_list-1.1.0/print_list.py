f0da3e38fa6a885afca8b34a76dd5fc1b5af35f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:53 +0000

    Add package haha_list/1.1.0/haha_list-1.1.0.tar.gz

---
+++ packages/haha_list/haha_list-1.1.0.tar.gz/haha_list-1.1.0/print_list.py
@@ -0,0 +1,15 @@
+#encoding=utf8
+
+"""这是print_list模块，提供了一个名为print_list()的函数，这个函数的作用是打印列表，其中有可能包含嵌套列表"""
+
+def print_list(lists,level):
+    """这个函数取一个位置参数，名为lists，这可以是任何python列表（也可以是包含嵌套列表的列表），所指定的列表中的每个数据项会
+    （递归的）输出到屏幕上，各数据项占一行"""
+    for each_item in lists:
+        if isinstance(each_item,list):
+            print_list(each_item,level+1)
+        else:
+            for tab in range(level):
+                print("\t",end="")
+            print(each_item)
+
