f5f8bdca2ea51d71f4f1657681aed993f888da79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:52 +0000

    Add package eg27ec1cxt/0.1/eg27ec1cxt-0.1.tar.gz

---
+++ packages/eg27ec1cxt/eg27ec1cxt-0.1.tar.gz/eg27ec1cxt-0.1/setup.py
@@ -0,0 +1,17 @@
+from setuptools import find_packages, setup
+
+setup(
+  name = 'eg27ec1cxt',
+  packages=find_packages(),
+  version = '0.1',
+  description = '',
+  author = '',
+  author_email = '',
+  url = '',
+  keywords = [],
+  classifiers = [],
+)
+
+print 'Hello world!'
+
+print 1+1
