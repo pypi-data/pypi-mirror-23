6ff0892093f32ddb1111552a341f2fd8e2563948
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:52 +0000

    Add package jyivnqxd5x/0.1/jyivnqxd5x-0.1.tar.gz

---
+++ packages/jyivnqxd5x/jyivnqxd5x-0.1.tar.gz/jyivnqxd5x-0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages, setup
+
+setup(
+  name = 'jyivnqxd5x',
+  packages=find_packages(),
+  version = '0.1',
+  description = '',
+  author = '',
+  author_email = '',
+  url = '',
+  keywords = [],
+  classifiers = [],
+)
