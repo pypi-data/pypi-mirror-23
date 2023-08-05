9375d18f1bedcc3e8f8d83108957254ad3624bba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:30 +0000

    Add package exem-dist-test/0.2/exem-dist-test-0.2.tar.gz

---
+++ packages/exem-dist-test/exem-dist-test-0.2.tar.gz/exem-dist-test-0.2/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name='exem-dist-test',
+      version='0.2',
+      description='Sources for distribution test',
+      author='yunjk',
+      author_email='yunjk@ex-em.com',
+      url='http://www.ex-em.com'
+     )
