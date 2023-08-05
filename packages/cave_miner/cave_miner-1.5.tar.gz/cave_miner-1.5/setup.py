94f807fe5a0918aa226c6a07b18a4c680dfe076b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:50 +0000

    Add package cave_miner/1.5/cave_miner-1.5.tar.gz

---
+++ packages/cave_miner/cave_miner-1.5.tar.gz/cave_miner-1.5/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+setup(
+  name = 'cave_miner',
+  packages = ['cave_miner', 'cave_miner.formats'],
+  entry_points={
+    'console_scripts': [
+      'cave_miner=cave_miner.main:main',
+    ],
+  },
+  install_requires = ['docopt==0.6.2', 'kaitaistruct==0.7'],
+  version = '1.5',
+  description = 'Search for code cave in all binaries',
+  author = 'DENIAU Antonin',
+  author_email = 'antonin.deniau@protonmail.com',
+  url = 'https://github.com/Antonin-Deniau/cave_miner',
+  keywords = ['code', 'cave', 'codecave', 'macho', 'mach-o', 'elf', 'pe'],
+  classifiers = [],
+)
+
