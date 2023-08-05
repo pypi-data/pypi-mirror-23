78a478f7dee12649d1580f5f3ea78adb2f0e04b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:35 +0000

    Add package DSPP-Keras/0.0.5/DSPP-Keras-0.0.5.tar.gz

---
+++ packages/DSPP-Keras/DSPP-Keras-0.0.5.tar.gz/DSPP-Keras-0.0.5/dsppkeras/datasets/dspp.py
@@ -0,0 +1,31 @@
+from ..utils.data_utils import get_file
+import numpy as np
+import json
+import tarfile
+import codecs
+
+def load_data(path='peptone_dspp.tar.gz'):
+    """Loads the MNIST dataset.
+
+    # Arguments
+        path: path where to cache the dataset locally
+            (relative to ~/.keras/datasets).
+
+    # Returns
+        Tuple of Numpy arrays: `(x_train, y_train), (x_test, y_test)`.
+    """
+    path = get_file(path, origin='https://github.com/PeptoneInc/dspp-data/blob/master/database.tar.gz?raw=true')
+    tar = tarfile.open(path, "r:gz")
+    #print("Open archive at {}".format(path))
+    for member in tar.getmembers():
+         f = tar.extractfile(member)
+         if f is None: continue
+         content = f.read()
+         #print("Load json {}".format(f))
+         database = json.loads(content.decode('utf-8'))
+         break
+
+    X, Y = database['X'], database['Y']
+    f.close()
+    X, Y = map(lambda element: np.array(list(element)), X), map(lambda element: np.array(element), Y)
+    return list(X), list(Y)
---
+++ packages/DSPP-Keras/DSPP-Keras-0.0.5.tar.gz/DSPP-Keras-0.0.5/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+from setuptools import find_packages
+
+setup(name='DSPP-Keras',
+      version='0.0.5',
+      description='Integration of Database of structural propensities of proteins (dSPP) with Keras Machine Learning Library',
+      author='Jan Domanski',
+      author_email='jan@peptone.io',
+      url='https://github.com/PeptoneInc/dspp-keras',
+      download_url='https://github.com/PeptoneInc/dspp-keras/archive/v0.0.3.tar.gz',
+      license='MIT',
+      install_requires=['keras', 'numpy', 'h5py'],
+      packages=find_packages())
