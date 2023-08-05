cc177a0ea3d5a0b2568ad6b0b7661681af59f1e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:56 +0000

    Add package pyfox/0.1/pyfox-0.1.tar.gz

---
+++ packages/pyfox/pyfox-0.1.tar.gz/pyfox-0.1/setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+setup(
+    name = 'pyfox',
+    packages = ['pyfox'], # this must be the same as the name above
+    version = '0.1',
+    description = 'Shell for foxtrot',
+    author = 'Shubham Sharma',
+    author_email = 'shubham.sha12@gmail.com',
+    url = 'https://github.com/gabber12/pyfox', # use the URL to the github repo
+    download_url = 'https://github.com/gabber12/pyfox/archive/0.1.tar.gz', # I'll explain this in a second
+    keywords = ['shell', 'foxtrot', 'cli'], # arbitrary keywords
+    classifiers = [],
+    entry_points = {
+                    'console_scripts': ['foxtrot=pyfox.commands:boot'],
+    }
+)
