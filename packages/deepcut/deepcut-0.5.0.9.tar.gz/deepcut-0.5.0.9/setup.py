4c5e81fb6ed59a4d3cc6eea4aa94934f150dac9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:02 +0000

    Add package deepcut/0.5.0.9/deepcut-0.5.0.9.tar.gz

---
+++ packages/deepcut/deepcut-0.5.0.9.tar.gz/deepcut-0.5.0.9/setup.py
@@ -0,0 +1,23 @@
+"""
+Thai word Segmentation using Convolutional Neural Network
+"""
+
+from distutils.core import setup
+import setuptools
+
+setup(
+  name = 'deepcut',
+  packages = ['deepcut'], 
+  package_dir={'deepcut': 'deepcut'},
+  package_data={'mypkg': ['weight/*']},
+  version = '0.5.0.9',
+  install_requires=['keras', 'pandas', 'scipy', 'numpy'],
+  license='MIT',
+  description = 'A Thai word tokenization library using Deep Neural Network',
+  author = 'Rakpong Kittinaradorn',
+  author_email = 'r.kittinaradorn@gmail.com',
+  url = 'https://github.com/rkcosmos/deepcut',
+  download_url = 'https://github.com/rkcosmos/deepcut/package/0.5.zip', 
+  keywords = ['thai word segmentation deep learning neural network development'],
+  classifiers = ['Development Status :: 3 - Alpha'],
+)
