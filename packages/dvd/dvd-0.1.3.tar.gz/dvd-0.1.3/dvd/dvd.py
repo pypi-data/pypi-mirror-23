9018e0b9d42f85b2d15017783f093ede25b6ef94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:21 +0000

    Add package dvd/0.1.3/dvd-0.1.3.tar.gz

---
+++ packages/dvd/dvd-0.1.3.tar.gz/dvd-0.1.3/dvd/dvd.py
@@ -0,0 +1,36 @@
+import tensorflow as tf
+import numpy as np
+from scipy.misc import imread, imresize
+import vgg16
+from vgg16 import *
+
+
+def get_embedding_x(img):
+    '''
+            Args    : Numpy Image Vector
+            Returns : Embedded feature vector of length 4096
+    '''
+    sess = tf.Session()
+    imgs = tf.placeholder(tf.float32, [None, 224, 224, 3])
+    vgg = vgg16(imgs, '/tmp/vgg16_weights.npz', sess)
+    img = imresize(img, (224, 224))
+    img = np.array(img)
+    emb = sess.run(vgg.emb, feed_dict={vgg.imgs: [img]})
+    return emb
+
+
+def get_embedding_X(img):
+    '''
+            Args 	: Numpy Images vector
+            Returns : Embedded Matrix of length Samples, 4096
+    '''
+    sess = tf.Session()
+    imgs = tf.placeholder(tf.float32, [None, 224, 224, 3])
+    vgg = vgg16(imgs, 'vgg16_weights.npz', sess)
+
+    img = [imresize(img_x, (224, 224)) for img_x in img]
+    img = np.array(img)
+    emb = sess.run(vgg.emb, feed_dict={vgg.imgs: img})
+    return emb
+
+
