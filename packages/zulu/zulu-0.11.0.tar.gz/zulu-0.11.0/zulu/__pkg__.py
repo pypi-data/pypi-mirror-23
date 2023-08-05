81e15fff71c5b08e9e363fe4dd1017b76afb51d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:15 +0000

    Add package zulu/0.11.0/zulu-0.11.0.tar.gz

---
+++ packages/zulu/zulu-0.11.0.tar.gz/zulu-0.11.0/zulu/__pkg__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""Define project metadata
+"""
+
+__package_name__ = 'zulu'
+__description__ = ('A drop-in replacement for native datetimes '
+                   'that embraces UTC')
+__url__ = 'https://github.com/dgilland/zulu'
+__version__ = '0.11.0'
+__author__ = 'Derrick Gilland'
+__email__ = 'dgilland@gmail.com'
+__license__ = 'MIT License'
