6cd17b5d6d002a36bb607eabc859d025df765474
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:18 +0000

    Add package redshirt/0.1.0a1/redshirt-0.1.0a1.tar.gz

---
+++ packages/redshirt/redshirt-0.1.0a1.tar.gz/redshirt-0.1.0a1/setup.py
@@ -0,0 +1,21 @@
+#! /usr/bin/env python
+
+from setuptools import setup
+
+setup(name="redshirt",
+      version="0.1.0a1",
+      scripts=['redshirt.py'],
+      author="Jack Laxson",
+      author_email="jack@getpizza.cat",
+      description="A HTTP management api for taskwarrior's taskd",
+      keywords=["taskd", "taskwarrior", "api", "management"],
+      license="GPL v3",
+      zip_safe=False,
+      install_requires=["bottle", "baker==1.3", "psutil", "packaging", "requests"],
+      url="https://github.com/jrabbit/taskd-redshirt",
+      classifiers=["Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
+                   "Topic :: System :: Monitoring",
+                   "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+                   "Programming Language :: Python :: 2.7",
+                   "Programming Language :: Python :: 3.5",
+                   ])
