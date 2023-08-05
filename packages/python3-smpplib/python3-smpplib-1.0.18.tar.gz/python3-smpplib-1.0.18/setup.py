c213f16d65f3ee9eb326fa151efd4e75b2d2cb7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:43 +0000

    Add package python3-smpplib/1.0.18/python3-smpplib-1.0.18.tar.gz

---
+++ packages/python3-smpplib/python3-smpplib-1.0.18.tar.gz/python3-smpplib-1.0.18/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+import sys
+
+extra = {}
+if sys.version_info >= (3,):
+    extra['use_2to3'] = True
+
+setup(name="python3-smpplib",
+      version='1.0.18',
+      description='SMPP library for python (fork https://github.com/podshumok/python-smpplib',
+      packages=find_packages(),
+      zip_safe=True,
+      classifiers=[
+        'Development Status :: 4 - Beta',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Communications :: Telephony',
+        'Intended Audience :: Telecommunications Industry',
+        'License :: OSI Approved',
+        ],
+      **extra
+)
