1acbb00a70891ab513b9a9f491644e92eca07b2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:40 +0000

    Add package csvmapper/0.4/csvmapper-0.4.tar.gz

---
+++ packages/csvmapper/csvmapper-0.4.tar.gz/csvmapper-0.4/csvmapper/__init__.py
@@ -0,0 +1,18 @@
+# =============================
+#
+# CSV-Mapper
+# Copyright (C) Samarjeet Singh
+#
+# =============================
+
+# the parser
+from csv_parser import CSVParser, CSVWriter
+
+# import all mappers
+from mapper import *
+
+# utils
+from utils import CSVObject
+
+# converter
+from converter import *
