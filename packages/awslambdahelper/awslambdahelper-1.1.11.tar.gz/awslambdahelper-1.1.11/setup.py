a428d24da2b809d82bb99b6a3a8d7694ca7f95ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:51 +0000

    Add package awslambdahelper/1.1.11/awslambdahelper-1.1.11.tar.gz

---
+++ packages/awslambdahelper/awslambdahelper-1.1.11.tar.gz/awslambdahelper-1.1.11/setup.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+from setuptools.command.install import install as _install
+
+class install(_install):
+    def pre_install_script(self):
+        pass
+
+    def post_install_script(self):
+        pass
+
+    def run(self):
+        self.pre_install_script()
+
+        _install.run(self)
+
+        self.post_install_script()
+
+if __name__ == '__main__':
+    setup(
+        name = 'awslambdahelper',
+        version = '1.1.11',
+        description = '',
+        long_description = '',
+        author = 'Drew J. Sonne',
+        author_email = 'drew.sonne@gmail.com',
+        license = 'LGLP',
+        url = 'http://lambda.awshelpers.com/',
+        scripts = ['scripts/lambdahelper-bundler'],
+        packages = ['awslambdahelper'],
+        namespace_packages = [],
+        py_modules = [],
+        classifiers = [
+            'Development Status :: 3 - Alpha',
+            'Programming Language :: Python'
+        ],
+        entry_points = {},
+        data_files = [],
+        package_data = {},
+        install_requires = [
+            'boto3',
+            'pip>=7.1',
+            'setuptools~=35.0',
+            'wheel'
+        ],
+        dependency_links = [],
+        zip_safe = True,
+        cmdclass = {'install': install},
+        keywords = '',
+        python_requires = '',
+        obsoletes = [],
+    )
