e53795c6705bc61087b8ca294a0fcef3b8e17a2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:43 +0000

    Add package justpith/0.0.4/justpith-0.0.4.tar.gz

---
+++ packages/justpith/justpith-0.0.4.tar.gz/justpith-0.0.4/justpith/rabbit/receiver.py
@@ -0,0 +1,18 @@
+class Receiver:
+    def __init__(self,rabbit_instance,queue_name):
+        self.rabbit = rabbit_instance
+        self.queue_name = queue_name
+        self.channel = self.rabbit.channel()
+        self.queue = self.rabbit.queue(self.channel, queue_name)
+
+    def receiver_callback(self,channel, method, properties, body):
+        print(" [x] Received %r" % body)
+        if self.rabbit.no_ack == False:
+            channel.basic_ack(delivery_tag=method.delivery_tag)
+
+    def bind(self,exchange_name,binding_key):
+        self.exchange_name = exchange_name
+        self.rabbit.queue_bind(self.channel,exchange_name,self.queue,binding_key)
+
+    def consume(self,callback):
+        self.rabbit.consume(channel=self.channel, callback=callback, queue=self.queue_name)
