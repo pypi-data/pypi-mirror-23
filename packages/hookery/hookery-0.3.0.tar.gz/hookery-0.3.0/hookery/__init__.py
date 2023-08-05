c5120fde1544710f89eff0bad84ec4f871fd0bef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:09:39 +0000

    Add package hookery/0.3.0/hookery-0.3.0.tar.gz

---
+++ packages/hookery/hookery-0.3.0.tar.gz/hookery-0.3.0/hookery/__init__.py
@@ -0,0 +1,104 @@
+import collections
+
+try:
+    from inspect import signature
+except ImportError:
+    from funcsigs import signature
+
+
+class Event(object):
+    def __init__(self, name, register_hook_func, trigger_func=None, stop_condition=None):
+        self.name = name
+
+        self._register_hook_func = register_hook_func
+        self._trigger_func = trigger_func
+
+        if stop_condition is None:
+            self.stop_condition = lambda result: result is not None
+        elif callable(stop_condition):
+            self.stop_condition = stop_condition
+        else:
+            self.stop_condition = lambda result, sc=stop_condition: result == sc
+
+    def __call__(self, hook_func):
+        return self.register_hook(hook_func)
+
+    def __repr__(self):
+        return '<{} {!r}>'.format(self.__class__.__name__, self.name)
+
+    def register_hook(self, hook_func):
+        return self._register_hook_func(self.name, hook_func)
+
+    def trigger(self, **kwargs):
+        self._trigger_func(self, **kwargs)
+
+
+class HookRegistry(object):
+    def __init__(self, owner=None):
+        self._owner = owner
+
+        self._events = {}
+        self._hooks = collections.defaultdict(list)
+
+        # Internal event names start with double underscore
+        self.hook_registered = self.register_event('__hook_registered')
+
+    def register_event(self, name):
+        assert name not in self._events
+        event = Event(name, register_hook_func=self.register_hook, trigger_func=self.handle)
+        self._events[name] = event
+        return event
+
+    def register_hook(self, event_name, hook):
+        assert callable(hook)
+        if isinstance(event_name, Event):
+            event_name = event_name.name
+        assert event_name in self._events
+        self._hooks[event_name].append((signature(hook), hook))
+        if not event_name.startswith('__'):
+            self.hook_registered.trigger(event=self._events[event_name], hook=hook)
+        return hook
+
+    def unregister_hook(self, event_name, hook):
+        if isinstance(event_name, Event):
+            event_name = event_name.name
+        self._hooks[event_name] = [h for h in self._hooks[event_name] if h[1] is not hook]
+
+    def handle(self, event_, **kwargs):
+        if isinstance(event_, Event):
+            event_name = event_.name
+        else:
+            event_name = event_
+
+        event_ = self._events[event_name]
+
+        result = None
+
+        for hook_signature, hook in self._hooks[event_.name]:
+
+            # Collect kwargs which the hook is interested in.
+            # If hook wants **kwargs, that means it is interested in all,
+            # otherwise pass only the mentioned ones.
+
+            hook_kwargs = None
+            for _, param in hook_signature.parameters.items():
+                if param.kind == param.VAR_KEYWORD:
+                    hook_kwargs = kwargs
+
+            if hook_kwargs is None:
+                hook_kwargs = {}
+                for param in kwargs:
+                    if param in hook_signature.parameters:
+                        hook_kwargs[param] = kwargs[param]
+
+            # Prepare args and kwargs for the hook
+            bound_args = hook_signature.bind_partial(**hook_kwargs)
+
+            # Call the hook
+            result = hook(*bound_args.args, **bound_args.kwargs)
+
+            # Decide whether we should continue
+            if event_.stop_condition(result):
+                return result
+
+        return result
---
+++ packages/hookery/hookery-0.3.0.tar.gz/hookery-0.3.0/tests/test_hookery.py
@@ -0,0 +1,186 @@
+import time
+
+from hookery import HookRegistry
+
+
+def test_e2e():
+
+    class Collection(object):
+        def __init__(self):
+            self._hooks = HookRegistry(self)
+            self._data = set()
+            self.item_added = self._hooks.register_event('item_added')
+            self.item_removed = self._hooks.register_event('item_removed')
+
+        def add(self, item):
+            self._data.add(item)
+            self._hooks.handle(self.item_added, item=item, timestamp=time.time())
+
+        def remove(self, item):
+            self._data.remove(item)
+            self._hooks.handle(self.item_removed, item=item, timestamp=time.time())
+
+    bag = Collection()
+
+    calls = []
+
+    @bag.item_added
+    def item_added(item, timestamp):
+        assert isinstance(timestamp, float)
+        assert item == 'hello'
+        calls.append('first')
+
+    @bag.item_added
+    def item_added(timestamp, item):
+        assert isinstance(timestamp, float)
+        assert item == 'hello'
+        calls.append('second')
+        return True
+
+    @bag.item_added
+    def item_added(timestamp, item):
+        assert isinstance(timestamp, float)
+        assert item == 'hello'
+        calls.append('third')
+
+    @bag.item_removed
+    def item_removed(item):
+        assert item == 'hello'
+        calls.append('removed')
+
+    assert len(calls) == 0
+
+    bag.add('hello')
+    assert len(calls) == 2
+    assert calls == ['first', 'second']
+
+    bag.add('hello')
+    assert len(calls) == 4
+    assert calls[-2:] == ['first', 'second']
+
+    bag.remove('hello')
+    assert len(calls) == 5
+    assert calls[-1] == 'removed'
+
+    # Change the stop condition, just for the purposes of testing
+    bag.item_added.stop_condition = lambda result: False
+
+    bag.add('hello')
+    assert len(calls) == 8
+    assert calls[-3:] == ['first', 'second', 'third']
+
+
+def test_calling_event_returns_same_function():
+    hooks = HookRegistry()
+    a_event = hooks.register_event('a_event')
+
+    def f1():
+        pass
+
+    f2 = lambda x: x
+
+    assert a_event(f1) is f1
+    assert a_event(f2) is f2
+
+
+def test_hook_registered_hook():
+    hooks = HookRegistry()
+
+    hooks_registered = []
+
+    @hooks.hook_registered
+    def hook_registered(event, hook):
+        hooks_registered.append(('hook_registered', event.name, hook.__name__))
+
+    assert len(hooks_registered) == 0
+
+    something_happened = hooks.register_event('something_happened')
+
+    assert len(hooks_registered) == 0
+
+    @something_happened
+    def first_hook():
+        pass
+
+    assert len(hooks_registered) == 1
+
+    @something_happened
+    def second_hook():
+        pass
+
+    assert len(hooks_registered) == 2
+
+    assert hooks_registered == [
+        ('hook_registered', 'something_happened', 'first_hook'),
+        ('hook_registered', 'something_happened', 'second_hook'),
+    ]
+
+
+def test_hook_with_kwargs_gets_all_event_kwargs():
+    hooks = HookRegistry()
+    hooks.event1 = hooks.register_event('event1')
+
+    calls = []
+
+    @hooks.event1
+    def event1_handler1(**kwargs):
+        assert kwargs == {'a': 1,  'b': 2, 'c': 3, 'd': 4}
+        calls.append('handler1')
+
+    @hooks.event1
+    def event1_handler2(c, d):
+        assert c == 3
+        assert d == 4
+        calls.append('handler2')
+
+    assert len(calls) == 0
+
+    hooks.handle('event1', a=1, b=2, c=3, d=4)
+
+    assert len(calls) == 2
+
+
+def test_event_register_hook_and_trigger_methods():
+    hooks = HookRegistry()
+
+    calls = []
+
+    event1 = hooks.register_event('event1')
+
+    @event1
+    def handle_event(id):
+        calls.append(id)
+
+    event1.trigger(id=1)
+    assert calls == [1]
+
+    event1.trigger(id=2)
+    assert calls == [1, 2]
+
+
+def test_unregister_hook():
+    hooks = HookRegistry()
+
+    calls = []
+
+    a_event = hooks.register_event('a_event')
+
+    @a_event
+    def x_hook(id):
+        calls.append('x{}'.format(id))
+
+    @a_event
+    def y_hook(id):
+        calls.append('y{}'.format(id))
+
+    a_event.trigger(id=1)
+    a_event.trigger(id=2)
+
+    assert calls == ['x1', 'y1', 'x2', 'y2']
+
+    hooks.unregister_hook(a_event, x_hook)
+
+    a_event.trigger(id=3)
+    a_event.trigger(id=4)
+
+    assert calls == ['x1', 'y1', 'x2', 'y2', 'y3', 'y4']
