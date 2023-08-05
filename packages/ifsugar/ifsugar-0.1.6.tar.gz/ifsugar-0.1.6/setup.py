3b891023f119ebf6117bd94401f544e1e5ba5e02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:37 +0000

    Add package ifsugar/0.1.6/ifsugar-0.1.6.tar.gz

---
+++ packages/ifsugar/ifsugar-0.1.6.tar.gz/ifsugar-0.1.6/setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='ifsugar',
+       version='0.1.6',
+       description='syntax sugars for easyer understood coding: _if, _try, _times',
+       author='qorzj',
+       author_email='inull@qq.com',
+       maintainer='qorzj',
+       maintainer_email='inull@qq.com',
+       url='https://github.com/qorzj/ifsugar',
+       keywords='ifsugar syntax sugar times try if',
+       packages=['ifsugar'],
+       long_description="syntax sugars for easyer understood coding: _if, _try, _times",
+       license="Public domain",
+       platforms=["any"],
+       install_requires=[],
+      )
+
