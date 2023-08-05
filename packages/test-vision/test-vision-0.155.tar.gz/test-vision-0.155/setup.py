a3ddf918c5ef6e2ea172fd8a5cba4ece189c2bb5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:16 +0000

    Add package test-vision/0.155/test-vision-0.155.tar.gz

---
+++ packages/test-vision/test-vision-0.155.tar.gz/test-vision-0.155/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+
+
+setup(
+    name='test-vision',
+    packages=['vision'],
+    version='0.155',
+    description='An upload to s3 utility',
+    author='Fathom',
+    author_email='mirabel.ekwenugo@andela.com',
+    keywords=['upload', 'files', 's3'],
+    classifiers=[],
+    install_requires=[
+        'requests==2.13.0',
+        'appdirs==1.4.3',
+        'boto==2.46.1',
+        'click==6.7',
+        'cx-Freeze==5.0.1',
+        'Flask==0.12',
+        'Gooey==0.9.2.3',
+        'itsdangerous==0.24',
+        'mock==2.0.0',
+        'nose==1.3.7',
+        'packaging==16.8',
+        'PyInstaller==3.2.1',
+        'pypref==2.0.0',
+        'six==1.10.0',
+        'watchdog==0.8.3',
+        'Werkzeug==0.12',
+    ],
+)
