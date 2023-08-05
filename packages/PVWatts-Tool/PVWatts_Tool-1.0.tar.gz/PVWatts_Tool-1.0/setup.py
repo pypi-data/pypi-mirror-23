cb3b0fe0e652c37afc81a0a92725264f496d7429
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:25 +0000

    Add package PVWatts-Tool/1.0/PVWatts_Tool-1.0.tar.gz

---
+++ packages/PVWatts-Tool/PVWatts_Tool-1.0.tar.gz/PVWatts_Tool-1.0/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(
+    name='PVWatts_Tool',
+    version='1.0',
+    url='https://github.com/warnuk/PVWatts_Tool',
+    packages=['PVWatts_Tool'],
+    license='MIT',
+    author='warnuk',
+    author_email='warnuk@umich.edu',
+    description='A tool that calls on NREL APIs to generate useful information for solar PV resource planning.',
+    install_requires = ['pandas', 'requests', 'PyQt5'],
+    python_requires = '>=3',
+)
