22ffb3709a0f8b73001ab30ffb3a5c2b6d26716e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:15 +0000

    Add package analytracks/0.1/analytracks-0.1.tar.gz

---
+++ packages/analytracks/analytracks-0.1.tar.gz/analytracks-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='analytracks',
+      version='0.1',
+      description='The funniest joke in the world',
+      url='http://github.com/ddolbecke/analytracks',
+      author='dimds',
+      author_email='dimitri.desmet@uclouvain.be',
+      license='MIT',
+      packages=['analytracks'],
+      zip_safe=True)
