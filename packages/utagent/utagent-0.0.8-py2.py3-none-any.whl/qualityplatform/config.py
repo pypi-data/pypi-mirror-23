bd829fabdb5d5ff5e0dad54bbe7c2b9c74dfbcf8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:49 +0000

    Add package utagent/0.0.8/utagent-0.0.8-py2.py3-none-any.whl

---
+++ packages/utagent/utagent-0.0.8-py2.py3-none-any.whl/qualityplatform/config.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+config = {
+    "macaca_dir": None,
+    "verbose": None,
+}
+
+API_PORT = 9096
+
+# 工作目录
+WORK_SPACE = "/Users/mafei/UITest/"
+
+# 测试用例目录
+TEST_CASE_SPACE = "/Users/mafei/UITest/eleme/"
