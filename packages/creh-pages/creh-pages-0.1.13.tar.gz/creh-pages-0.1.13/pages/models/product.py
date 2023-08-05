34cc8b5ed8dd666f7d59a7e3376e8c438d327176
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:09:23 +0000

    Add package creh-pages/0.1.13/creh-pages-0.1.13.tar.gz

---
+++ packages/creh-pages/creh-pages-0.1.13.tar.gz/creh-pages-0.1.13/pages/models/product.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from django.db import models
+
+from pages.utils.database import AuditableMixin
+
+
+class Product(AuditableMixin):
+
+    page = models.ForeignKey('Page')
+
+    reference_id = models.CharField(
+        max_length=20,
+        unique=True
+    )
+
+    def __str__(self):
+        return self.reference_id
+
+    class Meta:
+        db_table = 'products'
+
