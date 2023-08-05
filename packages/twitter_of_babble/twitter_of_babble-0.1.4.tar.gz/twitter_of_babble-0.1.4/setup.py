ef432aa1e5577e680b61251c78b1a5b815b3167f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:42 +0000

    Add package twitter_of_babble/0.1.4/twitter_of_babble-0.1.4.tar.gz

---
+++ packages/twitter_of_babble/twitter_of_babble-0.1.4.tar.gz/twitter_of_babble-0.1.4/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='twitter_of_babble',
+      version='0.1.4',
+      description='Twitter Topic Models and Linguistic Diversity',
+      url='https://github.com/jcbain/twitter_of_babble',
+      author='James Bain',
+      author_email='jamescbain@gmail.com',
+      license='',
+      packages=['twitter_of_babble'],
+      zip_safe=False)
---
+++ packages/twitter_of_babble/twitter_of_babble-0.1.4.tar.gz/twitter_of_babble-0.1.4/twitter_of_babble/database_connector.py
@@ -0,0 +1,33 @@
+import psycopg2
+
+def pullQuery(dbn,usr,hst,pswrd,stmnt):
+    '''
+    connects to a postgres database, executes a statement
+    and returns results to dict
+
+      [dbn] <str> : database name
+      [usr] <str> : username for database
+      [hst] <str> : database host
+      [pswrd] <str> : password for database user
+      [stmnt] <str> : sql statement to pull data
+
+      returns : dict object of query results
+    '''
+
+
+    try:
+        conn = psycopg2.connect("dbname='{}' user='{}' host='{}'password='{}'".format(dbn,usr,hst,pswrd))
+        cursor = conn.cursor()
+        cursor.execute(stmnt)
+
+        column_names = [desc[0] for desc in cursor.description]
+
+        rows = cursor.fetchall()
+
+        tweet_dict = {}
+        for i in list(range(len(column_names))):
+            tweet_dict['{}'.format(column_names[i])] = [x[i] for x in rows]
+
+    except Exception as e:
+        print("Uh oh, can't connect. Invalid dbname, user or password?")
+        print(e)
