8b85a8b2f8cb499b0895e46686bc77b9762cbb65
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:32 +0000

    Add package playlistfromsong/1.0.0/playlistfromsong-1.0.0.tar.gz

---
+++ packages/playlistfromsong/playlistfromsong-1.0.0.tar.gz/playlistfromsong-1.0.0/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+
+setup(
+    name='playlistfromsong',
+    packages=['playlistfromsong'],
+    version='1.0.0',
+    description='An offline music station generator',
+    author='schollz',
+    url='https://github.com/schollz/playlistfromsong',
+    author_email='hypercube.platforms@gmail.com',
+    download_url='https://github.com/schollz/playlistfromsong/archive/v1.0.0.tar.gz',
+    keywords=['music', 'youtube', 'playlist'],
+    classifiers=[],
+    install_requires=[
+        "requests",
+        "youtube_dl",
+        'appdirs',
+        'pyyaml',
+        'beautifulsoup4',
+    ],
+    setup_requires=['pytest-runner'],
+    tests_require=[
+        'pytest-flake8',
+        'pytest',
+        'pytest-cov'
+
+    ],
+    entry_points={'console_scripts': [
+        'playlistfromsong = playlistfromsong.__main__:main',
+    ], },
+)
