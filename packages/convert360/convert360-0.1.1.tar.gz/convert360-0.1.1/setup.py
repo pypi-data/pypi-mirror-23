cf7a77bee834ac17a55614470b410be7a369f9f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:44 +0000

    Add package convert360/0.1.1/convert360-0.1.1.tar.gz

---
+++ packages/convert360/convert360-0.1.1.tar.gz/convert360-0.1.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(name='convert360',
+      version='0.1.1',
+      description='Tool to convert 360 videos between different projections.',
+      url='https://github.com/MateusZitelli/convert360',
+      author='Mateus Zitelli',
+      author_email='zitellimateus@gmail.com',
+      license='GPLv3',
+      packages=['convert360'],
+      zip_safe=True,
+      install_requires=[
+          'imageio',
+          'PyOpenGL',
+          'PyOpenGL_Accelerate',
+          'tqdm'
+      ],
+      scripts=['bin/convert360'])
