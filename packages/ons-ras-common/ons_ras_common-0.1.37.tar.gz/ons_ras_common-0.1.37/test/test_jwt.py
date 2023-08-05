8f964c990cbb996d7161da902458683795a90959
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:22 +0000

    Add package ons-ras-common/0.1.37/ons_ras_common-0.1.37.tar.gz

---
+++ packages/ons-ras-common/ons_ras_common-0.1.37.tar.gz/ons_ras_common-0.1.37/test/test_jwt.py
@@ -0,0 +1,59 @@
+import unittest
+from datetime import datetime, timedelta
+from ons_ras_common import ons_env
+
+
+class TestStringMethods(unittest.TestCase):
+
+    def setUp(self):
+        ons_env.setup_ini()
+        ons_env._jwt.activate()
+        self.ons_token = ons_env.jwt
+        self.now = datetime.now()
+
+    def test_validate_good_token(self):
+        """
+        Generate a valid token and make sure it passes validation
+        """
+        jwt = {
+            'expires_at': (self.now + timedelta(seconds=60)).timestamp(),
+            'scope': ['ci:read', 'ci:write']
+        }
+        token = self.ons_token.encode(jwt)
+        self.assertTrue(self.ons_token.validate(['ci:read'], token))
+
+    def test_validate_expired_token(self):
+        """
+        Generate an  invalid token and make sure it fails validation
+        """
+        jwt = {
+            'expires_at': (self.now - timedelta(seconds=60)).timestamp(),
+            'scope': ['ci:read', 'ci:write']
+        }
+        token = self.ons_token.encode(jwt)
+        self.assertFalse(self.ons_token.validate(['ci:read'], token))
+
+    def test_validate_no_token(self):
+        """
+        Generate an empty token and make sure it fails validation
+        """
+        jwt = {}
+        token = self.ons_token.encode(jwt)
+        self.assertFalse(self.ons_token.validate(['ci:read'], token))
+
+    def test_invalid_scope(self):
+        """
+        Generate a token with an invalid scope and make sure it fails
+        """
+        jwt = {
+            'expires_at': (self.now + timedelta(seconds=60)).timestamp(),
+            'scope': ['cx:read', 'cx:write']
+        }
+        token = self.ons_token.encode(jwt)
+        self.assertFalse(self.ons_token.validate(['ci:read'], token))
+
+    def test_invalid_token(self):
+        """
+        Try to decode something that really isn't a valid token
+        """
+        self.assertFalse(self.ons_token.validate(['ci:read'], 'not a token'))
