5cd3d01139f6fbe0942beb03f76317c41b24308b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:07 +0000

    Add package diguihanshu/1.0.0/diguihanshu-1.0.0.zip

---
+++ packages/diguihanshu/diguihanshu-1.0.0.zip/diguihanshu-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='diguihanshu',
+    version='1.0.0',
+    py_modules=['diguihanshu'],
+    author='good0612',
+    author_email='104458118@qq.com',
+    url='http://pypi.python.org',
+    description='A simple printer of diguihanshu lists',
+    )
