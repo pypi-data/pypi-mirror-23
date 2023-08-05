bcf8beea8811ff5f9becd79ab8318fa7fc3641f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:31 +0000

    Add package ice-yml-log-formatter/0.1.1/ice-yml-log-formatter-0.1.1.tar.gz

---
+++ packages/ice-yml-log-formatter/ice-yml-log-formatter-0.1.1.tar.gz/ice-yml-log-formatter-0.1.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='ice-yml-log-formatter',
+    packages=['ice_yml_log_formatter'],
+    version='0.1.1',
+    description='ZeroC Ice logging utils',
+    author='3g0r',
+    author_email='eg0r.n1k0l43v@gmail.com',
+    url='https://github.com/3g0r/ice-yml-log-formatter',
+    keywords=['ice', 'yml', 'log'],
+    classifiers=[],
+    long_description=open('README.rst').read(),
+    license='MIT',
+    install_requires=[
+        'PyYAML==3.12',
+        'zeroc-ice==3.6.3'
+    ],
+    include_package_data=True,
+    package_data={'': ['README.rst']},
+)
