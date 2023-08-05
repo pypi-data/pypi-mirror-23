73a954d91f485bbe5acdfb6cbc68befbf2f1de1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:16 +0000

    Add package utagent/0.0.31/utagent-0.0.31-py2.py3-none-any.whl

---
+++ packages/utagent/utagent-0.0.31-py2.py3-none-any.whl/slave/config.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+config = {
+    "macaca_dir": None,
+    "environment": None,
+}
+
+environment = {
+    "alpha": "https://httpizza.alpha.elenet.me/waimai.slave/",
+}
+
+API_PORT = 9096
---
+++ packages/utagent/utagent-0.0.31-py2.py3-none-any.whl/slave/handler/macaca_task.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import subprocess
+import os
+import webbrowser
+
+from slave.util.file import File_Downloader
+from slave.config import config
+
+
+def start_macaca_server():
+    subprocess.Popen("macaca server --port 4444 --verbose", shell=True)
+
+
+def run_macaca_test(file_name, case_url):
+    File_Downloader.download(config["macaca_dir"] + '/eleme', file_name, case_url)
+    os.chdir(config["macaca_dir"])
+    subprocess.call('source run.sh eleme/%s' % file_name, shell=True)
+
+
+def ui_recorder():
+    os.chdir(config["macaca_dir"])
+
+    subprocess.Popen('uirecorder --mobile', shell=True)
+
+
+def init_uirecorder():
+    os.chdir(config["macaca_dir"])
+
+    subprocess.Popen('uirecorder init --mobile', shell=True)
+
+
+def reports():
+    webbrowser.open_new(config["macaca_dir"]+"/reports/index.html")
