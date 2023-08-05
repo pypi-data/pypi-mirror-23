54dcbe9240ceee0b5a231cc53d893c436b5c3ec6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:09:20 +0000

    Add package pybcca/0.2/pybcca-0.2.tar.gz

---
+++ packages/pybcca/pybcca-0.2.tar.gz/pybcca-0.2/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(
+  name = 'pybcca',
+  packages = ['pybcca'],
+  version = '0.2',
+  description = 'Helpers for Base Camp Coding Academy',
+  author = 'Nate Clark',
+  author_email = 'natec425@gmail.com',
+  url = 'https://github.com/natec425/pybcca',
+)
