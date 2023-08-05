6598f064aa54ae6734f20de73d7c0997cf6651dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:09:01 +0000

    Add package web-trawler/0.1.0/web_trawler-0.1.0.tar.gz

---
+++ packages/web-trawler/web_trawler-0.1.0.tar.gz/web_trawler-0.1.0/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+import version
+
+setuptools.setup(
+    name="web_trawler",
+    version=version.__version__,
+    description="Trawl web pages for files to download",
+    url="https://gitlab.com/dlab-indecol/web_trawler",
+    author="Gorm Roedder",
+    author_email="gormroedder@gmail.com",
+    license="GPLv3",
+    package_data={"web_trawler": ["config.json", "bin/web_trawler"]},
+    zip_safe=False,
+    install_requires=[
+        'lxml>=3.7.3',
+        'cssselect>=1.0.1'
+    ],
+    entry_points={'console_scripts': ['web_trawler = web_trawler:main']},
+)
