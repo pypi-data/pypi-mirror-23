13ee6e119832ae4369cfaefcfa67bec8cfd333e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:08:12 +0000

    Add package datasploit/0.1/datasploit-0.1.tar.gz

---
+++ packages/datasploit/datasploit-0.1.tar.gz/datasploit-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='datasploit',
+      version='0.1',
+      description='A tool to perform various OSINT techniques, aggregate all the raw data, and give data in multiple formats.',
+      url='https://github.com/DataSploit/datasploit',
+      author='Shubham Mittal',
+      author_email='upgoingstaar@gmail.com',
+      license='GPL',
+      packages=['datasploit'],
+      zip_safe=False)
