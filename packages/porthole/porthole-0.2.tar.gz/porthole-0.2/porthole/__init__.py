0bc0cf377cc175e612cf8ef5277bfa6a57acc847
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:25:37 +0000

    Add package porthole/0.2/porthole-0.2.tar.gz

---
+++ packages/porthole/porthole-0.2.tar.gz/porthole-0.2/porthole/__init__.py
@@ -0,0 +1,9 @@
+from .app import config
+from .config import write_default_config
+from .connection_manager import ConnectionManager
+from .queries import QueryGenerator, QueryReader, QueryResult
+from .xlsx import WorkbookBuilder
+from .mailer import Mailer
+from .workflows import SimpleWorkflow
+from .related_record import RelatedRecord, ChildRecord
+from .reports import GenericReport
