4d21ccdd108ba719d4313cbe5f8ab7c04c93e523
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:47 +0000

    Add package okpy/1.12.3/okpy-1.12.3.tar.gz

---
+++ packages/okpy/okpy-1.12.3.tar.gz/okpy-1.12.3/client/exceptions.py
@@ -0,0 +1,41 @@
+"""Client exceptions."""
+
+
+class OkException(BaseException):
+    """Base exception class for OK."""
+
+
+class AuthenticationException(OkException):
+    """Exceptions related to authentication."""
+
+
+class ProtocolException(OkException):
+    """Exceptions related to protocol errors."""
+
+
+class EarlyExit(OkException):
+    """Exceptions related to early exits that are NOT errors."""
+
+
+# TODO(albert): extend from a base class designed for student bugs.
+class Timeout(OkException):
+    """Exception for timeouts."""
+    _message = 'Evaluation timed out!'
+
+    def __init__(self, timeout):
+        """Constructor.
+
+        PARAMTERS:
+        timeout -- int; number of seconds before timeout error occurred
+        """
+        super().__init__(self)
+        self.timeout = timeout
+        self.message = self._message
+
+
+class LoadingException(OkException):
+    """Exception related to loading assignments."""
+
+
+class SerializeException(LoadingException):
+    """Exceptions related to de/serialization."""
