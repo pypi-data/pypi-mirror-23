68278413109848645a52a5670e1de96538dae86d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:40 +0000

    Add package overview-upload/0.9.7/overview_upload-0.9.7.tar.gz

---
+++ packages/overview-upload/overview_upload-0.9.7.tar.gz/overview_upload-0.9.7/overview_upload/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+"""Utilities for uploading to www.overviewdocs.com via its API
+"""
+
+from overview_upload._upload import Upload
+from overview_upload._document_set import create_document_set
