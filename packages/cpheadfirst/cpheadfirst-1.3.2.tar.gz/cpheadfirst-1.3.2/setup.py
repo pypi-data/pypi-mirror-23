6d4a9c924600517e0e473a900321a626d62db93b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:43 +0000

    Add package cpheadfirst/1.3.2/cpheadfirst-1.3.2.tar.gz

---
+++ packages/cpheadfirst/cpheadfirst-1.3.2.tar.gz/cpheadfirst-1.3.2/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+	name = 'cpheadfirst',
+	version = '1.3.2',
+	py_modules = ['nester'],
+	author = 'cppython',
+	author_email = 'agarwal1810@gmail.com',
+	description = 'A simple printer for nested lists',
+)
