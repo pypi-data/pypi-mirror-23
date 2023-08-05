5ea5b8c261b202d325c898110fbd0403078ce34d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:16 +0000

    Add package shellviz/0.1.3/shellviz-0.1.3.tar.gz

---
+++ packages/shellviz/shellviz-0.1.3.tar.gz/shellviz-0.1.3/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+setup(
+  name = 'shellviz',
+  packages = ['shellviz'],
+  version = '0.1.3',
+  description = 'Visualize data graphically directly from the shell',
+  author = 'Jean-Marc Skopek',
+  author_email = 'jskopek@gmail.com',
+  url = 'https://github.com/jskopek/shellviz',
+  keywords = ['visualization', 'graphs'],
+  classifiers = [],
+)
