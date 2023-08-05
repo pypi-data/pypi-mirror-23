400a8b933c1f0f9a3c95b64701803b155e6b9251
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:24 +0000

    Add package sve_common_tools/0.1.7.0/sve_common_tools-0.1.7.0.tar.gz

---
+++ packages/sve_common_tools/sve_common_tools-0.1.7.0.tar.gz/sve_common_tools-0.1.7.0/sve_common_tools/Sve_SingletonBuffer.py
@@ -0,0 +1,61 @@
+import json
+
+
+class SingletonBuffer:
+    # Here will be the instance stored.
+    __instance = None
+
+    @staticmethod
+    def getInstance():
+        """ Static access method. """
+        if SingletonBuffer.__instance == None:
+            SingletonBuffer()
+        return SingletonBuffer.__instance
+
+    def __init__(self):
+        """ Virtually private constructor. """
+        if SingletonBuffer.__instance != None:
+            raise Exception("This class is a singleton!")
+        else:
+            SingletonBuffer.__instance = self
+            self.bufferString = ""
+            self.bufferJsonArray = []
+
+    # -----------------------------------------------------------------------------------------
+    # -----------------------------------------------------------------------------------------
+    # -----------------------------------------------------------------------------------------
+
+    def bufferString_set(self, str):
+        self.bufferString = str + "\n"
+
+    def bufferString_add(self, str):
+        self.bufferString += str + "\n"
+
+    def bufferString_get(self):
+        return self.bufferString
+
+    def bufferString_save(self, fileName, overWrite):
+        mod = "a"
+        if overWrite: mod = "w"
+
+        with open(fileName, mod) as f:
+            f.write(self.bufferString)
+
+    # -----------------------------------------------------------------------------------------
+    # -----------------------------------------------------------------------------------------
+    # -----------------------------------------------------------------------------------------
+
+    def bufferJsonArray_set(self, jobjArr):
+        self.bufferJsonArray = jobjArr
+
+    def bufferJsonArray_append(self, jobj):
+        self.bufferJsonArray.append(jobj)
+
+    def bufferJsonArray_get(self):
+        return self.bufferJsonArray
+
+    def bufferJsonArray_save(self, fileName, overWrite):
+        mod = "a"
+        if overWrite: mod = "w"
+        with open(fileName, mod) as f:
+            f.write(json.dumps(self.bufferString))
