1c52f98f28d65d528fba566e27497ce53a19337a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:07:03 +0000

    Add package django-db-cascade/0.1.2/django-db-cascade-0.1.2.tar.gz

---
+++ packages/django-db-cascade/django-db-cascade-0.1.2.tar.gz/django-db-cascade-0.1.2/django_db_cascade/fields.py
@@ -0,0 +1,26 @@
+from django.db.models import ForeignKey as FK
+from django.db.models import OneToOneField as OTO
+from django_db_cascade.deletions import DB_CASCADE
+
+class ForeignKey(FK):
+    def __init__(self, to, on_delete, **kwargs):
+        self.on_delete_db_cascade = True if on_delete == DB_CASCADE else False
+        super().__init__(to, on_delete, **kwargs)
+
+    def deconstruct(self):
+        name, path, args, kwargs = super(ForeignKey, self).deconstruct()
+        if self.on_delete_db_cascade:
+            kwargs['on_delete'] = DB_CASCADE
+        return name, path, args, kwargs
+
+class OneToOneField(OTO):
+    def __init__(self, to, on_delete, **kwargs):
+        self.on_delete_db_cascade = True if on_delete == DB_CASCADE else False
+        super().__init__(to, on_delete, **kwargs)
+
+    def deconstruct(self):
+        name, path, args, kwargs = super(OneToOneField, self).deconstruct()
+        if self.on_delete_db_cascade:
+            kwargs['on_delete'] = DB_CASCADE
+        return name, path, args, kwargs
+
