e74e0f7c160db22e8b139368f60b6c28e7d4ed28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:16 +0000

    Add package havij/0.9/havij-0.9.tar.gz

---
+++ packages/havij/havij-0.9.tar.gz/havij-0.9/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name='havij',
+      version='0.9',
+      description='The Havijest in the world',
+      url='http://github.com/havij',
+      author='EmAm',
+      author_email='example@example.com',
+      license='MIT',
+      packages=['havij'],
+      install_requires=[
+          'markdown',
+      ],
+      zip_safe=False)
