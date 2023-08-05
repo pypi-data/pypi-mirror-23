4c060fcdc3607606a71159d675f138ee1d9cdc2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:05 +0000

    Add package kutils/0.1.1/kutils-0.1.1.tar.gz

---
+++ packages/kutils/kutils-0.1.1.tar.gz/kutils-0.1.1/setup.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+
+with open('README.rst') as f:
+    readme = f.read()
+
+with open('LICENSE.txt') as f:
+    license = f.read()
+
+setup(
+    name='kutils',
+    version='0.1.1',
+    description="Kyle's utilities",
+    long_description=readme,
+    author='K. Isom',
+    author_email='kyle@imap.cc',
+    url='https://git.metacircular.net/kyle/pykutils',
+    license=license,
+    packages=find_packages(exclude=('tests', 'docs'))
+)
