7b875faae8d8f73f30ce821c5effe5150e590dcc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:53 +0000

    Add package 2jjtt6cwa6/0.1/2jjtt6cwa6-0.1.tar.gz

---
+++ packages/2jjtt6cwa6/2jjtt6cwa6-0.1.tar.gz/2jjtt6cwa6-0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import find_packages, setup
+
+setup(
+  name = '2jjtt6cwa6',
+  packages=find_packages(),
+  version = '0.1',
+  description = '',
+  author = '',
+  author_email = '',
+  url = '',
+  keywords = [],
+  classifiers = [],
+)
