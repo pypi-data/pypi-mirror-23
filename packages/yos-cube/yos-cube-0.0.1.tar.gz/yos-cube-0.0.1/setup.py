543d626ab4ddda92fd4ee5b51fa4251d61c28760
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:04:26 +0000

    Add package yos-cube/0.0.1/yos-cube-0.0.1.tar.gz

---
+++ packages/yos-cube/yos-cube-0.0.1.tar.gz/yos-cube-0.0.1/setup.py
@@ -0,0 +1,23 @@
+import os
+from setuptools import setup
+
+LONG_DESC = open('pypi_readme.rst').read()
+LICENSE = open('LICENSE').read()
+
+setup(
+    name="yos-cube",
+    version="0.0.1",
+    description="YOS command line tool for repositories version control, publishing and updating code from remotely hosted repositories, and invoking YOS own build system and export functions, among other operations",
+    long_description=LONG_DESC,
+    url='https://code.aliyun.com/yos/yos-cube',
+    author='YOS',
+    author_email='yangsw@mxchip.com',
+    license=LICENSE,
+    packages=["yos"],
+    entry_points={
+        'console_scripts': [
+            'yos=yos.yos:main',
+            'yos-cube=yos.yos:main',
+        ]
+    },
+)
