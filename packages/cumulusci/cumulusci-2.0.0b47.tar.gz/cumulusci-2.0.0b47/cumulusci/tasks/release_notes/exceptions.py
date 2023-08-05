ef8483b8fe89d182d3bde7c7be79bca6bbf1c6c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:43 +0000

    Add package cumulusci/2.0.0b47/cumulusci-2.0.0b47.tar.gz

---
+++ packages/cumulusci/cumulusci-2.0.0b47.tar.gz/cumulusci-2.0.0b47/cumulusci/tasks/release_notes/exceptions.py
@@ -0,0 +1,21 @@
+from cumulusci.core.exceptions import CumulusCIException
+
+
+class GithubApiError(CumulusCIException):
+    pass
+
+
+class GithubApiNotFoundError(CumulusCIException):
+    pass
+
+
+class GithubApiNoResultsError(CumulusCIException):
+    pass
+
+
+class GithubApiUnauthorized(CumulusCIException):
+    pass
+
+
+class LastReleaseTagNotFoundError(CumulusCIException):
+    pass
