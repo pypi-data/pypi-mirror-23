c22adb1acdb5c0364108601f98bbfd03a2f91a27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:07:35 +0000

    Add package tian_nesternester/1.0.0/tian_nesternester-1.0.0.zip

---
+++ packages/tian_nesternester/tian_nesternester-1.0.0.zip/tian_nesternester-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+    name='tian_nesternester',
+    version='1.0.0',
+    py_modules=['nester'],
+    autor='tiantian',
+    autor_email='1126298530@qq.com',
+    url='http://www.headfirstlabs.com',
+    description='A simple printer of nested lists',
+)
