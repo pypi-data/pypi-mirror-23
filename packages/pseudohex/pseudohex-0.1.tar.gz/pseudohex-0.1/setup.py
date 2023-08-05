b0d00f3667d5381894bc42384ca4ad67e624f4ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:24:33 +0000

    Add package pseudohex/0.1/pseudohex-0.1.tar.gz

---
+++ packages/pseudohex/pseudohex-0.1.tar.gz/pseudohex-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pseudohex',
+      version='0.1',
+      description='Pseudohex module (from Traveller/Cepheus System',
+      url='http://github.com/storborg/funniest',
+      author='Arthur Green',
+      author_email='arthur.green045@gmail.com',
+      license='MIT',
+      packages=['pseudohex'],
+      zip_safe=False)
