31974d18e6880aabee998dc1db03e03b80db1b4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:56 +0000

    Add package ninjadog/0.3.9/ninjadog-0.3.9.tar.gz

---
+++ packages/ninjadog/ninjadog-0.3.9.tar.gz/ninjadog-0.3.9/setup.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""The setup script."""
+
+from setuptools import setup, find_packages
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('requirements.txt') as requirements_file:
+    requirements = requirements_file.readlines()
+
+with open('requirements_dev.txt') as dev_requirements_file:
+    dev_requirements = dev_requirements_file.readlines()
+
+setup_requirements = [
+    'pytest-runner',
+]
+
+test_requirements = [
+    'pytest',
+    'WebTest',
+]
+
+entry_points = {
+    'console_scripts': ['ninjadog=ninjadog.cli:main'],
+}
+
+extras = {
+    'testing': test_requirements,
+    'dev': dev_requirements,
+}
+
+setup(
+    name='ninjadog',
+    version='0.3.9',
+    description="Pug template support in Python",
+    long_description=readme,
+    author="Stephan Fitzpatrick",
+    author_email='knowsuchagency@gmail.com',
+    url='https://github.com/knowsuchagency/ninjadog',
+    packages=find_packages(include=['ninjadog']),
+    include_package_data=True,
+    install_requires=requirements,
+    license="MIT license",
+    zip_safe=False,
+    keywords='ninjadog pug jinja2 html templating',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+    test_suite='tests',
+    tests_require=test_requirements,
+    setup_requires=setup_requirements,
+    entry_points=entry_points,
+    extras_require=extras,
+)
