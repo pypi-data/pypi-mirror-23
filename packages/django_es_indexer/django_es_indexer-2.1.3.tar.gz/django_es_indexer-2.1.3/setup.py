2ecd5818bd1f5e5666dbb00b18d665b2cb2af531
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:45 +0000

    Add package django_es_indexer/2.1.3/django_es_indexer-2.1.3.tar.gz

---
+++ packages/django_es_indexer/django_es_indexer-2.1.3.tar.gz/django_es_indexer-2.1.3/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="django_es_indexer",
+    version="2.1.3",
+    description="Elasticsearch indexing from a django app",
+    author="Mirus Research",
+    author_email="frank@mirusresearch.com",
+    packages=find_packages(),
+    url='https://bitbucket.org/mirusresearch/django_es_indexer',
+    license='MIT license, see LICENSE.txt',
+    # install_requires=[
+    #     ],
+)
