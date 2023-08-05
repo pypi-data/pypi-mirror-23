4fb721a3513a60e79ab117c2cadf5b8c51b18c93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:45 +0000

    Add package nester_MrDrucha/1.4.3/nester_MrDrucha-1.4.3.tar.gz

---
+++ packages/nester_MrDrucha/nester_MrDrucha-1.4.3.tar.gz/nester_MrDrucha-1.4.3/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name	= 'nester_MrDrucha',
+	version = '1.4.3',
+	py_modules = ['nester'],
+	author = 'mrdrucha',
+	author_email = 'some@email.com',
+	url = 'http://www.somekind.com',
+    description = 'some description'
+)
