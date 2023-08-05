e2f414bd80c5b47913620d5c5f60d749e60efd9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:57 +0000

    Add package connix/1.12/connix-1.12.tar.gz

---
+++ packages/connix/connix-1.12.tar.gz/connix-1.12/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+setup(
+  name = 'connix',
+  packages = ['connix'],
+  version = '1.12',
+  description = 'Connix is a general purpose Python 3.x library that contains a lot of commonly done operations inside of a single package.',
+  author = 'Patrick Lambert',
+  license = 'MIT',
+  author_email = 'dendory@live.ca',
+  url = 'https://github.com/dendory/connix', 
+  download_url = 'https://github.com/dendory/connix/archive/master.zip',
+  keywords = ['connix', 'util'],
+  classifiers = [],
+)
