8cb9e0f7944b24847d3e06be551e7f71b2dadc7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 15:23:53 +0000

    Add package pyfox/0.18/pyfox-0.18.tar.gz

---
+++ packages/pyfox/pyfox-0.18.tar.gz/pyfox-0.18/setup.py
@@ -0,0 +1,23 @@
+from distutils.core import setup
+setup(
+    name = 'pyfox',
+    packages = ['pyfox'],
+    version = '0.18',
+    description = 'Shell for foxtrot',
+    author = 'Shubham Sharma',
+    author_email = 'shubham.sha12@gmail.com',
+    url = 'https://github.com/gabber12/pyfox',
+    download_url = 'https://github.com/gabber12/pyfox/archive/0.18.tar.gz',
+    classifiers = [],
+    entry_points = {
+                    'console_scripts': ['foxtrot=pyfox.commands:main'],
+    },
+    install_requires=[
+        'requests',
+        'click',
+        'json-logic',
+        'requests',
+        'prompt_toolkit',
+        'pygments'
+    ]
+)
