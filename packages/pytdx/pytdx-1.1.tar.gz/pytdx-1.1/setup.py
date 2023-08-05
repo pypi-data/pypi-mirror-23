79a5b71a8207a097698c26cce8ce3b9ac54be5de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:16 +0000

    Add package pytdx/1.1/pytdx-1.1.tar.gz

---
+++ packages/pytdx/pytdx-1.1.tar.gz/pytdx-1.1/setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(name='pytdx',
+      version='1.1',
+      description='A Python Interface to TDX protocol',
+      author='RainX<Jing Xu>',
+      author_email='i@rainx.cc',
+      url='http://www.zhikuang.org',
+      packages=find_packages(),
+      install_requires=[
+          'click',
+          'pandas',
+      ],
+      entry_points={
+          'console_scripts': [
+              'hqget=pytdx.bin.hqget:main'
+          ]
+      }
+
+      )
