68098e7f3d19da27cfbc485771be2c46842b7587
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:10 +0000

    Add package muniverse/0.1.0/muniverse-0.1.0-py2.py3-none-any.whl

---
+++ packages/muniverse/muniverse-0.1.0-py2.py3-none-any.whl/muniverse-0.1.0.dist-info/DESCRIPTION.rst
@@ -0,0 +1,7 @@
+muniverse
+=========
+
+This is a Python binding for `muniverse <https://github.com/unixpickle/muniverse>`_,
+an API for using HTML5 games as reinforcement learning environments.
+
+
---
+++ packages/muniverse/muniverse-0.1.0-py2.py3-none-any.whl/muniverse/__init__.py
@@ -0,0 +1,8 @@
+"""
+Bindings for the muniverse suite of Reinforcement Learning
+environments.
+"""
+
+from .env import Env
+from .spec import spec_for_name
+from .actions import MouseAction, KeyAction, key_for_code
