24b747fddcf82f206153597f8391019fd58747e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:56:51 +0000

    Add package goodreads2/0.3.4/goodreads2-0.3.4-py3-none-any.whl

---
+++ packages/goodreads2/goodreads2-0.3.4-py3-none-any.whl/goodreads/apikey.py
@@ -0,0 +1,4 @@
+key = 'fCUHaHylHEkSW3jJcZJTQ'
+secret = 'dOKwd2Kewa5sIpoAynUEVl44s5wscrwWoAiGwBuijTI'
+oauth_access_token = 'cj5UNVnlHntyVnC8lFor0g'
+oauth_access_token_secret = 'ohVsMpJ3esC6cWcOMnhu1Um2aTE3Umr6Cg7vef8xU'
---
+++ packages/goodreads2/goodreads2-0.3.4-py3-none-any.whl/goodreads/ff-project.py
@@ -0,0 +1,36 @@
+from goodreads import client
+import json
+
+def singleArray(inputlist):
+    outputlist = []
+    for i in inputlist:
+      outputlist.extend(i)
+    return outputlist
+
+def writeToJSONFile(path, fileName, data):
+    filePathNameWExt = './' + path + '/' + fileName + '.json'
+    with open(filePathNameWExt, 'w') as fp:
+        json.dump(data, fp)
+
+#authetification
+gc = client.GoodreadsClient('fCUHaHylHEkSW3jJcZJTQ', 'dOKwd2Kewa5sIpoAynUEVl44s5wscrwWoAiGwBuijTI')
+
+#get all books by query
+books = gc.search_books('Lesbian', search_field='genre')
+
+#parse books to single array
+#books = singleArray(books)
+
+#to json
+data = []
+for book in books:
+    i = {}
+    i['isbn'] = book.isbn
+    i['isbn13'] = book.isbn13
+    i['title'] = book.title
+    i['author'] = book.authors[0].name
+    i['link'] = book.link
+    i['popular_shelves'] = [shelve.name for shelve in book.popular_shelves]
+    data.append(i)
+    
+writeToJSONFile('./','ff-books',data)
