e51ca73d1b4eed237d7642b272b536007aae8303
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:30:55 +0000

    Add package aws_ir/0.3.0/aws_ir-0.3.0.tar.gz

---
+++ packages/aws_ir/aws_ir-0.3.0.tar.gz/aws_ir-0.3.0/setup.py
@@ -0,0 +1,39 @@
+from distutils.command.build import build
+from setuptools import setup
+from setuptools.command.install import install as _install
+
+class install(_install):
+    def run(self):
+        self.run_command('build')
+        _install.run(self)
+
+setup(name="aws_ir",
+
+      version="0.3.0",
+      author="Andrew Krug, Alex McCormack, Joel Ferrier",
+      author_email="andrewkrug@gmail.com,developer@amccormack.net,joel@ferrier.io",
+      packages=["aws_ir", "aws_ir/libs", "aws_ir/plans"],
+      license="MIT",
+      description="AWS Incident Response ToolKit",
+      scripts=['bin/aws_ir'],
+      url='https://github.com/ThreatResponse/aws_ir',
+
+      download_url="https://github.com/ThreatResponse/aws_ir/archive/v0.3.0.tar.gz",
+
+      use_2to3=True,
+      install_requires=['boto3>=1.3.0',
+                        'progressbar_latest',
+                        'logutils',
+                        'requests',
+                        'structlog',
+                        'pytz',
+                        'jinja2',
+                        'margaritashotgun>=0.4.1',
+                        'aws-ir-plugins>=0.0.2'
+                        ],
+      tests_require=['pytest',
+                     'pytest-cov',
+                     'moto',
+                     'mock',
+                     'magicmock'],
+      )
