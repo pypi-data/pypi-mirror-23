8a8a3a2a36c4854acd4ba97d9070b4b41d3cfe76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:12:54 +0000

    Add package gempy/0.9/gempy-0.9.tar.gz

---
+++ packages/gempy/gempy-0.9.tar.gz/gempy-0.9/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='gempy',
+    version='0.9',
+    packages=['gempy'],
+    url='https://github.com/cgre-aachen/gempy',
+    download_url='https://github.com/cgre-aachen/gempy/archive/0.9.tar.gz',
+    license='MIT',
+    author='Miguel de la Varga, Alexander Schaaf',
+    author_email='varga@aices.rwth-aachen.de',
+    description='An Open-source, Python-based 3-D structural geological modeling software.',
+    keywords=['geology', '3-D modeling', 'structural geology']
+)
