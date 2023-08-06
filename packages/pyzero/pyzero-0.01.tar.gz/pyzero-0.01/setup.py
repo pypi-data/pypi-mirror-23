aa484a164df15af14dbc4feb8d5faffe6b7892cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:35 +0000

    Add package pyzero/0.01/pyzero-0.01.tar.gz

---
+++ packages/pyzero/pyzero-0.01.tar.gz/pyzero-0.01/setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+
+setup(
+  name = 'pyzero',
+  version = '0.01',
+  description = 'PyZero library.',
+  url = 'https://github.com/mfhepp/pyzero',
+  author = 'Martin Hepp',
+  author_email = 'mfhepp@gmail.com',
+  license = 'MIT',
+  packages = ['pyzero'],
+  download_url = 'https://github.com/mfhepp/pyzero/archive/0.1.tar.gz',
+  keywords = ['python', 'learning', 'programming'], # arbitrary keywords
+  classifiers = []
+)
---
+++ packages/pyzero/pyzero-0.01.tar.gz/pyzero-0.01/test/test.py
@@ -0,0 +1,2 @@
+print("Hello Hepp")
+print 5 / 2
