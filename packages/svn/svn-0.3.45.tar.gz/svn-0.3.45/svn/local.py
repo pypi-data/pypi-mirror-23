6aa06289ae246004838508400ebe7bef46d57156
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:14:31 +0000

    Add package svn/0.3.45/svn-0.3.45.tar.gz

---
+++ packages/svn/svn-0.3.45.tar.gz/svn-0.3.45/svn/local.py
@@ -0,0 +1,38 @@
+import os.path
+
+import svn.constants
+import svn.common
+
+
+class LocalClient(svn.common.CommonClient):
+    def __init__(self, path_, *args, **kwargs):
+        if os.path.exists(path_) is False:
+            raise EnvironmentError("Path does not exist: %s" % path_)
+
+        super(LocalClient, self).__init__(
+            path_,
+            svn.constants.LT_PATH,
+            *args, **kwargs)
+
+    def __repr__(self):
+        return '<SVN(LOCAL) %s>' % self.path
+
+    def add(self, rel_path):
+        self.run_command(
+            'add',
+            [rel_path],
+            wd=self.path)
+
+    def commit(self, message, rel_filepaths=[]):
+        args = ['-m', message] + rel_filepaths
+
+        self.run_command(
+            'commit',
+            args,
+            wd=self.path)
+
+    def update(self, rel_filepaths=[]):
+        self.run_command(
+            'update',
+            rel_filepaths,
+            wd=self.path)
