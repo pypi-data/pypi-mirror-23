b1c0213718c723f8c69040681341e28d00cbfc20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:03 +0000

    Add package python-spectacles/1.4.3/python-spectacles-1.4.3.tar.gz

---
+++ packages/python-spectacles/python-spectacles-1.4.3.tar.gz/python-spectacles-1.4.3/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+"""
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+"""
+setup(name='python-spectacles',
+      version='1.4.3',
+      description='Description',
+      author=u'Christo Crampton',
+      license='MIT',
+      packages=['spectacles'],
+      install_requires=[
+          'splinter==0.7.5',
+          'requests==2.10.0',
+          'PyYAML==3.11',
+          'click==6.6'
+      ],
+      zip_safe=False)
