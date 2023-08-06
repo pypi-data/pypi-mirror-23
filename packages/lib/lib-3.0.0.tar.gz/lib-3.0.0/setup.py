de8eea87876ae33a6aa0f6185b65de8aa0cbd2f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:17:40 +0000

    Add package lib/3.0.0/lib-3.0.0.tar.gz

---
+++ packages/lib/lib-3.0.0.tar.gz/lib-3.0.0/setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+setup(
+  name = 'lib',
+  packages = ['lib'],
+  version = '3.0.0',
+  description = 'StdLib: Standard Library for Microservices Python Bindings',
+  author = 'Keith Horwood',
+  author_email = 'keithwhor@gmail.com',
+  url = 'https://github.com/stdlib/lib-python',
+  keywords = ['stdlib', 'microservice', 'serverless', 'faas', 'lib'],
+  license='MIT',
+  classifiers = [
+    'Programming Language :: Python :: 2',
+    'Programming Language :: Python :: 3'
+  ],
+)
