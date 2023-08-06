dc62603d7383c2d43d39af396cbe6df7cf427f7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:25 +0000

    Add package scrolls/0.0.4066718/scrolls-0.0.4066718-py2.py3-none-any.whl

---
+++ packages/scrolls/scrolls-0.0.4066718-py2.py3-none-any.whl/test/configuration_tests.py
@@ -0,0 +1,57 @@
+from test.ditestcase import DITestCase
+from mock import Mock, patch
+from os.path import expanduser
+
+
+class ConfigurationTests(DITestCase):
+
+    def setUp(self):
+        super(ConfigurationTests, self).setUp()
+        self.patchers = {
+            'configparser': patch('scrolls.configuration.configparser'),
+            'os': patch('scrolls.configuration.os'),
+        }
+        configparser = self.patchers['configparser'].start()
+        self.os = self.patchers['os'].start()
+        self.os.path.isfile.return_value = False  # default: no conf file
+        self.os.path.expanduser = expanduser
+        self.parser = Mock()
+        self.parser.sections.return_value = ['scrolls']
+        configparser.ConfigParser.return_value = self.parser
+
+    def tearDown(self):
+        super(ConfigurationTests, self).tearDown()
+        for patcher in self.patchers.values():
+            patcher.stop()
+
+    def test_defaults(self):
+        from scrolls.configuration import Configuration
+        config = Configuration(self.dependencies)
+        self.assertEqual(config.server, '0.0.0.0')
+
+    def test_useCommandlineArgs_overrides_defaults_and_config_file(self):
+        from scrolls.configuration import Configuration
+        config = Configuration(self.dependencies)
+        self.assertEqual(config.server, '0.0.0.0')
+        self.assertEqual(config.dry_run, False)
+        self.parser.get.side_effect = lambda s, k: 'somewhere.else'
+        args = Mock()
+        args.server = 'remote.com'
+        args.dry_run = True
+        config.useCommandlineArgs(args)
+        self.assertEqual(config.server, 'remote.com')
+        self.assertEqual(config.dry_run, True)
+
+    def test_If_config_file_reads_it(self):
+        from scrolls.configuration import Configuration
+        self.os.path.isfile.return_value = True
+        Configuration(self.dependencies)
+        self.parser.read.assert_called_with(expanduser('~/scrolls.conf'))
+
+    def test_Uses_values_in_file(self):
+        from scrolls.configuration import Configuration
+        self.os.path.isfile.return_value = True
+        self.parser.getboolean.side_effect = lambda s, k: True
+        self.parser.get.side_effect = lambda s, k: 'mothership'
+        config = Configuration(self.dependencies)
+        self.assertEqual('mothership', config.ticket_secret)
