2e09b1511b112846796fa56fd15f4bf927fd1ab9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:16 +0000

    Add package smaliemu/0.0.1/smaliemu-0.0.1.tar.gz

---
+++ packages/smaliemu/smaliemu-0.0.1.tar.gz/smaliemu-0.0.1/setup.py
@@ -0,0 +1,30 @@
+﻿from setuptools import setup
+
+
+if __name__ == "__main__":
+
+    setup(
+        name="smaliemu",
+        version="0.0.1",
+        description=("smaliemu"),
+
+        packages=[
+            "smaliemu",
+            "smaliemu.objects",
+        ],
+        # package_dir={"": "src"},
+
+        keywords="",
+        license="GPL V3",
+        classifiers=[
+            "Development Status :: 4 - Beta",
+            "License :: OSI Approved :: Apache Software License",
+            "Programming Language :: Python :: 3 :: Only",
+            "Topic :: Utilities",
+        ],
+
+        author="mikusjelly",
+        author_email="mikusjelly@gmail.com",
+        url="https://github.com/mikusjelly/smaliemu",
+
+    )
