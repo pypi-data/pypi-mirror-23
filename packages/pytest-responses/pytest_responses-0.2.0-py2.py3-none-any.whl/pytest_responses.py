b64323dc60cc9b5ad859104b54b352dc299f89cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:05 +0000

    Add package pytest-responses/0.2.0/pytest_responses-0.2.0-py2.py3-none-any.whl

---
+++ packages/pytest-responses/pytest_responses-0.2.0-py2.py3-none-any.whl/pytest_responses.py
@@ -0,0 +1,25 @@
+import pytest
+
+import responses as responses_
+
+
+# pytest plugin support
+def pytest_runtest_setup(item):
+    if not item.get_marker('withoutresponses'):
+        responses_.start()
+
+
+def pytest_runtest_teardown(item):
+    if not item.get_marker('withoutresponses'):
+        try:
+            responses_.stop()
+        except RuntimeError:
+            # patcher was already uninstalled and responses doesnt let us
+            # force maintain it
+            pass
+
+
+@pytest.yield_fixture
+def responses():
+    with responses_.RequestsMock() as rsps:
+        yield rsps
