66c439556b3507f84e0f9be697dd71b6abd020c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:16 +0000

    Add package AynRandom/0.2/AynRandom-0.2.zip

---
+++ packages/AynRandom/AynRandom-0.2.zip/AynRandom-0.2/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+
+from distutils.core import setup
+setup(
+  name = 'AynRandom',
+  packages = ['aynrandom'],
+  version = '0.2',
+  description = 'The Ayn Random Number Generator favours certain numbers, because they are intrinsically better.',
+  author = 'Jake Saville',
+  author_email = 'jakesaville@protonmail.com',
+  url = 'https://github.com/ThePythonist/AynRandom',
+  download_url = 'https://github.com/ThePythonist/AynRandom/archive/0.2.tar.gz',
+  keywords = ['random', 'xkcd', 'aynrand'],
+  classifiers = [],
+)
