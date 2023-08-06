15bab440cc71f593509a43530a9aeb0c0b1ffed2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:08:52 +0000

    Add package terminal-colours/0.4/terminal_colours-0.4.tar.gz

---
+++ packages/terminal-colours/terminal_colours-0.4.tar.gz/terminal_colours-0.4/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(name='terminal_colours',
+      version='0.4',
+      description='A simple package to allow the use of colours when outputting to the terminal',
+      url='https://github.com/g-farrow/terminal_colours',
+      author='Greg Farrow',
+      author_email='greg.farrow1@gmail.com',
+      license='MIT',
+      packages=['terminal_colours'],
+      python_requires='>=3',
+      keywords='terminal output colours colors color colour print format formatting',
+      classifiers=['Programming Language :: Python :: 3',
+                   'Programming Language :: Python :: 3.2',
+                   'Programming Language :: Python :: 3.3',
+                   'Programming Language :: Python :: 3.4'],
+      zip_safe=False)
