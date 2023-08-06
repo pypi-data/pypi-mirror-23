856599054a571576852aa19bee0ca40421e928ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:49:21 +0000

    Add package yelluw_matches/0.1.1/yelluw_matches-0.1.1.tar.gz

---
+++ packages/yelluw_matches/yelluw_matches-0.1.1.tar.gz/yelluw_matches-0.1.1/setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+
+
+setup(
+    name="yelluw_matches",
+    packages=["yelluw_matches"],
+    version="0.1.1",
+    description="matches is a simple and tiny pattern matching library",
+    author="Pablo Rivera",
+    author_email="pr@yelluw.com",
+    url="https://github.com/yelluw/matches",
+    download_url="https://github.com/yelluw/matches/archive/0.1.1.tar.gz",
+    keywords=["pattern matching", "Python 3", "Yelluw"],
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        ],
+)
