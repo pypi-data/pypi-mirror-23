b778b87ccc0c208f92f476cae78c6a102da4f451
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:57:40 +0000

    Add package pyko/0.1.1/pyko-0.1.1-py3-none-any.whl

---
+++ packages/pyko/pyko-0.1.1-py3-none-any.whl/pyko/__init__.py
@@ -0,0 +1,42 @@
+# coding: utf-8
+import os
+from pkg_resources import resource_filename
+
+from jpype import getDefaultJVMPath
+import jpype
+
+
+def java_str(func):
+    def wrapper(self, text):
+        text = jpype.java.lang.String(text)
+        return func(self, text)
+    return wrapper
+
+
+class OpenKoreanTextProcessor:
+    def __init__(self):
+        if not jpype.isJVMStarted():
+            libpath = resource_filename(__name__, 'java')
+            jars = os.listdir(libpath)
+            jars = [os.path.join(libpath, item) for item in jars]
+            jvm_arg = '-Djava.class.path={}'.format(os.pathsep.join(jars))
+
+            jpype.startJVM(getDefaultJVMPath(), jvm_arg)
+
+        self._processor = jpype.JClass(
+            'org.openkoreantext.processor.OpenKoreanTextProcessorJava')
+
+    @java_str
+    def normalize(self, text):
+        return self._processor.normalize(text)
+
+    @java_str
+    def tokenize(self, text):
+        tokens = self._processor.tokenize(text)
+        tokens = self._processor.tokensToJavaStringList(tokens)
+        tokens = [t for t in tokens]
+        return tokens
+
+    @java_str
+    def stem(self, text):
+        raise NotImplemented('Dependency API change pending')
