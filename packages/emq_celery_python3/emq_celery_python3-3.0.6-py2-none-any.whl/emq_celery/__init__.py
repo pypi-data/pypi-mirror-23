3effd118793615d986a1beebead59d9d2a7620f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:57:35 +0000

    Add package emq_celery_python3/3.0.6/emq_celery_python3-3.0.6-py2-none-any.whl

---
+++ packages/emq_celery_python3/emq_celery_python3-3.0.6-py2-none-any.whl/emq_celery/__init__.py
@@ -0,0 +1,20 @@
+﻿#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from kombu.transport import TRANSPORT_ALIASES
+import re
+# from celery.backends import BACKEND_ALIASES
+
+TRANSPORT_ALIASES["emq"] = __name__ + '.transport:EMQTransport'
+
+
+# BACKEND_ALIASES["emqcache"] = 'emq_celery.backend:EMQCacheBackend'
+
+def _format_queue_name(name):
+    """
+    格式化celery的quque名称，因为emq对名称有格式限制
+    :param name:
+    :return:
+    """
+    name, number = re.subn('[^a-zA-Z0-9_]', '_', name)
+    return name
