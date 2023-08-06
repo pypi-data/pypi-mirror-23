28248cc9e22f056cd4581f781b6b1c9855c25853
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:33:10 +0000

    Add package GoldenListGenerator/0.3/GoldenListGenerator-0.3.zip

---
+++ packages/GoldenListGenerator/GoldenListGenerator-0.3.zip/GoldenListGenerator-0.3/setup.py
@@ -0,0 +1,35 @@
+from GoldenListGenerator import __version__
+
+try:
+    from setuptools import setup, find_packages
+except:
+    from distutils.core import setup
+
+dependencies = ['PyYAML', 'cx_Oracle']
+
+setup(
+    name='GoldenListGenerator',
+    version=".".join(str(x) for x in __version__),
+    description='Generates a golden list(CSV file) using user specified fields. Program takes in two data sets from the '
+                'database and finds common fields between them to generate a golden list',
+    url='',
+    author='Imran Ali',
+    author_email='imran.ali@kapsch.net',
+    install_requires=dependencies,
+    packages=['GoldenListGenerator'],
+    entry_points={
+        'console_script':[
+            'GoldenListGenerator=GoldenListGenerator.__main__:main'
+        ],
+    },
+    classifiers=(
+        'Development Status :: 5 - Production/Stable',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+    ),
+    include_package_data=True,
+    package_data={
+        'GoldenListGenerator': [
+            'config.yaml'],
+    },
+)
