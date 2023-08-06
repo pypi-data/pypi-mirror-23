3d8e99fa47f3cbc071fab14b9d8d5520eb428ff5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:33 +0000

    Add package pyecharts/0.1.5/pyecharts-0.1.5.tar.gz

---
+++ packages/pyecharts/pyecharts-0.1.5.tar.gz/pyecharts-0.1.5/pyecharts/__version__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+#coding=utf-8
+
+__title__ = 'pyecharts'
+__description__ = 'Python echarts, make charting easier'
+__url__ = 'https://github.com/chenjiandongx/pyecharts'
+__version__ = '0.1.5'
+__author__ = 'chenjiandongx'
+__author_email__ = 'chenjiandongx@qq.com'
+__license__ = 'MIT'
+__requires__ = ['pprint', 'Image']
+__packages__ = ['pyecharts', 'pyecharts/charts']
+__keywords__ = ['echarts', 'charts']
