06fa8ba7c303bcd6b8e78beaeadaf9629f65fec8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:32:32 +0000

    Add package infi.gevent-utils/0.2.3/infi.gevent_utils-0.2.3.tar.gz

---
+++ packages/infi.gevent-utils/infi.gevent_utils-0.2.3.tar.gz/infi.gevent_utils-0.2.3/src/infi/gevent_utils/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.2.3"
+__git_commiter_name__ = "Arnon Yaari"
+__git_commiter_email__ = "arnony@infinidat.com"
+__git_branch__ = u'(HEAD detached at v0.2.3)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = '30f82b8ee896f5ea8b5e1b8d21490490c89c48ac'
+__git_head_subject__ = u'Finished Release v0.2.3'
+__git_head_message__ = u''
+__git_dirty_diff__ = u''
