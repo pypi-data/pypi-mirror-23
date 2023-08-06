9e2e111350388dc5c949e11aa44c22f2fb21231b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:35:53 +0000

    Add package spygame/0.1a2/spygame-0.1a2.tar.gz

---
+++ packages/spygame/spygame-0.1a2.tar.gz/spygame-0.1a2/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+import os
+
+
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+def read(file_name):
+    return open(os.path.join(os.path.dirname(__file__), file_name)).read()
+
+_VERSION = '0.1-alpha.2'
+
+setup(
+    name="spygame",
+    version=_VERSION,
+    packages=["spygame"],
+    url="http://www.github.com/sven1977/spygame",
+    download_url="https://github.com/sven1977/spygame/archive/spygame-"+_VERSION+".zip",
+    license="MIT",
+    author="Sven Mika",
+    author_email="sven.mika@ducandu.com",
+    description="2D game engine based on pygame and level-tmx files (soon to be: fully openAI gym integrated)",
+    long_description=read("README.rst"),
+    install_requires=["pygame", "pytmx", "numpy"]
+)
