d5de2ff940ac63998d557d308334b8ab71cde269
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:39 +0000

    Add package opsimulate/0.0.3/opsimulate-0.0.3.tar.gz

---
+++ packages/opsimulate/opsimulate-0.0.3.tar.gz/opsimulate-0.0.3/opsimulate/constants.py
@@ -0,0 +1,30 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# Copyright (c) 2017 James Wen
+
+import os
+
+HOME = os.path.expanduser("~")
+OPSIMULATE_HOME = os.path.join(HOME, '.opsimulate')
+
+KEYS_DIR_NAME = os.path.join(OPSIMULATE_HOME, 'keys')
+PRIVATE_KEY_FILE = os.path.join(KEYS_DIR_NAME, 'opsimulate')
+PUBLIC_KEY_FILE = os.path.join(KEYS_DIR_NAME, 'opsimulate.pub')
+
+SAVED_SELECTED_MODULE_PATH = os.path.join(OPSIMULATE_HOME,
+                                          'selected_module.txt')
+
+HINT_HISTORY_FILE = os.path.join(OPSIMULATE_HOME, 'hint-history.yml')
+
+MODULE_START_SCRIPT = 'initiate'
+MODULE_CHECK_SCRIPT = 'check'
+MODULE_RESOLVE_SCRIPT = 'resolve'
+MODULE_METADATA = 'metadata.yml'
+
+SERVICE_ACCOUNT_FILE = os.path.join(OPSIMULATE_HOME, 'service-account.json')
+
+ZONE = 'us-east4-a'
+MACHINE_TYPE = 'n1-standard-1'
+UBUNTU_VERSION = 'ubuntu-1404-lts'
+INSTANCE_NAME = 'opsimulate-gitlab'
+VM_USERNAME = 'opsimulate'
