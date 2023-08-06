1341eac6a3f372621c438683106ebef6df266130
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:14:04 +0000

    Add package prf/0.0.7/prf-0.0.7.tar.gz

---
+++ packages/prf/prf-0.0.7.tar.gz/prf-0.0.7/prf/tests/test_dictset.py
@@ -0,0 +1,24 @@
+import pytest
+from datetime import datetime
+from prf.utils import dictset
+from slovar.errors import DKeyError, DValueError
+
+
+class TestDictSet():
+    def test_asbool(self):
+        assert dictset(a=True).asbool('a') == True
+
+    def test_aslist(self):
+        assert dictset(a=[]).aslist('a') == []
+
+    def test_asint(self):
+        assert dictset(a=1).asint('a') == 1
+
+    def test_asfloat(self):
+        assert dictset(a='1.1').asfloat('a') == 1.1
+
+    def test_asdict(self):
+        assert dictset(a='a:1').asdict('a') == {'a':'1'}
+
+    def test_asdt(self):
+        assert dictset(a='2000-01-01T01:01:01').asdt('a') == datetime(2000,01,01,01,01,01)
