e0d3e74dd4a6ce790e1e5cc3feec0e37eb65f50e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:06 +0000

    Add package lbp-print/0.0.11/lbp_print-0.0.11.tar.gz

---
+++ packages/lbp-print/lbp_print-0.0.11.tar.gz/lbp_print-0.0.11/setup.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+import os
+from setuptools import setup, find_packages
+
+
+about = {}
+with open(os.path.join('lbp_print', '__about__.py')) as fp:
+    exec(fp.read(), about)
+
+setup(name='lbp_print',
+      version=about['__version__'],
+      packages=find_packages(),
+      dependency_links=[
+          "git+https://github.com/lombardpress/lbppy.git@f272e44429fcbd3f19384471c9cfb3903100fb30#egg=lbppy",
+      ],
+      install_requires=[
+          'docopt==0.6.2',
+          'isodate==0.5.4',
+          'lbppy>=0.0.0',
+          'lxml==3.7.3',
+          'pyparsing==2.2.0',
+          'rdflib==4.2.2',
+          'SPARQLWrapper==1.8.0',
+          'untangle==1.1.0',
+      ],
+      classifiers=[
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.6',
+      ],
+      entry_points={
+          'console_scripts': ['lbp_print=lbp_print.cli:main']
+      },
+
+      description='Collection of utility scripts for handling LombardPress material.',
+      url='https://github.com/stenskjaer/lbp_print',
+      author='Michael Stenskjær Christensen',
+      author_email='michael.stenskjaer@gmail.com',
+      license='MIT',
+)
