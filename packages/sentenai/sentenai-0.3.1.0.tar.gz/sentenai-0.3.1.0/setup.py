465ef139c4bd95303b378437c694b9e3c170ad85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:32 +0000

    Add package sentenai/0.3.1.0/sentenai-0.3.1.0.tar.gz

---
+++ packages/sentenai/sentenai-0.3.1.0.tar.gz/sentenai-0.3.1.0/setup.py
@@ -0,0 +1,40 @@
+
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+
+setup(
+    name='sentenai',
+    version='0.3.1.0',
+    description='Client library for Sentenai',
+    long_description="",
+    url='https://github.com/sentenai/py-sentenai',
+
+    author='Sentenai, Inc.',
+    author_email='info@sentenai.com',
+
+    license='BSD',
+
+    classifiers=[
+        'Development Status :: 4 - Beta',
+
+        'Intended Audience :: Developers',
+        'Topic :: Database',
+
+        'License :: OSI Approved :: BSD License',
+
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+    keywords='sentenai cloud sensor database',
+    packages=['sentenai'],
+
+    install_requires=['dateutils', 'pandas', 'pytz', 'requests', 'shapely'],
+    extras_require={},
+    package_data={},
+    data_files=[],
+    entry_points={},
+)
+
