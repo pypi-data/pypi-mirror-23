05a6c8f1ba53155a43124b1d48f888e4d4aec4f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:09:17 +0000

    Add package redis-interval/0.1.0/redis_interval-0.1.0.tar.gz

---
+++ packages/redis-interval/redis_interval-0.1.0.tar.gz/redis_interval-0.1.0/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+
+setup(
+    name="redis_interval",
+    version="0.1.0",
+    description="Extension of the python library for Redis with the Interval"
+                "sets extension of Redis",
+    author="Gustavo Pantuza",
+    author_email="gustavopantuza@gmail.com",
+    url="https://github.com/globocom/redis-pyinterval",
+    download_url="https://github.com/globocom/redis-pyinterval/archive/0.1.0.tar.gz",
+    install_requires=[
+        "redis==2.10.5",
+    ],
+    setup_requires=["pytest-runner"],
+    tests_require=["pytest"],
+    packages=['redis_interval'],
+    classifiers=[],
+)
