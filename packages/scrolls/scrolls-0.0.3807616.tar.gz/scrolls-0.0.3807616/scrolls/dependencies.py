14a8991dd647e43c0e1776287a73a17cd44cc54d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:47:50 +0000

    Add package scrolls/0.0.3807616/scrolls-0.0.3807616.tar.gz

---
+++ packages/scrolls/scrolls-0.0.3807616.tar.gz/scrolls-0.0.3807616/scrolls/dependencies.py
@@ -0,0 +1,39 @@
+from scrolls.clock import Clock
+from scrolls.configuration import Configuration
+from scrolls.filesystem import Filesystem
+from scrolls.repositories.message import MessageRepository
+from scrolls.rsyslog import RSyslog
+from scrolls.security import Security
+from scrolls.server import Server
+
+
+class Dependencies(object):
+    """
+    Dependency Injection service.
+    """
+
+    def getClock(self):
+        return Clock()
+
+    def getConfiguration(self):
+        return Configuration(self)
+
+    def getFilesystem(self):
+        return Filesystem(self)
+
+    def getListener(self):
+        import scrolls.listener
+        from socketserver import UDPServer
+        return scrolls.listener.Listener(UDPServer, self)
+
+    def getMessageRepository(self):
+        return MessageRepository(self)
+
+    def getRSyslog(self):
+        return RSyslog(self)
+
+    def getSecurity(self):
+        return Security(self)
+
+    def getServer(self):
+        return Server(self)
