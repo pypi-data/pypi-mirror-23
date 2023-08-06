914deff59ef5df8747bb9baa7adabc422f1710d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:07:59 +0000

    Add package westera/1.1.0/westera-1.1.0.tar.gz

---
+++ packages/westera/westera-1.1.0.tar.gz/westera-1.1.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+        name         ='westera',
+        version      ='1.1.0',
+	    py_modules   =['westera'],
+	    author       ='ZhuoFu',
+	    author_email ='1136634662@qq.com',
+	    url          ='http://www.headfirstlabs.com',
+	    description  ='A simple printer of westera lists',
+
+)
