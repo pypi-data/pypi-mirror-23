f11fd51f3e5ef7ed1cecdc47e67fdf681fb2f903
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:01 +0000

    Add package buildbot-worker/0.9.9/buildbot-worker-0.9.9.tar.gz

---
+++ packages/buildbot-worker/buildbot-worker-0.9.9.tar.gz/buildbot-worker-0.9.9/buildbot_worker/__init__.py
@@ -0,0 +1,76 @@
+# This file is part of Buildbot.  Buildbot is free software: you can
+# redistribute it and/or modify it under the terms of the GNU General Public
+# License as published by the Free Software Foundation, version 2.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, write to the Free Software Foundation, Inc., 51
+# Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+# Copyright Buildbot Team Members
+#
+# Keep in sync with master/buildbot/__init__.py
+#
+# We can't put this method in utility modules, because they import dependency packages
+#
+
+from __future__ import division
+from __future__ import print_function
+
+import os
+import re
+
+from subprocess import PIPE
+from subprocess import Popen
+from subprocess import STDOUT
+
+
+def getVersion(init_file):
+    """
+    Return BUILDBOT_VERSION environment variable, content of VERSION file, git
+    tag or 'latest'
+    """
+
+    try:
+        return os.environ['BUILDBOT_VERSION']
+    except KeyError:
+        pass
+
+    try:
+        cwd = os.path.dirname(os.path.abspath(init_file))
+        fn = os.path.join(cwd, 'VERSION')
+        with open(fn) as f:
+            return f.read().strip()
+    except IOError:
+        pass
+
+    # accept version to be coded with 2 or 3 parts (X.Y or X.Y.Z),
+    # no matter the number of digits for X, Y and Z
+    VERSION_MATCH = re.compile(br'(\d+\.\d+(\.\d+)?(\w|-)*)')
+
+    try:
+        p = Popen(['git', 'describe', '--tags', '--always'], stdout=PIPE, stderr=STDOUT, cwd=cwd)
+        out = p.communicate()[0]
+
+        if (not p.returncode) and out:
+            v = VERSION_MATCH.search(out)
+            if v:
+                version = v.group(1)
+                # Always return version of type str on Python 2 and 3.
+                if isinstance(version, str):
+                    # Python 2
+                    return version
+                # else:
+                # Python 3
+                return version.decode("utf-8")
+    except OSError:
+        pass
+
+    return "latest"
+
+
+version = getVersion(__file__)
