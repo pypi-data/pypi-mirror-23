1515a39333de9d962993099e81648942295bc14c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:52 +0000

    Add package foo2221/1.0/foo2221-1.0.tar.gz

---
+++ packages/foo2221/foo2221-1.0.tar.gz/foo2221-1.0/setup.py
@@ -0,0 +1,11 @@
+# coding=utf-8
+from distutils.core import setup  
+setup(
+	name='foo2221',  
+    version='1.0',  
+    py_modules=['foo2221'],  
+    author='Zonglong Fan',
+    author_email='lazyboy.fan@gmail.com',
+    maintainer='Zonglong Fan',
+    maintainer_email='lazyboy.fan@gmail.com',
+      ) 
