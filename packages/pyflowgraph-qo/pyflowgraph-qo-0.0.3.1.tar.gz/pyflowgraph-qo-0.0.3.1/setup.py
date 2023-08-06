599417a9c74a7f5d176866c1d25986f53e947f9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:07 +0000

    Add package pyflowgraph-qo/0.0.3.1/pyflowgraph-qo-0.0.3.1.tar.gz

---
+++ packages/pyflowgraph-qo/pyflowgraph-qo-0.0.3.1.tar.gz/pyflowgraph-qo-0.0.3.1/setup.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+long_description = """An interactive data flow graph editor."""
+
+setup(name='pyflowgraph-qo',
+      version='0.0.3.1',
+      description='An interactive data flow graph editor (QO internal version)',
+      long_description=long_description,
+      url='https://github.com/EricTRocks/pyflowgraph',
+      author='Eric Thivierge',
+      author_email='ethivierge@gmail.com',
+      license='BSD 3-clause "New" or "Revised" License',
+      classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: User Interfaces',
+
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: BSD License',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+      ],
+      keywords='data flow graph',
+      packages=find_packages(exclude=['tests']),
+      install_requires=['qtpy','six','future'],
+      zip_safe=False)
+
