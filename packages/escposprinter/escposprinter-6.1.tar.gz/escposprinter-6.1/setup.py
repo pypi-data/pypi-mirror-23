b595d3bec3d1f7f1dc449c81a886230afb44a2fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:09:50 +0000

    Add package escposprinter/6.1/escposprinter-6.1.tar.gz

---
+++ packages/escposprinter/escposprinter-6.1.tar.gz/escposprinter-6.1/setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+setup(
+    name='escposprinter',
+    version='6.1',
+    url='https://github.com/Simonefardella/escposprinter',
+    download_url='https://github.com/Simonefardella/escposprinter/archive/master.zip',
+    description='Python library to manipulate ESC/POS Printers with support for python >= 3',
+    long_description=open('README').read(),
+    license='LGPL',
+    author=['Simone Fardella'],
+    author_email='fardella93@gmail.com',
+    platforms=['linux'],
+    packages=[
+        'escposprinter',
+    ],
+    package_data = {'escposprinter':['nc.exe']},
+
+    install_requires=[
+        'pyusb',
+        'Pillow>=2.0',
+        'qrcode>=4.0',
+        'pyserial',
+    ],
+)
