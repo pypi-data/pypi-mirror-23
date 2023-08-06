c1a45d38ca139de12010cbe6ba079e6d88794e48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:16 +0000

    Add package sqlmix/0.10.6/sqlmix-0.10.6.tar.gz

---
+++ packages/sqlmix/sqlmix-0.10.6.tar.gz/sqlmix-0.10.6/setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+"""Setup script for smurf's Python database module"""
+
+from distutils.core import setup
+
+description = "Simple syntax for any database"
+long_description = \
+"""
+This module contains database access methods which are very easy
+to use and work with any Python database module without touching
+parameterized SQL syntax.
+
+"""
+
+setup (name = "sqlmix",
+       version = "0.10.6",
+       description = description,
+       long_description = long_description,
+       author = "Matthias Urlichs",
+       author_email = "smurf@smurf.noris.de",
+        classifiers=[
+            'Intended Audience :: Developers',
+            'Programming Language :: Python :: 2.7',
+            'Programming Language :: Python :: 3.5',
+            'Programming Language :: Python :: 3.6',
+            'Topic :: Database :: Front-Ends',
+            'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        ],
+
+       url = "http://smurf.noris.de/code/",
+       license = 'Python',
+       platforms = ['POSIX'],
+       packages = ['sqlmix'],
+      )
