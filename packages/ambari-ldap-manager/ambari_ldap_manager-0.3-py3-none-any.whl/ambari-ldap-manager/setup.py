dde175121c16449dec6c330dc593b880f0d39134
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:09:28 +0000

    Add package ambari-ldap-manager/0.3/ambari_ldap_manager-0.3-py3-none-any.whl

---
+++ packages/ambari-ldap-manager/ambari_ldap_manager-0.3-py3-none-any.whl/ambari-ldap-manager/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+setup(
+  name = 'ambari-ldap-manager',
+  packages = ['ambari-ldap-manager'], # this must be the same as the name above
+  version = '0.3',
+  description = 'A tool to manage Ambari users and groups when authentication uses LDAP.',
+  author = 'James Barney',
+  author_email = 'jamesbarney71@gmail.com',
+  url = 'https://github.com/Barneyjm/ambari-ldap-manager',
+  download_url = 'https://github.com/Barneyjm/ambari-ldap-manager/archive/0.3.tar.gz',
+  keywords = ['hadoop', 'ldap', 'active-directory', 'ambari', 'manager'],
+  entry_points={
+          'console_scripts': [
+              'ambari-ldap-manager = ldap_management_server.main:main'
+          ]
+      },
+  install_requires=[
+          'requests',
+          'flask'
+      ],
+  classifiers = []
+)
