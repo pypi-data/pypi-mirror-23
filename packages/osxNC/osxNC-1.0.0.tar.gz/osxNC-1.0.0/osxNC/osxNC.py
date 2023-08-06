dc53979cc54bb3593413892155fa8b735b74b4a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:08:30 +0000

    Add package osxNC/1.0.0/osxNC-1.0.0.tar.gz

---
+++ packages/osxNC/osxNC-1.0.0.tar.gz/osxNC-1.0.0/osxNC/osxNC.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import os
+import subprocess
+
+# Check if terminal-notifier is installed
+proc = subprocess.Popen(["which", "terminal-notifier"], stdout=subprocess.PIPE)
+env_bin_path = proc.communicate()[0].strip()
+
+if not os.path.exists(env_bin_path):
+	raise Exception("Dependency terminal-notifier is not installed. Download it at github.com/julienXX/terminal-notifier")
+
+arguments = ['title', 'subtitle', 'sound', 'reply', 'actions', 'dropdownLabel', 'closeLabel', 'timeout',
+	'group', 'activate', 'sender', 'appIcon', 'contentImage', 'open', 'execute']
+params = ['reply', 'json']
+
+def Notify(message, **kwargs):
+	items = kwargs.items()
+
+	for i in items:
+		if not i[0] in arguments:
+			print(str(i) + ' is not a valid argument. Removed from args list.')
+			items.remove(i)
+
+			if i in params:
+				if not type(items[i]) == bool:
+					items.remove(i)
+
+	message = message.encode('utf-8')
+
+	args = ['-message', message]
+	args += [a for b in [("-%s" % arg, "'"+str(key)+"'") for arg, key in kwargs.items()] for a in b]
+
+	os.system('terminal-notifier ' + ' '.join(args))
+
+def RemoveByID(id):
+	# Use ID 'ALL' to remove all messages.
+	os.system('terminal-notifier -remove ' + str(id))
+
+def ListByID(id):
+	# use ID 'ALL' to list details about all messages.
+	os.system('terminal-notifier -list ' + str(id))
---
+++ packages/osxNC/osxNC-1.0.0.tar.gz/osxNC-1.0.0/setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+setup(
+	name="osxNC",
+	version="1.0.0",
+	packages=find_packages(),
+	author="Tyler Georgiadis",
+	author_email="tyler.j.georgiadis@gmail.com",
+	description="Improved Python Wrapper for Mac OS 10.12 Notification Center",
+	license="MIT",
+	keywords="mac osx notification center wrapper",
+)
