155d61bb63b21fd7509eaba60c1d9a86bf4b23a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:46:54 +0000

    Add package stickbugml/1.0/stickbugml-1.0-py3-none-any.whl

---
+++ packages/stickbugml/stickbugml-1.0-py3-none-any.whl/stickbugml/singleton.py
@@ -0,0 +1,9 @@
+__copyright__ = 'Copyright 2017 Aaron Janse'
+__license__ = 'Apache 2.0'
+
+class Singleton(object):
+  _instances = {}
+  def __new__(class_, *args, **kwargs):
+    if class_ not in class_._instances:
+        class_._instances[class_] = super(Singleton, class_).__new__(class_, *args, **kwargs)
+    return class_._instances[class_]
