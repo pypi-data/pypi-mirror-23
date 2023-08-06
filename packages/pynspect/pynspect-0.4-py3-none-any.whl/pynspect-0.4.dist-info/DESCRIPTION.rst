9a4640493c0b986104c1773236872a61f7f3edce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:43 +0000

    Add package pynspect/0.4/pynspect-0.4-py3-none-any.whl

---
+++ packages/pynspect/pynspect-0.4-py3-none-any.whl/pynspect-0.4.dist-info/DESCRIPTION.rst
@@ -0,0 +1,10 @@
+pynspect
+================================================================================
+
+Python 3 library for filtering, querying or inspecting almost arbitrary data
+structures.
+
+This README file is work in progress, for more information please consult source
+code and unit tests.
+
+
---
+++ packages/pynspect/pynspect-0.4-py3-none-any.whl/pynspect/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#-------------------------------------------------------------------------------
+# This file is part of Mentat system (https://mentat.cesnet.cz/).
+#
+# Copyright (C) since 2011 CESNET, z.s.p.o (http://www.ces.net/)
+# Use of this source is governed by the MIT license, see LICENSE file.
+#-------------------------------------------------------------------------------
+
+__version__ = "0.3"
