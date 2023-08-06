d420a2bd29b04368c47e5f9f72d0c43ad2edad23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:35:18 +0000

    Add package yd/1.3.3/yd-1.3.3.tar.gz

---
+++ packages/yd/yd-1.3.3.tar.gz/yd-1.3.3/setup.py
@@ -0,0 +1,27 @@
+import os
+import sys
+
+from setuptools import setup
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+setup(
+    name = "yd",
+    version = '1.3.3',
+    author = "wierton",
+    author_email = 'nickouxianfei@gmail.com',
+    url = "https://github.com/wierton/dict",
+    description = 'A terminal bilingual dictionary',
+    long_description = readme(),
+    packages=['yd'],
+    include_package_data=True,
+    #install_requires=['MySQL'],
+    entry_points={
+        'console_scripts':[
+            'yd=yd.yd:main'
+            ]
+        },
+    zip_safe=False,
+)
