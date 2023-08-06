2dc1b8272406abf18c060185755e806f71836c29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:10:38 +0000

    Add package focus-package/2.1.0/focus_package-2.1.0.tar.gz

---
+++ packages/focus-package/focus_package-2.1.0.tar.gz/focus_package-2.1.0/focus_tools/pykriber.py
@@ -0,0 +1,148 @@
+#!/usr/bin/env python
+
+import os
+import sys
+import shutil
+
+import subprocess as sp
+from subprocess import PIPE, STDOUT
+
+drc = os.path.abspath(os.path.dirname( __file__ )) # get path of program
+
+
+def clean():
+    files = ["symdat", "distdat", "coseq"]
+    for f in files:
+        if os.path.exists(f):
+            os.remove(f)
+
+
+def setup():
+    files = ["symdat", "distdat", "coseq"]
+    for f in files:
+
+        src = os.path.join(drc, "..", "resources", f)
+        target = os.path.join(os.path.abspath("."), f)
+
+        shutil.copyfile(src, target)
+
+
+def prepare():
+    kriber_exe = os.path.join(drc, "..", "bin", "kriber.x")
+    kriber_exe_dev = os.path.join(drc, "..", "bin_windows", "bin", "kriber.x") # check developer path
+
+    clean()
+    setup()
+
+    if not os.path.exists(kriber_exe):
+        if not os.path.exists(kriber_exe_dev):
+            print "Cannot find", kriber_exe
+            sys.exit()
+        kriber_exe = kriber_exe_dev
+
+    files = ["symdat", "distdat", "coseq"]
+    for f in files:
+        assert os.path.exists(f)
+
+    if not os.path.exists("strudat"):
+        print ">> Warning: Cannot find 'strudat' file\n"
+
+    return kriber_exe
+
+
+def move(fname, target):
+    if os.path.exists(target):
+        os.remove(target)
+    os.rename(fname, target)
+
+
+def extract_all_keys_from_strudat():
+    keys = []
+    try:
+        strudat = open("strudat", "r")
+    except IOError:
+        print "Cannot find 'strudat' file"
+        sys.exit()
+
+    for line in strudat:
+        if line.startswith("*"):
+            keys.append(line.strip()[1:])
+    return keys
+
+
+def strudat2cif(args=[], keys=[], rename=True, verbose=True):
+    from subprocess import PIPE, STDOUT
+    
+    if isinstance(keys, str):
+        keys = keys.split()
+
+    kriber_exe = prepare()
+    
+    if not keys:
+        keys = extract_all_keys_from_strudat()
+    
+    for key in keys:
+        p = sp.Popen(['kriber', 'reacs'] + args + ['wricif', 'exit'], stdout=PIPE, stdin=PIPE, stderr=STDOUT)
+        out = p.communicate(input=key)
+
+        if out[0]:
+            for line in out[0].split("\n"):
+                if "ERROR" in line:
+                    raise RuntimeError("{}  ->  KRIBER {}".format(key, line))
+        if out[1]:
+            print out[1]
+
+        move("structure.cif", key+".cif")
+        if verbose:
+            print " >> Wrote file {}".format(key+".cif")
+
+    clean()
+
+
+def strudat2dls(args=[], keys=[], verbose=True):
+    from subprocess import PIPE, STDOUT
+    
+    if isinstance(keys, str):
+        keys = keys.split()
+
+    kriber_exe = prepare()
+    
+    if not keys:
+        keys = extract_all_keys_from_strudat()
+
+    for key in keys:
+        p = sp.Popen(['kriber', 'reacs'] + args + ['wriid', 'exit'], stdout=PIPE, stdin=PIPE, stderr=STDOUT)
+        out = p.communicate(input=key)
+
+        if out[0]:
+            for line in out[0].split("\n"):
+                if "ERROR" in line:
+                    raise RuntimeError("{}  ->  KRIBER {}".format(key, line))
+        if out[1]:
+            print out[1]
+
+        # move("structure.cif", key+".cif")
+        # print " >> Wrote file {}".format(key+".cif")
+
+    clean()
+
+
+def kriber(args=[]):
+    kriber_exe = prepare()
+    sp.call([kriber_exe] + args)
+    clean()
+
+
+def strudat2cif_entry():
+    fns = sys.argv[1:]
+    strudat2cif(args=fns)
+
+
+def kriber_entry():
+    fns = sys.argv[1:]
+    kriber(args=fns) 
+
+
+if __name__ == '__main__':
+    run_kriber()
+    # strudat2cif()
---
+++ packages/focus-package/focus_package-2.1.0.tar.gz/focus_package-2.1.0/readme.md
@@ -0,0 +1,76 @@
+FOCUS
+=====
+
+FOCUS is a program for structure determination of zeolites. It combines automatic Fourier recycling with a specialized topology search specific to zeolites or zeolite-like materials. This makes it ideal for structure determination from powder of electron diffraction data, because the framework search can make up for the loss of information from peak overlap or dynamical effects. This package also includes KRIBER, DLS76, and several python helper scripts that work together to analyze the results, optimize the framework geometry, and produce CIF files.
+
+Quick installation
+==================
+
+Download the FOCUS package from the [releases page](https://github.com/stefsmeets/focus_package/releases).
+
+1.	Unzip or extract using:  `tar -xvzf focus-osx.tar.gz`
+2.	Install with python: `python setup.py install`
+
+Focus and all included scripts have been tested to work on recent versions of Windows, OS X, Ubuntu.
+
+### Windows
+
+The windows version includes all dependencies and can be used as is. In case Windows complains, it is sometimes needed to install [Visual C++ Redistributable for Visual Studio 2015](https://www.microsoft.com/en-us/download/details.aspx?id=48145) (vc_redist.x86.exe)
+
+### OSX
+
+The OSX version of DLS76 has been compiled using brew gfortran [homebrew](http://brew.sh/)  
+To install gfortran:
+    
+    brew install gfortran
+
+Programs included
+=================
+
+ - focus
+ - kriber
+ - sginfo
+ - dls76       
+ - kriber      
+ - fo2cif      
+ - fo2hist     
+ - fo2strudat  
+ - strudat2cif 
+ - dlsall      
+ - cdlsall     
+ - multifocal  
+
+(please see the [manual](../master/manuals))
+
+Installation from source
+========================
+
+Extra dependencies for installation from source:
+
+### OS X
+
+- command line tools (xcode-select install)
+- gfortran
+
+### Linux
+
+- svn
+- curl
+
+Extract using:
+
+    tar -xvzf focus_package-src.tar.gz
+
+Build scripts:
+ - [./focus/focus_build_script.sh](../master/focus/focus_build_script.sh)
+ - [./src/kriber/makefile](../master/src/kriber/makefile)
+ - [./src/dls76/makefile](../master/src/dls76/makefile)
+ 
+References
+==========
+ 
+S. Smeets, L. B. McCusker, C. Baerlocher, E. Mugnaioli, and U. Kolb. [Using FOCUS to solve zeolite structures from three-dimensional electron diffraction data](http://dx.doi.org/10.1107/S0021889813014817). J. Appl. Crystallogr., 46:1017–1023, 2013
+
+R. W. Grosse-Kunstleve, L. B. McCusker, C. Baerlocher. [Zeolite structure determination from powder diffraction data: applications of the FOCUS method](http://dx.doi.org/10.1107/S0021889899003453). J. Appl. Crystallogr., 32():536-542, 1999
+
+R. W. Grosse-Kunstleve, L. B. McCusker, C. Baerlocher. [Powder diffraction data and crystal chemical information combined in an automated structure determination procedure for zeolites](http://dx.doi.org/10.1107/S0021889897005013). J. Appl. Crystallogr., 30():985-995, 1997
