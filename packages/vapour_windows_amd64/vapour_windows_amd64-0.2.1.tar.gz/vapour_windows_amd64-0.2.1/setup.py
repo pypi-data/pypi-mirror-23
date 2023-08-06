958d633d61c8f630d9010f3a4c2ef08017f0af3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:12 +0000

    Add package vapour_windows_amd64/0.2.1/vapour_windows_amd64-0.2.1.tar.gz

---
+++ packages/vapour_windows_amd64/vapour_windows_amd64-0.2.1.tar.gz/vapour_windows_amd64-0.2.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(
+    name='vapour_windows_amd64',
+    packages=['vapour_windows_amd64'],
+    package_data={'vapour_windows_amd64': ['*.zip']},
+    version='0.2.1',
+    description='This package contains binary executable dependencies for the master.',
+    author='VapourApps',
+    author_email='vapour@vapour.com'
+)
