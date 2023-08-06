f0b511f7afae9a63f1e7a4361dc4d3b697277bdd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:36:20 +0000

    Add package rootlocker/1.0.4/rootlocker-1.0.4.tar.gz

---
+++ packages/rootlocker/rootlocker-1.0.4.tar.gz/rootlocker-1.0.4/setup.py
@@ -0,0 +1,57 @@
+from setuptools import setup, find_packages  # Always prefer setuptools over distutils
+from codecs import open  # To use a consistent encoding
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the relevant file
+with open(path.join(here, 'README'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='rootlocker',
+
+    # Versions should comply with PEP440.  For a discussion on single-sourcing
+    # the version across setup.py and the project code, see
+    # http://packaging.python.org/en/latest/tutorial.html#version
+    version='1.0.4',
+
+    description='global analytic equation solver',
+    long_description=long_description,  
+
+    # Author details
+    author='maxence Miguel-Brebion',
+    author_email='bmsofts.contact@gmail.com',
+
+    # Choose your license
+    license='MIT',
+
+    # See https://PyPI.python.org/PyPI?%3Aaction=list_classifiers
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 4 - Beta',
+
+        # Indicate who your project is intended for
+        "Intended Audience :: Education",
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering :: Mathematics",
+
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: MIT License',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 2.7',
+    ],
+
+    # What does your project relate to?
+    keywords='root finding algorithm analytic argument principle',
+    packages = find_packages(exclude=[]),
+    install_requires=[
+       'ad',
+       'numpy'
+          ],
+)
