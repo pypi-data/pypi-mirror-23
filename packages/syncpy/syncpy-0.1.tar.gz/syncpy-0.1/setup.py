44bc207e9f7749ccfbc1c7b9f61c5897564c41a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:09:19 +0000

    Add package syncpy/0.1/syncpy-0.1.tar.gz

---
+++ packages/syncpy/syncpy-0.1.tar.gz/syncpy-0.1/setup.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul  7 15:25:48 2017
+
+@author: isir
+"""
+
+from setuptools import setup
+
+setup(name='syncpy',
+      version='0.1',
+      description='The funniest joke in the world',
+      url='http://github.com/storborg/funniest',
+      author='wail',
+      author_email='elbaniwail@gmail.com',
+      license='MIT',
+      packages=['syncpy'],
+      zip_safe=False)
