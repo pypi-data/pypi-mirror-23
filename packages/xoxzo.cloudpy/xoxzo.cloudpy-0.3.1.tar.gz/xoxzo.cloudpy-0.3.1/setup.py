b2e4d336fb4e682822d02ef1afd84aabd965eecf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:00 +0000

    Add package xoxzo.cloudpy/0.3.1/xoxzo.cloudpy-0.3.1.tar.gz

---
+++ packages/xoxzo.cloudpy/xoxzo.cloudpy-0.3.1.tar.gz/xoxzo.cloudpy-0.3.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="xoxzo.cloudpy",
+    version="0.3.1",
+    author="Xoxzo Inc.",
+    author_email="help@xoxzo.com",
+    description=("Xoxzo Public API library"),
+    license="MIT",
+    keywords="xoxzo telephony api",
+    url="https://github.com/xoxzo/xoxzo.cloudpy",
+    packages=find_packages(),
+    install_requires=[
+        'requests >=2.9.1',
+    ],
+    test_suite='tests'
+)
+
