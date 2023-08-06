a5704057b7a7d352b70b6f8c0bfb2d6f53eb1861
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:10:18 +0000

    Add package pybrain3/3.0.4/PyBrain3-3.0.4.tar.gz

---
+++ packages/pybrain3/PyBrain3-3.0.4.tar.gz/PyBrain3-3.0.4/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='PyBrain3',
+    version='3.0.4',
+    packages=find_packages(),
+    description='PyBrain wrapper for Python3',
+    author='Alex Permenev',
+    author_email='permenev.alex@ya.ru',
+    url='https://github.com/AlexProgramm/pybrain3',
+)
