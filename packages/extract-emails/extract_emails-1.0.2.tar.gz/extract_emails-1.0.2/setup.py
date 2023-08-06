a76c46947b68908dd4faa208bb54d306ccf3b161
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:10:18 +0000

    Add package extract-emails/1.0.2/extract_emails-1.0.2.tar.gz

---
+++ packages/extract-emails/extract_emails-1.0.2.tar.gz/extract_emails-1.0.2/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+with open('README.rst') as readme:
+    r = str(readme.read())
+
+setup(
+    name='extract_emails',
+    version='1.0.2',
+    packages=['extract_emails'],
+    url='https://github.com/dmitriiweb/extract-emails',
+    license='MIT',
+    author='Dmitrii K',
+    author_email='winston.smith.spb@gmail.com',
+    description='Extract email addresses from given URL.',
+    long_description=r,
+    install_requires=[
+        'requests',
+        'beautifulsoup4',
+    ],
+)
