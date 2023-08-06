83260beb2113da64b4dfdf7d998f7ae2315a8d70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:56:11 +0000

    Add package Kup/1.0/Kup-1.0.zip

---
+++ packages/Kup/Kup-1.0.zip/Kup-1.0/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+setup(name='Kup',
+      version='1.0',
+      description='Simple HTTP Server With Upload',
+      author='kion',
+      author_email='yufan2hao@sina.com',
+      github='https://github.com/NextOneWxf',
+      py_modules=['Kup'],
+)
