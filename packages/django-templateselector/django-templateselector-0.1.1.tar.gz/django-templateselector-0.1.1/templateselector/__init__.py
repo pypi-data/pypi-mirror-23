680a65f56a7931a8501a3bf7fc1cb4917f889939
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:56:01 +0000

    Add package django-templateselector/0.1.1/django-templateselector-0.1.1.tar.gz

---
+++ packages/django-templateselector/django-templateselector-0.1.1.tar.gz/django-templateselector-0.1.1/templateselector/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import
+from __future__ import unicode_literals
+
+__version_info__ = '0.1.1'
+__version__ = '0.1.1'
+version = '0.1.1'
+VERSION = '0.1.1'
+
+def get_version():
+    return version  # pragma: no cover
