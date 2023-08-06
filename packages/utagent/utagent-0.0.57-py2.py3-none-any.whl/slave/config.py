1726428de4c953085d88e168936b37358b471eaf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:08:37 +0000

    Add package utagent/0.0.57/utagent-0.0.57-py2.py3-none-any.whl

---
+++ packages/utagent/utagent-0.0.57-py2.py3-none-any.whl/slave/config.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+config = {
+    "macaca_dir": None,
+    "environment": None,
+    "port": 9099,
+}
+
+environment = {
+    "alpha": "http://qp.alpha.elenet.me/api/",
+    "beta": "http://qp.beta.elenet.me/api/",
+    "prod": "http://qp.elenet.me/api/",
+}
