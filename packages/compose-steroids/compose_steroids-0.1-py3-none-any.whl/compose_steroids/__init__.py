cba65700ef29065dd516dd07fc8c02869d1fda93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:19 +0000

    Add package compose-steroids/0.1/compose_steroids-0.1-py3-none-any.whl

---
+++ packages/compose-steroids/compose_steroids-0.1-py3-none-any.whl/compose_steroids/__init__.py
@@ -0,0 +1,12 @@
+__version__ = "0.1"
+
+__title__ = "compose_steroids"
+__description__ = ""
+__uri__ = "https://bitbucket.org/merixstudio/compose_steroids"
+__doc__ = __description__ + " <" + __uri__ + ">"
+
+__author__ = "MERIXSTUDIO"
+__email__ = "code@merixstudio.com"
+
+__license__ = "MIT"
+__copyright__ = "Copyright (c) 2017 MERIXSTUDIO"
