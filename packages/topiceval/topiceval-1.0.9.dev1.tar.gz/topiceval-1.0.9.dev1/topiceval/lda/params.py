43154e95dd7b423b6f07c703e152566805baa34a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:57:42 +0000

    Add package topiceval/1.0.9.dev1/topiceval-1.0.9.dev1.tar.gz

---
+++ packages/topiceval/topiceval-1.0.9.dev1.tar.gz/topiceval-1.0.9.dev1/topiceval/lda/params.py
@@ -0,0 +1,22 @@
+# Number of passes over the data if num documents are less
+passes_low = 10
+# Number of passes over the data if num documents are moderate
+passes_mid = 5
+# Number of passes over the data if num documents are high
+passes_high = 2
+# Sparsity of document-topic prior
+alpha = 'symmetric'
+# prior over topic word distribution
+eta = None
+# number of iterations per pass
+iterations = 100
+# --
+gamma_threshold = 0.001
+# controls filtering the topics returned for a document (bow)
+minimum_probability = 0.01
+# seeding
+random_state = 1
+# --
+minimum_phi_value = 0.01
+# evaluation of perplixity
+eval_every = None
