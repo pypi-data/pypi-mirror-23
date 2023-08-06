85be62db4211fc87cbae6f990d844c274378c9fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:08:01 +0000

    Add package mss/3.0.0/mss-3.0.0.tar.gz

---
+++ packages/mss/mss-3.0.0.tar.gz/mss-3.0.0/mss/base.py
@@ -0,0 +1,258 @@
+# coding: utf-8
+"""
+mss.base
+~~~~~~~~
+
+This module contains the basic classes that power MSS.
+
+.. note::
+
+    The :class:`MSSBase <MSSBase>` class must be the parent class
+    for every OS speicific implementation.
+"""
+
+import collections
+
+from .exception import ScreenShotError
+from .tools import to_png
+
+
+class MSSBase(object):
+    """ This class will be overloaded by a system specific one. """
+
+    _monitors = []  # type: List[Dict[str, int]]
+
+    def __enter__(self):
+        # type: () -> MSSBase
+        """ For the cool call `with MSS() as mss:`. """
+
+        return self
+
+    def __exit__(self, *_):
+        # type: (*str) -> None
+        """ For the cool call `with MSS() as mss:`. """
+
+    def grab(self, monitor):
+        # type: (Dict[str, int]) -> ScreenShot
+        """
+        Retrieve screen pixels for a given monitor.
+
+        :param monitor: The coordinates and size of the box to capture.
+                        See :meth:`monitors <monitors>` for object details.
+        :return :class:`ScreenShot <ScreenShot>`.
+        """
+
+        raise NotImplementedError('Subclasses need to implement this!')
+
+    @property
+    def monitors(self):
+        # type: () -> List[Dict[str, int]]
+        """
+        Get positions of all monitors.
+        If the monitor has rotation, you have to deal with it
+        inside this method.
+
+        This method has to fill self._monitors with all informations
+        and use it as a cache:
+            self._monitors[0] is a dict of all monitors together
+            self._monitors[N] is a dict of the monitor N (with N > 0)
+
+        Each monitor is a dict with:
+        {
+            'left':   the x-coordinate of the upper-left corner,
+            'top':    the y-coordinate of the upper-left corner,
+            'width':  the width,
+            'height': the height
+        }
+        """
+
+        raise NotImplementedError('Subclasses need to implement this!')
+
+    def save(self, mon=0, output='monitor-%d.png', callback=None):
+        # type: (int, str, Callable[[str], None]) -> Iterator[str]
+        """
+        Grab a screenshot and save it to a file.
+
+        :param int mon: The monitor to screenshot (default=0).
+                        -1: grab one screenshot of all monitors
+                         0: grab one screenshot by monitor
+                        N: grab the screenshot of the monitor N
+
+        :param str output: The output filename.
+                           %d, if present, will be replaced by
+                           the monitor number.
+
+        :param callable callback: Callback called before saving
+                                  the screenshot to a file.
+                                  Take the ``output`` argument as parameter.
+
+        :return generator: Created file(s).
+        """
+
+        monitors = self.monitors
+        if not monitors:
+            raise ScreenShotError('No monitor found.')
+
+        if mon == 0:
+            # One screenshot by monitor
+            for i, monitor in enumerate(monitors[1:], 1):
+                fname = output
+                if '%d' in output:
+                    fname = output.replace('%d', str(i))
+                if callable(callback):
+                    callback(fname)
+                sct = self.grab(monitor)
+                to_png(sct.rgb, sct.size, fname)
+                yield fname
+        else:
+            # A screenshot of all monitors together or
+            # a screenshot of the monitor N.
+            mon_number = 0 if mon == -1 else mon
+            try:
+                monitor = monitors[mon_number]
+            except IndexError:
+                raise ScreenShotError('Monitor does not exist.', locals())
+
+            if '%d' in output:
+                output = output.replace('%d', str(mon_number))
+            if callable(callback):
+                callback(output)
+            sct = self.grab(monitor)
+            to_png(sct.rgb, sct.size, output)
+            yield output
+
+    def shot(self, **kwargs):
+        """
+        Helper to save the screenshot of the 1st monitor, by default.
+        You can pass the same arguments as for ``save``.
+        """
+
+        kwargs['mon'] = kwargs.get('mon', 1)
+        return next(self.save(**kwargs))
+
+
+class ScreenShot(object):
+    """
+    Screen shot object.
+
+    .. note::
+
+        A better name would have  been *Image*, but to prevent collisions
+        with PIL.Image, it has been decided to use *ScreenShot*.
+    """
+
+    __pixels = None  # type: List[Tuple[int, int, int]]
+    __rgb = None  # type: bytes
+
+    def __init__(self, data, monitor):
+        # type: (bytearray, Dict[str, int]) -> None
+        #: Bytearray of the raw BGRA pixels retrieved by ctype
+        #: OS independent implementations.
+        self.raw = bytearray(data)  # type: bytearray
+
+        #: NamedTuple of the screen shot coordinates.
+        self.pos = collections.namedtuple('pos', 'left, top')(
+            monitor['left'], monitor['top'])  # type: Any
+
+        #: NamedTuple of the screen shot size.
+        self.size = collections.namedtuple('size', 'width, height')(
+            monitor['width'], monitor['height'])  # type: Any
+
+    def __repr__(self):
+        # type: () -> str
+        return ('<{!s}'
+                ' pos={cls.left},{cls.top}'
+                ' size={cls.width}x{cls.height}'
+                '>').format(type(self).__name__, cls=self)
+
+    @property
+    def __array_interface__(self):
+        # type: () -> Dict[str, Any]
+        """
+        Numpy array interface support.
+        It uses raw data in BGRA form.
+
+        See https://docs.scipy.org/doc/numpy/reference/arrays.interface.html
+        """
+
+        return dict(version=3,
+                    shape=(self.height, self.width, 4),
+                    typestr='|u1',
+                    data=self.raw)
+
+    @classmethod
+    def from_size(cls, data, width, height):
+        # type: (bytearray, int, int) -> ScreenShot
+        """ Instanciate a new class given only screenshot's data and size. """
+
+        monitor = {'left': 0, 'top': 0, 'width': width, 'height': height}
+        return cls(data, monitor)
+
+    @property
+    def top(self):
+        # type: () -> int
+        """ Conveniant accessor to the top position. """
+        return self.pos.top
+
+    @property
+    def left(self):
+        # type: () -> int
+        """ Conveniant accessor to the left position. """
+        return self.pos.left
+
+    @property
+    def width(self):
+        # type: () -> int
+        """ Conveniant accessor to the width size. """
+        return self.size.width
+
+    @property
+    def height(self):
+        # type: () -> int
+        """ Conveniant accessor to the height size. """
+        return self.size.height
+
+    @property
+    def pixels(self):
+        # type: () -> List[Tuple[int, int, int]]
+        """
+        :return list: RGB tuples.
+        """
+
+        if not self.__pixels:
+            rgb_tuples = zip(self.raw[2::4], self.raw[1::4], self.raw[0::4])
+            self.__pixels = list(zip(*[iter(rgb_tuples)] * self.width))
+
+        return self.__pixels
+
+    def pixel(self, coord_x, coord_y):
+        # type: (int, int) -> Tuple[int, int, int]
+        """
+        Returns the pixel value at a given position.
+
+        :param int coord_x: The x coordinate.
+        :param int coord_y: The y coordinate.
+        :return tuple: The pixel value as (R, G, B).
+        """
+
+        try:
+            return self.pixels[coord_y][coord_x]
+        except IndexError:
+            raise ScreenShotError('Pixel location out of range.', locals())
+
+    @property
+    def rgb(self):
+        # type: () -> bytes
+        """
+        Compute RGB values from the BGRA raw pixels.
+
+        :return bytes: RGB pixels.
+        """
+
+        if not self.__rgb:
+            self.__rgb = bytearray(self.height * self.width * 3)
+            self.__rgb[0::3], self.__rgb[1::3], self.__rgb[2::3] = \
+                self.raw[2::4], self.raw[1::4], self.raw[0::4]
+            self.__rgb = bytes(self.__rgb)
+
+        return self.__rgb
---
+++ packages/mss/mss-3.0.0.tar.gz/mss-3.0.0/mss/windows.py
@@ -0,0 +1,226 @@
+# coding: utf-8
+""" This is part of the MSS Python's module.
+    Source: https://github.com/BoboTiG/python-mss
+"""
+
+import ctypes
+import ctypes.wintypes
+
+from .base import MSSBase, ScreenShot
+from .exception import ScreenShotError
+
+__all__ = ['MSS']
+
+
+class BITMAPINFOHEADER(ctypes.Structure):
+    """ Information about the dimensions and color format of a DIB. """
+
+    _fields_ = [('biSize', ctypes.wintypes.DWORD),
+                ('biWidth', ctypes.wintypes.LONG),
+                ('biHeight', ctypes.wintypes.LONG),
+                ('biPlanes', ctypes.wintypes.WORD),
+                ('biBitCount', ctypes.wintypes.WORD),
+                ('biCompression', ctypes.wintypes.DWORD),
+                ('biSizeImage', ctypes.wintypes.DWORD),
+                ('biXPelsPerMeter', ctypes.wintypes.LONG),
+                ('biYPelsPerMeter', ctypes.wintypes.LONG),
+                ('biClrUsed', ctypes.wintypes.DWORD),
+                ('biClrImportant', ctypes.wintypes.DWORD)]
+
+
+class BITMAPINFO(ctypes.Structure):
+    """
+    Structure that defines the dimensions and color information for a DIB.
+    """
+
+    _fields_ = [('bmiHeader', BITMAPINFOHEADER),
+                ('bmiColors', ctypes.wintypes.DWORD * 3)]
+
+
+class MSS(MSSBase):
+    """ Multiple ScreenShots implementation for Microsoft Windows. """
+
+    def __init__(self):
+        # type: () -> None
+        """ Windows initialisations. """
+
+        self.monitorenumproc = ctypes.WINFUNCTYPE(
+            ctypes.wintypes.INT,
+            ctypes.wintypes.DWORD,
+            ctypes.wintypes.DWORD,
+            ctypes.POINTER(ctypes.wintypes.RECT),
+            ctypes.wintypes.DOUBLE
+        )
+        set_argtypes(self.monitorenumproc)
+        set_restypes()
+
+    @property
+    def monitors(self):
+        # type: () -> List[Dict[str, int]]
+        """ Get positions of monitors (see parent class). """
+
+        if not self._monitors:
+            # All monitors
+            sm_xvirtualscreen, sm_yvirtualscreen = 76, 77
+            sm_cxvirtualscreen, sm_cyvirtualscreen = 78, 79
+            left = ctypes.windll.user32.GetSystemMetrics(sm_xvirtualscreen)
+            right = ctypes.windll.user32.GetSystemMetrics(sm_cxvirtualscreen)
+            top = ctypes.windll.user32.GetSystemMetrics(sm_yvirtualscreen)
+            bottom = ctypes.windll.user32.GetSystemMetrics(sm_cyvirtualscreen)
+            self._monitors.append({
+                'left': int(left),
+                'top': int(top),
+                'width': int(right - left),
+                'height': int(bottom - top),
+            })
+
+            # Each monitors
+            def _callback(monitor, data, rect, dc_):
+                # type: (Any, Any, Any, float) -> int
+                """
+                Callback for monitorenumproc() function, it will return
+                a RECT with appropriate values.
+                """
+
+                del monitor, data, dc_
+                rct = rect.contents
+                self._monitors.append({
+                    'left': int(rct.left),
+                    'top': int(rct.top),
+                    'width': int(rct.right - rct.left),
+                    'height': int(rct.bottom - rct.top),
+                })
+                return 1
+
+            callback = self.monitorenumproc(_callback)
+            ctypes.windll.user32.EnumDisplayMonitors(0, 0, callback, 0)
+
+        return self._monitors
+
+    def grab(self, monitor):
+        # type: (Dict[str, int]) -> ScreenShot
+        """ Retrieve all pixels from a monitor. Pixels have to be RGB.
+
+            In the code, there are few interesting things:
+
+            [1] bmi.bmiHeader.biHeight = -height
+
+            A bottom-up DIB is specified by setting the height to a
+            positive number, while a top-down DIB is specified by
+            setting the height to a negative number.
+            https://msdn.microsoft.com/en-us/library/ms787796.aspx
+            https://msdn.microsoft.com/en-us/library/dd144879%28v=vs.85%29.aspx
+
+
+            [2] bmi.bmiHeader.biBitCount = 32
+                image_data = create_string_buffer(height * width * 4)
+
+            We grab the image in RGBX mode, so that each word is 32bit
+            and we have no striding, then we transform to RGB.
+            Inspired by https://github.com/zoofIO/flexx
+
+
+            [3] bmi.bmiHeader.biClrUsed = 0
+                bmi.bmiHeader.biClrImportant = 0
+
+            When biClrUsed and biClrImportant are set to zero, there
+            is "no" color table, so we can read the pixels of the bitmap
+            retrieved by gdi32.GetDIBits() as a sequence of RGB values.
+            Thanks to http://stackoverflow.com/a/3688682
+        """
+
+        srcdc = memdc = bmp = None
+        try:
+            bmi = BITMAPINFO()
+            bmi.bmiHeader.biSize = ctypes.sizeof(BITMAPINFOHEADER)
+            bmi.bmiHeader.biWidth = monitor['width']
+            bmi.bmiHeader.biHeight = -monitor['height']  # Why minus? See [1]
+            bmi.bmiHeader.biPlanes = 1  # Always 1
+            bmi.bmiHeader.biBitCount = 32  # See [2]
+            bmi.bmiHeader.biCompression = 0  # 0 = BI_RGB (no compression)
+            bmi.bmiHeader.biClrUsed = 0  # See [3]
+            bmi.bmiHeader.biClrImportant = 0  # See [3]
+
+            buf_len = monitor['width'] * monitor['height'] * 4  # See [2]
+            data = ctypes.create_string_buffer(buf_len)
+            srcdc = ctypes.windll.user32.GetWindowDC(0)
+            memdc = ctypes.windll.gdi32.CreateCompatibleDC(srcdc)
+            bmp = ctypes.windll.gdi32.CreateCompatibleBitmap(
+                srcdc, monitor['width'], monitor['height'])
+
+            ctypes.windll.gdi32.SelectObject(memdc, bmp)
+            ctypes.windll.gdi32.BitBlt(memdc, 0, 0,
+                                       monitor['width'], monitor['height'],
+                                       srcdc,
+                                       monitor['left'], monitor['top'],
+                                       0xcc0020)  # SRCCOPY
+
+            bits = ctypes.windll.gdi32.GetDIBits(
+                memdc, bmp, 0, monitor['height'], data, bmi, 0)
+            if bits != monitor['height']:
+                raise ScreenShotError('gdi32.GetDIBits() failed.', locals())
+        finally:
+            # Clean up
+            if srcdc:
+                ctypes.windll.gdi32.DeleteObject(srcdc)
+            if memdc:
+                ctypes.windll.gdi32.DeleteObject(memdc)
+            if bmp:
+                ctypes.windll.gdi32.DeleteObject(bmp)
+
+        return ScreenShot(data, monitor)
+
+
+def set_argtypes(callback):
+    # type: (Callable[[int, Any, Any, Any, float], int]) -> None
+    """ Functions arguments. """
+
+    ctypes.windll.user32.GetSystemMetrics.argtypes = [ctypes.wintypes.INT]
+    ctypes.windll.user32.EnumDisplayMonitors.argtypes = [
+        ctypes.wintypes.HDC,
+        ctypes.c_void_p,
+        callback,
+        ctypes.wintypes.LPARAM]
+    ctypes.windll.user32.GetWindowDC.argtypes = [ctypes.wintypes.HWND]
+    ctypes.windll.gdi32.CreateCompatibleDC.argtypes = [ctypes.wintypes.HDC]
+    ctypes.windll.gdi32.CreateCompatibleBitmap.argtypes = [
+        ctypes.wintypes.HDC,
+        ctypes.wintypes.INT,
+        ctypes.wintypes.INT]
+    ctypes.windll.gdi32.SelectObject.argtypes = [ctypes.wintypes.HDC,
+                                                 ctypes.wintypes.HGDIOBJ]
+    ctypes.windll.gdi32.BitBlt.argtypes = [
+        ctypes.wintypes.HDC,
+        ctypes.wintypes.INT,
+        ctypes.wintypes.INT,
+        ctypes.wintypes.INT,
+        ctypes.wintypes.INT,
+        ctypes.wintypes.HDC,
+        ctypes.wintypes.INT,
+        ctypes.wintypes.INT,
+        ctypes.wintypes.DWORD]
+    ctypes.windll.gdi32.DeleteObject.argtypes = [ctypes.wintypes.HGDIOBJ]
+    ctypes.windll.gdi32.GetDIBits.argtypes = [
+        ctypes.wintypes.HDC,
+        ctypes.wintypes.HBITMAP,
+        ctypes.wintypes.UINT,
+        ctypes.wintypes.UINT,
+        ctypes.c_void_p,
+        ctypes.POINTER(BITMAPINFO),
+        ctypes.wintypes.UINT]
+
+
+def set_restypes():
+    # type: () -> None
+    """ Functions return type. """
+
+    ctypes.windll.user32.GetSystemMetrics.restype = ctypes.wintypes.INT
+    ctypes.windll.user32.EnumDisplayMonitors.restype = ctypes.wintypes.BOOL
+    ctypes.windll.user32.GetWindowDC.restype = ctypes.wintypes.HDC
+    ctypes.windll.gdi32.CreateCompatibleDC.restype = ctypes.wintypes.HDC
+    ctypes.windll.gdi32.CreateCompatibleBitmap.restype = \
+        ctypes.wintypes.HBITMAP
+    ctypes.windll.gdi32.SelectObject.restype = ctypes.wintypes.HGDIOBJ
+    ctypes.windll.gdi32.BitBlt.restype = ctypes.wintypes.BOOL
+    ctypes.windll.gdi32.GetDIBits.restype = ctypes.wintypes.INT
+    ctypes.windll.gdi32.DeleteObject.restype = ctypes.wintypes.BOOL
