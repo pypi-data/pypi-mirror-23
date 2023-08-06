c9ba6964abf6e76782268c20b83ee3fac56493d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:04 +0000

    Add package rapydo-controller/0.4.2/rapydo_controller-0.4.2.tar.gz

---
+++ packages/rapydo-controller/rapydo_controller-0.4.2.tar.gz/rapydo_controller-0.4.2/rapydo/do/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+# __version__ = '0.5.0'
+__version__ = '0.4.2'
+
+FRAMEWORK_NAME = 'RAPyDo'
+
+# PROJECT_YAML_SPECSDIR = 'specs'
+COMPOSE_ENVIRONMENT_FILE = '.env'
+SUBMODULES_DIR = 'submodules'
+PLACEHOLDER = '#@$%-REPLACE-#@%$-ME-#@$%'
