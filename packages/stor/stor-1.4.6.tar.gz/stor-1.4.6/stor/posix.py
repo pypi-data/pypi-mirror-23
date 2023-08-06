7100c400a6e5841cc756e101e4e4c4d64ea7081a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:11:45 +0000

    Add package stor/1.4.6/stor-1.4.6.tar.gz

---
+++ packages/stor/stor-1.4.6.tar.gz/stor-1.4.6/stor/posix.py
@@ -0,0 +1,45 @@
+"""
+Provides functionality for accessing resources on Posix file systems.
+"""
+import posixpath
+
+from stor import base
+from stor import utils
+
+
+class PosixPath(base.FileSystemPath):
+    """Represents a posix path.
+
+    This class inherits a vendored path.py ``Path`` object and
+    overrides methods for cross compatibility with swift.
+
+    This class overrides ``Path`` object's
+    ``module`` attribute and sets it to ``posixpath`` to ensure
+    that it always represents a posix path.
+    """
+    path_module = posixpath
+
+    def list(self):
+        """
+        List all files and directories under a path.
+
+        Returns:
+            List[str]: A list of all files and directories.
+        """
+        return list(utils.walk_files_and_dirs([self]).keys())
+
+    def walkfiles(self, pattern=None):
+        """Iterate over files recursively.
+
+        Args:
+            pattern (str, optional): Limits the results to files
+                with names that match the pattern.  For example,
+                ``mydir.walkfiles('*.tmp')`` yields only files with the ``.tmp``
+                extension.
+
+        Returns:
+            Iter[Path]: Files recursively under the path
+        """
+        for f in self.list():
+            if pattern is None or f.fnmatch(pattern):
+                yield f
