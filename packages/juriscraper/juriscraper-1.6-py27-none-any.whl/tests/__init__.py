0d1666bffa8821a94302eeac72965976eea0c2b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:46:49 +0000

    Add package juriscraper/1.6/juriscraper-1.6-py27-none-any.whl

---
+++ packages/juriscraper/juriscraper-1.6-py27-none-any.whl/tests/__init__.py
@@ -0,0 +1,10 @@
+import juriscraper
+import os
+
+JURISCRAPER_ROOT = os.path.realpath(
+    os.path.join(
+        os.path.realpath(juriscraper.__file__),
+        '..'
+    )
+)
+TESTS_ROOT = os.path.realpath(os.path.join(JURISCRAPER_ROOT, '../tests'))
