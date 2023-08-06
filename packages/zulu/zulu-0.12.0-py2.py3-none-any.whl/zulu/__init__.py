2f0e979b598f8e44a7d2dbf6f389afc53d88e5e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:12:36 +0000

    Add package zulu/0.12.0/zulu-0.12.0-py2.py3-none-any.whl

---
+++ packages/zulu/zulu-0.12.0-py2.py3-none-any.whl/zulu/__init__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""The zulu module.
+"""
+
+from .__pkg__ import (
+    __description__,
+    __url__,
+    __version__,
+    __author__,
+    __email__,
+    __license__
+)
+
+
+from .api import (
+    create,
+    now,
+    parse,
+    parse_delta,
+    range,
+    span_range
+)
+from .zulu import Zulu
+from .parser import ParseError, ISO8601, TIMESTAMP
+from .delta import Delta
