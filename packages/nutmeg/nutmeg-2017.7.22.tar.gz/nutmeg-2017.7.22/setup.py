76a455b79b63872828e2353b2ebd4971e5edd969
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:51 +0000

    Add package nutmeg/2017.7.22/nutmeg-2017.7.22.tar.gz

---
+++ packages/nutmeg/nutmeg-2017.7.22.tar.gz/nutmeg-2017.7.22/setup.py
@@ -0,0 +1,30 @@
+
+from __future__ import division, print_function, unicode_literals, absolute_import
+
+import setuptools
+
+"""
+PyPi Instructions:
+https://packaging.python.org/distributing/#uploading-your-project-to-pypi
+
+twin command-line tool:
+https://github.com/pypa/twine
+"""
+
+version = '2017.7.22'
+
+dependencies = ['sarge', 'ordered-namespace']
+
+setuptools.setup(install_requires=dependencies,
+                 include_package_data=True,
+                 packages=setuptools.find_packages(),
+                 # zip_safe=False,
+
+                 name='nutmeg',
+                 description='Simple video processing tools. Python + fmpeg.',
+                 author='Pierre V. Villeneuve',
+                 author_email='pierre.villeneuve@gmail.com',
+                 url='https://github.com/Who8MyLunch/NutMeg',
+                 download_url='https://github.com/Who8MyLunch/NutMeg/archive/{}.tar.gz'.format(version),
+                 version=version,
+                 keywords=['video', 'ffmpeg', 'intra', 'clip', 'probe'])
