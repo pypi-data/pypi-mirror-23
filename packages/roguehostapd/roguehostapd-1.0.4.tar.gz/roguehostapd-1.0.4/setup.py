0230775441e405380a3fdebadc054134d9e0863c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:20 +0000

    Add package roguehostapd/1.0.4/roguehostapd-1.0.4.tar.gz

---
+++ packages/roguehostapd/roguehostapd-1.0.4.tar.gz/roguehostapd-1.0.4/setup.py
@@ -0,0 +1,45 @@
+"""
+Module for setup hostapd shared library
+"""
+
+import os
+from distutils.command.build import build
+from subprocess import call
+from setuptools import setup, find_packages
+
+BASEPATH = os.path.dirname(os.path.abspath(__file__))
+HOSTAPD_BUILD_PATH = os.path.join(BASEPATH, 'roguehostapd/hostapd-2.6/hostapd')
+
+
+class HostapdBuild(build):
+    """
+    Class for build the shared library of hostapd
+    """
+
+    def run(self):
+
+        build.run(self)
+        make_cmd = ['make', 'hostapd_lib']
+        cp_cmd = ['cp', 'defconfig', '.config']
+
+        def compile_hostapd():
+            """
+            Compile the shared library of hostapd
+            """
+            call(cp_cmd, cwd=HOSTAPD_BUILD_PATH)
+            call(make_cmd, cwd=HOSTAPD_BUILD_PATH)
+
+        self.execute(compile_hostapd, [], 'Compiling hostapd shared library')
+
+setup(
+    name='roguehostapd',
+    packages=find_packages(exclude=["docs", "tests"]),
+    version='1.0.4',
+    description='Hostapd wrapper for hostapd',
+    url='https://github.com/wifiphisher/roguehostapd',
+    include_package_data=True,
+    author='Anakin',
+    cmdclass={
+        'build': HostapdBuild,
+        }
+)
