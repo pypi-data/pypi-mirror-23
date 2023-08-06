574496aeeeb2e73a9037edab21f3c086e4f3d816
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:42 +0000

    Add package igdb-api-python/0.1/igdb-api-python-0.1.tar.gz

---
+++ packages/igdb-api-python/igdb-api-python-0.1.tar.gz/igdb-api-python-0.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'igdb-api-python',
+  packages = ['igdb-api-python'], # this must be the same as the name above
+  version = '0.1',
+  description = 'Python wrapper for IGDB.com API',
+  author = 'Sander Brauwers',
+  author_email = 'sander@igdb.com',
+  url = 'https://github.com/igdb/igdb-api-python', # use the URL to the github repo
+  download_url = 'https://github.com/peterldowns/mypackage/archive/0.1.tar.gz', # I'll explain this in a second
+  keywords = ['igdb', 'videogame', 'api','database'], # arbitrary keywords
+  classifiers = [],
+)
