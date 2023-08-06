0c7ec7fe32a8df74cde0190dfd46077ee5ef8c1e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:08:33 +0000

    Add package meet/1.2.9/meet-1.2.9.tar.gz

---
+++ packages/meet/meet-1.2.9.tar.gz/meet-1.2.9/meet/terminal/terminal.py
@@ -0,0 +1,36 @@
+#!usr/bin/python
+#  terminal.py
+
+import argparse
+
+
+def main():
+    pass
+
+
+def get_user_input():
+    try:
+        parser = argparse.ArgumentParser(description='a cli application to \
+                create a meeting notes google doc or local markdown file \
+                current or impending meeting on your google calendar')
+        group = parser.add_mutually_exclusive_group()
+        group.add_argument('-g', '--google',
+                           help='create a new google doc with minutes',
+                           action='store_true',
+                           dest='google')
+        group.add_argument('-m', '--markdown',
+                           help='create a local markdown file with minutes',
+                           action='store_true',
+                           dest='markdown')
+        parser.add_argument('-s', '--share',
+                            help='automatically share the google doc with \
+                                meeting participants',
+                            action='store_true',
+                            dest='share')
+        args = parser.parse_args()
+        return args
+    except ImportError:
+        flags = None
+
+if __name__ == '__main__':
+    main()
