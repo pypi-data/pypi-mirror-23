7fc1ffa35ab729ebe52c810b71e2f277eb024cdc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:08:18 +0000

    Add package add-swap/1.0/add_swap-1.0.tar.gz

---
+++ packages/add-swap/add_swap-1.0.tar.gz/add_swap-1.0/add_swap/__init__.py
@@ -0,0 +1,4 @@
+def add_swap(a, b):
+    c = a + b
+    print('The sum is {0} and a is {1} and b is {2}'.format(c, b, a))
+    return;
---
+++ packages/add-swap/add_swap-1.0.tar.gz/add_swap-1.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name = 'add_swap',
+    version = '1.0',
+    description = 'Add numbers and swap them',
+    url = 'http://github.com',
+    author = 'Sanchay',
+    author_email = 'sanchay.pahalwani@gmail.com',
+    license = 'MIT',
+    packages = ['add_swap'],
+    zip_safe = False
+)
