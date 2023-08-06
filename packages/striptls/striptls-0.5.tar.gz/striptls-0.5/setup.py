0c59aaa80bc727f037d9aad7804672749a24fb2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:27:35 +0000

    Add package striptls/0.5/striptls-0.5.tar.gz

---
+++ packages/striptls/striptls-0.5.tar.gz/striptls-0.5/setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+from setuptools import setup
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name="striptls",
+    version="0.5",
+    packages=["striptls"],
+    author="tintinweb",
+    author_email="tintinweb@oststrom.com",
+    description=(
+        "poc implementation of STARTTLS stripping attacks"),
+    license="GPLv2",
+    keywords=["striptls", "starttls", "strip", "attack", "proxy"],
+    url="https://github.com/tintinweb/striptls/",
+    download_url="https://github.com/tintinweb/striptls/tarball/v0.5",
+    #python setup.py register -r https://testpypi.python.org/pypi
+    long_description=read("README.rst") if os.path.isfile("README.rst") else read("README.md"),
+    install_requires=[],
+    package_data={
+                  'striptls': ['striptls'],
+                  },
+)
