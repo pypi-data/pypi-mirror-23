c0638605cec4a93a24c42fc0afe757702c06d129
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:46 +0000

    Add package deepdiff/3.3.0/deepdiff-3.3.0.tar.gz

---
+++ packages/deepdiff/deepdiff-3.3.0.tar.gz/deepdiff-3.3.0/setup.py
@@ -0,0 +1,44 @@
+import os
+from setuptools import setup
+
+# if you are not using vagrant, just delete os.link directly,
+# The hard link only saves a little disk space, so you should not care
+if os.environ.get('USER', '') == 'vagrant':
+    del os.link
+
+try:
+    with open('README.txt') as file:
+        long_description = file.read()
+except:
+    long_description = "Deep Difference and Search of any Python object/data."
+
+setup(name='deepdiff',
+      version='3.3.0',
+      description='Deep Difference and Search of any Python object/data.',
+      url='https://github.com/seperman/deepdiff',
+      download_url='https://github.com/seperman/deepdiff/tarball/master',
+      author='Seperman',
+      author_email='sep@zepworks.com',
+      license='MIT',
+      packages=['deepdiff'],
+      zip_safe=False,
+      test_suite="tests",
+      tests_require=['mock'],  # 'numpy==1.11.2' numpy is needed but comes already installed with travis
+      long_description=long_description,
+      install_requires=[
+          'jsonpickle'
+      ],
+      classifiers=[
+          "Intended Audience :: Developers",
+          "Operating System :: OS Independent",
+          "Topic :: Software Development",
+          "Programming Language :: Python :: 2.7",
+          "Programming Language :: Python :: 3.3",
+          "Programming Language :: Python :: 3.4",
+          "Programming Language :: Python :: 3.5",
+          "Programming Language :: Python :: 3.6",
+          "Programming Language :: Python :: Implementation :: PyPy",
+          "Development Status :: 5 - Production/Stable",
+          "License :: OSI Approved :: MIT License"
+      ],
+      )
