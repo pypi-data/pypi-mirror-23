4d830835b707a38941acb92c42276c04b76f6bfc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:13:44 +0000

    Add package APMonitor/0.31/APMonitor-0.31.zip

---
+++ packages/APMonitor/APMonitor-0.31.zip/APMonitor-0.31/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'APMonitor',
+  packages = ['APMonitor'], # this must be the same as the name above
+  version = '0.31',
+  description = 'Python package to interact with APMonitor models',
+  author = 'John Hedengren',
+  author_email = 'john_hedengren@byu.edu',
+  url = 'https://github.com/okesontj/APMonitor', # use the URL to the github repo
+  download_url = 'https://github.com/okesontj/APMonitor/tarball/0.3',
+  keywords = ['modeling', 'optimization', 'control'],
+  classifiers = [],
+)
