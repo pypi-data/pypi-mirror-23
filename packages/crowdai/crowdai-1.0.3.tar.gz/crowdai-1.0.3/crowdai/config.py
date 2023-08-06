6ff786951bc7865fe52ded1ad1ac61b4af5e54bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:11 +0000

    Add package crowdai/1.0.3/crowdai-1.0.3.tar.gz

---
+++ packages/crowdai/crowdai-1.0.3.tar.gz/crowdai-1.0.3/crowdai/config.py
@@ -0,0 +1,17 @@
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+
+from .challenges.opensnp_challenge_2017 import OpenSNPChallenge2017
+
+config = {
+    'remote_host' : 'grader.crowdai.org',
+    'remote_port' : 80,
+    'TIMEOUT_AUTHENTICATE' : 10,
+    'challenges' : {
+        'OpenSNPChallenge2017': {
+            "class": OpenSNPChallenge2017,
+            "TIMEOUT_EXECUTION" : 20000
+            }
+    }
+}
