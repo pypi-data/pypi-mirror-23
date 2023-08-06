caa48ce7c52fe7c275bc61699ad141eceab9b873
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:33:57 +0000

    Add package py3douyu/0.0.1/py3douyu-0.0.1.tar.gz

---
+++ packages/py3douyu/py3douyu-0.0.1.tar.gz/py3douyu-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'py3douyu',
+  packages = ['douyu', 'douyu.chat', 'douyu.chat.network'],
+  version = '0.0.1',
+  description = 'Python3 Wrapper for DouyuTV APIs, including support for accessing ChatRoom, e.g. DanMu',
+  author = 'Lovephy',
+  author_email = 'hypu1215@gmail.com',
+  url = 'https://github.com/lovephy/py3douyu',
+  download_url = 'https://github.com/lovephy/py3douyu',
+  keywords = ['douyu', 'douyutv', 'danmu', 'chat'],
+  classifiers = [],
+)
