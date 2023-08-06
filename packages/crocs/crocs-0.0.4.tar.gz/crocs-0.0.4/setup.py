7c38136259d22777d9064005fcbfec71edc9e997
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:13:39 +0000

    Add package crocs/0.0.4/crocs-0.0.4.tar.gz

---
+++ packages/crocs/crocs-0.0.4.tar.gz/crocs-0.0.4/setup.py
@@ -0,0 +1,50 @@
+
+from distutils.core import setup
+setup(name="crocs",
+      version="0.0.4",
+      py_modules=["crocs"],
+      author="Iury O. G. Figueiredo",
+      author_email="ioliveira@id.uff.br",
+      url='https://github.com/iogf/crocs',
+      download_url='https://github.com/iogf/crocs/releases',
+      keywords=[],
+      classifiers=[],
+      description="",)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
