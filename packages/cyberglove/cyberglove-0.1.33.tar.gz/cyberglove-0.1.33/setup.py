ff3f158259eb25fa66eaf3ce4e53fe5d07bd4e11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:07:31 +0000

    Add package cyberglove/0.1.33/cyberglove-0.1.33.tar.gz

---
+++ packages/cyberglove/cyberglove-0.1.33.tar.gz/cyberglove-0.1.33/setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+
+setup(name='cyberglove',
+      version='0.1.33',
+      description='Communicate with cyberglove from python',
+      packages=find_packages(),
+      author='Alex Ray',
+      author_email='aray@openai.com',
+      install_requires=['numpy>=1.12.1', 'pyserial>=3.3', 'mujoco_py>=1.50.0.0'],
+      include_package_data=True)
