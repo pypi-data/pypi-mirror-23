8dc0d861ea02844d35eff38c85099528da70deb9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:09:17 +0000

    Add package redis-interval/0.1/redis_interval-0.1-py2.7.egg

---
+++ packages/redis-interval/redis_interval-0.1-py2.7.egg/redis_interval/client.py
@@ -0,0 +1,46 @@
+from redis.client import StrictRedis
+
+
+class RedisInterval(StrictRedis):
+    """ Class that extends the redis client class adding methods for interval
+    set extensions
+    """
+    def __init__(self, *args, **kwargs):
+        """ Extends the callbacks dictionary to add interval sets
+        implementation """
+
+        self.RESPONSE_CALLBACKS['IADD'] = self.iadd_callback
+        self.RESPONSE_CALLBACKS['IREM'] = self.irem_callback
+        # self.RESPONSE_CALLBACKS['IREMBYSTAB'] = self.irembystab_callback
+        self.RESPONSE_CALLBACKS['ISTAB'] = self.istab_callback
+
+        super(RedisInterval, self).__init__(*args, **kwargs)
+
+    def iadd(self, key=None, start=0, end=0, value=None):
+        """ Adds interval set on redis """
+        return self.execute_command("IADD", key, start, end, value)
+
+    def iadd_callback(self, response, **options):
+        if response != 1:
+            raise Exception("Failed to insert Interval - IADD: %d" % response)
+        return 'OK'
+
+    def irem(self, key, value=None):
+        """ Removes interval from redis """
+        return self.execute_command("IREM", key, value)
+
+    def irem_callback(self, response, **options):
+        if response >= 1:
+            return 'OK'
+        return None
+
+    def irembystab(self, stab=None):
+        """ Removes interval from redis searching by stabs """
+        pass
+
+    def istab(self, key=None, value=None):
+        """ Searches for a range from stab """
+        return self.execute_command("ISTAB", key, value)
+
+    def istab_callback(self, response, **options):
+        return response
