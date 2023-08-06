ee53ebcc1ad227b58428f9dc3cea6d6af63abf0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:10:25 +0000

    Add package gym-mazeexplorer/0.0.1/gym_mazeexplorer-0.0.1.tar.gz

---
+++ packages/gym-mazeexplorer/gym_mazeexplorer-0.0.1.tar.gz/gym_mazeexplorer-0.0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+from setuptools import find_packages
+
+setup(name='gym_mazeexplorer',
+      version='0.0.1',
+      author='Mr-Yellow',
+      author_email='mr-yellow@mr-yellow.com',
+      description='A maze exploration environment for openai/gym',
+      packages=find_packages(),
+      url='https://github.com/mryellow/gym-mazeexplorer',
+      license='MIT',
+      install_requires=['gym', 'mazeexp']
+)
+
+
+#package_dir={'gym_mazeexplorer' : 'gym_mazeexplorer/envs'},
