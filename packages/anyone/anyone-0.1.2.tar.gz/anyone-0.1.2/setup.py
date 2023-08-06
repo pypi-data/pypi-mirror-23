7549e0c1b6cf56eaf00770163ad843ea5d33c21a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:07:55 +0000

    Add package anyone/0.1.2/anyone-0.1.2.tar.gz

---
+++ packages/anyone/anyone-0.1.2.tar.gz/anyone-0.1.2/setup.py
@@ -0,0 +1,45 @@
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.srt'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='anyone',
+
+    # Version
+    version='0.1.2',
+
+    description='anyone lived in a pretty how town - the poem',
+    long_description=long_description,
+    
+    url='https://github.com/herereadthis/anyone',
+
+    # Author details
+    author='herereadthis',
+    author_email='herereadthis@gmail.com',
+
+    license='MIT',
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3'
+    ],
+    keywords='sample development poem',
+    packages=find_packages(),
+    data_files=[
+        ('', ['anyone/data.json'])
+    ],
+    entry_points={
+        'console_scripts': [
+            'anyone=anyone:main'
+        ]
+    },
+    zip_safe=False
+)
