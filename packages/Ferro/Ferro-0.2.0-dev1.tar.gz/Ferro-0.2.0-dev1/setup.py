ee3cf7831ea1d87866df2eab490eee1cd71a55c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:27:32 +0000

    Add package Ferro/0.2.0-dev1/Ferro-0.2.0-dev1.tar.gz

---
+++ packages/Ferro/Ferro-0.2.0-dev1.tar.gz/Ferro-0.2.0-dev1/setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""
+Created on Thu Jun  1 08:33:01 2017
+
+@author: Jackson Anderson
+"""
+
+from distutils.core import setup
+
+setup(
+    name='Ferro',
+    version='0.2.0-dev1',
+    author='Jackson Anderson',
+    author_email='jda4923@rit.edu',
+	url = 'https://github.com/JAnderson419/Ferro',
+	download_url = 'https://github.com/JAnderson419/Ferro/archive/0.2.0-dev1.tar.gz',
+    packages=['ferro',],
+    license='Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International',
+    description='Manipulation and Modeling of Ferroelectric Test Data',
+    long_description=open('README.txt').read(),
+    install_requires=[
+        'scipy',
+        'numpy',
+        'matplotlib',
+        'mpldatacursor',
+#        'copy',
+#        're',
+#        'os',
+#        'csv',
+    ],
+)
