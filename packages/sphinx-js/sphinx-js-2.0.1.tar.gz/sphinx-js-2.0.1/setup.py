168bc3aabb929449143227fce11f4433f23edd0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:47:20 +0000

    Add package sphinx-js/2.0.1/sphinx-js-2.0.1.tar.gz

---
+++ packages/sphinx-js/sphinx-js-2.0.1.tar.gz/sphinx-js-2.0.1/setup.py
@@ -0,0 +1,44 @@
+# Prevent spurious errors during `python setup.py test` in 2.6, a la
+# http://www.eby-sarna.com/pipermail/peak/2010-May/003357.html:
+try:
+    import multiprocessing
+except ImportError:
+    pass
+
+from io import open
+from setuptools import setup, find_packages
+
+long_description=open('README.rst', 'r', encoding='utf8').read()
+
+setup(
+    name='sphinx-js',
+    version='2.0.1',
+    description='Support for using Sphinx on JSDoc-documented JS code',
+    long_description=long_description,
+    author='Erik Rose',
+    author_email='erikrose@grinchcentral.com',
+    license='MIT',
+    packages=find_packages(exclude=['ez_setup']),
+    tests_require=['nose',
+                   # Sphinx's plain-text renderer changes behavior slightly
+                   # with regard to how it emits class names in 1.6b1, which
+                   # breaks our admittedly brittle tests. 1.6b2 has a bug that
+                   # makes it crash on run, so we pin to 1.6b1.
+                   'Sphinx==1.6b1'],
+    test_suite='nose.collector',
+    url='https://github.com/erikrose/sphinx-js',
+    include_package_data=True,
+    install_requires=['docutils', 'Jinja2>2.0,<3.0', 'parsimonious>=0.7.0,<0.8.0', 'six<2.0', 'Sphinx<2.0'],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Topic :: Documentation :: Sphinx',
+        'Topic :: Software Development :: Documentation'
+        ],
+    keywords=['sphinx', 'documentation', 'docs', 'javascript', 'js', 'jsdoc', 'restructured'],
+)
---
+++ packages/sphinx-js/sphinx-js-2.0.1.tar.gz/sphinx-js-2.0.1/sphinx_js/jsdoc.py
@@ -0,0 +1,94 @@
+from collections import defaultdict
+from json import load
+from os.path import abspath, relpath, sep
+from subprocess import PIPE, Popen
+from tempfile import TemporaryFile
+
+from .parsers import path_and_formal_params, PathVisitor
+from .suffix_tree import SuffixTree
+
+
+def run_jsdoc(app):
+    """Run JSDoc across a whole codebase, and squirrel away its results."""
+    # Uses cwd, which Sphinx seems to set to the dir containing conf.py:
+    source_path = abspath(app.config.js_source_path)
+
+    # JSDoc defaults to utf8-encoded output.
+    jsdoc_command = ['jsdoc', source_path, '-X']
+    if app.config.jsdoc_config_path:
+        jsdoc_command.extend(['-c', app.config.jsdoc_config_path])
+    
+    # Use a temporary file to handle large output volume
+    with TemporaryFile() as temp:
+        p = Popen(jsdoc_command, stdout=temp, stderr=PIPE)
+        p.wait()
+        # Once output is finished, move back to beginning of file and load it:
+        temp.seek(0)
+        doclets = load(temp)
+
+    # 2 doclets are made for classes, and they are largely redundant: one for
+    # the class itself and another for the constructor. However, the
+    # constructor one gets merged into the class one and is intentionally
+    # marked as undocumented, even if it isn't. See
+    # https://github.com/jsdoc3/jsdoc/issues/1129.
+    doclets = [d for d in doclets if d.get('comment')
+                                     and not d.get('undocumented')]
+
+    # Build table for lookup by name, which most directives use:
+    app._sphinxjs_doclets_by_path = SuffixTree()
+    for d in doclets:
+        app._sphinxjs_doclets_by_path.add(
+            doclet_full_path(d, source_path),
+            d)
+
+    # Build lookup table for autoclass's :members: option. This will also
+    # pick up members of functions (inner variables), but it will instantly
+    # filter almost all of them back out again because they're undocumented.
+    # We index these by unambiguous full path. Then, when looking them up by
+    # arbitrary name segment, we disambiguate that first by running it through
+    # the suffix tree above. Expect trouble due to jsdoc's habit of calling
+    # things (like ES6 class methods) "<anonymous>" in the memberof field, even
+    # though they have names. This will lead to multiple methods having each
+    # other's members. But if you don't have same-named inner functions or
+    # inner variables that are documented, you shouldn't have trouble.
+    app._sphinxjs_doclets_by_class = defaultdict(lambda: [])
+    for d in doclets:
+        of = d.get('memberof')
+        if of:  # speed optimization
+            segments = doclet_full_path(d, source_path, longname_field='memberof')
+            app._sphinxjs_doclets_by_class[tuple(segments)].append(d)
+
+
+def doclet_full_path(d, base_dir, longname_field='longname'):
+    """Return the full, unambiguous list of path segments that points to an
+    entity described by a doclet.
+
+    Example: ``['./', 'dir/', 'dir/', 'file/', 'object.', 'object#', 'object']``
+
+    :arg d: The doclet
+    :arg base_dir: Absolutized value of the jsdoc_source_path option
+    :arg longname_field: The field to look in at the top level of the doclet
+        for the long name of the object to emit a path to
+    """
+    meta = d['meta']
+    rel = relpath(meta['path'], base_dir)
+    if not rel.startswith(('../', './')) and rel not in ('..', '.'):
+        # It just starts right out with the name of a folder in the cwd.
+        rooted_rel = './%s' % rel
+    else:
+        rooted_rel = rel
+
+    # Building up a string and then parsing it back down again is probably
+    # not the fastest approach, but it means knowledge of path format is in
+    # one place: the parser.
+    path = '%s/%s.%s' % (rooted_rel,
+                         without_ending(meta['filename'], '.js'),
+                         d[longname_field])
+    return PathVisitor().visit(
+        path_and_formal_params['path'].parse(path))
+
+
+def without_ending(str, ending):
+    if str.endswith(ending):
+        return str[:-len(ending)]
+    return str
