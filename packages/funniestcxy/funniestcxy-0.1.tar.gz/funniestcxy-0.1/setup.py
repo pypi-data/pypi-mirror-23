959799560f63e9d8d85dea0e55ad6e77341555c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:57:14 +0000

    Add package funniestcxy/0.1/funniestcxy-0.1.tar.gz

---
+++ packages/funniestcxy/funniestcxy-0.1.tar.gz/funniestcxy-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='funniestcxy',
+      version='0.1',
+      description='The funniest joke in the world',
+      url='http://github.com/storborg/funniest',
+      author='cxy',
+      author_email='crouchred@gmail.com',
+      license='MIT',
+      packages=['funniestcxy'],
+      zip_safe=False)
