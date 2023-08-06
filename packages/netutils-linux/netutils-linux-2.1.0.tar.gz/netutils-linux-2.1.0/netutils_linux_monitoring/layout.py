7c53ad27bbe7e8962a5f0d7123c01aea00ce0834
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:56:18 +0000

    Add package netutils-linux/2.1.0/netutils-linux-2.1.0.tar.gz

---
+++ packages/netutils-linux/netutils-linux-2.1.0.tar.gz/netutils-linux-2.1.0/netutils_linux_monitoring/layout.py
@@ -0,0 +1,28 @@
+# coding=utf-8
+
+""" Everything about console output's layout """
+
+from six import print_
+from prettytable import PrettyTable
+
+
+def make_table(header, align_map=None, rows=None):
+    """ Wrapper for pretty table """
+    t = PrettyTable()
+    t.horizontal_char = t.vertical_char = t.junction_char = ' '
+    t.field_names = header
+    if align_map:
+        for field, align in zip(header, align_map):
+            t.align[field] = align
+    if rows:
+        for row in rows:
+            if len(row) < len(t.field_names):
+                continue
+            try:
+                t.add_row(row)
+            except Exception as err:
+                print_('fields:', t.field_names)
+                print_('row:', row)
+                print_('rows:', rows)
+                raise err
+    return t
