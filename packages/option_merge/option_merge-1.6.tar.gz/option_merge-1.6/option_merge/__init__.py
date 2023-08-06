84ff0523272b8389bc357e6bd32a0fba8d3a990c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:33:37 +0000

    Add package option_merge/1.6/option_merge-1.6.tar.gz

---
+++ packages/option_merge/option_merge-1.6.tar.gz/option_merge-1.6/option_merge/__init__.py
@@ -0,0 +1,13 @@
+from option_merge.merge import ConverterProperty, KeyValuePairsConverter, AttributesConverter
+from option_merge.merge import MergedOptions
+from option_merge.converter import Converter
+from option_merge.not_found import NotFound
+
+# Explicitly make these part of this module
+NotFound = NotFound
+Converter = Converter
+MergedOptions = MergedOptions
+ConverterProperty = ConverterProperty
+AttributesConverter = AttributesConverter
+KeyValuePairsConverter = KeyValuePairsConverter
+
