c5a84a5be0d39c66dd188ac513285a4ff5bd5c7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:00 +0000

    Add package OnePy_trader/0.64.1/OnePy_trader-0.64.1.tar.gz

---
+++ packages/OnePy_trader/OnePy_trader-0.64.1.tar.gz/OnePy_trader-0.64.1/OnePy/OnePy.py
@@ -0,0 +1,177 @@
+#coding=utf8
+
+import pandas as pd
+import itertools
+import copy
+import Queue
+
+from event import events
+from fill import Fill
+
+import os,sys
+import matplotlib.pyplot as plt
+import matplotlib.style as style
+
+import feed as Feed
+import plot as Plot
+
+from tools.print_formater import dict_to_table
+from collections import OrderedDict
+
+class OnePiece():
+    def __init__(self):
+        self.feed_list = []
+        self.strategy_list = []
+
+        self.portfolio = None
+        self.broker = None
+
+        self.live_mode = False
+        self.target = None     # Forex, Futures, Stock
+        self.fill = Fill()
+        self.hedge_mode = False
+
+    def sunny(self):
+        # run_once function
+        Feed.run_first(self.feed_list)
+        self.fill.run_first(self.feed_list)
+
+        while 1:
+            try:
+                event = events.get(False)
+            except Queue.Empty:
+                Feed.load_all_feed(self.feed_list)
+                self.fill.update_timeindex(self.feed_list)
+
+                for f in self.feed_list:
+                    f._check_onoff = True        # 开启检查挂单
+            else:
+                if event.type is 'Market':
+                    self._pass_to_market(event) # 将fill的数据传送到各模块
+
+                    for s in self.strategy_list:
+                        s(event).run_strategy()
+
+                if event.type is 'Signal':
+                    self.portfolio(event).run_portfolio()
+
+                if event.type is 'Order':
+                    self.broker.run_broker(event)
+
+                if event.type is 'Fill':
+                    self.fill.run_fill(event)
+                    self._check_limit_stop_above_below(event)
+
+                if self._check_finish_backtest(self.feed_list):
+                    self._output_summary()
+                    break
+
+################### In Loop #######################
+    def _check_limit_stop_above_below(self,fillevent):
+        for f in self.feed_list:    # 判断属于哪个feed_list
+            if fillevent.instrument is f.instrument \
+            and f._check_onoff:
+                """检查之前在fill中有没有挂单成交等"""
+                self.fill.check_trade_list(f)
+                self.fill.check_order_list(f)
+                f._check_onoff = False       # 每个bar只检查一次挂单
+
+
+################### before #######################
+    def _adddata(self, feed_list):
+        [self.feed_list.append(data) for data in feed_list]
+
+    def _set_portfolio(self, portfolio):
+        self.portfolio = portfolio
+
+    def _addstrategy(self, strategy_list):
+        [self.strategy_list.append(st) for st in strategy_list]
+
+    def _set_broker(self,broker):
+        self.broker = broker()
+
+    def _set_target(self,target):
+        self.broker.target = target   # 将target传递给broker使用
+        for f in self.feed_list:
+            f.target = target
+
+    def set_backtest(self, feed_list,strategy_list,portfolio,broker,target='Forex'):
+        '''因为各个模块之间相互引用，所以要按照顺序add和set模块'''
+
+        # check target
+        if target != 'Forex' and target != 'Futures' and target != 'Stock':
+            raise SyntaxError('Target should be one of "Forex","Futures","Stock"')
+
+        if not isinstance(feed_list,list): feed_list = [feed_list]
+        if not isinstance(strategy_list,list): strategy_list = [strategy_list]
+
+        self.target = target
+        self._adddata(feed_list)
+        self._set_portfolio(portfolio)
+        self._addstrategy(strategy_list)
+        self._set_broker(broker)
+        self._set_target(target)
+
+    def set_commission(self,commission,margin,mult,commtype='fixed'):
+        if self.live_mode:
+            raise SyntaxError("Can't set commission in live_mode")
+        self.broker.commission = commission
+        self.broker.commtype = commtype
+        self.broker.margin = margin
+        self.broker.mult = mult
+
+        for st in self.strategy_list:
+            st._mult = mult
+
+        self.fill._mult = mult
+
+    def set_pricetype(self,pricetype ='close'):
+        for st in self.strategy_list:
+            st.pricetype = pricetype
+        self.fill.pricetype = pricetype
+
+    def set_cash(self,cash=100000):
+        self.fill.initial_cash = cash
+
+    def set_hedge(self,on=True):
+        self.hedge_mode = True
+
+    def set_notify(self,onoff=True):
+        self.broker._notify_onoff = onoff
+
+################### middle #######################
+    def _pass_to_market(self,marketevent):
+        """因为Strategy模块用到的是marketevent，所以通过marketevent传进去"""
+        m = marketevent
+        m.fill = self.fill
+        self.portfolio.fill = self.fill
+        self.broker.fill = self.fill
+
+    def _check_finish_backtest(self,feed_list):
+        # if finish, sum(backtest) = 0 + 0 + 0 = 0 -> False
+        backtest = [i.continue_backtest for i in feed_list]
+        return not sum(backtest)
+
+
+################### after #######################
+    def _output_summary(self):
+        from statistics import create_drawdowns, create_sharpe_ratio
+        total = pd.DataFrame(self.fill.total_list)[1:]
+        total.set_index('date',inplace=True)
+        pct_returns = total.pct_change()
+        m,d = create_drawdowns(pct_returns['total'])
+        d = OrderedDict()
+        d['Final_Value'] = round(self.fill.total_list[-1]['total'],3)
+        d['Total_return'] = round(d['Final_Value']/self.fill.initial_cash-1,5)
+        d['Total_return'] = str(d['Total_return']*100)+'%'
+        d['Max_Drawdown'],d['Duration']=create_drawdowns(pct_returns['total'])
+        d['Max_Drawdown'] = str(d['Max_Drawdown'])+'%'
+        d['Sharpe_Ratio'] = round(create_sharpe_ratio(pct_returns),3)
+        print dict_to_table(d)
+
+
+    def plot(self,name,instrument=None):
+        if instrument is None:
+            instrument = self.feed_list[0].instrument
+        fig = Plot.matplotlib(self.fill)
+        fig.plot(name,instrument)
---
+++ packages/OnePy_trader/OnePy_trader-0.64.1.tar.gz/OnePy_trader-0.64.1/OnePy/strategy.py
@@ -0,0 +1,271 @@
+#coding=utf8
+
+from event import events, SignalEvent
+from portfolio import *
+
+from utils.py3 import with_metaclass
+from utils.metabase import MetaParams
+
+from indicator import indicator
+
+class StrategyBase(with_metaclass(MetaParams, object)):
+    def __init__(self,marketevent):
+
+        m = marketevent
+        self.m = marketevent
+        self.pricetype = 'open'             # 控制计算的价格，可以再OnePy中用 set_pricetype控制
+
+        self.bar = m.cur_bar_list
+        self.bar_dict = m.bar_dict
+        self.data = m.cur_bar_list[0]
+        self.close = [i['close'] for i in m.bar_dict[m.instrument]]
+        self.instrument = m.instrument
+        self.cash = [i['cash'] for i in m.fill.cash_list]
+        self.position = [i['position'] for i in m.fill.position_dict[m.instrument]]
+        self.margin = [i['margin'] for i in m.fill.margin_dict[m.instrument]]
+        self.total = [i['total'] for i in m.fill.total_list]
+        self.avg_price = [i['avg_price'] for i in m.fill.avg_price_dict[m.instrument]]
+        self.unre_profit = [i['unre_profit'] for i in m.fill.unre_profit_dict[m.instrument]]
+        self.re_profit = [i['re_profit'] for i in m.fill.re_profit_dict[m.instrument]]
+
+
+    def set_indicator(self):
+        self.indicator = indicator()
+        self.indicator._set_feed(self.m)
+        self.i = self.indicator     # shortcut
+
+
+    def pips(self,n):
+        n = n*1.0/self._mult
+        pips_cls = type('pips',(),dict(pips=n))
+        pips_cls.type = 'pips'
+        return pips_cls
+
+    def pct(self,n):
+        """若输入1，则为原价格的1%"""
+        n = n*0.01
+        pct_cls = type('pct',(),dict(pct=n))
+        pct_cls.type = 'pct'
+        return pct_cls
+
+    def _check_pips_or_pct(self,signal_type,price,info):
+        """控制挂单，控制止盈止损和移动止损"""
+        """用于Buy和Sell中，应放到price确定之后"""
+        if 'Buy' in signal_type:        # 包含了挂单
+            mark = 1
+        elif 'Sell' in signal_type:       # 与Buy相反，包含了挂单
+            mark = -1
+        else:
+            raise SyntaxError
+
+        if self.pricetype is 'open': p = self.bar[1]['open']
+        elif self.pricetype is 'close': p = self.bar[0]['close']
+
+        #判断开盘价还是收盘价
+        if price is 'open':
+            price = self.bar[1]['open']
+            info['price'] = price
+        elif price is 'close':
+            price = self.bar[0]['close']
+            info['price'] = price
+
+        #控制挂单
+        if type(price) is type:
+            if price.type is 'pips':
+                price = price.pips + p
+                info['price'] = price
+            elif price.type is 'pct':
+                price = p * price.pct
+                info['price'] = price
+            else:
+                raise SyntaxError('price should be pips or pct!')
+
+        #控制止盈止损
+        limit = info['limit']
+        stop = info['stop']
+        if limit:
+            if limit.type is 'pips':
+                info['limit'] = price + limit.pips * mark
+            elif limit.type is 'pct':
+                info['limit'] = price * (1+limit.pct * mark)
+            else:
+                raise SyntaxError('limit should be pips or pct!')
+        if stop:
+            if stop.type is 'pips':
+                info['stop'] = price - stop.pips * mark
+            elif stop.type is 'pct':
+                info['stop'] = price * (1-stop.pct * mark)
+            else:
+                raise SyntaxError('stop should be pips or pct!')
+
+
+
+
+    def Buy(self,size,
+                limit=None,
+                stop=None,
+                trailingstop=None,
+                instrument=None,
+                price = None):
+
+        if price is None:
+            price =  self.pricetype
+        if instrument is None:
+            instrument = self.instrument
+
+        info = dict(signal_type='Buy',
+                    date=self.bar[0]['date'],
+                    size=size,
+                    price=price,
+                    limit=limit,
+                    stop=stop,
+                    trailingstop=trailingstop,
+                    oco=False,
+                    instrument=instrument,
+                    executetype = 'MKT',
+                    direction = 1.0)
+
+        self._check_pips_or_pct('Buy',price,info)
+
+        if info['price'] > self.bar[1]['open']:
+            info['signal_type'] = 'Buyabove'
+        elif info['price'] < self.bar[1]['open']:
+            info['signal_type'] = 'Buybelow'
+
+        signalevent = SignalEvent(info)
+        events.put(signalevent)
+
+    def Sell(self,size,
+                limit=None,
+                stop=None,
+                trailingstop=None,
+                instrument=None,
+                price = None):
+        if price is None:
+            price =  self.pricetype
+
+        if instrument is None or instrument is self.instrument:
+            instrument = self.instrument
+
+        info = dict(signal_type='Sell',
+                    date=self.bar[0]['date'],
+                    size=size,price=price,
+                    limit=limit,
+                    stop=stop,
+                    trailingstop=trailingstop,
+                    oco=False,
+                    instrument=instrument,
+                    executetype = 'MKT',
+                    direction = -1.0)
+
+        self._check_pips_or_pct('Sell',price,info)
+
+        if info['price'] > self.bar[1]['open']:
+            info['signal_type'] = 'Sellabove'
+        elif info['price'] < self.bar[1]['open']:
+            info['signal_type'] = 'Sellbelow'
+
+
+        signalevent = SignalEvent(info)
+        events.put(signalevent)
+
+
+
+    def Exitall(self,size='all',instrument=None,price=None):
+
+        if price is None:
+            price =  self.pricetype
+
+        if instrument is None or instrument is self.instrument:
+            instrument = self.instrument
+
+        info = dict(signal_type='Exitall',
+                    date=self.bar[0]['date'],
+                    size=size,price=price,
+                    limit=None,
+                    stop=None,
+                    trailingstop=None,
+                    oco=False,
+                    instrument=instrument,
+                    executetype = 'MKT')
+
+
+        if price is 'open':
+            price = self.bar[1]['open']
+            info['price'] = price
+        elif price is 'close':
+            price = self.bar[0]['close']
+            info['price'] = price
+
+
+        if self.position[-1] < 0:
+            info['signal_type'] = 'Buy'
+            info['size'] = self.position[-1] * (-1.0)
+            info['direction'] = 1.0
+        elif self.position[-1] > 0:
+            info['signal_type'] = 'Sell'
+            info['size'] = self.position[-1] * 1.0
+            info['direction'] = -1.0
+        if self.position[-1] == 0:
+            pass
+        else:
+            signalevent = SignalEvent(info)
+            events.put(signalevent)
+
+
+    def Cancel(self):
+        pass
+
+
+
+    def prestart(self):
+        pass
+
+    def start(self):
+        self.set_indicator()
+
+    def prenext(self):
+        pass
+
+    def next(self):
+        """这里写主要的策略思路"""
+        pass
+
+    def stop(self):
+        pass
+
+    def run_strategy(self):
+        self.prestart()
+        self.start()
+        self.prenext()
+        self.next()
+        self.stop()
+
+
+
+class DIYStrategy(with_metaclass(MetaParams, StrategyBase)):
+    def __init__(self,marketevent):
+        super(MyStrategy,self).__init__(marketevent)
+
+
+    def set_indicator(self):
+        pass
+
+    def prestart(self):
+        pass
+
+    def start(self):
+        self.set_indicator()
+
+    def prenext(self):
+        pass
+
+    def next(self):
+        """这里写主要的策略思路"""
+        if self.data['close'] > self.data['open']:
+            self.Buy(0.1)
+        if self.data['close'] < self.data['open']:
+            self.Sell(0.1)
+
+    def stop(self):
+        pass
