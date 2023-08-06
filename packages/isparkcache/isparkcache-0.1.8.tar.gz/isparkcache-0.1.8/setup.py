05ef3037687e624c3d12fec3aad775733ee0fa87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:24 +0000

    Add package isparkcache/0.1.8/isparkcache-0.1.8.tar.gz

---
+++ packages/isparkcache/isparkcache-0.1.8.tar.gz/isparkcache-0.1.8/setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(
+    name='isparkcache',
+    version='0.1.8',
+    packages=['isparkcache', 'isparkcache.fs'],
+    install_requires=["snakebite"],
+    url='https://github.com/bataeves/isparkcache',
+    download_url='https://github.com/bataeves/isparkcache/archive/0.1.8.tar.gz',
+    license='',
+    author='Bataev Evgeny',
+    author_email='bataev.evgeny@gmail.com',
+    description='Cache Spark Dataframes for Jupyter',
+    long_description=open("README.rst").read(),
+    keywords=['spark', 'jupyter', 'ipython']
+)
