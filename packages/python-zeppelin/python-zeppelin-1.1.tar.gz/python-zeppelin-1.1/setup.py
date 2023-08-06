bf52671abbe4cd0511a772df5810c666019ea8a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:12:20 +0000

    Add package python-zeppelin/1.1/python-zeppelin-1.1.tar.gz

---
+++ packages/python-zeppelin/python-zeppelin-1.1.tar.gz/python-zeppelin-1.1/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+from setuptools import setup, find_packages
+
+setup(
+    name='python-zeppelin',
+    author='Wendy Fu',
+    author_email='wfu@mozilla.com',
+    description='Converts Zeppelin JSON files to Markdown',
+    version='1.1',
+    keywords='zeppelin notebook converter markdown',
+    url='https://github.com/comloo/python-zeppelin',
+    entry_points={
+    	'console_scripts': [
+    		'zeppelin-convert = zeppelin.cli.convert:main',
+    		'zeppelin-execute = zeppelin.cli.execute:main'
+    	],
+    },
+    packages=find_packages(),
+    install_requires=['pytest-runner', 'cairosvg', 'python-dateutil'],
+    tests_require=['pytest']
+)
