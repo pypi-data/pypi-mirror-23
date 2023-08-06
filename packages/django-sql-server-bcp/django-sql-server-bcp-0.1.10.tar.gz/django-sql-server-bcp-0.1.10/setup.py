a51aa94ec63460f3f94dd5e225c13f3a650694ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:08:54 +0000

    Add package django-sql-server-bcp/0.1.10/django-sql-server-bcp-0.1.10.tar.gz

---
+++ packages/django-sql-server-bcp/django-sql-server-bcp-0.1.10.tar.gz/django-sql-server-bcp-0.1.10/setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name='django-sql-server-bcp',
+    version='0.1.10',
+    author='Exotic Objects LLC',
+    author_email='git@extc.co',
+    license='MIT',
+    url='https://github.com/ExoticObjects/django-sql-server-bcp',
+    include_package_data=True,
+    long_description=open('README.md').read(),
+    description='Utility for using mssql-tools BCP command with Django models',
+    packages=find_packages(),
+)
