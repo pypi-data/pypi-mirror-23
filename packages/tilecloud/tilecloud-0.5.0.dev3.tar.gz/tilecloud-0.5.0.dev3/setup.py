af0752db9f7e27b325b6701ba9b7b3238196ab0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:13:33 +0000

    Add package tilecloud/0.5.0.dev3/tilecloud-0.5.0.dev3.tar.gz

---
+++ packages/tilecloud/tilecloud-0.5.0.dev3.tar.gz/tilecloud-0.5.0.dev3/setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup, find_packages
+from glob import glob
+import os
+
+version = '0.5.0.dev3'
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.rst')).read()
+
+install_requires = open(os.path.join(here, 'requirements.txt')).read().splitlines()
+
+setup(
+    name='tilecloud',
+    version=version,
+    description='Tools for managing tiles',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Topic :: Scientific/Engineering :: GIS',
+    ],
+    author='Tom Payne',
+    author_email='twpayne@gmail.com',
+    url='http://github.com/twpayne/tilecloud',
+    license='BSD',
+    packages=find_packages(exclude=['tiles', 'tilecloud.tests']),
+    zip_safe=True,
+    install_requires=install_requires,
+    test_suite='tilecloud.tests',
+    scripts=glob('tc-*'),
+    entry_points="""
+    # -*- Entry points: -*-
+    """,
+    long_description=README,
+)
