ce8ad92c6b54916841bfed7b62866d58eeebd2a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:28 +0000

    Add package firefly-python/0.1.0/firefly-python-0.1.0.tar.gz

---
+++ packages/firefly-python/firefly-python-0.1.0.tar.gz/firefly-python-0.1.0/setup.py
@@ -0,0 +1,42 @@
+from setuptools import setup, find_packages
+import os.path
+import sys
+
+PY2 = (sys.version_info.major == 2)
+
+def get_version():
+    """Returns the package version taken from version.py.
+    """
+    root = os.path.dirname(__file__)
+    version_path = os.path.join(root, "firefly/version.py")
+    with open(version_path) as f:
+        code = f.read()
+        env = {}
+        exec(code, env, env)
+        return env['__version__']
+
+install_requires = [
+    'gunicorn==19.7.1',
+    'WebOb==1.7.2',
+    'requests==2.18.1',
+    'PyYAML==3.12'
+]
+
+if PY2:
+    install_requires.append('funcsigs==1.0.2')
+
+__version__ = get_version()
+
+setup(
+    name='firefly-python',
+    version=__version__,
+    author='rorodata',
+    author_email='rorodata.team@gmail.com',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=install_requires,
+    entry_points='''
+        [console_scripts]
+        firefly=firefly.main:main
+    ''',
+)
---
+++ packages/firefly-python/firefly-python-0.1.0.tar.gz/firefly-python-0.1.0/tests/test_app.py
@@ -0,0 +1,66 @@
+from webob import Request, Response
+from firefly.app import Firefly, FireflyFunction
+
+def square(a):
+    '''Computes square'''
+    return a**2
+
+class TestFirefly:
+    def test_generate_function_list(self):
+        firefly = Firefly()
+        assert firefly.generate_function_list() == {}
+
+        firefly.add_route("/square", square, "square")
+        returned_dict = {
+                "square": {
+                    "path": "/square",
+                    "doc": "Computes square"
+                }
+            }
+        assert firefly.generate_function_list() == returned_dict
+
+    def test_generate_function_list_for_func_name(self):
+        firefly = Firefly()
+        firefly.add_route("/sq2", square, "sq")
+        returned_dict = {
+                "sq": {
+                    "path": "/sq2",
+                    "doc": "Computes square"
+                }
+            }
+        assert firefly.generate_function_list() == returned_dict
+
+    def test_function_call(self):
+        app = Firefly()
+        app.add_route("/", square)
+
+        request = Request.blank("/", POST='{"a": 3}')
+        response = app.process_request(request)
+        assert response.status == '200 OK'
+        assert response.text == '9'
+
+    def test_auth_failure(self):
+        app = Firefly(auth_token='abcd')
+        app.add_route("/", square)
+
+        request = Request.blank("/", POST='{"a": 3}')
+        response = app.process_request(request)
+        print(response.text)
+        assert response.status == '403 Forbidden'
+
+        headers = {
+            "Authorization": "token bad-token"
+        }
+        request = Request.blank("/", POST='{"a": 3}', headers=headers)
+        response = app.process_request(request)
+        assert response.status == '403 Forbidden'
+
+
+class TestFireflyFunction:
+    def test_call(self):
+        func = FireflyFunction(square)
+        request = Request.blank("/square", POST='{"a": 3}')
+        response = func(request)
+        assert response.status == '200 OK'
+        assert response.text == '9'
+
