a9fe365368f6794974eac1e1d2839cdade8a43d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:33:32 +0000

    Add package putcall/0.1/putcall-0.1.tar.gz

---
+++ packages/putcall/putcall-0.1.tar.gz/putcall-0.1/README.rst
@@ -0,0 +1,63 @@
+=================================
+putcall - option pricing formulae
+=================================
+
+.. image:: https://img.shields.io/codeship/b731e790-9d6a-0134-75d5-4e318545e956/master.svg
+    :target: https://codeship.com//projects/188643
+
+Collection of classical option pricing formulas.
+
+
+Example Usage
+-------------
+
+.. code-block:: python
+
+    from datetime import date
+
+    from putcall import black_scholes
+
+
+Install
+-------
+
+The latest stable version can always be installed or updated via pip:
+
+.. code-block:: bash
+
+    $ pip install putcall
+
+If the above fails, please try easy_install instead:
+
+.. code-block:: bash
+
+    $ easy_install putcall
+
+
+Development Version
+-------------------
+
+The latest development version can be installed directly from GitHub:
+
+.. code-block:: bash
+
+    $ pip install --upgrade git+https://github.com/pbrisk/putcall.git
+
+
+Contributions
+-------------
+
+.. _issues: https://github.com/pbrisk/putcall/issues
+.. __: https://github.com/pbrisk/putcall/pulls
+
+Issues_ and `Pull Requests`__ are always welcome.
+
+
+License
+-------
+
+.. __: https://github.com/pbrisk/putcall/raw/master/LICENSE
+
+Code and documentation are available according to the Apache Software License (see LICENSE__).
+
+
---
+++ packages/putcall/putcall-0.1.tar.gz/putcall-0.1/setup.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+
+#  putcall
+#  ------------
+#  Collection of classical option pricing formulas.
+#
+#  Author:  pbrisk <pbrisk_at_github@icloud.com>
+#  Copyright: 2016, 2017 Deutsche Postbank AG
+#  Website: https://github.com/pbrisk/putcall
+#  License: APACHE Version 2 License (see LICENSE file)
+
+
+import codecs
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+setup(
+    name='putcall',
+    description='Collection of classical option pricing formulas.',
+    version='0.1',
+    author='Deutsche Postbank AG [pbrisk]',
+    author_email='pbrisk_at_github@icloud.com',
+    url='https://github.com/pbrisk/putcall',
+    bugtrack_url='https://github.com/pbrisk/putcall/issues',
+    license='Apache License 2.0',
+    packages=['putcall',
+              'putcall.formulas',
+              'putcall.formulas.plain_vanilla_options',
+              'putcall.formulas.exotic_options',
+              'putcall.formulas.interest_rate_options',
+              'putcall.calibration'],
+    install_requires=['mathtoolspy'],
+    long_description=codecs.open('README.rst', encoding='utf-8').read(),
+    platforms='any',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Education',
+        'Intended Audience :: Financial and Insurance Industry',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Education',
+        'Topic :: Office/Business',
+        'Topic :: Office/Business :: Financial',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Scientific/Engineering :: Mathematics',
+        'Topic :: Utilities',
+        'Topic :: Office/Business :: Scheduling',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Software Development :: Localization',
+    ],
+)
