5c87cb19d08d6a35e0ab7d96f183764fcd9f3896
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:38 +0000

    Add package overgang/0.1.0/overgang-0.1.0.tar.gz

---
+++ packages/overgang/overgang-0.1.0.tar.gz/overgang-0.1.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='overgang',
+      version='0.1.0',
+      description='transition matrix estimation',
+      url='http://github.com/ulf1/overgang',
+      author='Ulf Hamster',
+      author_email='554c46@gmail.com',
+      license='MIT',
+      packages=['overgang'],
+      zip_safe=False)
+
