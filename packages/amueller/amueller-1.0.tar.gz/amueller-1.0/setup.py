a8f3a04d6e7df3925a24ac943493ed1a3f66271b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:15 +0000

    Add package amueller/1.0/amueller-1.0.tar.gz

---
+++ packages/amueller/amueller-1.0.tar.gz/amueller-1.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    author="Andreas Mueller",
+    author_email="t3kcit+vanity@gmail.com",
+    name='amueller',
+    version='1.0',
+    url='https://amueller.github.io',
+    description="It's me",
+    license='MIT',
+    packages=['amueller'],
+)
