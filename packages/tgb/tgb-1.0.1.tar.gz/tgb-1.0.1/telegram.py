2370b20c1f1c6b9b697afa87f8001d5b94180e5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:08:52 +0000

    Add package tgb/1.0.1/tgb-1.0.1.tar.gz

---
+++ packages/tgb/tgb-1.0.1.tar.gz/tgb-1.0.1/telegram.py
@@ -0,0 +1,21 @@
+import requests
+from functools import partial
+
+
+class TelegramApi:
+
+    def __init__(self, token):
+        self.url = f'https://api.telegram.org/bot{token}/'
+
+    def __getattr__(self, name):
+        method = self.snake_to_camel(name)
+        return partial(self.call, method)
+
+    def call(self, method, **params):
+        response = requests.get(self.url + method, params=params)
+        response.raise_for_status()
+        return response.json()
+
+    def snake_to_camel(self, text):
+        words = text.split('_')
+        return words[0] + ''.join([w.title() for w in words[1:]])
