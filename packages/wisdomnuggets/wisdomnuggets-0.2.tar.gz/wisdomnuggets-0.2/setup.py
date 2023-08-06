7e627f4c43c253b33a552cc0b7703bf160a7406a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:14:41 +0000

    Add package wisdomnuggets/0.2/wisdomnuggets-0.2.tar.gz

---
+++ packages/wisdomnuggets/wisdomnuggets-0.2.tar.gz/wisdomnuggets-0.2/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='wisdomnuggets',
+	version='0.2',
+	description='The wisest maxim in the world',
+	url='https://github.com/ss1921',
+	author='1921Designs',
+	author_email='im1921designs@gmail.com',
+	license='MIT',
+	packages=['wisdomnuggets'],
+	install_requires=['markdown'],
+	zip_safe=False)
