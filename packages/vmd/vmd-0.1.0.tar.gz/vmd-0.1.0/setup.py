e1131d4a63a3535e48274e04488d8c47ba853eeb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:11 +0000

    Add package vmd/0.1.0/vmd-0.1.0.tar.gz

---
+++ packages/vmd/vmd-0.1.0.tar.gz/vmd-0.1.0/setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup, find_packages
+
+#try:
+#    import pypandoc
+#    description = pypandoc.convert('README.md', 'rst')
+#except (IOError, ImportError):
+#    description = ''
+
+
+setup(
+    name = 'vmd',
+    packages = find_packages(),
+    package_data = {
+        'vmd': ['themes/*']
+    },
+    version = '0.1.0',
+    description = 'Terminal Markdown Renderer',
+    author = 'Charles Pascoe',
+    url='https://github.com/cpascoe95/vmd',
+    author_email = 'charles@cpascoe.co.uk',
+    keywords = ['terminal', 'markdown', 'viewer', 'md'],
+    classifiers = [ # https://pypi.python.org/pypi?%3Aaction=list_classifiers
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Text Processing :: Markup',
+        'Topic :: Utilities'
+    ],
+    install_requires = [
+        'Markdown==2.6.8'
+    ],
+    entry_points={
+        'console_scripts': [
+            'vmd = vmd:main'
+        ]
+    }
+)
