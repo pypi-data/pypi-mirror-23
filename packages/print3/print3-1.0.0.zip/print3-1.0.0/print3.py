72d52719879b112999d4a4ae1cf7124227a0dd51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:49:12 +0000

    Add package print3/1.0.0/print3-1.0.0.zip

---
+++ packages/print3/print3-1.0.0.zip/print3-1.0.0/print3.py
@@ -0,0 +1,10 @@
+"""
+in the python2,use print like python3
+end to control should or shouldn't change row
+"""
+def print3(s,end = '1'):
+    r = str(s)
+    if len(end)==0:
+        print r,
+    else:
+        print r
---
+++ packages/print3/print3-1.0.0.zip/print3-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name        = 'print3',
+    version     = '1.0.0',
+    py_modules  = ['print3'],
+    author      = 'AlexLi',
+    author_email= 'yunbo_l@163.com',
+    url     =   'http://www.headfirstlabs.com',
+    )
