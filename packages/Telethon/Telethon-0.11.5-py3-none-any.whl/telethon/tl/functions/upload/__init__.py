8b41e9658951c1f75a49bbadec428027d2f3db7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:06 +0000

    Add package Telethon/0.11.5/Telethon-0.11.5-py3-none-any.whl

---
+++ packages/Telethon/Telethon-0.11.5-py3-none-any.whl/telethon/tl/functions/upload/__init__.py
@@ -0,0 +1,6 @@
+from .save_file_part import SaveFilePartRequest
+from .get_file import GetFileRequest
+from .save_big_file_part import SaveBigFilePartRequest
+from .get_web_file import GetWebFileRequest
+from .get_cdn_file import GetCdnFileRequest
+from .reupload_cdn_file import ReuploadCdnFileRequest
