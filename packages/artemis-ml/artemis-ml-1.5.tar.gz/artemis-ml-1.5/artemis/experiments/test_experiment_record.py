a20eb7743ab478bcd026ef6c55c166ae6a7865ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:10:30 +0000

    Add package artemis-ml/1.5/artemis-ml-1.5.tar.gz

---
+++ packages/artemis-ml/artemis-ml-1.5.tar.gz/artemis-ml-1.5/artemis/experiments/test_experiment_record.py
@@ -0,0 +1,249 @@
+import time
+import warnings
+
+import itertools
+import matplotlib.pyplot as plt
+import numpy as np
+from artemis.experiments.experiment_record import \
+    experiment_id_to_latest_record_id, get_experiment_info, load_experiment_record, ExperimentRecord, record_experiment, \
+    delete_experiment_with_id, get_current_experiment_dir, experiment_function, open_in_experiment_dir, \
+    experiment_testing_context, ExpInfoFields, ExpStatusOptions
+from artemis.experiments.deprecated import start_experiment, end_current_experiment
+from artemis.general.test_mode import set_test_mode
+
+__author__ = 'peter'
+
+"""
+The experiment interface.  Currently, this can be used in many ways.  We'd like to
+converge on just using the interface demonstrated in test_get_latest_identifier.  So
+use that if you're looking for an example.
+"""
+
+
+@experiment_function
+def experiment_test_function(seed=1234):
+
+    rng = np.random.RandomState(seed)
+    with warnings.catch_warnings():  # This is just to stop that stupid matplotlib warning from screwing up our logs.
+        warnings.simplefilter('ignore')
+
+        print 'aaa'
+        plt.figure('sensible defaults')
+        dat = rng.randn(4, 5)
+        plt.subplot(211)
+        plt.imshow(dat)
+        plt.subplot(212)
+        plt.imshow(dat, interpolation = 'nearest', cmap = 'gray')
+        plt.show()
+        print 'bbb'
+        plt.figure()
+        plt.plot(rng.randn(10))
+        plt.show()
+
+
+def assert_experiment_record_is_correct(exp_rec, show_figures=False):
+
+    assert exp_rec.get_log() == 'aaa\nbbb\n'
+    assert len(exp_rec.get_figure_locs()) == 2
+
+    # Now assert that you can load an experiment from file and again display the figures.
+    exp_dir = exp_rec.get_dir()
+    exp_rec_copy = ExperimentRecord(exp_dir)
+    assert exp_rec_copy.get_log() == 'aaa\nbbb\n'
+    if show_figures:
+        exp_rec_copy.show_figures()
+    assert len(exp_rec_copy.get_figure_locs()) == 2
+
+
+def test_experiment_with():
+    """
+    DEPRECATED INTERFACE
+
+    This syntax uses the record_experiment function directly instead of hiding it.
+    """
+
+    with experiment_testing_context():
+        delete_experiment_with_id('test_exp')
+        with record_experiment(identifier = 'test_exp', print_to_console=True) as exp_rec:
+            experiment_test_function()
+            assert_experiment_record_is_correct(exp_rec, show_figures=False)
+
+
+def test_start_experiment():
+    """
+    DEPRECATED INTERFACE
+
+    An alternative syntax to the with statement - less tidy but possibly better
+    for notebooks and such because it avoids you having to indent all code in the
+    experiment.
+    """
+
+    delete_experiment_with_id('start_stop_test')
+    with experiment_testing_context():
+        record = start_experiment('start_stop_test')
+        experiment_test_function()
+        end_current_experiment()
+        assert_experiment_record_is_correct(record, show_figures=False)
+
+
+def test_run_and_show():
+    """
+
+    This is nice because it no longer required that an experiment be run and shown in a
+    single session - each experiment just has a unique identifier that can be used to show
+    its results whenevs.
+    """
+    with experiment_testing_context():
+        experiment_record = experiment_test_function.run()
+        assert_experiment_record_is_correct(experiment_record, show_figures=False)
+        experiment_record.show()
+
+
+def test_get_latest():
+    with experiment_testing_context():
+        record_1 = experiment_test_function.run()
+        time.sleep(0.01)
+        record_2 = experiment_test_function.run()
+        identifier = experiment_id_to_latest_record_id('experiment_test_function')
+        assert identifier == record_2.get_identifier()
+
+
+def test_get_latest_identifier():
+
+    with experiment_testing_context():
+        exp_rec = experiment_test_function.run()
+        print get_experiment_info('experiment_test_function')
+        assert_experiment_record_is_correct(exp_rec)
+        last_experiment_identifier = experiment_id_to_latest_record_id('experiment_test_function')
+        assert last_experiment_identifier is not None, 'Experiment was run, this should not be none'
+        same_exp_rec = load_experiment_record(last_experiment_identifier)
+        assert_experiment_record_is_correct(same_exp_rec)
+
+
+def test_accessing_experiment_dir():
+
+    with experiment_testing_context():
+
+        @experiment_function
+        def access_dir_test():
+            print '123'
+            print 'abc'
+            dir = get_current_experiment_dir()
+            with open_in_experiment_dir('my_test_file.txt', 'w') as f:
+                f.write('Experiment Directory is: {}'.format(dir))
+
+        record = access_dir_test.run()
+
+        filepaths = record.list_files()
+
+        assert 'my_test_file.txt' in filepaths
+
+        with record.open_file('my_test_file.txt') as f:
+            assert f.read() == 'Experiment Directory is: {}'.format(record.get_dir())
+
+        with record.open_file('output.txt') as f:
+            assert f.read() == '123\nabc\n'
+
+
+@experiment_function
+def add_some_numbers_test_experiment(a=1, b=1):
+    c = a + b
+    print c
+    return c
+
+
+def test_saving_result():
+    # Run root experiment
+    with experiment_testing_context():
+        rec = add_some_numbers_test_experiment.run()
+        assert rec.get_result() == 2
+
+
+def test_variants():
+
+    @experiment_function
+    def add_some_numbers(a=1, b=1):
+        c=a+b
+        print c
+        return c
+
+    with experiment_testing_context():
+
+        # Create a named variant
+        e1=add_some_numbers.add_variant('b is 3', b=3)
+        assert e1.run().get_result()==4
+
+        # Creata a sub-variant
+        e11 = e1.add_variant('a is 2', a=2)
+        assert e11.run().get_result() == 5
+
+        # Create unnamed variant
+        e2=add_some_numbers.add_variant(b=4)
+        assert e2.run().get_result()==5
+        assert e2.get_name() == 'add_some_numbers.b=4'
+
+        # Create array of variants
+        e_list = [add_some_numbers.add_variant(b=i) for i in xrange(5, 8)]
+        assert [e.get_name() for e in e_list] == ['add_some_numbers.b=5', 'add_some_numbers.b=6', 'add_some_numbers.b=7']
+        assert [e.run().get_result()==j for e, j in zip(e_list, range(6, 11))]
+
+        # Create grid of variants
+        e_grid = [add_some_numbers.add_variant(a=a, b=b) for a, b in itertools.product([2, 3], [4, 5, 6])]
+        assert [e.get_name() for e in e_grid] == ['add_some_numbers.a=2,b=4', 'add_some_numbers.a=2,b=5', 'add_some_numbers.a=2,b=6',
+                                                  'add_some_numbers.a=3,b=4', 'add_some_numbers.a=3,b=5', 'add_some_numbers.a=3,b=6']
+        assert add_some_numbers.get_unnamed_variant(a=2, b=4).run().get_result()==6
+        assert add_some_numbers.get_unnamed_variant(a=3, b=5).run().get_result()==8
+
+        experiments = add_some_numbers.get_all_variants(include_roots=True, include_self=True)
+        assert len(experiments)==13
+
+
+def test_experiment_api(try_browse=False):
+
+    with experiment_testing_context():
+
+        @experiment_function
+        def my_api_test(a=1, b=3):
+            print 'aaa'
+            return a*b
+
+        my_api_test.add_variant('a2b2', a=2, b=2)
+        my_api_test.add_variant('a3b2', a=3, b=2)
+
+        my_api_test.get_variant('a2b2').run()
+        record = my_api_test.get_variant('a2b2').get_latest_record()
+
+        assert record.get_log() == 'aaa\n'
+        assert record.get_result() == 4
+        assert record.get_args() == [('a', 2), ('b', 2)]
+        assert record.get_status() == ExpStatusOptions.FINISHED
+
+    if try_browse:
+        my_api_test.browse()
+
+
+def test_figure_saving(show_them = False):
+
+    with experiment_testing_context():
+        record = experiment_test_function.run()
+
+    plt.close('all')  # Close all figures
+    figs = record.load_figures()
+    assert len(figs)==2
+    if show_them:
+        plt.show()
+
+
+if __name__ == '__main__':
+
+    set_test_mode(True)
+    test_get_latest_identifier()
+    test_get_latest()
+    test_run_and_show()
+    test_experiment_with()
+    test_start_experiment()
+    test_accessing_experiment_dir()
+    test_saving_result()
+    test_variants()
+    test_experiment_api(try_browse=False)
+    test_figure_saving(show_them=False)
---
+++ packages/artemis-ml/artemis-ml-1.5.tar.gz/artemis-ml-1.5/artemis/general/test_mymath.py
@@ -0,0 +1,226 @@
+import pytest
+
+from artemis.general.mymath import softmax, cummean, cumvar, sigm, expected_sigm_of_norm, mode, cummode, normalize, is_parallel, \
+    align_curves, angle_between, fixed_diff, decaying_cumsum, geosum
+import numpy as np
+__author__ = 'peter'
+
+
+def test_softmax():
+    x = np.random.randn(3, 4, 5)
+
+    s = softmax(x, axis=1)
+    assert s.shape==(3, 4, 5) and (s>0).all() and (s<1).all() and np.allclose(np.sum(s, axis=1), 1)
+
+
+def test_cummean():
+
+    arr = np.random.randn(3, 4)
+    cum_arr = cummean(arr, axis = 1)
+    assert np.allclose(cum_arr[:, 0], arr[:, 0])
+    assert np.allclose(cum_arr[:, 1], np.mean(arr[:, :2], axis = 1))
+    assert np.allclose(cum_arr[:, 2], np.mean(arr[:, :3], axis = 1))
+
+
+def test_cumvar():
+
+    arr = np.random.randn(3, 4)
+    cum_arr = cumvar(arr, axis = 1, sample = False)
+    assert np.allclose(cum_arr[:, 0], 0)
+    assert np.allclose(cum_arr[:, 1], np.var(arr[:, :2], axis = 1))
+    assert np.allclose(cum_arr[:, 2], np.var(arr[:, :3], axis = 1))
+
+
+def test_exp_sig_of_norm():
+
+    mean = 1
+    std = 0.8
+    n_points = 1000
+    seed = 1234
+
+    inputs = np.random.RandomState(seed).normal(mean, std, size = n_points)
+    vals = sigm(inputs)
+    sample_mean = np.mean(vals)
+
+    for method in ('maclauren-2', 'maclauren-3', 'probit'):
+        approx_true_mean = expected_sigm_of_norm(mean, std, method = method)
+        approx_sample_mean = expected_sigm_of_norm(np.mean(inputs), np.std(inputs), method = method)
+        true_error = np.abs(approx_true_mean-sample_mean)/sample_mean
+        sample_error = np.abs(approx_sample_mean-sample_mean)/sample_mean
+        print 'Error for %s: %.4f True, %.4f Sample.' % (method, true_error, sample_error)
+        assert true_error < 0.02, 'Method %s did pretty bad' % (method, )
+
+
+def test_mode():
+
+    arr = np.random.RandomState(0).randint(low=0, high=2, size=(3, 5, 7))
+
+    m0 = mode(arr, axis = 0)
+    assert m0.shape == (5, 7)
+    assert np.all(np.sum(m0[None, :, :] == arr, axis = 0) > np.sum(m0[None, :, :] != arr, axis = 0))
+
+    m1 = mode(arr, axis = 1)
+    assert m1.shape == (3, 7)
+    assert np.all(np.sum(m1[:, None, :] == arr, axis = 1) > np.sum(m1[:, None, :] != arr, axis = 1))
+
+    m2 = mode(arr, axis = 2)
+    assert m2.shape == (3, 5)
+
+
+@pytest.mark.skipif(True, reason='Requires scipy weave, which does not install reliably.')
+def test_cummode():
+
+    arr = np.random.RandomState(0).randint(low=0, high=3, size=(5, 7))
+
+    m = cummode(arr, axis = 1)  # (n_samples, n_events)
+    assert m.shape == arr.shape
+
+    uniques = np.unique(arr)
+
+    for j in xrange(arr.shape[1]):
+        n_elements_of_mode_class = np.sum(arr[:, :j+1] == m[:, j][:, None], axis = 1)  # (n_samples, )
+        for k, u in enumerate(uniques):
+            n_elements_of_this_class = np.sum(arr[:, :j+1] == u, axis = 1)  # (n_samples, )
+            assert np.all(n_elements_of_mode_class >= n_elements_of_this_class)
+
+
+@pytest.mark.skipif(True, reason='Requires scipy weave, which does not install reliably.')
+def test_cummode_weighted():
+
+    arr = np.random.RandomState(0).randint(low=0, high=3, size=(5, 7))
+    w = np.random.rand(5, 7)
+
+    m = cummode(arr, weights=w, axis = 1)  # (n_samples, n_events)
+    assert m.shape == arr.shape
+
+    uniques = np.unique(arr)
+
+    for j in xrange(arr.shape[1]):
+        bool_ixs_of_mode_class = arr[:, :j+1] == m[:, j][:, None]  # (n_samples, j+1)
+        weights_of_mode_class = np.sum(w[:, :j+1]*bool_ixs_of_mode_class, axis = 1)  # (n_samples, )
+
+        for k, u in enumerate(uniques):
+            bool_ixs_of_this_class = arr[:, :j+1] == u  # (n_samples, j+1)
+            weights_of_this_class = np.sum(w[:, :j+1]*bool_ixs_of_this_class, axis = 1)  # (n_samples, )
+            assert np.all(weights_of_mode_class >= weights_of_this_class)
+
+
+def test_normalize():
+
+    # L1 - positive values
+    arr = np.random.rand(5, 4)
+    norm_arr = normalize(arr, degree=1, axis = 1)
+    assert np.allclose(norm_arr.sum(axis=1), 1)
+
+    # L1 - with negative values
+    arr = np.random.randn(5, 4)
+    norm_arr = normalize(arr, degree=1, axis = 1)
+    assert np.allclose(np.abs(norm_arr).sum(axis=1), 1)
+
+    # L2
+    arr = np.random.randn(5, 4)
+    norm_arr = normalize(arr, degree=2, axis = 1)
+    assert np.allclose(np.sqrt((norm_arr**2).sum(axis=1)), 1)
+
+    # L1 - zero row with nan handling
+    arr = np.random.rand(5, 4)
+    arr[2, :] = 0
+    norm_arr = normalize(arr, degree=1, axis = 1)
+    assert np.all(np.isnan(norm_arr[2, :]))
+
+    # L1 - zero row with nan handling
+    arr = np.random.rand(5, 4)
+    arr[2, :] = 0
+    norm_arr = normalize(arr, degree=1, axis = 1, avoid_nans=True)
+    assert np.allclose(np.abs(norm_arr).sum(axis=1), 1)
+    assert np.allclose(norm_arr[2, :], 1./arr.shape[1])
+
+    # L2 - zero row with nan handling
+    arr = np.random.rand(5, 4)
+    arr[2, :] = 0
+    norm_arr = normalize(arr, degree=2, axis = 1, avoid_nans=True)
+    assert np.allclose(np.sqrt((norm_arr**2).sum(axis=1)), 1)
+    assert np.allclose(norm_arr[2, :], np.sqrt(1./arr.shape[1]))
+
+
+def test_is_parallel():
+
+    assert is_parallel([1, 2], [2, 4])
+    assert not is_parallel([1, 2], [2, 5])
+    assert is_parallel([1, 2], [2, 5], angular_tolerance=0.5)
+    assert not is_parallel([1, 2], [-2, -4])
+
+
+def test_align_curves():
+
+    n_curves = 30
+
+    n_points = [np.random.randint(20) for _ in xrange(n_curves)]
+
+    xs = [np.sort(np.random.rand(n)) for n in n_points]
+    ys = [np.random.randn(n) for n in n_points]
+
+    new_xs, new_ys = align_curves(xs=xs, ys=ys, n_bins=25, spacing='lin')
+    assert new_xs.shape == (25, )
+    assert new_ys.shape == (n_curves, 25)
+
+    new_xs, new_ys = align_curves(xs=xs, ys=ys, n_bins=25, spacing='log')
+    assert new_xs.shape == (25, )
+    assert new_ys.shape == (n_curves, 25)
+
+
+def test_angle_between():
+
+    a = np.array([[0, 1], [1, 1], [1, 0]])
+    b = np.array([[1, 0], [1, 0], [1, 0]])
+    angles = angle_between(a, b, in_degrees=True, axis=-1)
+    assert np.allclose(angles, [90, 45, 0])
+    assert np.allclose(angle_between([2, 1], [1, 0], in_degrees=True), np.arctan(1/2.)*180/np.pi)
+
+
+def test_fixed_diff():
+
+    a = np.random.randn(2, 3, 4)
+
+    da = fixed_diff(a, axis=1, initial_value=0)
+    assert np.array_equal(da[:, 1:, :], np.diff(a, axis=1))
+    assert np.array_equal(da[:, 0, :], a[:, 0, :])
+
+    da = fixed_diff(a, axis=1, initial_value='first')
+    assert np.array_equal(da[:, 1:, :], np.diff(a, axis=1))
+    assert np.array_equal(da[:, 0, :], np.zeros_like(da[:, 0, :]))
+
+    assert np.allclose(a, np.cumsum(fixed_diff(a, axis=1), axis=1))
+
+
+def test_decaying_cumsum():
+
+    a = np.random.randn(2, 3, 4)
+
+    ca = decaying_cumsum(a, axis=1, memory=0)
+    assert np.array_equal(ca, a)
+
+    ca = decaying_cumsum(a, axis=1, memory=.6)
+    assert np.allclose(ca[:, 2, :], 0.4*(0.6**2*a[:, 0, :] + 0.6**1*a[:, 1, :] + a[:, 2, :]))
+
+
+def test_geosum():
+    assert geosum(0.5, t_end=4, t_start=2) == 0.5**2 + 0.5**3 + 0.5**4 == 0.4375
+    assert geosum(1, t_end=4, t_start=2) == 1**2+1**3+1**4 == 3
+
+
+if __name__ == '__main__':
+    test_decaying_cumsum()
+    test_fixed_diff()
+    test_angle_between()
+    test_align_curves()
+    test_is_parallel()
+    test_normalize()
+    test_cummode_weighted()
+    test_cummode()
+    test_mode()
+    test_exp_sig_of_norm()
+    test_cumvar()
+    test_cummean()
+    test_softmax()
+    test_geosum()
