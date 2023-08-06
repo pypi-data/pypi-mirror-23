54230dfb358ddd6440b1937aa50b1edb03e2e2c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:34:32 +0000

    Add package roboticia-drive/0.0.6/roboticia-drive-0.0.6.tar.gz

---
+++ packages/roboticia-drive/roboticia-drive-0.0.6.tar.gz/roboticia-drive-0.0.6/roboticia_drive/sensor/_nogit_credential.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 28 21:03:24 2017
+
+@author: julien
+"""
+
+GOOGLE_CLOUD_SPEECH_CREDENTIALS = r"""{
+  "type": "service_account",
+  "project_id": "roboticia-speak",
+  "private_key_id": "6b462cec044af33c924fe6f37561045a28d172ea",
+  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQDMvje/lnlyGd1S\nMm/VKsXeeR4ypnmtDGrFQfY124xGA7FhTKsg/dUI/vsjwL2nZCTCy6RHZnABvQdr\ns+ib8y1g/DFtuGkMeIap/XcAJtRZ/AWh3ES9yz/IV6QAYTWzB7IQmMh1aWpOMz4r\nOipCIGLpozXzZGk9uC8PMsZkQDE/yltBZwgAZ2eyEXUG22dFBh1bqDpU6MQdR17V\nKq9l4+IJraEXbfXxGVS+xnonLq/t6Lt8+YJtvS2g/xlFo7pZgKllKW0J+WmQ9Iag\n11INr6gumAMb3Q2lXWDikwr29XPDrqX73RWkMKuZbhAIPCRGSTNfeMovs7fTV1lT\nzCRsg6hZAgMBAAECggEAAxQrL3DaU6pq5xUGFH0Hq7BDkXxqYP1KT7wNsUKjadwd\nWzq1ahIXFYMMEL4RvAYn/VvllQNH1ObcSTTT3yB5grdYIO/AeKUJVV4P34s170J4\nbTYsPHRleaRDvZO4pIANn+1EOmvC4vknSqwdvY/SHPRM1+OfJ4PxBTf8vr4uX2ir\nJ/l5g/DIpDl9pxQjRFcZQCTfJf3LpprYHcO/robCbZx36sYpUxzT0aUwROKM6uSg\n6oFrKktCexdq03LyAfxQ0RQYvi6wpr+8ob6f0vEx5W997ybqwbE58HBFf+WjU3xx\nmpi61d1VyI9EkDzohdaGL+NxZEscDj7870mk8kCdVwKBgQD/92fPwxLh17Wvl+vC\nTMF+HHAKI2uQvXxAm96trWTZF1jerEQhzdbEGleVV7x6840liCMxle1X+PhaCS1k\n6gMeUEqwa2sq/7EoZU8ARJFYFRxEp7Le4UtpyIB7mcJGCPWSHU59Wn7BaebLS17J\ncpmqVvKc2D4NnL9QyIQ0/u2VCwKBgQDMxRej71KNpM/rNhoGxxPDKV4JyI7T75NX\nNt067YD+uCuUPz4Ql5iQx3/uRdEStBQzFJJXq+5JKUlmCoQ+Ew8xnpGh67lHalPD\nrR0l3ieTmuBOt3ukq+qTkYKqVgVFmH5Y07CkV62XFnKVyI+/Kaf+3DM9IEJFM5hg\nRghcjo+OqwKBgBB9YcSyrn66JMxBH0EPLM/xCQ6+JWnXqy+GWxwZNYv+wwzlUCZV\nWmabw9StKElLc2ZdlNbW78lhEq7KNJBxuu2zAnzHDspgWotdq286+5oPEhkiLJXL\nMc6Iyi4sQ6W7YgaduLa+o7fWGLY0DtEvftLIcyzDiYva80BDCrFXUvrtAoGBALVF\nRMg69DmvzN/L5P5a/YOm+zht8wJq82QhjXa9CZR0NR/nXGoTyVthQD8OTybfg4I0\nk9hIu4jIo9FRPgA1nx2t4f4VTs+JsN1oQoVjRGXGlQsaIrqD9gTsq19fBwu1EyRP\nQfA6LdGybX1rLtVrauySwkk6ZXAdXKbdWEmXaj5BAoGBALJ6LT817fG8L1RcJkxy\nRDUvQl90cvgEMaYbkHR+BaI5YaEwyyK2Cad6hCvslHY7FZ1ouFC+Qg3o7rTokO7H\n2GPvwiAPmAx2vfJsWlrzNd+N7G2v6wGntED6AbFiM3CmxJtJkLGM9WefzeJRNiht\nGK6ENbtLMIgpSlxLnRNJf87z\n-----END PRIVATE KEY-----\n",
+  "client_email": "roboticia@roboticia-speak.iam.gserviceaccount.com",
+  "client_id": "112996321986966721778",
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://accounts.google.com/o/oauth2/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/roboticia%40roboticia-speak.iam.gserviceaccount.com"
+}"""
