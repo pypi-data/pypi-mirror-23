b110b243748f58168bf7dfef8e408d528a46b7a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:22 +0000

    Add package dmsclient/1.0.1/dmsclient-1.0.1.tar.gz

---
+++ packages/dmsclient/dmsclient-1.0.1.tar.gz/dmsclient-1.0.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+
+pkg = __import__('dmsclient')
+
+setup(name='dmsclient',
+      version=pkg.__version__,
+      description=pkg.__description__,
+      long_description=open('README.rst').read(),
+      author=pkg.__author__,
+      author_email=pkg.__author_email__,
+      packages=['dmsclient', 'dmsclient.core'],
+      install_requires=[
+          'docopt>=0.6.0',
+          'requests>=2.18.0',
+          'tabulate>=0.7.0',
+      ],
+      entry_points={
+          'console_scripts': ['dms=dmsclient.cli:main']
+      },
+      license='MIT')
