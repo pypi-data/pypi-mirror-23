729e2fc272c762cb3f806ae6e8b9f97d63e6c53e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:34:31 +0000

    Add package papertrail-cli/1.0.14/papertrail-cli-1.0.14.tar.gz

---
+++ packages/papertrail-cli/papertrail-cli-1.0.14.tar.gz/papertrail-cli-1.0.14/setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+import os
+from setuptools import find_packages, setup
+
+requirements=[
+    'Click',
+    'requests',
+    'progressbar2',
+    'termcolor',
+    'colorama',
+    'watchdog'
+]
+
+if os.name == 'posix':
+    requirements.append('sh')
+
+setup(
+    name='papertrail-cli',
+    version='1.0.' + os.environ['CIRCLE_BUILD_NUM'],
+    install_requires=requirements,
+    author='Egis Software',
+    url='https://github.com/egis/papertrail-python-cli',
+    description='Papertrail Command Line Utils',
+    packages=find_packages(),
+    entry_points={
+        "console_scripts": [
+            "pt = pt.pt:main",
+        ]
+    }
+)
