ec4db48e001761ced6d516ab7c6301cc296a5c05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:08:47 +0000

    Add package DeepGraph/0.1.0/DeepGraph-0.1.0.tar.gz

---
+++ packages/DeepGraph/DeepGraph-0.1.0.tar.gz/DeepGraph-0.1.0/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="DeepGraph",
+    version='0.1.0',
+    packages=find_packages(),
+    author="Dominik Traxl",
+    author_email="dominik.traxl@posteo.org",
+    url='https://github.com/deepgraph/deepgraph/',
+    download_url='https://github.com/deepgraph/deepgraph/tarball/v0.1.0',
+    description=("DeepGraph is a scalable, general-purpose data analysis "
+                 "package. It implements a network representation based on "
+                 "pandas DataFrames and provides methods to construct, "
+                 "partition and plot graphs, to interface with popular "
+                 "network packages and more."),
+    install_requires=['numpy>=1.6',
+                      'pandas>=0.17.0'],
+    license="BSD",
+    classifiers=[
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Scientific/Engineering :: Information Analysis',
+        'Topic :: Scientific/Engineering :: Mathematics',
+        'Topic :: Scientific/Engineering :: Physics'],
+    package_data={'deepgraph': ['../tests/*.py', '../LICENSE.txt']},
+)
