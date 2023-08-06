3a48512f324a81869304faa10777035378efb2be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:13:13 +0000

    Add package BayesFlare/1.0.4/bayesflare-1.0.4.tar.gz

---
+++ packages/BayesFlare/bayesflare-1.0.4.tar.gz/bayesflare-1.0.4/bayesflare/stats/__init__.py
@@ -0,0 +1,12 @@
+"""
+BayesFlare Statistics
+==================
+
+Functions and classes for carrying out Bayesian analysis on the 
+lightcurves.
+
+"""
+
+#from .thresholding import Thresholder
+#from general import *
+from .bayes import Bayes, ParameterEstimationGrid
