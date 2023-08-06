c08817d2426cd648c42f8d8112243ad43fb9ae79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:14:50 +0000

    Add package showprogress/0.0.21/showprogress-0.0.21.tar.gz

---
+++ packages/showprogress/showprogress-0.0.21.tar.gz/showprogress-0.0.21/showprogress/progress.py
@@ -0,0 +1,35 @@
+from .memory import get_available_memory
+from .memory import get_process_memory
+
+import time
+import sys
+import datetime
+
+def progress(iterable, head=None, item_length=0, memory_usage=True, memory_available=False, check_points=1):
+    begin_time = time.time()
+    n = len(iterable)
+    
+    for i, item in enumerate(iterable):
+        yield item
+        
+        if check_points > 1 and (i % check_points) != 0:
+            continue
+            
+        n_block = int(20 * i / n)
+        n_bar = 20 - n_block
+        process_time = time.time() - begin_time
+        remained_time = (n - i) * process_time / (i+1) 
+        
+        args = (head+': ' if head else '', '█'*n_block, '-'*n_bar, 100*i/n, '%', datetime.timedelta(seconds=int(process_time)), datetime.timedelta(seconds=int(remained_time)))
+        message = '%s|%s%s| %.3f %s, [%s < %s]  ' % args
+        
+        if item_length > 0:
+            message += (str(item)[:item_length] + '  ')
+        if memory_usage:
+            message += ('%.3f Gb used  ' % get_process_memory())
+        if memory_available:
+            message += ('%.3f Gb available  ' % get_available_memory())
+        sys.stdout.write('\r%s' % message.strip())
+        
+    print('\r%sdone' % (head + ' ' if head else ''))
+        
