58aaad87f622b6622374810d5b3822059641848b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:35 +0000

    Add package scikit-credit/0.0.5/scikit-credit-0.0.5.tar.gz

---
+++ packages/scikit-credit/scikit-credit-0.0.5.tar.gz/scikit-credit-0.0.5/scikit_credit/test/test_encoder.py
@@ -0,0 +1,20 @@
+# -*- coding:utf-8 -*-
+
+import encoder
+from sklearn.model_selection import cross_val_score
+from sklearn.metrics import roc_curve, auc, roc_auc_score, accuracy_score
+
+__author__ = 'jiyue'
+
+import pandas as pd
+
+df = pd.read_excel(
+    '/home/jiyue/git/myproject/deeplearning/seven_online/risk_control/授信增信/待分箱数据.xls',
+    sep='\t')
+
+y = df['label']
+x = df.drop(['label'], axis=1)
+
+woeEncoder = encoder.WoeEncoder()
+woeEncoder.fit_transform(x.values, y.values)
+print woeEncoder._features_iv
