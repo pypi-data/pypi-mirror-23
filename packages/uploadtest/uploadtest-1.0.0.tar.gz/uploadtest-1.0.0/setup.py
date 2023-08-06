966ad8dca23c562d225a57492a519b089f0bd05c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:30:18 +0000

    Add package uploadtest/1.0.0/uploadtest-1.0.0.tar.gz

---
+++ packages/uploadtest/uploadtest-1.0.0.tar.gz/uploadtest-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+        name='uploadtest',
+        version='1.0.0',
+        py_modules=['uploadtest'],
+        author='jhdaihust',
+        author_email='jhdaihust@hust.edu.cn',
+        url='http://www.headfirstlabs.com',
+        description='A simple printer of lists',
+    )
