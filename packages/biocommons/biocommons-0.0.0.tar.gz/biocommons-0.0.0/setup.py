f2db7af033897b76ff9d91bb98488e2648ea8707
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:36:30 +0000

    Add package biocommons/0.0.0/biocommons-0.0.0.tar.gz

---
+++ packages/biocommons/biocommons-0.0.0.tar.gz/biocommons-0.0.0/setup.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+setup(
+    author="Reece Hart <reecehart@gmail.com>",
+    description="ensures that the biocommons namespace is correctly declared",
+    name="biocommons",
+    packages=find_packages(),
+    url="https://github.com/biocommons/biocommons-namespace-declaration",
+    version='0.0.0',
+    zip_safe=True,
+
+    install_requires=[
+        ]
+)
