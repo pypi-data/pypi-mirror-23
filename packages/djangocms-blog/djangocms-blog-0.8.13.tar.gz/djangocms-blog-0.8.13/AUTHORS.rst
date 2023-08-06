2e873cec7965a591da314532a77c545d0e7d0d54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:14 +0000

    Add package djangocms-blog/0.8.13/djangocms-blog-0.8.13.tar.gz

---
+++ packages/djangocms-blog/djangocms-blog-0.8.13.tar.gz/djangocms-blog-0.8.13/AUTHORS.rst
@@ -0,0 +1,36 @@
+=======
+Credits
+=======
+
+Development Lead
+----------------
+
+* Iacopo Spalletti <i.spalletti@nephila.it>
+
+Contributors
+------------
+
+* alex
+* Carlo Ascani
+* Christoph Reimers
+* cluster-master
+* danra
+* Davide Truffo
+* frnhr
+* furiousdave
+* Georgiy Kutsurua
+* German Ilyin
+* Jens Diemer
+* Kamil Gałuszka
+* Leonardo Cavallucci
+* Marco Federighi
+* Max Vyaznikov
+* MotleyTech
+* Nico Schottelius
+* Olivier Tabone
+* Petr Klus
+* Sami Kalliomäki
+* Stefan Wehrmeyer
+* Sylvain Fankhauser
+* Tadas Dailyda
+* Zan Anderle
---
+++ packages/djangocms-blog/djangocms-blog-0.8.13.tar.gz/djangocms-blog-0.8.13/setup.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import djangocms_blog
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+version = djangocms_blog.__version__
+
+
+readme = open('README.rst').read()
+history = open('HISTORY.rst').read().replace('.. :changelog:', '')
+
+
+setup(
+    name='djangocms-blog',
+    version=version,
+    description='A djangoCMS 3 blog application',
+    long_description=readme + '\n\n' + history,
+    author='Iacopo Spalletti',
+    author_email='i.spalletti@nephila.it',
+    url='https://github.com/nephila/djangocms-blog',
+    packages=[
+        'djangocms_blog',
+    ],
+    include_package_data=True,
+    install_requires=[
+        'django-parler>=1.5',
+        'django-cms>3.0.11',
+        'django-taggit>=0.12.2',
+        'django-filer>=1.1',
+        'pytz',
+        'django-taggit-templatetags',
+        'django-taggit-autosuggest',
+        'djangocms-text-ckeditor',
+        'cmsplugin-filer>=1.0',
+        'django-meta>=1.2',
+        'django-meta-mixin>=0.3',
+        'aldryn-apphooks-config>=0.2.6',
+        'djangocms-apphook-setup',
+        'aldryn-search'
+    ],
+    license='BSD',
+    zip_safe=False,
+    keywords='djangocms-blog, blog, django, wordpress, multilingual',
+    test_suite='cms_helper.run',
+    extras_require={
+        'admin-enhancer': ['django-admin-enhancer'],
+    },
+
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Framework :: Django',
+        'Framework :: Django :: 1.6',
+        'Framework :: Django :: 1.7',
+        'Framework :: Django :: 1.8',
+        'Framework :: Django :: 1.9',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+)
