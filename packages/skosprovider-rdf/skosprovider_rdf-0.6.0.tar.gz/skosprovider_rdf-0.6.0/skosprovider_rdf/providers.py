36602a62c229084e6c8a53f1c3415ffc97f507e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:18:15 +0000

    Add package skosprovider-rdf/0.6.0/skosprovider_rdf-0.6.0.tar.gz

---
+++ packages/skosprovider-rdf/skosprovider_rdf-0.6.0.tar.gz/skosprovider_rdf-0.6.0/skosprovider_rdf/providers.py
@@ -0,0 +1,241 @@
+# -*- coding: utf-8 -*-
+
+'''
+This module contains an RDFProvider, an implementation of the 
+:class:`skosprovider.providers.VocabularyProvider` interface that uses a 
+:class:`rdflib.graph.Graph` as input.
+'''
+
+import logging
+import rdflib
+from rdflib.term import Literal, URIRef
+from skosprovider_rdf.utils import text_, _df_writexml
+
+log = logging.getLogger(__name__)
+logging.basicConfig(level=logging.DEBUG)
+
+from skosprovider.providers import MemoryProvider
+from skosprovider.uri import (
+    DefaultConceptSchemeUrnGenerator
+)
+from skosprovider.skos import (
+    Concept,
+    Collection,
+    ConceptScheme,
+    Label,
+    Note,
+    Source
+)
+
+from rdflib.namespace import RDF, SKOS, DC, DCTERMS
+SKOS_THES = rdflib.Namespace('http://purl.org/iso25964/skos-thes#')
+
+from language_tags import tags
+
+from xml.dom.minidom import DocumentFragment
+DocumentFragment.writexml = _df_writexml
+
+
+class RDFProvider(MemoryProvider):
+    '''
+    A simple vocabulary provider that use an :class:`rdflib.graph.Graph`
+    as input. The provider expects a RDF graph with elements that represent
+    the SKOS concepts and collections.
+
+    Please be aware that this provider needs to load the entire graph in memory.
+    '''
+
+    def __init__(self, metadata, graph, **kwargs):
+        self.graph = graph
+        if not 'concept_scheme' in kwargs:
+            kwargs['concept_scheme'] = self._cs_from_graph(metadata)
+        super(RDFProvider, self).__init__(metadata, [], **kwargs)
+        self.list = self._from_graph()
+
+    def _cs_from_graph(self, metadata):
+        cslist = []
+        for sub in self.graph.subjects(RDF.type, SKOS.ConceptScheme):
+            uri = self.to_text(sub)
+            cs = ConceptScheme(
+                uri=uri,
+                labels = self._create_from_subject_typelist(sub, Label.valid_types),
+                notes = self._create_from_subject_typelist(sub, Note.valid_types),
+                sources = self._create_sources(sub),
+                languages = self._create_languages(sub)
+            )
+            cslist.append(cs)
+        if len(cslist) == 0:
+            return ConceptScheme(
+                uri=DefaultConceptSchemeUrnGenerator().generate(
+                    id=metadata.get('id')
+                )
+            )
+        elif len(cslist) == 1:
+            return cslist[0]
+        else:
+            raise RuntimeError(
+                'This RDF file contains more than one ConceptScheme.'
+            )
+
+    def _from_graph(self):
+        clist = []
+        for sub, pred, obj in self.graph.triples((None, RDF.type, SKOS.Concept)):
+            uri = self.to_text(sub)
+            matches = {}
+            for k in Concept.matchtypes:
+                matches[k] = self._create_from_subject_predicate(sub, URIRef(SKOS + k +'Match'))
+            con = Concept(
+                id = self._get_id_for_subject(sub, uri), 
+                uri=uri,
+                concept_scheme = self.concept_scheme,
+                labels = self._create_from_subject_typelist(sub, Label.valid_types),
+                notes = self._create_from_subject_typelist(sub, Note.valid_types),
+                sources = self._create_sources(sub),
+                broader = self._create_from_subject_predicate(sub, SKOS.broader),
+                narrower = self._create_from_subject_predicate(sub, SKOS.narrower),
+                related = self._create_from_subject_predicate(sub, SKOS.related),
+                member_of = [],
+                subordinate_arrays = self._create_from_subject_predicate(sub, SKOS_THES.subordinateArray),
+                matches = matches
+            )
+            clist.append(con)
+
+        for sub, pred, obj in self.graph.triples((None, RDF.type, SKOS.Collection)):
+            uri = self.to_text(sub)
+            col = Collection(
+                id=self._get_id_for_subject(sub, uri), 
+                uri=uri,
+                concept_scheme = self.concept_scheme,
+                labels = self._create_from_subject_typelist(sub, Label.valid_types),
+                notes = self._create_from_subject_typelist(sub, (Note.valid_types)),
+                sources = self._create_sources(sub),
+                members = self._create_from_subject_predicate(sub, SKOS.member),
+                member_of = [],
+                superordinates = self._create_from_subject_predicate(sub, SKOS_THES.superOrdinate)
+            )
+            clist.append(col)
+        self._fill_member_of(clist)
+        return clist
+
+    def _fill_member_of(self, clist):
+        collections = list(set([c for c in clist if isinstance(c, Collection)]))
+        for col in collections:
+            for c in clist:
+                if c.id in col.members:
+                    c.member_of.append(col.id)
+        return
+
+    def _create_from_subject_typelist(self,subject,typelist):
+        list = []
+        for p in typelist:
+            term=SKOS.term(p)
+            list.extend(self._create_from_subject_predicate(subject,term))
+        return list
+
+    def _get_id_for_subject(self, subject, uri):
+        for stmt in self.graph:
+            print(stmt)
+        if (subject, DCTERMS.identifier, None) in self.graph:
+            return self.to_text(self.graph.value(subject=subject, predicate=DCTERMS.identifier, any=False))
+        elif (subject, DC.identifier, None) in self.graph:
+            return self.to_text(self.graph.value(subject=subject, predicate=DC.identifier, any=False))
+        else:
+            return uri
+
+    def _create_from_subject_predicate(self, subject, predicate):
+        list = []
+        for s, p, o in self.graph.triples((subject, predicate, None)):
+            type = predicate.split('#')[-1]
+            if Label.is_valid_type(type):
+                o = self._create_label(o, type)
+            elif Note.is_valid_type(type):
+                o = self._create_note(o, type)
+            else:
+                o = self._get_id_for_subject(o, self.to_text(o))
+            list.append(o)
+        return list
+
+    def _create_label(self, literal, type):
+        if not Label.is_valid_type(type):
+            raise ValueError(
+                'Type of Label is not valid.'
+            )
+        return Label(self.to_text(literal), type, self._get_language_from_literal(literal))
+
+    def _read_markupped_literal(self, literal):
+        if literal.datatype is None:
+            return (literal, self._get_language_from_literal(literal), None)
+        elif literal.datatype == RDF.HTML:
+            df = literal.value.cloneNode(True)
+            if df.firstChild and df.firstChild.attributes and 'xml:lang' in df.firstChild.attributes.keys():
+                lang = self._scrub_language(df.firstChild.attributes.get('xml:lang').value)
+                del df.firstChild.attributes['xml:lang']
+            else:
+                lang = 'und'
+            return(df.toxml(), lang, 'HTML')
+        else:
+            raise ValueError(
+                'Unable to process literal of type %s.' % literal.datatype
+            )
+
+    def _create_note(self, literal, type):
+        if not Note.is_valid_type(type):
+            raise ValueError(
+                'Type of Note is not valid.'
+            )
+        l = self._read_markupped_literal(literal)
+        return Note(self.to_text(l[0]), type, l[1], l[2])
+
+    def _create_sources(self, subject):
+        '''
+        Create the sources for this subject.
+
+        :param subject: Subject to get the sources for.
+        :returns: A :class:`list` of :class:`skosprovider.skos.Source` objects.
+        '''
+        ret = []
+        for s, p, o in self.graph.triples((subject, DCTERMS.source, None)):
+            for si, pi, oi in self.graph.triples((o, DCTERMS.bibliographicCitation, None)):
+                ret.append(
+                    Source(
+                        self.to_text(oi),
+                        'HTML' if oi.datatype == RDF.HTML else None
+                )
+                )
+        return ret
+
+    def _create_languages(self, subject):
+        '''
+        Create the languages for this subject.
+
+        :param subject: Subject to get the sources for.
+        :returns: A :class:`list` of IANA language tags.
+        '''
+        ret = set()
+        for s, p, o in self.graph.triples((subject, DCTERMS.language, None)):
+            ret.add(self.to_text(self._scrub_language(o)))
+        for s, p, o in self.graph.triples((subject, DC.language, None)):
+            ret.add(self.to_text(self._scrub_language(o)))
+        return ret
+
+    def _scrub_language(self, language):
+        if tags.check(language):
+            return language
+        else:
+            log.warn('Encountered an invalid language %s. Falling back to "und".' % language)
+            return 'und'
+
+    def _get_language_from_literal(self, data):
+        if not isinstance(data, Literal):
+            return None
+        if data.language is None:
+            return None
+        return self.to_text(self._scrub_language(data.language))
+
+    def to_text(self, data):
+        """
+        data of binary type or literal type that needs to be converted to text.
+        :param data
+        :return: text representation of the data
+        """
+        return text_(data.encode('utf-8'), 'utf-8')
---
+++ packages/skosprovider-rdf/skosprovider_rdf-0.6.0.tar.gz/skosprovider_rdf-0.6.0/skosprovider_rdf/utils.py
@@ -0,0 +1,308 @@
+# -*- coding: utf-8 -*-
+'''
+This module contains utility functions for dealing with skos providers.
+'''
+from __future__ import unicode_literals
+import logging
+import warnings
+import sys
+
+log = logging.getLogger(__name__)
+
+from rdflib import Graph, Literal, Namespace
+from rdflib.term import URIRef, BNode
+from rdflib.namespace import RDF, SKOS, DCTERMS, VOID
+SKOS_THES = Namespace('http://purl.org/iso25964/skos-thes#')
+from skosprovider.skos import (
+    Concept,
+    Collection
+)
+
+from xml.dom.minidom import Node, Element
+import html5lib
+
+PY3 = sys.version_info[0] == 3
+
+if PY3:  # pragma: no cover
+    binary_type = bytes
+else:  # pragma: no cover
+    binary_type = str
+
+
+def rdf_dumper(provider):
+    '''
+    Dump a provider to a format that can be passed to a
+    :class:`skosprovider.providers.RDFProvider`.
+
+    :param skosprovider.providers.VocabularyProvider provider: The provider
+        that wil be turned into an :class:`rdflib.graph.Graph`.
+
+    :rtype: :class:`rdflib.graph.Graph`
+    '''
+    return _rdf_dumper(provider, None)
+
+
+def rdf_c_dumper(provider, c):
+    '''
+    Dump one concept or collection from a provider to a format that can be passed to a
+    :class:`skosprovider.providers.RDFProvider`.
+
+    :param skosprovider.providers.VocabularyProvider provider: The provider
+        that wil be turned into an :class:`rdflib.graph.Graph`.
+
+    :param String c: identifier
+
+    :rtype: :class:`rdflib.graph.Graph`
+    '''
+    return _rdf_dumper(provider, [c])
+
+
+def _rdf_dumper(provider, id_list=None):
+    '''
+    Dump a provider to a format that can be passed to a
+    :class:`skosprovider.providers.RDFProvider`.
+
+    :param skosprovider.providers.VocabularyProvider provider: The provider
+        that wil be turned into an :class:`rdflib.graph.Graph`.
+
+    :param List id_list: List of id's of the data to dump.
+
+    :rtype: :class:`rdflib.graph.Graph`
+    '''
+    graph = Graph()
+    graph.namespace_manager.bind("skos", SKOS)
+    graph.namespace_manager.bind("dcterms", DCTERMS)
+    graph.namespace_manager.bind("skos-thes", SKOS_THES)
+    graph.namespace_manager.bind("void", VOID)
+    conceptscheme = URIRef(provider.concept_scheme.uri)
+    _add_in_dataset(graph, conceptscheme, provider)
+    graph.add((conceptscheme, RDF.type, SKOS.ConceptScheme))
+    graph.add((conceptscheme, DCTERMS.identifier, Literal(provider.metadata['id'])))
+    _add_labels(graph, provider.concept_scheme, conceptscheme)
+    _add_notes(graph, provider.concept_scheme, conceptscheme)
+    _add_sources(graph, provider.concept_scheme, conceptscheme)
+    _add_languages(graph, provider.concept_scheme, conceptscheme)
+    # Add triples using store's add method.
+    if not id_list:
+        id_list = [x['id'] for x in provider.get_all()]
+        for c in provider.get_top_concepts():
+            graph.add((conceptscheme, SKOS.hasTopConcept, URIRef(c['uri'])))
+    for id in id_list:
+        c = provider.get_by_id(id)
+        subject = URIRef(c.uri)
+        _add_in_dataset(graph, subject, provider)
+        graph.add((subject, DCTERMS.identifier, Literal(c.id)))
+        graph.add((subject, SKOS.inScheme, conceptscheme))
+        _add_labels(graph, c, subject)
+        _add_notes(graph, c, subject)
+        _add_sources(graph, c, subject)
+        if isinstance(c, Concept):
+            graph.add((subject, RDF.type, SKOS.Concept))
+            for b in c.broader:
+                broader = provider.get_by_id(b)
+                if broader:
+                    graph.add((subject, SKOS.broader, URIRef(broader.uri)))
+                else:
+                    warnings.warn(_warning(b), UserWarning)
+            for n in c.narrower:
+                narrower = provider.get_by_id(n)
+                if narrower:
+                    graph.add((subject, SKOS.narrower, URIRef(narrower.uri)))
+                else:
+                    warnings.warn(_warning(n), UserWarning)
+            for r in c.related:
+                related = provider.get_by_id(r)
+                if related:
+                    graph.add((subject, SKOS.related, URIRef(related.uri)))
+                else:
+                    warnings.warn(_warning(r), UserWarning)
+            for s in c.subordinate_arrays:
+                subordinate_array = provider.get_by_id(s)
+                if subordinate_array:
+                    graph.add((subject, SKOS_THES.subordinateArray, URIRef(subordinate_array.uri)))
+                else:
+                    warnings.warn(_warning(s), UserWarning)
+            for k in c.matches.keys():
+                for uri in c.matches[k]:
+                    graph.add((subject, URIRef(SKOS + k +'Match'), URIRef(uri)))
+        elif isinstance(c, Collection):
+            graph.add((subject, RDF.type, SKOS.Collection))
+            for m in c.members:
+                member = provider.get_by_id(m)
+                if member:
+                    graph.add((subject, SKOS.member, URIRef(member.uri)))
+                else:
+                    warnings.warn(_warning(m), UserWarning)
+            for s in c.superordinates:
+                superordinate = provider.get_by_id(s)
+                if superordinate:
+                    graph.add((subject, SKOS_THES.superOrdinate, URIRef(superordinate.uri)))
+                else:
+                    warnings.warn(_warning(s), UserWarning)
+
+    return graph
+
+
+def rdf_conceptscheme_dumper(provider):
+    '''
+    Dump all information of the conceptscheme of a provider to a format that can be passed to a
+    :class:`skosprovider.providers.RDFProvider`.
+
+    :param skosprovider.providers.VocabularyProvider provider: The provider
+        that wil be turned into an :class:`rdflib.graph.Graph`.
+
+    :rtype: :class:`rdflib.graph.Graph`
+    '''
+    graph = Graph()
+    graph.namespace_manager.bind("skos", SKOS)
+    graph.namespace_manager.bind("dcterms", DCTERMS)
+    graph.namespace_manager.bind("skos-thes", SKOS_THES)
+    graph.namespace_manager.bind("void", VOID)
+    conceptscheme = URIRef(provider.concept_scheme.uri)
+    _add_in_dataset(graph, conceptscheme, provider)
+    graph.add((conceptscheme, RDF.type, SKOS.ConceptScheme))
+    graph.add((conceptscheme, DCTERMS.identifier, Literal(provider.metadata['id'])))
+    _add_labels(graph, provider.concept_scheme, conceptscheme)
+    _add_notes(graph, provider.concept_scheme, conceptscheme)
+    _add_sources(graph, provider.concept_scheme, conceptscheme)
+    _add_languages(graph, provider.concept_scheme, conceptscheme)
+    for c in provider.get_top_concepts():
+        graph.add((conceptscheme, SKOS.hasTopConcept, URIRef(c['uri'])))
+
+    return graph
+
+
+def _warning(id):
+    return 'id %s could not be resolved' % id
+
+
+def _add_in_dataset(graph, subject, provider):
+    '''
+    Checks if the provider says something about a dataset and if so adds 
+    void.inDataset statements.
+
+    :param rdflib.graph.Graph graph: The graph to add statements to.
+    :param rdflib.term.URIRef subject: The subject to add an inDataset statement to.
+    :param skosprovider.providers.VocabularyProvider provider:
+    '''
+
+    duri = provider.get_metadata().get('dataset', {}).get('uri', None)
+    if duri:
+        graph.add((subject, VOID.inDataset, URIRef(duri)))
+
+
+def _add_labels(graph, c, subject):
+    for l in c.labels:
+        predicate = URIRef(SKOS + l.type)
+        lang = extract_language(l.language)
+        graph.add((subject, predicate, Literal(l.label, lang=lang)))
+
+
+def _add_notes(graph, c, subject):
+    for n in c.notes:
+        predicate = URIRef(SKOS + n.type)
+        lang = extract_language(n.language)
+        if n.markup is None:
+            graph.add((subject, predicate, Literal(n.note, lang=lang)))
+        else:
+            html = _add_lang_to_html(n.note, lang)
+            graph.add((subject, predicate, Literal(html, datatype=RDF.HTML)))
+
+def _add_lang_to_html(htmltext, lang):
+    '''
+    Take a piece of HTML and add an xml:lang attribute to it.
+    '''
+    if lang == 'und':
+        return htmltext
+    parser = html5lib.HTMLParser(
+        tree=html5lib.treebuilders.getTreeBuilder("dom")
+    )
+    html = parser.parseFragment(htmltext)
+    html.normalize()
+    if len(html.childNodes) == 0:
+        return '<div xml:lang="%s"></div>' % lang
+    elif len(html.childNodes) == 1:
+        node = html.firstChild
+        if node.nodeType == Node.TEXT_NODE:
+            div = Element('div')
+            div.ownerDocument = html
+            div.setAttribute('xml:lang', lang)
+            div.childNodes = [node]
+            html.childNodes = [div]
+        else:
+            node.setAttribute('xml:lang', lang)
+    else:
+        #add a single encompassing div
+        div = Element('div')
+        div.ownerDocument = html
+        div.setAttribute('xml:lang', lang)
+        div.childNodes = html.childNodes
+        html.childNodes = [div]
+    return html.toxml()
+
+def _add_sources(graph, c, subject):
+    '''
+    Add sources to the RDF graph.
+
+    :param rdflib.graph.Graph graph: An RDF Graph.
+    :param c: A :class:`skosprovider.skos.ConceptScheme`, 
+        :class:`skosprovider.skos.Concept` or :class:`skosprovider.skos.Collection`
+    :param subject: The RDF subject to add the sources to.
+    '''
+    for s in c.sources:
+        source = BNode()
+        graph.add((source, RDF.type, DCTERMS.BibliographicResource))
+        if s.markup is None:
+            graph.add((source, DCTERMS.bibliographicCitation, Literal(s.citation)))
+        else:
+            graph.add((source, DCTERMS.bibliographicCitation, Literal(s.citation, datatype=RDF.HTML)))
+        graph.add((subject, DCTERMS.source, source))
+
+def _add_languages(graph, c, subject):
+    '''
+    Add languages to the RDF graph.
+
+    :param rdflib.graph.Graph graph: An RDF Graph.
+    :param c: A :class:`skosprovider.skos.ConceptScheme`.
+    :param subject: The RDF subject to add the sources to.
+    '''
+    for l in c.languages:
+        lang = extract_language(l)
+        graph.add((subject, DCTERMS.language, Literal(l)))
+
+def extract_language(lang):
+    '''
+    Turn a language in our domain model into a IANA tag.
+    '''
+    if lang is None:
+        lang = 'und'  # return undefined code when no language
+    else:
+        lang = text_(lang, encoding="UTF-8")
+    return lang
+
+
+def text_(s, encoding='latin-1', errors='strict'):
+    """ If ``s`` is an instance of ``binary_type``, return
+    ``s.decode(encoding, errors)``, otherwise return ``s``"""
+    if isinstance(s, binary_type):
+        return s.decode(encoding, errors)
+    return s
+
+def _df_writexml(self, writer, indent="", addindent="", newl=""):
+    '''
+    Monkeypatch method for unexisting `writexml` in
+    :class:`xml.dom.minidom.DocumentFragment`.
+    '''
+    # indent = current indentation
+    # addindent = indentation to add to higher levels
+    # newl = newline string
+    if self.childNodes:
+        if (len(self.childNodes) == 1 and
+            self.childNodes[0].nodeType == Node.TEXT_NODE):
+            self.childNodes[0].writexml(writer, '', '', '')
+        else:
+            for node in self.childNodes:
+                node.writexml(writer, indent+addindent, addindent, newl)
+
+from xml.dom.minidom import DocumentFragment
+DocumentFragment.writexml = _df_writexml
