b337efa6f4caf3db5baec2d5f834cad4b843b424
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:10:11 +0000

    Add package dsspc-common/0.1.0/dsspc_common-0.1.0.tar.gz

---
+++ packages/dsspc-common/dsspc_common-0.1.0.tar.gz/dsspc_common-0.1.0/dsspc_common/metrics/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+
+class Metrics(object):
+
+    def write_point(self, measurement, fields, tags, time):
+        pass
+
+    def query_point(self, db, query):
+        pass
---
+++ packages/dsspc-common/dsspc_common-0.1.0.tar.gz/dsspc_common-0.1.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='dsspc_common',
+    version='0.1.0',
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+    license='MIT',
+    author='VicYu',
+    author_email='i@vicyu.com',
+    description='A common tool for DSSPC',
+    url='https://github.com/Vic020/dsspc_common'
+)
