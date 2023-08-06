ac891dc3a5dec7667f51efcafd061bf0e207ee76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:34 +0000

    Add package gino/0.1.0/gino-0.1.0.tar.gz

---
+++ packages/gino/gino-0.1.0.tar.gz/gino-0.1.0/gino/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for GINO."""
+
+__author__ = """Fantix King"""
+__email__ = 'fantix.king@gmail.com'
+__version__ = '0.1.0'
+
+from .declarative import Gino
