79edad911e420da912fe675460c3a0594f017882
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:32:42 +0000

    Add package django-excel/0.0.8/django_excel-0.0.8-py2.py3-none-any.whl

---
+++ packages/django-excel/django_excel-0.0.8-py2.py3-none-any.whl/polls/urls.py
@@ -0,0 +1,26 @@
+from django.conf.urls import url
+from polls import views
+
+
+urlpatterns = [
+    url(r'^$', views.upload, name='uplink'),
+    url(r'^download/(.*)', views.download, name="download"),
+    url(r'^download_attachment/(.*)/(.*)', views.download_as_attachment,
+        name="download_attachment"),
+    url(r'^exchange/(.*)', views.exchange, name="exchange"),
+    url(r'^parse/(.*)', views.parse, name="parse"),
+    url(r'^import/', views.import_data, name="import"),
+    url(r'^import_sheet/', views.import_sheet, name="import_sheet"),
+    url(r'^export/(.*)', views.export_data, name="export"),
+    url(r'^handson_view/', views.handson_table, name="handson_view"),
+
+    # handson table view
+    url(r'^embedded_handson_view/',
+        views.embed_handson_table, name="embed_handson_view"),
+    url(r'^embedded_handson_view_single/',
+        views.embed_handson_table_from_a_single_table,
+        name="embed_handson_view"),
+    # survey_result
+    url('^survey_result/',
+        views.survey_result, name='survey_result'),
+]
