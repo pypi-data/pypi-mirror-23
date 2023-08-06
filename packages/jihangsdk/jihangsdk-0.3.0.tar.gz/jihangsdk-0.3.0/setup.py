8768900b31cbddaf20f117f7b4d395ec6a63844c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:07:59 +0000

    Add package jihangsdk/0.3.0/jihangsdk-0.3.0.tar.gz

---
+++ packages/jihangsdk/jihangsdk-0.3.0.tar.gz/jihangsdk-0.3.0/setup.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
+#############################################
+# File Name: setup.py
+# Author: bangguo
+# Mail: willyxq@gmail.com
+# Created Time:  2017-07-05 10:26:34 PM
+#############################################
+
+from setuptools import setup, find_packages
+
+setup(
+    name = "jihangsdk",
+    version = "0.3.0",
+    keywords = ("pip", "datacanvas", "jihang", "bangguo"),
+    description = "jihang sdk",
+    long_description = "jihang sdk for python",
+    license = "MIT Licence",
+                            
+    url = "http://xiaoh.me",
+    author = "bangguo",
+    author_email = "willyxq@gmail.com",
+
+    packages = find_packages(),
+    include_package_data = True,                           
+    platforms = "any",
+    install_requires = [],
+
+    scripts = [],
+    entry_points = {
+        'console_scripts': [
+            'jihangsdk = jihangsdk.help:main'
+            ]
+        }
+    )
+
