24f56ef126185a90a45e70b2b1a05b9faa4f4a0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:17:37 +0000

    Add package astro-gala/0.2/astro-gala-0.2.tar.gz

---
+++ packages/astro-gala/astro-gala-0.2.tar.gz/astro-gala-0.2/gala/coordinates/__init__.py
@@ -0,0 +1,7 @@
+from .sgr import *
+from .orphan import *
+from .gd1 import *
+from .propermotion import *
+from .velocity_frame_transforms import *
+from .poincarepolar import *
+from .quaternion import *
