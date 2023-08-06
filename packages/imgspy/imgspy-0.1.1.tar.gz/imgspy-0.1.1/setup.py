e4685d7a4b3eae006d5bad804ca1a1da2bf95943
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:57:09 +0000

    Add package imgspy/0.1.1/imgspy-0.1.1.tar.gz

---
+++ packages/imgspy/imgspy-0.1.1.tar.gz/imgspy-0.1.1/setup.py
@@ -0,0 +1,20 @@
+# coding: utf-8
+from setuptools import setup
+
+
+with open('readme.rst') as f:
+    readme = f.read()
+
+
+setup(
+    name='imgspy',
+    version='0.1.1',
+    description='Find the size or type of the image without '
+                'fetching the whole content.',
+    long_description=readme,
+    url='https://github.com/nkanaev/imgspy',
+    license='MIT',
+    author='Nazar Kanaev',
+    author_email='nkanaev@live.com',
+    py_modules=['imgspy'],
+    test_suite='tests')
