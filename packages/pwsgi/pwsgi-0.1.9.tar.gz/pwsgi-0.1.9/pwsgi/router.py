c81ad58cf1f4f5e0a72a57cc9895ddb40c42bcfd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:08:36 +0000

    Add package pwsgi/0.1.9/pwsgi-0.1.9.tar.gz

---
+++ packages/pwsgi/pwsgi-0.1.9.tar.gz/pwsgi-0.1.9/pwsgi/router.py
@@ -0,0 +1,32 @@
+from pulsar.apps.wsgi import Router
+
+
+__all__ = ['router']
+
+
+def router(app, rule, method):
+    '''Map a function to :class:`Router` and add to the :attr:`routes` list.
+    Typical usage:
+
+    app = WsgiApplication('/')
+
+    @app.router('/hello', method='post')
+    def world(request):
+        return wsgi.WsgiResponse(200, 'world')
+    '''
+    def handler(fn):
+        app.add_child(app.make_router(rule, method, fn))
+        return fn
+    return handler
+
+
+class BluePrint(Router):
+    '''
+    To patch pulsar.wsgi, if it dosent have `router` decorator
+    '''
+
+    def __init__(self, *args, **kwargs) -> None:
+        if not hasattr(Router, 'router'):
+            self.router = router
+            # the classmethod fn is actually `partial(self, fn)`
+        super().__init__(*args, **kwargs)
