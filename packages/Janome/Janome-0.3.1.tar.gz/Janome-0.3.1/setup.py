37fa6717fce26959948c8554ddc6b61b64afffe6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:57:02 +0000

    Add package Janome/0.3.1/Janome-0.3.1.tar.gz

---
+++ packages/Janome/Janome-0.3.1.tar.gz/Janome-0.3.1/setup.py
@@ -0,0 +1,51 @@
+from setuptools import setup
+import sys
+
+sys.path.append('./janome')
+sys.path.append('./tests')
+
+import os
+from zipfile import ZipFile
+from janome.dic import *
+
+dicdir = 'ipadic'
+
+if not os.path.exists('sysdic') and os.path.exists(os.path.join('ipadic', 'sysdic.zip')):
+    print('Unzip dictionary data...')
+    with ZipFile(os.path.join(dicdir, 'sysdic.zip')) as zf:
+        zf.extractall()
+
+version = '0.3.1'
+name = 'janome'
+
+with open(os.path.join(os.path.abspath(os.path.dirname(__file__)), 'README.rst')) as f:
+    long_description = f.read()
+
+classifiers = [
+    "Development Status :: 3 - Alpha",
+    "License :: OSI Approved :: Apache Software License",
+    "Natural Language :: Japanese",
+    "Programming Language :: Python :: 2.7",
+    "Programming Language :: Python :: 3.3",
+    "Programming Language :: Python :: 3.4",
+    "Programming Language :: Python :: 3.5",
+    "Programming Language :: Python :: 3.6"
+]
+
+setup(
+    name='Janome',
+    version=version,
+    description='Japanese morphological analysis engine.',
+    long_description=long_description,
+    author='Tomoko Uchida',
+    author_email='tomoko.uchida.1111@gmail.com',
+    license='AL2',
+    classifiers=classifiers,
+    url='http://mocobeta.github.io/janome/',
+    packages=['janome','sysdic'],
+    package_data={'sysdic': ['fst.data']},
+    py_modules=['janome.dic','janome.fst','janome.lattice','janome.tokenizer'],
+    scripts=['bin/janome'],
+    test_suite = 'suite'
+)
+
