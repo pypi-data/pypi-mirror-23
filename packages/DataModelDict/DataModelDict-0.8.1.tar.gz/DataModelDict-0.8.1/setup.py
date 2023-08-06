abf2a64a373697ccba8bc9b7a27ad3f333ca36ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:48:59 +0000

    Add package DataModelDict/0.8.1/DataModelDict-0.8.1.tar.gz

---
+++ packages/DataModelDict/DataModelDict-0.8.1.tar.gz/DataModelDict-0.8.1/setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+import DataModelDict
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+    
+setup(name = 'DataModelDict',
+      version =      DataModelDict.__version__,
+      description =  DataModelDict.__doc__,
+      author =       DataModelDict.__author__,
+      author_email = DataModelDict.__email__,
+ #     url = 'https://github.com/usnistgov/DataModelDict/',
+      long_description=readme(),
+      classifiers = [
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Science/Research',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Scientific/Engineering :: Physics'
+      ],
+      keywords = 'json xml dictionary ', 
+      py_modules = ['DataModelDict'],
+      install_requires=[
+        'xmltodict'
+      ],
+      include_package_data = True,
+      zip_safe = False)
