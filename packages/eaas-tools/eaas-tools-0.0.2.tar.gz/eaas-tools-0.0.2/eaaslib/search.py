4ee4310594ebe71536aa9d38a28185a0758439fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:58 +0000

    Add package eaas-tools/0.0.2/eaas-tools-0.0.2.tar.gz

---
+++ packages/eaas-tools/eaas-tools-0.0.2.tar.gz/eaas-tools-0.0.2/eaaslib/search.py
@@ -0,0 +1,69 @@
+import etcd
+import common
+
+
+def do_list(server=common.ETCD):
+    """
+    list eaas machines
+    :param server:
+    """
+    client = etcd.Client(host=server)
+    print "EAAS Nodes:"
+    print "=" * 50
+    nodes = client.read("/eaas/nodes", recursive=False)
+    for child in nodes.children:
+        address = child.key.rsplit("/", 1)[-1]
+        # caps
+        caps = client.read("{}/caps".format(child.key)).value
+        print " {} caps={}".format(address, caps.split(","))
+        containers = client.read("{}/containers".format(child.key)).children
+        for container in containers:
+            containername = container.key.rsplit("/", 1)[-1]
+            containerinfo = client.read("{}/image".format(container.key))
+            print "   {} image={}".format(containername, containerinfo.value)
+
+
+def get_nodes(server=common.ETCD, filtercaps=None):
+    """
+    List the EAAS nodes we can see
+    :param server:
+    :param filtercaps: filter to only include nodes with these caps
+    :return:
+    """
+    nodelist = {}
+    client = etcd.Client(host=server)
+    nodes = client.read("/eaas/nodes", recursive=False)
+    for child in nodes.children:
+        address = child.key.rsplit("/", 1)[-1]
+
+        try:
+            caps = client.read("{}/caps".format(child.key)).value
+            caps = caps.decode("utf-8", 'ignore')
+            caps = caps.strip().split()
+
+            if filtercaps:
+                skip = False
+                for cap in filtercaps:
+                    if cap not in caps:
+                        skip = True
+                if skip:
+                    continue
+
+            nodelist[address] = {
+                "caps": caps
+            }
+            nodelist[address]["containers"] = []
+            containers = client.read("{}/containers".format(child.key)).children
+            for container in containers:
+                containername = container.key.rsplit("/", 1)[-1]
+                containerinfo = client.read("{}/image".format(container.key))
+                nodelist[address]["containers"].append(
+                    {
+                        "name": containername,
+                        "image": containerinfo.value
+                    }
+                )
+        except etcd.EtcdKeyNotFound:
+            pass
+        
+    return nodelist
---
+++ packages/eaas-tools/eaas-tools-0.0.2.tar.gz/eaas-tools-0.0.2/setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+
+from setuptools import setup
+
+setup(
+    name="eaas-tools",
+    version="0.0.2",
+    description="Client Tools for EAAS",
+    author="Ian Norton",
+    author_email="inorton@gmail.com",
+    url="https://github.com/inorton/eaas",
+    packages=["eaaslib"],
+    scripts=["eaas", "eaas-list"],
+    platforms=["any"],
+    license="License :: OSI Approved :: MIT License",
+    install_requires=["python-etcd"],
+    long_description="Find and use EAAS instances")
+
