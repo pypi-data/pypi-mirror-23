dcd9a9f0c37510c49613ab6775aad82ea235ddf6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:02 +0000

    Add package justpith/0.0.15/justpith-0.0.15.tar.gz

---
+++ packages/justpith/justpith-0.0.15.tar.gz/justpith-0.0.15/justpith/mongo/mongo.py
@@ -0,0 +1,50 @@
+from pymongo import MongoClient
+from time import time
+
+class Mongo:
+
+    ##### GENERAL #####
+    def __init__(self, host, port, db):
+        self.host       = host
+        self.port       = port
+        self.db         = db
+
+        self.client     = None
+        self.connection = None
+
+    def connect(self):
+        self.client     = MongoClient(self.host, self.port)
+        self.connection = self.client[self.db]
+
+
+    ##### JUSTPITH SERVER #####
+    def insert_news(self, news):
+        selected_collection = self.connection['News']
+        result = selected_collection.insert(news)
+
+    def update_news_report(self, id_news, queue_name, queue_op):
+        selected_collection = self.connection['News']
+        result = selected_collection.update_one({'_id': id_news}, {'$push': {'report': {'queue_name': queue_name, 'queue_op': queue_op, 'timestamp': str(time())}}}, upsert=True)
+
+    def update_news_after_parsing(self, id_news, ttl, article, article_cleaned, queue_name):
+        selected_collection = self.connection['News']
+        result = selected_collection.update_one({'_id': id_news}, {'$set': {'TTL': ttl, 'article': article, 'article_cleaned': article_cleaned}, '$push': {'report': {'queue_name': queue_name, 'queue_op': 'IN', 'timestamp': str(time())}}}, upsert=True)
+
+    def update_news_after_similarity(self, id_news, doc_sim, topic_sim):
+        selected_connection = self.connection['News']
+        result = selected_connection.update_one({'_id': id_news}, {'$set': {'doc_sims': doc_sim, 'topic_sims': topic_sim}})
+
+
+    ##### JUSTPITH WEB SERVICE TEXT ANALYSIS #####
+    def get_docs_to_build_model(self, category):
+        selected_collection = self.connection['News']
+        result = selected_collection.find({'category_title': category})
+        # docs = []
+        # for doc in result:
+        #     docs.append(doc['article'])
+        # return docs
+        return result
+
+    def update_news_id_for_model(self, id_news, id_news_corpus):
+        selected_connection = self.connection['News']
+        result = selected_connection.update_one({'_id': id_news}, {'$set': {'id_corpus': id_news_corpus}})
