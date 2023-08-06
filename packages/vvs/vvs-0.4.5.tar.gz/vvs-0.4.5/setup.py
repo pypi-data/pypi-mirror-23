5b8e924a1d76bcd7852063f0b18d677e477de66e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:11:30 +0000

    Add package vvs/0.4.5/vvs-0.4.5.tar.gz

---
+++ packages/vvs/vvs-0.4.5.tar.gz/vvs-0.4.5/setup.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+"""
+from setuptools import setup, find_packages
+
+
+ENTRY_POINTS = """
+    [console_scripts]
+    vvs=vvs.cli:main
+"""
+
+
+def setup_package():
+    setup(name='vvs',
+          description='Displays upcoming departure times from stations on the Stuttgart public transport network',
+          author='Aengus Walton',
+          author_email='ventolin@gmail.com',
+          url='https://github.com/kopf/vvs',
+          license='Apache',
+          packages=find_packages(exclude=['tests', 'test']),
+          classifiers=[
+              'Development Status :: 4 - Beta',
+              'License :: OSI Approved :: Apache Software License',
+              'Programming Language :: Python',
+              'Programming Language :: Python :: 2.7',
+              'Programming Language :: Python :: 3.4',
+              'Programming Language :: Python :: 3.5'
+          ],
+          zip_safe=False,
+          include_package_data=True,
+          setup_requires=['setuptools_scm'],
+          install_requires=['beautifulsoup4', 'click', 'requests', 'pytz'],
+          use_scm_version=True,
+          entry_points=ENTRY_POINTS)
+
+
+if __name__ == "__main__":
+    setup_package()
