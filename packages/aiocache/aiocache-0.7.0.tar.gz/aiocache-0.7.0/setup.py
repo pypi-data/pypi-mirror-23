7716ca0f4d52726fd9b5304a5341bb0e30a59514
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:56:56 +0000

    Add package aiocache/0.7.0/aiocache-0.7.0.tar.gz

---
+++ packages/aiocache/aiocache-0.7.0.tar.gz/aiocache-0.7.0/setup.py
@@ -0,0 +1,50 @@
+import re
+import os
+
+from setuptools import setup, find_packages
+from distutils.util import strtobool
+
+with open(
+        os.path.join(
+            os.path.abspath(os.path.dirname(__file__)),
+            'aiocache/_version.py')) as fp:
+    try:
+        version = re.findall(
+            r"^__version__ = '([^']+)'\r?$", fp.read(), re.M)[0]
+    except IndexError:
+        raise RuntimeError('Unable to determine version.')
+
+
+aioredis = "aioredis>=0.3.3,<1"
+aiomcache = "aiomcache>=0.5.2"
+
+install_requires = set((aioredis, aiomcache))
+
+setup_kwargs = {
+    'name': "aiocache",
+    'version': version,
+    'author': "Manuel Miranda",
+    'url': "https://github.com/argaen/aiocache",
+    'author_email': "manu.mirandad@gmail.com",
+    'description': "multi backend asyncio cache",
+    'classifiers': [
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Framework :: AsyncIO",
+    ],
+    'packages': find_packages(),
+}
+
+
+if not strtobool(os.environ.get("AIOCACHE_REDIS", "yes")):
+    print("Installing without aioredis")
+    install_requires.remove(aioredis)
+
+if not strtobool(os.environ.get("AIOCACHE_MEMCACHED", "yes")):
+    print("Installing without aiomcache")
+    install_requires.remove(aiomcache)
+
+
+setup_kwargs['install_requires'] = install_requires
+setup(**setup_kwargs)
