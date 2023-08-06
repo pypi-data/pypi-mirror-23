d436750a63a48d7063dae74a1f37aa0c07c96118
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:46:58 +0000

    Add package rapydo-utils/0.4.8/rapydo-utils-0.4.8.tar.gz

---
+++ packages/rapydo-utils/rapydo-utils-0.4.8.tar.gz/rapydo-utils-0.4.8/setup.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+
+from utilities import __version__
+from utilities import DEFAULT_FILENAME
+from setuptools import setup
+
+setup(
+    name='rapydo-utils',
+    description='A set of python utilities used across all RAPyDo projects',
+    version=__version__,
+    author="Paolo D'Onorio De Meo",
+    author_email='p.donorio.de.meo@gmail.com',
+    url='https://github.com/rapydo/utils',
+    license='MIT',
+    packages=[
+        # NOTE to self: it was not a good idea to share the package prefix
+        # 'utilities'
+        'utilities'
+    ],
+    package_data={
+        # 'utilities':
+        'utilities':
+        [
+            'logging.ini',
+            '%s.yaml' % DEFAULT_FILENAME
+        ]
+    },
+    python_requires='>=3.4',
+    install_requires=[
+        # NOTE: install_requires specify what a project
+        # minimally needs to run correctly
+        "beeprint",
+        "PyYAML",
+        "pytz",
+    ],
+    classifiers=[
+        'Programming Language :: Python',
+        'Intended Audience :: Developers',
+    ],
+    keywords=['utilities', 'rapydo', 'logs', 'tools']
+)
