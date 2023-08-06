0ca4d8084ac2959b0ccd3655c7abf0e8205f9d31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:10:05 +0000

    Add package dfgsheet/0.1/dfgsheet-0.1.tar.gz

---
+++ packages/dfgsheet/dfgsheet-0.1.tar.gz/dfgsheet-0.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'dfgsheet',
+  packages = ['dfgsheet'], # this must be the same as the name above
+  version = '0.1',
+  description = 'google sheet to dataframe',
+  author = 'Jialin Liu',
+  author_email = 'valiantljk@gmail.com',
+  url = 'https://github.com/valiantljk/dfgsheet.git', # use the URL to the github repo
+  download_url = 'https://github.com/valiantljk/dfgsheet/archive/0.1.tar.gz', # I'll explain this in a second
+  keywords = ['dataframe', 'python', 'sheet'], # arbitrary keywords
+  classifiers = [],
+)
