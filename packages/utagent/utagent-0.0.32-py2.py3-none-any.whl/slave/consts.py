7c6ef615a508ab10febc0a4c1961033bc1eafad8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:07 +0000

    Add package utagent/0.0.32/utagent-0.0.32-py2.py3-none-any.whl

---
+++ packages/utagent/utagent-0.0.32-py2.py3-none-any.whl/slave/consts.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import enum
+
+
+class TaskType(enum.IntEnum):
+    unknown = 0
+    macaca_test = 1
+    ui_recorder = 2
+    macaca_server = 3
+    init_uirecorder = 4
+    reports = 5
