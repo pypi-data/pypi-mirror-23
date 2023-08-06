b1b5fc159d12b39c124349a2b4e9410df5a1e5ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:08:23 +0000

    Add package lwe-pyloader/0.2.3/lwe-pyloader-0.2.3.tar.gz

---
+++ packages/lwe-pyloader/lwe-pyloader-0.2.3.tar.gz/lwe-pyloader-0.2.3/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+
+setup(
+    name='lwe-pyloader',
+    version='0.2.3',
+    description='A simple, easy to use, multi-threaded downloader with queuing support.',
+    long_description='For an overview and some examples, head over to `Github <https://github.com/linuxwhatelse/pyloader>`_',
+    url='https://github.com/linuxwhatelse/pyloader',
+    author='linuxwhatelse',
+    author_email='info@linuxwhatelse.com',
+    license='GPLv3',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+    keywords='pyloader download multi-threaded queue',
+    py_modules=[
+        'pyloader'
+    ],
+    install_requires=[
+        'requests'
+    ]
+)
