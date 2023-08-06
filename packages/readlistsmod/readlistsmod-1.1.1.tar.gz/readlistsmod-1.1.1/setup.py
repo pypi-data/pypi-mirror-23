71e27ed53a1b27ff06f852bbd673bd5c33c3f7dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:16:00 +0000

    Add package readlistsmod/1.1.1/readlistsmod-1.1.1.tar.gz

---
+++ packages/readlistsmod/readlistsmod-1.1.1.tar.gz/readlistsmod-1.1.1/setup.py
@@ -0,0 +1,12 @@
+# -*- coding:utf-8 -*-
+#从python发布工具中导入setup函数
+from distutils.core import setup
+setup(
+    name =  'readlistsmod',
+    version = '1.1.1',
+    py_modules = ['readlistsmod'],
+    author = 'Hisea',
+    author_email = 'hiseamail@foxmail.com',
+    url = '',
+    description= '对列表进行简单的打印，可根据用户需求，对内嵌列表进行缩进操作',
+	)
