a240df34d6edfce591a4d07dfabfd89876d211fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:08:31 +0000

    Add package commonware/0.5.0/commonware-0.5.0.tar.gz

---
+++ packages/commonware/commonware-0.5.0.tar.gz/commonware-0.5.0/commonware/response/middleware.py
@@ -0,0 +1,75 @@
+from django.conf import settings
+
+
+class FrameOptionsHeader(object):
+    """
+    Set an X-Frame-Options header. Default to DENY. Set
+    response['X-Frame-Options'] = 'SAMEORIGIN'
+    to override.
+    """
+
+    def process_response(self, request, response):
+        if hasattr(response, 'no_frame_options'):
+            return response
+
+        if 'X-Frame-Options' not in response:
+            response['X-Frame-Options'] = 'DENY'
+
+        return response
+
+
+class RobotsTagHeader(object):
+    """Set an X-Robots-Tag header.
+
+    Default to noodp to avoid using directories for page titles. Set
+    a value of response['X-Robots-Tag'] or use the relevant decorators
+    to override.
+
+    Change the default in settings by setting X_ROBOTS_DEFAULT = ''.
+    """
+
+    def process_response(self, request, response):
+        if getattr(response, 'no_robots_tag', False):
+            return response
+
+        if 'X-Robots-Tag' not in response:
+            default = getattr(settings, 'X_ROBOTS_DEFAULT', 'noodp')
+            response['X-Robots-Tag'] = default
+
+        return response
+
+
+class StrictTransportMiddleware(object):
+    """
+    DEPRECATED: https://docs.djangoproject.com/en/1.8/ref/middleware/#security-middleware
+    """
+
+    def __init__(self):
+        raise DeprecationWarning("https://docs.djangoproject.com/en/1.8"
+                                 "/ref/middleware/#security-middleware")
+
+
+class XSSProtectionHeader(object):
+    """
+    Set the X-XSS-Protection header on responses. Defaults to
+    '1; mode=block'. Set response['X-XSS-Protection'] = '0' (disable)
+    or '1' (rewrite mode) to override.
+    """
+
+    def process_response(self, request, response):
+        if 'X-XSS-Protection' not in response:
+            response['X-XSS-Protection'] = '1; mode=block'
+        return response
+
+
+class ContentTypeOptionsHeader(object):
+    """
+    Set the X-Content-Type-Options header on responses. Defaults
+    to 'nosniff'. Set response['X-Content-Type-Options'] = ''
+    to override.
+    """
+
+    def process_response(self, request, response):
+        if 'X-Content-Type-Options' not in response:
+            response['X-Content-Type-Options'] = 'nosniff'
+        return response
---
+++ packages/commonware/commonware-0.5.0.tar.gz/commonware-0.5.0/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+from commonware import __version__
+
+setup(
+    name='commonware',
+    version=__version__,
+    description='A place to keep stuff we want to share, like middleware.',
+    long_description=open('README.rst').read(),
+    author='James Socol',
+    author_email='james@mozilla.com',
+    url='http://github.com/mozilla/commonware',
+    license='BSD',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Environment :: Web Environment :: Mozilla',
+        'Intended Audience :: Developers',
+        'Framework :: Django',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
+)
