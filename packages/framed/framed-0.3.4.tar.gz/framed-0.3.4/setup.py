e8bc543724848c0c9cbdabe34af1d88ce6261c0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:40 +0000

    Add package framed/0.3.4/framed-0.3.4.tar.gz

---
+++ packages/framed/framed-0.3.4.tar.gz/framed-0.3.4/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'framed',
+    version = '0.3.4',
+    package_dir = {'':'src'},
+    packages = find_packages('src'),
+    install_requires = ["numpy",
+                        "scipy",
+                        "sympy",
+                        "python-libsbml",
+                        "matplotlib"],
+    scripts = ["scripts/framed-smetana-pipeline"],
+    author = 'Daniel Machado',
+    author_email = 'cdanielmachado@gmail.com',
+    description = 'framed - metabolic modeling for python',
+    license = 'Apache License Version 2.0',
+    keywords = 'metabolism modeling',
+    url = 'https://github.com/cdanielmachado/framed',
+    long_description = open('README.rst').read(),
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Topic :: Utilities',
+        'Programming Language :: Python :: 2.7',
+        'License :: OSI Approved :: Apache Software License',
+    ],
+)
