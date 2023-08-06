c4dfd0b329116d1f50e33185060ffc7691dd3f8f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:08:14 +0000

    Add package snipsskillscore/0.1.0/snipsskillscore-0.1.0.tar.gz

---
+++ packages/snipsskillscore/snipsskillscore-0.1.0.tar.gz/snipsskillscore-0.1.0/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='snipsskillscore',
+    version='0.1.0',
+    description='The Snips Skills core utilities for greating end-to-end assistants',
+    author='Michael Fester',
+    author_email='michael.fester@gmail.com',
+    url='https://github.com/snipsco/snips-skills-core',
+    download_url='',
+    license='MIT',
+    install_requires=['enum'],
+    test_suite="tests",
+    keywords=['snips'],
+    packages=[
+        'snipsskillscore'
+    ]
+)
