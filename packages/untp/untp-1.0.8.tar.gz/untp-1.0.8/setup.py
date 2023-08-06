bf0984d51987c927755041f1ad25cee3da6ae621
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:47:52 +0000

    Add package untp/1.0.8/untp-1.0.8.tar.gz

---
+++ packages/untp/untp-1.0.8.tar.gz/untp-1.0.8/setup.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name = 'untp',
+    version = '1.0.8',
+    keywords = ('untp', 'texturepacker'),
+    description = 'A command line tool to split TexturePacker publish file.',
+    license = 'MIT License',
+    install_requires = [
+        'Pillow',
+        'parse'
+    ],
+    url = 'https://github.com/justbilt/untp',
+    author = 'justbilt',
+    author_email = 'wangbilt@gmail.com',
+    packages = find_packages("src"),
+    package_dir = {'':'src'},
+    entry_points = {
+        'console_scripts': [
+            'untp = untp.untp:main',
+        ],
+    }
+)
