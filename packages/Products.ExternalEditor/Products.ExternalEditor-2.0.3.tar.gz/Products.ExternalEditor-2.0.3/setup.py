0b07c5fa1fe3c2d95bac22a1c9ed517d72f32c3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:13:07 +0000

    Add package Products.ExternalEditor/2.0.3/Products.ExternalEditor-2.0.3.tar.gz

---
+++ packages/Products.ExternalEditor/Products.ExternalEditor-2.0.3.tar.gz/Products.ExternalEditor-2.0.3/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+setup(name='Products.ExternalEditor',
+      version='2.0.3',
+      description="Zope External Editor",
+      long_description=(open("README.rst").read() + "\n" +
+                        open("CHANGES.rst").read()),
+      classifiers=[
+          'Framework :: Zope2',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2.6',
+          'Programming Language :: Python :: 2.7',
+      ],
+      author="Zope Foundation and Contributors",
+      author_email="zope-dev@zope.org",
+      maintainer="Chris McDonough",
+      maintainer_email="chrism@plope.com",
+      license="ZPL 2.1",
+      keywords="zope external editor",
+      url="https://pypi.python.org/pypi/Products.ExternalEditor",
+      packages=find_packages(),
+      namespace_packages=['Products'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+      ],
+      entry_points="""
+      [console_scripts]
+          zopeedit=Products.ExternalEditor.zopeedit:main
+      """,
+      )
