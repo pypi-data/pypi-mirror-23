ae55d445bddefa6d5e71e3c0a82ffd90a1bb29e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:47 +0000

    Add package django-chronograph-ispm-new/0.3.4/django-chronograph-ispm-new-0.3.4.tar.gz

---
+++ packages/django-chronograph-ispm-new/django-chronograph-ispm-new-0.3.4.tar.gz/django-chronograph-ispm-new-0.3.4/setup.py
@@ -0,0 +1,46 @@
+"""A setuptools based setup module.
+See:
+https://packaging.python.org/en/latest/distributing.html
+https://github.com/pypa/sampleproject
+"""
+
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='django-chronograph-ispm-new',
+    version='0.3.4',
+    description='Django Chronograph ISPM customized by mportela',
+    long_description=long_description,
+    author='ISPM',
+    author_email='dev@ispm.com',
+    license='BSD',
+    keywords='cron django-chronograph',
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+    url='https://github.com/ISPM/ispm-django-chronograph',
+    classifiers = [
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Django',
+    ],
+    zip_safe = False,
+    extras_require = {
+        'Django': ['Django>=1.5'],
+        'DateUtil': ['python-dateutil<=1.5']
+    },
+    dependency_links = ['http://labix.org/download/python-dateutil/python-dateutil-1.5.tar.gz'],
+    package_data = {
+        '': ['docs/*.txt', 'docs/*.py'],
+        'chronograph': ['templates/*.*', 'templates/*/*.*', 'templates/*/*/*.*', 'fixtures/*'],
+    },
+    scripts = ['bin/chronograph'],
+)
