f6661647e574e4fc54881e06174184efeebeeca5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:32:26 +0000

    Add package nester_liusia/1.3.0/nester_liusia-1.3.0.tar.gz

---
+++ packages/nester_liusia/nester_liusia-1.3.0.tar.gz/nester_liusia-1.3.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+    name = 'nester_liusia',
+    version = '1.3.0',
+    py_modules = ['nester_liusia'],
+    author = 'hello',
+    author_email = 'hello@headfirstlabs.com',
+    url = 'http://www.headfirstlabs.com',
+    description = 'A simple printer of nested lists'
+    )
