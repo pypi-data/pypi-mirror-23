857f2b572a3974a3fc0d916f92425db936ffc916
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:58:33 +0000

    Add package miniflow/0.2.0/miniflow-0.2.0.tar.gz

---
+++ packages/miniflow/miniflow-0.2.0.tar.gz/miniflow-0.2.0/miniflow/miniflow.py
@@ -0,0 +1,32 @@
+# Copyright 2017 The Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import graph
+import session
+import ops
+
+# Alias MiniFlow APIs for TensorFlow APIs, import miniflow.miniflow as tf
+int32 = int
+float32 = float
+float64 = float
+Graph = graph.Graph
+Session = session.Session
+Variable = ops.VariableOp
+placeholder = ops.PlaceholderOp
+constant = ops.ConstantOp
+add = ops.AddOp
+minus = ops.MinusOp
+multiple = ops.MultipleOp
+divide = ops.DivideOp
+square = ops.SquareOp
