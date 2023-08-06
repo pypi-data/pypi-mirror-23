1e002aed84bb009b9c22e5752bd95f6e5202c3b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:16 +0000

    Add package ai-libs/1.0.0.1/ai-libs-1.0.0.1.tar.gz

---
+++ packages/ai-libs/ai-libs-1.0.0.1.tar.gz/ai-libs-1.0.0.1/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+	name = 'ai-libs',
+	version = '1.0.0.1',
+	packages = ['ai.autoencoder'],
+	py_modules = ['ai/autoencoder/agn_autoencoder','ai/autoencoder/test'],
+	author = 'geekbruce',
+	author_email = 'bruce.shaoheng@gmail.com',
+	url = 'http://tikiapp.im',
+	description = 'the simple test',
+)
