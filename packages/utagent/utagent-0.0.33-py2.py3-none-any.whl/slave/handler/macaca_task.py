fee9d1eca4bc370907dc21fa7215038ada5f90c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:08 +0000

    Add package utagent/0.0.33/utagent-0.0.33-py2.py3-none-any.whl

---
+++ packages/utagent/utagent-0.0.33-py2.py3-none-any.whl/slave/handler/macaca_task.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import subprocess
+import os
+import webbrowser
+import pexpect
+
+from slave.util.file import File_Downloader
+from slave.config import config
+
+
+def start_macaca_server():
+    subprocess.Popen("macaca server --port 4444 --verbose", shell=True)
+
+
+def run_macaca_test(file_name, case_url):
+    File_Downloader.download(config["macaca_dir"] + '/eleme', file_name, case_url)
+    os.chdir(config["macaca_dir"])
+    subprocess.call('source run.sh eleme/%s' % file_name, shell=True)
+
+
+def ui_recorder():
+    os.chdir(config["macaca_dir"])
+
+    subprocess.Popen('uirecorder --mobile', shell=True)
+
+
+def init_uirecorder():
+    os.chdir(config["macaca_dir"])
+
+    child = pexpect.spawn('uirecorder init --mobile')
+    child.expect('host')
+    child.sendline("127.0.0.1")
+    child.expect('port')
+    child.sendline("4444")
+    # subprocess.Popen('uirecorder init --mobile', shell=True)
+
+
+def reports():
+    webbrowser.open_new(config["macaca_dir"]+"/reports/index.html")
