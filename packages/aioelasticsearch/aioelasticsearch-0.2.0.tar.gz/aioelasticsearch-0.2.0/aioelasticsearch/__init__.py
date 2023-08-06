84c80775d535adaddf309c01053eda79e62393c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:17:28 +0000

    Add package aioelasticsearch/0.2.0/aioelasticsearch-0.2.0.tar.gz

---
+++ packages/aioelasticsearch/aioelasticsearch-0.2.0.tar.gz/aioelasticsearch-0.2.0/aioelasticsearch/__init__.py
@@ -0,0 +1,46 @@
+import asyncio
+
+from elasticsearch import Elasticsearch as _Elasticsearch  # noqa # isort:skip
+
+from elasticsearch.connection_pool import (ConnectionSelector, # noqa # isort:skip
+                                           RoundRobinSelector)
+from elasticsearch.serializer import JSONSerializer  # noqa # isort:skip
+
+from .compat import PY_350  # noqa # isort:skip
+from .exceptions import *  # noqa # isort:skip
+from .pool import AIOHttpConnectionPool  # noqa # isort:skip
+from .transport import AIOHttpTransport  # noqa # isort:skip
+
+
+__version__ = '0.2.0'
+
+
+class Elasticsearch(_Elasticsearch):
+
+    def __init__(
+        self,
+        hosts=None,
+        transport_class=AIOHttpTransport,
+        *,
+        loop=None,
+        **kwargs
+    ):
+        if loop is None:
+            loop = asyncio.get_event_loop()
+
+        self.loop = loop
+
+        kwargs['loop'] = self.loop
+
+        super().__init__(hosts, transport_class=transport_class, **kwargs)
+
+    def close(self):
+        return self.transport.close()
+
+    if PY_350:
+        @asyncio.coroutine
+        def __aenter__(self):  # noqa
+            return self
+
+        def __aexit__(self, *exc_info):  # noqa
+            return self.close()
