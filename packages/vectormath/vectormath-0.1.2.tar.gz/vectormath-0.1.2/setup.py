af55b68f4a507b53f197f3ae4a1263dba0abbeac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:35 +0000

    Add package vectormath/0.1.2/vectormath-0.1.2.tar.gz

---
+++ packages/vectormath/vectormath-0.1.2.tar.gz/vectormath-0.1.2/setup.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+"""
+    Vector math utilities for Python
+"""
+
+from distutils.core import setup
+from setuptools import find_packages
+
+CLASSIFIERS = [
+    'Development Status :: 4 - Beta',
+    'Programming Language :: Python',
+    'Topic :: Scientific/Engineering',
+    'Topic :: Scientific/Engineering :: Mathematics',
+    'Topic :: Scientific/Engineering :: Physics',
+    'Operating System :: Microsoft :: Windows',
+    'Operating System :: POSIX',
+    'Operating System :: Unix',
+    'Operating System :: MacOS',
+    'Natural Language :: English',
+]
+
+with open('README.rst') as f:
+    LONG_DESCRIPTION = ''.join(f.readlines())
+
+setup(
+    name='vectormath',
+    version='0.1.2',
+    packages=find_packages(),
+    install_requires=[
+        'numpy>=1.7',
+    ],
+    author='ARANZ Geo Limited',
+    author_email='it@aranzgeo.com',
+    description='vectormath: vector math utilities for Python',
+    long_description=LONG_DESCRIPTION,
+    keywords='linear algebra, vector, plane, math',
+    url='https://github.com/aranzgeo/vectormath',
+    download_url='http://github.com/aranzgeo/vectormath',
+    classifiers=CLASSIFIERS,
+    platforms=['Windows', 'Linux', 'Solaris', 'Mac OS-X', 'Unix'],
+    license='MIT License',
+    use_2to3=False,
+)
