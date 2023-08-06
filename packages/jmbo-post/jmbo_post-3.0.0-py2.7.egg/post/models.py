aa2a7e7b9423b16ce466420b85197238dd4c3312
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:34:36 +0000

    Add package jmbo-post/3.0.0/jmbo_post-3.0.0-py2.7.egg

---
+++ packages/jmbo-post/jmbo_post-3.0.0-py2.7.egg/post/models.py
@@ -0,0 +1,34 @@
+import markdown
+from bs4 import BeautifulSoup
+
+from django.db import models
+from django.core.urlresolvers import reverse
+
+from simplemde.fields import SimpleMDEField
+
+from jmbo.models import ModelBase
+
+
+class Post(ModelBase):
+    autosave_fields = ("markdown",)
+
+    markdown = SimpleMDEField(null=True, blank=True)
+
+    class Meta:
+        verbose_name = "Post"
+        verbose_name_plural = "Posts"
+
+    @property
+    def content(self):
+        if not self.markdown:
+            return ""
+        return markdown.markdown(self.markdown)
+
+    @property
+    def content_pages(self):
+        marker = "--m-a-r-k-er--"
+        soup = BeautifulSoup(self.content, "html.parser")
+        elems = soup.find_all("hr")
+        for elem in elems:
+            elem.replace_with(marker)
+        return soup.renderContents().split(marker)
---
+++ packages/jmbo-post/jmbo_post-3.0.0-py2.7.egg/post/tests/urls.py
@@ -0,0 +1,22 @@
+from django.conf.urls import include, url
+from django.contrib import admin
+
+from rest_framework import routers
+from rest_framework_extras import discover
+
+from post import api as post_api
+
+
+admin.autodiscover()
+
+router = routers.SimpleRouter()
+post_api.register(router)
+discover(router)
+
+urlpatterns = [
+    url(r"^admin/", include(admin.site.urls)),
+    url(r'^api/(?P<version>(v1))/', include(router.urls)),
+    url(r"^jmbo/", include("jmbo.urls", namespace="jmbo")),
+    url(r"^post/", include("post.urls", namespace="jmbo")),
+    url(r"^comments/", include("django_comments.urls")),
+]
