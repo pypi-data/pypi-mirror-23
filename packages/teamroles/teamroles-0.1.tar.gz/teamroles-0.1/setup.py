79b3d7f66b991c9d2879a3a7c08bcf687553206a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:14 +0000

    Add package teamroles/0.1/teamroles-0.1.tar.gz

---
+++ packages/teamroles/teamroles-0.1.tar.gz/teamroles-0.1/setup.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+setup(
+  name='teamroles',
+  packages=['teamroles'],  # this must be the same as the name above
+  version='0.1',
+  description='Test Paketi',
+  author='Mevlana Ayas',
+  author_email='mevlanaayas@gmail.com',
+  url='https://github.com/mevlanaayas/django-teams.git',  # Github URL
+  download_url='https://github.com/barisariburnu/python-package/tarball/0.1',
+  keywords=['testing', 'logging', 'example', 'django-teams', 'django teams'],  # Descriptive keywords
+  classifiers=[],
+)
