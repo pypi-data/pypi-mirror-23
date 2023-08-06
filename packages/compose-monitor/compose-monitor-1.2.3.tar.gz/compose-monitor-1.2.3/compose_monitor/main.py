fbeed2a79ed888b3968627cc593b7ddcefe87abe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:33:58 +0000

    Add package compose-monitor/1.2.3/compose-monitor-1.2.3.tar.gz

---
+++ packages/compose-monitor/compose-monitor-1.2.3.tar.gz/compose-monitor-1.2.3/compose_monitor/main.py
@@ -0,0 +1,58 @@
+import argparse
+import logging
+
+import logger
+from monitor import Monitor
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-c", "--compose-file", dest="path",
+    default=".", help="Path to the docker-compose.yml")
+parser.add_argument("-t", "--timeout", dest="timeout",
+    default=10, help="Check&update timeout")
+parser.add_argument("-e", "--external-scheduler", dest="scheduler",
+    action="store_true", help="Don't use built-in timer")
+parser.add_argument("-o", "--options", dest="options",
+    help="Options for project")
+parser.add_argument("--no-recreate", dest="norecreate",
+    action="store_true", help="Don't recreate, only pull")
+parser.add_argument("--no-deps", dest="nodeps",
+    action="store_true", help="Don't restart services dependencies")
+parser.add_argument("--affect-only-running", dest="running",
+    action="store_true",
+    help="Restart only running services and don't touch stopped")
+parser.add_argument("-l", "--log", dest="log",
+    help="Redirect logging to file")
+args = parser.parse_args()
+
+if args.log is not None:
+    log = logging.getLogger(__name__)
+    log.addHandler(logger.FileHandler(args.log))
+    log.setLevel(logging.DEBUG)
+else:
+    log = logging.getLogger(__name__)
+    log.addHandler(logger.StreamHandler())
+    log.setLevel(logging.DEBUG)
+
+
+def main():
+    try:
+        if args.options is None:
+            monitor = Monitor(args.path, dict(),
+                args.norecreate, args.nodeps, args.running, args.log)
+        else:
+            monitor = Monitor(args.path,
+                dict(zip(args.options.split()[0::2],
+                         args.options.split()[1::2])),
+                args.norecreate, args.nodeps, args.running, args.log)
+        log.info("Monitor created successfully")
+
+        log.info("Starting of monitor...")
+        monitor.run(int(args.timeout), args.scheduler)
+
+    except KeyboardInterrupt:
+        print('\nThe process was interrupted by the user')
+        raise SystemExit
+
+if __name__ == "__main__":
+    main()
---
+++ packages/compose-monitor/compose-monitor-1.2.3.tar.gz/compose-monitor-1.2.3/compose_monitor/monitor.py
@@ -0,0 +1,104 @@
+import time
+import logging
+import traceback
+
+from compose.cli import command
+from compose.config.errors import ConfigurationError
+
+import logger
+
+from cStringIO import StringIO
+import sys
+
+
+class Capturing(list):
+    def __enter__(self):
+        self._stdout = sys.stdout
+        self._stderr = sys.stderr
+        sys.stdout = sys.stderr = self._out = StringIO()
+        return self
+
+    def __exit__(self, *args):
+        self.extend(self._out.getvalue().splitlines())
+        del self._out
+        sys.stdout = self._stdout
+        sys.stderr = self._stderr
+
+
+class Monitor(object):
+    def __init__(self, path, options,
+                 norecreate, nodeps, running, filelog=None):
+        global log
+
+        if "log" not in globals():
+            if filelog is not None:
+                log = logging.getLogger(__name__)
+                log.addHandler(logger.FileHandler(filelog))
+                log.setLevel(logging.DEBUG)
+            else:
+                log = logging.getLogger(__name__)
+                log.addHandler(logger.StreamHandler())
+                log.setLevel(logging.DEBUG)
+
+        self.path = path
+        self.options = options
+        self.norecreate = norecreate
+        self.nodeps = nodeps
+        self.running = running
+        try:
+            self.project = command.project_from_options(self.path,
+                                                        self.options)
+        except ConfigurationError:
+            log.error("Can't create a monitor unit\n{}".
+                format(traceback.format_exc()))
+            raise SystemExit
+
+    def _pullup(self):
+        try:
+            self.project.pull()
+            log.info("Images pulled successfully")
+        except Exception:
+            log.error("Can't pull images")
+            raise
+
+        if self.norecreate:
+            return
+
+        if self.running:
+            services = [el.name for el in self._fully_running_services()]
+        else:
+            services = None
+
+        try:
+            self.project.up(services, start_deps=not self.nodeps)
+            log.info("Project updated successfully")
+        except Exception:
+            log.error("Can't update project")
+            raise
+
+    def _fully_running_services(self):
+        fully_running = []
+
+        for service in self.project.get_services():
+            if all(container.is_running for container in service.containers(stopped=True)):
+                fully_running.append(service)
+            else:
+                log.warning("Some containers for service \"{}\" are down". \
+                    format(service.name))
+
+        return fully_running
+
+    def run(self, timeout, scheduler):
+        log.info("Monitor started successfully")
+        while True:
+            try:
+                with Capturing() as output:
+                    self._pullup()
+            except Exception:
+                log.error("Service checking failed\n{}".
+                    format(traceback.format_exc()))
+            if scheduler:
+                break
+
+            log.info("Checked successfully\n{}".format(output))
+            time.sleep(timeout)
