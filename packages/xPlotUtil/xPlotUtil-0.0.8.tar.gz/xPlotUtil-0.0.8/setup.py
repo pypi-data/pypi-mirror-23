bbf958d34f7a950735209957d6251a429c844a3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:09:33 +0000

    Add package xPlotUtil/0.0.8/xPlotUtil-0.0.8.tar.gz

---
+++ packages/xPlotUtil/xPlotUtil-0.0.8.tar.gz/xPlotUtil-0.0.8/setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+"""
+Copyright (c) UChicago Argonne, LLC. All rights reserved.
+See LICENSE file.
+"""
+
+from setuptools import setup
+
+setup(
+  name='xPlotUtil',
+  version='0.0.8',
+  description='The program provides a GUI for the user to graph the data in different forms, normalize and fit it.',
+  author='Phaulo C. Escalante',
+  author_email='escalante.phaulo@outlook.com',
+  url='https://github.com/AdvancedPhotonSource/xPlotUtil',
+  packages=['xPlotUtil', ],
+  install_requires=['spec2nexus',
+                    'matplotlib',
+                    'numpy',
+                    'future',
+                    'scipy',
+                    'peakutils',
+                    ],
+  license='See LICENSE File',
+  platforms='any',
+  scripts=['Scripts/xPlotUtil.bat', ],
+  entry_points={
+    'setuptools.installation': ['eggsecutable = xPlotUtil.xPlotUtil.PlotWindow:main', ]
+  }
+)
