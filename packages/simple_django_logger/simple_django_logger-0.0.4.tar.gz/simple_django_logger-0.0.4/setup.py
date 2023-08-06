ce54758d7f5bdb5d8a29ccb514601b719c7cee27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:28 +0000

    Add package simple_django_logger/0.0.4/simple_django_logger-0.0.4.tar.gz

---
+++ packages/simple_django_logger/simple_django_logger-0.0.4.tar.gz/simple_django_logger-0.0.4/setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup
+# Read more here: https://www.codementor.io/arpitbhayani/host-your-python-package-using-github-on-pypi-du107t7ku
+
+setup(
+    name='simple_django_logger',
+    packages=[
+        'simple_django_logger',  # this must be the same as the name above
+        'simple_django_logger.middleware',
+        'simple_django_logger.migrations'],
+    include_package_data=True,
+    version='0.0.4',
+    description='A basic logger for Django',
+    author='Eshan Das',
+    author_email='eshandasnit@gmail.com',
+    url='https://github.com/eshandas/simple_django_logger',  # use the URL to the github repo
+    download_url='https://github.com/eshandas/simple_django_logger/archive/0.0.4.tar.gz',  # Create a tag in github
+    keywords=['django', 'logger'],
+    classifiers=[],
+)
