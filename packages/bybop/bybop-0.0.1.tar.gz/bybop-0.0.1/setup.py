e8a6f1fa2674a6c45ab53a69ea4e27654004c9b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:57:08 +0000

    Add package bybop/0.0.1/bybop-0.0.1.tar.gz

---
+++ packages/bybop/bybop-0.0.1.tar.gz/bybop-0.0.1/setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+from setuptools import setup, find_packages
+
+setup(
+        name="bybop",
+        version="0.0.1",
+        packages=find_packages('src'),
+        package_dir={'': 'src'},
+        zip_safe=False,
+        include_package_data=True,
+        author="Ya-Liang Chang",
+        author_email="allen314x@yahoo.com.tw",
+        description="Bebop Drone control for python",
+        platforms="Linux",
+        license="BSD",
+        keywords="bebop python control",
+        url="https://github.com/amjltc295/bybop"
+    )
