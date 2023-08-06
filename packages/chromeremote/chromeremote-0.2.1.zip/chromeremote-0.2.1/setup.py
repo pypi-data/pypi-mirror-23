711dd4351be1716ec95399d11a7109d5203123a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:08:32 +0000

    Add package chromeremote/0.2.1/chromeremote-0.2.1.zip

---
+++ packages/chromeremote/chromeremote-0.2.1.zip/chromeremote-0.2.1/setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+setup(
+    name="chromeremote",
+    version="0.2.1",
+    description="Chrome Remote Dev Tools",
+    author="sadnoodles",
+    author_email="sadnoodles@gmail.com",
+    url="https://github.com/sadnoodles/chromeremote",
+    download_url="https://github.com/sadnoodles/chromeremote/archive/v0.2.0.tar.gz",
+    license="GPL",
+    py_modules=["chromeremote"],
+    install_requires=[
+         'requests',
+         'websocket-client',
+    ],
+    platforms="any"
+)
