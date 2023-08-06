ce64abed78a78a02053f45bc8bd5ccaead0336cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:42 +0000

    Add package pip_debugger_parikshit/1.0.0/pip_debugger_parikshit-1.0.0.tar.gz

---
+++ packages/pip_debugger_parikshit/pip_debugger_parikshit-1.0.0.tar.gz/pip_debugger_parikshit-1.0.0/pip_debug/pip_debug.py
@@ -0,0 +1,7 @@
+
+def pip_debug():
+	return "debdafda"
+
+
+if __name__=="__main__":
+	print pip_debug()
---
+++ packages/pip_debugger_parikshit/pip_debugger_parikshit-1.0.0.tar.gz/pip_debugger_parikshit-1.0.0/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='pip_debugger_parikshit',
+      version='1.0.0',
+      description='Debugging pip',
+      packages=['pip_debug'],
+     )  
