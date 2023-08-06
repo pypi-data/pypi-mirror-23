f5d9e5357e21bf942e6ace3d528993e63060d92b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:57:42 +0000

    Add package analytic-wfm/0.2/analytic_wfm-0.2.tar.gz

---
+++ packages/analytic-wfm/analytic_wfm-0.2.tar.gz/analytic_wfm-0.2/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+   name='analytic_wfm',
+   version='0.2',
+   author='None',
+   url='https://gist.github.com/1178136.git',
+   description='peak detection algorithms',
+   packages=find_packages(),
+   install_requires=['numpy', 'scipy'],
+   tests_require=['matplotlib'],
+)
