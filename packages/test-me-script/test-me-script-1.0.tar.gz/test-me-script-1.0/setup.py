19b1d70f1b465f59cc8b6fca39bdfdcc2d4b9cb2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:10:21 +0000

    Add package test-me-script/1.0/test-me-script-1.0.tar.gz

---
+++ packages/test-me-script/test-me-script-1.0.tar.gz/test-me-script-1.0/setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+name='test-me-script',    # This is the name of your PyPI-package.
+version='1.0',                          # Update the version number for new releases
+scripts=['helloworld']                  # The name of your scipt, and also the command you'll be using for calling it
+)
