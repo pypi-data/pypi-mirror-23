77fe86fe9dbef6b35841b5e69d93b1586619ed39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:07:51 +0000

    Add package legendarium/1.3.2/legendarium-1.3.2.tar.gz

---
+++ packages/legendarium/legendarium-1.3.2.tar.gz/legendarium-1.3.2/legendarium/utils.py
@@ -0,0 +1,39 @@
+# coding: utf-8
+
+translations = {
+    "volume": {
+        "pt": "Volume",
+        "es": "Volumen",
+        "en": "Volume"
+    },
+    "number": {
+        "pt": u"Número",
+        "es": "Numero",
+        "en": "Number"
+    },
+    "pages": {
+        "pt": u"Páginas",
+        "es": "Paginas",
+        "en": "Pages"
+    },
+    "supplement": {
+        "pt": "Suplemento",
+        "es": "Suplemento",
+        "en": "Supplement"
+    },
+    "suppl": {
+        "pt": "supl",
+        "es": "supl",
+        "en": "suppl"
+    },
+    "article number": {
+        "pt": "Número do artigo",
+        "es": "Numero del articulo",
+        "en": "Article number"
+    },
+    "published": {
+        "pt": "Publicado",
+        "es": "Publicado",
+        "en": "Published"
+    }
+}
