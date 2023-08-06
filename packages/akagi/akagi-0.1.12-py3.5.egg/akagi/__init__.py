6a8823bc239ed8a1d6ccab15ea625bbc7845d055
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:58 +0000

    Add package akagi/0.1.12/akagi-0.1.12-py3.5.egg

---
+++ packages/akagi/akagi-0.1.12-py3.5.egg/akagi/__init__.py
@@ -0,0 +1,15 @@
+from __future__ import absolute_import
+
+# -*- coding: utf-8 -*-
+__author__ = """Yuichiro Someya"""
+__email__ = 'ayemos.y@gmail.com'
+__version__ = '0.1.12'
+
+
+from akagi import data_file
+from akagi import data_files
+
+from akagi import data_file_bundle
+from akagi import data_file_bundles
+
+from akagi import iterators
