00831d16543f282172a94d6f87b4d8db9e8d12fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:55 +0000

    Add package btcmarkets/0.0.6/btcmarkets-0.0.6.tar.gz

---
+++ packages/btcmarkets/btcmarkets-0.0.6.tar.gz/btcmarkets-0.0.6/setup.py
@@ -0,0 +1,17 @@
+
+from setuptools import setup
+from btcmarkets import __version__
+
+setup(
+    name="btcmarkets",
+    packages=["btcmarkets"],
+    version=__version__,
+    description="Python wrapper for the BTCMarkets API",
+    author="Bradley McElroy",
+    author_email="bradley.mcelroy@live.com",
+    url="https://github.com/limx0/btcmarkets",
+    extras_require={
+      'testing': ['vcrpy'],
+    }
+    )
+
