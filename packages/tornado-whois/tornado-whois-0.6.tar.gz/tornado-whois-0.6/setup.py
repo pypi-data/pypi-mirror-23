94057f4f4cbf37e505be505102fba912c1a81eb9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:12:52 +0000

    Add package tornado-whois/0.6/tornado-whois-0.6.tar.gz

---
+++ packages/tornado-whois/tornado-whois-0.6.tar.gz/tornado-whois-0.6/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+try:
+    with open('README.rst') as f:
+        readme = f.read()
+except IOError:
+    readme = ''
+
+setup(
+    name="tornado-whois",
+    version="0.6",
+    keywords=["tornado", "whois", "tornado-whois", "asyncwhois"],
+    description="Asynchronous python tornado whois client",
+    long_description=readme,
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "License :: OSI Approved :: MIT License",
+    ],
+    install_requires=["tornado>=4.3"],
+    requires=["tornado (>=4.3)"],
+)
