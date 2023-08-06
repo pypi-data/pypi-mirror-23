3e9f9ae010258634ce048f511c00e996c8e71f72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:56:18 +0000

    Add package django-rest-framework/0.1.0/django-rest-framework-0.1.0.tar.gz

---
+++ packages/django-rest-framework/django-rest-framework-0.1.0.tar.gz/django-rest-framework-0.1.0/setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+
+setup(
+    name='django-rest-framework',
+    version='0.1.0',
+    url='http://www.django-rest-framework.org',
+    license='BSD',
+    description='alias.',
+    author='Tom Christie',
+    author_email='tom@tomchristie.com',
+    packages=['empty'],
+    install_requires=['djangorestframework'],
+    zip_safe=False,
+    classifiers=[
+        'Topic :: Internet :: WWW/HTTP',
+    ]
+)
