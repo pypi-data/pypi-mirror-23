6f8baf153858a6d92d2a9e00d744bbb60a13da3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:32:32 +0000

    Add package infi.mount_utils/0.0.8/infi.mount_utils-0.0.8-py2-none-any.whl

---
+++ packages/infi.mount_utils/infi.mount_utils-0.0.8-py2-none-any.whl/infi/mount_utils/__version__.py
@@ -0,0 +1,11 @@
+__version__ = "0.0.8"
+__git_commiter_name__ = "Arnon Yaari"
+__git_commiter_email__ = "arnony@infinidat.com"
+__git_branch__ = u'(HEAD detached at v0.0.8)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = 'ef304d0c139465a2d10ec77d383174b122720ad7'
+__git_head_subject__ = u'Finished Release v0.0.8'
+__git_head_message__ = u''
+__git_dirty_diff__ = u''
+__git_commit_date__ = '2017-07-09 14:59:20'
