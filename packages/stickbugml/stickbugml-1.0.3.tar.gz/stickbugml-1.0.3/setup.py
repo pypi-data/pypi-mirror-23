aa28a9208fd608c022edbe4bb9c5a6d555ef5375
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:46:57 +0000

    Add package stickbugml/1.0.3/stickbugml-1.0.3.tar.gz

---
+++ packages/stickbugml/stickbugml-1.0.3.tar.gz/stickbugml-1.0.3/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+setup(
+    name='stickbugml',
+    version='1.0.3',
+    description='A framework to organize the process of designing supervised machine learning systems',
+    keywords='stick bug, ml, machine learning, ai, artificial intelligence, framework, organization, organize',
+    license="Apache 2.0",
+    author='Aaron Janse',
+    author_email='gitduino@gmail.com',
+    url='https://github.com/aaronduino/stick-bug-ml',
+    packages=['stickbugml'],  #same as name
+    install_requires=['sklearn', 'pandas', 'numpy'],
+    long_description=long_description,
+    #external packages as dependencies
+    python_requires='>=3',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+    ],
+)
