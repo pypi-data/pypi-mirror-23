2b25d1f42a0ddcb82596bce497cf7c3ab7742861
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:56:08 +0000

    Add package macosko2015/2.0.2/macosko2015-2.0.2-py2.py3-none-any.whl

---
+++ packages/macosko2015/macosko2015-2.0.2-py2.py3-none-any.whl/macosko2015-2.0.2.dist-info/DESCRIPTION.rst
@@ -0,0 +1,3 @@
+See https://github.com/olgabot/macosko2015
+
+
---
+++ packages/macosko2015/macosko2015-2.0.2-py2.py3-none-any.whl/macosko2015/__init__.py
@@ -0,0 +1,4 @@
+__version__ = '2.0.2'
+
+from .main import load_amacrine, load_big_clusters, BASE_URL, \
+    load_differential_clusters
