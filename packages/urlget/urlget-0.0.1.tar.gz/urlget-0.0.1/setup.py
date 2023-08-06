3f308007299fd9d360c25d28ec4432d7cf241ae8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:09 +0000

    Add package urlget/0.0.1/urlget-0.0.1.tar.gz

---
+++ packages/urlget/urlget-0.0.1.tar.gz/urlget-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name='urlget',
+	version='0.0.1',
+	description='Simple site Scraper',
+	author = 'Moon, Heung-sub',
+	author_email = 'mhs9089@gmail.com',
+	py_modules = ['urlget'],
+	install_requires=['requests', 'bs4', 'tqdm'],
+)
