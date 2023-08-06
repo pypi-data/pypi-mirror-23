7e06c2a80730ec01a0caf258cda29bd76b458659
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:08:08 +0000

    Add package pywal/0.3.8/pywal-0.3.8.tar.gz

---
+++ packages/pywal/pywal-0.3.8.tar.gz/pywal-0.3.8/pywal/settings.py
@@ -0,0 +1,11 @@
+"""
+Global Constants.
+"""
+import pathlib
+
+
+__version__ = "0.3.8"
+
+
+COLOR_COUNT = 16
+CACHE_DIR = pathlib.Path.home() / ".cache/wal/"
---
+++ packages/pywal/pywal-0.3.8.tar.gz/pywal-0.3.8/setup.py
@@ -0,0 +1,43 @@
+"""wal - setup.py"""
+import setuptools
+
+try:
+    import pywal
+except (ImportError, SyntaxError):
+    raise ImportError('pywal requires Python 3.6 or greater.')
+
+
+DESC = (
+    "View the DOCS at: https://github.com/dylanaraps/pywal\n\n"
+    "Pypi doesn't like markdown OR rst with anchor links so "
+    "you'll have to view the documentation elsewhere.\n"
+)
+
+VERSION = pywal.__version__
+DOWNLOAD = "https://github.com/dylanaraps/pywal/archive/%s.tar.gz" % VERSION
+
+
+setuptools.setup(
+    name="pywal",
+    version=VERSION,
+    author="Dylan Araps",
+    author_email="dylan.araps@gmail.com",
+    description="Generate and change colorschemes on the fly",
+    long_description=DESC,
+    license="MIT",
+    url="https://github.com/dylanaraps/pywal",
+    download_url=DOWNLOAD,
+    classifiers=[
+        "Environment :: X11 Applications",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 3.6",
+    ],
+    packages=["pywal"],
+    entry_points={
+        "console_scripts": ["wal=pywal.__main__:main"]
+    },
+    python_requires=">=3.6",
+    test_suite="tests",
+    include_package_data=True
+)
