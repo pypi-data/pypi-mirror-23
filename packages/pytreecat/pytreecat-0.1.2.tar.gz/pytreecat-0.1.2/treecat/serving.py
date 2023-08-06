322757c1c633f1f4165521d9aba379719f1fceff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:08:28 +0000

    Add package pytreecat/0.1.2/pytreecat-0.1.2.tar.gz

---
+++ packages/pytreecat/pytreecat-0.1.2.tar.gz/pytreecat-0.1.2/treecat/serving.py
@@ -0,0 +1,285 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import logging
+
+import numpy as np
+from scipy.misc import logsumexp
+from scipy.stats import entropy
+
+from treecat.structure import TreeStructure
+from treecat.structure import make_propagation_program
+from treecat.util import profile
+from treecat.util import sample_from_probs2
+
+logger = logging.getLogger(__name__)
+
+
+def correlation(probs):
+    """Compute correlation rho(X,Y) = sqrt(1 - exp(-2 I(X;Y)))."""
+    assert probs.shape[0] == probs.shape[1]
+    mutual_information = (entropy(probs.sum(0)) + entropy(probs.sum(1)) -
+                          entropy(probs.flatten()))
+    return np.sqrt(1.0 - np.exp(-2.0 * mutual_information))
+
+
+class ServerBase(object):
+    """Base class for TreeCat and Ensemble servers."""
+
+    def __init__(self, ragged_index):
+        self._ragged_index = ragged_index
+        self._zero_row = np.zeros(ragged_index[-1], np.int8)
+
+    @property
+    def ragged_index(self):
+        return self._ragged_index
+
+    def make_zero_row(self):
+        """Make an empty data row."""
+        return self._zero_row.copy()
+
+
+class TreeCatServer(ServerBase):
+    """Class for serving queries against a trained TreeCat model."""
+
+    def __init__(self, tree, suffstats, config):
+        logger.info('TreeCatServer with %d features', tree.num_vertices)
+        assert isinstance(tree, TreeStructure)
+        ragged_index = suffstats['ragged_index']
+        ServerBase.__init__(self, ragged_index)
+        self._tree = tree
+        self._config = config
+        self._program = make_propagation_program(tree.tree_grid)
+
+        # These are useful dimensions to import into locals().
+        V = self._tree.num_vertices
+        E = V - 1  # Number of edges in the tree.
+        M = self._config[
+            'model_num_clusters']  # Clusters in each mixture model.
+        self._VEM = (V, E, M)
+
+        # Use Jeffreys priors.
+        vert_prior = 0.5
+        edge_prior = 0.5 / M
+        feat_prior = 0.5 / M
+        meas_prior = feat_prior * np.array(
+            [(ragged_index[v + 1] - ragged_index[v]) for v in range(V)],
+            dtype=np.float32).reshape((V, 1))
+
+        # These are posterior marginals for vertices and pairs of vertices.
+        self._vert_probs = suffstats['vert_ss'].astype(np.float32) + vert_prior
+        self._vert_probs /= self._vert_probs.sum(axis=1, keepdims=True)
+        self._edge_probs = suffstats['edge_ss'].astype(np.float32) + edge_prior
+        self._edge_probs /= self._edge_probs.sum(axis=(1, 2), keepdims=True)
+
+        # This represents information in the pairwise joint posterior minus
+        # information in the individual factors.
+        self._edge_trans = self._edge_probs.copy()
+        for e, v1, v2 in tree.tree_grid.T:
+            self._edge_trans[e, :, :] /= self._vert_probs[v1, np.newaxis, :]
+            self._edge_trans[e, :, :] /= self._vert_probs[v2, :, np.newaxis]
+
+        # This is the conditional distribution of features given latent.
+        self._feat_cond = suffstats['feat_ss'].astype(np.float32) + feat_prior
+        meas_probs = suffstats['meas_ss'].astype(np.float32) + meas_prior
+        for v in range(V):
+            beg, end = ragged_index[v:v + 2]
+            self._feat_cond[beg:end, :] /= meas_probs[v, np.newaxis, :]
+
+    @profile
+    def sample(self, N, counts, data=None):
+        """Draw N samples from the posterior distribution.
+
+        Args:
+          size: The number of samples to draw.
+          counts: A [V]-shaped numpy array of requested counts of multinomials
+            to sample.
+          data: An optional single row of conditioning data, as a ragged nummpy
+            array of multinomial counts.
+
+        Returns:
+          An [N, _]-shaped numpy array of sampled multinomial data.
+        """
+        logger.debug('sampling data')
+        V, E, M = self._VEM
+        if data is None:
+            data = self._zero_row
+        assert data.shape == self._zero_row.shape
+        assert data.dtype == self._zero_row.dtype
+        assert counts.shape == (V, )
+        assert counts.dtype == np.int8
+        edge_trans = self._edge_trans
+        feat_cond = self._feat_cond
+
+        messages_in = self._vert_probs.copy()
+        messages_out = np.tile(self._vert_probs[:, np.newaxis, :], (1, N, 1))
+        vert_samples = np.zeros([V, N], np.int8)
+        feat_samples = np.zeros([N, self._zero_row.shape[0]], np.int8)
+        range_N = np.arange(N, dtype=np.int32)
+
+        for op, v, v2, e in self._program:
+            if op == 0:  # OP_UP
+                # Propagate upward from observed to latent.
+                message = messages_in[v, :]
+                beg, end = self._ragged_index[v:v + 2]
+                for r in range(beg, end):
+                    # This uses a with-replacement approximation which is exact
+                    # for categorical data but approximate for multinomial.
+                    message *= feat_cond[r, :]**data[r]
+            elif op == 1:  # OP_IN
+                # Propagate latent state inward from children to v.
+                message = messages_in[v, :]
+                trans = edge_trans[e, :, :]
+                if v > v2:
+                    trans = trans.T
+                message *= np.dot(trans, messages_in[v2, :])
+                message /= message.sum()
+            else:  # OP_ROOT or OP_OUT
+                message = messages_out[v, :, :]
+                message[...] = messages_in[v, np.newaxis, :]
+                # Propagate latent state outward from parent to v.
+                if op == 3:  # OP_OUT
+                    trans = edge_trans[e, :, :]
+                    if v2 > v:
+                        trans = trans.T
+                    message *= trans[vert_samples[v2, :], :]
+                message /= message.sum(axis=1, keepdims=True)
+                vert_samples[v, :] = sample_from_probs2(message)
+                # Propagate downward from latent to observed.
+                beg, end = self._ragged_index[v:v + 2]
+                feat_block = feat_cond[beg:end, :].T
+                probs = feat_block[vert_samples[v, :], :]
+                samples_block = feat_samples[:, beg:end]
+                for _ in range(counts[v]):
+                    samples_block[range_N, sample_from_probs2(probs)] += 1
+
+        return feat_samples
+
+    @profile
+    def logprob(self, data):
+        """Compute non-normalized log probabilies of many rows of data.
+
+        To compute conditional probabilty, use the identity:
+
+          log P(data|cond_data) = server.logprob(data + cond_data)
+                                - server.logprob(cond_data)
+
+        Args:
+          data: A [N, _]-shaped ragged nummpy array of multinomial count data,
+            where N is the number of rows.
+
+        Returns:
+          An [N]-shaped numpy array of log probabilities.
+        """
+        logger.debug('computing logprob')
+        assert len(data.shape) == 2
+        assert data.shape[1] == self._ragged_index[-1]
+        assert data.dtype == np.int8
+        N = data.shape[0]
+        V, E, M = self._VEM
+        edge_trans = self._edge_trans
+        feat_cond = self._feat_cond
+
+        messages = np.tile(self._vert_probs[:, :, np.newaxis], (1, 1, N))
+        assert messages.shape == (V, M, N)
+        logprob = np.zeros(N, np.float32)
+
+        for op, v, v2, e in self._program:
+            message = messages[v, :, :]
+            if op == 0:  # OP_UP
+                # Propagate upward from observed to latent.
+                beg, end = self._ragged_index[v:v + 2]
+                for r in range(beg, end):
+                    # This uses a with-replacement approximation which is exact
+                    # for categorical data but approximate for multinomial.
+                    power = data[np.newaxis, :, r]
+                    message *= feat_cond[r, :, np.newaxis]**power
+            elif op == 1:  # OP_IN
+                # Propagate latent state inward from children to v.
+                trans = edge_trans[e, :, :]
+                if v > v2:
+                    trans = trans.T
+                message *= np.dot(trans, messages[v2, :, :])
+                message_sum = message.sum(axis=0, keepdims=True)
+                message /= message_sum
+                logprob += np.log(message_sum[0, :])
+            elif op == 2:  # OP_ROOT
+                # Aggregate the total logprob.
+                logprob += np.log(message.sum(axis=0))
+                return logprob
+
+    @profile
+    def correlation(self):
+        """Compute correlation matrix among latent features.
+
+        This computes the generalization of Pearson's correlation to discrete
+        data. Let I(X;Y) be the mutual information. Then define correlation as
+
+          rho(X,Y) = sqrt(1 - exp(-2 I(X;Y)))
+
+        Returns:
+          An [V, V] numpy array of feature-feature correlations.
+        """
+        logger.debug('computing correlation')
+        V, E, M = self._VEM
+        edge_probs = self._edge_probs
+        vert_probs = self._vert_probs
+        result = np.zeros([V, V], np.float32)
+        for root in range(V):
+            messages = np.empty([V, M, M])
+            program = make_propagation_program(self._tree.tree_grid, root)
+            for op, v, v2, e in program:
+                if op == 2:  # OP_ROOT
+                    messages[v, :, :] = np.diagflat(vert_probs[v, :])
+                elif op == 3:  # OP_OUT
+                    trans = edge_probs[e, :, :]
+                    if v > v2:
+                        trans = trans.T
+                    messages[v, :, :] = np.dot(trans /
+                                               vert_probs[v2, np.newaxis, :],
+                                               messages[v2, :, :])
+            for v in range(V):
+                result[root, v] = correlation(messages[v, :, :])
+        return result
+
+
+def serve_model(tree, suffstats, config):
+    return TreeCatServer(tree, suffstats, config)
+
+
+class EnsembleServer(ServerBase):
+    """Class for serving queries against a trained TreeCat ensemble model."""
+
+    def __init__(self, ensemble):
+        logger.info('EnsembleServer of size %d', len(ensemble))
+        assert ensemble
+        ServerBase.__init__(self, ensemble[0]['suffstats']['ragged_index'])
+        self._ensemble = [
+            TreeCatServer(model['tree'], model['suffstats'], model['config'])
+            for model in ensemble
+        ]
+
+    def sample(self, N, counts, data=None):
+        size = len(self._ensemble)
+        pvals = np.ones(size, dtype=np.float32) / size
+        sub_Ns = np.random.multinomial(N, pvals)
+        samples = np.concatenate([
+            server.sample(sub_N, counts, data)
+            for server, sub_N in zip(self._ensemble, sub_Ns)
+        ])
+        np.random.shuffle(samples)
+        assert samples.shape[0] == N
+        return samples
+
+    def logprob(self, data):
+        logprobs = np.stack(
+            [server.logprob(data) for server in self._ensemble])
+        logprobs = logsumexp(logprobs, axis=0)
+        logprobs -= np.log(len(self._ensemble))
+        assert logprobs.shape == (data.shape[0], )
+        return logprobs
+
+
+def serve_ensemble(ensemble):
+    return EnsembleServer(ensemble)
---
+++ packages/pytreecat/pytreecat-0.1.2.tar.gz/pytreecat-0.1.2/treecat/structure_test.py
@@ -0,0 +1,185 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+from collections import defaultdict
+
+import numpy as np
+import pytest
+from goftests import multinomial_goodness_of_fit
+
+from treecat.structure import OP_IN
+from treecat.structure import OP_OUT
+from treecat.structure import OP_ROOT
+from treecat.structure import OP_UP
+from treecat.structure import find_center_of_tree
+from treecat.structure import make_complete_graph
+from treecat.structure import make_propagation_program
+from treecat.structure import make_tree
+from treecat.structure import sample_tree
+from treecat.testutil import numpy_seterr
+from treecat.util import set_random_seed
+
+numpy_seterr()
+
+# https://oeis.org/A000272
+NUM_SPANNING_TREES = [1, 1, 1, 3, 16, 125, 1296, 16807, 262144, 4782969]
+
+EXAMPLE_TREES = [
+    [],
+    [(0, 1)],
+    [(0, 1)],
+    [(0, 1), (1, 2)],
+    [(0, 1), (0, 2)],
+    [(0, 2), (1, 2)],
+    [(0, 1), (1, 2), (1, 3)],
+    [(0, 1), (1, 2), (2, 3), (3, 4), (4, 5)],
+    [(0, 1), (1, 2), (2, 3), (3, 4), (3, 5)],
+    [(0, 1), (1, 2), (1, 3), (3, 4), (3, 5)],
+]
+
+
+@pytest.mark.parametrize('num_vertices,expected_grid', [
+    (0, []),
+    (1, []),
+    (2, [[0], [0], [1]]),
+    (3, [[0, 1, 2], [0, 0, 1], [1, 2, 2]]),
+    (4, [[0, 1, 2, 3, 4, 5], [0, 0, 1, 0, 1, 2], [1, 2, 2, 3, 3, 3]]),
+])
+def test_make_complete_graph(num_vertices, expected_grid):
+    V = num_vertices
+    K = V * (V - 1) // 2
+    expected_grid = np.array(expected_grid, dtype=np.int32).reshape([3, K])
+
+    grid = make_complete_graph(V)
+    np.testing.assert_array_equal(grid, expected_grid)
+
+
+@pytest.mark.parametrize('edges,expected_grid', [
+    ([], []),
+    ([(0, 1)], [[0], [0], [1]]),
+    ([(0, 1), (1, 2)], [[0, 1], [0, 1], [1, 2]]),
+    ([(0, 1), (0, 2)], [[0, 1], [0, 0], [1, 2]]),
+    ([(2, 1), (1, 0)], [[0, 1], [0, 1], [1, 2]]),
+])
+def test_make_tree(edges, expected_grid):
+    E = len(edges)
+    expected_grid = np.array(expected_grid, dtype=np.int32)
+    expected_grid.shape = (3, E)
+
+    grid = make_tree(edges)
+    assert grid.shape == expected_grid.shape
+    assert (grid == expected_grid).all()
+
+
+@pytest.mark.parametrize('edges', EXAMPLE_TREES)
+def test_make_tree_runs(edges):
+    E = len(edges)
+    V = E + 1
+    grid = make_tree(edges)
+    assert grid.shape == (3, len(edges))
+    if edges:
+        assert set(grid[0, :]) == set(range(E))
+        assert set(grid[1, :]) | set(grid[2, :]) == set(range(V))
+
+
+@pytest.mark.parametrize('expected_vertex,edges', [
+    (0, []),
+    (0, [(0, 1)]),
+    (0, [(0, 1), (0, 2)]),
+    (1, [(0, 1), (1, 2)]),
+    (2, [(0, 2), (1, 2)]),
+    (1, [(0, 1), (1, 2), (2, 3)]),
+    (1, [(0, 1), (1, 2), (1, 3)]),
+    (2, [(0, 1), (1, 2), (2, 3), (3, 4)]),
+    (2, [(0, 2), (1, 2), (2, 3), (2, 4)]),
+])
+def test_find_center_of_tree(expected_vertex, edges):
+    grid = make_tree(edges)
+
+    v = find_center_of_tree(grid)
+    assert v == expected_vertex
+
+
+EXAMPLE_ROOTED_TREES = [(edges, root)
+                        for edges in EXAMPLE_TREES
+                        for root in [None] + list(range(1 + len(edges)))]
+
+
+@pytest.mark.parametrize('edges,root', EXAMPLE_ROOTED_TREES)
+def test_make_propagation_program(edges, root):
+    E = len(edges)
+    V = E + 1
+    grid = make_tree(edges)
+    neighbors = {v: set() for v in range(V)}
+    for e, v1, v2 in grid.T:
+        neighbors[v1].add(v2)
+        neighbors[v2].add(v1)
+
+    # Generate a program.
+    program = make_propagation_program(grid, root)
+    assert program.shape == (V + E + 1 + E, 4)
+    assert program.dtype == np.int16
+
+    # Check topology.
+    if root is not None:
+        assert program[V + E][0] == OP_ROOT
+        assert program[V + E][1] == root
+    assert set(row[1] for row in program) == set(range(V)), 'bad vertex set'
+    for op, v, v2, e in program:
+        if op == OP_ROOT:
+            if root is not None:
+                assert v == root
+        elif op != OP_UP:
+            assert v != v2
+            assert v2 in neighbors[v]
+            assert grid[1, e] == min(v, v2)
+            assert grid[2, e] == max(v, v2)
+
+    # Check inward ordering.
+    state = np.zeros(V, np.int8)
+    for op, v, v2, e in program:
+        if op == OP_UP:
+            assert state[v] == 0
+            state[v] = 1
+        elif op == OP_IN:
+            assert state[v] == 1
+            assert state[v2] == 1
+        elif op == OP_ROOT:
+            assert state[v] == 1
+            state[v] = 2
+        elif op == OP_OUT:
+            assert state[v] == 1
+            assert state[v2] == 2
+            state[v] = 2
+    assert np.all(state == 2)
+
+
+@pytest.mark.parametrize('num_edges', [1, 2, 3, 4])
+def test_sample_tree_gof(num_edges):
+    set_random_seed(0)
+    E = num_edges
+    V = 1 + E
+    grid = make_complete_graph(V)
+    K = grid.shape[1]
+    edge_logits = np.random.random([K])
+    edge_probs = np.exp(edge_logits)
+    edge_probs_dict = {(v1, v2): edge_probs[k] for k, v1, v2 in grid.T}
+
+    # Generate many samples via MCMC.
+    num_samples = 2000
+    counts = defaultdict(lambda: 0)
+    edges = [(v, v + 1) for v in range(V - 1)]
+    for _ in range(num_samples):
+        edges = sample_tree(grid, edge_logits, edges)
+        counts[tuple(edges)] += 1
+    assert len(counts) == NUM_SPANNING_TREES[V]
+
+    # Check accuracy using Pearson's chi-squared test.
+    keys = counts.keys()
+    counts = np.array([counts[key] for key in keys])
+    probs = np.array(
+        [np.prod([edge_probs_dict[edge] for edge in key]) for key in keys])
+    probs /= probs.sum()
+    gof = multinomial_goodness_of_fit(probs, counts, num_samples, plot=True)
+    assert 1e-2 < gof
