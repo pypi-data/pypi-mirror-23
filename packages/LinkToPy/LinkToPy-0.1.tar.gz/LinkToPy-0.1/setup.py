3131aa093651d2128c10cfe44b42d35740dd6881
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:42 +0000

    Add package LinkToPy/0.1/LinkToPy-0.1.tar.gz

---
+++ packages/LinkToPy/LinkToPy-0.1.tar.gz/LinkToPy-0.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'LinkToPy',
+  packages = ['LinkToPy'], # this must be the same as the name above
+  version = '0.1',
+  description = 'A python wrapper for Ableton Link using TCP and Carabiner',
+  author = 'Ben Yetton',
+  author_email = 'bdyetton@gmail.com',
+  url = 'https://github.com/bdyetton/LinkToPy', # use the URL to the github repo
+  download_url = 'https://github.com/bdyetton/LinkToPy/archive/0.1.tar.gz', # I'll explain this in a second
+  keywords = ['link','python','lighting' ,'serato', 'abelton'], # arbitrary keywords
+  classifiers = [],
+)
