4a6b49d79d31f51e95b2ed3ec66e084c8f761c03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:08:33 +0000

    Add package slothTw/4.3/slothTw-4.3.tar.gz

---
+++ packages/slothTw/slothTw-4.3.tar.gz/slothTw-4.3/setup.py
@@ -0,0 +1,22 @@
+from distutils.core import setup
+
+setup(
+    name = 'slothTw',
+    packages = ['slothTw'],
+    package_dir={'slothTw':'slothTw'},
+    package_data={'slothTw':['management/commands/*', 'migrations/*']},
+    version = '4.3',
+    description = 'API for feedback of Course',
+    author = 'davidtnfsh',
+    author_email = 'davidtnfsh@gmail.com',
+    url = 'https://github.com/Stufinite/slothTw',
+    download_url = 'https://github.com/Stufinite/slothTw/archive/v4.3.tar.gz',
+    keywords = ['feedback', 'campass'],
+    classifiers = [],
+    license='GPL3.0',
+    install_requires=[
+        'djangoApiDec',
+        'simplejson'
+    ],
+    zip_safe=True
+)
