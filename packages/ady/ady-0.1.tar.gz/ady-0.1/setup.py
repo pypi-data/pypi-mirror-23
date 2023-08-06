00cc1016f0c67de0cfc10cd6add228a07b74d84c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:08:44 +0000

    Add package ady/0.1/ady-0.1.tar.gz

---
+++ packages/ady/ady-0.1.tar.gz/ady-0.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'ady',
+  packages = ['ady'],
+  version = '0.1',
+  description = 'ADY - Schedule between Baku Sumgait Baku',
+  author = 'Chingiz Huseynzade',
+  author_email = 'chingiz.h@gmail.com',
+  url = 'https://github.com/Chingiz/ADY-Hereket-Cedveli',
+  download_url = 'https://github.com/Chingiz/ADY-Hereket-Cedveli/archive/0.1.tar.gz',
+  keywords = ['ady', 'schedule', 'baku', 'sumgait'],
+  classifiers = [],
+)
