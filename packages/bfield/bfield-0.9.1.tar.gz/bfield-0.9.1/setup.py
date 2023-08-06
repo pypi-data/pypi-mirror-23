de569ac97b49c70b1c89e3933e56f33fa3de0b19
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:51 +0000

    Add package bfield/0.9.1/bfield-0.9.1.tar.gz

---
+++ packages/bfield/bfield-0.9.1.tar.gz/bfield-0.9.1/setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+"""
+Setup / installer for zk-bitfield.
+
+:author: Zygmunt Krynicki <me@zygoon.pl>
+:copyright: Copyright (c) 2017 Zygmunt Krynicki.
+:license: MIT
+"""
+from setuptools import setup  # type: ignore
+
+setup()
