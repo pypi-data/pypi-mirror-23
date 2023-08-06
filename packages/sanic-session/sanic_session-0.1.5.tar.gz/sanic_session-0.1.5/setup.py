af4d3f1130560b0d7640e701cc7760da67be84bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:09:16 +0000

    Add package sanic-session/0.1.5/sanic_session-0.1.5.tar.gz

---
+++ packages/sanic-session/sanic_session-0.1.5.tar.gz/sanic_session-0.1.5/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+try:
+    import pypandoc
+    long_description = pypandoc.convert('README.md', 'rst')
+except:
+    long_description = ''
+
+setup(
+    name='sanic_session',
+    version='0.1.5',
+    description='Provides server-backed sessions for Sanic using Redis, Memcache and more.',
+    long_description=long_description,
+    url='http://github.com/subyraman/sanic_session',
+    author='Suby Raman',
+    license='MIT',
+    packages=['sanic_session'],
+    install_requires=('sanic', 'ujson'),
+    zip_safe=False,
+    keywords=['sessions', 'sanic', 'redis', 'memcache'],
+    classifiers=[
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3 :: Only',
+        'Topic :: Internet :: WWW/HTTP :: Session',
+    ]
+)
