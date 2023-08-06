ede4667dadced58ed9d78865833e711494d3c819
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:07:59 +0000

    Add package panfig/0.1.1/panfig-0.1.1.tar.gz

---
+++ packages/panfig/panfig-0.1.1.tar.gz/panfig-0.1.1/panfig/__init__.py
@@ -0,0 +1,26 @@
+from typing import Any
+
+import pandocfilters
+
+from .panfigblock import PanfigBlock
+from . import aliases, errors
+
+
+def transform_pandoc_element(alias_set:aliases.AliasSet, key:str, value:Any):
+  if alias_set.can_be_updated_from_element(key, value):
+    try:
+      alias_set.update_from_element(key, value)
+      return pandocfilters.Null()
+    except Exception as exception:
+      return errors.make_diagnostic_code_block(key, value, exception)
+
+  if PanfigBlock.is_element_a_figure_block(key, value):
+    try:
+      block = PanfigBlock.from_pandoc_element(alias_set, key, value)
+      return block.build_replacement_pandoc_element()
+    except Exception as exception:
+      return errors.make_diagnostic_code_block(key, value, exception)
+
+def main():
+  alias_set = aliases.DEFAULT_ALIASES.copy()
+  pandocfilters.toJSONFilter(lambda key, value, format, meta: transform_pandoc_element(alias_set, key, value))
---
+++ packages/panfig/panfig-0.1.1.tar.gz/panfig-0.1.1/panfig/__main__.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+
+import panfig
+
+panfig.main()
