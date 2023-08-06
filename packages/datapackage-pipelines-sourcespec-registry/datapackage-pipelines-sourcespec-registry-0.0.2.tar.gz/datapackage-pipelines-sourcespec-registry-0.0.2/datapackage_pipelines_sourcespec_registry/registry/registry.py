def5d4c34db81c8e1a5d6215a219855bcd88095a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:09:49 +0000

    Add package datapackage-pipelines-sourcespec-registry/0.0.2/datapackage-pipelines-sourcespec-registry-0.0.2.tar.gz

---
+++ packages/datapackage-pipelines-sourcespec-registry/datapackage-pipelines-sourcespec-registry-0.0.2.tar.gz/datapackage-pipelines-sourcespec-registry-0.0.2/datapackage_pipelines_sourcespec_registry/registry/registry.py
@@ -0,0 +1,105 @@
+import json
+from uuid import uuid4
+
+import datetime
+from sqlalchemy import DateTime, types
+from sqlalchemy.ext.declarative import declarative_base
+
+from sqlalchemy import Column, Unicode, String, create_engine
+from sqlalchemy.orm import sessionmaker
+
+from datapackage_pipelines.specs.resolver import resolve_generator
+
+# ## SQL DB
+Base = declarative_base()
+
+
+# ## Json as string Type
+class JsonType(types.TypeDecorator):
+    impl = types.Unicode
+
+    def process_bind_param(self, value, dialect):
+        return json.dumps(value)
+
+    def process_result_value(self, value, dialect):
+        if value:
+            return json.loads(value)
+        else:
+            return None
+
+    def copy(self, **kw):
+        return JsonType(self.impl.length)
+
+
+# ## SourceSpec
+class SourceSpec(Base):
+    __tablename__ = 'specs'
+    uuid = Column(String(128), primary_key=True)
+    owner = Column(String(128))
+    module = Column(Unicode)
+    contents = Column(JsonType)
+    created_at = Column(DateTime)
+    updated_at = Column(DateTime)
+
+
+class SourceSpecRegistry:
+
+    def __init__(self, db_connection_string):
+        self._db_connection_string = db_connection_string
+        self._engine = None
+        self._session = None
+
+    @staticmethod
+    def _verify_contents(module, contents, ignore_missing):
+        generator = resolve_generator(module)
+        if generator is None:
+            if ignore_missing:
+                return True
+            else:
+                raise ImportError("module %s not found" % module)
+        if not generator.internal_validate(contents):
+            raise ValueError("Contents invalid for module %s" % module)
+
+    @property
+    def engine(self):
+        if self._engine is None:
+            self._engine = create_engine(self._db_connection_string)
+            Base.metadata.create_all(self._engine)
+        return self._engine
+
+    @property
+    def session(self):
+        if self._session is None:
+            self._session = sessionmaker(bind=self.engine)()
+        return self._session
+
+    def list_source_specs(self):
+        yield from self.session.query(SourceSpec).all()
+
+    def get_source_spec(self, uuid):
+        return self.session.query(SourceSpec).filter_by(uuid=uuid).first()
+
+    def put_source_spec(self, owner, module, contents,
+                        uuid=None, ignore_missing=False):
+
+        self._verify_contents(module, contents, ignore_missing)
+
+        spec = None
+        now = datetime.datetime.now()
+        if uuid is None:
+            uuid = uuid4().hex
+        else:
+            spec = self.get_source_spec(uuid)
+        if spec is None:
+            spec = SourceSpec(uuid=uuid, created_at=now)
+
+        spec.owner = owner
+        spec.module = module
+        spec.contents = contents
+        spec.updated_at = now
+
+        s = self.session
+        s.add(spec)
+        s.commit()
+
+        return uuid
