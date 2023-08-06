85f4ac54dc3aa5a83db63906f44712612a4e9149
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:00 +0000

    Add package jsonrpcdb/0.1.4/jsonrpcdb-0.1.4-py2.py3-none-any.whl

---
+++ packages/jsonrpcdb/jsonrpcdb-0.1.4-py2.py3-none-any.whl/jsonrpcdb/auth.py
@@ -0,0 +1,117 @@
+from requests import auth
+import json
+import requests
+
+from .error import OperationalError, check_response
+
+
+FN_USERNAME = 'user'
+"""str: Default field name for username.
+
+Using in params options of json-rpc payload.
+"""
+FN_PASSWORD = 'password'
+"""str: Default field name for password.
+
+Using in params options of json-rpc payload.
+"""
+FN_AUTH = 'auth'
+"""str: Default field name for authentication.
+
+Using in payload of json-rpc.
+"""
+DEFAULT_AUTH_METHOD = 'login'
+"""Default login method."""
+
+
+class TokenAuth(auth.AuthBase):
+    """Auth class for token authentication """
+
+    def __init__(self, conn, username, password,
+                 login_method=DEFAULT_AUTH_METHOD,
+                 fn_auth=FN_AUTH,
+                 fn_username=FN_USERNAME,
+                 fn_password=FN_PASSWORD,
+                 **kwargs):
+        """
+    Create AuthToken instance.
+        Args:
+            conn (Connection): Reference to connection object.
+            username (str): Username.
+            password (str): Password.
+            login_method (str): Login method.
+            fn_auth (str): Auth field name for payload.
+            fn_username (str): Name of username field for params dict in payload.
+            fn_password: Name of password field for params dict in payload.
+        """
+        self.conn = conn
+        self.username = username
+        self.password = password
+        self.login_method = login_method
+        self.fn_auth = fn_auth
+        self.fn_username = fn_username
+        self.fn_password = fn_password
+        self.token = None
+        if conn:
+            self._login()
+
+    def __call__(self, r):
+        """Change body of request.
+
+        Add auth field with auth token.
+
+        Args:
+            r (PreparedRequest): Prepared request.
+
+        Returns:
+            PreparedRequest: Modified request.
+        """
+        if not self.is_auth():
+            self._login()
+        json_body = json.loads(r.body)
+        json_body[self.fn_auth] = self.token
+        r.body = json.dumps(json_body)
+        return r
+
+    def is_auth(self):
+        """Return True if we have token."""
+        return True if self.token else False
+
+    def get_auth_dict(self):
+        """Get auth dictionary.
+
+        Returns:
+            dict: Contain username and password.
+        """
+        return {
+            self.fn_username: self.username,
+            self.fn_password: self.password
+        }
+
+    def set_token(self, token):
+        """Set auth token.
+
+        Args:
+            token (str): Token.
+        """
+        self.token = token
+
+    def _login(self):
+        """Try login and get auth token."""
+        payload = {
+            "method": self.login_method,
+            "params": self.get_auth_dict(),
+            "jsonrpc": "2.0",
+            "id": 0,
+        }
+        headers = {'content-type': 'application/json'}
+        response = requests.post(self.conn.get_url(),
+                                 json.dumps(payload),
+                                 headers=headers)
+        try:
+            response = response.json()
+            check_response(response)
+            self.set_token(response['result'])
+        except ValueError as e:
+            self.set_token(None)
+            raise OperationalError()
---
+++ packages/jsonrpcdb/jsonrpcdb-0.1.4-py2.py3-none-any.whl/jsonrpcdb/error.py
@@ -0,0 +1,83 @@
+try:
+    from exceptions import Exception, StandardError
+except ImportError:
+    # Python 3
+    StandardError = Exception
+
+# Standart json-rpc error codes
+PARSE_ERROR = -32700
+INVALID_REQUEST = -32600
+METHOD_NOT_FOUND = -32601
+INVALID_PARAMS = -32602
+INTERNAL_ERROR = -32603
+
+
+class Error(Exception):
+    """Exception that is the base class of all other error exceptions
+        (not Warning)."""
+
+
+class InterfaceError(Error):
+    """Exception raised for errors that are related to the database
+        interface rather than the database itself."""
+
+
+class DatabaseError(Error):
+    """Exception raised for errors that are related to the
+       database."""
+
+
+class DataError(DatabaseError):
+    """Exception raised for errors that are due to problems with the
+       processed data like division by zero, numeric value out of range,
+       etc."""
+
+
+class OperationalError(DatabaseError):
+    """Exception raised for errors that are related to the database's
+       operation and not necessarily under the control of the programmer,
+       e.g. an unexpected disconnect occurs, the data source name is not
+       found, a transaction could not be processed, a memory allocation
+       error occurred during processing, etc."""
+
+
+class IntegrityError(DatabaseError):
+    """Exception raised when the relational integrity of the database
+       is affected, e.g. a foreign key check fails, duplicate key,
+       etc."""
+
+
+class InternalError(DatabaseError):
+    """Exception raised when the database encounters an internal
+       error, e.g. the cursor is not valid anymore, the transaction is
+       out of sync, etc."""
+
+
+class ProgrammingError(DatabaseError):
+    """Exception raised for programming errors, e.g. table not found
+        or already exists, syntax error in the SQL statement, wrong number
+        of parameters specified, etc."""
+
+
+class NotSupportedError(DatabaseError):
+    """Exception raised in case a method or database API was used
+        which is not supported by the database, e.g. requesting a
+        .rollback() on a connection that does not support transaction or
+        has transactions turned off."""
+
+
+def check_response(response):
+    if 'error' not in response:
+        return
+    error = response['error']
+    code = error.get('code', None)
+    message = error.get('message', '')
+    data = error.get('data', '')
+    if code == PARSE_ERROR or code == INVALID_REQUEST:
+        raise InterfaceError(code, message, data)
+    elif code == METHOD_NOT_FOUND or code == INVALID_PARAMS:
+        raise ProgrammingError(code, message, data)
+    elif code == INTERNAL_ERROR:
+        raise InternalError(code, message, data)
+    else:
+        raise DatabaseError(code, message, data)
