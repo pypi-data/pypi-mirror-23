a4a4d2105b51c3a611d5b710038f64bd4d113446
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:15 +0000

    Add package pybili/0.2.7/pybili-0.2.7.tar.gz

---
+++ packages/pybili/pybili-0.2.7.tar.gz/pybili-0.2.7/setup.py
@@ -0,0 +1,26 @@
+from distutils.core import setup
+
+setup(
+  name = 'pybili',
+  packages = ['pybili'], # this must be the same as the name above
+  version = '0.2.7',
+  description = 'A helper library for bilibili.com',
+  author = 'Kliner',
+  author_email = 'kliner@live.cn',
+  url = 'https://github.com/kliner/pyBili', # use the URL to the github repo
+  download_url = 'https://github.com/kliner/pyBili/archive/0.2.5.tar.gz',
+  keywords = ['bili', 'live', 'danmaku'], # arbitrary keywords
+  classifiers = [],
+
+  install_requires=['requests', 'pymongo', 'emoji_list'],
+
+  entry_points={
+    'console_scripts':[
+      'bili-danmuji = pybili.GUI:main',
+      'bili-dm = pybili.DanMuJi:main',
+      'bili-music = pybili.AutoDianGeJi:main',
+      'bili-sender = pybili.bili_sender:main',
+      'bili-config = pybili.bili_config:main',
+    ]
+  }
+)
