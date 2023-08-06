1c9fb2c84f1a33f6137db6fd0d2d6eae4bb194bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:20 +0000

    Add package easyPyMySQL/1.1/easyPyMySQL-1.1.tar.gz

---
+++ packages/easyPyMySQL/easyPyMySQL-1.1.tar.gz/easyPyMySQL-1.1/setup.py
@@ -0,0 +1,30 @@
+﻿from setuptools import setup, find_packages
+from distutils.core import setup
+# install_requires=["bs4","pymysql"]
+# include_package_data=True
+# package_data = ['demo']
+
+setup(
+    name="easyPyMySQL",
+    version="1.1",
+    description="简单易用的数据库ORM模块",
+    url='https://github.com/AkiYama-Ryou/easyPyMySQL',
+    author="ChenRuohan",
+    author_email='crh51306@gmail.com',
+    packages= ['easyPyMySQL'],
+    )
+
+# setup(
+#     name='easyPyMySQL',
+#     version='1.0',
+#     keywords = ('mysql', 'orm'),
+#     description='简单易用的数据库ORM模块',
+#     license='Free',
+#     author='ChenRuohan',
+#     author_email='crh51306@gmail.com',
+#     url='https://github.com/AkiYama-Ryou/easyPyMySQL',
+#     platforms = 'mysql',
+#     packages = find_packages(),
+#     package_dir = {'demo':'demo'}
+# )
+
