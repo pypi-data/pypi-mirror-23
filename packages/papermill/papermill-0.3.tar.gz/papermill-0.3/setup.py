a601f90e64cc65347c49324f73fb185244c542ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:14:17 +0000

    Add package papermill/0.3/papermill-0.3.tar.gz

---
+++ packages/papermill/papermill-0.3.tar.gz/papermill-0.3/setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#!/usr/bin/env python
+
+
+from os.path import exists
+from setuptools import setup
+import versioneer
+
+setup(name='papermill',
+      version=versioneer.get_version(),
+      cmdclass=versioneer.get_cmdclass(),
+      description='Map Reduce for Notebooks',
+      author='nteract contributors',
+      author_email='jupyter@googlegroups.com',
+      license='BSD',
+      keywords="jupyter mapreduce",
+      long_description=(open('README.rst').read() if exists('README.rst') else ''),
+      url='https://github.com/nteract/papermill',
+      packages=['papermill'],
+      entry_points={
+              'console_scripts': [
+                  'papermill = papermill.cli:papermill'
+              ]
+          }
+     )
