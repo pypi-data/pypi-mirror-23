369c2d7348bedf2781535ac0a459fd9980a6c22c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:39 +0000

    Add package ascii-game/0.0.3/ascii-game-0.0.3.tar.gz

---
+++ packages/ascii-game/ascii-game-0.0.3.tar.gz/ascii-game-0.0.3/setup.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+setup(
+    name='ascii-game',
+    version='0.0.3',
+    description='Terminal-Based Game Framework',
+    url='https://github.com/lauryndbrown/AsciiGame',
+    author='Lauryn Brown',
+    author_email='lauryndbrown@gmail.com',
+    license='GNU GPLv3',
+    packages=find_packages(),
+    install_requires=[
+        'pillow',
+        'Click',
+    ],
+    entry_points={
+        'console_scripts':[
+            'ascii-game=ascii_game.ascii_game:cli',
+        ]
+    }, 
+    include_package_data=True,
+    zip_safe=False)
