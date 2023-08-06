96718539673eb2b0a83965da24eb176f042851bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:33:55 +0000

    Add package unipy-db/0.0.5/unipy_db-0.0.5.tar.gz

---
+++ packages/unipy-db/unipy_db-0.0.5.tar.gz/unipy_db-0.0.5/unipy_db/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jan  8 03:46:03 2017
+
+@author: Young Ju Kim
+"""
+
+
+from unipy_db import database
+
+from unipy_db.database import *
+
+__all__ = []
+__all__ += database.__all__
