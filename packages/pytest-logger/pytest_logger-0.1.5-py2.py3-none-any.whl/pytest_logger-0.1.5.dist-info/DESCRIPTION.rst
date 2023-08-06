92e1c9485a09098a59392aeb09a3c6e71f176994
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:17 +0000

    Add package pytest-logger/0.1.5/pytest_logger-0.1.5-py2.py3-none-any.whl

---
+++ packages/pytest-logger/pytest_logger-0.1.5-py2.py3-none-any.whl/pytest_logger-0.1.5.dist-info/DESCRIPTION.rst
@@ -0,0 +1,114 @@
+.. image:: https://img.shields.io/pypi/v/pytest-logger.svg
+    :target: https://pypi.python.org/pypi/pytest-logger
+    :alt: Pypi Package Version
+.. image:: https://img.shields.io/pypi/pyversions/pytest-logger.svg
+    :target: https://pypi.python.org/pypi/pytest-logger
+    :alt: Supported Python Versions
+.. image:: https://readthedocs.org/projects/pytest-logger/badge
+    :target: http://pytest-logger.readthedocs.io/en/latest
+    :alt: Documentation Status
+.. image:: https://coveralls.io/repos/github/aurzenligl/pytest-logger/badge.svg?branch=master
+    :target: https://coveralls.io/github/aurzenligl/pytest-logger?branch=master
+    :alt: Coverage Status
+.. image:: https://travis-ci.org/aurzenligl/pytest-logger.svg?branch=master
+    :target: https://travis-ci.org/aurzenligl/pytest-logger
+    :alt: Travis-CI Build Status
+.. image:: https://ci.appveyor.com/api/projects/status/1h4cy8tk220pn03o?svg=true
+    :target: https://ci.appveyor.com/project/aurzenligl/pytest-logger
+    :alt: AppVeyor-CI Build Status
+
+Pytest-logger is a pytest plugin configuring handlers for loggers from Python logging module.
+
+You can install pytest-logger via ``pip`` from ``PyPI``::
+
+    $ [sudo] pip install pytest-logger
+
+Plugin puts logs on per-logger basis to:
+
+- standard output,
+- files within log-specific directory under pytest's ``tmpdir_factory`` session directory.
+
+You can setup plugin using hooks::
+
+    #conftest.py
+    import os
+
+    def pytest_logger_stdoutloggers(item):
+        return ['foo', 'bar']
+
+    def pytest_logger_fileloggers(item):
+        return ['foo', 'bar']
+
+    def pytest_logger_logdirlink(config):
+        return os.path.join(os.path.dirname(__file__), 'mylogs')
+
+have logging tests or libraries (including fixtures)::
+
+    #test_something.py
+    import pytest
+    import logging
+
+    foo = logging.getLogger('foo')
+    foo.setLevel(logging.INFO)
+
+    bar = logging.getLogger('bar')
+
+    @pytest.yield_fixture(scope='session')
+    def session_thing():
+        foo.info('constructing session thing')
+        yield
+        foo.info('destroying session thing')
+
+    @pytest.yield_fixture
+    def testcase_thing():
+        foo.info('constructing testcase thing')
+        yield
+        foo.info('destroying testcase thing')
+
+    def test_one(session_thing, testcase_thing):
+        foo.info('one executes')
+        bar.warning('this test does nothing aside from logging')
+
+    def test_two(session_thing, testcase_thing):
+        foo.info('two executes')
+        bar.warning('neither does this')
+
+and expect output in terminal (if not captured)::
+
+    $ py.test -s -v
+    (...)
+    test_something.py::test_one
+    00:00.001 inf foo: constructing session thing
+    00:00.001 inf foo: constructing testcase thing
+    00:00.002 inf foo: one executes
+    00:00.002 wrn bar: this test does nothing aside from logging
+    PASSED
+    00:00.002 inf foo: destroying testcase thing
+
+    test_something.py::test_two
+    00:00.000 inf foo: constructing testcase thing
+    00:00.000 inf foo: two executes
+    00:00.000 wrn bar: neither does this
+    PASSED
+    00:00.001 inf foo: destroying testcase thing
+    00:00.001 inf foo: destroying session thing
+    (...)
+
+and - the same - in filesystem::
+
+    $ file mylogs
+    mylogs: symbolic link to `/tmp/pytest-of-aurzenligl/pytest-48/logs'
+
+    $ tree mylogs
+    mylogs
+    `-- test_something.py
+        |-- test_one
+        |   |-- bar
+        |   `-- foo
+        `-- test_two
+            |-- bar
+            `-- foo
+
+Distributed under the terms of the ``MIT`` license, pytest-logger is free and open source software.
+
+
---
+++ packages/pytest-logger/pytest_logger-0.1.5-py2.py3-none-any.whl/pytest_logger/plugin.py
@@ -0,0 +1,258 @@
+import os
+import sys
+import re
+import py
+import pytest
+import logging
+import time
+import datetime
+
+PY2 = sys.version_info[0] == 2
+
+if PY2:
+    string_type = basestring
+else:
+    string_type = str
+
+
+def pytest_addoption(parser):
+    """Add options to control logger"""
+    parser.addini(
+        name='logger_logsdir',
+        help='base directory with log files for file loggers [basetemp]',
+        default=None,
+    )
+    parser.getgroup('logger').addoption('--logger-logsdir', dest='logger_logsdir')
+
+
+def pytest_configure(config):
+    config.pluginmanager.register(LoggerPlugin(config), '_logger')
+
+
+def pytest_addhooks(pluginmanager):
+    pluginmanager.add_hookspecs(LoggerHookspec)
+
+
+class LoggerPlugin(object):
+    def __init__(self, config):
+        self.config = config
+        self.logdirlinks = config.hook.pytest_logger_logdirlink(config=config)
+        self._logsdir = None
+
+    def logsdir(self):
+        ldir = self._logsdir
+        if ldir:
+            return ldir
+        logger_logsdir = self.config.getoption('logger_logsdir')
+        if not logger_logsdir:
+            logger_logsdir = self.config.getini('logger_logsdir')
+        if logger_logsdir:
+            ldir = _make_logsdir_dir(logger_logsdir)
+        else:
+            ldir = _make_logsdir_tmpdir(self.config._tmpdirhandler)
+
+        self._logsdir = ldir
+
+        for link in self.logdirlinks:
+            _refresh_link(str(ldir), link)
+
+        return ldir
+
+    def pytest_runtest_setup(self, item):
+        def to_loggers(configs_lists):
+            def to_logger_and_level(cfg):
+                if isinstance(cfg, string_type):
+                    name, level = cfg, logging.NOTSET
+                else:
+                    name, level = cfg
+                logger = logging.getLogger(name)
+                return logger, level
+            return [to_logger_and_level(cfg) for configs in configs_lists for cfg in configs]
+
+        stdoutloggers = to_loggers(item.config.hook.pytest_logger_stdoutloggers(item=item))
+        fileloggers = to_loggers(item.config.hook.pytest_logger_fileloggers(item=item))
+        item._logger = state = LoggerState(plugin=self,
+                                           item=item,
+                                           stdoutloggers=stdoutloggers,
+                                           fileloggers=fileloggers)
+        state.on_setup()
+
+    def pytest_runtest_teardown(self, item, nextitem):
+        logger = getattr(item, '_logger', None)
+        if logger:
+            logger.on_teardown()
+
+    def pytest_runtest_makereport(self, item, call):
+        logger = getattr(item, '_logger', None)
+        if logger:
+            if call.when == 'teardown':
+                logger.on_makereport()
+
+
+class LoggerState(object):
+    def __init__(self, plugin, item, stdoutloggers, fileloggers):
+        self._put_newlines = bool(item.config.option.capture == 'no' and stdoutloggers)
+        self.handlers = _make_handlers(stdoutloggers, fileloggers, item)
+
+    def put_newline(self):
+        if self._put_newlines:
+            sys.stdout.write('\n')
+
+    def on_setup(self):
+        self.put_newline()
+        _enable(self.handlers)
+
+    def on_teardown(self):
+        self.put_newline()
+
+    def on_makereport(self):
+        _disable(self.handlers)
+
+
+class LoggerHookspec(object):
+    def pytest_logger_stdoutloggers(self, item):
+        """ called before testcase setup. If implemented, given loggers
+        will emit their output to terminal output.
+
+        :arg item: test item for which handlers are to be setup.
+
+        :return list: List should contain logger name strings
+                      or tuples with logger name string and logging level.
+        """
+
+    def pytest_logger_fileloggers(self, item):
+        """ called before testcase setup. If implemented, given loggers
+        will emit their output to files within logs temporary directory.
+
+        :arg item: test item for which handlers are to be setup.
+
+        :return list: List should contain logger name strings
+                      or tuples with logger name string and logging level.
+        """
+
+    def pytest_logger_logdirlink(self, config):
+        """ called after cmdline options parsing.
+        If implemented, symlink to logs directory will be created.
+
+        :arg config: pytest config object, holds e.g. options
+
+        :return string: Absolute path of requested link to logs directory.
+        """
+
+
+class Formatter(logging.Formatter):
+    short_level_names = {
+        logging.FATAL: 'ftl',
+        logging.ERROR: 'err',
+        logging.WARN: 'wrn',
+        logging.INFO: 'inf',
+        logging.DEBUG: 'dbg',
+    }
+
+    def __init__(self, fmt):
+        logging.Formatter.__init__(self, fmt)
+        self._start = time.time()
+
+    def formatTime(self, record, datefmt=None):
+        ct = record.created - self._start
+        dt = datetime.datetime.utcfromtimestamp(ct)
+        return dt.strftime("%M:%S.%f")[:-3]  # omit useconds, leave mseconds
+
+    def format(self, record):
+        record.levelshortname = Formatter.short_level_names.get(record.levelno,
+                                                                'l%s' % record.levelno)
+        return logging.Formatter.format(self, record)
+
+
+@pytest.fixture
+def logdir(request):
+    return _make_logdir(request._pyfuncitem)
+
+
+def _sanitize(filename):
+    filename = filename.replace('::()::', '.')
+    filename = filename.replace('::', '/')
+    filename = re.sub(r'\[(.+)\]', r'-\1', filename)
+    return filename
+
+
+def _refresh_link(source, link_name):
+    try:
+        os.unlink(link_name)
+    except OSError:
+        pass
+    try:
+        os.symlink(source, link_name)
+    except (OSError, AttributeError, NotImplementedError):
+        pass
+
+
+def _make_logsdir_tmpdir(tmpdirhandler):
+    logsdir = tmpdirhandler.getbasetemp()
+    if logsdir.basename.startswith('popen-gw'):
+        logsdir = logsdir.join('..')
+    logsdir = logsdir.join('logs').ensure(dir=1)
+    return logsdir
+
+
+def _make_logsdir_dir(dstname, cleandir=True):
+    logsdir = py.path.local(dstname)
+    if cleandir:
+        if logsdir.check():
+            logsdir.remove()
+        logsdir.mkdir()
+    return logsdir
+
+
+def _make_logdir(item):
+    plugin = item.config.pluginmanager.getplugin('_logger')
+    return plugin.logsdir().join(_sanitize(item.nodeid)).ensure(dir=1)
+
+
+def _enable(handlers):
+    for hdlr in handlers:
+        hdlr.logger.addHandler(hdlr)
+
+
+def _disable(handlers):
+    for hdlr in handlers:
+        hdlr.logger.removeHandler(hdlr)
+        hdlr.close()
+
+
+def _make_handlers(stdoutloggers, fileloggers, item):
+    FORMAT = '%(asctime)s %(levelshortname)s %(name)s: %(message)s'
+    fmt = Formatter(fmt=FORMAT)
+    handlers = []
+    if stdoutloggers:
+        handlers += _make_stdout_handlers(stdoutloggers, fmt)
+    if fileloggers:
+        logdir = _make_logdir(item)
+        handlers += _make_file_handlers(fileloggers, fmt, logdir)
+    return handlers
+
+
+def _make_stdout_handlers(loggers, fmt):
+    def make_handler(logger_and_level, fmt):
+        logger, level = logger_and_level
+        handler = logging.StreamHandler(sys.stdout)
+        handler.setFormatter(fmt)
+        handler.setLevel(level)
+        handler.logger = logger
+        return handler
+
+    return [make_handler(lgr, fmt) for lgr in loggers]
+
+
+def _make_file_handlers(loggers, fmt, logdir):
+    def make_handler(logdir, logger_and_level, fmt):
+        logger, level = logger_and_level
+        name = logger.name == 'root' and 'logs' or logger.name
+        logfile = str(logdir.join(name))
+        handler = logging.FileHandler(filename=logfile, mode='w', delay=True)
+        handler.setFormatter(fmt)
+        handler.setLevel(level)
+        handler.logger = logger
+        return handler
+
+    return [make_handler(logdir, lgr, fmt) for lgr in loggers]
