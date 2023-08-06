c8795c6af941f202d5044c60785657ba192f2779
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:14:10 +0000

    Add package gecaso/0.1.1/gecaso-0.1.1.tar.gz

---
+++ packages/gecaso/gecaso-0.1.1.tar.gz/gecaso-0.1.1/gecaso/__init__.py
@@ -0,0 +1,5 @@
+from gecaso.cache import cached
+from gecaso.storage import BaseStorage, LocalMemoryStorage
+
+
+__all__ = ['cached', 'BaseStorage', 'LocalMemoryStorage']
---
+++ packages/gecaso/gecaso-0.1.1.tar.gz/gecaso-0.1.1/gecaso/cache.py
@@ -0,0 +1,37 @@
+import asyncio
+
+from . import utils
+from . import storage
+
+
+def cached(cache_storage, loop=None, **params):
+    loop = loop or asyncio.get_event_loop()
+    if not isinstance(cache_storage, storage.BaseStorage):
+        raise ValueError('Provided storage is not subclass of base storage')
+    return _cached(cache_storage, loop, **params)
+
+
+def _cached(cache_storage, loop, **params):
+    storage_get = utils.asyncify(cache_storage.get)
+    storage_set = utils.asyncify(cache_storage.set)
+
+    def wrapper(function):
+        async def wrapped_function(*args, **kwargs):
+            key = utils.make_key(function, *args, **kwargs)
+            try:
+                result = await storage_get(key)
+            except KeyError:
+                result = await function(*args, **kwargs)
+                await storage_set(key, result, **params)
+            return result
+
+        def sync_wrapped_function(*args, **kwargs):
+            return loop.run_until_complete(wrapped_function(*args, **kwargs))
+
+        if utils.is_async_function(function):
+            return utils.wrap(function, wrapped_function)
+        else:
+            function = utils.asyncify(function)
+            return utils.wrap(function, sync_wrapped_function)
+
+    return wrapper
