24acade61b47e545a28eb5082c531eb7d0d603e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:13:56 +0000

    Add package PyMapRetinotopic/1.0.0/PyMapRetinotopic-1.0.0.tar.gz

---
+++ packages/PyMapRetinotopic/PyMapRetinotopic-1.0.0.tar.gz/PyMapRetinotopic-1.0.0/setup.py
@@ -0,0 +1,56 @@
+__author__ = 'junz'
+
+from setuptools import setup, find_packages
+import io
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+def read(*filenames, **kwargs):
+    encoding = kwargs.get('encoding', 'utf-8')
+    sep = kwargs.get('sep', '\n')
+    buf = []
+    for filename in filenames:
+        with io.open(filename, encoding=encoding) as f:
+            buf.append(f.read())
+    return sep.join(buf)
+
+long_description = read('README.md')
+
+print long_description
+
+def prepend_find_packages(*roots):
+    '''
+    Recursively traverse nested packages under the root directories
+    '''
+    packages = []
+
+    for root in roots:
+        packages += [root]
+        packages += [root + '.' + s for s in find_packages(root)]
+
+    return packages
+
+setup(
+      name='PyMapRetinotopic',
+      version = '1.0.0',
+      url='https://github.com/zhuangjun1981/retinotopic_mapping',
+      author='Jun Zhuang @ Allen Institute for Brain Science',
+      install_requires=['numpy', 'scipy', 'scikit-image', 'tifffile',
+                        'psychopy','PyDAQmx'],
+      author_email='junz@alleninstitute.org',
+      description='retinotopic mapping tools',
+      long_description=long_description,
+      license='GNU',
+      keywords='retinotopic visual neuroscience',
+      packages=prepend_find_packages('retinotopic_mapping'),
+      include_package_data=True,
+      package_data={'':['*.md']},
+      platforms='any',
+      classifiers = [
+                     'Programming Language :: Python',
+                     'Development Status :: 4 - Beta',
+                     'Natural Language :: English',
+                     'Operating System :: OS Independent',
+                     ]
+       )
