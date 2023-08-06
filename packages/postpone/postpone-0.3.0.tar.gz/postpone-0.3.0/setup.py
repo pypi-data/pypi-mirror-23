7c432af5092f82503294d227e05c8d641606f93a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:23 +0000

    Add package postpone/0.3.0/postpone-0.3.0.tar.gz

---
+++ packages/postpone/postpone-0.3.0.tar.gz/postpone-0.3.0/setup.py
@@ -0,0 +1,39 @@
+from distutils.core import setup
+
+with open('README.rst') as fd:
+    long_description = fd.read()
+
+setup(
+    name='postpone',
+    version='0.3.0',
+    py_modules=['postpone'],
+    author = 'Benjamin Le Forestier',
+    author_email = 'benjamin@leforestier.org',
+    url = 'https://github.com/leforestier/postpone',
+    keywords = [
+        "lazy", "string", "i18n", "internationalization", "translation", "translate", "gettext", "ugettext", 
+        "delayed", "evaluation", "message", "strings", "web"],
+    description = """\
+A lazy string implementation that doesn't rely on a global or thread local state.""",
+    long_description = long_description,
+    classifiers = [
+        'Environment :: Web Environment',
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Internationalization',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]  
+)
