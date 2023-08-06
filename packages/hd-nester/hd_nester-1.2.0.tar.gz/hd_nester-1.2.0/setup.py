e4426785a78f9202227e2058942372907e81aade
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:35:04 +0000

    Add package hd-nester/1.2.0/hd_nester-1.2.0.tar.gz

---
+++ packages/hd-nester/hd_nester-1.2.0.tar.gz/hd_nester-1.2.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='hd_nester',
+    version='1.2.0',
+    py_modules=['hd_nester'],
+    author='head_first',
+    author_email='hfpython@headfirstlabs.com',
+    url='http://www.headfirstlabs.com',
+    description='head_first_a simple printer of nested lists'
+)
