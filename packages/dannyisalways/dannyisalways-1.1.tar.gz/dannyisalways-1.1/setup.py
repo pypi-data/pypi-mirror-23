6dc167c2fb8b1436aeb236090501f8e9d73d63ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:50 +0000

    Add package dannyisalways/1.1/dannyisalways-1.1.tar.gz

---
+++ packages/dannyisalways/dannyisalways-1.1.tar.gz/dannyisalways-1.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'dannyisalways',
+    #packages = ['tryanderror'],
+    packages = find_packages(),
+    version = '1.1',
+    description = 'My first project',
+    author = 'ekils',
+    author_email = 'bobobo746@hotmail.com',
+    url = 'https://github.com/ekils/dannyisalways',
+    download_url = 'https://github.com/ekils/dannyisalways.git',
+    keywords = ['Good Project'],
+    classifiers = [],
+
+)
---
+++ packages/dannyisalways/dannyisalways-1.1.tar.gz/dannyisalways-1.1/tryanderror/ttest.py
@@ -0,0 +1,8 @@
+#coding=utf-8
+import numpy as np
+def hello():
+    print 'hello world'
+
+def world(a,b):
+     ap= np.max([a,b])
+     print  ap
