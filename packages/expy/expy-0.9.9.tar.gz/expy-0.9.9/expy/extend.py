07baf111bf7f9e7b7bb8615d4715b122f699048d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:19 +0000

    Add package expy/0.9.9/expy-0.9.9.tar.gz

---
+++ packages/expy/expy-0.9.9.tar.gz/expy-0.9.9/expy/extend.py
@@ -0,0 +1,49 @@
+# coding:utf-8
+from expy import *
+np = shared.np
+
+'"Extend.py" is a play for any kind of plugin function'
+
+def normal_procedure(trial_func, trial_list='trial_list.csv',  practice_list=None, practice_func=None, onset_block=1, instruction_setting='instruction'):
+    # Preload file of stimuli and then process it
+    pre_load_file = readStimuli(trial_list, return_list=False)
+    if 'block' in pre_load_file.columns:
+        shared.blockCount = pre_load_file.block.iloc[-1]
+        single_block=False
+    else:
+        shared.blockCount = 1
+        single_block=True
+
+    # Experiment start
+    getSubjectID(shared.setting['subject_name_query'])
+    instruction(shared.setting[instruction_setting])
+
+    # Practice stage
+    if practice_list:
+        if not practice_func:
+            practice_func = trial_func
+        stimuli = readStimuli(practice_list)
+        for data in stimuli:
+            practice_func(data)
+
+    # Regular stage
+    def block(blockID):
+        if single_block:
+            stimuli = readStimuli(trial_list)
+        else:
+            stimuli = readStimuli(trial_list, query='block==%s' %blockID)                
+
+        alertAndGo(shared.setting['block_start'])
+        
+        result = [trial_func(data) for data in stimuli]
+
+        saveResult(result, blockID, columns=['respKey','RT'], stim=stimuli)
+
+        if blockID < shared.blockCount:
+            restTime()  
+
+    for blockID in range(onset_block, shared.blockCount+1):
+        block(blockID)
+
+    # Experiment end
+    alertAndQuit(shared.setting['experiment_end'])
