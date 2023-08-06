3fa2eca7abc307b02cbd945c3eddc0372bb79ae5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:33:33 +0000

    Add package Flask-Extension/1.0/Flask-Extension-1.0.tar.gz

---
+++ packages/Flask-Extension/Flask-Extension-1.0.tar.gz/Flask-Extension-1.0/setup.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Flask-Extension
+--------------
+
+Demo for flask extension.
+"""
+from setuptools import setup
+
+setup(
+    name='Flask-Extension',
+    version='1.0',
+    url='https://github.com/greyli/flask-extension',
+    license='MIT',
+    author='Grey Li',
+    author_email='withlihui@gmail.com',
+    description='Demo for flask extension.',
+    platforms='any',
+    install_requires=[
+        'Flask'
+    ],
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+
+)
