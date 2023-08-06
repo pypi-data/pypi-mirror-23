67092f1a5cf871b2bf5fe3322303542e460dc730
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:11:26 +0000

    Add package deploy-test-exem/0.1/deploy-test-exem-0.1.tar.gz

---
+++ packages/deploy-test-exem/deploy-test-exem-0.1.tar.gz/deploy-test-exem-0.1/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='deploy-test-exem',
+      version='0.1',
+      description='Sources for distribution test, Change module name',
+      author='yunjk',
+      author_email='yunjk@ex-em.com',
+      url='http://www.ex-em.com',
+	  packages=['deploy_test']
+     )
