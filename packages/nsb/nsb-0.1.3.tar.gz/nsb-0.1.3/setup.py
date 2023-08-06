a7c065f42798a7f42f2a22e8400c4ab7f0b45318
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:29 +0000

    Add package nsb/0.1.3/nsb-0.1.3.tar.gz

---
+++ packages/nsb/nsb-0.1.3.tar.gz/nsb-0.1.3/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+setup(
+    name = 'nsb',
+    packages = ['nsb', 'nsb/gaia', 'nsb/mypycat'],
+    version = '0.1.3',
+    description = 'Draws nightsky allskymaps',
+    long_description = 'Draws nightsky allskymaps corresponding to KRISCIUNAS Model of the Brightness of Moonlight, '
+                'together with star data obtained from the GAIA public data release catalog. '
+                'The result is a 2D Pixel array with physical brightness values for each sky position.',
+    author='Matthias Buechele',
+    author_email='matthias.buechele@fau.de',
+    url='https://git.ecap.work/mbuechele/allskymaps',
+    download_url='https://git.ecap.work/mbuechele/allskymaps/repository/archive.tar?ref=0.0.2',
+    install_requires=['healpy','tqdm', 'ephem', 'astropy', 'scipy', 'numpy', 'matplotlib'],
+    package_dir={'nsb': 'nsb'},
+    package_data={'nsb': ['gaia/ducaticat.txt']},
+    entry_points = {
+        "console_scripts": ['nsb = nsb.nsb:main']
+        },
+)
+# package_data={'nsb': ['gaia/ducaticat.txt','mypycat/cat.csv', 'config.cfg']},
