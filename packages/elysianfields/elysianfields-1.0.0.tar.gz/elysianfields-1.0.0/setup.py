d9e3e2f0575b3c890cd8b6298b9ce0442e78138a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:08:34 +0000

    Add package elysianfields/1.0.0/elysianfields-1.0.0.tar.gz

---
+++ packages/elysianfields/elysianfields-1.0.0.tar.gz/elysianfields-1.0.0/setup.py
@@ -0,0 +1,32 @@
+
+from setuptools import setup
+
+setup(
+    name='elysianfields',
+
+    version='1.0.0',
+
+    packages=['elysianfields'],
+
+    scripts=[],
+
+    package_data={},
+
+    license='Apache 2.0',
+
+    description='Elsyian Fields',
+
+    long_description=open('README.rst').read(),
+
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+
+        'Intended Audience :: Developers',
+
+        'License :: OSI Approved :: Apache Software License',
+
+        'Programming Language :: Python :: 2.7',
+    ],
+
+    install_requires=[]
+)
