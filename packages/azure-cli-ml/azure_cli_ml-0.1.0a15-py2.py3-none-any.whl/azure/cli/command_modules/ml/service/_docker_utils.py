04b66ea76f790557fbc9c537cf2b13301da70aa9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:35:42 +0000

    Add package azure-cli-ml/0.1.0a15/azure_cli_ml-0.1.0a15-py2.py3-none-any.whl

---
+++ packages/azure-cli-ml/azure_cli_ml-0.1.0a15-py2.py3-none-any.whl/azure/cli/command_modules/ml/service/_docker_utils.py
@@ -0,0 +1,102 @@
+# --------------------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for license information.
+# --------------------------------------------------------------------------------------------
+
+
+"""
+Utilities to work with docker.
+
+"""
+
+
+import base64
+import json
+import os
+import docker
+import re
+from azure.cli.core.util import CLIError
+
+
+def check_docker_credentials(acr_home, acr_user, acr_pw, verbose):
+    """
+    Create a ~/.docker/config.json file if none exists.
+    Ensure that the config.json file has credentials for the ACR passed in.
+    :param acr_home: The login server URL of the ACR
+    :param acr_user: The username to access the ACR
+    :param acr_pw: The password for the above username
+    :param verbose: Whether to print verbose output or not
+    :returns None
+    """
+    if not os.path.exists(os.path.expanduser('~/.docker/config.json')):
+        if verbose:
+            print('Docker config not found. Creating new config file with ACR credentials.')
+        if not os.path.exists(os.path.expanduser('~/.docker')):
+            os.mkdir(os.path.expanduser('~/.docker'))
+
+        add_docker_credentials(acr_home, acr_user, acr_pw, verbose)
+        return
+    else:
+        try:
+            with open(os.path.expanduser('~/.docker/config.json'), 'r') as docker_config_file:
+                docker_config = docker_config_file.read()
+        except IOError:
+            print('Error configuring docker for your ACR. Please try the following:')
+            print('docker login {}'.format(acr_home))
+            return
+        try:
+            docker_config = json.loads(docker_config)
+        except ValueError:
+            print('Your docker configuration file is corrupt. Please try the following:')
+            print('docker login {}'.format(acr_home))
+            return
+        if 'auths' in docker_config and acr_home in docker_config['auths']:
+            return
+        else:
+            connection_string = base64.b64encode(bytearray(acr_user + ':' + acr_pw, 'utf-8'))
+            docker_auth = {'auth': connection_string.decode('ascii')}
+            docker_config['auths'][acr_home] = docker_auth
+            if verbose:
+                print(json.dumps(docker_config))
+            with open(os.path.expanduser('~/.docker/config.json'), 'w+') as dockerconfig_file:
+                dockerconfig_file.write(json.dumps(docker_config))
+
+
+def add_docker_credentials(acr_home, acr_user, acr_pw, verbose):
+    """
+    Adds the given credentials to the docker config file.
+    :param acr_home: The login server URL of the ACR
+    :param acr_user: The username to access the ACR
+    :param acr_pw: The password for the above username
+    :param verbose: Whether to print verbose output or not
+    :returns None
+    """
+    connection_string = base64.b64encode(bytearray(acr_user + ':' + acr_pw, 'utf-8'))
+    docker_auths = {acr_home: {'auth': connection_string.decode('ascii')}}
+    docker_config = {'auths': docker_auths}
+    if verbose:
+        print(json.dumps(docker_config))
+    with open(os.path.expanduser('~/.docker/config.json'), 'w+') as dockerconfig_file:
+        dockerconfig_file.write(json.dumps(docker_config))
+
+
+def get_docker_client():
+    try:
+        client = docker.from_env()
+    except docker.errors.DockerException as exc:
+        msg = 'Failed to create Docker client: {}'.format(exc)
+        raise CLIError(msg)
+    try:
+        client.containers.list()
+    except docker.errors.APIError as exc:
+        api_regex = r'server API version: (?P<server_version>[^\)]+)'
+        s = re.search(api_regex, str(exc))
+        if not s:
+            raise CLIError(str(exc))
+
+        client = docker.DockerClient()
+        # specify version 1.24 for now, as that's what runs on centOS DSVM
+        client.api = docker.APIClient(base_url='unix://var/run/docker.sock',
+                                      version=s.group('server_version'))
+
+    return client
