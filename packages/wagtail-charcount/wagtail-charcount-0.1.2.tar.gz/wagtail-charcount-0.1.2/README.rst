84cccb3ab9e5b78e5990e6d1dca1086e13844785
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:56:58 +0000

    Add package wagtail-charcount/0.1.2/wagtail-charcount-0.1.2.tar.gz

---
+++ packages/wagtail-charcount/wagtail-charcount-0.1.2.tar.gz/wagtail-charcount-0.1.2/README.rst
@@ -0,0 +1,22 @@
+===============
+wagtail-charcount
+===============
+
+A wagtail character and word counting plugin for RichTextFields.
+
+Installing
+==========
+
+Install using pip::
+
+    pip install wagtail-charcount
+
+It works with Wagtail 1.0 and upwards.
+
+Using
+=====
+To use this plugin firstly you will need to add ``wagtailcharcount`` to your installed apps.
+
+This will add a character/word count next to where your help text would normally appear, this also works fine with help text.
+
+.. image:: https://i.imgur.com/5NoSHBz.png
---
+++ packages/wagtail-charcount/wagtail-charcount-0.1.2.tar.gz/wagtail-charcount-0.1.2/wagtailcharcount/static/hallo_charcount.js
@@ -0,0 +1,107 @@
+$(function (){
+	// Rich text
+	var rich_text_areas = $('.rich_text_area');
+	rich_text_areas.each(function(){
+		var specificRichText = $(this)
+		var parentElement = specificRichText.parent().parent().parent().parent();
+		var helpBox = parentElement.find('.object-help');
+		if (helpBox.html()) {
+			helpBox.addClass('charcount');
+		} else {
+			var helpBoxExists = true
+			var elem = parentElement.append("<div style='opacity:1;' class='object-help help charcount'></div>");
+			var helpBox = parentElement.find('.charcount');
+			helpBox.hide();
+		}
+		var whiteSpace = /\s\s+/gm;
+		var wordsRegex = /\s+/gi;
+		var charCountElemText = helpBox.text();
+		var maxChars = specificRichText.find('textarea').attr('maxlength');
+
+		$(specificRichText.find('.richtext')).bind('hallomodified', function(event) {
+			var text = event.currentTarget.innerText;
+			var textNoWhitespace = text.replace(whiteSpace, ' ');
+			var textWordCount = textNoWhitespace.trim().replace(wordsRegex, ' ').split(' ').length;
+			var textCharCount = textNoWhitespace.length - 1;
+			var backendCount = $(specificRichText).find('.richtext').html().length;
+			if (charCountElemText.length > 1) {
+				helpBox.css({opacity :1});
+				helpBox.html(
+					charCountElemText + '<br>' +
+					"<br>" + textWordCount + " words" +
+					"<br><span class='count'>" + backendCount + ' / ' + maxChars + '</span> characters');
+				if (backendCount > maxChars) {
+					$(helpBox.find('.count')).css({color: 'red'})
+				} else if (backendCount + 20  > maxChars) {
+					$(helpBox.find('.count')).css({color: 'orange'})
+				}
+			} else {
+				helpBox.show();
+				helpBox.html(
+					textWordCount + " words" +
+					"<br><span class='count'>" + backendCount + ' / ' + maxChars + '</span> characters');
+				if (backendCount  > maxChars) {
+					$(helpBox.find('.count')).css({color: 'red'})
+				} else if (backendCount + 20  > maxChars) {
+					$(helpBox.find('.count')).css({color: 'orange'})
+				}
+			}
+		});
+	})
+	// Normal text fields
+	var text_fields = $($('form').find('.field.char_field').not('.rich_text_area'));
+	text_fields.each(function(){
+		var specificTextBox = $(this)
+		var parentElement = specificTextBox.parent().parent().parent().parent();
+		var helpBox = parentElement.find('.object-help');
+		if (helpBox.html()) {
+			helpBox.addClass('charcount');
+		} else {
+			var helpBoxExists = true
+			var elem = parentElement.append("<div style='opacity:1;' class='object-help help charcount'></div>");
+			var helpBox = parentElement.find('.charcount');
+			helpBox.hide();
+		}
+		var whiteSpace = /\s\s+/gm;
+		var wordsRegex = /\s+/gi;
+		var charCountElemText = helpBox.text();
+		var maxChars = specificTextBox.find('input').attr('maxlength');
+
+		$(specificTextBox.find('input')).bind('input propertychange', function() {
+			var text = this.value;
+			var textNoWhitespace = text.replace(whiteSpace, ' ');
+			var textCharCount = textNoWhitespace.length;
+			if (textCharCount == 0) {
+				var textWordCount = 0
+			} else {
+				var textWordCount = textNoWhitespace.trim().replace(wordsRegex, ' ').split(' ').length;
+			}
+			if (charCountElemText.length > 1) {
+				helpBox.css({opacity :1});
+				var helpBoxContent = (charCountElemText + '<br>' +
+					"<br>" + textWordCount + " words" +
+					"<br><span class='count'>" + textCharCount + ' / ' + maxChars + '</span> characters');
+
+				helpBox.html(helpBoxContent);
+				if (textCharCount > maxChars) {
+					$(helpBox.find('.count')).css({color: 'red'})
+				} else if (textCharCount + 20  > maxChars) {
+					$(helpBox.find('.count')).css({color: 'orange'})
+				}
+			} else {
+				helpBox.show();
+				helpBox.html(
+					textWordCount + " words" +
+					"<br><span class='count'>" + textCharCount + ' / ' + maxChars + '</span> characters'
+				);
+				if (textCharCount > maxChars) {
+					$(helpBox.find('.count')).css({color: 'red'})
+				} else if (textCharCount + 20  > maxChars) {
+					$(helpBox.find('.count')).css({color: 'orange'})
+				}
+			}
+		});
+	})
+
+
+});
