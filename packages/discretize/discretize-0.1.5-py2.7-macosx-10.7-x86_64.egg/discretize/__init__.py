5edf6505598261f409b0cc9fedcc96a47419dded
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:09:34 +0000

    Add package discretize/0.1.5/discretize-0.1.5-py2.7-macosx-10.7-x86_64.egg

---
+++ packages/discretize/discretize-0.1.5-py2.7-macosx-10.7-x86_64.egg/discretize/__init__.py
@@ -0,0 +1,11 @@
+from discretize.BaseMesh import BaseMesh
+from discretize.TensorMesh import TensorMesh
+from discretize.CylMesh import CylMesh
+from discretize.CurvilinearMesh import CurvilinearMesh
+from discretize.TreeMesh import TreeMesh
+from discretize import Tests
+
+__version__   = '0.1.5'
+__author__    = 'SimPEG Team'
+__license__   = 'MIT'
+__copyright__ = '2013 - 2017, SimPEG Developers, http://simpeg.xyz'
