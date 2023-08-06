80cd4313fb36310e1f30135b494de7e5fa14ecf6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:56:57 +0000

    Add package py-config-parser/1.2/py-config-parser-1.2.tar.gz

---
+++ packages/py-config-parser/py-config-parser-1.2.tar.gz/py-config-parser-1.2/setup.py
@@ -0,0 +1,32 @@
+"""A setuptools based setup module.
+See:
+https://packaging.python.org/en/latest/distributing.html
+https://github.com/pypa/sampleproject
+"""
+from setuptools import setup, find_packages
+
+setup(
+    name='py-config-parser',
+    version='1.2',
+    description='Simple python json configuration file parser',
+    url='https://github.com/damirazo/py-config-parser',
+    author='damirazo',
+    author_email='me@damirazo.ru',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Code Generators',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.0',
+        'Programming Language :: Python :: 3.1',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+    keywords='config parser json',
+    packages=find_packages(),
+)
