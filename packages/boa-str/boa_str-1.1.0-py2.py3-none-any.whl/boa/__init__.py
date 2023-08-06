05e669a86f8e891b47fc2ee7c766f270ce55eac3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:13:52 +0000

    Add package boa-str/1.1.0/boa_str-1.1.0-py2.py3-none-any.whl

---
+++ packages/boa-str/boa_str-1.1.0-py2.py3-none-any.whl/boa/__init__.py
@@ -0,0 +1,32 @@
+"""
+Convert strings to snakecase.
+"""
+
+import re
+
+spaces_re = re.compile(r'[\s\-]+')
+strip_re = re.compile(r'[^\w]+')
+first_cap_re = re.compile(r'(.)([A-Z][a-z]+)')
+all_cap_re = re.compile(r'([a-z0-9])([A-Z])')
+dedupe_re = re.compile(r'\_+')
+
+def constrict(string):
+    """Convert the input string to snakecase + lowercase."""
+
+    if string is None:
+        return ''
+
+    # Whitespace to underscores
+    output = spaces_re.sub('_', string)
+
+    # Strip non-alphanumeric/underscore chars
+    output = strip_re.sub('', output)
+
+    # Snake case
+    output = first_cap_re.sub(r'\1_\2', output)
+    output = all_cap_re.sub(r'\1_\2', output).lower()
+
+    # Remove multiple sequential underscores
+    output = dedupe_re.sub('_', output)
+
+    return output
