8a73a9e15762516bdd376a111f3f91ce02fb071b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:13:30 +0000

    Add package wagtail-flags/2.0.5/wagtail-flags-2.0.5.tar.gz

---
+++ packages/wagtail-flags/wagtail-flags-2.0.5.tar.gz/wagtail-flags-2.0.5/setup.py
@@ -0,0 +1,42 @@
+from setuptools import find_packages, setup
+
+try:
+    import pypandoc
+    long_description = pypandoc.convert('README.md', 'rst')
+except (IOError, ImportError):
+    long_description = open('README.md', 'r').read()
+
+
+install_requires = [
+    'Django>=1.8,<1.12',
+    'wagtail>=1.8,<1.11',
+]
+
+
+testing_extras = [
+    'mock>=2.0.0',
+    'coverage>=3.7.0',
+    'flake8>=2.2.0',
+]
+
+
+setup(
+    name='wagtail-flags',
+    url='https://github.com/cfpb/wagtail-flags',
+    author='CFPB',
+    author_email='tech@cfpb.gov',
+    description='Feature flags for Wagtail sites',
+    long_description=long_description,
+    license='CC0',
+    version='2.0.5',
+    include_package_data=True,
+    packages=find_packages(),
+    install_requires=install_requires,
+    extras_require={
+        'testing': testing_extras,
+    },
+    classifiers=[
+        'License :: Public Domain',
+        'License :: CC0 1.0 Universal (CC0 1.0) Public Domain Dedication'
+    ]
+)
