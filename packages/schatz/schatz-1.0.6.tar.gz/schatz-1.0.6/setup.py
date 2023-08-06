84e72f130afad916e899a3ea1095aa6a6a734372
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:17 +0000

    Add package schatz/1.0.6/schatz-1.0.6.tar.gz

---
+++ packages/schatz/schatz-1.0.6.tar.gz/schatz-1.0.6/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='schatz',
+    version='1.0.6',
+    license='Apache 2.0',
+    maintainer='Egor Litvinenko',
+    maintainer_email='e.v.litvinenko.1@gmail.com',
+    packages=['schatz'],
+    description='Schatz integration package for Python 3',
+    install_requires=[
+        'requests',
+        'SQLAlchemy',
+        'schatz-sqlalchemy-clickhouse',
+    ]
+)
