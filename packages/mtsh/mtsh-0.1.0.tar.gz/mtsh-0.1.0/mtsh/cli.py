a8ff0c7c3a79ad3249642034afc5a2999d883d46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:31 +0000

    Add package mtsh/0.1.0/mtsh-0.1.0.tar.gz

---
+++ packages/mtsh/mtsh-0.1.0.tar.gz/mtsh-0.1.0/mtsh/cli.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+"""Console script for mtsh."""
+
+import click
+
+
+@click.command()
+def main(args=None):
+    """Console script for mtsh."""
+    click.echo("Replace this message by putting your code into "
+               "mtsh.cli.main")
+    click.echo("See click documentation at http://click.pocoo.org/")
+
+
+if __name__ == "__main__":
+    main()
---
+++ packages/mtsh/mtsh-0.1.0.tar.gz/mtsh-0.1.0/setup.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""The setup script."""
+
+from setuptools import setup, find_packages
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read()
+
+requirements = [
+    'Click>=6.0',
+    # TODO: put package requirements here
+]
+
+setup_requirements = [
+    # TODO(zdzielinski): put setup requirements (distutils extensions, etc.) here
+]
+
+test_requirements = [
+    # TODO: put package test requirements here
+]
+
+setup(
+    name='mtsh',
+    version='0.1.0',
+    description="multi server ssh interface",
+    long_description=readme + '\n\n' + history,
+    author="Zachariah Dzielinski",
+    author_email='zachdzielinski@gmail.com',
+    url='https://github.com/zdzielinski/mtsh',
+    packages=find_packages(include=['mtsh']),
+    entry_points={
+        'console_scripts': [
+            'mtsh=mtsh.cli:main'
+        ]
+    },
+    include_package_data=True,
+    install_requires=requirements,
+    license="GNU General Public License v3",
+    zip_safe=False,
+    keywords='mtsh',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Natural Language :: English',
+        "Programming Language :: Python :: 2",
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+    test_suite='tests',
+    tests_require=test_requirements,
+    setup_requires=setup_requirements,
+)
