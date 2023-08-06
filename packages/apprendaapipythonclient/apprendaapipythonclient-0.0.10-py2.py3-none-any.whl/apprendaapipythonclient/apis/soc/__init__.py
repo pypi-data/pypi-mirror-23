6eedae03033d08e5388d20eee7f39b85331c59a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:13:42 +0000

    Add package apprendaapipythonclient/0.0.10/apprendaapipythonclient-0.0.10-py2.py3-none-any.whl

---
+++ packages/apprendaapipythonclient/apprendaapipythonclient-0.0.10-py2.py3-none-any.whl/apprendaapipythonclient/apis/soc/__init__.py
@@ -0,0 +1,24 @@
+from __future__ import absolute_import
+
+# import apis into api package
+from .add_ons_api import AddOnsApi
+from .applications_api import ApplicationsApi
+from .audit_records_api import AuditRecordsApi
+from .bootstrap_policies_api import BootstrapPoliciesApi
+from .clouds_api import CloudsApi
+from .clusters_api import ClustersApi
+from .components_api import ComponentsApi
+from .custom_properties_api import CustomPropertiesApi
+from .deployment_policies_api import DeploymentPoliciesApi
+from .encryption_api import EncryptionApi
+from .extracts_api import ExtractsApi
+from .hosts_api import HostsApi
+from .logs_api import LogsApi
+from .new_integration_api import NewIntegrationApi
+from .node_state_api import NodeStateApi
+from .nodes_api import NodesApi
+from .platform_databases_api import PlatformDatabasesApi
+from .registry_api import RegistryApi
+from .services_api import ServicesApi
+from .versions_api import VersionsApi
+from .workloads_api import WorkloadsApi
