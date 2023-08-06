cbddde2bcf1a018885bdaf87ae7d30e24876b969
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:10:32 +0000

    Add package otree-core/1.4.0b4/otree-core-1.4.0b4.tar.gz

---
+++ packages/otree-core/otree-core-1.4.0b4.tar.gz/otree-core-1.4.0b4/otree/__init__.py
@@ -0,0 +1,12 @@
+# REMEMBER TO ALSO UPDATE THE PROJECT TEMPLATE
+__version__ = '1.4.0b4'
+
+default_app_config = 'otree.apps.OtreeConfig'
+
+
+# =============================================================================
+# FUNCTIONS
+# =============================================================================
+
+def get_version():
+    return __version__
