1f5e94639d7619946f194124f31af0142fe3588c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:20 +0000

    Add package gepard-python/1.0.20/gepard-python-1.0.20.tar.gz

---
+++ packages/gepard-python/gepard-python-1.0.20.tar.gz/gepard-python-1.0.20/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+from glob import glob
+
+import os
+longdescription = 'Add a fallback short description here'
+if os.path.exists('README.rst'):
+    longdescription = open('README.rst').read()
+
+setup(
+    name="gepard-python",
+    version="1.0.20",
+    license='MIT',
+    install_requires=[
+        "python-dateutil"
+    ],
+    author = 'Hans-Juergen Gessinger',
+    author_email = 'gessinger.hj@gmail.com',
+    url = 'https://github.com/gessinger-hj/gepard-python',
+    py_modules=['gepard'],
+    entry_points= {
+    },
+    keywords = "distributed-applications events semaphores locks messages JavaScript Java Python PHP messages",
+    description = "Python client for gepard:General purpose communications for distributed applications / Microservices / events, semaphores and messages for JavaScript, Java, Python and PHP",
+    long_description = longdescription,
+    package_data = {
+        'xmp': ['*']
+   },
+)
