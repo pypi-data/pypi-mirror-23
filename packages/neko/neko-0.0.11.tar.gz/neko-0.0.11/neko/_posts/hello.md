c9d4b200178b800fc07b51914df9afbb7b580f63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:05 +0000

    Add package neko/0.0.11/neko-0.0.11.tar.gz

---
+++ packages/neko/neko-0.0.11.tar.gz/neko-0.0.11/neko/_posts/hello.md
@@ -0,0 +1,7 @@
+---
+title: Hello
+date: 2017-07-19
+description: This is my first post
+---
+
+This is my first post.
---
+++ packages/neko/neko-0.0.11.tar.gz/neko-0.0.11/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='neko',
+    packages=find_packages(exclude=['tests']),
+    version='0.0.11',
+    entry_points={
+        'console_scripts': [
+            'neko = neko.main:main'
+        ]
+    },
+    description='A simple static site generator.',
+    url='https://github.com/Frederick-S/neko',
+    install_requires=[
+        'python-frontmatter',
+        'Markdown',
+        'Jinja2'
+    ],
+    include_package_data=True
+)
