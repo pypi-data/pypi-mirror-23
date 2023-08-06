ec4f478f7ee883a7a1b827540caa934901bcf307
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:57:43 +0000

    Add package Arbit/0.0.1a3/Arbit-0.0.1a3.tar.gz

---
+++ packages/Arbit/Arbit-0.0.1a3.tar.gz/Arbit-0.0.1a3/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Arbit",
+    version="0.0.1a3",
+    python_requires='>=3',
+    author='Tjaden Hess',
+    author_email='tjaden@1protocol.com',
+    url="https://github.com/1protocol/arbit-native",
+    license='GPL3',
+    description='Native Arbit client',
+    setup_requires=["cffi>=1.0.0"],
+    cffi_modules=["arbit/arbit_build.py:ffibuilder"],
+    install_requires=["cffi>=1.0.0"],
+    packages=["arbit"]
+)
