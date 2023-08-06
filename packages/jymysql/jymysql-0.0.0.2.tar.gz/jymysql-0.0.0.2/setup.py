94f35e17363ebba47fed431a1b7c86fff93fe43e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:32 +0000

    Add package jymysql/0.0.0.2/jymysql-0.0.0.2.tar.gz

---
+++ packages/jymysql/jymysql-0.0.0.2.tar.gz/jymysql-0.0.0.2/setup.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+from setuptools import setup, find_packages
+
+setup(
+    name='jymysql',
+    version='0.0.0.2',
+    description=(
+        '对mysql操作的orm'
+    ),
+    long_description=open('README.rst').read(),
+    author='Jython',
+    author_email='ijython@qq.com',
+    maintainer='Jython',
+    maintainer_email='ijython@qq.com',
+    license='BSD License',
+    packages=find_packages(),
+    platforms=[
+        "all"],
+    url='',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Operating System :: OS Independent',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: Implementation',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Software Development :: Libraries'
+    ],
+    install_requires=[
+        #'MySQL-python>=1.2.3',
+    ]
+)
