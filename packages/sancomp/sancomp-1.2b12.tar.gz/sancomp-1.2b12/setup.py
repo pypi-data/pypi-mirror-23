bdb28038314e9edc4c75519b8ddf59871c1bdf82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:35:37 +0000

    Add package sancomp/1.2b12/sancomp-1.2b12.tar.gz

---
+++ packages/sancomp/sancomp-1.2b12.tar.gz/sancomp-1.2b12/setup.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup,find_packages
+
+setup(
+	name='sancomp',
+	version='1.2b12',
+	url='https://github.com/douglaskorvo/sancomp',
+	license='GPL 3.0',
+	author='Douglas Rodrigues',
+	author_email='douglas.souza85@gmail.com',
+	keywords='sandwich structures analysis software',
+	description=u'Sandwich Structures Analysis Software',
+	#python_requires='==2.7',
+	packages=['sancomp'],
+	package_data={'sancomp': ['*.*','image/*.*'],},
+	#data_files={'sancomp': ['*.*'],},,
+	install_requires=['numpy >=1.11.2'],
+)
