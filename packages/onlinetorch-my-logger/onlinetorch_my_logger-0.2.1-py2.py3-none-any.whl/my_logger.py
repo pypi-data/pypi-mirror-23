44ebffe36d1e2d6a458835266d940c08c9cae33e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:47:08 +0000

    Add package onlinetorch-my-logger/0.2.1/onlinetorch_my_logger-0.2.1-py2.py3-none-any.whl

---
+++ packages/onlinetorch-my-logger/onlinetorch_my_logger-0.2.1-py2.py3-none-any.whl/my_logger.py
@@ -0,0 +1,36 @@
+import logging
+
+class MyLogger:
+
+  # level e.g logging.DEBUG | logging.INFO | logging.WARNING | logging.ERROR
+  def __init__(self, level, logger_name):
+    if level == "debug":
+      level = logging.DEBUG
+    elif level == "warning":
+      level = logging.WARNING
+    elif level == "error":
+      level = logging.ERROR
+    else
+      level = logging.INFO
+
+    # Init logger
+    logging.basicConfig(level=level, format='%(name)s - %(levelname)s - %(message)s')
+    self.logger = logging.getLogger(logger_name)
+
+  def log(self, message, level="info"):
+    if level == "error":
+      self.logger.error(message)
+    elif level == "warning":
+      self.logger.warning(message)
+    elif level == "debug":
+      self.logger.debug(message)
+    else:
+      self.logger.info(message)
+
+
+if __name__ == '__main__':
+  ml = MyLogger('debug','MyLogger')
+  ml.log("debug", "debug")
+  ml.log("info")
+  ml.log("warning", "warning")
+  ml.log("error", "error")
