43dd829f0b6a3facf071aaf7344e461de4af7f0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:30:27 +0000

    Add package kuorra/0.6.4.2/kuorra-0.6.4.2-py2-none-any.whl

---
+++ packages/kuorra/kuorra-0.6.4.2-py2-none-any.whl/kuorra/mvc_sessions/application/controllers/main/login.py
@@ -0,0 +1,25 @@
+from . import config
+import app
+import hashlib
+
+
+class Login:
+
+    def GET(self):
+        form = config.form()
+        return config.render.login(form)
+
+    def POST(self):
+        i = config.web.input()
+        pwdhash = hashlib.md5(i.password).hexdigest()
+        check = config.model.validate_user(i.username, pwdhash)
+        username = check['username']
+        privilege = check['privilege']
+        if check:
+            app.session.loggedin = True
+            app.session.username = username
+            app.session.privilege = privilege
+            raise config.web.seeother('/')
+        else:
+            form = config.form()
+            return config.render.login(form)
