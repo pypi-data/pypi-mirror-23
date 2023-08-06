16474299f4a2b481ff3e2a1c75d629f709648acd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:20 +0000

    Add package chemevolve/0.0.6/chemevolve-0.0.6.tar.gz

---
+++ packages/chemevolve/chemevolve-0.0.6.tar.gz/chemevolve-0.0.6/setup.py
@@ -0,0 +1,30 @@
+# Copyright 2016 ELIFE. All rights reserved.
+# Use of this source code is governed by a MIT
+# license that can be found in the LICENSE file.
+from setuptools import setup,find_packages
+
+
+with open('docs/README.rst') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='chemevolve',
+    version='0.0.6',
+    description='Tools for Simulating Chemical Evoltion',
+    long_description=readme,
+    maintainer='Cole Mathis',
+    maintainer_email='cole.mathis@asu.edu',
+    url='https://github.com/elife-asu/chemevolve',
+    license=license,
+    install_requires=['numpy', 'matplotlib', 'seaborn'],
+    packages=find_packages(),
+    include_package_data = True
+   
+)
+
+
+
+
