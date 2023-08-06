1ff47c1d4c32fe20f369848a400e4196a5c5f569
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:13:33 +0000

    Add package spycy/0.2/spycy-0.2.tar.gz

---
+++ packages/spycy/spycy-0.2.tar.gz/spycy-0.2/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup( name='spycy'
+     , python_requires='>=3'
+     , version='0.2'
+     , description='Small library for auto-currying functions and improved functional application'
+     , url='http://github.com/kanales/spycy'
+     , author='Ivan Canales'
+     , author_email='kanales.contact@gmail.com'
+     , license='MIT'
+     , packages=['spycy']
+     , zip_safe=False
+     )
