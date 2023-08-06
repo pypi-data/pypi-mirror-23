7cfe40acf7ab0892fb239aceafce56c3dd777def
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:01 +0000

    Add package dryice/0.0.1/dryice-0.0.1.tar.gz

---
+++ packages/dryice/dryice-0.0.1.tar.gz/dryice-0.0.1/setup.py
@@ -0,0 +1,15 @@
+"""A setuptools based setup module.
+
+See:
+https://packaging.python.org/en/latest/distributing.html
+https://github.com/pypa/sampleproject
+"""
+
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+
+setup(
+    name='dryice',
+    version='0.0.1',
+    description='A sample Python project',
+)
