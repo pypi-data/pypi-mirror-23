4c4f4f379de2ed00065f75a5d0cfe823948e5b2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:15:34 +0000

    Add package psychrochart/0.1.5/psychrochart-0.1.5.tar.gz

---
+++ packages/psychrochart/psychrochart-0.1.5.tar.gz/psychrochart-0.1.5/psychrochart/__main__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+A python library to make psychrometric charts and overlay information in them.
+
+"""
+from psychrochart.chart import PsychroChart
+
+
+def main():
+    """CLI entry point to show the default psychrometric chart."""
+    PsychroChart().plot()
+
+
+if __name__ == '__main__':
+    main()
