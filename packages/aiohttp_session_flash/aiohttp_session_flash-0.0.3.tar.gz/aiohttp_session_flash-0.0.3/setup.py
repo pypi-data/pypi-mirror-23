5d281dc0877102fce8b074d06adf63945eed97ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:49 +0000

    Add package aiohttp_session_flash/0.0.3/aiohttp_session_flash-0.0.3.tar.gz

---
+++ packages/aiohttp_session_flash/aiohttp_session_flash-0.0.3.tar.gz/aiohttp_session_flash-0.0.3/setup.py
@@ -0,0 +1,17 @@
+"""Session flash messages for aiohttp.web"""
+from setuptools import setup, find_packages
+
+
+setup(
+	name='aiohttp_session_flash',
+	version='0.0.3',
+	url='https://github.com/IlyaSemenov/aiohttp_session_flash',
+	license='BSD',
+	author='Ilya Semenov',
+	author_email='ilya@semenov.co',
+	description=__doc__,
+	long_description=open('README.rst').read(),
+	packages=['aiohttp_session_flash'],
+	install_requires=['aiohttp>=0.21.6,<1.1', 'aiohttp-session>=0.5'],
+	classifiers=[],
+)
