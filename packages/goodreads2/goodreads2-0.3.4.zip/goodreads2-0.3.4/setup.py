3fa6f8e69ed0c155db499a317c633a1b98f7ebd1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:56:51 +0000

    Add package goodreads2/0.3.4/goodreads2-0.3.4.zip

---
+++ packages/goodreads2/goodreads2-0.3.4.zip/goodreads2-0.3.4/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+
+setup(
+    name='goodreads2',
+    description="Python wrapper for Goodreads API",
+    long_description=open("README.rst").read(),
+    url='https://github.com/tatianass/goodreads2',
+
+    author='Tatiana Saturno',
+    author_email='tatianassaturno@gmail.com',
+
+    packages=['goodreads'],
+    version='0.3.4',
+    install_requires=['nose', 'xmltodict', 'requests', 'rauth'],
+
+    license='MIT',
+
+    keywords = 'goodreads API',
+
+    classifiers = [
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.5",
+    ]
+    
+)
