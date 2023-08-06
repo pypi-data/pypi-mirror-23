e9d17210bdcbf84341e958c353338fedbe2852cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:32:32 +0000

    Add package infi.cwrap/0.2.12/infi.cwrap-0.2.12-py2-none-any.whl

---
+++ packages/infi.cwrap/infi.cwrap-0.2.12-py2-none-any.whl/infi/cwrap/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.2.12"
+__git_commiter_name__ = "Arnon Yaari"
+__git_commiter_email__ = "arnony@infinidat.com"
+__git_branch__ = u'(HEAD detached at v0.2.12)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = '913ef2e2c0f806a7a92665aaceaca3d283be5e5e'
+__git_head_subject__ = u'Finished Release v0.2.12'
+__git_head_message__ = u''
+__git_dirty_diff__ = u''
