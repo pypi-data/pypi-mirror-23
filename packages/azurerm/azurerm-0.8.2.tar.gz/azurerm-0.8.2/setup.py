d3b42ba82f7e524d18da0355ba09ad8342347efb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:52 +0000

    Add package azurerm/0.8.2/azurerm-0.8.2.tar.gz

---
+++ packages/azurerm/azurerm-0.8.2.tar.gz/azurerm-0.8.2/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(name='azurerm',
+      version='0.8.2',
+      description='Azure Resource Manager REST wrappers',
+      url='http://github.com/gbowerman/azurerm',
+      author='sendmarsh',
+      author_email='guybo@outlook.com',
+      license='MIT',
+      packages=['azurerm'],
+      install_requires=[
+          'adal',
+          'requests',
+      ],
+      zip_safe=False)
