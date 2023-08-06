627f7d86baf5ebd3ea92f96db514e8f699110b9d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:56:46 +0000

    Add package docusign_exporter/1.0.3/docusign_exporter-1.0.3-py2-none-any.whl

---
+++ packages/docusign_exporter/docusign_exporter-1.0.3-py2-none-any.whl/docusign_exporter-1.0.3.dist-info/DESCRIPTION.rst
@@ -0,0 +1,11 @@
+# DocuSign exporter
+
+A simple tool to bulk export all documents / contracts / envelopes from your DocuSign account using the API. This downloads all documents of the account to your local drive. 
+
+install with: `pip install docusign_exporter`
+
+usage: `docusign_exporter [email] [password] [integrator_key]`
+
+This service is provided by [SignRequest](https://signrequest.com) for free.
+
+
---
+++ packages/docusign_exporter/docusign_exporter-1.0.3-py2-none-any.whl/docusign_exporter/docusign_exporter.py
@@ -0,0 +1,83 @@
+from __future__ import print_function
+import requests
+import json
+import os
+import sys
+
+
+class DocusignService(object):
+    def __init__(
+            self,
+            login=None,
+            password=None,
+            integrator_key=None):
+        self.session = requests.Session()
+        self.login_accounts = self.authenticate(login, password, integrator_key)
+        self.base_url = self.login_accounts['loginAccounts'][0]['baseUrl']
+        self.envelopes = None
+        self.get_envelopes()
+        self.get_documents()
+
+    def authenticate(self, login, password, integrator_key):
+        auth_data = {
+            'Username': login,
+            'Password': password,
+            'IntegratorKey': integrator_key
+        }
+
+        self.session.headers['X-DocuSign-Authentication'] = json.dumps(auth_data)
+        response = self.session.get('https://www.docusign.net/restapi/v2/login_information')
+        if response.ok:
+            return response.json()
+        else:
+            print(response.json())
+            sys.exit()
+
+    def get_envelopes(self):
+        response = self.session.get(self.base_url + '/envelopes?from_date=1960-01-01T00:00:00Z')
+        if response.ok:
+            self.envelopes = response.json()['envelopes']
+            while response.json()['nextUri']:
+                response = self.session.get(self.base_url + response.json()['nextUri'])
+                if response.ok:
+                    self.envelopes.extend(response.json()['envelopes'])
+
+    def get_documents(self):
+        if self.envelopes:
+            for envelope in self.envelopes:
+                envelop_dir = os.path.join('.', '{}-{}'.format(
+                    envelope['statusChangedDateTime'],
+                    envelope['envelopeId']
+                ))
+                print('Downloading envelope: {}'.format(envelope['envelopeId']))
+                if not os.path.exists(envelop_dir):
+                    os.mkdir(envelop_dir)
+                response = self.session.get(self.base_url + envelope['documentsUri'])
+                if response.ok and response.json():
+                    envelope_documents = response.json()['envelopeDocuments']
+                    for document in envelope_documents:
+                        print('Downloading document: {}'.format(document['name']))
+                        with open(os.path.join(envelop_dir, document['name']), 'wb+') as f:
+                            content = self.session.get(self.base_url + document['uri'])
+                            if content.ok:
+                                f.write(content.content)
+
+                response = self.session.get(self.base_url + envelope['recipientsUri'])
+                if response.ok and response.json():
+                    for recipient_type in response.json().keys():
+                        if recipient_type in ['currentRoutingOrder', 'recipientCount']:
+                            continue
+                        if response.json()[recipient_type]:
+                            with open(os.path.join(envelop_dir, recipient_type), 'wb+') as f:
+                                json.dump(response.json()[recipient_type], f)
+
+
+def main():
+    args = sys.argv
+    if '--help' in args or '-h' in args:
+        print('usage: python docusign-exporter.py [email] [password] [integrator_key]')
+    else:
+        DocusignService(*args[1:])
+
+if __name__ == '__main__':
+    main()
