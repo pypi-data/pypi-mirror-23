22a37a1e8c11e0fdccf1ce5b3de990c491b32016
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:32:32 +0000

    Add package infi.nose_plugins/0.0.7/infi.nose_plugins-0.0.7.tar.gz

---
+++ packages/infi.nose_plugins/infi.nose_plugins-0.0.7.tar.gz/infi.nose_plugins-0.0.7/src/infi/nose_plugins/__version__.py
@@ -0,0 +1,11 @@
+__version__ = "0.0.7"
+__git_commiter_name__ = "Arnon Yaari"
+__git_commiter_email__ = "arnony@infinidat.com"
+__git_branch__ = u'(HEAD detached at v0.0.7)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = 'a7e45e6d6018075e1ebc7b9e31f6a206d826f959'
+__git_head_subject__ = u'Finished Release v0.0.7'
+__git_head_message__ = u''
+__git_dirty_diff__ = u''
+__git_commit_date__ = '2017-07-09 15:01:50'
