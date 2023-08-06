52d61fef7b27c9a92733e872d3c52ba52768a7c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:33:32 +0000

    Add package gmaps/0.5.3rc1/gmaps-0.5.3rc1.tar.gz

---
+++ packages/gmaps/gmaps-0.5.3rc1.tar.gz/gmaps-0.5.3rc1/gmaps/__init__.py
@@ -0,0 +1,17 @@
+from ._version import __version__  # noqa
+
+from .figure import *  # noqa
+from .maps import *  # noqa
+from .marker import *  # noqa
+from .heatmap import *  # noqa
+from .directions import *  # noqa
+from .geojson_layer import *  # noqa
+
+
+def _jupyter_nbextension_paths():
+    return [{
+        'section': 'notebook',
+        'src': 'static',
+        'dest': 'jupyter-gmaps',
+        'require': 'jupyter-gmaps/extension'
+    }]
