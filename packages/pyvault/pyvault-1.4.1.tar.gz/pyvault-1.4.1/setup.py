3f867caa5f7d830c9c287000d4f36f3b43e2652c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:34:23 +0000

    Add package pyvault/1.4.1/pyvault-1.4.1.tar.gz

---
+++ packages/pyvault/pyvault-1.4.1.tar.gz/pyvault-1.4.1/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+try:
+    import pypandoc
+    long_description = pypandoc.convert('README.md', 'rst')
+except(IOError, ImportError):
+    long_description = open('README.md').read()
+
+setup (
+    name = 'pyvault',
+    version = '1.4.1',
+    description = 'Python password manager',
+    long_description = long_description,
+    author = 'Gabriel Bordeaux',
+    author_email = 'pypi@gab.lc',
+    url = 'https://github.com/gabfl/vault',
+    license = 'MIT',
+    packages = find_packages(),
+    install_requires = ['pycryptodome', 'pyperclip', 'tabulate', 'argparse'], # external dependencies
+    entry_points = {
+        'console_scripts': [
+            'vault = src.vault:main',
+        ],
+    },
+)
