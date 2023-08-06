4e323fc41285cb14068f320de0e322b96c9785bd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:36 +0000

    Add package ivy/0.3.5/ivy-0.3.5.tar.gz

---
+++ packages/ivy/ivy-0.3.5.tar.gz/ivy-0.3.5/ivy/ini/site.py
@@ -0,0 +1,45 @@
+# --------------------------------------------------------------------------
+# Site Configuration File
+# --------------------------------------------------------------------------
+
+# This is a sample site configuration file. You can use this file to
+# customise how Ivy builds your site. Variables you set here will also be
+# available to themes and plugins.
+
+# --------------------------------------------------------------------------
+# Theme
+# --------------------------------------------------------------------------
+
+# Choose the theme to use when building your site. This attribute should
+# specify the name of a theme directory in your site's 'lib' folder.
+theme = "debug"
+
+# --------------------------------------------------------------------------
+# Theme Settings
+# --------------------------------------------------------------------------
+
+# Site title - how this is displayed depends on the theme.
+title = "Site Title"
+
+# Site tagline - how this is displayed depends on the theme.
+tagline = "Just another Ivy site"
+
+# Site copyright notice - how this is displayed depends on the theme.
+copyright = "Copyright &copy; 1901 Foobar Industries"
+
+# --------------------------------------------------------------------------
+# Root URL
+# --------------------------------------------------------------------------
+
+# Your root url can be an explicit domain ("http://example.com/") for
+# absolute urls, a single forward slash ("/") for site-relative urls, or an
+# empty string (the default) for page-relative urls.
+root = ""
+
+# --------------------------------------------------------------------------
+# File Extension
+# --------------------------------------------------------------------------
+
+# Choose a file extension for generated files, e.g. ".html" (the default),
+# or specify a forward slash ("/") to generate directory-style urls.
+extension = ".html"
