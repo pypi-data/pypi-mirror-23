18d71383648e44097baa886ff319aae9bdd19d59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:34:31 +0000

    Add package tensorpac/0.5.4/Tensorpac-0.5.4.tar.gz

---
+++ packages/tensorpac/Tensorpac-0.5.4.tar.gz/Tensorpac-0.5.4/tensorpac/__init__.py
@@ -0,0 +1,9 @@
+"""
+PHASE-AMPLITUDE TOOLBOX
+=======================
+
+Tensorpac is an open-source Python toolbox designed for computing
+Phase-Amplitude Coupling.
+"""
+from .pac import Pac
+from .utils import *
