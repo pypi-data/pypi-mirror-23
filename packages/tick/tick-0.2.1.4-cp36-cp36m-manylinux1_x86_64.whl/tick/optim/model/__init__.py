6924c36eff7ac201a4521e27c7d4e66821e73155
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:08:24 +0000

    Add package tick/0.2.1.4/tick-0.2.1.4-cp36-cp36m-manylinux1_x86_64.whl

---
+++ packages/tick/tick-0.2.1.4-cp36-cp36m-manylinux1_x86_64.whl/tick/optim/model/__init__.py
@@ -0,0 +1,28 @@
+# License: BSD 3 clause
+
+import tick.base
+
+from .linreg import ModelLinReg
+from .linreg_with_intercepts import ModelLinRegWithIntercepts
+from .logreg import ModelLogReg
+from .poisreg import ModelPoisReg
+
+from .coxreg_partial_lik import ModelCoxRegPartialLik
+
+from .hawkes_fixed_expkern_loglik import ModelHawkesFixedExpKernLogLik
+from .hawkes_fixed_expkern_leastsq import ModelHawkesFixedExpKernLeastSq
+from .hawkes_fixed_sumexpkern_leastsq import ModelHawkesFixedSumExpKernLeastSq
+
+from .sccs import ModelSCCS
+
+
+__all__ = ["ModelLinReg",
+           "ModelLinRegWithIntercepts",
+           "ModelLogReg",
+           "ModelPoisReg",
+           "ModelCoxRegPartialLik",
+           "ModelHawkesFixedExpKernLogLik",
+           "ModelHawkesFixedExpKernLeastSq",
+           "ModelHawkesFixedSumExpKernLeastSq",
+           "ModelSCCS"
+           ]
