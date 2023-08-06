d2534a6a3d49b97ee328c3f5a7631ced82513681
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:29 +0000

    Add package animalss/0.1/animalss-0.1.tar.gz

---
+++ packages/animalss/animalss-0.1.tar.gz/animalss-0.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+	name = 'animalss',
+        packages = ['animalss'], # this must be the same as the name above
+        version = '0.1',
+        description = 'A random test lib',
+        author = 'balakrishnan',
+        author_email = 'krishnasmartt@gmail.com',
+        url = 'https://github.com/balakrishnan2/ethjsonrpc', # use the URL to the github repo
+        download_url = 'https://github.com/balakrishnan2/ethjsonrpc/archive/0.1.tar.gz', # I'll explain this in a second
+        keywords = ['testing', 'logging', 'example'], # arbitrary keywords
+        classifiers = [],
+)
