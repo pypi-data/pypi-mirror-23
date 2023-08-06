4fb5b37ee5c34dcebc47e21048e4a8d18ae123fd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:49:13 +0000

    Add package paypalrestsdk/2.0.0b1/paypalrestsdk-2.0.0b1-py2.py3-none-any.whl

---
+++ packages/paypalrestsdk/paypalrestsdk-2.0.0b1-py2.py3-none-any.whl/paypalrestsdk/core/__init__.py
@@ -0,0 +1,5 @@
+from paypalrestsdk.core.access_token import *
+from paypalrestsdk.core.access_token_request import *
+from paypalrestsdk.core.environment import *
+from paypalrestsdk.core.paypal_http_client import *
+from paypalrestsdk.core.test import *
