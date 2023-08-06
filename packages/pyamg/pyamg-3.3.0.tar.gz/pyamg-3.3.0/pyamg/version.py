b523589879c95aa5eed1342e05576e0c4d086e8a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:34:18 +0000

    Add package pyamg/3.3.0/pyamg-3.3.0.tar.gz

---
+++ packages/pyamg/pyamg-3.3.0.tar.gz/pyamg-3.3.0/pyamg/version.py
@@ -0,0 +1,9 @@
+
+# THIS FILE IS GENERATED FROM SETUP.PY
+short_version = '3.3.0'
+version = '3.3.0'
+full_version = '3.3.0'
+git_revision = '14ecc8d5e4bca64752a1649ab3d99a9dba17fb69'
+release = True
+if not release:
+    version = full_version
