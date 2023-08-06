dcabf235b7ca68a3c45c7c9822144c2d5cb61fa4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:50 +0000

    Add package abilian-core/0.9.8/abilian-core-0.9.8.tar.gz

---
+++ packages/abilian-core/abilian-core-0.9.8.tar.gz/abilian-core-0.9.8/abilian/core/extensions/login.py
@@ -0,0 +1,22 @@
+# coding=utf-8
+"""
+"""
+from __future__ import absolute_import, division, print_function, \
+    unicode_literals
+
+from flask import current_app
+from flask_login import AnonymousUserMixin, LoginManager
+
+
+class AnonymousUser(AnonymousUserMixin):
+
+    def has_role(self, role):
+        return current_app.services['security'].has_role(self, role)
+
+    @property
+    def groups(self):
+        return set()
+
+
+login_manager = LoginManager()
+login_manager.anonymous_user = AnonymousUser
