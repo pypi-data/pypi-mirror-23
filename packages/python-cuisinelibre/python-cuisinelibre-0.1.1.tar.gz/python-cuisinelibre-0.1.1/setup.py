70d282553455495091da2496be7d00da17f14c42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:07 +0000

    Add package python-cuisinelibre/0.1.1/python-cuisinelibre-0.1.1.tar.gz

---
+++ packages/python-cuisinelibre/python-cuisinelibre-0.1.1.tar.gz/python-cuisinelibre-0.1.1/setup.py
@@ -0,0 +1,15 @@
+
+from setuptools import setup
+
+setup(
+	name='python-cuisinelibre',
+	version='0.1.1',
+	description='Easy-to-use Python API for the cusine-libre.fr cokking website.',
+	packages=['cuisinelibre'],
+	url='https://github.com/remaudcorentin-dev/python-cuisinelibre',
+	author='Corentin Remaud',
+	author_email='remaudcorentin.dev@gmail.com',
+	license='MIT',
+	zip_safe=False,
+	install_requires=['bs4'],
+	)
