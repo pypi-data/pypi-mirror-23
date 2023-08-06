95a5135b4a459061c4923e3119f7b413cde215ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:34:49 +0000

    Add package pyjsparser/2.5.0/pyjsparser-2.5.0.tar.gz

---
+++ packages/pyjsparser/pyjsparser-2.5.0.tar.gz/pyjsparser-2.5.0/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='pyjsparser',
+    version='2.5.0',
+    packages=['pyjsparser'],
+    url='https://github.com/PiotrDabkowski/pyjsparser',
+    install_requires = [],
+    license='MIT',
+    author='Piotr Dabkowski',
+    author_email='piodrus@gmail.com',
+    description='Fast javascript parser (based on esprima.js)'
+)
