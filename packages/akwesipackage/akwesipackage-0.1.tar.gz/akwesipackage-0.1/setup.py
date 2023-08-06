60e4c4f961ae87b54c4e5cfded855df7af3fe517
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:56:58 +0000

    Add package akwesipackage/0.1/akwesipackage-0.1.tar.gz

---
+++ packages/akwesipackage/akwesipackage-0.1.tar.gz/akwesipackage-0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name='akwesipackage',
+      version='0.1',
+      description='Placeholder dummy package',
+      url='http://github.com/akwesinketia/akwesipackage',
+      author='Tom',
+      author_email='nketiaakwesi@gmail.com',
+      license='MIT',
+      packages=['akwesipackage'],
+      install_requires=[
+        'pyhsmm'
+      ],
+      zip_safe=False)
