15b18d35a58bb6a61889834f8a14b9b2e7fc5321
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:53:52 +0000

    Add package webfriend/0.1.5/webfriend-0.1.5-py2-none-any.whl

---
+++ packages/webfriend/webfriend-0.1.5-py2-none-any.whl/webfriend/exceptions.py
@@ -0,0 +1,26 @@
+class ChromeProtocolError(Exception):
+    pass
+
+
+class TimeoutError(Exception):
+    pass
+
+
+class NotFound(Exception):
+    pass
+
+
+class HttpError(Exception):
+    pass
+
+
+class EmptyResult(Exception):
+    pass
+
+
+class TooManyResults(Exception):
+    pass
+
+
+class NotImplemented(Exception):
+    pass
