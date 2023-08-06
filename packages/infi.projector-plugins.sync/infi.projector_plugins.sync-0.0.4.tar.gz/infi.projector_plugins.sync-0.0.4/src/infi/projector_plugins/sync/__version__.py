ffee636a2f1da4dbfc76bcc3a55954856f5425d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:32:32 +0000

    Add package infi.projector-plugins.sync/0.0.4/infi.projector_plugins.sync-0.0.4.tar.gz

---
+++ packages/infi.projector-plugins.sync/infi.projector_plugins.sync-0.0.4.tar.gz/infi.projector_plugins.sync-0.0.4/src/infi/projector_plugins/sync/__version__.py
@@ -0,0 +1,11 @@
+__version__ = "0.0.4"
+__git_commiter_name__ = "Arnon Yaari"
+__git_commiter_email__ = "arnony@infinidat.com"
+__git_branch__ = u'(HEAD detached at v0.0.4)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = 'b93acd0daceef1d14b33ad7386c8a6e8aec71191'
+__git_head_subject__ = u'Finished Release v0.0.4'
+__git_head_message__ = u''
+__git_dirty_diff__ = u''
+__git_commit_date__ = '2017-07-09 15:08:24'
