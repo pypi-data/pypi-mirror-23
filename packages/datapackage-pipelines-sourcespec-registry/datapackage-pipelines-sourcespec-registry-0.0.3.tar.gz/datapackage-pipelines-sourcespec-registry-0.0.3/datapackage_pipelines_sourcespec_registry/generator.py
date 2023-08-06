b9ad2c239ddc5330432ccf8e826907073d850f3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:33:37 +0000

    Add package datapackage-pipelines-sourcespec-registry/0.0.3/datapackage-pipelines-sourcespec-registry-0.0.3.tar.gz

---
+++ packages/datapackage-pipelines-sourcespec-registry/datapackage-pipelines-sourcespec-registry-0.0.3.tar.gz/datapackage-pipelines-sourcespec-registry-0.0.3/datapackage_pipelines_sourcespec_registry/generator.py
@@ -0,0 +1,31 @@
+import json
+import os
+
+from datapackage_pipelines.generators import GeneratorBase
+
+from .registry import SourceSpecRegistry
+
+import logging
+log = logging.getLogger(__name__)
+
+
+ROOT_PATH = os.path.join(os.path.dirname(__file__), '..')
+SCHEMA_FILE = os.path.join(
+    os.path.dirname(__file__), 'schemas/sourcespec_registry_schema.json')
+
+
+class Generator(GeneratorBase):
+
+    @classmethod
+    def get_schema(cls):
+        return json.load(open(SCHEMA_FILE))
+
+    @classmethod
+    def generate_pipeline(cls, source):
+        db_connection_string = \
+            source.get('db-connection-string',
+                       os.environ.get('SOURCESPEC_REGISTRY_DB_ENGINE'))
+        registry = SourceSpecRegistry(db_connection_string)
+
+        for spec in registry.list_source_specs():
+            yield f':{spec.module}:', spec.contents
