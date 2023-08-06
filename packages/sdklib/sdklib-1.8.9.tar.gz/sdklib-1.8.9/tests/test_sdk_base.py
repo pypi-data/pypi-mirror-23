00b03d4b87f115c80024d7fb3ebcdfe117ea6b1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:13:06 +0000

    Add package sdklib/1.8.9/sdklib-1.8.9.tar.gz

---
+++ packages/sdklib/sdklib-1.8.9.tar.gz/sdklib-1.8.9/tests/test_sdk_base.py
@@ -0,0 +1,33 @@
+import unittest
+
+from sdklib.http import HttpSdk
+
+
+class TestSdkBase(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        pass
+
+    @classmethod
+    def tearDownClass(cls):
+        pass
+
+    def test_set_default_host_without_scheme(self):
+        default_host = HttpSdk.DEFAULT_HOST
+        HttpSdk.set_default_host("localhost:1234")
+        self.assertEqual(HttpSdk.DEFAULT_HOST, "http://localhost:1234")
+        HttpSdk.set_default_host(default_host)
+        self.assertEqual(HttpSdk.DEFAULT_HOST, default_host)
+
+    def test_set_default_host_none(self):
+        default_host = HttpSdk.DEFAULT_HOST
+        HttpSdk.set_default_host(None)
+        self.assertEqual(HttpSdk.DEFAULT_HOST, default_host)
+
+    def test_set_default_proxy_without_scheme(self):
+        default_proxy = HttpSdk.DEFAULT_PROXY
+        HttpSdk.set_default_proxy("localhost:1234")
+        self.assertEqual(HttpSdk.DEFAULT_PROXY, "http://localhost:1234")
+        HttpSdk.set_default_proxy(default_proxy)
+        self.assertEqual(HttpSdk.DEFAULT_PROXY, default_proxy)
