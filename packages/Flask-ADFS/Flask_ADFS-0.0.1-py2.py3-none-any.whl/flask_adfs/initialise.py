8a42d685c0c6cf49a6ee8971bfa86303f4d7c401
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:36:12 +0000

    Add package Flask-ADFS/0.0.1/Flask_ADFS-0.0.1-py2.py3-none-any.whl

---
+++ packages/Flask-ADFS/Flask_ADFS-0.0.1-py2.py3-none-any.whl/flask_adfs/initialise.py
@@ -0,0 +1,34 @@
+from cryptography.x509 import load_pem_x509_certificate
+from cryptography.hazmat.backends import default_backend
+from flask_login import LoginManager
+
+_public_key = None
+login_mgr = LoginManager()
+adfs_access = {}
+
+
+def pk():
+    return _public_key
+
+
+def adfs_init(app):
+    global _public_key
+    global login_mgr
+    login_mgr.init_app(app)
+    login_mgr.session_protection = "strong"
+
+    with open(app.config.get('ADFS_CERT_LOCATION')) as cert:
+        cert_str = cert.read().encode()
+    # extract public key
+    cert_obj = load_pem_x509_certificate(cert_str, default_backend())
+    _public_key = cert_obj.public_key()
+
+
+def set_access(access):
+    global adfs_access
+    adfs_access = access
+
+
+def get_access():
+    global adfs_access
+    return adfs_access
