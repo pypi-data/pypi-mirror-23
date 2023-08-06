30b900667e74f1c77c98fb083e4cd3cc00a6bf53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:08:28 +0000

    Add package bruker2nifti/0.0.5/bruker2nifti-0.0.5.tar.gz

---
+++ packages/bruker2nifti/bruker2nifti-0.0.5.tar.gz/bruker2nifti-0.0.5/setup.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+from bruker2nifti._definitions import version_bruker2nifti
+
+
+def requirements2list(pfi_txt='requirements.txt'):
+    f = open(pfi_txt, 'r')
+    l = []
+    for line in f.readlines():
+        l.append(line.replace('\n', ''))
+    return l
+
+
+infos = {
+         'name': 'bruker2nifti',
+         'version': version_bruker2nifti,
+         'description': 'From raw Bruker to nifti, home-made converter.',
+         'web_infos' : '',
+         'repository': {
+                        'type': 'git',
+                        'url': 'https://github.com/SebastianoF/bruker2nifti'
+                       },
+         'author': 'sebastiano ferraris',
+         'author_email': 'sebastiano.ferraris@gmail.com',
+         'dependencies': {
+                          # requirements.txt file automatically generated using pipreqs.
+                          'python' : 'requirements.txt'
+                          }
+         }
+
+setup(name=infos['name'],
+      version=infos['version'],
+      description=infos['description'],
+      author=infos['author'],
+      author_email=infos['author_email'],
+      url=infos['repository']['url'],
+      packages=find_packages(),
+      install_requires=requirements2list(),
+      entry_points={
+        'console_scripts': ['bruker2nifti=parsers.bruker2nifti:main'],
+        'gui_scripts' : ['bruker2nifti_gui=bruker2nifti.open_GUI:open_gui']
+      }
+      )
---
+++ packages/bruker2nifti/bruker2nifti-0.0.5.tar.gz/bruker2nifti-0.0.5/test/test_convert_banana.py
@@ -0,0 +1,83 @@
+import os
+
+import warnings
+import subprocess
+import platform
+
+from bruker2nifti.converter import Bruker2Nifti
+
+
+here = os.path.abspath(os.path.dirname(__file__))
+root_dir = os.path.dirname(here)
+
+
+def test_convert_the_banana(open_converted=False):
+
+    pfo_study_in = os.path.join(root_dir, 'test_data', 'bru_banana')
+    pfo_study_out = os.path.join(root_dir, 'test_data', 'nifti_banana')
+
+    bru = Bruker2Nifti(pfo_study_in, pfo_study_out, study_name='banana')
+
+    bru.verbose = 2
+    bru.correct_slope = True
+    bru.get_acqp = False
+    bru.get_method = False
+    bru.get_reco = False
+
+    bru.convert()
+
+    if open_converted:
+
+        if platform.system() == "Windows":
+            os.startfile(pfo_study_out.encode('string-escape'))
+        elif platform.system() == "Darwin":
+            subprocess.Popen(["open", pfo_study_out])
+        else:
+            subprocess.Popen(["xdg-open", pfo_study_out])
+
+
+def test_convert_the_banana_no_name(open_converted=False):
+
+    pfo_study_in = os.path.join(root_dir, 'test_data', 'bru_banana')
+    pfo_study_out = os.path.join(root_dir, 'test_data', 'nifti_banana')
+
+    bru = Bruker2Nifti(pfo_study_in, pfo_study_out)
+
+    bru.verbose=2,
+    bru.correct_slope=True,
+    bru.get_acqp=False,
+    bru.get_method=False,
+    bru.get_reco=False
+    bru.convert()
+
+    if open_converted:
+
+        if platform.system() == "Windows":
+            os.startfile(pfo_study_out.encode('string-escape'))
+        elif platform.system() == "Darwin":
+            subprocess.Popen(["open", pfo_study_out])
+        else:
+            subprocess.Popen(["xdg-open", pfo_study_out])
+
+
+test_convert_the_banana_no_name(open_converted=False)
+
+
+def test_warning_banana_bad_n():
+
+    for n in ['1', '2', '3']:
+
+        pfo_study_in = os.path.join(root_dir, 'test_data', 'bru_banana_bad_' + n)
+        pfo_study_out = os.path.join(root_dir, 'test_data', 'nifti_banana')
+
+        with warnings.catch_warnings(record=True) as wa:
+
+            warnings.simplefilter('always')
+
+            bru = Bruker2Nifti(pfo_study_in, pfo_study_out, study_name='banana')
+            bru.correct_slope = True
+            bru.verbose = 2
+            bru.convert()
+
+            assert len(wa) == 1
+            assert issubclass(wa[-1].category, Warning)
