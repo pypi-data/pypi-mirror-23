086aa4f4a8590c0d3116e8f20c89d2958161794a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:35:47 +0000

    Add package clcomuna/1.1.2/clcomuna-1.1.2.tar.gz

---
+++ packages/clcomuna/clcomuna-1.1.2.tar.gz/clcomuna-1.1.2/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+setup(name='clcomuna',
+      version='1.1.2',
+      description='Given the name of a comuna in Chile, it returns its code',
+      long_description=readme(),
+      url='http://github.com/slarrain/clcomuna',
+      author='Santiago Larrain',
+      author_email='santiagolarrain@gmail.com',
+      license='MIT',
+      packages=['clcomuna'],
+      install_requires=[
+          'fuzzywuzzy',
+      ],
+      #data_files=[('data', ['comunas.csv'])],
+      include_package_data=True,
+      test_suite='nose.collector',
+      tests_require=['nose'],
+      zip_safe=False)
