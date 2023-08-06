73441029cacb79c72313edde62385defce4cd1f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:18:06 +0000

    Add package random_color/0.1/random_color-0.1.tar.gz

---
+++ packages/random_color/random_color-0.1.tar.gz/random_color-0.1/setup.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+setup(name='random_color',
+      version='0.1',
+      description='Generate a random hex color',
+      url='http://github.com/ghitab/random_color',
+      author='Ghiță Bizău',
+      author_email='ghitabizau27@gmail.com',
+      license='MIT',
+      packages=['random_color'],
+      zip_safe=False)
