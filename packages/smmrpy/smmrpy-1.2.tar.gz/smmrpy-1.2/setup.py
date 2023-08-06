21ea34b5a642dc6da8685cbe8b1a1db1b5268c9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:47:58 +0000

    Add package smmrpy/1.2/smmrpy-1.2.tar.gz

---
+++ packages/smmrpy/smmrpy-1.2.tar.gz/smmrpy-1.2/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'smmrpy',
+  packages = ['smmrpy'], # this must be the same as the name above
+  version = '1.2',
+  description = 'Async api wrapper for the SMMRY api.',
+  author = 'Tyler Gibbs',
+  author_email = 'gibbstyler7@gmail.com',
+  url = 'https://github.com/TheTrain2000/smmrpy', # use the URL to the github repo
+  download_url = 'https://github.com/TheTrain2000/smmrpy/archive/1.2.tar.gz', # I'll explain this in a second
+  keywords = ['api', 'async', 'smmry', 'wrapper'], # arbitrary keywords
+  classifiers = [],
+)
