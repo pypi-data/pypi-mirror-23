bf1fcf6f2437886e85a399de6005379bb5e8d59c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:10:12 +0000

    Add package ai-libs/1.0.0.4/ai-libs-1.0.0.4.tar.gz

---
+++ packages/ai-libs/ai-libs-1.0.0.4.tar.gz/ai-libs-1.0.0.4/setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+from setuptools import find_packages
+setup(
+	name = 'ai-libs',
+	version = '1.0.0.4',
+	packages = find_packages(),
+	py_modules = [
+		'ai/autoencoder/agn_autoencoder',
+		'ai/autoencoder/test',
+		'ai/simple/softmax',
+	],
+	author = 'geekbruce',
+	author_email = 'bruce.shaoheng@gmail.com',
+	url = 'http://tikiapp.im',
+	description = 'this is the ai-libs which can help you build the deeplearning quickly',
+)
