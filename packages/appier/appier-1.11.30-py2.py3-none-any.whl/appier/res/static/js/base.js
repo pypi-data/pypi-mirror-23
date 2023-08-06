4cfb9454c9624c8e64c1665e0710bb0405d5d0d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:06 +0000

    Add package appier/1.11.30/appier-1.11.30-py2.py3-none-any.whl

---
+++ packages/appier/appier-1.11.30-py2.py3-none-any.whl/appier/res/static/js/base.js
@@ -0,0 +1,26 @@
+var load = function() {
+    if (document.querySelectorAll === undefined) {
+        return;
+    }
+
+    var elements = document.querySelectorAll(".line.opener");
+    for (var index = 0; index < elements.length; index++) {
+        var element = elements[index];
+        element.addEventListener("click", function() {
+            var targetId = this.getAttribute("data-id");
+            var target = document.querySelector(".lines-extra[data-id=\"" + targetId + "\"]");
+            var style = target.style;
+            if (style.display === "block") {
+                target.style.display = "none";
+            } else {
+                target.style.display = "block";
+            }
+        });
+    }
+
+    window.hljs && hljs.initHighlighting();
+}
+
+window.addEventListener !== undefined && window.addEventListener("load", function() {
+    load();
+});
