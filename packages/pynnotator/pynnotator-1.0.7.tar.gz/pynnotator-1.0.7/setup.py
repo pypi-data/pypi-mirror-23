3232c4f752dc9aa5f97e0b1a557a3c2e70f6872e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:13:43 +0000

    Add package pynnotator/1.0.7/pynnotator-1.0.7.tar.gz

---
+++ packages/pynnotator/pynnotator-1.0.7.tar.gz/pynnotator-1.0.7/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(name='pynnotator',
+      version='1.0.7',
+      description='A Python Annotation Framework for VCFs using multiple tools (Ex. VEP, SnpEff and SnpSift) and databases (Ex. 1000genomes, dbSNP and dbnfsp) .',
+      url='http://github.com/raonyguimaraes/pynnotator',
+      author='Raony Guimaraes',
+      author_email='raony@torchmed.com',
+      license='MIT',
+      packages=['pynnotator', 'pynnotator.helpers', 'pynnotator.tests'],
+      install_requires=[
+          'pysam',
+          'cython',
+          'wheel',
+      ],
+      test_suite='nose.collector',
+      tests_require=['nose'],
+      scripts=['bin/pynnotator'],
+      include_package_data=True,
+      zip_safe=False)
