a13f59f55b5b9ac254e3edd19a8a71128fe1569b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:09:37 +0000

    Add package ordo/0.1.6/ordo-0.1.6.tar.gz

---
+++ packages/ordo/ordo-0.1.6.tar.gz/ordo-0.1.6/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup (
+	name = 'ordo',
+	packages = ['ordo'],
+	version = '0.1.6',
+	description = 'A lightweight grading tool for nbgallery',
+	author = 'Andre J. Michell',
+	author_email = 'andre.j.michell2@gmail.com',
+	url = 'https://github.com/nbgallery/ordo',
+	keywords = ['nbgallery', 'Jupyter', 'grading'],
+	package_data = {'ordo': ['ordo.js'],},
+)
