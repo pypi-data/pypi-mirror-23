a56574ad23a835bf4e564c76f25958e583d16606
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:35:04 +0000

    Add package wefacts/0.1.4/wefacts-0.1.4.tar.gz

---
+++ packages/wefacts/wefacts-0.1.4.tar.gz/wefacts-0.1.4/wefacts/util.py
@@ -0,0 +1,26 @@
+"""
+Utils.
+"""
+
+import logging
+import os
+
+# base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+base_dir = ''
+
+# if not os.path.exists('%s/raw' % base_dir):
+#     os.makedirs('%s/raw' % base_dir)
+
+logger = logging.getLogger(__name__)
+formatter = logging.Formatter('%(asctime)s %(pathname)s %(lineno)d %(message)s')
+file_handler = logging.FileHandler('wefacts.log')
+file_handler.setFormatter(formatter)
+file_handler.setLevel(logging.DEBUG)
+console_handler = logging.StreamHandler()
+console_handler.setFormatter(formatter)
+console_handler.setLevel(logging.DEBUG)
+logger.addHandler(file_handler)
+logger.addHandler(console_handler)
+logger.setLevel(logging.DEBUG)
+
+__version__ = "0.0.1"
