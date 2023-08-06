259783f8acea08d36df7ff59b43c86e9aea88d35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:08:22 +0000

    Add package tick/0.2.1.4/tick-0.2.1.4-cp35-cp35m-manylinux1_x86_64.whl

---
+++ packages/tick/tick-0.2.1.4-cp35-cp35m-manylinux1_x86_64.whl/tick/optim/solver/__init__.py
@@ -0,0 +1,16 @@
+# License: BSD 3 clause
+
+import tick.base
+
+from .gd import GD
+from .agd import AGD
+from .bfgs import BFGS
+from .scpg import SCPG
+from .sgd import SGD
+from .svrg import SVRG
+from .sdca import SDCA
+from .gfb import GFB
+from .adagrad import AdaGrad
+
+__all__ = ["GD", "AGD", "BFGS", "SCPG", "SGD", "SVRG", "SDCA", "GFB",
+           "AdaGrad"]
