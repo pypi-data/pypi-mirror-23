abf5861ab0d5237a96120380862e1bc2790da661
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:56:18 +0000

    Add package tunable/0.0.1.dev3/tunable-0.0.1.dev3.tar.gz

---
+++ packages/tunable/tunable-0.0.1.dev3.tar.gz/tunable-0.0.1.dev3/tunable/tunable.py
@@ -0,0 +1,99 @@
+# -*- coding: utf-8 -*-
+"""
+documentation
+"""
+
+
+class TunableError(RuntimeError):
+    pass
+
+
+# noinspection PyPep8Naming
+class classproperty(object):
+    __slots__ = ('fget', '__doc__', )
+
+    def __init__(self, fget=None, doc=None):
+        self.fget = fget
+
+        if doc is None and fget is not None:
+            doc = fget.__doc__
+
+        self.__doc__ = doc
+
+    def __get__(self, obj, cls):
+        return self.fget(cls)
+
+
+class Tunable(object):
+
+    @classproperty
+    def value(cls):
+        return cls.reset()
+
+    default = None
+
+    convert_type = True
+    range = None
+    type_ = None
+
+    hash = True
+
+    documentation = None
+
+    @classproperty
+    def documentation(cls):
+        if cls.__doc__:
+            return cls.__doc__.strip()
+        else:
+            return ''
+
+    # noinspection PyUnusedLocal
+    @classmethod
+    def test(cls, value):
+        return True
+
+    @classmethod
+    def reset(cls):
+        return cls.set(cls.default)
+
+    @classmethod
+    def set(cls, value):
+
+        if value is None:
+            raise TunableError('Tunable has no value', cls)
+
+        if cls.type_ is None and cls.convert_type:
+            cls.type_ = type(cls.default)
+
+        if cls.type_ is not None and type(value) != cls.type_:
+            try:
+                value = cls.type_(value)
+            except ValueError as e:
+                raise TunableError(e)
+
+        if cls.range is not None and value not in cls.range \
+                and (type(cls.range) == range and value != cls.range.stop):
+            raise TunableError('Tunable not in range', cls)
+
+        if cls.test is not None and not cls.test(value):
+            raise TunableError('test() failed!')
+
+        cls.value = value
+        return value
+
+    def __new__(cls, *args, **kwargs):
+        if len(kwargs) == 0:
+            return cls.value
+        else:
+            class IntermediateTunable(Tunable):
+                pass
+
+            for k, v in kwargs.items():
+                if k == 'documentation':
+                    IntermediateTunable.__doc__ = v
+                elif hasattr(Tunable, k):
+                    setattr(IntermediateTunable, k, v)
+                else:
+                    raise TunableError('Unsupported attribute \"%s\"' % (k,))
+
+            return IntermediateTunable
