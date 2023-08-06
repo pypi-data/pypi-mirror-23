46c228f215f8efe27048d7724593b9964c45e7ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:18:49 +0000

    Add package pyetherscan/0.0.0/pyetherscan-0.0.0.tar.gz

---
+++ packages/pyetherscan/pyetherscan-0.0.0.tar.gz/pyetherscan-0.0.0/setup.py
@@ -0,0 +1,34 @@
+from distutils.core import setup
+from setuptools import find_packages
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='pyetherscan',
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+    version='0.0.0',
+    description='An unofficial wrapper for the Etherscan.io API',
+    long_description=long_description,
+    author='Michael Martorella',
+    author_email='michaelmartorella@gmail.com',
+    url='https://github.com/Marto32/pyetherscan',
+    download_url='https://github.com/Marto32/pyetherscan/archive/0.0.0.tar.gz',
+    keywords=['ethereum', 'blockchain', 'etherscan'],
+    license='MIT License',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+)
