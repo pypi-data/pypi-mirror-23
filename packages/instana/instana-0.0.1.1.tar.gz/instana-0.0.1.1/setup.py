48d8bbf4bbe7b2c0ed81497198fb504d56975864
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:57:46 +0000

    Add package instana/0.0.1.1/instana-0.0.1.1.tar.gz

---
+++ packages/instana/instana-0.0.1.1.tar.gz/instana-0.0.1.1/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+try:
+    import pypandoc
+    converted_readme = pypandoc.convert('README.md', 'rst')
+except(IOError, ImportError):
+    converted_readme = open('README.md').read()
+
+setup(name='instana',
+      version='0.0.1.1',
+      url='https://github.com/instana/python-sensor',
+      license='MIT',
+      author='Instana Inc.',
+      author_email='peter.lombardo@instana.com',
+      description='Metrics sensor and trace collector for Instana',
+      packages=find_packages(exclude=['tests', 'examples']),
+      long_description=converted_readme,
+      zip_safe=False,
+      setup_requires=['nose>=1.0',
+                      'fysom>=2.1.2',
+                      'opentracing>=1.2.1,<1.3',
+                      'basictracer>=2.2.0',
+                      'psutil>=5.1.3'],
+      test_suite='nose.collector',
+      keywords=['performance', 'opentracing', 'metrics', 'monitoring'])
