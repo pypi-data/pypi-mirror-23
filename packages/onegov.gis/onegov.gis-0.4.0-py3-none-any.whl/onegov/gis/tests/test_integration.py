bfc083e7a47837b11148a348874d756367153332
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:46:27 +0000

    Add package onegov.gis/0.4.0/onegov.gis-0.4.0-py3-none-any.whl

---
+++ packages/onegov.gis/onegov.gis-0.4.0-py3-none-any.whl/onegov/gis/tests/test_integration.py
@@ -0,0 +1,54 @@
+import morepath
+import pytest
+
+from onegov.core import Framework
+from onegov.core.utils import scan_morepath_modules
+from onegov.gis import MapboxApp
+from webtest import TestApp as Client
+
+
+def test_no_secret_keys(temporary_directory):
+
+    class App(Framework, MapboxApp):
+        pass
+
+    @App.webasset_output()
+    def get_output_path():
+        return temporary_directory
+
+    morepath.commit(App)
+    app = App()
+
+    with pytest.raises(AssertionError):
+        app.configure_application(mapbox_token='sk.asdf')
+
+
+def test_mapbox_token_tween(temporary_directory):
+
+    class App(Framework, MapboxApp):
+        pass
+
+    @App.webasset_output()
+    def get_output_path():
+        return temporary_directory
+
+    @App.path(path='')
+    class Root(object):
+        pass
+
+    @App.html(model=Root)
+    def view_root(self, request):
+        return '<body></body>'
+
+    scan_morepath_modules(App)
+    morepath.commit(App)
+
+    app = App()
+    app.configure_application()
+    app.namespace = 'foo'
+    app.set_application_id('foo/bar')
+
+    assert '<body>' in Client(app).get('/')
+
+    app.configure_application(mapbox_token='pk.asdf')
+    assert '<body data-mapbox-token="pk.asdf"></body>'
