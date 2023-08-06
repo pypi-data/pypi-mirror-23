dc336732ec2f6498950f82862d553e2d3f22c143
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:48:45 +0000

    Add package armada-heal/0.0.1/armada-heal-0.0.1.tar.gz

---
+++ packages/armada-heal/armada-heal-0.0.1.tar.gz/armada-heal-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='armada-heal',
+    description='Script for healing overloaded armada ship.',
+    version='0.0.1',
+    url='http://dev.git.gd/armada/armada-heal',
+    author='Cerebro',
+    author_email='cerebro@ganymede.eu',
+    scripts=['armada-heal'],
+)
