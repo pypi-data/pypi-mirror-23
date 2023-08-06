516fac764f267afc5856ed5664de6607b62c4845
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:35:52 +0000

    Add package mlogging/0.1/mlogging-0.1.tar.gz

---
+++ packages/mlogging/mlogging-0.1.tar.gz/mlogging-0.1/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name='mlogging',
+    version='0.1',
+    description='logging handler for the multiprocess',
+    author='Kies Lee',
+    author_email='lleelm@gmail.com',
+    packages=['mlogging'],
+    )
