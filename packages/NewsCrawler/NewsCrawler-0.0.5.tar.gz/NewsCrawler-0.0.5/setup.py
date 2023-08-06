cfb5eca4bbf0a1b159a35aec5675523b47e38452
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:56:41 +0000

    Add package NewsCrawler/0.0.5/NewsCrawler-0.0.5.tar.gz

---
+++ packages/NewsCrawler/NewsCrawler-0.0.5.tar.gz/NewsCrawler-0.0.5/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='NewsCrawler',
+    version='0.0.5',
+    packages=['crawler', 'config'],
+    package_data={'': ['config.yaml']},
+    scripts=['crawler/news_crawler.py'],
+    install_requires=['pyyaml'],
+    author='Ash Prince',
+    author_email='i7629228@bournemouth.ac.uk',
+    description='Creates yaml logs of latest article and twitter data'
+)
