9e4c1c9e8c37c8c39bb6d3961cf36f810606ee67
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:41 +0000

    Add package btrfsmaint/0.5.1/btrfsmaint-0.5.1.tar.gz

---
+++ packages/btrfsmaint/btrfsmaint-0.5.1.tar.gz/btrfsmaint-0.5.1/README.rst
@@ -0,0 +1,44 @@
+BTRFSMAINT
+==========
+
+This is a quick project based upon Marc MERLIN's
+```btrfscrub.sh`` <http://marc.merlins.org/linux/scripts/btrfs-scrub>`__.
+Fedora 24 doesn't have ``shlock`` in the INN package, and I decided to
+reimplement the script in Python.
+
+Installation
+------------
+
+Download the script, place in /usr/local/bin, setup a monthly cronjob to
+execute the script.
+
+Compatibility
+-------------
+
+-  Python 2.6+
+-  Python 3.0+
+-  PyPy 5+
+
+Library Dependencies
+--------------------
+
+-  `Click <http://click.pocoo.org>`__
+
+TODO
+----
+
+-  Perhaps a mutex of some sort or locking like unto shlock
+-  Make some default scripts - cron, systemd, etc.
+
+Files
+-----
+
+::
+
+    ├── btrfscrub.sh       # Reference Copy of Marc MERLIN's Script (Apache 2.0 License)
+    ├── btrfsmaint.py      # btrfsmaint script
+    ├── btrfstest.py       # function test script.
+    ├── README.md          # This File
+    ├── LICENSE            # GPL v2.0
+    ├── Copyright          # Copyright Statement
+    └── tests/             # Test output.
---
+++ packages/btrfsmaint/btrfsmaint-0.5.1.tar.gz/btrfsmaint-0.5.1/setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+    
+setup(
+    version="0.5.1",
+    name="btrfsmaint",
+    description='BTRFS Filesystem Maintenace Scripts.',
+    long_description=readme(),
+    url='https://github.com/joshland/btrfsmaint',
+    author='Joshua M. Schmidlkofer',
+    author_email='joshland@gmail.com',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: System Administrators',
+        'Topic :: System :: Filesystems',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+    install_requires=[
+        "click",
+    ],
+    keywords='btrfs maintenance administration',
+    packages=['btrfsmaint'],
+    platforms=['any'],
+    entry_points={
+        'console_scripts': [
+            'btrfsmaint=btrfsmaint:Main',
+        ],
+    },
+)
