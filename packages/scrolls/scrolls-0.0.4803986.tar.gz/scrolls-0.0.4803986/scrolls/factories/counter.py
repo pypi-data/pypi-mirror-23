c71ce7a058fc2bfd35b0ba3e2957fd875c8774ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:23 +0000

    Add package scrolls/0.0.4803986/scrolls-0.0.4803986.tar.gz

---
+++ packages/scrolls/scrolls-0.0.4803986.tar.gz/scrolls-0.0.4803986/scrolls/factories/counter.py
@@ -0,0 +1,17 @@
+from scrolls.models.counter import Counter
+
+
+class CounterFactory(object):
+    """Builds :class:`Counter` objects"""
+
+    def __init__(self, dependencies):
+        pass
+
+    def byHostname(self):
+        return Counter('hostname')
+
+    def byApp(self):
+        return Counter('app')
+
+    def byHour(self):
+        return Counter('hour', '%Y%m%d%H')
