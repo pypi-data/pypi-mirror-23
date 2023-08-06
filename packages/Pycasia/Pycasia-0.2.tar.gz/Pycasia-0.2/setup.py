06eabeda8a1377a1d67300a631249ef6cf06f41f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:08:59 +0000

    Add package Pycasia/0.2/Pycasia-0.2.tar.gz

---
+++ packages/Pycasia/Pycasia-0.2.tar.gz/Pycasia-0.2/setup.py
@@ -0,0 +1,25 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(
+    name = "Pycasia",
+    packages = find_packages(exclude=['contrib', 'docs', 'tests*']),
+    version = "v0.2",
+    description = "Pycasia is an open source library to work with the CASIA Chinese Handwriting library.",
+    author = "Lucas Kjaero",
+    author_email = "Lucas@LucasKjaero.com",
+    url = "https://github.com/lucaskjaero/pycasia",
+    keywords = ["handwriting", "Chinese", "CASIA"],
+    classifiers = [
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Intended Audience :: Developers",
+        "Development Status :: 2 - Pre-Alpha",
+        "License :: OSI Approved :: Apache Software License",
+        "Natural Language :: Chinese (Simplified)",
+        "Operating System :: OS Independent",
+        "Topic :: Text Processing :: Linguistic",
+    ],
+    long_description=open('README.txt').read(),
+    install_requires=['numpy', 'Pillow', 'scipy', 'tqdm'],
+)
