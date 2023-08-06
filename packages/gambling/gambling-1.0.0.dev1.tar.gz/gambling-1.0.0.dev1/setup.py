7f40b235bec0e1e24e9707747d4d8f3df60034a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:14:46 +0000

    Add package gambling/1.0.0.dev1/gambling-1.0.0.dev1.tar.gz

---
+++ packages/gambling/gambling-1.0.0.dev1.tar.gz/gambling-1.0.0.dev1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+setup(
+    name='gambling',
+    version='1.0.0.dev1',
+    url='https://github.com/SubZeb/gambling',
+    license='MIT',
+    author='CJ Stein',
+    author_email='cjstein9@gmail.com',
+    description='A package to create the necessary components for gambling including cards and dice.',
+    keywords='cards card dice gambling poker blackjack',
+    packages=find_packages()
+)
