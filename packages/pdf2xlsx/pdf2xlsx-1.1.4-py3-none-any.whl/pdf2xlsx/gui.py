97616f7d5f740e2408545f6b45b616437fb30d2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:36:20 +0000

    Add package pdf2xlsx/1.1.4/pdf2xlsx-1.1.4-py3-none-any.whl

---
+++ packages/pdf2xlsx/pdf2xlsx-1.1.4-py3-none-any.whl/pdf2xlsx/gui.py
@@ -0,0 +1,252 @@
+# -*- coding: utf-8 -*-
+"""
+Not so simple tkinter based gui around the pdf2xlsx.do_it function.
+"""
+from tkinter import Tk, ttk, filedialog, messagebox, StringVar, Toplevel, END
+import os
+import shutil
+from .managment import do_it, do_it2
+from .config import config
+
+__version__ = '0.2.0'
+
+
+class ConfOption:
+    """
+    This widget is used to place the configuration options to the ConfigWindow. It contains
+    a label to show what is the configuration and an entry with StringVar to provide override
+    possibility. The value of the config :class:`JsonDict` is converted to a string for the entry.
+    If the value of a configuration is a list, it is converted to a comma separated string.
+
+    :param Frame root: Tk parent frame
+    :param str key: Key to the "config" :class:`JsonDict`
+    :param int row: Parameter for grid window manager
+    """
+    def __init__(self, root, key, row):
+        self.key = key
+        dict_value = config[key]
+        ttk.Label(root, text=dict_value['text']).grid(row=row, column=0, sticky='w')
+        self.sv = StringVar()
+        if isinstance(dict_value['value'], list):
+            self.sv.set(", ".join(map(str, dict_value['value'])))
+        else:
+            self.sv.set(str(dict_value['value']))
+        self.entry = ttk.Entry(root, textvariable=self.sv, width=54)
+        self.entry.grid(row=row, column=1, sticky='e')
+        if dict_value['conf_method'] == 'filedialog':
+            ttk.Button(root, text='Sel',
+                       command=self.browse_callback,
+                       width=4).grid(row=row, column=2, sticky='w')
+
+    def update_config(self):
+        """
+        Write the current entry value to the configuration. The original type of the
+        config value is checked, and the string is converted to this value (int, list of
+        int, list of string...)
+        """
+        if isinstance(config[self.key]['value'], list):
+            if isinstance(config[self.key]['value'][0], int):
+                config[self.key]['value'] = list(map(int, self.sv.get().split(', ')))
+            else:
+                config[self.key]['value'] = self.sv.get().split(', ')
+        elif isinstance(config[self.key]['value'], int):
+            config[self.key]['value'] = int(self.sv.get())
+        else:
+            config[self.key]['value'] = self.sv.get()
+
+    def browse_callback(self):
+        """
+        Asks for the source zip file, the opened dialog filters for zip files by default
+        The src_entry attribute is updated based on selection
+        """
+        path = filedialog.askopenfilename(initialdir='.\\',
+                                          title="Choose file...",)
+        self.entry.delete(0, END)
+        self.entry.insert(0, path)
+
+
+class ConfigWindow:
+    """
+    Sub window for settings. The window is hidden by default, when the user clicks  to the settings
+    button it is activated. It contains the configuration options.
+    There are two buttons the Save ( which hides the window ), and the Accept, both of them updates
+    the configuration file. The configuration items are stored in a list.
+
+    :param master: Tk parent class
+    """
+    def __init__(self, master):
+        self.master = master
+        self.window = Toplevel(self.master)
+        self.window.resizable(False, False)
+        self.window.withdraw()
+        self.window.protocol("WM_DELETE_WINDOW", self._on_closing)
+        self.window.title('Settings...')
+        self.conf_list = []
+
+        self.main_frame = ttk.Frame(self.window)
+        self.main_frame.pack(padx=5, pady=5)
+        self.main_frame.grid_columnconfigure(1, minsize=20, weight=1)
+
+        ttk.Label(self.main_frame, text='Configuration:').grid(row=0, column=0,
+                                                               columnspan=2, sticky='w')
+
+        row = 1
+        for conf_element in config:
+            self.conf_list.append(
+                ConfOption(root=self.main_frame, key=conf_element, row=row))
+            row += 1
+
+        ttk.Button(self.main_frame, text='Save',
+                   command=self.save_callback).grid(row=row, column=0, sticky='e')
+
+        ttk.Button(self.main_frame, text='Accept',
+                   command=self.accept_callback).grid(row=row, column=1, sticky='w')
+
+    def save_callback(self):
+        """
+        Hides the ConfigWindow and updates and stores the configuration
+        """
+        self.window.withdraw()
+        self.accept_callback()
+
+    def accept_callback(self):
+        """
+        Goes through on every configuration item and updates them one by one. Stores the updated
+        configuration.
+        """
+        for conf in self.conf_list:
+            conf.update_config()
+        config.store()
+
+    def _on_closing(self):
+        self.window.withdraw()
+
+
+class PdfXlsxGui:
+    """
+    Simple GUI which lets the user select the source file zip and the destination directory
+    for the xlsx file. Contains a file dialog for selecting the zip file to work with.
+    There is a button to start the conversion, and also a Settings button to open the
+    settings window
+
+    :param master: Tk parent class
+    """
+
+    def __init__(self, master):
+        self.master = master
+        self.master.title('Convert Zip -> Pdf -> Xlsx')
+        self.master.resizable(False, False)
+
+        self.main_frame = ttk.Frame(self.master)
+        self.main_frame.pack(padx=5, pady=5)
+
+        self.option_list = ["zip pdf to xlsx", "order details"]
+        self.selected_task = StringVar(self.main_frame)
+        self.selected_task.set("zip Pdf to xlsx")  # default value
+
+        self.box = ttk.Combobox(self.main_frame, textvariable=self.selected_task, values=self.option_list)
+        self.box.bind("<<ComboboxSelected>>", self.update_task)
+        self.box.grid(row=0, column=0, columnspan=2)
+
+        self.task_do = self.unknown_task
+
+        ttk.Label(self.main_frame, text='Source File:').grid(row=1, column=0, sticky='w')
+        self.src_entry = ttk.Entry(self.main_frame, width=54)
+        self.src_entry.grid(row=1, column=0, sticky='e')
+        self.src_entry.insert(0, '.\\src.zip')
+        ttk.Button(self.main_frame, text='Browse...',
+                   command=self.browse_src_callback).grid(row=1, column=1, sticky='w')
+
+        ttk.Button(self.main_frame, text='Start conversion',
+                   command=self.execute_task).grid(row=5, column=0, sticky='w')
+
+        ttk.Button(self.main_frame, text='Settings',
+                   command=self.config_callback).grid(row=5, column=1, columnspan=1, sticky='e')
+
+        self.config_window = ConfigWindow(self.master)
+        self.filetypes = (("zip files", "*.zip"), ("all files", "*.*"))
+
+    def update_task(self, event):
+        print(event.widget.get())
+        if event.widget.get() == self.option_list[0]:
+            self.task_do = self.process_pdf
+            self.filetypes = (("zip files", "*.zip"), ("all files", "*.*"))
+        elif event.widget.get() == self.option_list[1]:
+            self.task_do = self.convert_xlsx
+            self.filetypes = (("xlsx files", "*.xlsx"), ("all files", "*.*"))
+        else:
+            self.task_do = self.unknown_task
+
+    def config_callback(self):
+        """
+        Bring the configuration window up
+        """
+        self.config_window.window.state('normal')
+        self.config_window.window.lift(self.master)
+
+    def browse_src_callback(self):
+        """
+        Asks for the source zip file, the opened dialog filters for zip files by default
+        The src_entry attribute is updated based on selection
+        """
+        path = filedialog.askopenfilename(initialdir=config['last_path']['value'],
+                                          title="Choose the Zip file...",
+                                          filetypes=self.filetypes)
+        config['last_path']['value'] = os.path.dirname(path)
+        config.store()
+        self.src_entry.delete(0, END)
+        self.src_entry.insert(0, path)
+
+    def execute_task(self):
+        self.task_do()
+
+    def process_pdf(self):
+        """
+        Facade for the do_it function. Only the src file and destination dir is updated
+        the other parameters are left for defaults.
+        """
+        try:
+            logger = do_it(src_name=self.src_entry.get(),
+                           dst_dir=config['tmp_dir']['value'],
+                           xlsx_name=config['xlsx_name']['value'],
+                           tmp_dir=config['tmp_dir']['value'],
+                           file_extension=config['file_extension']['value'])
+            # tmp_str = '{1} Invoices were found with the following number of Entries:\n{0!s}'
+            # messagebox.showinfo(title='Conversion Completed',
+            #                    message=tmp_str.format(logger, len(logger.invo_list)))
+        except PermissionError as exc:
+            messagebox.showerror('Exception', exc)
+
+    def convert_xlsx(self):
+        print("Convert those xlsx: {}".format(self.box.get()))
+        try:
+            logger = do_it2(src_name=self.src_entry.get(),
+                            dst_dir=config['tmp_dir']['value'],
+                            xlsx_name=config['xlsx_name']['value'],
+                            tmp_dir=config['tmp_dir']['value'])
+            # tmp_str = '{1} Invoices were found with the following number of Entries:\n{0!s}'
+            # messagebox.showinfo(title='Conversion Completed',
+            #                    message=tmp_str.format(logger, len(logger.invo_list)))
+        except PermissionError as exc:
+            messagebox.showerror('Exception', exc)
+
+    def unknown_task(self):
+        print("Unknown task selected: {}".format(self.box.get()))
+
+
+
+
+
+def main():
+    root = Tk()
+
+    def _post_clean_up():
+        shutil.rmtree(config['tmp_dir']['value'])
+        root.destroy()
+
+    root.protocol("WM_DELETE_WINDOW", _post_clean_up)
+    gui = PdfXlsxGui(root)
+    root.mainloop()
+
+if __name__ == '__main__':
+    main()
---
+++ packages/pdf2xlsx/pdf2xlsx-1.1.4-py3-none-any.whl/pdf2xlsx/managment.py
@@ -0,0 +1,216 @@
+# -*- coding: utf-8 -*-
+"""
+Contains framework to upen Zip a zip file which contains multiple pdf files
+representing invoices. The invoices are parsed into Invoice and Entry (invoice
+entries) classes. These are converted to XLSX format.
+"""
+
+import os
+import shutil
+import zipfile
+from subprocess import run
+from PyPDF2 import PdfFileReader
+from openpyxl import Workbook
+from .logger import StatLogger
+from .config import config
+from .invoice import EntryTuple, invo_parser
+from .utility import list2row
+from .order_detail_xlsx_parse import GetOrderDetail, read_xlsx, write_xlsx
+
+#[TODO] Put this to a manager class???
+def pdf2rawtxt(pdfile, logger):
+    """
+    Read out the given pdf file to Invoice and Entry classes to parse it. Utilize
+    PyPFD2 PdfFileReader. Go through every page of the pdf. When a new invoice
+    entry was found by the Entry.parse_line it is appended to the Invoice.entries
+
+    :param str pdfile: file path of the pdf to process
+    :param logger: :class:`StatLogger`, collect statistical data about parsing
+
+    :return: The invoice entry filled up with the information from pdf file
+    :rtype: :class:`Invoice`
+    """
+    with open(pdfile, 'rb') as filedesc:
+        return invo_parser(PdfFileReader(filedesc), logger)
+
+def _init_clean_up(tmp_dir='tmp'):
+    """
+    Create tmp directory, delete it first if it already exists, if possible
+
+    :param str tmp_dir: this is the directory to work during the execution
+    """
+    try:
+        shutil.rmtree(tmp_dir)
+    except FileNotFoundError:
+        #Do not print any silly message as this is the expected behaviour
+        pass
+    finally:
+        os.mkdir(tmp_dir)
+
+def extract_zip(src_name, directory):
+    """
+    Extract the pdf files from the zip, there is no sanity check for the arguments
+    :param str src_name: Path to a zip file to extract
+    :param str dir: Path to the target directory to extract the zip file
+    """
+    with zipfile.ZipFile(src_name) as myzip:
+        myzip.extractall(directory)
+
+def get_pdf_files(directory, extension='.pdf'):
+    """
+    Walks through the given **dir** and collects every files with **extension**
+
+    :param str dir: the root directory to start the walk
+    :param str extension: '.pdf' by default, if the file has this extension it is selected
+
+    :return: list of pdf file path
+    :rtype: list of str
+    """
+    pdf_list = []
+    for dir_path, _dummy, file_list in os.walk(directory):
+        for filename in file_list:
+            if filename.endswith(extension):
+                pdf_list.append(os.path.join(dir_path, filename))
+    return pdf_list
+
+def extract_invoces(pdf_list, logger):
+    """
+    Get the invoices from the pdf files in th pdf_list
+    Wrapper around the pdf2rawtxt call
+
+    :param list pdf_list: List of pdf files path to process.
+    :param logger: :class:`StatLogger`, collect statistical data about parsing
+
+    :return: list of invoices
+    :rtype: list of :class:`Invoice`
+    """
+    invoice_list = []
+    for pdfile in pdf_list:
+        invoice_list.append(pdf2rawtxt(pdfile, logger))
+    return invoice_list
+
+def _post_clean_up(tmp_dir='tmp'):
+    """
+    Cleanup after execution, remove the extracted zip file and tmp directory
+
+    :param str tmp_dir: Temporary directory to clean_up
+    """
+    shutil.rmtree(tmp_dir)
+
+
+def invoices2xlsx(invoices, directory='', name='Invoices01.xlsx'):
+    """
+    Write invoice information to xlsx template file. Go through every invoce and
+    write them out. Simple. Utilizes the openpyxl module
+
+    :param invoices list of Invocie: Representation of invoices from the pdf files
+    """
+    workbook = Workbook()
+    worksheet_invo = workbook.active
+    worksheet_entr = workbook.create_sheet()
+    row_invo = col_invo = row_entr = col_entr = 0
+
+    labels = ["Invoice Number", "Date of Invoice", "Payment Date", "Amount"]
+    positions = config['invo_header_ident']['value']
+    row_invo, col_invo = list2row(worksheet_invo, row_invo,
+                                  col_invo, labels, positions)
+
+    labels = ["Invoice Number"] + list(EntryTuple._fields)
+    row_entr, col_entr = list2row(worksheet_entr, row_entr,
+                                  col_entr, labels)
+    #[TODO] there is no specification how to write out invocie entries yet
+    for invo in invoices:
+        row_invo, col_invo = invo.xlsx_write(worksheet_invo, row_invo, col_invo)
+        for entr in invo.entries:
+            row_entr, col_entr = entr.xlsx_write(worksheet_entr, row_entr, col_entr)
+    #increase the invoice tab B column width to show the whole id
+    worksheet_invo.column_dimensions['B'].width = 15
+
+    workbook.save(os.path.join(directory, name))  
+
+def run_excel(xlsx_path):
+    """
+    Start up Excel, with the file from the argument. The location of the excel
+    executable should be set in the configuration
+
+    :param str xlsx_path: Path to the xlsx file to open
+    """
+    run([config['excel_path']['value'], xlsx_path])
+
+
+def do_it(src_name, dst_dir='', xlsx_name='Invoices01.xlsx',
+          tmp_dir='tmp', file_extension='.pdf'):
+    """
+    Main script to manage the zip to xls process. It is responsible to create/cleanup
+    temporary directories and files. After zip extraction, seraches every file which
+    ends with `file_extension` Then it builds up a list of invoices and writes them
+    to xlsx format and opens it up in the predefined xlsx_viewer. If the dst_dir happens
+    to be the same as the tmp_dir, the generated xlsx file is removed after the run.
+
+    :param str src_name: path to the zip file to extract
+    :param str dst_dir: path to the directory to put the generated xlsx file by default
+        the cwd
+    :param str tmp_dir: temporary directory to work in. **This directory is erased
+        at the beginning of the script** By default it is `tmp`
+    :param str file_extension: the file extension to use during file selection. By
+        default it is `.pdf`
+    :param str xlsx_name: Name of the oputput file
+    """
+    _init_clean_up(tmp_dir)
+
+    extract_zip(src_name, tmp_dir)
+
+    pdf_list = get_pdf_files(os.path.join(os.getcwd(), tmp_dir), file_extension)
+
+    logger = StatLogger()
+
+    invoice_list = extract_invoces(pdf_list, logger)
+
+    invoices2xlsx(invoice_list, dst_dir, name=xlsx_name)
+
+    run_excel(os.path.join(dst_dir, config['xlsx_name']['value']))
+
+    print(logger)
+
+    print("script has been finished")
+    return logger
+
+
+def do_it2(src_name, dst_dir='', xlsx_name='Invoices01.xlsx',
+           tmp_dir='tmp'):
+    """
+    Main script to manage the zip to xls process. It is responsible to create/cleanup
+    temporary directories and files. After zip extraction, seraches every file which
+    ends with `file_extension` Then it builds up a list of invoices and writes them
+    to xlsx format and opens it up in the predefined xlsx_viewer. If the dst_dir happens
+    to be the same as the tmp_dir, the generated xlsx file is removed after the run.
+
+    :param str src_name: path to the zip file to extract
+    :param str dst_dir: path to the directory to put the generated xlsx file by default
+        the cwd
+    :param str tmp_dir: temporary directory to work in. **This directory is erased
+        at the beginning of the script** By default it is `tmp`
+    :param str xlsx_name: Name of the oputput file
+    """
+    _init_clean_up(tmp_dir)
+
+    order_list = read_xlsx(GetOrderDetail, src_name)
+
+    write_xlsx(order_list, filename=os.path.join(dst_dir, config['xlsx_name']['value']))
+
+    run_excel(os.path.join(dst_dir, config['xlsx_name']['value']))
+
+    print("Order detail extraction script has been finished")
+    return True
+
+
+def main():
+    """
+    A simple wrapper around do it function, to demonstrate its usage
+    """
+    do_it(src_name='src.zip', dst_dir='', xlsx_name=config['xlsx_name']['value'],
+          tmp_dir=config['tmp_dir'][0], file_extension=config['file_extension']['value'])
+
+
+if __name__ == '__main__':
+    main()
