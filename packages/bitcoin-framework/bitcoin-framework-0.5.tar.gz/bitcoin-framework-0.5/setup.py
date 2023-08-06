6b16d2a5c853fccea594e8e4a0daa188b10ba4ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:34:11 +0000

    Add package bitcoin-framework/0.5/bitcoin-framework-0.5.tar.gz

---
+++ packages/bitcoin-framework/bitcoin-framework-0.5.tar.gz/bitcoin-framework-0.5/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+setup(
+  name = 'bitcoin-framework',
+  packages = find_packages(),
+  version = '0.5',
+  description = 'Python Bitcoin framework to create transactions with smart contracts based on puzzle-friendliness and OOP principles',
+  license = 'Apache License v2.0',
+  author = 'UAB Projects Team: davidlj95 & ccebrecos',
+  author_email = 'mail@uab.codes',
+  url = 'https://github.com/uab-projects/bitcoin-framework',
+  download_url = 'https://github.com/uab-projects/bitcoin-framework/archive/0.5.tar.gz',
+  keywords = ['bitcoin', 'bitcoin framework', 'bitcoin-framework','bitcoin api','bitcoin-api'],
+  install_requires=['python-bitcoinlib','bitcoin'],
+  classifiers = []
+)
