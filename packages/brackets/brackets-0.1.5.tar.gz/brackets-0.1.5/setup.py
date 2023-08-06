19459fd85332b36360371959680eb8826f00c1b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:34:17 +0000

    Add package brackets/0.1.5/brackets-0.1.5.tar.gz

---
+++ packages/brackets/brackets-0.1.5.tar.gz/brackets-0.1.5/setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup
+
+setup(
+    name='brackets',
+    version='0.1.5',
+    author='Pooya Eghbali',
+    author_email='persian.writer@gmail.com',
+    packages=['brackets'],
+    url='https://github.com/pooya-eghbali/brackets',
+    license='BSD',
+    description="""Use brackets instead of indentation.""",
+    classifiers= ['Intended Audience :: Developers',
+                  'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',
+                  'Operating System :: OS Independent',
+                  'Programming Language :: Python :: 2',
+                  'Programming Language :: Python :: 3'],
+    keywords = 'brackets, indentation, indent, indenting, parser, encoding',
+	platforms = ["Any"],
+	long_description=open('README.txt').read()
+)
