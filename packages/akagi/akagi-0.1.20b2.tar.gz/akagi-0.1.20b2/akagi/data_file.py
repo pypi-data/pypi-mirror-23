a33cf61d51c928cf837bbf306ba6ba6fa2994a45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:16:12 +0000

    Add package akagi/0.1.20b2/akagi-0.1.20b2.tar.gz

---
+++ packages/akagi/akagi-0.1.20b2.tar.gz/akagi-0.1.20b2/akagi/data_file.py
@@ -0,0 +1,31 @@
+import os
+from abc import ABCMeta, abstractproperty
+
+
+class DataFile(metaclass=ABCMeta):
+    def __next__(self):
+        raise NotImplementedError
+
+    def __iter__(self):
+        return iter(self.iterator_class(self.content))
+
+    def _is_gzip(self):
+        return os.path.splitext(self.filename)[-1] == '.gz'
+
+    @abstractproperty
+    def filename(self):
+        '''Retrieve filename'''
+
+    @abstractproperty
+    def key(self):
+        '''Retrieve key (used in caching function)'''
+
+    @abstractproperty
+    def content(self):
+        '''Content in data file.
+        Compressed archives will be automatically decompressed.
+        '''
+
+    @property
+    def raw_content(self):
+        '''Raw content in data file'''
