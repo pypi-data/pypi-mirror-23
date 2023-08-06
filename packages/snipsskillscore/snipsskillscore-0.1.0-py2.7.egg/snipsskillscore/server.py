6bf425dddc9196190afca3daff1897aa013e978e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:08:14 +0000

    Add package snipsskillscore/0.1.0/snipsskillscore-0.1.0-py2.7.egg

---
+++ packages/snipsskillscore/snipsskillscore-0.1.0-py2.7.egg/snipsskillscore/server.py
@@ -0,0 +1,11 @@
+# -*-: coding utf-8 -*-
+""" Snips core server. """
+
+class Server():
+    """ Snips core server. """
+
+    def __init__(self):
+        pass
+
+    def start(self):
+        pass
