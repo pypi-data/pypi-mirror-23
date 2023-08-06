00bf8585c8d32e237eee9ffa8c5a2508a1c6a0da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:52 +0000

    Add package pytablereader/0.12.3/pytablereader-0.12.3.tar.gz

---
+++ packages/pytablereader/pytablereader-0.12.3.tar.gz/pytablereader-0.12.3/pytablereader/error.py
@@ -0,0 +1,101 @@
+# encoding: utf-8
+
+"""
+.. codeauthor:: Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
+"""
+
+from __future__ import absolute_import
+
+import requests
+
+
+class ValidationError(Exception):
+    """
+    Exception raised when data is not properly formatted.
+    """
+
+
+class InvalidNameError(Exception):
+    """
+    Base name error class.
+    """
+
+
+class InvalidTableNameError(InvalidNameError):
+    """
+    Exception raised when invalid table name used.
+    """
+
+
+class InvalidHeaderNameError(InvalidNameError):
+    """
+    Exception raised when table header name is invalid.
+    """
+
+
+class InvalidPathError(Exception):
+    """
+    Base path error class.
+    """
+
+
+class InvalidFilePathError(InvalidPathError):
+    """
+    Exception raised when invalid file path used.
+    """
+
+
+class InvalidUrlError(InvalidPathError):
+    """
+    Exception raised when invalid URL used.
+    """
+
+
+class InvalidDataError(ValueError):
+    """
+    Exception raised when data is invalid to load.
+    """
+
+
+class EmptyDataError(InvalidDataError):
+    """
+    Exception raised when data do not include valid table data.
+    """
+
+
+class OpenError(IOError):
+    """
+    Exception raised when failed to open a file.
+    """
+
+
+class LoaderNotFoundError(Exception):
+    """
+    Exception raised when appropriate loader not found.
+    """
+
+
+class HTTPError(requests.RequestException):
+    """
+    An HTTP error occurred.
+
+    .. seealso::
+
+        http://docs.python-requests.org/en/master/api/#exceptions
+    """
+
+
+class ProxyError(requests.exceptions.ProxyError):
+    """
+    A proxy error occurred.
+
+    .. seealso::
+
+        http://docs.python-requests.org/en/master/_modules/requests/exceptions/
+    """
+
+
+class PypandocImportError(ImportError):
+    """
+    Exception raised when import error occurred with pypandoc package.
+    """
