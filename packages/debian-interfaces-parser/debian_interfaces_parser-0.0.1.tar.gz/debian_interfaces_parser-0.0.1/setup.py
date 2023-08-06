62716527b661a30380bd02c575b463af6d22d43d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:56:11 +0000

    Add package debian-interfaces-parser/0.0.1/debian_interfaces_parser-0.0.1.tar.gz

---
+++ packages/debian-interfaces-parser/debian_interfaces_parser-0.0.1.tar.gz/debian_interfaces_parser-0.0.1/setup.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+
+setup(name='debian_interfaces_parser',
+      version='0.0.1',
+      description='Simple parser for /etc/network/interfaces',
+      url='http://github.com/olegantonyan/debian_interfaces_parser',
+      author='Oleg Antonyan',
+      author_email='oleg.b.antonyan@gmail.com',
+      license='MIT',
+      packages=['debian_interfaces_parser'],
+      zip_safe=False)
