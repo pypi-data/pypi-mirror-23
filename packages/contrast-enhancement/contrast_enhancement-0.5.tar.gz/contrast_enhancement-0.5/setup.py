5f31f238b982660d1b045e9c848fae43436c8b22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:11:39 +0000

    Add package contrast-enhancement/0.5/contrast_enhancement-0.5.tar.gz

---
+++ packages/contrast-enhancement/contrast_enhancement-0.5.tar.gz/contrast_enhancement-0.5/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+from setuptools import setup
+setup(
+  name = 'contrast_enhancement',
+  packages = ['ce'], 
+  version = '0.5',
+  description = 'Enhance image contrast using standard deviation stretch',
+  author = 'Ashvitha Shetty',
+  author_email = 'ashvithashetty26@gmail.com',
+  url = 'https://github.com/ashvithashetty/contrast_enhancement',
+  download_url = 'https://github.com/ashvithashetty/contrast_enhancement/archive/0.5.tar.gz', 
+  classifiers = [],
+)
