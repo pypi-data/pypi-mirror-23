23405f0d425b2616796b1efc53c66bc3e139f45d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:46:11 +0000

    Add package pyrgg/0.1/pyrgg-0.1.tar.gz

---
+++ packages/pyrgg/pyrgg-0.1.tar.gz/pyrgg-0.1/setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+setup(
+  name = 'pyrgg',
+  packages = ['pyrgg'],
+  version = '0.1',
+  description = 'Python Random Graph Generator',
+  long_description="Self Loop , Parallel Arc , Directed Graph , Signed",
+  author = 'Sepand Haghighi',
+  author_email = 'sepand@qpage.ir',
+  url = 'https://github.com/sepandhaghighi/pyrgg',
+  download_url = 'https://github.com/sepandhaghighi/pyrgg/tarball/v0.1',
+  keywords = ['random', 'graph', 'python3','python','generator',"graph-process","generator","moduland"],
+  classifiers = [],
+  license='MIT',
+)
