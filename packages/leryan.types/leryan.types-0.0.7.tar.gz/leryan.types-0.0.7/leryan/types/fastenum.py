849871563b9fb8e52a70876607649d2609018284
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:42 +0000

    Add package leryan.types/0.0.7/leryan.types-0.0.7.tar.gz

---
+++ packages/leryan.types/leryan.types-0.0.7.tar.gz/leryan.types-0.0.7/leryan/types/fastenum.py
@@ -0,0 +1,28 @@
+from future.utils import with_metaclass
+
+class FastEnumMeta(type):
+
+    def __new__(cls, name, bases, attrs):
+        return type.__new__(cls, name, bases, attrs)
+
+    def __init__(cls, name, bases, attrs):
+        return type.__init__(cls, name, bases, attrs)
+
+    def __setattr__(cls, attr, value):
+        raise Exception('Cannot reassign member value.')
+
+
+class FastEnum(with_metaclass(FastEnumMeta)):
+
+    """
+    Fast and simple Enum implementation.
+
+    .. code-block:: python
+        class MyEnum(FastEnum):
+
+            MEMBER = 'value'
+            OTHER_MEMBER = 0
+
+        MyEnum.MEMBER
+    """
+    pass
