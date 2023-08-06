ce0b8a506d12e7593bdbba2ea2a87884b69d7d9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:55 +0000

    Add package nsb/0.1.8/nsb-0.1.8.tar.gz

---
+++ packages/nsb/nsb-0.1.8.tar.gz/nsb-0.1.8/nsb/__main__.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+"""
+allskymaps python package.
+
+Draws Allsky Nightsky Images with stars from the GAIA catalog
+
+    This work has made use of data from the European Space Agency (ESA)
+    mission {\\it Gaia} (\\url{https://www.cosmos.esa.int/gaia}), processed by
+    the {\\it Gaia} Data Processing and Analysis Consortium (DPAC,
+    \\url{https://www.cosmos.esa.int/web/gaia/dpac/consortium}). Funding
+    for the DPAC has been provided by national institutions, in particular
+    the institutions participating in the {\\it Gaia} Multilateral Agreement.
+
+and draws moonlight corresponding to the Model from Krisciunas et al.
+
+   author = {{Krisciunas}, K. and {Schaefer}, B.~E.},
+    title = "{A model of the brightness of moonlight}",
+  journal = {\pasp},
+     year = 1991,
+    month = sep,
+   volume = 103,
+    pages = {1033-1039},
+      doi = {10.1086/132921},
+   adsurl = {http://adsabs.harvard.edu/abs/1991PASP..103.1033K},
+
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>
+
+Created and maintained by Matthias Buechele [FAU].
+
+"""
+
+from .nsb import main
+
+if __name__ == '__main__':
+    main()
