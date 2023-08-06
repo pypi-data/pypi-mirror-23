dc4f2732620acd9e72ef7b0732c0f298efe417ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:08:29 +0000

    Add package tsc/0.1/tsc-0.1.tar.gz

---
+++ packages/tsc/tsc-0.1.tar.gz/tsc-0.1/setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(name='tsc',
+      version='0.1',
+      description='TimeSeries Compressor',
+      author='Jingchao Hu',
+      author_email='jingchaohu@gmail.com',
+      url='http://github.com/observerss/tsc',
+      packages=find_packages(),
+      package_data={'tsc': ['*.pxd', '*.pyx', 'klib/*.h']},
+      include_package_data=True,
+      install_requires=['numpy', 'cython', 'brotli', 'pandas'],
+      classifiers=[
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+      ]
+     )
