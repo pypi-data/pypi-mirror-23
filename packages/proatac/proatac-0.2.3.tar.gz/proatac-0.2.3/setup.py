5d02e51ffa9ccb037469b685dcdeb92e303ad351
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:48:59 +0000

    Add package proatac/0.2.3/proatac-0.2.3.tar.gz

---
+++ packages/proatac/proatac-0.2.3.tar.gz/proatac-0.2.3/setup.py
@@ -0,0 +1,46 @@
+"""
+Preprocessing ATAC and scATAC Data
+"""
+from setuptools import find_packages, setup
+
+dependencies = ['click', 'Numpy', 'editdistance', 'pytest', 'python-levenshtein', 'snakemake', 'biopython', 'optparse-pretty', 'regex', 'PyYAML']
+
+setup(
+    name='proatac',
+    version='0.2.3',
+    url='https://github.com/buenrostrolab/proatac',
+    license='MIT',
+    author='Caleb Lareau',
+    author_email='caleblareau@g.harvard.edu',
+    description='Processing and quality control of ATAC data.',
+    long_description=__doc__,
+    packages=find_packages(exclude=['tests']),
+    include_package_data=True,
+    zip_safe=False,
+    platforms='any',
+    install_requires=dependencies,
+    entry_points={
+        'console_scripts': [
+            'proatac = proatac.cli:main',
+        ],
+    },
+    classifiers=[
+        # As from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+        # 'Development Status :: 1 - Planning',
+         'Development Status :: 2 - Pre-Alpha',
+        # 'Development Status :: 3 - Alpha',
+        # 'Development Status :: 4 - Beta',
+        # 'Development Status :: 5 - Production/Stable',
+        # 'Development Status :: 6 - Mature',
+        # 'Development Status :: 7 - Inactive',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: POSIX',
+        'Operating System :: MacOS',
+        'Operating System :: Unix',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
+)
