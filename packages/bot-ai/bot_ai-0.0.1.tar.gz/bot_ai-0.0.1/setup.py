525c2c99d66fc0ccd234d036f632d3d66b78e37f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:12 +0000

    Add package bot-ai/0.0.1/bot_ai-0.0.1.tar.gz

---
+++ packages/bot-ai/bot_ai-0.0.1.tar.gz/bot_ai-0.0.1/setup.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+
+# with open('README.rst') as f:
+#     readme = f.read()
+#
+# with open('LICENSE') as f:
+#     license = f.read()
+
+# setup(
+#     name='sample',
+#     version='0.1.0',
+#     description='Sample package for Python-Guide.org',
+#     long_description=readme,
+#     author='Kenneth Reitz',
+#     author_email='me@kennethreitz.com',
+#     url='https://github.com/kennethreitz/samplemod',
+#     license=license,
+#     packages=find_packages(exclude=('tests', 'docs'))
+# )
+setup(
+    name='bot_ai',
+    version='0.0.1',
+    description='Artificial Intelligence',
+    author='Fernando Oliveira',
+    author_email='fernando.oliveira@virtus.ufcg.edu.br',
+    url='https://github.com/mateuscomp/bot_ai',
+    packages=find_packages(exclude=('tests', 'docs'))
+)
