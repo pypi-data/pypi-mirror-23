6f801d6bb74e73c3559f69009c2460059b17f43f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:22 +0000

    Add package statscraper/1.0.0.dev1/statscraper-1.0.0.dev1-py2.py3-none-any.whl

---
+++ packages/statscraper/statscraper-1.0.0.dev1-py2.py3-none-any.whl/statscraper/__init__.py
@@ -0,0 +1,11 @@
+# Exceptions
+from .base_scraper import NoSuchItem, InvalidData
+
+# Classes
+from .base_scraper import Result, ResultSet, Itemslist, Dimension, DimensionValue
+from .base_scraper import Item, Collection, Dataset
+from .base_scraper import BaseScraper
+from .datatypes import Datatype, NoSuchDatatype
+
+# Contants
+from .base_scraper import ROOT, TYPE_DATASET, TYPE_COLLECTION
