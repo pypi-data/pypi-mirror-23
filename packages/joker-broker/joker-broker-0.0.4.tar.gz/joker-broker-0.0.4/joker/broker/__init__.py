0c14ccd0c6ac5a56ec4dc870d5860362c583973d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:14:04 +0000

    Add package joker-broker/0.0.4/joker-broker-0.0.4.tar.gz

---
+++ packages/joker-broker/joker-broker-0.0.4.tar.gz/joker-broker-0.0.4/joker/broker/__init__.py
@@ -0,0 +1,24 @@
+from joker.broker.resource import ResourceBroker
+
+
+__version__ = '0.0.4'
+
+
+rb = None
+
+
+def get_rb(path=None):
+    import os
+    from joker.broker import ResourceBroker
+    global rb
+    if not path:
+        path = os.path.expanduser('~/.joker/broker.yml')
+    if isinstance(rb, ResourceBroker):
+        return rb
+    rb = ResourceBroker.create(path)
+    return rb
+
+
+def induct():
+    from joker.broker.userdir import dump_default_conf
+    dump_default_conf()
