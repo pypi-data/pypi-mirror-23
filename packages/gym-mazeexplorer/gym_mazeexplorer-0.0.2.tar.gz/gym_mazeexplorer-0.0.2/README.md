4408c3d18d58ccab17f4456ad04ad631a7f574b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:10:28 +0000

    Add package gym-mazeexplorer/0.0.2/gym_mazeexplorer-0.0.2.tar.gz

---
+++ packages/gym-mazeexplorer/gym_mazeexplorer-0.0.2.tar.gz/gym_mazeexplorer-0.0.2/README.md
@@ -0,0 +1,57 @@
+# gym-mazeexplorer
+
+A maze exploration environment for OpenAIGym wrapping [maze_explorer](https://github.com/mryellow/maze_explorer).
+
+## Installation
+
+### Source
+
+```bash
+git clone https://github.com/mryellow/gym-mazeexplorer.git
+cd gym-mazeexplorer
+pip install -e .
+```
+
+### Package
+
+* Note: Not registered yet.
+
+```bash
+pip install gym_mazeexplorer
+```
+
+## Quick example
+
+```python
+
+  import gym
+  import gym_mazeexplorer
+
+  env = gym.make('MazeExplorer-v0')
+  env.reset()
+
+  for _ in range(50):
+    env.render()
+    action = env.action_space.sample()
+    observation, reward, done, info = env.step(action)
+```
+
+## Test
+
+```bash
+python -m unittest discover
+```
+
+## Cite
+
+If you use Maze Explorer in your academic research, we would be grateful if you could cite it as follows:
+
+```
+@misc{king2017mazeexplorer,
+    author = {King},
+    title = {Maze Explorer: A maze exploration game for AI agents},
+    howpublished={Web page},
+    url = {https://github.com/mryellow/maze_explorer},
+    year = {2017}
+}
+```
---
+++ packages/gym-mazeexplorer/gym_mazeexplorer-0.0.2.tar.gz/gym_mazeexplorer-0.0.2/setup.py
@@ -0,0 +1,37 @@
+from setuptools import setup
+from setuptools import find_packages
+
+setup(name='gym_mazeexplorer',
+    version='0.0.2',
+    author='Mr-Yellow',
+    author_email='mr-yellow@mr-yellow.com',
+    description='A maze exploration environment for openai/gym',
+    packages=find_packages(),
+    url='https://github.com/mryellow/gym-mazeexplorer',
+    license='MIT',
+    install_requires=['gym', 'mazeexp'],
+    keywords='maze, game, maze-explorer, openaigym, openai-gym',
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: Artificial Intelligence',
+
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: MIT License',
+
+        # Specify the Python versions you support here. In particular, ensure
+        # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+    ],
+)
+
+
+#package_dir={'gym_mazeexplorer' : 'gym_mazeexplorer/envs'},
