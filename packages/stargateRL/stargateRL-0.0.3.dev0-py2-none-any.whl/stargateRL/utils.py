18834360caa96a9f6b96910a83bd08d737cf477b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:15:46 +0000

    Add package stargateRL/0.0.3.dev0/stargateRL-0.0.3.dev0-py2-none-any.whl

---
+++ packages/stargateRL/stargateRL-0.0.3.dev0-py2-none-any.whl/stargateRL/utils.py
@@ -0,0 +1,25 @@
+"""Contains useful methods and global variables."""
+import pyglet
+
+from stargateRL.engine.graphx import GxTileset
+from stargateRL.paths import DirectoryPaths
+from stargateRL.launcher.utils import load_config
+
+# Reset pyglet resources path then append custom ones
+pyglet.resource.path = []
+pyglet.resource.path.append((DirectoryPaths.BIN))
+pyglet.resource.path.append((DirectoryPaths.TILES))
+
+
+# Load the config
+CONFIG = load_config()
+
+# Load the tileset
+GX_TILESETS = {'MAIN': GxTileset(CONFIG['resources']['tileset'],
+                                 CONFIG['resources']['size'])}
+
+# Graphical settings
+INTENDED_SIZE = 16
+GL_SCALING =\
+    (INTENDED_SIZE / float(CONFIG['resources']['size']))\
+    * CONFIG['graphics']['scale']
