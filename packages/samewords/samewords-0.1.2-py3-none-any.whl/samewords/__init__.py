7293398b25569e2e4973cde159bdf0c7dea466b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:34:33 +0000

    Add package samewords/0.1.2/samewords-0.1.2-py3-none-any.whl

---
+++ packages/samewords/samewords-0.1.2-py3-none-any.whl/samewords/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Package for annotating references to potentially ambiguous words in critical editions made with LaTeX and reledmac. 
+"""
+__version__ = '0.1.2'
+__all__ = ['core', 'document', 'annotate', 'cli']
+
+import samewords.core
