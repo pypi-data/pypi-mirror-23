b0fd446211eb48eacb879bb35d10b6e1ede813a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:56:30 +0000

    Add package ImageMosaic/1.0/ImageMosaic-1.0.tar.gz

---
+++ packages/ImageMosaic/ImageMosaic-1.0.tar.gz/ImageMosaic-1.0/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='ImageMosaic',
+    version='1.0',
+    packages=['ImageMosaic',],
+    license='GNU General Public License Version 3',
+    long_description=open('README.md').read()
+)
