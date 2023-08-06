c7fed7f75c6553f7d77a13fbf02462b1181a6a10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:30 +0000

    Add package amino/9.10.1/amino-9.10.1.tar.gz

---
+++ packages/amino/amino-9.10.1.tar.gz/amino-9.10.1/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+version_parts = (9, 10, 1)
+version = '.'.join(map(str, version_parts))
+
+gh_lenses =\
+    'git+https://github.com/ingolemo/python-lenses.git#egg=lenses'
+
+setup(
+    name='amino',
+    description='functional data structures and type classes',
+    version=version,
+    author='Torsten Schmits',
+    author_email='torstenschmits@gmail.com',
+    license='MIT',
+    url='https://github.com/tek/amino',
+    packages=find_packages(exclude=['unit', 'unit.*']),
+    install_requires=[
+        'fn',
+        'toolz',
+        'lenses',
+    ],
+    dependency_links=[
+        gh_lenses,
+    ]
+)
