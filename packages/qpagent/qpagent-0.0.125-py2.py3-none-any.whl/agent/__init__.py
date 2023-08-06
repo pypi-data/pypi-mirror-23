88e79c4ea848f32e83baea00959e1a0d38294355
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:26 +0000

    Add package qpagent/0.0.125/qpagent-0.0.125-py2.py3-none-any.whl

---
+++ packages/qpagent/qpagent-0.0.125-py2.py3-none-any.whl/agent/__init__.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from logging.config import dictConfig
+
+__version__ = '0.0.125'
+
+LOGGING_CONFIG = dict({
+    'version': 1,
+    'disable_existing_loggers': False,
+    'root': {
+        'handlers': ['console'],
+        'level': 'INFO',
+    },
+    'loggers': {
+        'shortener': {
+            'handlers': ['console'],
+            'propagate': False,
+            'level': 'INFO',
+        },
+    },
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+            'formatter': 'console'
+        },
+    },
+    'formatters': {
+        'console': {
+            'format': '%(asctime)s [%(levelname)s] [%(name)s][%(process)d]'
+                      ': %(message)s',
+        },
+    }
+})
+dictConfig(LOGGING_CONFIG)
---
+++ packages/qpagent/qpagent-0.0.125-py2.py3-none-any.whl/agent/queue/worker.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import json
+import logging
+import time
+
+from agent.worker.macaca import Macaca
+from agent.queue import q
+from ..consts import AgentWorkerType
+from agent.qualityplatform.agent import get_task_by_id
+
+logger = logging.getLogger(__name__)
+
+
+def handle_task():
+    while True:
+        try:
+            data = q.get(timeout=3000)
+            if data:
+                agent_task = json.load(data)
+                task = get_task_by_id(agent_task.get('task_id'))
+                worker_data = task.get('params')
+                worker_type = worker_data.get('worker_type')
+                if worker_type is AgentWorkerType.macaca.value:
+                    Macaca(task).run()
+        except BaseException as e:
+            logger.exception(e)
