6a1a8904b3d06779ba7d9b50148f9488d34987a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:14:01 +0000

    Add package lxdock/0.3.0/lxdock-0.3.0.tar.gz

---
+++ packages/lxdock/lxdock-0.3.0.tar.gz/lxdock-0.3.0/lxdock/guests/__init__.py
@@ -0,0 +1,17 @@
+"""
+    Guest components
+    ================
+    This package contains guest definitions. Guests are used to perform operations on the "guest"
+    side (the containers).
+"""
+
+from .alpine import *  # noqa: F401, F403
+from .archlinux import *  # noqa: F401, F403
+from .base import *  # noqa: F401, F403
+from .centos import *  # noqa: F401, F403
+from .debian import *  # noqa: F401, F403
+from .fedora import *  # noqa: F401, F403
+from .gentoo import *  # noqa: F401, F403
+from .opensuse import *  # noqa: F401, F403
+from .oracle import *  # noqa: F401, F403
+from .ubuntu import *  # noqa: F401, F403
---
+++ packages/lxdock/lxdock-0.3.0.tar.gz/lxdock-0.3.0/tests/unit/provisioners/test_ansible.py
@@ -0,0 +1,69 @@
+import re
+import unittest.mock
+
+from lxdock.guests import AlpineGuest, DebianGuest
+from lxdock.hosts import Host
+from lxdock.provisioners import AnsibleProvisioner
+from lxdock.test import FakeContainer
+
+
+class TestAnsibleProvisioner:
+    @unittest.mock.patch('subprocess.Popen')
+    def test_can_run_ansible_playbooks(self, mock_popen):
+        host = Host()
+        guest = DebianGuest(FakeContainer())
+        lxd_state = unittest.mock.Mock()
+        lxd_state.network.__getitem__ = unittest.mock.MagicMock(
+            return_value={'addresses': [{'family': 'init', 'address': '0.0.0.0', }, ]})
+        guest.lxd_container.state.return_value = lxd_state
+        provisioner = AnsibleProvisioner('./', host, [guest], {'playbook': 'deploy.yml'})
+        provisioner.provision()
+        assert re.match(
+            'ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook --inventory-file /[/\w]+ '
+            './deploy.yml', mock_popen.call_args[0][0])
+
+    @unittest.mock.patch('subprocess.Popen')
+    def test_can_run_ansible_playbooks_with_the_vault_password_file_option(self, mock_popen):
+        host = Host()
+        guest = DebianGuest(FakeContainer())
+        lxd_state = unittest.mock.Mock()
+        lxd_state.network.__getitem__ = unittest.mock.MagicMock(
+            return_value={'addresses': [{'family': 'init', 'address': '0.0.0.0', }, ]})
+        guest.lxd_container.state.return_value = lxd_state
+        provisioner = AnsibleProvisioner(
+            './', host, [guest], {'playbook': 'deploy.yml', 'vault_password_file': '.vpass'})
+        provisioner.provision()
+        assert re.match(
+            'ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook --inventory-file /[/\w]+ '
+            '--vault-password-file ./.vpass ./deploy.yml', mock_popen.call_args[0][0])
+
+    @unittest.mock.patch('subprocess.Popen')
+    def test_can_run_ansible_playbooks_with_the_ask_vault_pass_option(self, mock_popen):
+        host = Host()
+        guest = DebianGuest(FakeContainer())
+        lxd_state = unittest.mock.Mock()
+        lxd_state.network.__getitem__ = unittest.mock.MagicMock(
+            return_value={'addresses': [{'family': 'init', 'address': '0.0.0.0', }, ]})
+        guest.lxd_container.state.return_value = lxd_state
+        provisioner = AnsibleProvisioner(
+            './', host, [guest], {'playbook': 'deploy.yml', 'ask_vault_pass': True})
+        provisioner.provision()
+        assert re.match(
+            'ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook --inventory-file /[/\w]+ '
+            '--ask-vault-pass ./deploy.yml', mock_popen.call_args[0][0])
+
+    def test_can_properly_setup_ssh_for_alpine_guests(self):
+        container = FakeContainer()
+        lxd_container = container._container
+        lxd_container.execute.return_value = ('ok', 'ok', '')
+        host = Host()
+        guest = AlpineGuest(container)
+        provisioner = AnsibleProvisioner('./', host, [guest], {'playbook': 'deploy.yml'})
+        provisioner.setup()
+        assert lxd_container.execute.call_count == 5
+        assert lxd_container.execute.call_args_list[0][0] == (['apk', 'update'], )
+        assert lxd_container.execute.call_args_list[1][0] == \
+            (['apk', 'add'] + AnsibleProvisioner.guest_required_packages_alpine, )
+        assert lxd_container.execute.call_args_list[2][0] == (['rc-update', 'add', 'sshd'], )
+        assert lxd_container.execute.call_args_list[3][0] == (['/etc/init.d/sshd', 'start'], )
+        assert lxd_container.execute.call_args_list[4][0] == (['mkdir', '-p', '/root/.ssh'], )
