80d2f96d92845ac7412d8d53e461d6930b8584cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:15:14 +0000

    Add package CPyCppyy/0.2.1/CPyCppyy-0.2.1.tar.gz

---
+++ packages/CPyCppyy/CPyCppyy-0.2.1.tar.gz/CPyCppyy-0.2.1/setup.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+
+import os, glob
+from setuptools import setup, find_packages, Extension
+from codecs import open
+
+
+here = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+
+setup(
+    name='CPyCppyy',
+    version='0.2.1',
+    description='Cling-based Python-C++ bindings',
+    long_description=long_description,
+
+    url='http://doc.pypy.org/en/latest/cppyy.html',
+
+    # Author details
+    author='Wim Lavrijsen',
+    author_email='WLavrijsen@lbl.gov',
+
+    license='LBNL BSD',
+
+    classifiers=[
+        'Development Status :: 4 - Beta',
+
+        'Intended Audience :: Developers',
+
+        'Topic :: Software Development',
+        'Topic :: Software Development :: Interpreters',
+
+        #'License :: OSI Approved :: MIT License',
+
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: C',
+        'Programming Language :: C++',
+
+        'Natural Language :: English'
+    ],
+
+    install_requires=['PyPy-cppyy-backend'],
+
+    keywords='C++ bindings',
+
+    package_dir={'': 'python'},
+    packages=find_packages('python'),
+
+    ext_modules=[Extension('libcppyy',
+        sources=glob.glob('src/*.cxx'),
+        headers=glob.glob('src/*.h')+glob.glob('include/*.h'),
+        extra_compile_args=['-std=c++11', '-O2'],
+        include_dirs=['include'])],
+
+)
