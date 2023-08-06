3e0cfe1846e21be84b00a6ba874c371e327ef56d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:08:30 +0000

    Add package avwx-engine/0.9.2/avwx_engine-0.9.2-py3-none-any.whl

---
+++ packages/avwx-engine/avwx_engine-0.9.2-py3-none-any.whl/avwx/exceptions.py
@@ -0,0 +1,12 @@
+"""
+Michael duPont - michael@mdupont.com
+AVWX-Engine : avwx/exceptions.py
+
+Contains avwx custom exceptions
+"""
+
+class BadStation(Exception):
+    pass
+
+class InvalidRequest(Exception):
+    pass
