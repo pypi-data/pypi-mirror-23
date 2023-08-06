7f566edb5cc26c00922b1574327ffb142137ebc4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:08:36 +0000

    Add package needinit/0.1.3/needinit-0.1.3.tar.gz

---
+++ packages/needinit/needinit-0.1.3.tar.gz/needinit-0.1.3/setup.py
@@ -0,0 +1,33 @@
+import os
+from setuptools import setup, find_packages
+from pip.req import parse_requirements
+from pip.download import PipSession
+
+
+def get_requirements():
+    req_file = os.path.join(os.path.dirname(__file__), "requirements.txt")
+    if os.path.exists(req_file):
+        requirements = parse_requirements(
+            os.path.join(os.path.dirname(__file__), "requirements.txt"),
+            session=PipSession())
+        return [str(req.req) for req in requirements]
+    return []
+
+
+def get_version():
+    __version__ = None
+    with open('needinit/_version.py') as version_src:
+        exec(version_src.read())
+    return __version__
+
+setup(
+    name='needinit',
+    version=get_version(),
+    description='Prevent callability on un-initialized objects',
+    url='https://github.com/steinnes/needinit',
+    author='Steinn Eldjarn Sigurdarson',
+    author_email='steinnes@gmail.com',
+    keywords=['library', 'object initialization'],
+    install_requires=get_requirements(),
+    packages=find_packages(),
+)
