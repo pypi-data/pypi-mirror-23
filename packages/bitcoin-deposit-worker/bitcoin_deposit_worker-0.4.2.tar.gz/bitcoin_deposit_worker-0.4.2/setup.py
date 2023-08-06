98ba26cfda6115ce3d1a0c8aff9b523be9371217
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:38 +0000

    Add package bitcoin-deposit-worker/0.4.2/bitcoin_deposit_worker-0.4.2.tar.gz

---
+++ packages/bitcoin-deposit-worker/bitcoin_deposit_worker-0.4.2.tar.gz/bitcoin_deposit_worker-0.4.2/setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+from setuptools import find_packages
+
+setup(
+  name='bitcoin_deposit_worker',
+  packages=find_packages(), # this must be the same as the name above
+  version='0.4.2',
+  description='A random test lib',
+  author='Yuanfei Zuu',
+  author_email='hi@yuanfeiz.cn',
+  url='https://github.com/yuanfeiz/bitcoin_deposit_worker', # use the URL to the github repo
+  download_url='https://github.com/yuanfeiz/bitcoin_deposit_worker/archive/master.zip', # I'll explain this in a second
+  keywords=['testing', 'logging', 'example'], # arbitrary keywords
+  classifiers=[],
+)
