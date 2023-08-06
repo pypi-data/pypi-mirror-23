38d5ffe0224303828db87d57fb0124650f0647a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:47:13 +0000

    Add package python-ldap/2.4.41/python-ldap-2.4.41.tar.gz

---
+++ packages/python-ldap/python-ldap-2.4.41.tar.gz/python-ldap-2.4.41/Tests/__init__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Automatic tests for python-ldap
+
+See http://www.python-ldap.org/ for details.
+
+$Id: __init__.py,v 1.5 2017/04/28 07:30:59 stroeder Exp $
+"""
+
+import t_cext
+import t_ldap_dn
+import t_ldap_filter
+import t_ldap_functions
+import t_ldap_modlist
+import t_ldap_schema_tokenizer
+import t_ldapurl
+import t_ldif
+import t_ldapobject
+import t_ldap_schema_subentry
