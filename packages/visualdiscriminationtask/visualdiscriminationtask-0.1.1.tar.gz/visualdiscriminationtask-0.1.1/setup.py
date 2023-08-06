2f881e5eb78bda078bedfe807ef2e314bdc4324e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:42 +0000

    Add package visualdiscriminationtask/0.1.1/visualdiscriminationtask-0.1.1.tar.gz

---
+++ packages/visualdiscriminationtask/visualdiscriminationtask-0.1.1.tar.gz/visualdiscriminationtask-0.1.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='visualdiscriminationtask',
+    description='A Visual Discrimination Task Whisker client',
+    version='0.1.1',
+    licence="MIT",
+    keywords="Whisker Visual Discrimination",
+    install_requires=[
+        'whisker',
+        'twisted'
+    ],
+    author="Tom Piercy",
+    author_email="tap32@medschl.cam.ac.uk",
+    packages=['visualdiscrimination'],
+    python_requires='>=3',
+    zip_safe=False
+)
