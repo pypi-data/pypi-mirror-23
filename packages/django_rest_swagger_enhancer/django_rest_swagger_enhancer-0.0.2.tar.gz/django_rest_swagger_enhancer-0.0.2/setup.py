5d22a960d2fba3c9e0d2bc953fa9130aefaceebe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:30 +0000

    Add package django_rest_swagger_enhancer/0.0.2/django_rest_swagger_enhancer-0.0.2.tar.gz

---
+++ packages/django_rest_swagger_enhancer/django_rest_swagger_enhancer-0.0.2.tar.gz/django_rest_swagger_enhancer-0.0.2/setup.py
@@ -0,0 +1,20 @@
+from setuptools import find_packages, setup
+# Read more here: https://www.codementor.io/arpitbhayani/host-your-python-package-using-github-on-pypi-du107t7ku
+
+setup(
+    name='django_rest_swagger_enhancer',
+    packages=find_packages(),
+    include_package_data=True,
+    version='0.0.2',
+    description='Django REST Swagger enhancement',
+    author='Eshan Das',
+    author_email='eshandasnit@gmail.com',
+    url='https://github.com/eshandas/django_rest_swagger_enhancer',  # use the URL to the github repo
+    download_url='https://github.com/eshandas/django_rest_swagger_enhancer/archive/0.0.2.tar.gz',  # Create a tag in github
+    keywords=['django', 'django rest framework', 'swagger', 'django rest swagger'],
+    classifiers=[],
+    install_requires=[
+        'Django>=1.11',
+        'djangorestframework>=3.6.3',
+        'django-rest-swagger>=2.1.2'],
+)
