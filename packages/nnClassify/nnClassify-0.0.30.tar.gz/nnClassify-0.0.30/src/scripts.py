a63230b9ddd7b7559e4a1d8562a5fe5d2fcd7205
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:01 +0000

    Add package nnClassify/0.0.30/nnClassify-0.0.30.tar.gz

---
+++ packages/nnClassify/nnClassify-0.0.30.tar.gz/nnClassify-0.0.30/src/scripts.py
@@ -0,0 +1,84 @@
+from src.Main_ import Main
+from src.ImageCropper_ import ImageCropper
+import argparse
+
+def create_model():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("Model_Name", help="What would you" 
+            + "like your model to be called")
+    args = parser.parse_args()
+
+    Main.create_model(args.Model_Name)
+    return
+
+def crop():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("Source_Folder", help="The path to the folder"
+            + " you want cropped")
+    parser.add_argument("Destination", help="The path to destination location")
+    args = parser.parse_args()
+    ImageCropper.crop_all_images(args.Source_Folder, args.Destination)
+    return
+
+def predict():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("Set_Name", help="The name of the set of wells to predict")
+    parser.add_argument("Model_Name", help="The name of the neural network to use")
+    parser.add_argument("Image_Confidance", type=float,
+            help="A floating point number between 0 and 1. The neural network" 
+            + " must be at least this confident to believe this images is a "
+            + "normal image")
+    parser.add_argument("Frame_Count",
+            help= "An integer greater than zero.", type=int,
+            + " We must see this many normal frames to predict the well as normal")
+    parser.add_argument("Maybe_Count", type=int,
+            help= "An integer greate than zero and less than Frame Count. "
+            + "If we see at least this many normal frames but less than Frame_Count"
+            + " then predict Not_Sure")
+
+    args = parser.parse_args()
+    Main.predict(args.Set_Name, args.Model_Name, args.Image_Confidance,
+            args.Frame_Count, args.Maybe_Count)
+    return
+
+def test_individual():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("Set_Name", help="The name of the test set")
+    parser.add_argument("Model_Name", help="The name of the neural network to use")
+    args = parser.parse_args()
+    Main.test_individual(args.Set_Name, args.Model_Name)
+    return
+
+def test_wells():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("Set_Name", help="The name of the test set")
+    parser.add_argument("Model_Name", help="The name of the neural network to use")
+    parser.add_argument("Image_Confidance", type=float,
+             help="A floating point number between 0 and 1. The neural network" 
+             + " must be at least this confident to believe this images is a "
+             + "normal image")
+    parser.add_argument("Frame_Count", type=int,
+             help= "An integer greater than zero."
+             + " We must see this many normal frames to predict the well as normal")
+    parser.add_argument("Maybe_Count", type=int,
+             help= "An integer greate than zero and less than Frame Count. "
+             + "If we see at least this many normal frames but less than Frame_Count"
+             + " then predict Not_Sure")
+
+    args = parser.parse_args()
+    Main.test_wells(args.Set_Name, args.Model_Name, args.Image_Confidance,
+            args.Frame_Count, args.Maybe_Count)
+    return
+
+def train():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("Training_Set", help="The name of the training set"
+            + " that you want to train on")
+    parser.add_argument("Model_Name", help="The model you want to train")
+    parser.add_argument("Epochs", help="The number of epochs you want to train on",
+            type=int)
+    args = parser.parse_args()
+    Main.train(args.Training_Set, args.Model_Name, args.Epochs)
+    return
+
+
