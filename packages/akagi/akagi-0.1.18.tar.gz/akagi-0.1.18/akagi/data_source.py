c3f10c74594524c013c96e03a9131a5880dd8eda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:35:17 +0000

    Add package akagi/0.1.18/akagi-0.1.18.tar.gz

---
+++ packages/akagi/akagi-0.1.18.tar.gz/akagi-0.1.18/akagi/data_source.py
@@ -0,0 +1,38 @@
+import os
+
+from akagi.log import logger
+
+
+class DataSource(object):
+    '''DataSource is an base class of all data sources
+    '''
+    def save(self, tar_dir, force=False):
+        paths = []
+
+        for d in self.bundle.data_files:
+            path = os.path.expanduser(os.path.join(tar_dir, d.key))
+            dirname = os.path.dirname(path)
+
+            if not os.path.isdir(dirname):
+                os.makedirs(dirname)
+
+            if os.path.isfile(path):
+                logger.debug("Skipped %(key)s" % ({"key": d.key}))
+            else:
+                with open(path, 'wb') as f:
+                    f.write(d.raw_content)
+                    logger.debug("Saved %(key)s to %(path)s" % ({"key": d.key, "path": path}))
+
+            paths.append(path)
+
+        return paths
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, *exc):
+        return False
+
+    def __iter__(self):
+        raise NotImplementedError
+
