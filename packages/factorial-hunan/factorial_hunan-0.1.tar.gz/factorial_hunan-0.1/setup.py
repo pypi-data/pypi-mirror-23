40957c5685d1265187f898362cd84a1e4f2f676d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:33:27 +0000

    Add package factorial-hunan/0.1/factorial_hunan-0.1.tar.gz

---
+++ packages/factorial-hunan/factorial_hunan-0.1.tar.gz/factorial_hunan-0.1/setup.py
@@ -0,0 +1,15 @@
+#! /usr/bin/env python3
+from setuptools import find_packages, setup
+
+setup(
+	name = "factorial_hunan", 
+	version = 0.1, 
+	description = "Factorial Module", 
+	long_description = "A test module", 
+	platforms = ["Linux"], 
+	author = "Nathan_Hu", 
+	author_email = "hunan1217@icloud.com",
+	url = "https://www.baidu.com",
+	license = "MIT", 
+	packages = find_packages()
+)
