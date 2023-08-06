7c4f4df3358d77e54867b80dcc7d6c44797b865a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:36:32 +0000

    Add package django-package-monitor/0.5.6/django-package-monitor-0.5.6.tar.gz

---
+++ packages/django-package-monitor/django-package-monitor-0.5.6.tar.gz/django-package-monitor-0.5.6/package_monitor/views.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from django.contrib.auth.decorators import user_passes_test
+from django.http import HttpResponse, HttpResponseRedirect
+
+from .management.commands import refresh_packages
+
+
+@user_passes_test(lambda u: u.is_staff)
+def reload(request):
+    """Reload local requirements file."""
+    refresh_packages.clean()
+    refresh_packages.local()
+    refresh_packages.remote()
+    url = request.META.get('HTTP_REFERER')
+    if url:
+        return HttpResponseRedirect(url)
+    else:
+        return HttpResponse('Local requirements list has been reloaded.')
