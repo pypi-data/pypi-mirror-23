5f71edb2b588f4b8d1b5e07d690344efc2877c43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:34:27 +0000

    Add package ftw.theming/1.10.2/ftw.theming-1.10.2.tar.gz

---
+++ packages/ftw.theming/ftw.theming-1.10.2.tar.gz/ftw.theming-1.10.2/setup.py
@@ -0,0 +1,61 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.10.2'
+
+tests_require = [
+    'ftw.builder',
+    'ftw.testbrowser',
+    'ftw.testing',
+    'plone.app.testing',
+]
+
+
+setup(name='ftw.theming',
+      version=version,
+      description="",
+      long_description=open("README.rst").read() + "\n" + open(
+          os.path.join("docs", "HISTORY.txt")).read(),
+
+      classifiers=[
+          "Environment :: Web Environment",
+          'Framework :: Plone',
+          'Framework :: Plone :: 4.3',
+          "Intended Audience :: Developers",
+          'License :: OSI Approved :: GNU General Public License (GPL)',
+          'Programming Language :: Python',
+          'Topic :: Software Development :: Libraries :: Python Modules',
+      ],
+
+      keywords='ftw theming',
+      author='4teamwork AG',
+      author_email='mailto:info@4teamwork.ch',
+      url='https://github.com/4teamwork/ftw.theming',
+      license='GPL2',
+
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['ftw'],
+      include_package_data=True,
+      zip_safe=False,
+
+      install_requires=[
+          'Plone',
+          'cssselect',
+          'ftw.upgrade',
+          'ftw.profilehook',
+          'lxml',
+          'path.py',
+          'pyScss >= 1.3',
+          'six >= 1.4.0',  # six.unichr
+          'setuptools',
+          'tarjan',
+      ],
+      tests_require=tests_require,
+      extras_require=dict(tests=tests_require),
+
+      entry_points="""
+      # -*- Entry points: -*-
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+)
