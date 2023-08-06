760168d2fd1f11a53b5da553d34c86e635ba98c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:49:16 +0000

    Add package webstarts/0.8.0/webstarts-0.8.0.tar.gz

---
+++ packages/webstarts/webstarts-0.8.0.tar.gz/webstarts-0.8.0/cli.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Applicable to webstarts"""
+import shlex
+import subprocess
+
+import click
+from pkg_resources import get_distribution
+from semantic_version import Version
+
+__author__ = "john"
+
+
+@click.group()
+def cli():
+  """CLI utilities"""
+
+
+@cli.command('rel')
+@click.option('-n', '--num')
+def release(num):
+  """Release prod
+
+  Notes:
+    setup.py develop
+    setup.py develop --uninstall
+  """
+  cmd('python setup.py develop --uninstall')
+  cmd('python setup.py develop')
+  version = num or _bump()
+  cmd(f'git hf release start {version}')
+  cmd(f'git hf release finish -M {version}')
+  print(f'Dev: {_version()}')
+  cmd('git checkout master')
+  print(f'Master: {_version()}')
+  cmd('python setup.py develop --uninstall')
+  cmd('python setup.py develop')
+  print(f'Deploy: {_version()}')
+  cmd('./deploy.sh')
+
+
+@cli.command()
+def version():
+  return print(f'Version: {_version()}')
+
+
+@cli.command()
+def bump():
+  return print(f'Next Version: {_bump()}')
+
+
+def _version():
+  return get_distribution('webstarts').version
+
+
+def _bump():
+  v = Version.coerce(_version())
+  if v.minor >= 9:
+    return v.next_major()
+  return v.next_minor()
+
+
+def cmd(line, **kwargs):
+  print(line)
+  return subprocess.run(shlex.split(line), check=True, stdout=subprocess.PIPE, encoding='utf-8', **kwargs)
