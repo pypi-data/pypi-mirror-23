e24510f5dd8277c644b0c9dcc6e370aa53371db8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:08:53 +0000

    Add package rprint/0.0.1/rprint-0.0.1.tar.gz

---
+++ packages/rprint/rprint-0.0.1.tar.gz/rprint-0.0.1/setup.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+setup(
+    name='rprint',
+    version='0.0.1',
+    url='https://github.com/cletrix/rprint.git',
+    license='MIT License',
+    author='Cleyton Pedroza',
+    author_email='cleytonpedroza@gmail.com',
+    keywords='UDP debug remote connection',
+    description=u'Class to remote debug with UDP connection',
+    packages=['rprint'],
+    install_requires=[],
+)
