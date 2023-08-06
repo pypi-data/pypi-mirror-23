c50f56739fe0b0bef7d6b75dd0a56a9c8e9c44ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:18:56 +0000

    Add package sentence-sem-sim/0.4/sentence-sem-sim-0.4.tar.gz

---
+++ packages/sentence-sem-sim/sentence-sem-sim-0.4.tar.gz/sentence-sem-sim-0.4/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+setup(
+  name = 'sentence-sem-sim',
+  packages = ['sentence-sem-sim'], # this must be the same as the name above
+  version = '0.4',
+  description = 'Sentence Semantic Similarity in Python.',
+  author = 'Shaun Chiang',
+  author_email = 'redbeansodapop@hotmail.com',
+  url = 'https://github.com/shaun-chiang/sentence-sem-sim/tree/master', # use the URL to the github repo
+  keywords = ['sentence', 'semantic', 'similarity'], # arbitrary keywords
+  classifiers = [],
+  license='MIT',
+  install_requires=['nltk>=3.2.1',
+                    'numpy>=1.11']
+)
