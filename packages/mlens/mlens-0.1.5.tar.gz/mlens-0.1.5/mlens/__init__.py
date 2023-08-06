5c9e44e846762eaa1499d954167aeeaf4e8d2f45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:14:18 +0000

    Add package mlens/0.1.5/mlens-0.1.5.tar.gz

---
+++ packages/mlens/mlens-0.1.5.tar.gz/mlens-0.1.5/mlens/__init__.py
@@ -0,0 +1,24 @@
+"""ML-ENSEMBLE
+
+:author: Sebastian Flennerhag
+:copyright: 2017
+:licence: MIT
+
+ML-Ensemble, a Python library for memory efficient parallelized ensemble
+learning.
+"""
+# Initialize configurations
+import mlens.config
+
+__version__ = "0.1.5"
+
+
+__all__ = ['base',
+           'ensemble',
+           'externals',
+           'metrics',
+           'model_selection',
+           'parallel',
+           'preprocessing',
+           'utils',
+           'visualization']
