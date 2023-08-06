351e5a7f9fd26561773c4000d40f00550cfc774b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:10 +0000

    Add package gecosistema_lite/0.0.3/gecosistema_lite-0.0.3.zip

---
+++ packages/gecosistema_lite/gecosistema_lite-0.0.3.zip/gecosistema_lite-0.0.3/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(name='gecosistema_lite',
+      version='0.0.3',
+      description='A simple python package',
+      author='Valerio Luzzi',
+      author_email='valerio.luzzi@gecosistema.it',
+      url='https://github.com/valluzzi/libcore/',
+      license='MIT',
+      install_requires=['numpy', 'gdal', 'pyproj'],
+      python_requires='>=2.7'
+      )
