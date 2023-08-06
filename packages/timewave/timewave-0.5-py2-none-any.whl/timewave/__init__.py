fbb93902709aba64c4271a079894d687938851bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:33:11 +0000

    Add package timewave/0.5/timewave-0.5-py2-none-any.whl

---
+++ packages/timewave/timewave-0.5-py2-none-any.whl/timewave/__init__.py
@@ -0,0 +1,8 @@
+# coding=utf-8
+import dill as pickle  # enable proper pickle for lambda expressions
+from engine import *
+from producers import *
+from consumers import *
+from stochasticprocess import *
+from stochasticproducer import *
+from stochasticconsumer import *
