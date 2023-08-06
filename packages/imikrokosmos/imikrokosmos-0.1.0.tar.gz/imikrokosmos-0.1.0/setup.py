15e5bffb541bf8b3ae9fb5e7b40864f510ed4c5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:55:54 +0000

    Add package imikrokosmos/0.1.0/imikrokosmos-0.1.0.tar.gz

---
+++ packages/imikrokosmos/imikrokosmos-0.1.0.tar.gz/imikrokosmos-0.1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='imikrokosmos',
+    version='0.1.0',
+    scripts=['mikrokosmoskernel.py'],
+    url='https://github.com/M42/jupyter-mikrokosmos',
+    author='Mario Román',
+    author_email='mromang08@gmail.com',
+    license='GPLv3'
+)
