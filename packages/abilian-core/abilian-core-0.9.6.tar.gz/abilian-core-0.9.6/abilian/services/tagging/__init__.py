339c46ab19a947051f16f89110863f74ae2489f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:13:23 +0000

    Add package abilian-core/0.9.6/abilian-core-0.9.6.tar.gz

---
+++ packages/abilian-core/abilian-core-0.9.6.tar.gz/abilian-core-0.9.6/abilian/services/tagging/__init__.py
@@ -0,0 +1,4 @@
+"""
+Simple tagging system.
+"""
+from __future__ import absolute_import, division, print_function
---
+++ packages/abilian-core/abilian-core-0.9.6.tar.gz/abilian-core-0.9.6/abilian/web/admin/panels/__init__.py
@@ -0,0 +1,3 @@
+""" Core panels
+"""
+from __future__ import absolute_import, division, print_function
