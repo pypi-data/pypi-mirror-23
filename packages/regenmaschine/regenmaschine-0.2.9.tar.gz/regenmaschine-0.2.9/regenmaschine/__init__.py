5af85f7969f0fcb9f8739ecba8b35908ba858d32
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:33:15 +0000

    Add package regenmaschine/0.2.9/regenmaschine-0.2.9.tar.gz

---
+++ packages/regenmaschine/regenmaschine-0.2.9.tar.gz/regenmaschine-0.2.9/regenmaschine/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# pylint: disable=wildcard-import
+"""
+File: __init__.py
+Author: Aaron Bach
+Email: bachya1208@gmail.com
+Github: https://github.com/bachya/regenmaschine
+"""
+
+from .__version__ import __author__, __author_email__, __copyright__, __license__
+from .__version__ import __description__, __title__, __url__, __version__
+from .auth import *
+from .client import *
