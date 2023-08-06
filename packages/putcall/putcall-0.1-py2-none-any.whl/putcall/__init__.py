7167beabe084bf5c2e8b76c6a8580a42db3d9fbb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:33:32 +0000

    Add package putcall/0.1/putcall-0.1-py2-none-any.whl

---
+++ packages/putcall/putcall-0.1-py2-none-any.whl/putcall/__init__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+#  putcall
+#  ------------
+#  Collection of classical option pricing formulas.
+#
+#  Author:  pbrisk <pbrisk_at_github@icloud.com>
+#  Copyright: 2016, 2017 Deutsche Postbank AG
+#  Website: https://github.com/pbrisk/putcall
+#  License: APACHE Version 2 License (see LICENSE file)
+
+
+from formulas import *
+from calibration import *
+from optionvaluator import *
