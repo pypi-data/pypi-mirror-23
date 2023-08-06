1119162d230997b046598ff0b3cbf56227c7c314
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:15:28 +0000

    Add package omnic/0.1.2/omnic-0.1.2-py2.py3-none-any.whl

---
+++ packages/omnic/omnic-0.1.2-py2.py3-none-any.whl/omnic/__init__.py
@@ -0,0 +1,12 @@
+__author__ = 'michaelb'
+__email__ = 'michaelpb@gmail.com'
+__version__ = '0.1.2'
+
+# flake8: noqa
+# Set up singleton system
+from omnic.utils.singleton import SingletonManager
+singletons = SingletonManager()
+
+# Ensure settings gets registered as a singleton
+from omnic.config.settingsmanager import SettingsManager
+singletons.register('settings', SettingsManager)
