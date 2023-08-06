dfd8a46b4100fb8b7bf84983cead256089174b40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:32:37 +0000

    Add package torcms/0.6.11/torcms-0.6.11.tar.gz

---
+++ packages/torcms/torcms-0.6.11.tar.gz/torcms-0.6.11/torcms/script/script_init.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+'''
+script for initialization.
+'''
+from .script_init_tabels import run_init_tables
+from .autocrud.base_crud import build_dir
+from .script_gen_category import run_gen_category
+from .autocrud.gen_html_file import generate_html_files as run_auto
+from .script_fetch_fe2lib import run_f2elib
+from .script_create_admin import run_create_admin
+from .script_whoosh import run_whoosh
+
+build_dir()
+
+
+def run_init(*args):
+    '''
+    running init.
+    :return:
+    '''
+    run_f2elib()
+    run_init_tables()
+    run_gen_category()
+    run_create_admin()
+    run_auto()
+    run_whoosh()
