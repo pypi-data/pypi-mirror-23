68fc687436af56a274aef3b99eb2226e73188914
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:08:58 +0000

    Add package emp/0.0/emp-0.0.tar.gz

---
+++ packages/emp/emp-0.0.tar.gz/emp-0.0/setup.py
@@ -0,0 +1,13 @@
+# coding: utf-8
+
+# standard library
+from setuptools import setup, find_packages
+
+
+# main
+setup(
+    name = 'emp',
+    version = '0.0',
+    url = 'https://github.com/snoopython/emp',
+    packages = find_packages(),
+)
