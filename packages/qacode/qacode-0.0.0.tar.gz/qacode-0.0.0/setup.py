d4b7d694705043412b4e95c96d5d021155198ee4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:20 +0000

    Add package qacode/0.0.0/qacode-0.0.0.tar.gz

---
+++ packages/qacode/qacode-0.0.0.tar.gz/qacode-0.0.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+
+setup(name='qacode',
+      version='0.0.0',
+      packages=['core', 'tests', 'configs', 'logs'],
+      description = 'Main automation lib',
+      author = 'Netzulo Open Source',
+      author_email = 'netzuleando@gmail.com',
+      url = 'https://github.com/netzulo/qacode', # use the URL to the github repo
+      download_url = 'https://github.com/netzulo/qacode/tarball/0.0.0',
+      keywords = ['testing', 'logging', 'functional','selenium', 'test'],
+      )
