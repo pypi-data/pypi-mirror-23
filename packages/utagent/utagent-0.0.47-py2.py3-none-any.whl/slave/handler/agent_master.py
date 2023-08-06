f978e76340d526523912fbdbd13269253cf4521a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:08:31 +0000

    Add package utagent/0.0.47/utagent-0.0.47-py2.py3-none-any.whl

---
+++ packages/utagent/utagent-0.0.47-py2.py3-none-any.whl/slave/handler/agent_master.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import json
+import logging
+
+from slave.globalvar import get_queue
+from slave.config import config
+from macaca_worker import MacacaWorker
+
+logger = logging.getLogger(__name__)
+
+
+def post_get(name, data):
+    return data.get(name)
+
+
+def handle_task():
+    while True:
+        try:
+            data = get_queue().get(timeout=3000)
+            if data:
+                agent_task = json.load(data)
+                worker_type = agent_task['worker_type']
+                worker_data = agent_task['worker_data']
+                if worker_type == 1:
+                    MacacaWorker(config["macaca_dir"], worker_data).run()
+        except KeyboardInterrupt:
+            pass
+        except Exception as e:
+            print(e.message)
+            logger.exception(e)
