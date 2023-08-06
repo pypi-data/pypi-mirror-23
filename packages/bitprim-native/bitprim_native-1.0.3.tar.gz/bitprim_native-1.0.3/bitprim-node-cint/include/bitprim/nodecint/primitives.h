f7bea18fed79a06111efbc94432d62b788059135
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:17 +0000

    Add package bitprim-native/1.0.3/bitprim_native-1.0.3.tar.gz

---
+++ packages/bitprim-native/bitprim_native-1.0.3.tar.gz/bitprim_native-1.0.3/bitprim-node-cint/include/bitprim/nodecint/primitives.h
@@ -0,0 +1,84 @@
+/**
+ * Copyright (c) 2017 Bitprim developers (see AUTHORS)
+ *
+ * This file is part of Bitprim.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef BITPRIM_NODECINT_PRIMITIVES_H_
+#define BITPRIM_NODECINT_PRIMITIVES_H_
+
+#include <stdio.h>
+#include <stdint.h>
+
+#include <bitprim/nodecint/visibility.h>
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef enum point_kind {output = 0, spend = 1} point_kind_t;
+
+typedef struct executor* executor_t;
+typedef void* chain_t;
+
+//typedef struct output_point_t {
+//    uint8_t* hash;
+//    uint32_t index;
+//} output_point_t;
+
+typedef void* block_t;
+typedef void* compact_block_t;
+typedef void* header_t;
+typedef void* history_compact_t;
+typedef void* history_compact_list_t;
+typedef void* input_t;
+typedef void* merkle_block_t;
+typedef void* script_t;
+typedef void* output_point_t;
+typedef void* output_t;
+typedef void* point_t;
+typedef void* point_list_t;
+typedef void* transaction_t;
+typedef void* payment_address_t;
+
+typedef uint8_t const* hash_t;
+typedef uint8_t* long_hash_t;
+//typedef char const* zstring_t;
+typedef void* word_list_t;
+
+
+typedef void (*run_handler_t)(executor_t exec, void* context, int error);
+
+
+typedef void (*block_fetch_handler_t)(chain_t chain, void* context, int error, block_t block, size_t h);
+typedef void (*block_height_fetch_handler_t)(chain_t chain, void* context, int error, size_t h);
+typedef void (*block_header_fetch_handler_t)(chain_t chain, void* context, int error, header_t header, size_t h);
+typedef void (*compact_block_fetch_handler_t)(chain_t chain, void* context, int error, compact_block_t block, size_t h);
+typedef void (*history_fetch_handler_t)(chain_t chain, void* context, int error, history_compact_list_t history);
+typedef void (*last_height_fetch_handler_t)(chain_t chain, void* context, int error, size_t h);
+typedef void (*merkle_block_fetch_handler_t)(chain_t chain, void* context, int error, merkle_block_t block, size_t h);
+typedef void (*output_fetch_handler_t)(chain_t chain, void* context, int error, output_t output);
+typedef void (*spend_fetch_handler_t)(chain_t chain, void* context, int error, input_t output);
+typedef void (*transaction_fetch_handler_t)(chain_t chain, void* context, int error, transaction_t transaction, size_t h, size_t i);
+typedef void (*transaction_index_fetch_handler_t)(chain_t chain, void* context, int error, size_t position, size_t height);
+typedef void (*validate_tx_handler_t)(chain_t chain, void* context, int error, char* message);
+
+#ifdef __cplusplus
+} // extern "C"
+#endif
+
+#endif /* BITPRIM_NODECINT_PRIMITIVES_H_ */
---
+++ packages/bitprim-native/bitprim_native-1.0.3.tar.gz/bitprim_native-1.0.3/setup.py
@@ -0,0 +1,123 @@
+# 
+# Copyright (c) 2017 Bitprim developers (see AUTHORS)
+# 
+# This file is part of Bitprim.
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+# 
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# 
+
+from setuptools import setup, find_packages
+from setuptools.extension import Extension
+import platform
+
+extensions = [
+	Extension('bitprim_native',
+        sources = ['bitprimmodule.c'],
+        include_dirs=['bitprim-node-cint/include'],
+        library_dirs=['bitprim-node-cint/lib'],
+        libraries = ['bitprim-node-cint'],
+
+        # define_macros=list(EXTRA_DEFINES.iteritems()),
+        # extra_compile_args=conf["CXXFLAGS"],
+        # extra_link_args=conf["LDFLAGS"],
+    	# extra_link_args= ['-Wl,-rpath,'+lib_path]
+    ),
+    # Extension(
+    #     "myPackage.myModule",
+    #     ["myPackage/myModule.pyx"],
+    #     include_dirs=['/some/path/to/include/'], # not needed for fftw unless it is installed in an unusual place
+    #     libraries=['fftw3', 'fftw3f', 'fftw3l', 'fftw3_threads', 'fftw3f_threads', 'fftw3l_threads'],
+    #     library_dirs=['/some/path/to/include/'], # not needed for fftw unless it is installed in an unusual place
+    # ),
+]
+
+# if platform.system() == 'Darwin':
+# 	lib_path = '/usr/local/lib'
+# 	extensions[0].extra_link_args.append('-Wl,-rpath,'+lib_path)
+
+# print(extensions[0].extra_link_args)
+
+setup(
+    name='bitprim_native',
+    version='1.0.3',
+
+    description='Bitprim Platform',
+    long_description='Bitprim Platform',
+    url='https://github.com/bitprim/bitprim-py',
+
+    # Author details
+    author='Bitprim Inc',				#TODO!
+    author_email='dev@bitprim.org',		#TODO!
+
+    # Choose your license
+    license='MIT',    					#TODO!
+
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        # How mature is this project? Common values are
+        #   3 - Alpha
+        #   4 - Beta
+        #   5 - Production/Stable
+        'Development Status :: 3 - Alpha',
+
+        # Indicate who your project is intended for
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+
+        # Pick your license as you wish (should match "license" above)
+        'License :: OSI Approved :: MIT License',
+
+        'Programming Language :: C++',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+
+    # What does your project relate to?
+    keywords='bitcoin litecoin money bitprim',
+
+    # You can just specify the packages manually here if your project is
+    # simple. Or you can use find_packages().
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+
+    # Alternatively, if you want to distribute just a my_module.py, uncomment
+    # this:
+    #   py_modules=["my_module"],
+
+    # # List run-time dependencies here.  These will be installed by pip when
+    # # your project is installed. For an analysis of "install_requires" vs pip's
+    # # requirements files see:
+    # # https://packaging.python.org/en/latest/requirements.html
+    # install_requires=['peppercorn'],
+
+    # List additional groups of dependencies here (e.g. development
+    # dependencies). You can install these using the following syntax,
+    # for example:
+    # $ pip install -e .[dev,test]
+    extras_require={
+        'dev': ['check-manifest'],
+        'test': ['coverage'],
+    },
+
+#	data_files = [('lib\\site-packages',['C:\\development\\bitprim\\build\\bitprim-node-cint\\bitprim-node-cint.dll'])],
+# 	data_files = [('lib\\site-packages',['C:\\development\\bitprim\\build\\bitprim-node-cint\\bitprim-node-cint.dll'])],
+
+# tion="-I/home/fernando/dev/bitprim/bitprim-node-cint/include" --global-option="-L/home/fernando/dev/bitprim/build/bitprim-node-cint" -e .
+
+
+    ext_modules = extensions
+)
