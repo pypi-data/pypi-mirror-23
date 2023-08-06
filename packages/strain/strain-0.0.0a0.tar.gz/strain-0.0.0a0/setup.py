37203130f0d74a59f9010555b08bb90311959339
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:43 +0000

    Add package strain/0.0.0a0/strain-0.0.0a0.tar.gz

---
+++ packages/strain/strain-0.0.0a0.tar.gz/strain-0.0.0a0/setup.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import re
+
+from setuptools import setup, find_packages
+
+readme = """Implementation of strains for III-V materials."""
+
+with open('./strain/_version.py', 'r') as f:
+    match_expr = "__version__[^'\"]+(['\"])([^'\"]+)"
+    version = re.search(match_expr, f.read()).group(2).strip()
+
+setup(
+    name='strain',
+    version=version,
+    author='Dominik Gresch',
+    author_email='greschd@gmx.ch',
+    install_requires=['numpy', 'pymatgen'],
+    extras_require={'test': ['pytest']},
+    long_description=readme,
+    classifiers=[
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Natural Language :: English',
+        'Operating System :: Unix',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: Physics'
+    ],
+    license='GPL',
+    packages=find_packages()
+)
---
+++ packages/strain/strain-0.0.0a0.tar.gz/strain-0.0.0a0/strain/_strain.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import six
+import abc
+import copy
+
+import numpy as np
+import pymatgen as mg
+from pymatgen.analysis.elasticity.strain import Deformation
+
+class Strain(six.with_metaclass(abc.ABCMeta, object)):
+    @abc.abstractmethod
+    def apply(structure, strength_multiplier=1.):
+        pass
+
+class CartesianStrain(Strain):
+    def __init__(self, deformation_matrix, pos_displacement_matrices=None):
+        self.deformation_matrix = deformation_matrix
+        self.pos_displacement_matrices = pos_displacement_matrices
+
+    def apply(self, structure, strength_multiplier=1.):
+        deformation = Deformation(
+            np.eye(3) + strength_multiplier * self.deformation_matrix
+        )
+        new_structure = deformation.apply_to_structure(structure)
+        # move positions
+        if self.pos_displacement_matrices is not None:
+            for idx, mat in self.pos_displacement_matrices:
+                new_structure.translate_sites(
+                    indices=[idx],
+                    # use original cartesian positions
+                    vector=strength_multiplier * np.dot(
+                        mat, structure.cart_coords[idx]
+                    ),
+                    frac_coords=False
+                )
+        return new_structure
