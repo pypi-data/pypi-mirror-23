49ddc453af35a888a95a91a1c6c3ddef41e83915
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:02 +0000

    Add package audiblerangepy/0.0.1/audiblerangepy-0.0.1.tar.gz

---
+++ packages/audiblerangepy/audiblerangepy-0.0.1.tar.gz/audiblerangepy-0.0.1/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+
+version = '0.0.1'
+name = 'audiblerangepy'
+short_description = '`audiblerangepy` is a package for checking some frequency is audible or not for some human-being and animals'
+long_description = """\
+`audiblerangepy` is a package for checking some frequency is audible or not for some human-being and animals
+
+Requirements
+------------
+* Python 3.x
+(you may work by Python2.x)
+
+Features
+--------
+* nothing
+
+History
+-------
+0.0.1 (2017-7-15)
+~~~~~~~~~~~~~~~~~~
+* first release
+
+"""
+
+setup(
+    name=name,
+    version=version,
+    description=short_description,
+    long_description=long_description,
+    keywords=['audible','audible range'],
+    packages=find_packages(),
+    author='Kosuke Mawatari',
+    license='MIT',
+)
