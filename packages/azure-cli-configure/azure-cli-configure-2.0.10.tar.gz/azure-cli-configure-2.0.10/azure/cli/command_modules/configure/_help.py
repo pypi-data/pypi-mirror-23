257e6ee639de2f2a4c01127489ba17145ee24362
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:09:32 +0000

    Add package azure-cli-configure/2.0.10/azure-cli-configure-2.0.10.tar.gz

---
+++ packages/azure-cli-configure/azure-cli-configure-2.0.10.tar.gz/azure-cli-configure-2.0.10/azure/cli/command_modules/configure/_help.py
@@ -0,0 +1,12 @@
+# --------------------------------------------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for license information.
+# --------------------------------------------------------------------------------------------
+
+from azure.cli.core.help_files import helps
+
+
+helps['configure'] = """
+            type: command
+            short-summary: Configure Azure CLI 2.0 or view your configuration. The command is interactive, so just type `az configure` and respond to the prompts.
+"""
