1954e67d33d49d98af82817eec58ba44d7637f04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:36:27 +0000

    Add package econobox/1.0.0/econobox-1.0.0.tar.gz

---
+++ packages/econobox/econobox-1.0.0.tar.gz/econobox-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='econobox',
+    version='1.0.0',
+    packages=['econobox'],
+    url='https://github.com/beltekylevi/econobox/',
+    license='MIT',
+    author='',
+    author_email='',
+    description=''
+)
