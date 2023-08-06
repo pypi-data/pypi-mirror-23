495ce75fbb55ce655fc405e458576ada697f95cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:56:02 +0000

    Add package ItChatSh/1.0/ItChatSh-1.0.tar.gz

---
+++ packages/ItChatSh/ItChatSh-1.0.tar.gz/ItChatSh-1.0/setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='ItChatSh',
+      version='1.0',
+      description='Shell on WeChat based on ItChat',
+      author='LI ZEYAN',
+      author_email='zy-li14@hotmail.com',
+      packages=['itchatsh'], requires=['itchat'],
+      )
