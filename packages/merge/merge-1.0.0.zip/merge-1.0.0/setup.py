41f2b23478eafea1d8e6f0ac6beb749703e6e388
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:13:06 +0000

    Add package merge/1.0.0/merge-1.0.0.zip

---
+++ packages/merge/merge-1.0.0.zip/merge-1.0.0/setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup
+
+setup(
+    
+    name         = 'merge',
+    
+    version      = '1.0.0',
+    
+    py_modules   = ['merge'],
+    
+    author       = 'Mingo',
+
+    author_email = 'www.sukm@qq.com',
+
+    url          = 'https://github.com/MingoSue/chips/blob/master/merge.py',
+    
+    description  = 'A simple function of merge-sort',
+
+    )
