fc8c4d501d4ef77548e0e8f132af5c38ec108bc3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:14:17 +0000

    Add package s3-environ/0.0.2/s3-environ-0.0.2.tar.gz

---
+++ packages/s3-environ/s3-environ-0.0.2.tar.gz/s3-environ-0.0.2/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+
+setup(
+    author='Jonatas Baldin',
+    author_email='jonatas.baldin@gmail.com',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Utilities'
+    ],
+    description='Load environment variables from a AWS S3 file!',
+    license='MIT License',
+    url='https://github.com/jonatasbaldin/s3-environ',
+    include_package_data=True,
+    name='s3-environ',
+    packages=['s3-environ'],
+    version='0.0.2'
+)
