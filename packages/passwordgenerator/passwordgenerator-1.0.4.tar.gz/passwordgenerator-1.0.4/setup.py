1bda383d49ae328c0d46e1fa21daf6fbaa90cd8d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:33:22 +0000

    Add package passwordgenerator/1.0.4/passwordgenerator-1.0.4.tar.gz

---
+++ packages/passwordgenerator/passwordgenerator-1.0.4.tar.gz/passwordgenerator-1.0.4/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+try:
+    import pypandoc
+    long_description = pypandoc.convert('README.md', 'rst')
+except(IOError, ImportError):
+    long_description = open('README.md').read()
+
+setup (
+    name = 'passwordgenerator',
+    version = '1.0.4',
+    description = 'Passwords easy for humans, hard for computers',
+    long_description = long_description,
+    author = 'Gabriel Bordeaux',
+    author_email = 'pypi@gab.lc',
+    url = 'https://github.com/gabfl/password-generator-py/',
+    license = 'MIT',
+    packages = ['passwordgenerator', 'passwordgenerator.data'],
+    package_dir = { 'passwordgenerator': 'src' },
+    install_requires = ['argparse'], # external dependencies
+    entry_points = {
+        'console_scripts': [
+            'passwordgenerator = passwordgenerator.pwgenerator:main',
+        ],
+    },
+)
