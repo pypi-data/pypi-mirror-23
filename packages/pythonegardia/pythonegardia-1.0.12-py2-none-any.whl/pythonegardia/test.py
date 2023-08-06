6ca82d4bd27d3187834c8b7a114f694fb11f7e4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:48:44 +0000

    Add package pythonegardia/1.0.12/pythonegardia-1.0.12-py2-none-any.whl

---
+++ packages/pythonegardia/pythonegardia-1.0.12-py2-none-any.whl/pythonegardia/test.py
@@ -0,0 +1,7 @@
+import egardiadevice
+e = egardiadevice.EgardiaDevice("192.168.1.129",80,"terheerdt","MIy1j8vyG7l624M","")
+print(e.getstate())
+
+import egardiaserver
+es = egardiaserver.EgardiaServer(85)
+
