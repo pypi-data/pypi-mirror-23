5d8bc5d569d6baafa619b09d15bfa3952d5eb1c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:27:27 +0000

    Add package crap/1.5/crap-1.5.tar.gz

---
+++ packages/crap/crap-1.5.tar.gz/crap-1.5/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+	name = 'crap',
+	version = '1.5',
+	description = 'Encodes and decodes files in .crap format. This allows one to execute the file in any languge, immediately upon import.',
+	author = 'Nirman Dave',
+	author_email = 'nirman@sourcenet.in',
+	packages = ['crap'],
+	)
