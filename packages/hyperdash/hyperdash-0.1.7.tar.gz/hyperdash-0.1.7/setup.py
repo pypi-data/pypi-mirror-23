7082a9446548e1f1556416c5be26f5ba65f48f0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:57:10 +0000

    Add package hyperdash/0.1.7/hyperdash-0.1.7.tar.gz

---
+++ packages/hyperdash/hyperdash-0.1.7.tar.gz/hyperdash-0.1.7/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+
+version = "0.1.7"
+
+setup(
+    name='hyperdash',
+    packages=['hyperdash', 'hyperdash_cli'],
+    install_requires=[
+        'autobahn',
+        'Twisted',
+        'pyOpenSSL',
+        'six'
+    ],
+    entry_points={
+        'console_scripts': ['hyperdash = hyperdash_cli.cli:main']
+    },
+    version=version,
+    description='Hyperdash.io CLI and SDK',
+    author='Hyperdash',
+    author_email='support@hyperdash.io',
+    url='https://hyperdash.io',
+)
