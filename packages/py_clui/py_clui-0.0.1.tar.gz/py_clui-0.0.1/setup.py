373e47d9d17346ad761d16bbf2f92a0d6eca7412
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:34:16 +0000

    Add package py_clui/0.0.1/py_clui-0.0.1.tar.gz

---
+++ packages/py_clui/py_clui-0.0.1.tar.gz/py_clui-0.0.1/setup.py
@@ -0,0 +1,17 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+setup(
+    name='py_clui',
+    version='0.0.1',
+    description='',
+    url='https://github.com/hmleal/py-clui',
+    author='Henrique Leal',
+    author_email='hm.leal@hotmail.com',
+    packages=[
+        'py_clui'
+    ],
+)
