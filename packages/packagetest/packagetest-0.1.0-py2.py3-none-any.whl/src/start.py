a5300bc3d9a51db2e7442037f57b1358c5216673
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:07:55 +0000

    Add package packagetest/0.1.0/packagetest-0.1.0-py2.py3-none-any.whl

---
+++ packages/packagetest/packagetest-0.1.0-py2.py3-none-any.whl/src/start.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+# Server process that consumes sensor readings and transmitts them to the core IoT engine via MQTTF
+#
+import os
+import time
+
+VERSION = "2.0.0"
+SECSINDAY = 86400
+
+def joke():
+	return ("hahahahah")
+
+def main():
+	print joke()
+
+
+if __name__ == '__main__':
+    main()
