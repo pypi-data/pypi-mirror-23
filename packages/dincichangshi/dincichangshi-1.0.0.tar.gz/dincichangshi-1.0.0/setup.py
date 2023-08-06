42c7060a4ed91d34e288bf789dd8b63159265a42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:26 +0000

    Add package dincichangshi/1.0.0/dincichangshi-1.0.0.tar.gz

---
+++ packages/dincichangshi/dincichangshi-1.0.0.tar.gz/dincichangshi-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+name ='dincichangshi',
+version ='1.0.0',
+py_modules =['dincichangshi'],
+author ='sofia',
+author_email ='15620980196@163.com',
+url ='http://www.headfirstlabs.com',
+description ='A simple printer of nested lists',
+)
