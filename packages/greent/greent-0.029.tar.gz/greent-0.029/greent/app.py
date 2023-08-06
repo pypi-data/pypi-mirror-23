4fb4819b6baed19d48b27ac46181fc50611eb7b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:45:53 +0000

    Add package greent/0.029/greent-0.029.tar.gz

---
+++ packages/greent/greent-0.029.tar.gz/greent-0.029/greent/app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask_graphql import GraphQLView
+from schema import Schema
+
+def create_app(path='/graphql', **kwargs):
+    app = Flask(__name__)
+    app.debug = True
+    app.add_url_rule(path, view_func=GraphQLView.as_view('graphql', schema=Schema, **kwargs))
+    return app
+
+def main ():
+    app = create_app(graphiql=True)
+    app.run()
+
+if __name__ == '__main__':
+    main ()
