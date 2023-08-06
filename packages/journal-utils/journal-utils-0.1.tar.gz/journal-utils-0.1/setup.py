ada928b33dbd3b0c55a8bcec68c62db7a8766c1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:00 +0000

    Add package journal-utils/0.1/journal-utils-0.1.tar.gz

---
+++ packages/journal-utils/journal-utils-0.1.tar.gz/journal-utils-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='journal-utils',
+      version='0.1',
+      description='Useful commands for handling and downloading peer-reviewed journal references.',
+      url='https://github.com/firstworldproblems/journal-utils',
+      author='faustus',
+      author_email='vince.cakic@gmail.com',
+      license='MIT',
+      packages=['journal'],
+      zip_safe=False)
