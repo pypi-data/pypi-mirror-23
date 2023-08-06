71c452b3db905f39b9409749be8bf3971e00af20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:08:28 +0000

    Add package meinconf/0.0.4/meinconf-0.0.4-py3-none-any.whl

---
+++ packages/meinconf/meinconf-0.0.4-py3-none-any.whl/meinconf/__init__.py
@@ -0,0 +1,6 @@
+from meinconf.config import Config
+from meinconf.eventful import EventfulConfig
+from meinconf.flask import FlaskConfig
+from meinconf._flask import ConfigAttribute
+
+__all__ = ['ConfigAttribute', 'Config', 'FlaskConfig', 'EventfulConfig']
