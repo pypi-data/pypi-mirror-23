bdbc01c2c22b8f8038e4c467cae953baefec0bbf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:36:00 +0000

    Add package nina-helper/2.0/nina_helper-2.0.tar.gz

---
+++ packages/nina-helper/nina_helper-2.0.tar.gz/nina_helper-2.0/setup.py
@@ -0,0 +1,15 @@
+"""Utiltiy functions for workign on the NinaPro Databases (1 & 2)."""
+
+from setuptools import setup, find_packages
+
+
+setup(name='nina_helper',
+      version='2.0',
+      description='Utiltiy functions for workign on the NinaPro Databases (1 & 2)',
+      author='Lif3line',
+      author_email='adamhartwell2@gmail.com',
+      license='MIT',
+      packages=find_packages(),
+      url='https://github.com/Lif3line/nina_helper_package_mk2',  # use the URL to the github repo
+      download_url='https://github.com/Lif3line/nina_helper_package_mk2/archive/2.0.tar.gz',  # Hack from github address
+      keywords='ninapro emg')
