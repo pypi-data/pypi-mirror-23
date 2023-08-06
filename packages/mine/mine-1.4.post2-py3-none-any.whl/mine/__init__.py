24bd455177eda8661e8f3c16ec4ec2c3d95b8689
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:14:55 +0000

    Add package mine/1.4.post2/mine-1.4.post2-py3-none-any.whl

---
+++ packages/mine/mine-1.4.post2-py3-none-any.whl/mine/__init__.py
@@ -0,0 +1,8 @@
+"""Package for mine."""
+
+__project__ = 'mine'
+__version__ = '1.4.post2'
+
+CLI = 'mine'
+VERSION = '{0} v{1}'.format(__project__, __version__)
+DESCRIPTION = 'Share application state across computers using Dropbox.'
