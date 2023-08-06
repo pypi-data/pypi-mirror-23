59fa974df896d8c13abad0d43c7c34d598ef0607
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:58:32 +0000

    Add package MyPackage_Class/1.0.0/MyPackage_Class-1.0.0.tar.gz

---
+++ packages/MyPackage_Class/MyPackage_Class-1.0.0.tar.gz/MyPackage_Class-1.0.0/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup ( name = 'MyPackage_Class',
+        version = '1.0.0',
+        py_modules = ['MyPackage_Class'],
+        author = 'rajesh kaushal',
+        author_email = 'rajesh.kaushal@chitkara.edu.in',
+        description = 'sample person class file'
+        )
