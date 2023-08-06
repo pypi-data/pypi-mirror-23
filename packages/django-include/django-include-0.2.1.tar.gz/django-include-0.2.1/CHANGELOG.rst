ecc0545b1782b950bfa9f7e49bf7a43ea9eaa500
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:46:25 +0000

    Add package django-include/0.2.1/django-include-0.2.1.tar.gz

---
+++ packages/django-include/django-include-0.2.1.tar.gz/django-include-0.2.1/CHANGELOG.rst
@@ -0,0 +1,16 @@
+Changelog
+---------
+
+0.2.1 (2017-07-13)
+++++++++++++++++++
+* Recognize OneToOne fields as singular related fields
+
+0.2.0 (2017-07-13)
+++++++++++++++++++
+* Add support for many to many fields
+* Restructure the backend to be more extensible
+
+0.1.0 (2017-04-17)
+++++++++++++++++++
+
+* First PyPI release.
---
+++ packages/django-include/django-include-0.2.1.tar.gz/django-include-0.2.1/tests/models.py
@@ -0,0 +1,35 @@
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.fields import GenericRelation
+from django.contrib.contenttypes.models import ContentType
+from django.db import models
+
+from include import IncludeManager
+
+
+class Alias(models.Model):
+    content_type = models.ForeignKey(ContentType, null=True, blank=True)
+    describes = GenericForeignKey()
+    name = models.CharField(max_length=32)
+    object_id = models.PositiveIntegerField(null=True, blank=True)
+
+    class Meta:
+        ordering = ('name', )
+
+
+class Archetype(models.Model):
+    aliases = GenericRelation(Alias)
+    color = models.CharField(max_length=75)
+    num_toes = models.PositiveIntegerField(default=5)
+
+    objects = IncludeManager()
+
+
+class Cat(models.Model):
+    aliases = GenericRelation(Alias)
+    archetype = models.ForeignKey(Archetype)
+    name = models.CharField(max_length=50)
+    parent = models.ForeignKey('Cat', null=True, related_name='children')
+    siblings = models.ManyToManyField('Cat', related_name='related_to')
+    emergency_contact = models.OneToOneField('Cat', null=True, related_name='emergency_contact_for')
+
+    objects = IncludeManager()
