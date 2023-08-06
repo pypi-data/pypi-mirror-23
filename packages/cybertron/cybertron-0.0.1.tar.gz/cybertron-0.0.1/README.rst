d2a0d6aa92b693cc82ee47aeeb60d5ff69830979
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:30:26 +0000

    Add package cybertron/0.0.1/cybertron-0.0.1.tar.gz

---
+++ packages/cybertron/cybertron-0.0.1.tar.gz/cybertron-0.0.1/README.rst
@@ -0,0 +1,6 @@
+About
+=====
+
+**Cybertron is a set of tools for writing twitter bots**
+
+.. image:: https://upload.wikimedia.org/wikipedia/en/2/2d/MovieCybertron.jpg
---
+++ packages/cybertron/cybertron-0.0.1.tar.gz/cybertron-0.0.1/cybertron/__init__.py
@@ -0,0 +1,12 @@
+__title__ = "brittle_wit"
+__description__ = "Tools for writing twitter bots"
+__uri__ = "https://github.com/jbn/cybertron"
+__doc__ = __description__ + " <" + __uri__ + ">"
+__license__ = "MIT"
+__copyright__ = "Copyright (c) 2017 John Bjorn Nelson"
+__version__ = "0.0.1"
+__author__ = "John Bjorn Nelson"
+__email__ = "jbn@abreka.com"
+
+import os
+
