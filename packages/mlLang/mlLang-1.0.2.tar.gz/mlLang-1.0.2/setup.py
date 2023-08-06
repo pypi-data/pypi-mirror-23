9c7a24d7295e01d698855d4aeb702c5d7bc83979
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:26 +0000

    Add package mlLang/1.0.2/mlLang-1.0.2.tar.gz

---
+++ packages/mlLang/mlLang-1.0.2.tar.gz/mlLang-1.0.2/setup.py
@@ -0,0 +1,73 @@
+"""A setuptools based setup module.
+
+See:
+https://packaging.python.org/en/latest/distributing.html
+https://github.com/pypa/sampleproject
+"""
+
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='mlLang',
+
+    version='1.0.2',
+
+    description='Unified Machine Learning Language',
+    long_description=long_description,
+
+    # The project's main homepage.
+    url='https://github.com/sfeuerriegel/mllang',
+    download_url='https://github.com/ndomar/mllang/archive/1.0.0.tar.gz',
+
+    # Author details
+    author='Stefan Feuerriegel',
+    author_email='stefan.feuerriegel@is.uni-freiburg.de',
+
+
+    license='MIT',
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries',
+
+        'License :: OSI Approved :: MIT License',
+
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+
+    keywords='classifier model-learning xml',
+
+    packages=find_packages(),
+
+    # List run-time dependencies here.  These will be installed by pip when
+    # your project is installed. For an analysis of "install_requires" vs pip's
+    # requirements files see:
+    # https://packaging.python.org/en/latest/requirements.html
+    install_requires=['sklearn', 'numpy', 'lxml'],
+
+    # List additional groups of dependencies here (e.g. development
+    # dependencies). You can install these using the following syntax,
+    # for example:
+    # $ pip install -e .[dev,test]
+    extras_require={
+        'dev': ['check-manifest'],
+        'test': ['coverage'],
+    },
+)
