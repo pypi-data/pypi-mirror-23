b1b5d986bcecb999193e61dbebf553afc4a1ecf8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:37 +0000

    Add package web.dispatch/3.0.0/web.dispatch-3.0.0.tar.gz

---
+++ packages/web.dispatch/web.dispatch-3.0.0.tar.gz/web.dispatch-3.0.0/web/dispatch/core/__init__.py
@@ -0,0 +1,2 @@
+from .crumb import Crumb
+from .util import nodefault, ipeek, prepare_path, opts
---
+++ packages/web.dispatch/web.dispatch-3.0.0.tar.gz/web.dispatch-3.0.0/web/dispatch/core/release.py
@@ -0,0 +1,16 @@
+# encoding: utf-8
+
+"""Release information about Dispatch."""
+
+from __future__ import unicode_literals
+
+from collections import namedtuple
+
+
+version_info = namedtuple('version_info', ('major', 'minor', 'micro', 'releaselevel', 'serial'))(3, 0, 0, 'final', 0)
+version = ".".join([str(i) for i in version_info[:3]]) + ((version_info.releaselevel[0] + str(version_info.serial)) if version_info.releaselevel != 'final' else '')
+
+author = namedtuple('Author', ['name', 'email'])("Alice Bevan-McGregor", 'alice@gothcandy.com')
+description = "Dispatch; a method to resolve path components to Python objects and enumerate available children using pluggable search mechanisms."
+copyright = "2009-2017, Alice Bevan-McGregor and contributors"
+url = 'https://github.com/marrow/web.dispatch'
