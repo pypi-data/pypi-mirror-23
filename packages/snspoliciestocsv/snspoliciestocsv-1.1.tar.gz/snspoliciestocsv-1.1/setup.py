7ead1188ae3bdfb0d8fd6273052e89e8426a9b68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:52 +0000

    Add package snspoliciestocsv/1.1/snspoliciestocsv-1.1.tar.gz

---
+++ packages/snspoliciestocsv/snspoliciestocsv-1.1.tar.gz/snspoliciestocsv-1.1/setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+setup(name='snspoliciestocsv',
+      version='1.1',
+      description='A simple script to extract policies from a Stormshield Network Security device configuration file to CSV',
+      long_description=open('snspoliciestocsv/README.md').read(),
+      url='https://github.com/maaaaz/snspoliciestocsv',
+      author='Thomas D.',
+      author_email='tdebize@mail.com',
+      license='LGPL',
+      classifiers=[
+        'Topic :: Security',
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Information Technology',
+        'License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)',
+      ],
+      keywords='stormshield network security configuration csv',
+      packages=find_packages(),
+      python_requires='>=3',
+      entry_points = {
+        'console_scripts': ['snspoliciestocsv=snspoliciestocsv.snspoliciestocsv:main'],
+      },
+      include_package_data=True)
