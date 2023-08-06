73ab89982c43ee39af846200938b5bdffc1519cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:46:39 +0000

    Add package graphite-analytics/0.1/graphite-analytics-0.1.tar.gz

---
+++ packages/graphite-analytics/graphite-analytics-0.1.tar.gz/graphite-analytics-0.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'graphite-analytics',
+  packages = ['graphite'], # this must be the same as the name above
+  version = '0.1',
+  description = 'Create a print-out template for your google analytics data',
+  author = 'Arian Moslem',
+  author_email = 'amoslem678@gmail.com',
+  url = 'https://github.com/ARM-open/Graphite', # use the URL to the github repo
+  download_url = 'https://github.com/airrbear/Graphite/archive/0.1.tar.gz', # I'll explain this in a second
+  keywords = ['Google analytics', 'analytics', 'templates'], # arbitrary keywords
+  classifiers = [],
+)
