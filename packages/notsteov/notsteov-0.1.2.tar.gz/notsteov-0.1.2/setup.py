0e906b3fcffafb29f83fc0775ab193565c8278a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:14:26 +0000

    Add package notsteov/0.1.2/notsteov-0.1.2.tar.gz

---
+++ packages/notsteov/notsteov-0.1.2.tar.gz/notsteov-0.1.2/setup.py
@@ -0,0 +1,15 @@
+import json
+from setuptools import setup
+
+with open("config.json", "rb") as fp:
+    config = json.loads(fp.read().decode("utf-8"))
+
+with open("VERSION.txt", "rb") as fp:
+    config["version"] = fp.read().strip().decode("ascii")
+
+config["long_description"] = "There is no long description yet"
+config["packages"] = [ config["name"] ]
+config["url"] = "https://github.com/FoleyDiver/{name}".format(**config)
+config["download_url"] = "{url}/archive/v{version}.tar.gz".format(**config)
+
+setup(**config)
