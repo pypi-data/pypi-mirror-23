985f2250bee771bf8d3704ab09e9d6782db3d185
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:16:04 +0000

    Add package frctba/1.2/frctba-1.2.tar.gz

---
+++ packages/frctba/frctba-1.2.tar.gz/frctba-1.2/setup.py
@@ -0,0 +1,13 @@
+
+from distutils.core import setup
+setup(name='frctba',
+      version='1.2',
+      author='Josh Bacon',
+      author_email='bacon.josh09@gmail.com',
+      packages=['tba',],
+      url='https://github.com/CrispyBacon1999/WoodieFlowersBot',
+      requires=[
+            'requests',
+            'json',
+            're'
+          ])
