94eb61df2fcc63d259aecea7d7954423e3d8bba7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:33:22 +0000

    Add package multiplexcd/1.1/multiplexcd-1.1.tar.gz

---
+++ packages/multiplexcd/multiplexcd-1.1.tar.gz/multiplexcd-1.1/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(name='multiplexcd',
+		version='1.1',
+		description='Functions to peform multiplex community detection',
+		author='Michael Siemon',
+		author_email='mcs296@cornell.edu',
+		url='https://github.com/michaelsiemon/multiplexcd',
+		py_modules=['multiplexcd'],
+		license='MIT',
+		keywords='network graph community detection multiplex',
+		requires=['igraph', 'numpy', 'scipy']
+		)
