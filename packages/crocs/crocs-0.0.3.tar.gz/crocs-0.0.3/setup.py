660f1df5fd746cc899350f3d16dfd38276dc5834
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:33:55 +0000

    Add package crocs/0.0.3/crocs-0.0.3.tar.gz

---
+++ packages/crocs/crocs-0.0.3.tar.gz/crocs-0.0.3/setup.py
@@ -0,0 +1,48 @@
+#! /usr/bin/env python2
+
+from distutils.core import setup
+setup(name="crocs",
+      version="0.0.3",
+      py_modules=["crocs"],
+      author="Iury O. G. Figueiredo",
+      author_email="ioliveira@id.uff.br",
+      url='https://github.com/iogf/crocs',
+      download_url='https://github.com/iogf/crocs/releases',
+      keywords=[],
+      classifiers=[],
+      description="",)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
