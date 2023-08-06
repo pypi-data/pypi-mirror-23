4e0f3697686636cce4e32512b15fc9aa96b5c4bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:43 +0000

    Add package acerim/0.0.5.dev0/acerim-0.0.5.dev0-py2.py3-none-any.whl

---
+++ packages/acerim/acerim-0.0.5.dev0-py2.py3-none-any.whl/acerim/version.py
@@ -0,0 +1,14 @@
+# Format expected by setup.py and doc/source/conf.py: string of form "X.Y.Z"
+_version_major = 0
+_version_minor = 0
+_version_maintenance = 5  # use '' for first of series, number for 1 and above
+_version_extra = 'dev' # dev for in development (NEVER push with dev), '' for full release
+
+# Construct full version string from these.
+_ver = [_version_major, _version_minor]
+if _version_maintenance:
+    _ver.append(_version_maintenance)
+if _version_extra:
+    _ver.append(_version_extra)
+
+__version__ = '.'.join(map(str, _ver))
