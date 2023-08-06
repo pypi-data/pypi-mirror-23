ca6cd632fdc614a406aa44b4ed095c3ec9eab28d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:48 +0000

    Add package nexus_uploader/1.0.9/nexus_uploader-1.0.9.tar.gz

---
+++ packages/nexus_uploader/nexus_uploader-1.0.9.tar.gz/nexus_uploader-1.0.9/setup.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+
+import io, sys
+from os import path
+from setuptools import setup, find_packages
+
+version = '1.0.9'
+setup_requires = []
+
+if 'nexus_upload' in sys.argv:
+    sys.argv.remove('nexus_upload')
+    setup_requires.append('nexus_uploader')
+
+if '--snapshot' in sys.argv:
+    sys.argv.remove('--snapshot')
+    version += '-SNAPSHOT'
+
+here = path.abspath(path.dirname(__file__))
+
+with io.open(path.join(here, 'README.rst'), encoding='utf-8') as long_desc_file:
+    long_description = long_desc_file.read()
+
+with io.open(path.join(here, 'requirements.txt')) as requirements_file:
+    requirements = requirements_file.readlines()
+
+setup(
+    name='nexus_uploader',
+    description='CLI tool to upload Python packages listed in a requirements.txt file into a Sonatype Nexus (from Pypi), and return the list of the artifact URLs',
+    long_description=long_description,
+    author='Lucas Cimon',
+    url='http://github.com/voyages-sncf-technologies/nexus_uploader',
+    install_requires=requirements,
+    packages=find_packages(),
+    version=version,
+    setup_requires=setup_requires,
+    zip_safe=True,  # http://peak.telecommunity.com/DevCenter/setuptools#setting-the-zip-safe-flag
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: Apache Software License',
+        'Natural Language :: English',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 2',
+        'Operating System :: OS Independent',
+        'Topic :: Utilities',
+    ],
+    keywords='nexus artifacts packages upload requirements pip-compile',
+    license='Apache License 2.0',
+    include_package_data=True,  # Active la prise en compte du fichier MANIFEST.in
+    entry_points={
+        'console_scripts': ['pyRequirements2nexus = nexus_uploader.pyrequirements2nexus:main'],
+        'distutils.commands': ['nexus_upload = nexus_uploader.nexus_upload_cmd:NexusUploadCmd'],
+    },
+)
