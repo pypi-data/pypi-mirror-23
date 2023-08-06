a2f79d5d3e730415ee6fb19177c250aa4b11267e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:18:10 +0000

    Add package pg_populator_lib/0.0.3/pg_populator_lib-0.0.3.tar.gz

---
+++ packages/pg_populator_lib/pg_populator_lib-0.0.3.tar.gz/pg_populator_lib-0.0.3/pg_populator_lib/config.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+# Author: Jose Antonio Quevedo <joseantonio.quevedo@gmail.com>
+# 2017-07-09
+
+
+# CONSTANTS
+DB_USER = "carto_test_user"
+POSTGRESQL_LIB_DIR = '/var/lib/postgresql/9.6/'
+DB_PREFIX = "db_carto_test_"
+TABLE_PREFIX = "mytable_"
