10c6eb92df98d0cd2ffa62f5bd80116b3df41981
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:32:32 +0000

    Add package infi.eventlog/0.1.10/infi.eventlog-0.1.10-py2-none-any.whl

---
+++ packages/infi.eventlog/infi.eventlog-0.1.10-py2-none-any.whl/infi.eventlog-0.1.10.dist-info/DESCRIPTION.rst
@@ -0,0 +1,3 @@
+Bindings to Windows Event Log
+
+
---
+++ packages/infi.eventlog/infi.eventlog-0.1.10-py2-none-any.whl/infi/eventlog/__version__.py
@@ -0,0 +1,11 @@
+__version__ = "0.1.10"
+__git_commiter_name__ = "Arnon Yaari"
+__git_commiter_email__ = "arnony@infinidat.com"
+__git_branch__ = u'(HEAD detached at v0.1.10)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = '153b8cab2d19aba69cb997ef30b4f0bfe66e1470'
+__git_head_subject__ = u'Finished Release v0.1.10'
+__git_head_message__ = u''
+__git_dirty_diff__ = u''
+__git_commit_date__ = '2017-07-09 14:42:03'
