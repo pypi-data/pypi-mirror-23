5c3614a7e4a88f9285866e9ecb5f5bcd5cf26b2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:15:33 +0000

    Add package wisdomnuggets/1.2/wisdomnuggets-1.2.tar.gz

---
+++ packages/wisdomnuggets/wisdomnuggets-1.2.tar.gz/wisdomnuggets-1.2/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+def readme():
+	with opne('README.rst') as f:
+		return f.read()
+
+setup(name='wisdomnuggets',
+	version='1.2',
+	description='The wisest maxim in the world',
+	url='https://github.com/ss1921',
+	author='1921Designs',
+	author_email='im1921designs@gmail.com',
+	license='MIT',
+	packages=['wisdomnuggets'],
+	install_requires=['markdown'],
+	include_package_data=True,
+	zip_safe=False,
+	entry_points = {
+        'console_scripts': ['Wisest-maxim=wisdomnuggets.command_line:main'],
+    },
+	test_suite='nose.collector',
+	tests_require=['nose'],
+	)
---
+++ packages/wisdomnuggets/wisdomnuggets-1.2.tar.gz/wisdomnuggets-1.2/wisdomnuggets/text.py
@@ -0,0 +1,4 @@
+from markdown import markdown
+
+def maxim():
+	return("What does not challenge you, will not change you.")
