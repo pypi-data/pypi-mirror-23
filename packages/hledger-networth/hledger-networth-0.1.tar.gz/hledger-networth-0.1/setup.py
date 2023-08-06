e200f5eb5845d3aeda304777e25814cdee1f7cf2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:33:03 +0000

    Add package hledger-networth/0.1/hledger-networth-0.1.tar.gz

---
+++ packages/hledger-networth/hledger-networth-0.1.tar.gz/hledger-networth-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='hledger-networth',
+      version='0.1',
+      description='Generate networth report',
+      url='https://gitlab.com/vincecima/hledger-networth',
+      author='Vince Cima',
+      license='MIT',
+      packages=['hledger-networth'],
+      zip_safe=False)
