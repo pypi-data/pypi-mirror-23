9d9da8f42d45aa7a0b9e80580065268174a4885c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:32:32 +0000

    Add package infi.blocking/0.0.2/infi.blocking-0.0.2-py2-none-any.whl

---
+++ packages/infi.blocking/infi.blocking-0.0.2-py2-none-any.whl/infi/blocking/__version__.py
@@ -0,0 +1,11 @@
+__version__ = "0.0.2"
+__git_commiter_name__ = "Arnon Yaari"
+__git_commiter_email__ = "arnony@infinidat.com"
+__git_branch__ = u'(HEAD detached at v0.0.2)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = '4afc45c19ff13c71c947a411db7ab06650f28279'
+__git_head_subject__ = u'Finished Release v0.0.2'
+__git_head_message__ = u''
+__git_dirty_diff__ = u''
+__git_commit_date__ = '2017-07-09 14:34:23'
