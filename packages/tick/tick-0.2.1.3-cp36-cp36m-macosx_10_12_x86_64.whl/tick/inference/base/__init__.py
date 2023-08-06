3efc8239cbcec3d3e4d09a2e816b6fb7d50f0ad0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:09 +0000

    Add package tick/0.2.1.3/tick-0.2.1.3-cp36-cp36m-macosx_10_12_x86_64.whl

---
+++ packages/tick/tick-0.2.1.3-cp36-cp36m-macosx_10_12_x86_64.whl/tick/inference/base/__init__.py
@@ -0,0 +1,7 @@
+# License: BSD 3 clause
+
+
+from .learner_optim import LearnerOptim
+from .learner_glm import LearnerGLM
+from .learner_hawkes_param import LearnerHawkesParametric
+from .learner_hawkes_noparam import LearnerHawkesNoParam
