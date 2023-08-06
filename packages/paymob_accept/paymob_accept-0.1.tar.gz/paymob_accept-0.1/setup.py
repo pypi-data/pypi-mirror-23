9d8675c727d7a3b87863adcd1d1c539b55516609
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:44 +0000

    Add package paymob_accept/0.1/paymob_accept-0.1.tar.gz

---
+++ packages/paymob_accept/paymob_accept-0.1.tar.gz/paymob_accept-0.1/setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name='paymob_accept',
+    packages=['paymob_accept'],
+    version='0.1',
+    description='Easy integration with Accept API offered by PayMob',
+    author='Mohamed Ihab',
+    author_email='mohamedihab@paymobsolutions.com',
+    url='https://github.com/MohamedIhab/PayMob-Accept-Integration/releases/tag/0.1',
+    download_url='https://github.com/MohamedIhab/PayMob-Accept-Integration/releases/tag/0.1',
+    keywords=['paymob', 'PayMob', 'Paymob', 'Pay mob', 'Pay Mob', 'accept', 'Accept', 'paymob_accept'],
+    classifiers=[],
+)
