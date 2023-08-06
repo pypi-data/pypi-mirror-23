b6e5d73428fa9c4af0dca3864bf5000a4539ea4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:11:40 +0000

    Add package tapioca-meetup-client/0.3/tapioca-meetup-client-0.3.tar.gz

---
+++ packages/tapioca-meetup-client/tapioca-meetup-client-0.3.tar.gz/tapioca-meetup-client-0.3/tapioca_meetup/__init__.py
@@ -0,0 +1,8 @@
+# coding: utf-8
+
+__author__ = 'Luiz Menezes'
+__email__ = 'luiz.menezesf@gmail.com'
+__version__ = '0.3'
+
+
+from .tapioca_meetup import Meetup  # noqa
---
+++ packages/tapioca-meetup-client/tapioca-meetup-client-0.3.tar.gz/tapioca-meetup-client-0.3/tapioca_meetup/resource_mapping.py
@@ -0,0 +1,8 @@
+# coding: utf-8
+
+RESOURCE_MAPPING = {
+    'event_attendances': {
+        'resource': 'https://api.meetup.com/{urlname}/events/{event_id}/attendance',
+        'docs': 'http://www.documentation.com/link/to/resource/documentation'
+    },
+}
