0c890e15498bc08adff36a25ff05a9ad5da49593
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:30:07 +0000

    Add package snipssmartercoffee/0.1.0/snipssmartercoffee-0.1.0-py2.py3-none-any.whl

---
+++ packages/snipssmartercoffee/snipssmartercoffee-0.1.0-py2.py3-none-any.whl/snipssmartercoffee/snipssmartercoffee.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# encoding: utf-8
+
+import socket
+
+
+class SnipsSmarterCoffee:
+
+    SMARTER_PORT = 2081
+    BUFFER_SIZE = 10
+
+    def __init__(self, hostname):
+        self.hostname = hostname
+
+    def brew(self):
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.connect((self.hostname, self.SMARTER_PORT))
+            s.send("7")
+            data = s.recv(self.BUFFER_SIZE)
+            s.close()
+        except socket.error, msg:
+            return
