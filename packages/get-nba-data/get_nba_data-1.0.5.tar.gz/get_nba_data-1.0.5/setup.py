f145da6ec8b29f12015ac58d4bb031beeb404814
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:35 +0000

    Add package get-nba-data/1.0.5/get_nba_data-1.0.5.tar.gz

---
+++ packages/get-nba-data/get_nba_data-1.0.5.tar.gz/get_nba_data-1.0.5/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+from codecs import open  # To use a consistent encoding
+from os import path
+
+setup(name='get_nba_data',
+      version='1.0.5',
+      description='easy tool to load nba data with',
+      url='http://github.com/jkim65537/get_nba_data',
+      author='Jun Kim',
+      author_email='jkim65537@gmail.com',
+      license='MIT',
+      packages=['get_nba_data'],
+      download_url = 'https://github.com/jkim65537/get_nba_data/dist/get_nba_data-1.0.5.tar.gz',
+      install_requires=['requests', 'pandas'],
+      zip_safe=False)
