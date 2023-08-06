dd7eca934b02cd9ff7c2aab9909ec74374fdcc58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:49:21 +0000

    Add package pairplotr/1.2.3/pairplotr-1.2.3.tar.gz

---
+++ packages/pairplotr/pairplotr-1.2.3.tar.gz/pairplotr-1.2.3/setup.py
@@ -0,0 +1,31 @@
+import os
+from setuptools import setup
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "pairplotr",
+    version = "1.2.3",
+    author = "Christopher Shymansky",
+    author_email = "CMShymansky@gmail.com",
+    description = ("Pairplotr is a Python library used to graph combinations " \
+                   "of numerical and categorical data in a pair plot"),
+    license = "OSI Approved :: Apache Software License",
+    keywords = "scikit-learn pandas data visualization pairplot",
+    url = "http://packages.python.org/pairplotr",
+    packages=['pairplotr'],
+    long_description=read('README.md'),
+    install_requires=[
+            'seaborn',
+            'inspect',
+            'numpy',
+            'matplotlib',
+            'pandas'
+        ],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: Apache Software License",
+    ],
+)
