aa3e355af387ac5b4c4e1f4116b236be20c4fb82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:08:54 +0000

    Add package flake8-config-4catalyzer/0.2.1/flake8-config-4catalyzer-0.2.1.tar.gz

---
+++ packages/flake8-config-4catalyzer/flake8-config-4catalyzer-0.2.1.tar.gz/flake8-config-4catalyzer-0.2.1/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup
+
+
+setup(
+    name='flake8-config-4catalyzer',
+    version='0.2.1',
+    url='https://github.com/4Catalyzer/python/tree/packages/flake8-config-4catalyzer',
+    author="Alex Rothberg",
+    author_email='arothberg@4catalyzer.com',
+    license='MIT',
+    classifiers=(
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Framework :: Flake8',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Software Development :: Quality Assurance',
+    ),
+    keywords='flake8',
+    install_requires=(
+        'flake8',
+        'flake8-commas',
+        'flake8-import-order',
+    ),
+    extras_require={
+        ':python_version>="3.5"': ('flake8-bugbear',),
+    },
+)
