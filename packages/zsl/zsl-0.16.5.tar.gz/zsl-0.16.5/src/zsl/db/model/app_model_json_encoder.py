6fde16af09e0c728d1af3600fd336c6f34e1255b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:59 +0000

    Add package zsl/0.16.5/zsl-0.16.5.tar.gz

---
+++ packages/zsl/zsl-0.16.5.tar.gz/zsl-0.16.5/src/zsl/db/model/app_model_json_encoder.py
@@ -0,0 +1,23 @@
+"""
+:mod:`zsl.db.model.app_model_json_encoder`
+------------------------------------------
+
+.. moduleauthor:: Martin Babka
+"""
+from __future__ import unicode_literals
+from json.encoder import JSONEncoder
+from zsl.db.model import AppModel
+
+
+class AppModelJSONEncoder(JSONEncoder):
+    def default(self, o):
+        if isinstance(o, AppModel):
+            d = dict(o.__dict__)
+            for k in o._not_serialized_attributes:
+                if k in d:
+                    d.pop(k)
+            return d
+        elif isinstance(o, object):
+            return dict(o.__dict__)
+        else:
+            return JSONEncoder.default(self, o)
