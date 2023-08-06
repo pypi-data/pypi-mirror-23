e48a9af42d9bc525de54f374edfcb2b697556a81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:56:39 +0000

    Add package mlutility/0.1.1/mlutility-0.1.1.tar.gz

---
+++ packages/mlutility/mlutility-0.1.1.tar.gz/mlutility-0.1.1/mlutility/__init__.py
@@ -0,0 +1,6 @@
+'''start to import some packages'''
+import pandas as pd
+import numpy as np
+from sklearn import linear_model
+import datetime
+import tensorflow as tf
---
+++ packages/mlutility/mlutility-0.1.1.tar.gz/mlutility-0.1.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(name='mlutility',
+      version='0.1.1',
+      description='Some utilities for machine learning',
+      url='https://github.com/Wulala526/MLUtility',
+      author='Wulala',
+      author_email='tyhh_123@hotmail.com',
+      license='BSD 2-Clause License',
+      packages=['mlutility'],
+      zip_safe=False)
