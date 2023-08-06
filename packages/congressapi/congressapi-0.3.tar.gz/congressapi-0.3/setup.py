15e8e3b65e0ed03caebe33bca4ded9e030fc3148
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:34:23 +0000

    Add package congressapi/0.3/congressapi-0.3.tar.gz

---
+++ packages/congressapi/congressapi-0.3.tar.gz/congressapi-0.3/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(name='congressapi',
+      version='0.3',
+      description='Wrapper for ProPublica Congress API',
+      url='http://github.com/kjchavez/congressapi',
+      author='Kevin Chavez',
+      author_email='kevin.j.chavez@gmail.com',
+      license='MIT',
+      install_requirements = [
+          "enum",
+          "requests"
+      ],
+      packages=['congressapi'],
+      zip_safe=False)
