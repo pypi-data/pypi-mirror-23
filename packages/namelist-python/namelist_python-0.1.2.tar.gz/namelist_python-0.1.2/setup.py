754e15f9c962dfdc80a0e5b5002374ca52a600b7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:28:45 +0000

    Add package namelist-python/0.1.2/namelist_python-0.1.2.tar.gz

---
+++ packages/namelist-python/namelist_python-0.1.2.tar.gz/namelist_python-0.1.2/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+  name = 'namelist_python',
+  packages = ['namelist_python'],
+  version = '0.1.2',
+  description = 'Fortran namelist file parser in Python',
+  author = 'Leif Denby',
+  author_email = 'leifdenby@gmail.com',
+  url = 'https://github.com/leifdenby/namelist_python',
+  download_url = 'https://github.com/leifdenby/namelist_python/archive/0.1.tar.gz',
+  keywords = ['namelist', 'fortran', 'parsing'],
+  classifiers = [],
+)
