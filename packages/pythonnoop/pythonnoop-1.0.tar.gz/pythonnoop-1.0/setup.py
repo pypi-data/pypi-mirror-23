d4fc4f48da91bfeef3a61912d0e8bbeb7b6ec859
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:46:07 +0000

    Add package pythonnoop/1.0/pythonnoop-1.0.tar.gz

---
+++ packages/pythonnoop/pythonnoop-1.0.tar.gz/pythonnoop-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='pythonnoop',
+    version='1.0',
+    description='no-ops modules for you',
+    author='Josh Palmer',
+    author_email='josh@joshpalmer.co.uk'
+)
