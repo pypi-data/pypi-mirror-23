9d3f5f62a95fcda654dd7bdc40811956afece85a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:10:20 +0000

    Add package skitai/0.26.7.1/skitai-0.26.7.1.tar.gz

---
+++ packages/skitai/skitai-0.26.7.1.tar.gz/skitai-0.26.7.1/skitai/server/handlers/pingpong_handler.py
@@ -0,0 +1,11 @@
+from . import wsgi_handler
+
+class Handler (wsgi_handler.Handler):
+	def match (self, request):
+		return request.uri == "/ping"
+		
+	def handle_request (self, request):
+		request.response.start_response ("200 OK", [("Content-Type", "text/plain"), ("Content-Length", "4")])
+		request.response.push (b"pong")
+		request.response.done ()
+		
