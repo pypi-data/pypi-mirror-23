32712949d9182ddc6dba011573608fa56d6d1546
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:01 +0000

    Add package aiokafka/0.2.3/aiokafka-0.2.3.tar.gz

---
+++ packages/aiokafka/aiokafka-0.2.3.tar.gz/aiokafka-0.2.3/aiokafka/__init__.py
@@ -0,0 +1,23 @@
+import sys
+
+from .errors import ConsumerStoppedError
+
+try:
+    from asyncio import ensure_future
+except ImportError:
+    from asyncio import async as ensure_future
+
+__version__ = '0.2.3'
+PY_35 = sys.version_info >= (3, 5)
+
+from .client import AIOKafkaClient  # noqa
+from .producer import AIOKafkaProducer  # noqa
+from .consumer import AIOKafkaConsumer  # noqa
+
+__all__ = [
+    "AIOKafkaProducer",
+    "AIOKafkaConsumer",
+    "ConsumerStoppedError",
+]
+
+(AIOKafkaClient, ensure_future)
