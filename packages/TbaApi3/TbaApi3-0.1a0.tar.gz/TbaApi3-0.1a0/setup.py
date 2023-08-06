6b16fa98c84e83e419598859c9e15a1bcf79e895
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:08:02 +0000

    Add package TbaApi3/0.1a0/TbaApi3-0.1a0.tar.gz

---
+++ packages/TbaApi3/TbaApi3-0.1a0.tar.gz/TbaApi3-0.1a0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='TbaApi3',
+      version='0.1a',
+      description='A Python library for the TBA API v3',
+      url='https://github.com/DanWaxman/TbaApi3',
+      author='Dan Waxman',
+      author_email='dan.waxman1@gmail.com',
+      license='MIT',
+      packages=['TbaApi3'],
+      zip_safe=False)
