0531c0b1d7902ded31078b6afdac365ffa299436
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:15 +0000

    Add package jumpy/0.2.2/jumpy-0.2.2.tar.gz

---
+++ packages/jumpy/jumpy-0.2.2.tar.gz/jumpy-0.2.2/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+from setuptools import find_packages
+
+
+setup(name='jumpy',
+      version='0.2.2',
+      description='Numpy and nd4j interop',
+      author='Adam Gibson',
+      author_email='adam@skymind.io',
+      url='https://github.com/deeplearning4j/jumpy',
+      license='Apache',
+      install_requires=['six', 'pyjnius'],
+      extras_require={
+          'visualize': ['pydot-ng'],
+          'tests': ['pytest',
+                    'pytest-pep8',
+                    'pytest-xdist'],
+      },
+      packages=find_packages())
