f952e46cdb934cae9ba6015e3efe79234171d99c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:36:17 +0000

    Add package crosscompute-types/0.6.9/crosscompute-types-0.6.9.tar.gz

---
+++ packages/crosscompute-types/crosscompute-types-0.6.9.tar.gz/crosscompute-types-0.6.9/setup.py
@@ -0,0 +1,35 @@
+from os.path import abspath, dirname, join
+from setuptools import setup
+
+
+FOLDER = dirname(abspath(__file__))
+DESCRIPTION = '\n\n'.join(open(join(FOLDER, x)).read().strip() for x in [
+    'README.rst'])
+setup(
+    name='crosscompute-types',
+    version='0.6.9',
+    description='Default data type plugins for CrossCompute',
+    long_description=DESCRIPTION,
+    classifiers=[
+        'Programming Language :: Python',
+        'Framework :: Pyramid',
+        'Topic :: Internet :: WWW/HTTP :: WSGI :: Application',
+        'License :: OSI Approved :: MIT License',
+    ],
+    author='CrossCompute Inc',
+    author_email='support@crosscompute.com',
+    url='https://crosscompute.com/docs',
+    keywords='web pyramid pylons crosscompute',
+    zip_safe=True,
+    setup_requires=[
+        'pytest-runner'
+    ],
+    install_requires=[
+        'crosscompute>=0.6.9',
+        'crosscompute-integer>=0.6.7',
+        'crosscompute-text>=0.6.7',
+        'crosscompute-select>=0.6.7',
+        'crosscompute-table>=0.6.7.1',
+        'crosscompute-image>=0.6.7.2',
+        'crosscompute-geotable>=0.6.7',
+    ])
