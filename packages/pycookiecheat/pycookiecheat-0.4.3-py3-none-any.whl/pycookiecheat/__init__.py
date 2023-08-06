c1264941247738a6b5be2e39e6a6fbefddce60cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:34:33 +0000

    Add package pycookiecheat/0.4.3/pycookiecheat-0.4.3-py3-none-any.whl

---
+++ packages/pycookiecheat/pycookiecheat-0.4.3-py3-none-any.whl/pycookiecheat/__init__.py
@@ -0,0 +1,7 @@
+"""__init__.py :: Exposes chrome_cookies function."""
+
+from pycookiecheat.pycookiecheat import chrome_cookies  # noqa
+
+__author__ = 'Nathan Henrie'
+__email__ = 'nate@n8henrie.com'
+__version__ = '0.4.3'
