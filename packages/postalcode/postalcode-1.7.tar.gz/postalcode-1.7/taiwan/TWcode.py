6122d049ea71c4439ca50c4f7c6fd6623db90b14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:56:14 +0000

    Add package postalcode/1.7/postalcode-1.7.tar.gz

---
+++ packages/postalcode/postalcode-1.7.tar.gz/postalcode-1.7/taiwan/TWcode.py
@@ -0,0 +1,10 @@
+#coding=utf-8
+import pandas as pd
+from . import _chp_csv_path
+
+def ZipCodeTW(city,district):
+    city=unicode(city,'utf-8')
+    district= unicode(district,'utf-8')
+    r= pd.read_excel(_chp_csv_path,sheetname=u'{}'.format(city))
+    return r[u'{}'.format(district)][0]
+#ZipCodeTW('新北市','新店區')
