f62dd3363bf038ce67838c195b6e31e482f24d9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:56:38 +0000

    Add package pickler-magic/1.0/pickler-magic-1.0.tar.gz

---
+++ packages/pickler-magic/pickler-magic-1.0.tar.gz/pickler-magic-1.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pickler-magic',
+      version='1.0',
+      author='Dmitry Lunin',
+      author_email='mail.lunin@gmail.com',
+      packages=['pickler'],
+      requires=['IPython'],
+      description='IPython Notebook magic to make and load pickle dumps of all assignments in a cell or a line',
+      url='https://github.com/DLunin/pickler',
+      download_url='https://github.com/DLunin/pickler/archive/1.0.tar.gz')
