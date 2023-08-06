9e99eca94d55763cbebad770263fb4796abcb822
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:08:38 +0000

    Add package utagent/0.0.58/utagent-0.0.58-py2.py3-none-any.whl

---
+++ packages/utagent/utagent-0.0.58-py2.py3-none-any.whl/slave/api/api_server.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from bottle import Bottle, run, request
+from slave.config import config
+from slave.util.adb import AdbTools
+from slave.globalvar import get_queue
+import json
+import logging
+
+logger = logging.getLogger(__name__)
+
+app = Bottle()
+a = AdbTools()
+
+
+def start_api_server():
+    # api.post_device(sysInfo.get_host_ip(), a.get_device_brand(), a.get_device_model(), a.get_device_sdk_version(),
+    #                 a.get_device_android_version(), a.get_device_id())
+    run(app, host='', port=config["port"])
+
+
+@app.post('/jobs')
+def task():
+    get_queue().put(request.body.read())
+
+
+@app.get('/ping')
+def ping():
+    return json.dumps(True)
