622b31d81de4ad866aaedcab9b0a0097c6222073
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:02 +0000

    Add package nbsolutions/0.1.1/nbsolutions-0.1.1-py3-none-any.whl

---
+++ packages/nbsolutions/nbsolutions-0.1.1-py3-none-any.whl/nbsolutions/__init__.py
@@ -0,0 +1,7 @@
+def _jupyter_nbextension_paths():
+    return [{
+        "section": "notebook",
+        "dest": "nbsolutions",
+        "src": "static",
+        "require": "nbsolutions/main"
+    }]
