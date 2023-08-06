3029692f04bb9c390acf27fbb4904f526efa2ac6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:40 +0000

    Add package SimpleSQLite/0.12.1/SimpleSQLite-0.12.1.tar.gz

---
+++ packages/SimpleSQLite/SimpleSQLite-0.12.1.tar.gz/SimpleSQLite-0.12.1/simplesqlite/__init__.py
@@ -0,0 +1,30 @@
+# encoding: utf-8
+
+"""
+.. codeauthor:: Tsuyoshi Hombashi <tsuyoshi.hombashi@gmail.com>
+"""
+
+from __future__ import absolute_import
+
+from ._error import (
+    DatabaseError,
+    NullDatabaseConnectionError,
+    TableNotFoundError,
+    AttributeNotFoundError,
+    InvalidTableNameError,
+    InvalidAttributeNameError,
+    SqlSyntaxError,
+    OperationalError,
+)
+from ._func import (
+    validate_table_name,
+    validate_attr_name,
+    append_table,
+    copy_table,
+    connect_sqlite_db_mem,
+)
+from ._logger import (
+    set_logger,
+    set_log_level,
+)
+from .core import SimpleSQLite
