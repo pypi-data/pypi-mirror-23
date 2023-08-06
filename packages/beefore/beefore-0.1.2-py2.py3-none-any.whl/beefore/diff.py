8c1188fe9ee263f0681a497ade9d5452351a80ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:34 +0000

    Add package beefore/0.1.2/beefore-0.1.2-py2.py3-none-any.whl

---
+++ packages/beefore/beefore-0.1.2-py2.py3-none-any.whl/beefore/diff.py
@@ -0,0 +1,39 @@
+import re
+
+FILE_START = re.compile("diff --git a/(.*) b/(.*)")
+LINE_RANGE = re.compile("@@ -(\d+),(\d+) \+(\d+),(\d+) @@")
+
+
+def positions(diff, filename):
+    """Given a diff payload, look for all the line ranges described for a given file.
+
+    Returns a map of source file lines, mapped to the position in the diff
+    that the line is referenced.
+    """
+    current_file = None
+    start = None
+    offset = None
+    for i, d in enumerate(diff):
+        match = FILE_START.match(d)
+        if match:
+            if match.group(1) == filename:
+                current_file = {}
+            else:
+                if current_file is not None:
+                    return current_file
+        elif current_file is not None:
+            match = LINE_RANGE.match(d)
+            if match:
+                # Found the start of a new range in the file
+                first_line = int(match.group(3))
+                start = i
+                offset = 1 if match.group(1) == '0' else 0
+            elif start and len(d) > 0:
+                if d[0] == '-':
+                    offset += 1
+                else:
+                    line = first_line + i - start - offset
+                    position = i - start
+                    current_file[line] = position
+
+    return current_file
