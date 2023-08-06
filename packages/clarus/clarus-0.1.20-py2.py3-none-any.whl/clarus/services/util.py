e02b95faf1bf6f2760acafd4c787fedaa9d5eb35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:13:29 +0000

    Add package clarus/0.1.20/clarus-0.1.20-py2.py3-none-any.whl

---
+++ packages/clarus/clarus-0.1.20-py2.py3-none-any.whl/clarus/services/util.py
@@ -0,0 +1,17 @@
+import clarus.services
+
+def domain(output=None, **params):
+    return clarus.services.api_request('Util', 'Domain', output=output, **params)
+
+def grid(output=None, **params):
+    return clarus.services.api_request('Util', 'Grid', output=output, **params)
+
+def periodlength(output=None, **params):
+    return clarus.services.api_request('Util', 'PeriodLength', output=output, **params)
+
+def tickers(output=None, **params):
+    return clarus.services.api_request('Util', 'Tickers', output=output, **params)
+
+def vbafunctions(output=None, **params):
+    return clarus.services.api_request('Util', 'VbaFunctions', output=output, **params)
+
