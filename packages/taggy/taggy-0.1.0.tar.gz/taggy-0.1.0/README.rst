d102f4141feed4d09cc93d5cfe9dc02cc6a05d5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:56:45 +0000

    Add package taggy/0.1.0/taggy-0.1.0.tar.gz

---
+++ packages/taggy/taggy-0.1.0.tar.gz/taggy-0.1.0/README.rst
@@ -0,0 +1,18 @@
+======
+Tagger
+======
+
+Command line utility to help create SemVer tags.
+
+----
+
+TODO
+----
+- Tests
+- Continuous Integration
+- Install instructions
+- Documentation
+- Demo screencast
+- File globs for find and replace
+- Find and replace sed preview
+- Handle prefixes better
---
+++ packages/taggy/taggy-0.1.0.tar.gz/taggy-0.1.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(
+    name='taggy',
+    description='Command line utility to help create SemVer tags.',
+    version='0.1.0',
+    py_modules=['taggy'],
+    author_email='jack@evans.gb.net',
+    license='MIT',
+    url='https://github.com/Jackevansevo/tagger',
+    install_requires=['semver'],
+    entry_points={'console_scripts': ['taggy=taggy:main']},
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
+    keywords='semver git tag',
+    python_requires='>=3.5',
+)
