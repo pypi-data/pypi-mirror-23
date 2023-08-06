1e01405d7a02ab0da7163212305cd14349b8ae02
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:54:30 +0000

    Add package instalooter/0.11.0/instaLooter-0.11.0.tar.gz

---
+++ packages/instalooter/instaLooter-0.11.0.tar.gz/instaLooter-0.11.0/instaLooter/__init__.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from __future__ import (
+    absolute_import,
+    unicode_literals,
+)
+
+
+__author__ = "althonos"
+__author_email__ = "martin.larralde@ens-cachan.fr"
+__version__ = "0.11.0"
+
+
+try:
+    from .cli import main
+    from .core import InstaLooter
+    from . import urlgen
+except ImportError:
+    pass
