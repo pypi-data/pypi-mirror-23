2602440486b7f69ee8831e464662fda30d914f5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:33:32 +0000

    Add package ipyvolume/0.4.0a1/ipyvolume-0.4.0a1.tar.gz

---
+++ packages/ipyvolume/ipyvolume-0.4.0a1.tar.gz/ipyvolume-0.4.0a1/ipyvolume/__init__.py
@@ -0,0 +1,16 @@
+from ._version import version_info, __version__
+
+from . import style
+from .volume import *
+from .transferfunction import *
+from .examples import  *
+from . import datasets
+from . import embed
+
+def _jupyter_nbextension_paths():
+    return [{
+        'section': 'notebook',
+        'src': 'static',
+        'dest': 'ipyvolume',
+        'require': 'ipyvolume/extension'
+    }]
