207437d88dd3e17cba8e501b78a21e302c86a3fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:38 +0000

    Add package PyFVCOM/1.6.1/PyFVCOM-1.6.1-py2.py3-none-any.whl

---
+++ packages/PyFVCOM/PyFVCOM-1.6.1-py2.py3-none-any.whl/PyFVCOM/__init__.py
@@ -0,0 +1,30 @@
+"""
+The FVCOM Python toolbox (PyFVCOM)
+
+"""
+
+__version__ = '1.6.1'
+__author__ = 'Pierre Cazenave'
+__credits__ = ['Pierre Cazenave']
+__license__ = 'MIT'
+__maintainer__ = 'Pierre Cazenave'
+__email__ = 'pica@pml.ac.uk'
+
+import inspect
+from warnings import warn
+
+# Import everything!
+from PyFVCOM import buoy_tools
+from PyFVCOM import cst_tools
+from PyFVCOM import ctd_tools
+from PyFVCOM import current_tools
+from PyFVCOM import grid_tools
+from PyFVCOM import ll2utm
+from PyFVCOM import ocean_tools
+from PyFVCOM import stats_tools
+from PyFVCOM import tide_tools
+from PyFVCOM import tidal_ellipse
+from PyFVCOM import process_results
+from PyFVCOM import read_results
+from PyFVCOM import plot
+
