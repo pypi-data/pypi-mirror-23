a78fa86a299077c8881619f421ab6572cfcdf427
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:09:37 +0000

    Add package sevenbridges-python/0.8.0/sevenbridges-python-0.8.0.tar.gz

---
+++ packages/sevenbridges-python/sevenbridges-python-0.8.0.tar.gz/sevenbridges-python-0.8.0/sevenbridges/models/member.py
@@ -0,0 +1,43 @@
+import logging
+import six
+
+from sevenbridges.decorators import inplace_reload
+from sevenbridges.errors import ResourceNotModified
+from sevenbridges.meta.fields import HrefField, StringField, CompoundField
+from sevenbridges.meta.resource import Resource
+from sevenbridges.models.compound.projects.permissions import Permissions
+
+logger = logging.getLogger(__name__)
+
+
+class Member(Resource):
+    """
+    Central resource for managing project members.
+    """
+    _URL = {
+        'permissions': '/permissions'
+    }
+
+    href = HrefField()
+    username = StringField(read_only=False)
+    email = StringField(read_only=False)
+    permissions = CompoundField(Permissions, read_only=False)
+
+    def __str__(self):
+        return six.text_type('<Member: username={username}>'
+                             .format(username=self.username))
+
+    @inplace_reload
+    def save(self, inplace=True):
+        """
+        Saves modification to the api server.
+        """
+        data = self._modified_data()
+        data = data['permissions']
+        if bool(data):
+            url = six.text_type(self.href) + self._URL['permissions']
+            extra = {'resource': self.__class__.__name__, 'query': data}
+            logger.info('Modifying permissions', extra=extra)
+            self._api.patch(url=url, data=data, append_base=False)
+        else:
+            raise ResourceNotModified()
