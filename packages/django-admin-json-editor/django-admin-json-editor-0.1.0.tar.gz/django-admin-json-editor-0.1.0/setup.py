53a8c7269728442f59102274203b47f4809b80f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:10:00 +0000

    Add package django-admin-json-editor/0.1.0/django-admin-json-editor-0.1.0.tar.gz

---
+++ packages/django-admin-json-editor/django-admin-json-editor-0.1.0.tar.gz/django-admin-json-editor-0.1.0/setup.py
@@ -0,0 +1,36 @@
+import os
+from setuptools import find_packages, setup
+
+with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
+    README = readme.read()
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='django-admin-json-editor',
+    version='0.1.0',
+    packages=['django_admin_json_editor'],
+    include_package_data=True,
+    license='MIT License',
+    description='A simple Django app to add JSON widget into Django Administration.',
+    long_description=README,
+    url='https://github.com/abogushov/django-admin-json-editor',
+    author='Alexander Bogushov',
+    author_email='abogushov@gmail.com',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+    install_requires=['Django'],
+)
