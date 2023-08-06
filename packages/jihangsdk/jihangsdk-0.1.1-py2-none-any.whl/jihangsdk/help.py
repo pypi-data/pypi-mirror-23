63b7c175a2b747c65220f069e1bc13d40871870c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:07:59 +0000

    Add package jihangsdk/0.1.1/jihangsdk-0.1.1-py2-none-any.whl

---
+++ packages/jihangsdk/jihangsdk-0.1.1-py2-none-any.whl/jihangsdk/help.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
+#############################################
+# File Name: help.py
+# Author: bangguo
+# Mail: willyxq@gmail.com
+# Created Time:  2017-07-07 10:25:50 PM
+#############################################
+
+
+def sum(*values):
+    s = 0
+    for v in values:
+        i = int(v)
+        s = s + i        
+    print s
+
+
+def output():
+    print 'hello jihang'
