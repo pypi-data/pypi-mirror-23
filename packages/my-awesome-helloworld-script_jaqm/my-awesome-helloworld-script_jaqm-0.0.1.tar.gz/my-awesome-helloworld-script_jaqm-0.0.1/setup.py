5e002dea30117c7700b8c9e45bb6201db2b5a76b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:17:48 +0000

    Add package my-awesome-helloworld-script_jaqm/0.0.1/my-awesome-helloworld-script_jaqm-0.0.1.tar.gz

---
+++ packages/my-awesome-helloworld-script_jaqm/my-awesome-helloworld-script_jaqm-0.0.1.tar.gz/my-awesome-helloworld-script_jaqm-0.0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(
+    name='my-awesome-helloworld-script_jaqm',    # This is the name of your PyPI-package.
+    version='0.0.1',                          # Update the version number for new releases
+    scripts=['helloworld']
+        # The name of your script, and also the command you'll be using for calling it
+)
