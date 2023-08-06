aa277189b12b905bb7ad383e4e34be9e70c98543
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:09:16 +0000

    Add package ncmirtools/0.4.0/ncmirtools-0.4.0.tar.gz

---
+++ packages/ncmirtools/ncmirtools-0.4.0.tar.gz/ncmirtools-0.4.0/setup.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read()
+
+requirements = [
+    "argparse",
+    "configparser",
+    "pg8000",
+    "ftpretty",
+    "paramiko"
+]
+
+test_requirements = [
+    "argparse",
+    "pg8000",
+    "configparser",
+    "ftpretty",
+    "paramiko",
+    "mock"
+]
+
+setup(
+    name='ncmirtools',
+    version='0.4.0',
+    description="Set of commandline tools for NCMIR",
+    long_description=readme + '\n\n' + history,
+    author="Christopher Churas",
+    author_email='churas.camera@gmail.com',
+    url='https://github.com/CRBS/ncmirtools',
+    packages=[
+        'ncmirtools', 'ncmirtools.kiosk',
+    ],
+    package_dir={'ncmirtools':
+                 'ncmirtools'},
+    include_package_data=True,
+    install_requires=requirements,
+    zip_safe=False,
+    keywords='ncmirtools',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+    scripts = [ 'ncmirtools/mpidir.py',
+                'ncmirtools/projectsearch.py',
+                'ncmirtools/projectdir.py',
+                'ncmirtools/mpidinfo.py',
+                'ncmirtools/imagetokiosk.py'
+              ],
+    test_suite='tests',
+    tests_require=test_requirements
+)
