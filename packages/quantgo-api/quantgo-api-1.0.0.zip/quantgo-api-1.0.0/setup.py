b17cd0ba4c007d94ecc3b3ab1560d5ee41123014
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:08:44 +0000

    Add package quantgo-api/1.0.0/quantgo-api-1.0.0.zip

---
+++ packages/quantgo-api/quantgo-api-1.0.0.zip/quantgo-api-1.0.0/setup.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+"""
+distutils/setuptools install script.
+"""
+
+import quantgo_api
+
+try:
+    from setuptools import setup
+    setup
+except ImportError:
+    from distutils.core import setup
+
+
+packages = ['quantgo_api', ]
+
+requires = ['argparse>=1.1',
+            'future',
+            ]
+
+
+setup(
+    name='quantgo-api',
+    version=quantgo_api.__version__,
+    description='QuantGo Python API and Command Line Tool.',
+    long_description=open('README.md').read(),
+    author='Yuri Poliukhovich',
+    author_email='yuri@quantgo.com',
+    url='https://quantgo.com',
+    packages=packages,
+    package_dir={'quantgo_api': 'quantgo_api'},
+    install_requires=requires,
+    license=open("LICENSE.txt").read(),
+    classifiers=(
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Operating System :: POSIX',
+        'Programming Language :: Python',
+        'Intended Audience :: End Users/Desktop',
+    ),
+    keywords=['quantgo', 'quantgo-api']
+)
