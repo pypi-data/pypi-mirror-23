3615658ff3b6613535eed917b6506b00e00212bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:32:32 +0000

    Add package infi.diskmanagement/0.3.11/infi.diskmanagement-0.3.11.tar.gz

---
+++ packages/infi.diskmanagement/infi.diskmanagement-0.3.11.tar.gz/infi.diskmanagement-0.3.11/src/infi/diskmanagement/__version__.py
@@ -0,0 +1,10 @@
+__version__ = "0.3.11"
+__git_commiter_name__ = "Arnon Yaari"
+__git_commiter_email__ = "arnony@infinidat.com"
+__git_branch__ = u'(HEAD detached at v0.3.11)'
+__git_remote_tracking_branch__ = '(No remote tracking)'
+__git_remote_url__ = '(Not remote tracking)'
+__git_head_hash__ = 'f21a3c6640dce7a62c1b14e11f975fc5ef0fcdcd'
+__git_head_subject__ = u'Finished Release v0.3.11'
+__git_head_message__ = u''
+__git_dirty_diff__ = u''
