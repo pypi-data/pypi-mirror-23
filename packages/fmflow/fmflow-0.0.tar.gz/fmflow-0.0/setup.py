184bc2fa83b6f35b89bf81bee79bbd8a5e5a5748
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:18:31 +0000

    Add package fmflow/0.0/fmflow-0.0.tar.gz

---
+++ packages/fmflow/fmflow-0.0.tar.gz/fmflow-0.0/setup.py
@@ -0,0 +1,13 @@
+# coding: utf-8
+
+# standard library
+from setuptools import setup, find_packages
+
+
+# main
+setup(
+    name = 'fmflow',
+    version = '0.0',
+    url = 'https://github.com/fmlo-dev/fmflow',
+    packages = find_packages(),
+)
