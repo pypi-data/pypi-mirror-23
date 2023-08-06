c2e0f4c9120eccd937490bed7de6a12358ff6e5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:57:16 +0000

    Add package wjtestPrint/0.0.0/wjtestPrint-0.0.0.tar.gz

---
+++ packages/wjtestPrint/wjtestPrint-0.0.0.tar.gz/wjtestPrint-0.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name = "wjtestPrint",
+    verson = "1.1.0",
+    py_modules = ["WJTestPrint"],
+    author = "wenjie",
+    author_email = "826834678@qq.com",
+    url = "http://www.baidu.com",
+    description = "test",
+    )
