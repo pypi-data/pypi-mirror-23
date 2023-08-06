6936eb7e21fd55767e6b23642608d4ecb2dbe696
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:15:35 +0000

    Add package workflow_manager/0.3/workflow_manager-0.3.tar.gz

---
+++ packages/workflow_manager/workflow_manager-0.3.tar.gz/workflow_manager-0.3/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+setup(name='workflow_manager',
+      version='0.3',
+      description='Wizard-link workflow manager',
+      long_description=readme(),
+      url='http://github.com/dimtruck/workflow-manager-py',
+      classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3 :: Only',
+        'Topic :: Software Development :: Libraries',
+      ],
+      author='Dimitry Ushakov',
+      author_email='me@dimitryushakov.com',
+      license='MIT',
+      packages=['workflow_manager'],
+      setup_requires=['pytest-runner', 'pytest'],
+      tests_require=['pytest', 'coverage','pytest-cov'],
+      zip_safe=False)
