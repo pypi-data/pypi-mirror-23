bfe616d5b114b7d457d6d3a4934961a93d2bfc7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:56:31 +0000

    Add package chatterbot-corpus/1.0.0/chatterbot_corpus-1.0.0-py2.py3-none-any.whl

---
+++ packages/chatterbot-corpus/chatterbot_corpus-1.0.0-py2.py3-none-any.whl/chatterbot_corpus/corpus.py
@@ -0,0 +1,93 @@
+import os
+
+
+class CorpusObject(list):
+
+    def __init__(self, *args, **kwargs):
+        if args:
+            super(CorpusObject, self).__init__(args[0])
+        else:
+            super(CorpusObject, self).__init__()
+
+        self.categories = []
+
+
+class Corpus(object):
+
+    def __init__(self):
+        current_directory = os.path.dirname(os.path.abspath(__file__))
+        self.data_directory = os.path.join(current_directory, 'data')
+
+    def get_file_path(self, dotted_path, extension='json'):
+        """
+        Reads a dotted file path and returns the file path.
+        """
+
+        # If the operating system's file path seperator character is in the string
+        if os.sep in dotted_path or '/' in dotted_path:
+            # Assume the path is a valid file path
+            return dotted_path
+
+        parts = dotted_path.split('.')
+        if parts[0] == 'chatterbot':
+            parts.pop(0)
+            parts[0] = self.data_directory
+
+        corpus_path = os.path.join(*parts)
+
+        if os.path.exists(corpus_path + '.{}'.format(extension)):
+            corpus_path += '.{}'.format(extension)
+
+        return corpus_path
+
+    def read_corpus(self, file_name):
+        """
+        Read and return the data from a corpus json file.
+        """
+        import io
+        from ruamel.yaml import YAML
+
+        yaml = YAML(typ='safe')
+
+        with io.open(file_name, encoding='utf-8') as data_file:
+            data = yaml.load(data_file)
+        return data
+
+    def list_corpus_files(self, dotted_path):
+        """
+        Return a list of file paths to each data file in
+        the specified corpus.
+        """
+        CORPUS_EXTENSION = 'yml'
+
+        corpus_path = self.get_file_path(dotted_path, extension=CORPUS_EXTENSION)
+        paths = []
+
+        if os.path.isdir(corpus_path):
+            for dirname, dirnames, filenames in os.walk(corpus_path):
+                for datafile in filenames:
+                    if datafile.endswith(CORPUS_EXTENSION):
+                        paths.append(os.path.join(dirname, datafile))
+        else:
+            paths.append(corpus_path)
+
+        paths.sort()
+        return paths
+
+    def load_corpus(self, dotted_path):
+        """
+        Return the data contained within a specified corpus.
+        """
+        data_file_paths = self.list_corpus_files(dotted_path)
+
+        corpora = CorpusObject()
+
+        for file_path in data_file_paths:
+            corpus = self.read_corpus(file_path)
+
+            corpora.categories = corpus.get('categories', [])
+            conversations = corpus.get('conversations', [])
+
+            corpora.extend([conversations])
+
+        return corpora
