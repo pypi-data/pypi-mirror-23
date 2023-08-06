866fe6cf5c2131c7b26c243fcc574b18d4e0f145
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:13:58 +0000

    Add package nbgrader/0.5.1/nbgrader-0.5.1.tar.gz

---
+++ packages/nbgrader/nbgrader-0.5.1.tar.gz/nbgrader-0.5.1/nbgrader/tests/apps/test_nbgrader_extension.py
@@ -0,0 +1,25 @@
+import os
+
+import nbgrader
+
+
+def test_nbextension():
+    from nbgrader import _jupyter_nbextension_paths
+    nbexts = _jupyter_nbextension_paths()
+    assert len(nbexts) == 4
+    assert nbexts[0]['section'] == 'tree'
+    assert nbexts[1]['section'] == 'notebook'
+    assert nbexts[2]['section'] == 'tree'
+    assert nbexts[3]['section'] == 'notebook'
+    paths = [ext['src'] for ext in nbexts]
+    for path in paths:
+        assert os.path.isdir(os.path.join(os.path.dirname(nbgrader.__file__), path))
+
+
+def test_serverextension():
+    from nbgrader import _jupyter_server_extension_paths
+    serverexts = _jupyter_server_extension_paths()
+    assert len(serverexts) == 3
+    assert serverexts[0]['module'] == 'nbgrader.server_extensions.assignment_list'
+    assert serverexts[1]['module'] == 'nbgrader.server_extensions.formgrader'
+    assert serverexts[2]['module'] == 'nbgrader.server_extensions.validate_assignment'
