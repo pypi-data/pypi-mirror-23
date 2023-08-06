73acdc0c9b2d138d853d4147dac1b873fd211e03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:45:58 +0000

    Add package analytracks/0.1.1.5/analytracks-0.1.1.5.tar.gz

---
+++ packages/analytracks/analytracks-0.1.1.5.tar.gz/analytracks-0.1.1.5/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(name='analytracks',
+      version='0.1.1.5',
+      description='The funniest joke in the world',
+      url='http://github.com/ddolbecke/analytracks',
+      author='Dimitri de Smet',
+      author_email='dimitri.desmet@uclouvain.be',
+      license='MIT',
+      packages=['analytracks','analytracks.tracks'],
+      package_dir={
+                    'analytracks.tracks':'analytracks/tracks',
+                  },
+      package_data={
+                    'analytracks.tracks': ['data/*.dat','data/*.key'],
+                    'analytracks': ['data/*.dat']
+                     },
+      install_requires=[
+                  'numpy', 'pandas', 'matplotlib', 'seaborn', 
+                  'lxml', 'simplejson', 'haversine', 
+                  'statsmodels', 'scipy', 'scikit-learn', 
+                  'SRTM.py', 'requests', 'mplleaflet'
+              ],
+      zip_safe=True)
