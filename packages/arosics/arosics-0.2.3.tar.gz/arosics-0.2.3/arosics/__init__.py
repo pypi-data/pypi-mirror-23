ed348b11ce022f7fef170a5667a89d92a33d2534
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:35:21 +0000

    Add package arosics/0.2.3/arosics-0.2.3.tar.gz

---
+++ packages/arosics/arosics-0.2.3.tar.gz/arosics-0.2.3/arosics/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for arosics."""
+
+__author__ = """Daniel Scheffler"""
+__email__ = 'daniel.scheffler@gfz-potsdam.de'
+__version__ = '0.2.3'
+__versionalias__ = '2017-07-03_03'
+
+
+from .CoReg import COREG
+from .CoReg_local import COREG_LOCAL
+from .DeShifter import DESHIFTER
+from .Tie_Point_Grid import Tie_Point_Grid
