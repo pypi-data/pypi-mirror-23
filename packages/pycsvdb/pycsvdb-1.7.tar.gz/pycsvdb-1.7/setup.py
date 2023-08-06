ab6aa033b85c86d0e85cb0592854f51e389bf80c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:18:56 +0000

    Add package pycsvdb/1.7/pycsvdb-1.7.tar.gz

---
+++ packages/pycsvdb/pycsvdb-1.7.tar.gz/pycsvdb-1.7/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='pycsvdb',
+      version='1.7',
+      description='Reading CSV file and insert into database',
+      url='https://github.com/sajeeshe/python-csv-easy',
+      author='Sajeesh E Namboothiri',
+      author_email='sajeeshe@gmail.com',
+      license='MIT',
+      packages=['pycsvdb'],
+      keywords='CSV database pandas',
+      zip_safe=False)
