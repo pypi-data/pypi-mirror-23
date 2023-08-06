7e389aa5ad7b5bbe0f7b8c2007d3a9d605f3615f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:07:31 +0000

    Add package envars/0.1.6/envars-0.1.6.tar.gz

---
+++ packages/envars/envars-0.1.6.tar.gz/envars-0.1.6/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='envars',
+      version='0.1.6',
+      description='Read environmental variables',
+      url='https://bitbucket.org/loisosubbelohde/envvars',
+      author='Santosh Philip    ',
+      author_email='santosh_philip@yahoo.com',
+      license='MIT',
+      packages=['envars'],
+      zip_safe=False)
