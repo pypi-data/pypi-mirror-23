0a1709259f8641ab02f7c01df4a5420ea4c6f8bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:39 +0000

    Add package nxp-imu/0.5.0/nxp_imu-0.5.0.tar.gz

---
+++ packages/nxp-imu/nxp_imu-0.5.0.tar.gz/nxp_imu-0.5.0/nxp_imu/__init__.py
@@ -0,0 +1,15 @@
+# from .FXAS21002 import FXAS21002
+# from .FXOS8700 import FXOS8700
+from .I2C import I2C
+from .IMU import IMU
+from .version import __version__
+
+
+# class Namespace(object):
+# 	def __init__(self, **kwds):
+# 		self.__dict__.update(kwds)
+
+
+__author__ = 'Kevin J. Walchko'
+__license__ = 'MIT'
+__copyright__ = '2017 Kevin J. Walchko'
