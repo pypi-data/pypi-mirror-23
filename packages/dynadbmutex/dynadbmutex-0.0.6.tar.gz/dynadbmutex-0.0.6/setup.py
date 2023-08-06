d235d13287fbb05b05a20ecd8d94fba94356af04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:34:19 +0000

    Add package dynadbmutex/0.0.6/dynadbmutex-0.0.6.tar.gz

---
+++ packages/dynadbmutex/dynadbmutex-0.0.6.tar.gz/dynadbmutex-0.0.6/setup.py
@@ -0,0 +1,58 @@
+from setuptools import setup
+import sys
+import os
+
+version = None
+
+if sys.argv[-2] == '--version' and 'sdist' in sys.argv:
+    version = sys.argv[-1]
+    sys.argv.pop()
+    sys.argv.pop()
+
+if 'sdist' in sys.argv and not version:
+    raise Exception("Please set a version with --version x.y.z")
+
+if not version:
+    if 'sdist' in sys.argv:
+        raise Exception("Please set a version with --version x.y.z")
+    else:
+        path_pkg_info = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'PKG-INFO')
+        if os.path.isfile(path_pkg_info):
+            with open(path_pkg_info, 'r')as f:
+                for l in f.readlines():
+                    if 'Version' in l:
+                        _, version = l.split(' ')
+        else:
+            print("WARNING: cannot set version in custom setup.py")
+
+print("version: %s" % version)
+
+setup(
+    name='dynadbmutex',
+    version=version,
+    url='https://github.com/erwan-lemonnier/dynamodbmutex',
+    license='BSD',
+    author='Erwan Lemonnier',
+    author_email='erwan@lemonnier.se',
+    description='A simple mutex implementation on top of DynamoDB.',
+    install_requires=[
+        'boto',
+        'dynadbobjectstore>=1.0.0',
+    ],
+    tests_require=[
+        'boto',
+        'dynadbobjectstore',
+    ],
+    test_suite='nose.collector',
+    py_modules=['dynadbmutex'],
+    platforms='any',
+    classifiers=[
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Topic :: Software Development :: Libraries :: Python Modules'
+    ]
+)
