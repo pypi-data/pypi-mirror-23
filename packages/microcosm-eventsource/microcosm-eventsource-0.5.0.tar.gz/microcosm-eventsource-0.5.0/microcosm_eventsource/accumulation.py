eba4a59a79e26483bacc30177ddd98dac2c452c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:10:07 +0000

    Add package microcosm-eventsource/0.5.0/microcosm-eventsource-0.5.0.tar.gz

---
+++ packages/microcosm-eventsource/microcosm-eventsource-0.5.0.tar.gz/microcosm-eventsource-0.5.0/microcosm_eventsource/accumulation.py
@@ -0,0 +1,44 @@
+"""
+Accumulation functions.
+
+"""
+
+
+def current():
+    """
+    Return the current event type.
+
+    """
+    return lambda state, event_type: [event_type]
+
+
+def keep():
+    """
+    Keep the current state.
+
+    """
+    return lambda state, event_type: state
+
+
+def alias(other_event_type):
+    """
+    Return another event type.
+
+    """
+    return lambda state, event_type: [other_event_type]
+
+
+def difference(other_event_type):
+    """
+    Remove one event type
+
+    """
+    return lambda state, event_type: sorted(state - {other_event_type, })
+
+
+def union():
+    """
+    Return the aggregated event type.
+
+    """
+    return lambda state, event_type: sorted(state | {event_type, })
