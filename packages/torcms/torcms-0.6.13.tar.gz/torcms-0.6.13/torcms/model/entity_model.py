c587513c8a27fe7e6f43dfb1d5b26e1537a69ec4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:48:33 +0000

    Add package torcms/0.6.13/torcms-0.6.13.tar.gz

---
+++ packages/torcms/torcms-0.6.13.tar.gz/torcms-0.6.13/torcms/model/entity_model.py
@@ -0,0 +1,61 @@
+# -*- coding:utf-8 -*-
+
+'''
+For file entities. Just like pdf, zipfile, docx, etc.
+'''
+
+import time
+from torcms.model.core_tab import TabEntity
+from torcms.model.abc_model import Mabc, MHelper
+from torcms.core.tools import logger
+
+
+class MEntity(Mabc):
+    '''
+    For file entities. Just like pdf, zipfile, docx, etc.
+    '''
+
+    @staticmethod
+    def query_all(limit=20):
+        return TabEntity.select().limit(limit)
+
+    @staticmethod
+    def get_id_by_impath(path):
+        logger.info('Get Entiry, Path: {0}'.format(path))
+
+        entity_list = TabEntity.select().where(TabEntity.path == path)
+        if entity_list.count() == 1:
+            return entity_list.get().uid
+        elif entity_list.count() > 1:
+            for rec in entity_list:
+                MEntity.delete(rec.uid)
+            return False
+        else:
+            return False
+
+    @staticmethod
+    def create_entity(signature, impath, kind='1'):
+        '''
+        create entity record in the database.
+        :param signature:
+        :param impath:
+        :param kind:
+        :return:
+        '''
+        if len(impath) == 0:
+            return False
+
+        try:
+            TabEntity.create(
+                uid=signature,
+                path=impath,
+                time_create=time.time(),
+                kind=kind
+            )
+            return True
+        except:
+            return False
+
+    @staticmethod
+    def delete(uid):
+        return MHelper.delete(TabEntity, uid)
---
+++ packages/torcms/torcms-0.6.13.tar.gz/torcms-0.6.13/torcms/script/script_migrate.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+
+from playhouse import migrate
+import config
+
+
+def run_migrate(*args):
+    '''
+    running some migration.
+    :return:
+    '''
+
+    print('Begin migrate ...')
+
+    torcms_migrator = migrate.PostgresqlMigrator(config.DB_CON)
+
+
+    # try:
+    #     migrate.migrate(
+    #         torcms_migrator.rename_table('e_layout', 'mablayout')
+    #     )
+    # except:
+    #     pass
+
+    memo_field = migrate.TextField(null=False, default='', help_text='Memo', )
+    try:
+        migrate.migrate(torcms_migrator.add_column('tabpost', 'memo', memo_field))
+    except:
+        pass
+
+    # try:
+    #     migrate.migrate(torcms_migrator.drop_column('tabtag', 'role_mask'))
+    # except:
+    #     pass
+
+    print('QED')
