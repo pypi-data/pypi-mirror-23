3f2ea5fe337af1ffd41115194cf569816e3cd3d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:10:06 +0000

    Add package zhihu/0.2.1/zhihu-0.2.1.tar.gz

---
+++ packages/zhihu/zhihu-0.2.1.tar.gz/zhihu-0.2.1/setup.py
@@ -0,0 +1,36 @@
+# -*- encoding: UTF-8 -*-
+from setuptools import find_packages
+from setuptools import setup
+
+VERSION = '0.2.1'
+
+with open("README.md") as f:
+    readme = f.read()
+
+install_requires = open("requirement.txt").readlines()
+
+setup(
+    name="zhihu",  # pip 安装时用的名字
+    version=VERSION,  # 当前版本，每次更新上传到pypi都需要修改
+    author="liuzhijun",
+    author_email="lzjun567@gmail.com",
+    url="https://github.com/lzjun567/zhihu-api",
+    packages=find_packages(),
+    keyworads="zhihu",
+    description="zhihu api from humans",
+    long_description=readme,
+    # packages=[
+    #     "zhihu",
+    #     "zhihu.models"
+    # ],
+    include_package_data=True,
+    license='MIT License',
+    classifiers=[],
+    install_requires=install_requires,
+    setup_requires=[
+        'pytest-runner',
+    ],
+    tests_require=[
+        "pytest",
+    ]
+)
