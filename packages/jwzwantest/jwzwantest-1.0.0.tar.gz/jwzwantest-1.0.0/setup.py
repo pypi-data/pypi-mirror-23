7a130f692be95bedb7573f0a310df91d73263363
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:13:36 +0000

    Add package jwzwantest/1.0.0/jwzwantest-1.0.0.tar.gz

---
+++ packages/jwzwantest/jwzwantest-1.0.0.tar.gz/jwzwantest-1.0.0/setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+
+setup(
+        name       ='jwzwantest',
+        version    ='1.0.0',
+        py_modules =['jwzwantest'],
+        author     ='jwzwan',
+        author_email='545502325@qq.com',
+        url        ='http://www.headfirstlabs.com',
+        description='1'
+
+
+
+
+
+
+
+    )
