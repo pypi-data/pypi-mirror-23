22ff6d14e03a856e15f7960355728f1cf2a2ff94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:56:30 +0000

    Add package pyTvocal/0.0.1/pyTvocal-0.0.1.tar.gz

---
+++ packages/pyTvocal/pyTvocal-0.0.1.tar.gz/pyTvocal-0.0.1/pyTvocal/__init__.py
@@ -0,0 +1,1 @@
+from .countDays import countTwoDates
---
+++ packages/pyTvocal/pyTvocal-0.0.1.tar.gz/pyTvocal-0.0.1/setup.py
@@ -0,0 +1,42 @@
+from distutils.core import setup
+
+NAME = 'pyTvocal'
+_MAJOR = 0
+_MINOR = 0
+_MICRO = 1
+VERSION = '%d.%d.%d' % (_MAJOR, _MINOR, _MICRO)
+DESCRIPTION = "A python tutorial for vocal detection @ZHANG Xu-long"
+
+
+def long_description():
+    readme = open('README.md', 'r').read()
+    changelog = open('CHANGELOG.md', 'r').read()
+    return changelog + '\n\n' + readme
+
+
+setup(
+    packages=['pyTvocal'],
+    name=NAME,
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description=long_description(),
+    author="ZHANG Xu-long",
+    author_email="fudan0027zxl@gmail.com",
+    license="BSD",
+    url="http://zhangxulong.site",
+    keywords='vocal',
+    classifiers=[
+        "Programming Language :: Python :: 2.7",
+        "Topic :: Multimedia :: Sound/Audio :: Analysis",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+
+    ],
+    install_requires=[ 'numpy',
+                       'h5py',
+                       'pydub',
+                       'scikit-learn',
+                       'Keras==1.2.2',
+                      ],
+)
