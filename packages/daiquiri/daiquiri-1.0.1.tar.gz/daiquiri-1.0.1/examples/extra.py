1f7025666d7f0d79e638b6a55c7a16d20b1af317
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:34:31 +0000

    Add package daiquiri/1.0.1/daiquiri-1.0.1.tar.gz

---
+++ packages/daiquiri/daiquiri-1.0.1.tar.gz/daiquiri-1.0.1/examples/extra.py
@@ -0,0 +1,12 @@
+import daiquiri
+import daiquiri.formatter
+
+daiquiri.setup(outputs=(
+    daiquiri.output.Stream(formatter=daiquiri.formatter.ColorFormatter(
+        fmt=(daiquiri.formatter.DEFAULT_FORMAT +
+             " [%(subsystem)s is %(mood)s]"))),
+    ))
+
+logger = daiquiri.getLogger(__name__, subsystem="example")
+logger.info("It works and log to stderr by default with color!",
+            mood="happy")
