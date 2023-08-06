443f5a708c30405da189d36d8ef3e7f88b552dca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:33 +0000

    Add package awacs/0.7.0/awacs-0.7.0.tar.gz

---
+++ packages/awacs/awacs-0.7.0.tar.gz/awacs-0.7.0/tests/test_aws.py
@@ -0,0 +1,24 @@
+import unittest
+
+from awacs.aws import Principal
+
+
+class TestPrincipal(unittest.TestCase):
+    def test_wildcard_principal(self):
+        p = Principal("*")
+        self.assertEqual(p.data, "*")
+        with self.assertRaises(ValueError):
+            p = Principal("*", "FAIL")
+
+    def test_normal_principal(self):
+        p = Principal("AWS", "arn:aws:iam::AccountNumber-WithoutHyphens:root")
+        self.assertEqual(
+            p.data,
+            {'AWS': 'arn:aws:iam::AccountNumber-WithoutHyphens:root'}
+        )
+        with self.assertRaises(ValueError):
+            p = Principal("AWS")
+
+    def test_invalid_principal_string(self):
+        with self.assertRaises(ValueError):
+            Principal("aws", "arn:aws:iam::AccountNumber-WithoutHyphens:root")
