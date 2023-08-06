295930397e6f1802ebdd663b8a556232a59e7ae8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:09:23 +0000

    Add package terrascript/0.2.0/terrascript-0.2.0.tar.gz

---
+++ packages/terrascript/terrascript-0.2.0.tar.gz/terrascript-0.2.0/terrascript/google/d.py
@@ -0,0 +1,19 @@
+from terrascript import _data
+class google_compute_network(_data): pass
+compute_network = google_compute_network
+
+class google_compute_subnetwork(_data): pass
+compute_subnetwork = google_compute_subnetwork
+
+class google_compute_zones(_data): pass
+compute_zones = google_compute_zones
+
+class google_container_engine_versions(_data): pass
+container_engine_versions = google_container_engine_versions
+
+class google_iam_policy(_data): pass
+iam_policy = google_iam_policy
+
+class google_storage_object_signed_url(_data): pass
+storage_object_signed_url = google_storage_object_signed_url
+
