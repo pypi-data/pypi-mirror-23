37ea6edb3041129e8d77e8eff9dbbdffe630df72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:14:49 +0000

    Add package russound-rio/0.1/russound_rio-0.1.tar.gz

---
+++ packages/russound-rio/russound_rio-0.1.tar.gz/russound_rio-0.1/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+        name='russound_rio',
+        version='0.1',
+        packages=['russound_rio'],
+        license='MIT',
+        author='Martin Donlon',
+        url='https://github.com/wickerwaka/russound_rio',
+        long_description=open('README.rst').read(),
+)
