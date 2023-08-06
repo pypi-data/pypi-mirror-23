1ed07a45899affea9c251162422fa9461b60d150
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:10:25 +0000

    Add package graphene-django/2.0.dev2017072501/graphene-django-2.0.dev2017072501.tar.gz

---
+++ packages/graphene-django/graphene-django-2.0.dev2017072501.tar.gz/graphene-django-2.0.dev2017072501/setup.py
@@ -0,0 +1,69 @@
+from setuptools import find_packages, setup
+
+rest_framework_require = [
+    'djangorestframework>=3.6.3',
+]
+
+
+tests_require = [
+    'pytest>=2.7.2',
+    'pytest-cov',
+    'coveralls',
+    'mock',
+    'pytz',
+    'django-filter',
+    'pytest-django==2.9.1',
+] + rest_framework_require
+
+setup(
+    name='graphene-django',
+    version='2.0.dev2017072501',
+
+    description='Graphene Django integration',
+    long_description=open('README.rst').read(),
+
+    url='https://github.com/graphql-python/graphene-django',
+
+    author='Syrus Akbary',
+    author_email='me@syrusakbary.com',
+
+    license='MIT',
+
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: Implementation :: PyPy',
+    ],
+
+    keywords='api graphql protocol rest relay graphene',
+
+    packages=find_packages(exclude=['tests']),
+
+    install_requires=[
+        'six>=1.10.0',
+        'graphene>=2.0.dev',
+        'Django>=1.8.0',
+        'iso8601',
+        'singledispatch>=3.4.0.3',
+        'promise>=2.1.dev',
+    ],
+    setup_requires=[
+        'pytest-runner',
+    ],
+    tests_require=tests_require,
+    rest_framework_require=rest_framework_require,
+    extras_require={
+        'test': tests_require,
+        'rest_framework': rest_framework_require,
+    },
+    include_package_data=True,
+    zip_safe=False,
+    platforms='any',
+)
