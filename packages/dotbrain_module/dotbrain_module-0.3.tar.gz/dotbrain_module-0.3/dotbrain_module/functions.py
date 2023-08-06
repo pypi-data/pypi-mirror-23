49dfcb1a78e7848abed57e5186973be1b2b1c6aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:09:45 +0000

    Add package dotbrain_module/0.3/dotbrain_module-0.3.tar.gz

---
+++ packages/dotbrain_module/dotbrain_module-0.3.tar.gz/dotbrain_module-0.3/dotbrain_module/functions.py
@@ -0,0 +1,86 @@
+def sendAnswerd(api_key=None,api_key_secret=None,token=None,value=None,base=None,numeric=None,user=None,weigth_key=None, printInfo=True):
+    import requests
+
+    url = 'http://dev.dotbrain.co/api/save/answer/'
+
+    data = {
+        'api_key': api_key,
+        'api_key_secret': api_key_secret,
+        'token': token,
+        'value': value,
+        'base': base,
+        'numeric': numeric,
+        'user': user,
+        'weigth_key': weigth_key,
+    }
+
+    r = requests.post(url=url, params=data)
+    data = {
+        'validate': True,
+        'text': r.text,
+        'status_code': r.status_code
+    }
+    if r.status_code != 200:
+        data['validate'] = False
+        if printInfo:
+            print('Error: %s' % str(r.text))
+        return data
+    else:
+        if printInfo:
+            print('All ok')
+        return data
+
+def getServiceBasicInfo(api_key=None,api_key_secret=None,token=None, printInfo=False):
+    import requests
+
+    url = 'http://dev.dotbrain.co/api/get/service/info/basic/'
+
+    data = {
+        'api_key': api_key,
+        'api_key_secret': api_key_secret,
+        'token': token,
+    }
+
+    r = requests.get(url=url, params=data)
+    data = {
+        'validate': True,
+        'text': r.text,
+        'status_code': r.status_code
+    }
+    if r.status_code != 200:
+        data['validate'] = False
+        if printInfo:
+            print('Error: %s' % str(r.text))
+        return data
+    else:
+        if printInfo:
+            print('All ok')
+        return data
+
+def setQuestionKey(api_key=None,api_key_secret=None,token=None, key=None, weigth=None, printInfo=False):
+    import requests
+
+    url = 'http://dev.dotbrain.co/api/set/weigth/key/'
+
+    data = {
+        'api_key': api_key,
+        'api_key_secret': api_key_secret,
+        'token_service': token,
+        'key': key,
+        'weigth': weigth,
+    }
+
+    r = requests.post(url=url, params=data)
+    data = {
+        'validate': True,
+        'text': r.text,
+        'status_code': r.status_code
+    }
+    if r.status_code != 200:
+        if printInfo:
+            print('Error: %s' % str(r.text))
+        return data
+    else:
+        if printInfo:
+            print('All ok')
+        return data
---
+++ packages/dotbrain_module/dotbrain_module-0.3.tar.gz/dotbrain_module-0.3/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+setup(
+  name = 'dotbrain_module',
+  packages = ['dotbrain_module'],
+  version = '0.3',
+  description = 'V 0.3 dotBrain Module',
+  author = 'Luis David Ocampo Aguirre',
+  author_email = 'luisda199824@gmail.com',
+  url = 'https://github.com/luisda199824/dotbrain_module',
+  download_url = 'https://github.com/luisda199824/dotbrain_module/archive/0.3.tar.gz',
+  keywords = ['testing', 'dotbrain', 'private'],
+  classifiers = [],
+  py_modules = ['requests'],
+)
