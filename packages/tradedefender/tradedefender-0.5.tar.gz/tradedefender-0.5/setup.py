0c2c34f25ce7f9c8849a1bf47c46ed5057971fd2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:17:36 +0000

    Add package tradedefender/0.5/tradedefender-0.5.tar.gz

---
+++ packages/tradedefender/tradedefender-0.5.tar.gz/tradedefender-0.5/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+
+setup(name='tradedefender',
+      version='0.5',
+      description='Interface for Trade Defender financial data',
+      url='https://tradedefender.com',
+      author='CJ Black',
+      author_email='cj@tradedefender.com',
+      license='MIT',
+      packages=['tradedefender'],
+      install_requires=['requests'],
+      zip_safe=False)
