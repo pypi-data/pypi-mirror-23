261fbf2740a624316cc5805992d029b15e5e54c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:56:28 +0000

    Add package aipy/2.1.4/aipy-2.1.4.tar.gz

---
+++ packages/aipy/aipy-2.1.4.tar.gz/aipy-2.1.4/aipy_src/__gitlog__.py
@@ -0,0 +1,6 @@
+__gitlog__ = """322c36c
+Included README.md in MANIFEST.in, incremented version to 2.1.4 for pypi distribution.
+--
+Aaron Parsons
+aparsons@berkeley.edu
+2017-07-02 08:37:45 -0700"""
