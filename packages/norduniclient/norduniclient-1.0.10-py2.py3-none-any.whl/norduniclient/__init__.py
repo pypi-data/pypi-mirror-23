b5b3d8badbb09231e0a121a9a4f0866a078d2b73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:13:26 +0000

    Add package norduniclient/1.0.10/norduniclient-1.0.10-py2.py3-none-any.whl

---
+++ packages/norduniclient/norduniclient-1.0.10-py2.py3-none-any.whl/norduniclient/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import
+
+from norduniclient.core import *
+
+__author__ = 'lundberg'
+
+
+# Init as singleton for easy use in Django
+neo4jdb = init_db()
+
