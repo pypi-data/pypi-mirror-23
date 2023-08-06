e657022258a781e3a32d0e84d18181c8ca464c7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:32:46 +0000

    Add package FaceExtractor/0.1.4/FaceExtractor-0.1.4.tar.gz

---
+++ packages/FaceExtractor/FaceExtractor-0.1.4.tar.gz/FaceExtractor-0.1.4/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+
+setup(name='FaceExtractor',
+      version='0.1.4',
+      description='Extract faces from images with OpenCV',
+      url='https://github.com/ducthienbui97/FaceExtractor',
+      author='Thien Bui',
+      author_email='thienbui797@gmail.com',
+      license='BSD-3-Clause',
+      packages=['face_extractor'],
+      install_requires=['opencv-python'],
+      package_data={'face_extractor':['data/*.xml']},
+      long_description=readme(),
+)
