dc3cef5fc60af5b630fd5f2cb240d78d201af6b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:07:00 +0000

    Add package sve_common_tools/0.1.7.7/sve_common_tools-0.1.7.7.tar.gz

---
+++ packages/sve_common_tools/sve_common_tools-0.1.7.7.tar.gz/sve_common_tools-0.1.7.7/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name='sve_common_tools',
+      version='0.1.7.7',
+      author='Riccardo Russo',
+      author_email='riccardo.russo79@gmail.com',
+      description='Utility pack per progetti sve',
+      long_description=open('README.rst').read(),
+      url='https://pypi.python.org/pypi/sve_common_tools',
+      license='MIT',
+      packages=['sve_common_tools'],
+      zip_safe=False,
+      install_requires=['paramiko>=1.10.0']
+      )
