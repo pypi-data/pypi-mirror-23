abe598d02f38419f1ba42e8c4eaf7343fa519ba9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:12 +0000

    Add package pypolyglot/0.7/pypolyglot-0.7-py2.7.egg

---
+++ packages/pypolyglot/pypolyglot-0.7-py2.7.egg/polyglot/__init__.py
@@ -0,0 +1,7 @@
+import utKit
+import cl_utils
+from printpdf import printpdf
+from authenticate import authenticate
+from htmlCleaner import htmlCleaner
+from ebook import ebook
+from kindle import kindle
