d20d7b5081b79a53478a57d3ee8d34d86df2416a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:56:39 +0000

    Add package libsasscompiler/0.1.4/libsasscompiler-0.1.4.tar.gz

---
+++ packages/libsasscompiler/libsasscompiler-0.1.4.tar.gz/libsasscompiler-0.1.4/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+
+setup(name='libsasscompiler',
+      version='0.1.4',
+      description='django pipeline scss/sass compiler, no needed of ruby',
+      url='https://github.com/sonic182/libsasscompiler',
+      author='Johanderson Mogollon',
+      author_email='johanderson@mogollon.com.ve',
+      license='MIT',
+      packages=['libsasscompiler'],
+      install_requires=[
+          'libsass<=1.0.0',
+          'django-pipeline<=1.7.0'
+      ],
+      extras_require={
+          'test': [
+              'pytest<=3.2.0',
+              'pytest-pep8<=1.8.0',
+              'pytest-cov<=2.6.0',
+              'django<=1.12.0',
+              'django-pipeline<=1.7.0'
+          ]
+      },
+      zip_safe=False)
