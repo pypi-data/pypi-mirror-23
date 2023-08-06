7272e2f98d3ef8e94ae9f85c9a077419b7e9ed03
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:56:27 +0000

    Add package proliantutils/2.3.1/proliantutils-2.3.1.tar.gz

---
+++ packages/proliantutils/proliantutils-2.3.1.tar.gz/proliantutils-2.3.1/proliantutils/redfish/resources/system/constants.py
@@ -0,0 +1,52 @@
+# Copyright 2017 Hewlett Packard Enterprise Development LP
+#
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+# Push power button action constants
+
+PUSH_POWER_BUTTON_PRESS = 'press'
+PUSH_POWER_BUTTON_PRESS_AND_HOLD = 'press and hold'
+
+# BIOS Settings boot mode constants
+
+BIOS_BOOT_MODE_LEGACY_BIOS = 'legacy bios'
+BIOS_BOOT_MODE_UEFI = 'uefi'
+
+# Persistent boot device for set
+
+BOOT_SOURCE_TARGET_CD = 'Cd'
+BOOT_SOURCE_TARGET_PXE = 'Pxe'
+BOOT_SOURCE_TARGET_UEFI_TARGET = 'UefiTarget'
+BOOT_SOURCE_TARGET_HDD = 'Hdd'
+
+# BIOS Sriov constants
+
+SRIOV_ENABLED = 'sriov enabled'
+SRIOV_DISABLED = 'sriov disabled'
+
+# Secure Boot current boot constants
+
+SECUREBOOT_CURRENT_BOOT_ENABLED = 'enabled'
+SECUREBOOT_CURRENT_BOOT_DISABLED = 'disabled'
+
+# Secure Boot reset keys constants
+
+SECUREBOOT_RESET_KEYS_DEFAULT = 'default'
+SECUREBOOT_RESET_KEYS_DELETE_ALL = 'delete all'
+SECUREBOOT_RESET_KEYS_DELETE_PK = 'delete pk'
+
+TPM_PRESENT_ENABLED = 'Tpm present enabled'
+TPM_PRESENT_DISABLED = 'Tpm present disabled'
+TPM_NOT_PRESENT = 'Tpm not present'
---
+++ packages/proliantutils/proliantutils-2.3.1.tar.gz/proliantutils-2.3.1/proliantutils/redfish/resources/system/system.py
@@ -0,0 +1,183 @@
+# Copyright 2017 Hewlett Packard Enterprise Development LP
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+__author__ = 'HPE'
+
+import sushy
+from sushy.resources import base
+from sushy.resources.system import system
+
+from proliantutils import exception
+from proliantutils import log
+from proliantutils.redfish.resources.system import bios
+from proliantutils.redfish.resources.system import mappings
+from proliantutils.redfish.resources.system import pci_device
+from proliantutils.redfish.resources.system import secure_boot
+from proliantutils.redfish import utils
+
+
+LOG = log.get_logger(__name__)
+
+PERSISTENT_BOOT_DEVICE_MAP = {
+    'CDROM': sushy.BOOT_SOURCE_TARGET_CD,
+    'NETWORK': sushy.BOOT_SOURCE_TARGET_PXE,
+    'ISCSI': sushy.BOOT_SOURCE_TARGET_UEFI_TARGET,
+    'HDD': sushy.BOOT_SOURCE_TARGET_HDD
+}
+
+
+class PowerButtonActionField(base.CompositeField):
+    allowed_values = base.Field('PushType@Redfish.AllowableValues',
+                                adapter=list)
+
+    target_uri = base.Field('target', required=True)
+
+
+class HpeActionsField(base.CompositeField):
+    computer_system_ext_powerbutton = (
+        PowerButtonActionField('#HpeComputerSystemExt.PowerButton'))
+
+
+class HPESystem(system.System):
+    """Class that extends the functionality of System resource class
+
+    This class extends the functionality of System resource class
+    from sushy
+    """
+
+    model = base.Field(['Model'])
+    rom_version = base.Field(['Oem', 'Hpe', 'Bios', 'Current',
+                             'VersionString'])
+    _hpe_actions = HpeActionsField(['Oem', 'Hpe', 'Actions'], required=True)
+    """Oem specific system extensibility actions"""
+
+    _bios_settings = None  # ref to BIOSSettings instance
+    _secure_boot = None  # ref to SecureBoot instance
+    _pci_devices = None
+
+    def _get_hpe_push_power_button_action_element(self):
+        push_action = self._hpe_actions.computer_system_ext_powerbutton
+        if not push_action:
+            raise exception.MissingAttributeError(
+                attribute='Oem/Hpe/Actions/#HpeComputerSystemExt.PowerButton',
+                resource=self.path)
+
+        return push_action
+
+    def push_power_button(self, target_value):
+        """Reset the system in hpe exclusive manner.
+
+        :param target_value: The target value to be set.
+        :raises: InvalidInputError, if the target value is not
+            allowed.
+        :raises: SushyError, on an error from iLO.
+        """
+        if target_value not in mappings.PUSH_POWER_BUTTON_VALUE_MAP_REV:
+            msg = ('The parameter "%(parameter)s" value "%(target_value)s" is '
+                   'invalid. Valid values are: %(valid_power_values)s' %
+                   {'parameter': 'target_value', 'target_value': target_value,
+                    'valid_power_values': (
+                        mappings.PUSH_POWER_BUTTON_VALUE_MAP_REV.keys())})
+            raise exception.InvalidInputError(msg)
+
+        value = mappings.PUSH_POWER_BUTTON_VALUE_MAP_REV[target_value]
+        target_uri = (
+            self._get_hpe_push_power_button_action_element().target_uri)
+
+        self._conn.post(target_uri, data={'PushType': value})
+
+    @property
+    def bios_settings(self):
+        """Property to provide reference to `BIOSSettings` instance
+
+        It is calculated once when the first time it is queried. On refresh,
+        this property gets reset.
+        """
+        if self._bios_settings is None:
+            self._bios_settings = bios.BIOSSettings(
+                self._conn, utils.get_subresource_path_by(self, 'Bios'),
+                redfish_version=self.redfish_version)
+
+        return self._bios_settings
+
+    def update_persistent_boot(self, devices=[], persistent=False,
+                               mac=None):
+        """Changes the persistent boot device order in BIOS boot mode for host
+
+        Note: It uses first boot device from the devices and ignores rest.
+
+        :param devices: ordered list of boot devices
+        :param persistent: Boolean flag to indicate if the device to be set as
+                           a persistent boot device
+        :param mac: intiator mac address, mandotory for iSCSI uefi boot
+        :raises: IloError, on an error from iLO.
+        :raises: IloInvalidInputError, if the given input is not valid.
+        """
+        device = PERSISTENT_BOOT_DEVICE_MAP.get(devices[0].upper())
+
+        if device == sushy.BOOT_SOURCE_TARGET_UEFI_TARGET:
+            if not mac:
+                msg = ('Mac is needed for uefi iscsi boot')
+                raise exception.IloInvalidInputError(msg)
+
+            try:
+                uefi_boot_string = (self.bios_settings.boot_settings.
+                                    get_uefi_boot_string(mac))
+            except sushy.exceptions.SushyError:
+                msg = ('The BIOS Boot Settings was not found.')
+                raise exception.IloError(msg)
+
+            uefi_boot_settings = {
+                'Boot': {'UefiTargetBootSourceOverride': uefi_boot_string}
+            }
+            self._conn.patch(self.path, data=uefi_boot_settings)
+        elif device is None:
+            device = sushy.BOOT_SOURCE_TARGET_NONE
+
+        tenure = (sushy.BOOT_SOURCE_ENABLED_CONTINUOUS
+                  if persistent else sushy.BOOT_SOURCE_ENABLED_ONCE)
+        self.set_system_boot_source(device, enabled=tenure)
+
+    @property
+    def pci_devices(self):
+        """Provides the collection of PCI devices
+
+        It is calculated once when the first time it is queried. On refresh,
+        this property gets reset.
+        """
+        if self._pci_devices is None:
+            self._pci_devices = pci_device.PCIDeviceCollection(
+                self._conn, utils.get_subresource_path_by(
+                    self, ['Oem', 'Hpe', 'Links', 'PCIDevices']))
+        return self._pci_devices
+
+    @property
+    def secure_boot(self):
+        """Property to provide reference to `SecureBoot` instance
+
+        It is calculated once when the first time it is queried. On refresh,
+        this property gets reset.
+        """
+        if self._secure_boot is None:
+            self._secure_boot = secure_boot.SecureBoot(
+                self._conn, utils.get_subresource_path_by(self, 'SecureBoot'),
+                redfish_version=self.redfish_version)
+
+        return self._secure_boot
+
+    def refresh(self):
+        super(HPESystem, self).refresh()
+        self._bios_settings = None
+        self._pci_devices = None
+        self._secure_boot = None
