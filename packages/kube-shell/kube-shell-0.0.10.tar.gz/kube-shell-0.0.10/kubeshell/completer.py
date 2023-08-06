8cbdc6d566ffdb9ae5d41b09d1d4eea9b3f07f51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:16 +0000

    Add package kube-shell/0.0.10/kube-shell-0.0.10.tar.gz

---
+++ packages/kube-shell/kube-shell-0.0.10.tar.gz/kube-shell-0.0.10/kubeshell/completer.py
@@ -0,0 +1,284 @@
+from subprocess import check_output
+from prompt_toolkit.completion import Completer, Completion
+from fuzzyfinder import fuzzyfinder
+import shlex
+import json
+import os
+import os.path
+from kubernetes import client, config
+
+class KubectlCompleter(Completer):
+
+    def __init__(self):
+        self.all_commands = []
+        self.all_args = []
+        self.all_opts = []
+        self.global_opts = []
+        try:
+            DATA_DIR = os.path.dirname(os.path.realpath(__file__))
+            DATA_PATH = os.path.join(DATA_DIR, 'data/cli.json')
+            with open(DATA_PATH) as json_file:
+                self.kubectl_dict = json.load(json_file)
+            self.populate_cmds_args_opts(self.kubectl_dict)
+        except Exception as ex:
+            print "got an exception" + ex.message
+
+    def populate_cmds_args_opts(self, key_map):
+        for key in key_map.keys():
+            self.all_commands.append(key)
+            self.all_args.extend(key_map[key]['args'])
+            if key == "kubectl":
+                for opt in key_map[key]['options'].keys():
+                    self.global_opts.append(opt)
+            else:
+                for opt in key_map[key]['options'].keys():
+                    self.all_opts.append(opt)
+
+            self.populate_cmds_args_opts(key_map[key]['subcommands'])
+
+    # Below is the grammer of how kubectl expects and parses argument, commands and options
+    # kubectl (global option)* command (global option | local option)* (subcommand (global option | local option)*)* (arg) (global option | local option)*
+    # This method parse command, args, global flags, local flags from the text before cursor and suggest based on that return state,
+    # inner most command seen, arg if any, and pass the dict corresponding to the command
+    #
+    # states:
+    #
+    #   INIT: this is starting state. In this state only 'kubectl' is expected as token
+    #
+    #   KUBECTL: State when first token is 'kubectl' and need to handle second token. In this state only commands of
+    #           kubectl or one or more global options can be specified
+    #
+    #   KUBECTL_CMD: State representing case where we have received 'kubectl' and a 'command'. In this state either args, subcommands
+    #           global options, or local options specifc to the commands can be specified
+    #
+    #   KUBECTL_ARG: In this state only global or local options for the commands can be specified. And also
+    #          name os the resource like pods can be specified .We will reach this
+    #          state from KUBCTL_CMD when we parse arg for a command
+    #
+    #   KUBECTL_LEAF: In this state only global or local options for the commands can be specified. We will reach this
+    #          state from KUBCTL_CMD when we longer have any sub-commands or args for the previous command
+    #
+    def parse_tokens(self, cmdline):
+        tokens = []
+        if cmdline is not None:
+            cmdline = cmdline.strip()
+            tokens = shlex.split(cmdline)
+
+        state = "INIT"
+        index = 0
+        key_map = self.kubectl_dict
+        last_token = False
+
+        command = ""
+        arg = ""
+        for token in tokens:
+            if state == "INIT" and tokens[0] == "kubectl":
+                state = "KUBECTL"
+                key_map = key_map['kubectl']
+                command = "kubectl"
+                continue
+            elif state == "KUBECTL":
+                if token.startswith("--"):
+                    continue
+                if token in key_map['subcommands'].keys():
+                    state = "KUBECTL_CMD"
+                    key_map = key_map['subcommands'][token]
+                    command = token
+                continue
+            elif state == "KUBECTL_CMD":
+                if token.startswith("--"):
+                    continue
+                if token in key_map['subcommands'].keys():
+                    state = "KUBECTL_CMD"
+                    key_map = key_map['subcommands'][token]
+                    command = token
+                    continue
+                if token in key_map['args']:
+                    state = "KUBECTL_ARG"
+                    arg = token
+                    continue
+                continue
+            elif state == "KUBECTL_ARG":
+                if token.startswith("--"):
+                    continue
+                resources = self.get_resources(arg, "default")
+                for resource_name, namespace in resources:
+                    if token == resource_name:
+                        state = "KUBECTL_LEAF"
+                        continue
+                continue
+            elif state == "KUBECTL_LEAF":
+                if token.startswith("--"):
+                    continue
+                continue
+        return state, command, arg, key_map
+
+    def get_completions(self, document, complete_event, smart_completion=None):
+
+        word_before_cursor = document.get_word_before_cursor(WORD=True)
+
+        tokens = []
+        cmdline = document.text_before_cursor
+        if cmdline is not None:
+            cmdline = cmdline.strip()
+            try:
+                tokens = shlex.split(cmdline)
+            except:
+                return
+
+        state, command, arg, key_map = self.parse_tokens(cmdline)
+
+        if state == "INIT":
+            if len(tokens) == 0:
+                yield Completion("kubectl", start_position=0, display="kubectl", display_meta=key_map['kubectl']['help'])
+                return
+            if len(tokens) == 1 and word_before_cursor == tokens[0]:
+                suggestions = fuzzyfinder(tokens[0], ['kubectl'])
+                for suggestion in suggestions:
+                    yield Completion(suggestion, -len(tokens[0]), display="kubectl", display_meta=self.kubectl_dict['kubectl']['help'])
+        elif state == "KUBECTL":
+            last_token = tokens[-1]
+            if word_before_cursor == last_token:
+                if last_token.startswith("--"):
+                    if last_token in self.global_opts:
+                        return
+                    global_opt_suggestions = fuzzyfinder(word_before_cursor, self.global_opts)
+                    for global_opt in global_opt_suggestions:
+                        help_msg = self.kubectl_dict['kubectl']['options'][global_opt]['help']
+                        yield Completion(global_opt, -len(word_before_cursor), display_meta=help_msg)
+                else:
+                    suggestions = fuzzyfinder(last_token, key_map['subcommands'].keys())
+                    for suggestion in suggestions:
+                        yield Completion(suggestion, -len(last_token), display=suggestion, display_meta=key_map['subcommands'][suggestion]['help'])
+            if word_before_cursor == "":
+                for cmd in key_map['subcommands'].keys():
+                    yield Completion(cmd, display=cmd, display_meta=key_map['subcommands'][cmd]['help'])
+        elif state == "KUBECTL_CMD":
+            last_token = tokens[-1]
+            if word_before_cursor == last_token:
+                if last_token.startswith("--"):
+                    if last_token in self.global_opts or last_token in key_map['options'].keys():
+                        return
+                    else:
+                        command_opts = fuzzyfinder(word_before_cursor, key_map['options'].keys())
+                        for command_opt in command_opts:
+                            help_msg = key_map['options'][command_opt]['help']
+                            yield Completion(command_opt, -len(word_before_cursor), display=command_opt, display_meta=help_msg)
+                        global_opt_suggestions = fuzzyfinder(word_before_cursor, self.global_opts)
+                        for global_opt in global_opt_suggestions:
+                            help_msg = self.kubectl_dict['kubectl']['options'][global_opt]['help']
+                            yield Completion(global_opt, -len(word_before_cursor), display=global_opt, display_meta=help_msg)
+                elif last_token != command:
+                    subcommands = key_map['subcommands'].keys()
+                    if len(subcommands) > 0 and last_token not in subcommands:
+                        suggestions = fuzzyfinder(last_token, subcommands)
+                        for suggestion in suggestions:
+                            yield Completion(suggestion, -len(last_token), display=suggestion, display_meta=key_map['subcommands'][suggestion]['help'])
+                    args = key_map['args']
+                    if len(args) > 0 and last_token not in args:
+                        suggestions = fuzzyfinder(last_token, args)
+                        for arg in suggestions:
+                            yield Completion(arg, -len(last_token))
+            elif word_before_cursor == "":
+                subcommands = key_map['subcommands'].keys()
+                for subcommand in subcommands:
+                    yield Completion(subcommand, display=subcommand, display_meta=key_map['subcommands'][subcommand]['help'])
+                args = key_map['args']
+                for arg in args:
+                    yield Completion(arg)
+        elif state == "KUBECTL_ARG":
+            if word_before_cursor == "":
+                for resourceName, namespace in self.get_resources(arg, "default"):
+                    yield Completion(resourceName, display=resourceName, display_meta=namespace)
+        elif state == "KUBECTL_LEAF":
+            last_token = tokens[-1]
+            if last_token.startswith("--"):
+                if last_token in self.global_opts or last_token in key_map['options'].keys():
+                    return
+                else:
+                    command_opts = fuzzyfinder(word_before_cursor, key_map['options'].keys())
+                    for command_opt in command_opts:
+                        help_msg = key_map['options'][command_opt]['help']
+                        yield Completion(command_opt, -len(word_before_cursor), display=command_opt, display_meta=help_msg)
+                    global_opt_suggestions = fuzzyfinder(word_before_cursor, self.global_opts)
+                    for global_opt in global_opt_suggestions:
+                        help_msg = self.kubectl_dict['kubectl']['options'][global_opt]['help']
+                        yield Completion(global_opt, -len(word_before_cursor), display=global_opt, display_meta=help_msg)
+        else:
+            pass
+        return
+
+    def get_resources(self, resource, namespace):
+        names = []
+        config.load_kube_config()
+
+        v1 = client.CoreV1Api()
+        v1Beta1 = client.AppsV1beta1Api()
+        extensionsV1Beta1 = client.ExtensionsV1beta1Api()
+        autoscalingV1Api = client.AutoscalingV1Api()
+        rbacAPi = client.RbacAuthorizationV1beta1Api()
+        batchV1Api = client.BatchV1Api()
+        batchV2Api = client.BatchV2alpha1Api()
+
+        if resource == "pod":
+            ret = v1.list_pod_for_all_namespaces(watch=False)
+        elif resource == "service":
+            ret = v1.list_service_for_all_namespaces(watch=False)
+        elif resource == "deployment":
+            ret = v1Beta1.list_deployment_for_all_namespaces(watch=False)
+        elif resource == "statefulset":
+            ret = v1Beta1.list_stateful_set_for_all_namespaces(watch=False)
+        elif resource == "node":
+            ret = v1.list_node(watch=False)
+        elif resource == "namespace":
+            ret = v1.list_namespace(watch=False)
+        elif resource == "daemonset":
+            ret = extensionsV1Beta1.list_daemon_set_for_all_namespaces(watch=False)
+        elif resource == "networkpolicy":
+            ret = extensionsV1Beta1.list_network_policy_for_all_namespaces(watch=False)
+        elif resource == "thirdpartyresource":
+            ret = extensionsV1Beta1.list_third_party_resource(watch=False)
+        elif resource == "replicationcontroller":
+            ret = v1.list_replication_controller_for_all_namespaces(watch=False)
+        elif resource == "replicaset":
+            ret = extensionsV1Beta1.list_replica_set_for_all_namespaces(watch=False)
+        elif resource == "ingress":
+            ret = extensionsV1Beta1.list_ingress_for_all_namespaces(watch=False)
+        elif resource == "endpoints":
+            ret = v1.list_endpoints_for_all_namespaces(watch=False)
+        elif resource == "configmap":
+            ret = v1.list_config_map_for_all_namespaces(watch=False)
+        elif resource == "event":
+            ret = v1.list_event_for_all_namespaces(watch=False)
+        elif resource == "limitrange":
+            ret = v1.list_limit_range_for_all_namespaces(watch=False)
+        elif resource == "configmap":
+            ret = v1.list_config_map_for_all_namespaces(watch=False)
+        elif resource == "persistentvolume":
+            ret = v1.list_persistent_volume(watch=False)
+        elif resource == "secret":
+            ret = v1.list_secret_for_all_namespaces(watch=False)
+        elif resource == "resourcequota":
+            ret = v1.list_resource_quota_for_all_namespaces(watch=False)
+        elif resource == "componentstatus":
+            ret = v1.list_component_status(watch=False)
+        elif resource == "podtemplate":
+            ret = v1.list_pod_template_for_all_namespaces(watch=False)
+        elif resource == "serviceaccount":
+            ret = v1.list_service_account_for_all_namespaces(watch=False)
+        elif resource == "horizontalpodautoscaler":
+            ret = autoscalingV1Api.list_horizontal_pod_autoscaler_for_all_namespaces(watch=False)
+        elif resource == "clusterrole":
+            ret = rbacAPi.list_cluster_role(watch=False)
+        elif resource == "clusterrolebinding":
+            ret = rbacAPi.list_cluster_role_binding(watch=False)
+        elif resource == "job":
+            ret = batchV1Api.list_job_for_all_namespaces(watch=False)
+        elif resource == "cronjob":
+            ret = batchV2Api.list_cron_job_for_all_namespaces(watch=False)
+        elif resource == "scheduledjob":
+            ret = batchV2Api.list_scheduled_job_for_all_namespaces(watch=False)
+        for i in ret.items:
+            names.append((i.metadata.name, i.metadata.namespace))
+        return names
+
---
+++ packages/kube-shell/kube-shell-0.0.10.tar.gz/kube-shell-0.0.10/kubeshell/kubeshell.py
@@ -0,0 +1,60 @@
+from __future__ import unicode_literals, print_function
+
+from prompt_toolkit import prompt
+from prompt_toolkit.history import FileHistory
+from prompt_toolkit.auto_suggest import AutoSuggestFromHistory
+from pygments.style import Style
+from pygments.token import Token
+from pygments.styles.default import DefaultStyle
+
+from style import StyleFactory
+from completer import KubectlCompleter
+from lexer import KubectlLexer
+
+import os
+import click
+import sys
+import subprocess
+
+
+class DocumentStyle(Style):
+    styles = {
+        Token.Menu.Completions.Completion.Current: 'bg:#00aaaa #000000',
+        Token.Menu.Completions.Completion: 'bg:#008888 #ffffff',
+        Token.Menu.Completions.ProgressButton: 'bg:#003333',
+        Token.Menu.Completions.ProgressBar: 'bg:#00aaaa',
+    }
+    styles.update(DefaultStyle.styles)
+
+
+def get_title():
+    return "kube-shell"
+
+class Kubeshell(object):
+
+    def __init__(self, refresh_resources=True):
+        self.history = FileHistory(os.path.expanduser('~/.kube-shell/history'))
+        if not os.path.exists(os.path.expanduser("~/.kube-shell/")):
+            os.makedirs(os.path.expanduser("~/.kube-shell/"))
+        self.completer = KubectlCompleter()
+
+    def run_cli(self):
+        while True:
+            user_input = prompt('kube-shell> ',
+                        history=self.history,
+                        auto_suggest=AutoSuggestFromHistory(),
+                        style=StyleFactory("vim").style,
+                        lexer=KubectlLexer,
+                        get_title=get_title,
+                        enable_history_search=False,
+                        vi_mode=True,
+                        completer=self.completer)
+            if user_input == "clear":
+                click.clear()
+            elif user_input == "exit":
+                sys.exit()
+            if user_input:
+                if '-o' in user_input and 'json' in user_input:
+                    user_input = user_input + ' | pygmentize -l json'
+                p = subprocess.Popen(user_input, shell=True)
+                p.communicate()
