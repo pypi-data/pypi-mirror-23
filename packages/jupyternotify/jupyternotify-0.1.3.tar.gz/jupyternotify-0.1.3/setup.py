77eb1b9ac08c627c2c85981149ac4e7dddcbe7a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:27:55 +0000

    Add package jupyternotify/0.1.3/jupyternotify-0.1.3.tar.gz

---
+++ packages/jupyternotify/jupyternotify-0.1.3.tar.gz/jupyternotify-0.1.3/setup.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+
+from setuptools import find_packages, setup
+
+with open('README.rst') as f:
+    readme = f.read()
+
+with open('LICENSE.txt') as f:
+    license = f.read()
+
+setup(
+    name='jupyternotify',
+    version='0.1.3',
+    description='A Jupyter Notebook %%magic for Browser Notifications of Cell Completion',
+    long_description=readme,
+    author='Michelangelo D\'Agostino',
+    author_email='mdagostino@shoprunner.com',
+    url='https://github.com/shoprunner/jupyter-notify',
+    license=license,
+    packages=find_packages(exclude=('tests', 'docs')),
+    package_data={'jupyternotify': ['js/*.js']},
+    install_requires=[
+        'ipython',
+        'jupyter'
+    ]
+)
