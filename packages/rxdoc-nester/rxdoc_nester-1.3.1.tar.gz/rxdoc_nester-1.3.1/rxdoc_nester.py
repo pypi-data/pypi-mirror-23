3416777d950778b4b7541f37ff651b0d2e149cda
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:56:01 +0000

    Add package rxdoc-nester/1.3.1/rxdoc_nester-1.3.1.tar.gz

---
+++ packages/rxdoc-nester/rxdoc_nester-1.3.1.tar.gz/rxdoc_nester-1.3.1/rxdoc_nester.py
@@ -0,0 +1,24 @@
+'''Simple recursive function that takes a list (which may or may not 
+    contain one or more levels of nested lists) as an argument and will 
+    print each individual element of all levels of a nested list'''
+    
+def print_lol(the_list, indent = False, level = 0):
+	'''will check to see if an item is a list and if so process
+		each item in that list so that the end result is a line by line
+		printout of all items of all nested levels of the parent list.
+		Indent parameter allows the printed list to show nested levels
+		and is set to off by default.  The level of indent can be 
+		changed to customize list output use a negative level to ignore
+		1 or more levels of nesting in your printout or a positive 
+		number to increase all indented levels'''	
+		
+	for each_item in the_list:
+		if isinstance(each_item, list):
+			print_lol(each_item, indent, level + 1)
+		else:
+			if indent == True:
+				for tab_stop in range(level):
+					print("\t", end="")
+				print(each_item)
+			else:
+				print(each_item)
