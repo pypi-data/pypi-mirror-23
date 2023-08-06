e7184566d5437c37e42ffa57e1f9880996067ec6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:53 +0000

    Add package envs/1.2.1/envs-1.2.1.tar.gz

---
+++ packages/envs/envs-1.2.1.tar.gz/envs-1.2.1/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+from pip.req import parse_requirements
+
+install_reqs = parse_requirements('requirements.txt', session=False)
+
+setup(
+    name='envs',
+    description='Easy access of environment variables from Python with support for strings, booleans, list, tuples, and dicts.',
+    url='https://github.com/bjinwright/envs',
+    author='Brian Jinwright',
+    license='GNU GPL v3',
+    keywords='environment variables',
+    install_requires=[str(ir.req) for ir in install_reqs],
+    packages=find_packages(),
+    py_modules=['envs.cli'],
+    include_package_data=True,
+    zip_safe=True,
+    version='1.2.1',
+    entry_points='''
+        [console_scripts]
+        envs=envs.cli:envs
+        ''',
+)
+
