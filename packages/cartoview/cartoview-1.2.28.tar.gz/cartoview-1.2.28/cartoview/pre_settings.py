0f614e3308f8694969ae449cb2d3dabaea43fb3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:49 +0000

    Add package cartoview/1.2.28/cartoview-1.2.28.tar.gz

---
+++ packages/cartoview/cartoview-1.2.28.tar.gz/cartoview-1.2.28/cartoview/pre_settings.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+import os
+# os.environ['Path'] = r'C:\Cartoview\GDAL;'+ os.environ['Path']
+# os.environ['GEOS_LIBRARY_PATH'] = r'C:\Cartoview\GDAL\geos_c.dll'
+# os.environ['GDAL_LIBRARY_PATH'] = r'C:\Cartoview\GDAL\gdal111.dll'
