f216e6df14d1bfef9ca854ad817c3b9590a5b2bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:35:56 +0000

    Add package Ciw/1.1.2/Ciw-1.1.2.tar.gz

---
+++ packages/Ciw/Ciw-1.1.2.tar.gz/Ciw-1.1.2/ciw/exit_node.py
@@ -0,0 +1,36 @@
+from __future__ import division
+
+class ExitNode(object):
+    """
+    Class for the exit node on our network
+    """
+    def __init__(self):
+        """
+        Initialise a node.
+        """
+        self.all_individuals = []
+        self.id_number = -1
+        self.next_event_date = float("Inf")
+        self.node_capacity = float("Inf")
+        self.number_completed = 0
+
+    def __repr__(self):
+        """
+        Representation of a node.
+        """
+        return 'Exit Node'
+
+    def accept(self, next_individual, current_time):
+        """
+        Adds customer to the list of completed customers
+        """
+        self.all_individuals.append(next_individual)
+        self.number_completed += 1
+
+    def update_next_event_date(self):
+        """
+        Finds the time of the next event at this node
+        Just passes as next_event_date always set to
+        the max_simulation_time
+        """
+        pass
