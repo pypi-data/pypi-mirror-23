0dc0e36216e47788a31ebf5dd12640c951d505d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:08:44 +0000

    Add package opentools/0.0.0/opentools-0.0.0.tar.gz

---
+++ packages/opentools/opentools-0.0.0.tar.gz/opentools-0.0.0/setup.py
@@ -0,0 +1,16 @@
+# setup.py
+
+from setuptools import setup, find_packages
+
+setup(name='opentools',
+      version='0.0.0',
+      description='opentools',
+      classifiers=[
+          "Programming Language :: Python",
+      ],
+      author='Lukas Kubis',
+      long_description='''darkskylib
+==========''',
+      author_email='contact@lukaskubis.com',
+      packages=find_packages(),
+      )
