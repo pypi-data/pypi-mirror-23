cbc1f990289e307adbbd101e2add4a75b8121c8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:32:35 +0000

    Add package gfxlcd/0.8.1/gfxlcd-0.8.1.tar.gz

---
+++ packages/gfxlcd/gfxlcd-0.8.1.tar.gz/gfxlcd-0.8.1/gfxlcd/driver/null/null_page.py
@@ -0,0 +1,44 @@
+"""Null Page test chip driver"""
+from gfxlcd.drawing.page import Page
+from gfxlcd.abstract.chip import Chip
+
+
+class NullPage(Page, Chip):
+    """Test chip driver for page drawing"""
+    def __init__(self, width, height, driver, auto_flush=True):
+        Chip.__init__(self, width, height, driver, auto_flush)
+        Page.__init__(self, driver)
+        self.rotation = 0
+        self.buffer = []
+        self.area = {
+            'start_x': 0,
+            'start_y': 0,
+            'end_x': width-1,
+            'end_y': height-1
+        }
+
+    def init(self):
+        """inits a device"""
+        Page.init(self)
+        Chip.init(self)
+
+    def _convert_color(self, color):
+        """convert color to avaiable one"""
+        if color['R'] == 0 and color['G'] == 0 and color['B'] == 0:
+            return 0
+
+        return 1
+
+    def flush(self, force=None):
+        """flush buffer to device
+        :force - boolean|None"""
+        return self.buffer
+
+    def set_area(self, pos_x1, pos_y1, pos_x2, pos_y2):
+        """set area to work on"""
+        self.area = {
+            'start_x': pos_x1,
+            'start_y': pos_y1,
+            'end_x': pos_x2,
+            'end_y': pos_y2
+        }
