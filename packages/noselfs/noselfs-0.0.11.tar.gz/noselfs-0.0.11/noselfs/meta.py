67f0a0dc61954c7617fe05e62dcfff8859aca039
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:29:23 +0000

    Add package noselfs/0.0.11/noselfs-0.0.11.tar.gz

---
+++ packages/noselfs/noselfs-0.0.11.tar.gz/noselfs-0.0.11/noselfs/meta.py
@@ -0,0 +1,17 @@
+__version__ = "0.0.11"
+"""
+Changelog:
+0.0.11: Minor Bug fix relating to very small test files. Now searches- correctly
+0.0.10: Minor Bug fix relating to very small test files. Now searches.
+0.0.9: Return the original function result through the decorator.
+0.0.8: Updated the testbase class to handle relative paths correctly
+0.0.7: Testbase remote fix
+0.0.6: Versioning weirdness
+0.0.5: Fix rel path error
+0.0.4: Add Base test class for sandboxed tests
+0.0.3: Change to the way lfs looks for files- has a greater chance of finding
+    them now.
+0.0.2: Change order of arguments- allows users to put they tests in more
+    places, i.e.doesn't restrict to data/'type' anymore
+0.0.1: Initial version. Probably buggy
+"""
