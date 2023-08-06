b4e1aaab2973a5d79abd3d9efcc3e993eb26ced6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:35:52 +0000

    Add package gdpy3/0.1.8/gdpy3-0.1.8-py3.6.egg

---
+++ packages/gdpy3/gdpy3-0.1.8-py3.6.egg/gdpy3/plot/enginelib/__init__.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2017 shmilee
+
+r'''
+    plot engines
+'''
+
+import logging
+from .mplwrapper import mplengine
+
+__all__ = ['engine_available', 'get_engine']
+
+log = logging.Logger('gdp')
+
+engine_available = [
+    'mpl', 'matplotlib',
+]
+default_engine = 'matplotlib'
+
+
+def get_engine(name):
+    '''
+    Return a Engine instance by name
+    '''
+
+    if name not in engine_available:
+        log.error("Plot engine '%s' not found in '%s'! Use default '%s'!"
+                  % (name, engine_available, default_engine))
+        name = default_engine
+    if name in ('mpl', 'matplotlib'):
+        log.debug("Use plot engine 'matplotlib'.")
+        return mplengine
+    else:
+        pass
+
+
+def style_available(engine=default_engine):
+    '''
+    Return a list of available styles in Engine *engine*.
+    '''
+    if engine not in engine_available:
+        log.error("Plot engine '%s' not found in '%s'!"
+                  % (engine, engine_available))
+        return []
+    if engine in ('mpl', 'matplotlib'):
+        return mplengine.style_available
+    else:
+        pass
