349eb08695e97ea5f7bb6b3b49fc7836675034df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:08:57 +0000

    Add package quantgo-api/1.0.8/quantgo-api-1.0.8.zip

---
+++ packages/quantgo-api/quantgo-api-1.0.8.zip/quantgo-api-1.0.8/bin/qg_cli.bat
@@ -0,0 +1,3 @@
+@echo off
+python -c "import sys; import quantgo_api.cli as cli; args = sys.argv[1:]; sys.exit(cli.main(args));" %*
+EXIT %ERRORLEVEL%
---
+++ packages/quantgo-api/quantgo-api-1.0.8.zip/quantgo-api-1.0.8/quantgo_api/qgservice.py
@@ -0,0 +1,52 @@
+"""
+QuantGo services definitions
+"""
+
+EQUITIES_TAQ = {
+                "name": "EQUITIES_TAQ",
+                "bucket": "us-equity-taq",
+                "type": "equities",
+                "file_structure": 0
+               }
+
+EQUITIES_1MIN = {
+                 "name": "EQUITIES_1MIN",
+                 "bucket": "us-equity-1min",
+                 "type": "equities",
+                 "file_structure": 0
+               }
+
+EQUITIES_TRADES = {
+                   "name": "EQUITIES_TRADES",
+                   "bucket": "us-equity-trades",
+                   "type": "equities",
+                   "file_structure": 0
+                   }
+
+EQUITIES_1MIN_TRADES = {
+                        "name": "EQUITIES_1MIN_TRADES",
+                        "bucket": "us-equity-1min",
+                        "type": "equities",
+                        "file_structure": 0
+                        }
+
+FUTURES_TAQ = {
+               "name": "FUTURES_TAQ",
+               "bucket": "us-futures-taq",
+               "type": "futures",
+               "file_structure": 1
+               }
+
+FUTURES_1MIN = {
+                "name": "FUTURES_1MIN",
+                "bucket": "us-futures-1min",
+                "type": "futures",
+                "file_structure": 1
+               }
+
+FUTURES_1MIN_TRADES = {
+                       "name": "FUTURES_1MIN_TRADES",
+                       "bucket": "us-futures-1min",
+                       "type": "futures",
+                       "file_structure": 1
+                       }
