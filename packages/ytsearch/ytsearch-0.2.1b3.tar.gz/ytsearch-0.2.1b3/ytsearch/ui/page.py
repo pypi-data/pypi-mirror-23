8acd96836172a8209b1ac76eeb5298d46d756b6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:09:04 +0000

    Add package ytsearch/0.2.1b3/ytsearch-0.2.1b3.tar.gz

---
+++ packages/ytsearch/ytsearch-0.2.1b3.tar.gz/ytsearch-0.2.1b3/ytsearch/ui/page.py
@@ -0,0 +1,121 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+from ytsearch import settings
+
+import urwid
+
+
+class Page:
+
+    """
+    The base class for all of the page widget sections.
+    All of the keypress actions that are the same in all of the pages
+    are defined here
+    """
+
+    video_list = None
+    results = []
+    title = ''
+    widgets = None
+    keybuffer = []
+
+    def __init__(self, parent):
+        self.parent = parent
+
+    def load_page(self):
+        """
+        The default action for loading the page, which returns a blank text.
+
+        :return: urwid.Filler: The filler around the empty text
+        """
+        return urwid.Filler(urwid.Text(''))
+
+    def update_video(self, video):
+        """
+        Find and update a certain video widget.
+
+        :video: ytsearch.Video: The video to find and update.
+        :return: None
+        """
+        if video not in self.results:
+            return None
+        index = self.results.index(video)
+        self.walker[index] = self.parent.create_video_widget(video)
+        return None
+
+    def event_MOVE_DOWN(self, index):
+        if self.video_list is not None and index < len(self.video_list.body) - 1:
+            self.video_list.set_focus(index + 1, 'above')
+        return None
+
+    def event_MOVE_UP(self, index):
+        if self.video_list is not None and index > 0:
+            self.video_list.set_focus(index - 1, 'below')
+        return None
+
+    def event_PLAY_AUDIO(self, index):
+        if index is None:
+            return None
+        video = self.results[index]
+        self.parent.play(video, audio=True)
+        self.event_MOVE_DOWN(index)
+        return None
+
+    def event_PLAY_VIDEO(self, index):
+        if index is None:
+            return None
+        video = self.results[index]
+        move = self.parent.play(video, audio=False)
+        if move:
+            self.event_MOVE_DOWN(index)
+        return None
+
+    def event_QUEUE_AUDIO(self, index):
+        if index is None:
+            return None
+
+        video = self.results[index]
+        video.audio = True
+        self.parent.queue_add(video)
+        self.event_MOVE_DOWN(index)
+        return None
+
+    def event_QUEUE_VIDEO(self, index):
+        if index is None:
+            return None
+
+        video = self.results[index]
+        video.audio = False
+        self.parent.queue_add(video)
+        self.event_MOVE_DOWN(index)
+        return None
+
+    def event_PLAYLIST_ADD(self, index):
+        if index is None or self.parent.playlist_add is None:
+            return None
+
+        video = self.results[index]
+        playlist_name = self.parent.playlist_add
+        data = {'name': video.name, 'location': video.location, 'resource':
+                video._resource, 'cache': video.cache}
+        if data in self.parent.playlists[playlist_name]:
+            return None
+
+        self.parent.playlists[playlist_name].append(data)
+        self.parent.set_video_status(video, 'Added')
+        return None
+
+    def event_DOWNLOAD_VIDEO(self, index):
+        if index is None:
+            return None
+
+        video = self.results[index]
+        self.parent.download_video(video)
+        return None
+
+    def event_NEXT_VIDEO(self, _):
+        if self.parent.playing is not None:
+            self.parent.playing.stop()
+        return None
