f8babb3001c3325d9926c7c434657192164851bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:46:08 +0000

    Add package sqre-labkubespawner/0.0.8/sqre_labkubespawner-0.0.8-py3-none-any.whl

---
+++ packages/sqre-labkubespawner/sqre_labkubespawner-0.0.8-py3-none-any.whl/sqre_labkubespawner/singleuser.py
@@ -0,0 +1,27 @@
+import os
+from jupyterlab.labhubapp import SingleUserLabApp
+
+
+class SQRESingleUserLabApp(SingleUserLabApp):
+
+    def init_webapp(self, *args, **kwargs):
+        super().init_webapp(*args, **kwargs)
+        settings = self.web_app.settings
+        if 'page_config_data' not in settings:
+            self.web_app.settings['page_config_data'] = {}
+        settings['page_config_data']['LSST'] = 'LSST SQuaRE'
+        if 'token' not in settings['page_config_data']:
+            settings['page_config_data']['token'] = ''
+        if settings['page_config_data']['token'] == '':
+            api_token = os.getenv('JUPYTERHUB_API_TOKEN')
+            if api_token:
+                self.token = api_token
+                settings['page_config_data']['token'] = api_token
+
+
+def main(argv=None):
+    return SQRESingleUserLabApp.launch_instance(argv)
+
+
+if __name__ == "__main__":
+    main()
