b92b960810d97a5827f1febd3fa2f2d0a8e15783
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:11:59 +0000

    Add package chall-cube/1.0.1/chall-cube-1.0.1.tar.gz

---
+++ packages/chall-cube/chall-cube-1.0.1.tar.gz/chall-cube-1.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='chall-cube',
+    version='1.0.1',
+    description='Base API for RaspberryPi challenges cube.',
+    url='http://github.com/alcalyn/chall-cube',
+    author='Alcalyn',
+    author_email='doubjulien@hotmail.fr',
+    license='AGPL-3.0',
+    packages=find_packages('.'),
+    zip_safe=False
+)
