ced9222438ed75b5e2c1f8e18ba389127d088cdd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 09:52:53 +0000

    Add package skyersfirstpythonmodule/1.0.0/skyersfirstpythonmodule-1.0.0.tar.gz

---
+++ packages/skyersfirstpythonmodule/skyersfirstpythonmodule-1.0.0.tar.gz/skyersfirstpythonmodule-1.0.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup (
+    name			= 'skyersfirstpythonmodule',
+    version         = '1.0.0',
+    py_modules  	= ['skyersfirstpythonmodule'],
+    author          = 'skyer',
+    author_email  	= '503612012@qq.com',
+    url   			= 'http://www.headfirstlabs.com',
+    description     = 'A simple printer of nested lists',
+    )
