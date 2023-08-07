01b7e8688bbd28e5969b43e7d40a01132bca91be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-07 09:52:47 +0000

    Add package python-to-json/1.0.1/python-to-json-1.0.1.tar.gz

---
+++ packages/python-to-json/python-to-json-1.0.1.tar.gz/python-to-json-1.0.1/setup.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+setup(
+    name='python-to-json',
+    version='1.0.1',
+    author='Ingresso',
+    author_email='systems@ingresso.co.uk',
+    url='https://github.com/ingresso-group/python-to-json/',
+    license='MIT',
+    description='Module for serializing python objects to json',
+    install_requires=[],
+    packages=['python_to_json'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 2.7',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+    ],
+)
